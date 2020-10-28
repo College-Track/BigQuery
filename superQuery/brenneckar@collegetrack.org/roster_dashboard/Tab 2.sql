a878407e1fa1a6a37dd56aa8696775af9ae7e605
Baker Renneckar <baker@bakerr.com>
2020-10-28 18:20:59 -0400

    Auto-commit: Wed Oct 28 2020 18:20:52 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
@@ -0,0 +1,44 @@
+WITH current_as AS (
+  SELECT
+    Contact_Id,
+    On_Track__c
+  FROM
+    `data-warehouse-289815.sfdc_templates.contact_at_template`
+  WHERE
+    Current_AS__c = True
+    AND College_Track_Status__c IN ('15A')
+),
+previous_as AS (
+  SELECT
+    Contact_Id,
+    On_Track__c
+  FROM
+    `data-warehouse-289815.sfdc_templates.contact_at_template`
+  WHERE
+    Previous_AS__c = True
+    AND College_Track_Status__c IN ('15A')
+),
+prev_prev_as AS (
+  SELECT
+    Contact_Id,
+    On_Track__c
+  FROM
+    `data-warehouse-289815.sfdc_templates.contact_at_template`
+  WHERE
+    Prev_Prev_As__c = True
+    AND College_Track_Status__c IN ('15A')
+),
+joined_on_track AS (
+  SELECT
+    CS.Contact_Id,
+    CS.On_Track__c AS current_as_on_track,
+    PS.On_Track__c AS previous_as_on_track,
+    PPS.On_Track__c AS prev_prev_as_on_track
+  FROM
+    current_as CS
+    LEFT JOIN previous_as PS ON PS.Contact_Id = CS.Contact_ID
+    LEFT JOIN prev_prev_as PPS ON PPS.Contact_Id = CS.Contact_ID
+)
+
+SELECT *
+FROM joined_on_track
