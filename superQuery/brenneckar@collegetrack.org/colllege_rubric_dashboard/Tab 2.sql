2777e78632fd2ae6141ee0fadac6cc6a57e43990
Baker Renneckar <baker@bakerr.com>
2020-10-29 17:45:40 -0400

    Auto-commit: Thu Oct 29 2020 17:45:34 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 2.sql
@@ -27,12 +27,22 @@ WITH contact_at AS (
       WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "Y" THEN 2
       WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "R" THEN 1
       ELSE NULL
-    END AS question_finance_Loans_score
+    END AS question_finance_Loans_score,
   FROM
     `data-warehouse-289815.sfdc_templates.contact_at_template`
 )
 SELECT
-  *
+  *,
+  case
+    when question_finance_Loans_score IS NOT NULL then 1
+    else 0
+  end + case
+    when question_finance_Filing_Status_score IS NOT NULL then 1
+    else 0
+  end + case
+    when question_finance_Financial_Aid_Package_score IS NOT NULL then 1
+    else 0
+  end AS total
 FROM
   contact_at
 LIMIT
