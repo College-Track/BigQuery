01b0cddbfea8e9d472ec9379b5bbc4da80b56538
Baker Renneckar <baker@bakerr.com>
2020-10-30 15:58:48 -0400

    Auto-commit: Fri Oct 30 2020 15:58:40 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/3_college_rubric_filtered.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/3_college_rubric_filtered.sql
@@ -301,8 +301,8 @@ joined_data AS (
 )
 SELECT
   Contact_Id,
-  Financial_Aid_Package__c,
-  Filing_Status__c,
+--   Financial_Aid_Package__c,
+--   Filing_Status__c,
   Loans__c,
     --  question_finance_Financial_Aid_Package_score,
     --  question_finance_Filing_Status_score,
