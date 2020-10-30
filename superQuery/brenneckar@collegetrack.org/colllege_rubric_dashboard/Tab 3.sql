e37e6fe4994d5cb84d69df0103167e3f0127fda5
Baker Renneckar <baker@bakerr.com>
2020-10-30 14:22:53 -0400

    Auto-commit: Fri Oct 30 2020 14:22:48 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 3.sql
@@ -1,6 +1,8 @@
 CREATE OR REPLACE FUNCTION `data-studio-260217.college_rubric.format_question_as_num`(question STRING)
 RETURNS FLOAT64
 LANGUAGE js AS """
+    if(question == null){return null}
+    
     answer = question.split("_").pop()
     if(answer == "G"){return 3}
     else{return 2}
