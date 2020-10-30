d9a079b1586e133b860e182625041eabfc381f5c
Baker Renneckar <baker@bakerr.com>
2020-10-30 14:24:38 -0400

    Auto-commit: Fri Oct 30 2020 14:24:31 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 2.sql
@@ -1,6 +1,7 @@
 WITH contact_at AS (
   SELECT
     Contact_Id,
+    Financial_Aid_Package__c,
     `data-studio-260217.college_rubric.format_question_as_num`(Financial_Aid_Package__c) as TEST,
     CASE
       WHEN Financial_Aid_Package__c IS NULL THEN NULL
@@ -10,22 +11,22 @@ WITH contact_at AS (
       WHEN SPLIT(Financial_Aid_Package__c, "_") [OFFSET(1)] = "R" THEN 1
       ELSE NULL
     END AS question_finance_Financial_Aid_Package_score,
-    CASE
-      WHEN Filing_Status__c IS NULL THEN NULL
-      WHEN Filing_Status__c = "N/A" THEN NULL
-      WHEN SPLIT(Filing_Status__c, "_") [OFFSET(1)] = "G" THEN 3
-      WHEN SPLIT(Filing_Status__c, "_") [OFFSET(1)] = "Y" THEN 2
-      WHEN SPLIT(Filing_Status__c, "_") [OFFSET(1)] = "R" THEN 1
-      ELSE NULL
-    END AS question_finance_Filing_Status_score,
-    CASE
-      WHEN Loans__c IS NULL THEN NULL
-      WHEN Loans__c = "N/A" THEN NULL
-      WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "G" THEN 3
-      WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "Y" THEN 2
-      WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "R" THEN 1
-      ELSE NULL
-    END AS question_finance_Loans_score,
+    -- CASE
+    --   WHEN Filing_Status__c IS NULL THEN NULL
+    --   WHEN Filing_Status__c = "N/A" THEN NULL
+    --   WHEN SPLIT(Filing_Status__c, "_") [OFFSET(1)] = "G" THEN 3
+    --   WHEN SPLIT(Filing_Status__c, "_") [OFFSET(1)] = "Y" THEN 2
+    --   WHEN SPLIT(Filing_Status__c, "_") [OFFSET(1)] = "R" THEN 1
+    --   ELSE NULL
+    -- END AS question_finance_Filing_Status_score,
+    -- CASE
+    --   WHEN Loans__c IS NULL THEN NULL
+    --   WHEN Loans__c = "N/A" THEN NULL
+    --   WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "G" THEN 3
+    --   WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "Y" THEN 2
+    --   WHEN SPLIT(Loans__c, "_") [OFFSET(1)] = "R" THEN 1
+    --   ELSE NULL
+    -- END AS question_finance_Loans_score,
   FROM
     `data-warehouse-289815.sfdc_templates.contact_at_template`
   WHERE
@@ -34,16 +35,16 @@ WITH contact_at AS (
 )
 SELECT
   *,
-  case
-    when question_finance_Loans_score IS NOT NULL then 1
-    else 0
-  end + case
-    when question_finance_Filing_Status_score IS NOT NULL then 1
-    else 0
-  end + case
-    when question_finance_Financial_Aid_Package_score IS NOT NULL then 1
-    else 0
-  end AS total
+--   case
+--     when question_finance_Loans_score IS NOT NULL then 1
+--     else 0
+--   end + case
+--     when question_finance_Filing_Status_score IS NOT NULL then 1
+--     else 0
+--   end + case
+--     when question_finance_Financial_Aid_Package_score IS NOT NULL then 1
+--     else 0
+--   end AS total
 FROM
   contact_at
 LIMIT
