13b881efc3f4390120a049b8bcf5416ccace14a3
Baker Renneckar <baker@bakerr.com>
2020-10-30 15:17:40 -0400

    Auto-commit: Fri Oct 30 2020 15:17:34 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 2.sql
@@ -1,8 +1,13 @@
-SELECT Contact_Id,
+WITH filter AS (SELECT Contact_Id,
 question_finance_Financial_Aid_Package_score,
 question_finance_Filing_Status_score,
 question_academic_Study_Resources_score,
-`data-studio-260217.college_rubric.calc_num_valid_questions`(TO_JSON_STRING(data), 'question_finance') AS TEST
-FROM `data-studio-260217.college_rubric.filtered_college_rubric` as data
+
+FROM `data-studio-260217.college_rubric.filtered_college_rubric`
 WHERE question_finance_Financial_Aid_Package_score IS NOT NULL
+)
+
+SELECT *,
+`data-studio-260217.college_rubric.calc_num_valid_questions`(TO_JSON_STRING(filter), 'question_finance') AS TEST
+FROM filter
 LIMIT 10
