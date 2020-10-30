05059584cf4d290ea099a9d0b3b63872dc939b5e
Baker Renneckar <baker@bakerr.com>
2020-10-30 19:38:57 -0400

    Auto-commit: Fri Oct 30 2020 19:38:48 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 6.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 6.sql
@@ -1,3 +1,3 @@
-SELECT Affiliation__c, Affiliation_Record_ID__c
+SELECT Affiliation__c, Affiliation_Record_ID__c, Affiliation__c == Affiliation_Record_ID__c
 FROM `data-warehouse-289815.salesforce_raw.Academic_Semester__c`
 LIMIT 1000
