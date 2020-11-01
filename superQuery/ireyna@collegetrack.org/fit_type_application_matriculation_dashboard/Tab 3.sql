ce5f71eef7fbdf329545bd28f3fdc72b7f2aef2b
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-01 15:00:36 -0800

    Auto-commit: Sun Nov 01 2020 15:00:28 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 3.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 3.sql
@@ -1,4 +1,4 @@
 SELECT * 
 FROM `data-warehouse-289815.salesforce_raw.College_Application__c`
 WHERE Type_of_School__c= "4 Year"
-AND Student__c = "0034600001TQsl3AAD"
+AND Student__c = "0034600001TQy4eAAD"
