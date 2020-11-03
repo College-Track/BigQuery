b287bace2a2fdfee257657b7d127adf824fa4fe5
Baker Renneckar <baker@bakerr.com>
2020-11-03 16:59:43 -0500

    Auto-commit: Tue Nov 03 2020 16:59:36 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/r/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/r/Tab 1.sql
@@ -1,2 +1,5 @@
-SELECT * 
-FROM `data-warehouse-289815.roles.role_table`
+INSERT `data-warehouse-289815.roles.role_table` (Role, Role_Id, Site)
+VALUES ('National','00E46000000YaSLEA0', 'College Track Crenshaw'),
+      ('Los Angeles Region','00E46000000YcNmEAK', 'College Track Crenshaw'),
+      ('CEO','00E46000000p9b8EAA', 'College Track Crenshaw'),
+      ('Program - Crenshaw','2F00E1M000001e0u3', 'College Track Crenshaw')
