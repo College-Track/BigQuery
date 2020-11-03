5fdf4041bc7b22ee9919e54aa45e60fe63926bd5
Baker Renneckar <baker@bakerr.com>
2020-11-03 17:59:15 -0500

    Auto-commit: Tue Nov 03 2020 17:59:08 GMT-0500 (Eastern Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 3.sql
@@ -0,0 +1,18 @@
+SELECT
+  * 
+FROM
+  `data-studio-260217.rosters.filtered_roster` AS Master
+RIGHT JOIN (
+  SELECT
+    roles.site_short,
+    roles.role
+  FROM
+    `data-warehouse-289815.salesforce_raw.User` AS Users
+  LEFT JOIN
+    `data-warehouse-289815.roles.role_table` AS Roles
+  ON
+    Roles.Role_Id=Users.UserRoleId
+  WHERE
+    LOWER(users.Email)=LOWER(SESSION_USER()) ) AS Permissions
+ON
+  Master.site_short=Permissions.site_short
