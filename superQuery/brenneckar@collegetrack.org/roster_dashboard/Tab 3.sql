caf217b2dfcf63bece8c15e1069e0cd73be4fd77
Baker Renneckar <baker@bakerr.com>
2020-11-03 18:05:04 -0500

    Auto-commit: Tue Nov 03 2020 18:05:00 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 3.sql
@@ -1,5 +1,5 @@
 SELECT
-  master.site_short, COUNT(Contact_Id) as cnt
+  *
 FROM
   `data-studio-260217.rosters.filtered_roster` AS Master
 RIGHT JOIN (
@@ -16,6 +16,3 @@ RIGHT JOIN (
     LOWER(users.Email)=LOWER(SESSION_USER()) ) AS Permissions
 ON
   Master.site_short=Permissions.site_short
-  
-GROUP BY master.site_short
-ORDER BY cnt
