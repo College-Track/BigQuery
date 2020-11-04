e0f54024a2c3ba388088d6097f7bf670407115c3
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:17:43 -0500

    Auto-commit: Wed Nov 04 2020 17:17:38 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
@@ -5,7 +5,7 @@ WITH name_score AS (
   FROM
     `data-warehouse-289815.salesforce_raw.Class_Attendance__c`)
   SELECT
-    *
+    Id
   FROM
     name_score
     CROSS JOIN UNNEST(name_score.Dosage) AS score;
