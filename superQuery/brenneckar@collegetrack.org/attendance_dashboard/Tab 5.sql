e8e305283a669ff602730e39d11c9130811e24b2
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:45:03 -0500

    Auto-commit: Wed Nov 04 2020 17:44:56 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,4 +1,3 @@
-SELECT COUNT(Id), Workshop_Global_Academic_Year__c
+SELECT Id
 FROM `data-warehouse-289815.salesforce_raw.Class_Attendance__c`
-WHERE Workshop_Dosage_Type__c IS NULL 
-GROUP BY Workshop_Global_Academic_Year__c
+WHERE Workshop_Dosage_Type__c IS NULL
