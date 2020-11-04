f4d9172f7b1d01423dcf859cd1ff6b451c8477b0
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:31:58 -0500

    Auto-commit: Wed Nov 04 2020 17:31:51 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(Id), Academic_Semester__c
 FROM `data-warehouse-289815.salesforce_raw.Class_Attendance__c`
-WHERE Workshop_Dosage_Type__c IS NULL AND Academic_Semester__c = 'a3646000000dMXhAAM'
+WHERE Workshop_Dosage_Type__c IS NULL AND Academic_Semester__c = 'a3646000000dMXiAAM'
 GROUP BY Academic_Semester__c
