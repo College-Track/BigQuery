091f406944bf1427b9565a1e4be0f28f08798b31
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:52:32 -0500

    Auto-commit: Wed Nov 04 2020 17:52:23 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,5 @@
-SELECT Id
-FROM `data-warehouse-289815.salesforce.class_attendance_c`
-WHERE Id ='a2z1M000000dHjaQAE'
+SELECT WSA.Id, W.dosage_types_c
+FROM `data-warehouse-289815.salesforce.class_attendance_c` WSA
+LEFT JOIN `data-warehouse-289815.salesforce.class_registration_c` WE ON WE.Id = WSA.workshop_enrollment_c
+LEFT JOIN `data-warehouse-289815.salesforce.class_c` W ON W.Id = WE.class_c
+WHERE WSA.Id ='a2z1M000000dHjaQAE'
