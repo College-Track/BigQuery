46356249e31a3359913d08899b86dca20da16bf2
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:17:12 -0500

    Auto-commit: Wed Nov 04 2020 17:17:07 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
@@ -1,11 +1,11 @@
 WITH name_score AS (
   SELECT
     Id,
-    SPLIT(Workshop_Dosage_Type__c, ';') AS Dosage
+    SPLIT(Workshop_Dosage_Type__c, ';') AS nested_dosage
   FROM
     `data-warehouse-289815.salesforce_raw.Class_Attendance__c`)
   SELECT
-    Id, 
+    Id,dosage
   FROM
     name_score
     CROSS JOIN UNNEST(name_score.Dosage) AS dosage;
