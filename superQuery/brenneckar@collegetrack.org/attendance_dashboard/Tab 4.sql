b3d0a86886077fd7851d9c980aa9de15315cf09a
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:26:22 -0500

    Auto-commit: Wed Nov 04 2020 17:26:17 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
@@ -1,13 +1,14 @@
 WITH name_score AS (
   SELECT
     Id,
-    SPLIT(Workshop_Dosage_Type__c, ';') AS dosage_combined
+    SPLIT(Workshop_Dosage_Type__c, ';') AS dosage_combined,
+    Attendance_Numerator__c
   FROM
     `data-warehouse-289815.salesforce_raw.Class_Attendance__c`)
+     LIMIT
+    10
   SELECT
-    Id, dosage_split
+    Id, dosage_split,Attendance_Numerator__c
   FROM
     name_score
-    CROSS JOIN UNNEST(name_score.dosage_combined) AS dosage_split;
-  LIMIT
-    10
+    CROSS JOIN UNNEST(name_score.dosage_combined) AS dosage_split
