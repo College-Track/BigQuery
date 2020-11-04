b1d4d3344059ab05477db1a222e4e43e5e40bbd3
Baker Renneckar <baker@bakerr.com>
2020-11-04 17:48:30 -0500

    Auto-commit: Wed Nov 04 2020 17:48:26 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
 SELECT Id, Workshop_Dosage_Type__c
-FROM `data-warehouse-289815.salesforce.Class_Attendance__c`
+FROM `data-warehouse-289815.salesforce.class_attendance_c`
 WHERE Id ='a2z1M000000dHjaQAE'
