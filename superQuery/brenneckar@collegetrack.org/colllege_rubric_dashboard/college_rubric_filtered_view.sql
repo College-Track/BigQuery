98935aa74543f2c03a80cf3cbd0398a64ad0d6b6
Baker Renneckar <baker@bakerr.com>
2020-11-04 14:45:25 -0500

    Auto-commit: Wed Nov 04 2020 14:45:16 GMT-0500 (Eastern Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/college_rubric_filtered_view.sql
@@ -0,0 +1,18 @@
+ SELECT
+  *
+FROM
+  `data-studio-260217.college_rubric.filtered_college_rubric` AS Master
+RIGHT JOIN (
+  SELECT
+    roles.site_short,
+    roles.role
+  FROM
+    `data-warehouse-289815.salesforce_raw.User` AS Users
+  LEFT JOIN
+    `data-warehouse-289815.roles.role_table` AS Roles
+  ON
+    Roles.Role_Id=Users.UserRoleId
+  WHERE
+    LOWER(users.Email)=LOWER(SESSION_USER())) AS Permissions
+ON
+  Master.site_short=Permissions.site_short
