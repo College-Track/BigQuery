408f4b33a673d3da22e78cfe97475c8d25736ed2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-04 22:48:49 -0800

    Auto-commit: Wed Nov 04 2020 22:48:45 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 3.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 3.sql
@@ -20,7 +20,7 @@ SELECT
         ON term.Affiliation_Record_ID__c = aff.Id
 
     --WHERE term.Indicator_Completed_CT_HS_Program__c = TRUE
-        AND term.Indicator_Years_Since_HS_Grad_to_Date__c IN (.33,.25) #Fall Year 1 term
+        WHERE term.Indicator_Years_Since_HS_Grad_to_Date__c IN (.33,.25) #Fall Year 1 term
         --High_School_Class IN (2016, 2017, 2018, 2019, 2020) 
         --AND matri.RecordTypeId = "01246000000RNnTAAW" #College/University
         --AND Predominant_Degree_Awarded__c = "Predominantly bachelor's-degree granting"
