ed882b6b25ebf6ab1f2ded4578f24d59c4d380f8
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-05 13:53:55 -0800

    Auto-commit: Thu Nov 05 2020 13:53:51 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 11.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 11.sql
@@ -3,4 +3,4 @@ SELECT aff.Fit_Type__c AS fit_type_affiliation,
     aff.Fit_Type_Current__c,
     aff.Situational_Fit_Type__c
 FROM `data-warehouse-289815.salesforce_raw.npe5__Affiliation__c` AS aff
-WHERE id = "a0H1M000014P00eUAC"
+WHERE aff.id = "a0H1M000014P00eUAC"
