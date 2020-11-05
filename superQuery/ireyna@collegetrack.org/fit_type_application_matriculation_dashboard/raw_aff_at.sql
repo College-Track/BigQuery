d809853c7ce988e4fcaa08fed9326174e3aa35ee
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-05 10:57:07 -0800

    Auto-commit: Thu Nov 05 2020 10:57:01 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/raw_aff_at.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/raw_aff_at.sql
@@ -4,13 +4,13 @@ SELECT
     term.Affiliation__c,
     aff.id,
     term.Name,
-    term.Fit_Type__c
+    term.Fit_Type__c,
     aff.Fit_Type__c AS fit_type_affiliation,
     aff.Best_Fit_Applied__c AS fit_type_start_of_affiliation,
     aff.Fit_Type_Current__c,
     aff.Situational_Fit_Type__c,
     --aff.Situational_Best_Fit_Context__c,
-    npe5__Organization__c AS Affiliation_School_id,
+    npe5__Organization__c AS Affiliation_School_id
     
 
 FROM `data-warehouse-289815.salesforce_raw.Academic_Semester__c` as term
