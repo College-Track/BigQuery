2b014d39572d77dd5f3448fac904b3fe4b1884ff
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-04 19:13:30 -0800

    Auto-commit: Wed Nov 04 2020 19:13:26 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 3.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 3.sql
@@ -1,4 +1,4 @@
-SELECT COUNT(contact_id) AS COUNT_STUDENT, High_School_Class, College_Fit_Type_Applied__c, college_app_id
+SELECT COUNT(contact_id) AS COUNT_STUDENT, contact_id, Full_Name__c, High_School_Class, College_Fit_Type_Applied__c, college_app_id
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
 WHERE College_Fit_Type_Applied__c = "Best Fit" AND High_School_Class >= 2016
-GROUP BY High_School_Class, College_Fit_Type_Applied__c, college_app_id
+GROUP BY contact_id, Full_Name__c, High_School_Class, College_Fit_Type_Applied__c, college_app_id
