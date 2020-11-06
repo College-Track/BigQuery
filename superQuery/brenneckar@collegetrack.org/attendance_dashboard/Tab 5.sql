f80dcb4c7935242d44e08fd11110548c628952e9
Baker Renneckar <baker@bakerr.com>
2020-11-06 15:49:14 -0500

    Auto-commit: Fri Nov 06 2020 15:49:09 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
-WHERE Workshop_Display_Name__c = 'Domine's Cohort F2' AND Date__c = "2020-10-09"
+WHERE Workshop_Display_Name__c = "Domine's Cohort F2" AND Date__c = "2020-10-09"
