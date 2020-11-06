62927d4eecfef8074d687e8ab4fe3ce6601fe9e1
Baker Renneckar <baker@bakerr.com>
2020-11-06 15:03:49 -0500

    Auto-commit: Fri Nov 06 2020 15:03:38 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/3_attendance_filtered_data.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/3_attendance_filtered_data.sql
@@ -6,48 +6,48 @@ WITH gather_data AS(
     WSA.Attendance_Numerator__c,
     WSA.Attendance_Denominator__c,
     0 AS group_count,
-    WSA.Class_Session__c,
-    -- W.Class__c,
-    WSA.Student__c,
-    CAT.Full_Name__c,
-    WSA.Date__c,
-    WSA.Outcome__c,
-    WSA.Workshop_Department__c,
-    WSA.Workshop_Display_Name__c,
-    WSA.Student_Site__c,
-    WSA.Student_High_School_Class__c,
-    WSA.Attendance_Excluded__c,
-    CAT.Indicator_High_Risk_for_Dismissal__c,
-    CAT.Indicator_Low_Income__c,
-    CAT.Ethnic_background__c,
-    CAT.First_Generation__c,
-    CAT.GPA_Bucket_running_cumulative__c,
-    CAT.College_Track_Status__c,
-    -- status.Status,
-    WSA.Academic_Semester__c,
-    REPLACE(
-      CAT.GAS_Name,
-      ' (Semester)',
-      ''
-    ) AS Workshop_Global_Academic_Semester__c,
-    CAT.Indicator_Student_on_Intervention__c,
-    CAT.GPA_prev_semester_cumulative__c,
-    CAT.Composite_Readiness_Most_Recent__c,
-    CASE
-      WHEN CAT.GPA_prev_semester_cumulative__c < 2.5 THEN '2.49 or less'
-      WHEN CAT.GPA_prev_semester_cumulative__c >= 2.5
-      AND CAT.GPA_prev_semester_cumulative__c < 2.75 THEN '2.5 - 2.74'
-      WHEN CAT.GPA_prev_semester_cumulative__c >= 2.75
-      AND CAT.GPA_prev_semester_cumulative__c < 3 THEN '2.75 - 2.99'
-      WHEN CAT.GPA_prev_semester_cumulative__c >= 3
-      AND CAT.GPA_prev_semester_cumulative__c < 3.5 THEN '3.0 - 3.49'
-      ELSE '3.5 or Greater'
-    END GPA_Bucket,
-    CAT.site_abrev,
-    CAT.Indicator_Sem_Attendance_Above_80__c,
-    CAT.region,
-    CAT.region_abrev,
-    CAT.College_Track_Status_Name
+    -- WSA.Class_Session__c,
+    -- -- W.Class__c,
+    -- WSA.Student__c,
+    -- CAT.Full_Name__c,
+    -- WSA.Date__c,
+    -- WSA.Outcome__c,
+    -- WSA.Workshop_Department__c,
+    -- WSA.Workshop_Display_Name__c,
+    -- WSA.Student_Site__c,
+    -- WSA.Student_High_School_Class__c,
+    -- WSA.Attendance_Excluded__c,
+    -- CAT.Indicator_High_Risk_for_Dismissal__c,
+    -- CAT.Indicator_Low_Income__c,
+    -- CAT.Ethnic_background__c,
+    -- CAT.First_Generation__c,
+    -- CAT.GPA_Bucket_running_cumulative__c,
+    -- CAT.College_Track_Status__c,
+    -- -- status.Status,
+    -- WSA.Academic_Semester__c,
+    -- REPLACE(
+    --   CAT.GAS_Name,
+    --   ' (Semester)',
+    --   ''
+    -- ) AS Workshop_Global_Academic_Semester__c,
+    -- CAT.Indicator_Student_on_Intervention__c,
+    -- CAT.GPA_prev_semester_cumulative__c,
+    -- CAT.Composite_Readiness_Most_Recent__c,
+    -- CASE
+    --   WHEN CAT.GPA_prev_semester_cumulative__c < 2.5 THEN '2.49 or less'
+    --   WHEN CAT.GPA_prev_semester_cumulative__c >= 2.5
+    --   AND CAT.GPA_prev_semester_cumulative__c < 2.75 THEN '2.5 - 2.74'
+    --   WHEN CAT.GPA_prev_semester_cumulative__c >= 2.75
+    --   AND CAT.GPA_prev_semester_cumulative__c < 3 THEN '2.75 - 2.99'
+    --   WHEN CAT.GPA_prev_semester_cumulative__c >= 3
+    --   AND CAT.GPA_prev_semester_cumulative__c < 3.5 THEN '3.0 - 3.49'
+    --   ELSE '3.5 or Greater'
+    -- END GPA_Bucket,
+    -- CAT.site_abrev,
+    -- CAT.Indicator_Sem_Attendance_Above_80__c,
+    -- CAT.region,
+    -- CAT.region_abrev,
+    -- CAT.College_Track_Status_Name
   FROM
     `data-warehouse-289815.salesforce_raw.Class_Attendance__c` AS WSA
     LEFT JOIN `data-warehouse-289815.sfdc_templates.contact_at_template` CAT
