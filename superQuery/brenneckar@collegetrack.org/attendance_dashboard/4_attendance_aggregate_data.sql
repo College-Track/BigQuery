a760b58d718e1d54c407f244a2c77f674a046156
Baker Renneckar <baker@bakerr.com>
2020-11-06 15:59:14 -0500

    Auto-commit: Fri Nov 06 2020 15:59:06 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_aggregate_data.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_aggregate_data.sql
@@ -5,7 +5,7 @@ SELECT
   SUM(Attendance_Denominator__c) AS Attendance_Denominator,
   SUM(mod_numerator) AS mod_numerator,
   SUM(mod_denominator) AS mod_denominator,
---   Date__c,
+  Date__c,
   Student_High_School_Class__c,
   Workshop_Dosage_Type__c,
   dosage_split,
@@ -17,7 +17,7 @@ SELECT
   Composite_Readiness_Most_Recent__c,
   Region,
   region_abrev,
---   Outcome__c,
+  Outcome__c,
   COUNT(Student__c) AS record_count
 FROM
   `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
@@ -25,12 +25,11 @@ WHERE
   Attendance_Excluded__c = FALSE
   AND Workshop_Dosage_Type__c IS NOT NULL
   AND (mod_denominator > 0 OR mod_numerator > 0)
-  AND  Workshop_Display_Name__c = "Catch All Senior Office Hour - Komal"
 
 GROUP BY
   Student_Site__c,
   Workshop_Display_Name__c,
---   Date__c,
+  Date__c,
   Student_High_School_Class__c,
   Workshop_Dosage_Type__c,
   GPA_Bucket,
@@ -41,6 +40,6 @@ GROUP BY
   Composite_Readiness_Most_Recent__c,
   Region,
   region_abrev,
---   Outcome__c,
+  Outcome__c,
   dosage_split
   ORDER BY Workshop_Dosage_Type__c
