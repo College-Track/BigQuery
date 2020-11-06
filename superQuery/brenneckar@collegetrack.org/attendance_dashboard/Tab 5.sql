e6c449762966c3f6cf3bea426e427da5fa1fe628
Baker Renneckar <baker@bakerr.com>
2020-11-06 15:46:57 -0500

    Auto-commit: Fri Nov 06 2020 15:46:52 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
-SELECT Id
-FROM `data-warehouse-289815.salesforce.class_c`
-WHERE Id = 'a321M000000kkB9QAI'
+SELECT *
+FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
+WHERE Workshop_Display_Name__c = 'TH2A Coaching - Michelle'
