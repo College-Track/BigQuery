c36b89312bd686ccc20fe4f60ab2f8b091b7accc
Baker Renneckar <baker@bakerr.com>
2020-11-06 13:30:28 -0500

    Auto-commit: Fri Nov 06 2020 13:30:23 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
@@ -1,4 +1,4 @@
-WITH name_score AS (
+WITH gather_attendance AS (
   SELECT
     Id,
     Workshop_Dosage_Type__c,
@@ -7,18 +7,33 @@ WITH name_score AS (
     Attendance_Denominator__c
   FROM
     `data-warehouse-289815.salesforce_raw.Class_Attendance__c`
-    WHERE Current_AT__c = TRUE
+  WHERE
+    Current_AT__c = TRUE
   LIMIT
     1000
+), mod_dosage AS (
+  SELECT
+    Id,
+    Workshop_Dosage_Type__c,
+    dosage_split,
+    Attendance_Numerator__c AS mod_numerator,
+    Attendance_Denominator__c AS mod_denominator
+  FROM
+    gather_attendance
+    CROSS JOIN UNNEST(name_score.dosage_combined) AS dosage_split;
 )
 SELECT
-
-  Id,
-  Workshop_Dosage_Type__c,
-  dosage_split,
-  Attendance_Numerator__c AS mod_numerator,
-  Attendance_Denominator__c AS mod_denominator
- 
+  mod_dosage.Id,
+  gather_attendance.Workshop_Dosage_Type__c,
+  mod_dosage.dosage_split,
+  mod_dosage.mod_numerator,
+  mod_dosage_mod_denominator
 FROM
-  name_score
-  CROSS JOIN UNNEST(name_score.dosage_combined) AS dosage_split
+  (
+    SELECT
+      DISTINCT id
+    FROM
+      gather_attendance
+  ) AS all_names
+  CROSS JOIN date_month
+  LEFT JOIN mod_dosage ON all_names.id = mod_dosage.id
