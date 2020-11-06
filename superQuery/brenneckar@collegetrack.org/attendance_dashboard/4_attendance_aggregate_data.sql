2cfbaa9e49184decb681b0c0800cbd3086363fdd
Baker Renneckar <baker@bakerr.com>
2020-11-06 15:43:29 -0500

    Auto-commit: Fri Nov 06 2020 15:43:22 GMT-0500 (Eastern Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_aggregate_data.sql
@@ -0,0 +1,44 @@
+SELECT
+  Student_Site__c,
+  Workshop_Display_Name__c,
+  SUM(Attendance_Numerator__c) AS Attendance_Numerator,
+  SUM(Attendance_Denominator__c) AS Attendance_Denominator,
+  SUM(mod_numerator) AS mod_numerator,
+  SUM(mod_numerator) AS mod_denominator,
+  Date__c,
+  Student_High_School_Class__c,
+  Workshop_Dosage_Type__c,
+  dosage_split,
+  GPA_Bucket,
+  Indicator_Student_on_Intervention__c,
+  site_abrev,
+--   Class__c,
+  Workshop_Global_Academic_Semester__c,
+  Composite_Readiness_Most_Recent__c,
+  Region,
+  region_abrev,
+  Outcome__c,
+  COUNT(Student__c) AS record_count
+FROM
+  `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
+WHERE
+  Attendance_Excluded__c = FALSE
+  AND Workshop_Dosage_Type__c IS NOT NULL
+  AND (Attendance_Denominator__c > 0 OR Attendance_Numerator__c > 0)
+GROUP BY
+  Student_Site__c,
+  Workshop_Display_Name__c,
+  Date__c,
+  Student_High_School_Class__c,
+  Workshop_Dosage_Type__c,
+  GPA_Bucket,
+  Indicator_Student_on_Intervention__c,
+  site_abrev,
+  Workshop_Global_Academic_Semester__c,
+--   Class__c,
+  Workshop_Global_Academic_Semester__c,
+  Composite_Readiness_Most_Recent__c,
+  Region,
+  region_abrev,
+  Outcome__c,
+  dosage_split
