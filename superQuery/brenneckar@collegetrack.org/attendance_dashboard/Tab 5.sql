ccfc84150b78a10835b279ffc1c0f0f82dfb4a33
Baker Renneckar <baker@bakerr.com>
2020-11-09 14:31:18 -0500

    Auto-commit: Mon Nov 09 2020 11:31:14 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,4 @@
 SELECT WSA_Id, Workshop_Dosage_Type__c, dosage_split, Attendance_Numerator__c, Attendance_Denominator__c
 FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
 WHERE  Workshop_Global_Academic_Semester__c = 'Fall 2020-21' AND GPA_Bucket = "3.5 or Greater" AND Date__c < "2020-08-25" AND Workshop_Dosage_Type__c LIKE "%Student Life%"
+ORDER BY WSA_ID
