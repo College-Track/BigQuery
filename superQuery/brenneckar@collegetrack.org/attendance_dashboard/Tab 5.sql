7f117965083a42ebc31c58f23ad4ebf5a980633b
Baker Renneckar <baker@bakerr.com>
2020-11-09 15:52:31 -0500

    Auto-commit: Mon Nov 09 2020 12:52:28 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,4 +1,4 @@
-SELECT GPA_Bucket, SUM(Attendance_Numerator__c) AS Num, SUM(Attendance_Denominator__c) AS Den, SUM(Attendance_Numerator__c) / SUM(Attendance_Denominator__c) AS rate
+SELECT GPA_Bucket, SUM(Attendance_Numerator__c) AS Num, SUM(Attendance_Denominator__c) AS Den, ROUND(SUM(Attendance_Numerator__c) / SUM(Attendance_Denominator__c),2) AS rate
 FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
 WHERE  Workshop_Global_Academic_Semester__c = 'Fall 2020-21' 
 GROUP BY GPA_Bucket
