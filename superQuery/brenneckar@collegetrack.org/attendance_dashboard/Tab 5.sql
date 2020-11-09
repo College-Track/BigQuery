29139b644045299946cdc5bde6f57b762016f2a2
Baker Renneckar <baker@bakerr.com>
2020-11-09 12:37:44 -0500

    Auto-commit: Mon Nov 09 2020 09:37:40 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
-SELECT dosage_split, SUM(Attendance_Numerator__c) 
+SELECT TRIM(dosage_split), SUM(Attendance_Numerator__c) 
 FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
-GROUP BY dosage_split
+GROUP BY TRIM(dosage_split)
