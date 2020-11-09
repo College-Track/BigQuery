8824fde35d414b7653fa0ed043353a0428cdf233
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-09 13:04:14 -0800

    Auto-commit: Mon Nov 09 2020 13:04:11 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 12.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 12.sql
@@ -1,4 +1,4 @@
-SELECT High_School_Class,COUNT(High_School_Class)
-FROM `data-studio-260217.fit_type_pipeline.filtered_college_application`
-where High_School_Class >= 2016
-GROUP BY High_School_Class
+select High_School_Class, College_Fit_Type_Applied__c
+FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
+GROUP BY 
+High_School_Class, College_Fit_Type_Applied__c
