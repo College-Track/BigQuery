15533df159596cb2317f30344fa9e4a5f92e1f39
Baker Renneckar <baker@bakerr.com>
2020-11-09 12:27:44 -0500

    Auto-commit: Mon Nov 09 2020 09:27:37 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
-SELECT *
-FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
-WHERE Workshop_Display_Name__c = "Domine's Cohort F2" AND Date__c = "2020-10-09" AND GPA_Bucket = "2.75 - 2.99"
+SELECT dosage_split, SUM(Attendance_Numerator) 
+FROM `data-studio-260217.attendance_dashboard.tmp_aggregate_attendance`
+GROUP BY dosage_split
