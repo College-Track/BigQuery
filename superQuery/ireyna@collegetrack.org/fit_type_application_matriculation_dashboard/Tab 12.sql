e8c10168ea238e29a4d7a69243486e24540ab3f4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-09 13:04:31 -0800

    Auto-commit: Mon Nov 09 2020 13:04:26 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 12.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 12.sql
@@ -1,4 +1,4 @@
-select High_School_Class, College_Fit_Type_Applied__c
+select High_School_Class--, College_Fit_Type_Applied__c
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
 GROUP BY 
-High_School_Class, College_Fit_Type_Applied__c
+High_School_Class--, College_Fit_Type_Applied__c
