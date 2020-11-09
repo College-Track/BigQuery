8e4d6d04609f6ecfaa301943e9402c36d9193b98
Baker Renneckar <baker@bakerr.com>
2020-11-09 12:47:34 -0500

    Auto-commit: Mon Nov 09 2020 09:47:30 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 6.sql
@@ -0,0 +1,4 @@
+SELECT Outcome__c, SUM(Attendance_Numerator__c) NUM, SUM(Attendance_Denominator__c) DEN
+FROM `data-warehouse-289815.salesforce_raw.Class_Attendance__c`
+WHERE Workshop_Global_Academic_Semester__c ='Fall 2020-21 (Semester)' AND Workshop_Dosage_Type__c LIKE '%Math%'
+GROUP BY Outcome__c
