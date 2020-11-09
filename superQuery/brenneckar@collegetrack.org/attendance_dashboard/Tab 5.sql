aa73555afaec55fb8bc0f0c9cef4a472437a889f
Baker Renneckar <baker@bakerr.com>
2020-11-09 13:42:12 -0500

    Auto-commit: Mon Nov 09 2020 10:42:09 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,4 +1,4 @@
-SELECT dosage_split, SUM(mod_numerator), SUM(mod_denominator)
+SELECT GPA_Bucket, SUM(Attendance_Numerator__c), SUM(Attendance_Denominator__c)
 FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
-WHERE Outcome__c = 'Absent' AND Workshop_Dosage_Type__c LIKE "%Math%" AND Workshop_Global_Academic_Semester__c = 'Fall 2020-21'
-GROUP BY dosage_split
+WHERE  Workshop_Global_Academic_Semester__c = 'Fall 2020-21'
+GROUP BY GPA_Bucket
