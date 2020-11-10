3581b52c43b123c5a791580ef5be981c360e963f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-10 11:17:59 -0800

    Auto-commit: Tue Nov 10 2020 11:17:50 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,5 +1,37 @@
-SELECT  Readiness_Math_Official__c
-FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
+SELECT
 
-
-GROUP BY Readiness_Math_Official__c
+#pull in demographics, academics
+    contact_id,
+    Full_Name__c,
+    High_School_Class,
+    site_short,
+    region_short,
+    GPA_Cumulative__c AS CGPA_11th,
+    CASE
+      WHEN GPA_Cumulative__c <= 2.49 THEN "Below 2.5"
+      WHEN GPA_Cumulative__c < 2.75 THEN "2.5 - 2.74"
+      WHEN GPA_Cumulative__c < 3 THEN "2.75 - 2.99"
+      WHEN GPA_Cumulative__c < 3.25 THEN "3.0 - 3.24"
+      WHEN GPA_Cumulative__c >= 3.25 THEN "3.25+"
+      ELSE "Missing"
+    END AS CGPA_11th_bucket,
+    
+#pull in college application data
+    college_app_id,
+    app.college_id,
+    accnt.Name AS account_name,
+    Type_of_School__c,
+    Application_status__c,
+    admission_status__c,
+     CASE
+        WHEN admission_status__c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred") THEN "Acceptance"
+        ELSE "N/A"
+        END AS acceptance_group,
+    College_Fit_Type_Applied__c,
+    Fit_Type_Enrolled__c
+    
+    --Join with Account object to pull in name of School/College
+    FROM `data-studio-260217.fit_type_pipeline.filtered_college_application` AS app
+    LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` AS accnt
+        ON app.college_id = accnt.id
+    WHERE Indicator_Completed_CT_HS_Program__c = TRUE
