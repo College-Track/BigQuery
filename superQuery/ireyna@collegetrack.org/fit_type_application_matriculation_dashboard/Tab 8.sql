97e7aa912716d225458c2243a2c9cef759ca3839
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-09 19:00:31 -0800

    Auto-commit: Mon Nov 09 2020 19:00:26 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,4 +1,5 @@
-SELECT count(contact_id), site_short
+SELECT count(contact_id), site_short, High_School_Class
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
 where AT_Enrollment_Status__c in ("Full-time","Part-time", "Approved Gap Year")
-group by site_short
+AND site_short="Oakland"
+group by site_short, High_School_Class
