f83b663868442a498398c996f534bce0f462122b
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-10 10:40:27 -0800

    Auto-commit: Tue Nov 10 2020 10:40:23 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,3 +1,4 @@
 SELECT  Readiness_Composite_Off__c ,Readiness_English_Official__c, Readiness_Math_Official__c, act_sat_readiness
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
 WHERE act_sat_readiness IN ("No Score on File", "Not/Near Ready")
+GROUP BY readiness_Composite_Off__c ,Readiness_English_Official__c, Readiness_Math_Official__c, act_sat_readiness
