f339c1fe0d62b0914b60508b6a2f716813eb1cd9
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-09 20:26:21 -0800

    Auto-commit: Mon Nov 09 2020 20:26:17 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,3 +1,3 @@
-SELECT count(distinct contact_id), count(fit_type_affiliation) AS fit_aff, count(Fit_Type_Enrolled__c) AS fit_enrolled, count(College_Fit_Type_Applied__c) AS fit_applied, site_short
+SELECT count(distinct contact_id) AS id, count(fit_type_affiliation) AS fit_aff, count(Fit_Type_Enrolled__c) AS fit_enrolled, count(College_Fit_Type_Applied__c) AS fit_applied, site_short
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
 group by site_short
