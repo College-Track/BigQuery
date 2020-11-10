a33d84df8de48c3120f716441a8679fd73436337
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-10 10:39:06 -0800

    Auto-commit: Tue Nov 10 2020 10:39:01 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,4 +1,3 @@
-SELECT  count(distinct contact_id),count(contact_id), CGPA_11th_bucket
+SELECT  Readiness_Composite_Off__c ,Readiness_English_Official__c, Readiness_Math_Official__c, act_sat_readiness
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
-where CGPA_11th_bucket = "3.25+"
-GROUP BY CGPA_11th_bucket
+WHERE act_sat_readiness = "No Score on File"
