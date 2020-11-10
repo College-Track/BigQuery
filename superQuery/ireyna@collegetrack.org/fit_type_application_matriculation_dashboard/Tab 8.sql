d683524ccf186686d6baf968a341495491514b2f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-10 10:05:39 -0800

    Auto-commit: Tue Nov 10 2020 10:05:35 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,4 +1,4 @@
 SELECT  count(distinct contact_id),count(contact_id), CGPA_11th_bucket
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
-where CGPA_11th_bucket = "2.75 - 2.99"
+where CGPA_11th_bucket = "3.0 - 3.24"
 GROUP BY CGPA_11th_bucket
