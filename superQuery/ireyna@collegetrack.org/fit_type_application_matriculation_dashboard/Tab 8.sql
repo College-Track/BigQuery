628e088980fa7487671361505615189588335126
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-10 17:58:19 -0800

    Auto-commit: Tue Nov 10 2020 17:58:15 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,6 +1,5 @@
-SELECT
-  fit_type_affiliation 
- 
+SElect
+    CASE WHEN fit_type_affiliation IS NULL THEN "did not enroll"
+    ELSE fit_type_affiliation
+    END AS fit_type_affiliation
  FROm `data-studio-260217.fit_type_pipeline.aggregate_data`
-
-  WHERE fit_type_affiliation IS NULL
