966948fb65b1284c5f617a61b9c41588f2e64578
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-10 17:57:36 -0800

    Auto-commit: Tue Nov 10 2020 17:57:32 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 8.sql
@@ -1,16 +1,6 @@
 SELECT
-    
-    School__c,
-    School_Name,
-    
-    FROM `data-warehouse-289815.sfdc_templates.contact_at_template` AS term
- --Join with Affiliation object
-    FULL JOIN `data-warehouse-289815.salesforce_raw.npe5__Affiliation__c` AS aff
-        ON term.Affiliation_Record_ID__c = aff.Id
+  fit_type_affiliation 
+ 
+ FROm `data-studio-260217.fit_type_pipeline.aggregate_data`
 
-    WHERE term.Indicator_Completed_CT_HS_Program__c = TRUE
-    AND term.Indicator_Years_Since_HS_Grad_to_Date__c IN (.33,.25) #Fall Year 1 term
-    AND AT_Enrollment_Status__c IN ("Full-time","Part-time", "Approved Gap Year")
-    AND High_School_Class__c IN ("2017", "2018", "2019", "2020") 
-        --AND term.RecordTypeId = "01246000000RNnTAAW" #College/University
-        --AND Predominant_Degree_Awarded__c = "Predominantly bachelor's-degree granting"
+  WHERE fit_type_affiliation IS NULL
