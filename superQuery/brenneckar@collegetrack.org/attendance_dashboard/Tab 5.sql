9a29076a146756a26c3073aad45367099456e92a
Baker Renneckar <baker@bakerr.com>
2020-11-10 19:05:02 -0500

    Auto-commit: Tue Nov 10 2020 16:04:58 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
 SELECT SUM(Attendance_Numerator__c) AS Num, SUM(Attendance_Denominator__c) AS Den, ROUND(SUM(Attendance_Numerator__c) / SUM(Attendance_Denominator__c),2) AS rate
-FROM `data-studio-260217.attendance_dashboard.tmp_filtered_attendance`
+FROM `data-studio-260217.attendance_dashboard.attendance_filtered_data`
 WHERE  Workshop_Global_Academic_Semester__c = 'Fall 2020-21' AND site_abrev ='PGC'
