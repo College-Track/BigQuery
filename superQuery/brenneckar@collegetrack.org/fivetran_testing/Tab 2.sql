86e208a31610201efcd06b0214c31ac4c9476788
Baker Renneckar <baker@bakerr.com>
2020-11-13 16:23:45 -0500

    Auto-commit: Fri Nov 13 2020 13:23:41 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 2.sql
@@ -1,4 +1,4 @@
-SELECT A.Id
+SELECT A.Id, CA.Id
 FROM `data-warehouse-289815.salesforce_raw.Account` A 
 LEFT JOIN `data-warehouse-289815.salesforce_raw.College_Application__c` CA
 ON A.Id = CA.College_University__c
