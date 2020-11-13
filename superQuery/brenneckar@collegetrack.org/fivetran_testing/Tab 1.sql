112c175ebcdc771165ab23858218b532de9d695c
Baker Renneckar <baker@bakerr.com>
2020-11-13 13:38:10 -0500

    Auto-commit: Fri Nov 13 2020 10:38:05 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,3 +1,3 @@
-SELECT id , academic_semester_c
-FROM `data-warehouse-289815.salesforce.bank_book_c`
-WHERE academic_semester_c IS NULL
+SELECT *
+FROM `data-warehouse-289815.salesforce.student_life_activity_c`
+LIMIT 100
