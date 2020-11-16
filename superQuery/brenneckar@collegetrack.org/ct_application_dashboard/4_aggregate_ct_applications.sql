00f77be0c4d520f92efb7694c20a8660816d38b5
Baker Renneckar <baker@bakerr.com>
2020-11-16 15:20:16 -0500

    Auto-commit: Mon Nov 16 2020 12:20:11 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/ct_application_dashboard/4_aggregate_ct_applications.sql
@@ -0,0 +1,34 @@
+WITH aggregate_data AS (
+  SELECT
+    site_short,
+    Gender__c,
+    First_Generation__c,
+    Indicator_Low_Income__c,
+    College_Track_Status_Name,
+    COUNT(Contact_Id) AS student_count,
+    MAX(College_Track_FY_HS_Planned_Enrollment__c) AS budget_target,
+    MAX(College_Track_High_School_Capacity__c) AS capacity_target
+  FROM
+    `data-studio-260217.ct_application.ct_application_filtered_data`
+  GROUP BY
+    site_short,
+    Gender__c,
+    First_Generation__c,
+    Indicator_Low_Income__c,
+    College_Track_Status_Name
+)
+
+SELECT
+  *,
+  CASE
+    WHEN College_Track_Status_Name = "Application in progress" THEN 1
+    WHEN College_Track_Status_Name = "Application submitted" THEN 2
+    WHEN College_Track_Status_Name = "Interview Scheduled" THEN 3
+    WHEN College_Track_Status_Name = "Interview Complete" THEN 4
+    WHEN College_Track_Status_Name = "Admitted" THEN 5
+    WHEN College_Track_Status_Name = "Wait-listed" THEN 6
+    WHEN College_Track_Status_Name = "Onboarding" THEN 7
+    ELSE 8
+  END AS sort_ct_status
+FROM
+  aggregate_data
