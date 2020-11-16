4e2aa986ff669fb2be09a048e4c3b64916657aca
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-15 23:29:49 -0800

    Auto-commit: Sun Nov 15 2020 23:29:45 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 9.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/Tab 9.sql
@@ -1,39 +1,73 @@
+WITH 
+fit_type_application AS
+(
+SELECT
+
+#demographics, academic data
+    app.contact_id,
+    app.Full_Name__c,
+    College_Track_Status_Name,
+    High_School_Class,
+    Site_Text__c AS site_full,
+    site_short,
+    region AS region_full,
+    region_short,
+    GPA_Cumulative__c AS CGPA_11th,
+    CASE
+      WHEN GPA_Cumulative__c <= 2.49 THEN "Below 2.5"
+      WHEN GPA_Cumulative__c < 2.75 THEN "2.5 - 2.74"
+      WHEN GPA_Cumulative__c < 3 THEN "2.75 - 2.99"
+      WHEN GPA_Cumulative__c < 3.25 THEN "3.0 - 3.24"
+      WHEN GPA_Cumulative__c >= 3.25 THEN "3.25+"
+      ELSE "Missing"
+    END AS CGPA_11th_bucket,
+    Readiness_English_Official__c,
+    Readiness_Math_Official__c,
+    Readiness_Composite_Off__c,
+    Indicator_Persisted_into_2nd_Year_CT__c,
+    Indicator_Persisted_into_Year_2_Wide__c,
+    Gender__c,
+    Ethnic_background__c,
+    Indicator_Low_Income__c,
+    First_Generation_FY20__c,
+    FA_Req_Expected_Financial_Contribution__c,
+    Indicator_Completed_CT_HS_Program__c,
+    
+#college application data
+    college_app_id,
+    app.college_id,
+    accnt.id AS account_id,
+    IF(app.college_id = '0014600000plKMXAA2',"Global Citizen Year",accnt.name) AS school_name_app,
+   -- school_name_enrolled,
+    Type_of_School__c,
+    Application_status__c,
+    app.admission_status__c,
+    CASE
+        WHEN admission_status__c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred") THEN "Acceptance"
+        ELSE "N/A"
+        END AS acceptance_group,
+    College_Fit_Type_Applied__c,
+    --app.Fit_Type_Enrolled__c,
+    --fit_type_enrolled_chart
+    
+    --Join with Account object to pull in name of School/College
+    FROM `data-studio-260217.fit_type_pipeline.filtered_college_application` AS app
+    LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` AS accnt
+        ON app.college_id = accnt.id
+    
+   
+        
+    WHERE app.Indicator_Completed_CT_HS_Program__c = TRUE
+
+)
+
+
+--Table houses Academic Term data to isolate Fall Year 1 (Matriculation), enrolled in any college (including vocational). Affiliation join to pull in Fit Type fields
+
 
 SELECT
-    term.contact_id,
-    term.Full_Name__c,
-    High_School_Class,
-    term.Site_Text__c AS site_full,
-    term.site_short,
-    term.region AS region_full,
-    term.region_short,
-    AT_Name AS academic_term_name,
-    term.AT_Id AS academic_term_id,
-    term.AT_Grade__c,
-    student_audit_status__c AS ct_status_at,
-    term.Indicator_Years_Since_HS_Grad_to_Date__c,
-    term.School_Name,
-    term.School_Predominant_Degree_Awarded__c,
-    Affiliation_Record_ID__c AS Affiliation_id,
-    npe5__Organization__c AS Affiliation_School_id,
-    aff.Situational_Fit_Type__c,
-    --aff.Situational_Best_Fit_Context__c,
-    aff.Fit_Type_Current__c,
-    aff.Fit_Type__c AS fit_type_affiliation,
-    term.Fit_Type__c AS fit_type_affiliation_at,
-    aff.Best_Fit_Applied__c AS fit_type_start_of_affiliation
-
---Join to align AT data with available college application data
-    FROM `data-warehouse-289815.sfdc_templates.contact_at_template` AS term
-    RIGHT JOIN `data-studio-260217.fit_type_pipeline.filtered_college_application` AS app
-      ON app.contact_id = term.contact_id
-
- --Join with Affiliation object to pull in Fit Type (Start of Affiliation) for older students
-    FULL JOIN `data-warehouse-289815.salesforce_raw.npe5__Affiliation__c` AS aff
-        ON term.Affiliation_Record_ID__c = aff.Id
-
-    WHERE term.Indicator_Completed_CT_HS_Program__c = TRUE
-        AND term.Indicator_Years_Since_HS_Grad_to_Date__c IN (.33,.25) #Fall Year 1 term
-        --High_School_Class IN (2016, 2017, 2018, 2019, 2020) 
-        --AND matri.RecordTypeId = "01246000000RNnTAAW" #College/University
-        --AND Predominant_Degree_Awarded__c = "Predominantly bachelor's-degree granting"
+    count(Contact_Id)
+    from fit_type_application
+    
+    WHERE High_School_Class >= 2017 #c/o 2017 and above have most completed Fit Type (applied) and Fit Type (enrolled) 
+AND Application_status__c = "Applied"
