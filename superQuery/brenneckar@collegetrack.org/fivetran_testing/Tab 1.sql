f1e589e8a175934b782917b5ae87f270625f29c5
Baker Renneckar <baker@bakerr.com>
2020-11-16 18:28:20 -0500

    Auto-commit: Mon Nov 16 2020 15:28:17 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,2 +1,2 @@
-SELECT done::date, count(*)*2 as approx_num_api_requests 
-FROM `data-warehouse-289815.salesforce._fivetran_query` group by done::date
+SELECT  count(*)*2 as approx_num_api_requests 
+FROM `data-warehouse-289815.salesforce._fivetran_query`
