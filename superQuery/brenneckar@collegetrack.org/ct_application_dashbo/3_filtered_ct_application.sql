737de05097871753ba164af5dcf1260da051f596
Baker Renneckar <baker@bakerr.com>
2020-11-16 13:09:57 -0500

    Auto-commit: Mon Nov 16 2020 10:09:53 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/ct_application_dashbo/3_filtered_ct_application.sql
+++ superQuery/brenneckar@collegetrack.org/ct_application_dashbo/3_filtered_ct_application.sql
@@ -1,10 +1,25 @@
 WITH gather_data AS (
-SELECT Contact_Id, College_Track_Status_Name, site_short, Gender__c, Indicator_First_Generation__c, Indicator_Low_Income__c, Contact_Record_Type_Name
-FROM `data-warehouse-289815.sfdc_templates.contact_template`
-WHERE Contact_Record_Type_Name NOT IN ('Student: Post-Secondary', 'Student: Alumni')
-
-
+  SELECT
+    Contact_Id,
+    College_Track_Status_Name,
+    site_short,
+    Gender__c,
+    Indicator_First_Generation__c,
+    Indicator_Low_Income__c,
+    Contact_Record_Type_Name,
+    HIGH_SCHOOL_GRADUATING_CLASS__c,
+    SITE__c,
+    A.College_Track_FY_HS_Planned_Enrollment__c,
+    A.College_Track_High_School_Capacity__c
+  FROM
+    `data-warehouse-289815.sfdc_templates.contact_template`
+    LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` A ON A.Id = SITE__c
+  WHERE
+    Contact_Record_Type_Name NOT IN ('Student: Post-Secondary', 'Student: Alumni')
 )
-SELECT *
-FROM gather_data
-LIMIT 1000
+SELECT
+  *
+FROM
+  gather_data
+LIMIT
+  1000
