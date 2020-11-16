dfde5c8b7c47ef02635c33b4f5a2f7e02b621be9
Baker Renneckar <baker@bakerr.com>
2020-11-16 13:07:36 -0500

    Auto-commit: Mon Nov 16 2020 10:07:32 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/ct_application_dashbo/3_filtered_ct_application.sql
+++ superQuery/brenneckar@collegetrack.org/ct_application_dashbo/3_filtered_ct_application.sql
@@ -1,6 +1,7 @@
 WITH gather_data AS (
 SELECT Contact_Id, College_Track_Status_Name, site_short, Gender__c, Indicator_First_Generation__c, Indicator_Low_Income__c, Contact_Record_Type_Name
 FROM `data-warehouse-289815.sfdc_templates.contact_template`
+WHERE Contact_Record_Type_Name NOT IN ('Student: Post-Secondary')
 
 
 )
