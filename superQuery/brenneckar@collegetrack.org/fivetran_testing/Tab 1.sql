283b5945f5f1c4c17de918ffc27cdcc3eb3c9327
Baker Renneckar <baker@bakerr.com>
2020-11-17 12:29:42 -0500

    Auto-commit: Tue Nov 17 2020 09:29:39 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,3 +1,3 @@
-SELECT attendance_denominator_c, COUNT(Id)
-FROM `data-warehouse-289815.salesforce.class_attendance_c`
-GROUP BY attendance_denominator_c
+SELECT grade_c, COUNT(Id)
+FROM `data-warehouse-289815.salesforce.contact``
+GROUP BY grade_c
