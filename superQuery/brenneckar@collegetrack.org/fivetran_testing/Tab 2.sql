44a5383855ca464918f4b6c35adfb8ca6ed0be3b
Baker Renneckar <baker@bakerr.com>
2020-11-17 19:09:33 -0500

    Auto-commit: Tue Nov 17 2020 16:09:29 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 2.sql
@@ -1,9 +1,1 @@
-SELECT
-    u.username,
-    s.schemaname,
-    has_schema_privilege(u.username,s.schemaname,'create') AS user_has_select_permission,
-    has_schema_privilege(u.username,s.schemaname,'usage') AS user_has_usage_permission
-FROM
-    pg_user u
-CROSS JOIN
-    (SELECT DISTINCT schemaname FROM pg_tables) s
+SELECT 10 AS Test
