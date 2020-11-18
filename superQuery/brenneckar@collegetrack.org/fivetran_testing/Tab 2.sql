21337915ecbed3376b5d261c2f73f6251da88768
Baker Renneckar <baker@bakerr.com>
2020-11-17 19:00:10 -0500

    Auto-commit: Tue Nov 17 2020 16:00:06 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 2.sql
@@ -1,5 +1,9 @@
-SELECT A.Id, CA.Id
-FROM `data-warehouse-289815.salesforce_raw.Account` A 
-LEFT JOIN `data-warehouse-289815.salesforce_raw.College_Application__c` CA
-ON A.Id = CA.College_University__c
-WHERE A.Id = '0014600000uT26qAAC'
+SELECT
+    u.username,
+    s.schemaname,
+    has_schema_privilege(u.username,s.schemaname,'create') AS user_has_select_permission,
+    has_schema_privilege(u.username,s.schemaname,'usage') AS user_has_usage_permission
+FROM
+    pg_user u
+CROSS JOIN
+    (SELECT DISTINCT schemaname FROM pg_tables) s
