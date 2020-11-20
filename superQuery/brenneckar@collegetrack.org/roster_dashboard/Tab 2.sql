489f32b6da5491749be55e4683e9e453579193f1
Baker Renneckar <baker@bakerr.com>
2020-11-20 15:17:42 -0500

    Auto-commit: Fri Nov 20 2020 12:17:39 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
@@ -1,3 +1,3 @@
-SELECT indicator_low_income_c, COUNT(*)
+SELECT enrollment_status_c, COUNT(*)
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-GROUP BY indicator_low_income_c
+GROUP BY enrollment_status_c
