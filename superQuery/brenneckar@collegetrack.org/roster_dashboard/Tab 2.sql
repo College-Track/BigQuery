07dd7140d9c69ae355c6faa50f66af2222589ec3
Baker Renneckar <baker@bakerr.com>
2020-11-20 15:20:43 -0500

    Auto-commit: Fri Nov 20 2020 12:20:39 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
@@ -1,3 +1,3 @@
-SELECT current_enrollment_status_c, COUNT(*)
+SELECT school_type, COUNT(*)
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
-GROUP BY current_enrollment_status_c
+GROUP BY school_type
