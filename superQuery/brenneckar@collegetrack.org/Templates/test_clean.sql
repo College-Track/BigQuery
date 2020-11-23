ac65aa923815dacfb47e74af17214031788eac8c
Baker Renneckar <baker@bakerr.com>
2020-11-23 14:43:19 -0500

    Auto-commit: Mon Nov 23 2020 11:43:16 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/Templates/test_clean.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/test_clean.sql
@@ -1,4 +1,4 @@
-SELECT  Current_School_Type_c_degree, COUNT(*)
+SELECT  school_type, COUNT(*)
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
 LEFT JOIN `data-warehouse-289815.salesforce.account` A ON A.Id = Current_School_c
-GROUP BY Current_School_Type_c_degree
+GROUP BY school_type
