70c222e742d45a2d15d720daa44d16bd202e3191
Baker Renneckar <baker@bakerr.com>
2020-11-23 12:53:15 -0500

    Auto-commit: Mon Nov 23 2020 09:53:09 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/overview_dashboard/4_hs_overview.sql
+++ superQuery/brenneckar@collegetrack.org/overview_dashboard/4_hs_overview.sql
@@ -2,15 +2,15 @@ WITH calculate_metrics AS(
   SELECT
     *,
     CASE
-      WHEN Most_Recent_GPA_Cumulative__c >= 3.25 THEN 1
+      WHEN Most_Recent_GPA_Cumulative_c >= 3.25 THEN 1
       ELSE 0
     END AS met_gpa_goal,
     CASE
-      WHEN CoVitality_Scorecard_Color_Most_Recent__c = "Green" THEN 1
+      WHEN co_vitality_scorecard_color_most_recent_c = "Green" THEN 1
       ELSE 0
     END AS met_covi_goal,
     CASE
-      WHEN Summer_Experiences_Previous_Summer__c >= 1 THEN 1
+      WHEN Summer_Experiences_Previous_Summer_c >= 1 THEN 1
       ELSE 0
     END AS met_mse_goal,
     CASE
@@ -22,11 +22,11 @@ WITH calculate_metrics AS(
       ELSE 0
     END AS met_cs_goal,
     CASE
-      WHEN college_applications_all_fit_types__c >= 1 THEN 1
+      WHEN college_applications_all_fit_types_c >= 1 THEN 1
       ELSE 0
     END AS met_college_app_goal
   FROM
-    `data-studio-260217.rosters.filtered_roster`
+    `data-studio-260217.rosters.filtered_roster_test`
   WHERE
     Contact_Record_Type_Name = "Student: High School"
 ),
@@ -36,9 +36,9 @@ senior_count AS (
     site_short,
     COUNT(Contact_Id) AS senior_count
   FROM
-    `data-studio-260217.rosters.filtered_roster`
+    `data-studio-260217.rosters.filtered_roster_test`
   WHERE
-    Grade__c = "12th Grade"
+    Grade_c = "12th Grade"
   GROUP BY
     region_short,
     site_short
@@ -46,7 +46,7 @@ senior_count AS (
 SELECT
   CM.region_short,
   CM.site_short,
-  CM.SITE__c,
+  CM.SITE_c,
   COUNT(CM.Contact_Id) AS student_count,
   SUM(CM.met_gpa_goal) AS met_gpa_goal,
   SUM(CM.met_covi_goal) AS met_covi_goal,
@@ -55,14 +55,14 @@ SELECT
   SUM(CM.met_cs_goal) AS met_cs_goal,
   SUM(CM.met_college_app_goal) as met_college_app_goal,
   MAX(SC.senior_count) as senior_count,
-  MAX(Account.College_Track_High_School_Capacity__c) AS hs_capacity,
-  MAX(Account.College_Track_FY_HS_Planned_Enrollment__c) AS  hs_budget_capacity
+  MAX(Account.College_Track_High_School_Capacity_c) AS hs_capacity,
+  MAX(Account.College_Track_FY_HS_Planned_Enrollment_c) AS  hs_budget_capacity
 FROM
   calculate_metrics CM
    LEFT JOIN senior_count SC ON SC.site_short = CM.site_short
-    LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` Account ON Account.Id = CM.SITE__c
+    LEFT JOIN `data-warehouse-289815.salesforce.account` Account ON Account.Id = CM.SITE_c
 
 GROUP BY
-  SITE__c,
+  SITE_c,
   region_short,
   site_short
