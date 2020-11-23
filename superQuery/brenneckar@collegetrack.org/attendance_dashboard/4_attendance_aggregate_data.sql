b207d283c993425f178aabde376b9bca6c248ace
Baker Renneckar <baker@bakerr.com>
2020-11-23 17:16:29 -0500

    Auto-commit: Mon Nov 23 2020 14:16:23 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_aggregate_data.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_aggregate_data.sql
@@ -1,45 +1,43 @@
 SELECT
-  Student_Site__c,
-  Workshop_Display_Name__c,
-  SUM(Attendance_Numerator__c) AS Attendance_Numerator,
-  SUM(Attendance_Denominator__c) AS Attendance_Denominator,
+  Workshop_Display_Name_c,
+  SUM(Attendance_Numerator_c) AS Attendance_Numerator,
+  SUM(Attendance_Denominator_c) AS Attendance_Denominator,
   SUM(mod_numerator) AS mod_numerator,
   SUM(mod_denominator) AS mod_denominator,
-  Date__c,
-  Student_High_School_Class__c,
-  Workshop_Dosage_Type__c,
+  Date_c,
+  HIGH_SCHOOL_GRADUATING_CLASS_c,
+  dosage_types_c,
   dosage_split,
   GPA_Bucket,
-  Indicator_Student_on_Intervention__c,
+  Indicator_Student_on_Intervention_c,
   site_abrev,
---   Class__c,
-  Workshop_Global_Academic_Semester__c,
-  Composite_Readiness_Most_Recent__c,
+--   Class_c,
+  Workshop_Global_Academic_Semester_c,
+  Composite_Readiness_Most_Recent_c,
   Region,
   region_abrev,
-  Outcome__c,
-  CoVitality_Scorecard_Color_Most_Recent__c,
-  COUNT(Student__c) AS record_count
+  Outcome_c,
+  Co_Vitality_Scorecard_Color_Most_Recent_c,
+  COUNT(Student_c) AS record_count
 FROM
   `data-studio-260217.attendance_dashboard.attendance_filtered_data`
 
 
 
 GROUP BY
-  Student_Site__c,
-  Workshop_Display_Name__c,
-  Date__c,
-  Student_High_School_Class__c,
-  Workshop_Dosage_Type__c,
+  Workshop_Display_Name_c,
+  Date_c,
+  HIGH_SCHOOL_GRADUATING_CLASS_c,
+  dosage_types_c,
   GPA_Bucket,
-  Indicator_Student_on_Intervention__c,
+  Indicator_Student_on_Intervention_c,
   site_abrev,
-  Workshop_Global_Academic_Semester__c,
---   Class__c,
-  Composite_Readiness_Most_Recent__c,
+  Workshop_Global_Academic_Semester_c,
+--   Class_c,
+  Composite_Readiness_Most_Recent_c,
   Region,
   region_abrev,
-  Outcome__c,
+  Outcome_c,
   dosage_split,
-  CoVitality_Scorecard_Color_Most_Recent__c
-  ORDER BY Workshop_Dosage_Type__c
+  Co_Vitality_Scorecard_Color_Most_Recent_c
+  ORDER BY dosage_types_c
