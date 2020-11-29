81bebbfe105be88c19f5fbeb55cf81f91054977a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-11-29 15:13:09 -0800

    Auto-commit: Sun Nov 29 2020 15:13:03 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/accepted_pipeline_draft.sql
+++ superQuery/ireyna@collegetrack.org/fit_type_application_matriculation_dashboard/accepted_pipeline_draft.sql
@@ -1,6 +1,8 @@
+WITH fit_type_acceptances AS #record count = 7870
+(
 SELECT
-    count(contact_id) AS contact_id_accepted,
-    /*acceptance_group,
+    contact_id AS contact_id_accepted,
+    acceptance_group,
     contact_id,
     Full_Name__c,
     College_Track_Status_Name,
@@ -12,6 +14,30 @@ SELECT
     college_app_id,
     school_name_app,
     admission_status__c,
-    acceptance_group AS acceptance_group_accepted*/
+    acceptance_group AS acceptance_group_accepted
 FROM `data-studio-260217.fit_type_pipeline.aggregate_data`
 WHERE acceptance_group = "Accepted"
+
+)
+
+SELECT
+    accept.*
+
+FROM fit_type_acceptances AS accept
+INNER JOIN `data-studio-260217.fit_type_pipeline.aggregate_data` AS agg
+    ON agg.Contact_Id = Contact_Id_accepted
+
+
+GROUP BY
+    contact_id_accepted,
+    accept.Full_Name__c,
+    accept.College_Track_Status_Name,
+    accept.High_School_Class,
+    accept.site_full,
+    accept.site_short,
+    accept.region_full,
+    accept.region_short,
+    accept.college_app_id,
+    accept.school_name_app,
+    accept.admission_status__c,
+    acceptance_group_accepted
