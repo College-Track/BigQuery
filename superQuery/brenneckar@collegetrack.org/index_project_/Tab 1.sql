176cce2a6615b87edae2974e162e42f2102352a9
Baker Renneckar <baker@bakerr.com>
2020-11-30 15:07:58 -0500

    Auto-commit: Mon Nov 30 2020 12:07:54 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_/Tab 1.sql
@@ -11,14 +11,16 @@ WITH gather_data AS (
     mailing_street,
     primary_contact_language_c,
     first_generation_fy_20_c,
-    CASE WHEN citizen_c_c = 'US Citizen' THEN true
-    ELSE false AS is_a_us_citizen
+    CASE
+      WHEN citizen_c_c = 'US Citizen' THEN true
+      ELSE false 
+      END AS is_a_us_citizen
+      FROM
+        `data-warehouse-289815.salesforce_clean.contact_template`
+      WHERE
+        college_track_status_c IN ('11A')
+    )
+  SELECT
+    *
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
-  WHERE
-    college_track_status_c IN ('11A')
-)
-SELECT
-  *
-FROM
-  gather_data
+    gather_data
