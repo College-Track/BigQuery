aaaba79502ba7462daaaeea129018828f7a43b0f
Baker Renneckar <baker@bakerr.com>
2020-11-30 15:00:56 -0500

    Auto-commit: Mon Nov 30 2020 12:00:52 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_/Tab 1.sql
@@ -1,5 +1,5 @@
 WITH gather_data AS (
-SELECT Contact_Id
+SELECT Contact_Id, site_sort, Gender_c, annual_household_income_c, mailing_city, mailing_postal_code, mailing_postal_code, mailing_state, mailing_street
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
 WHERE college_track_status_c IN ('11A')
 
