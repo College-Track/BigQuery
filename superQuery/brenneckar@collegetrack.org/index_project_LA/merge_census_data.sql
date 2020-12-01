187bb37dc49266b6d8315bf967e03a13980d79e8
Baker Renneckar <baker@bakerr.com>
2020-12-01 09:51:30 -0800

    Auto-commit: Tue Dec 01 2020 09:51:25 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
@@ -6,7 +6,12 @@ WITH merge_data AS (
     ACS.not_us_citizen_pop,
     ACS.median_income,
     ACS.income_per_capita,
-    ACS.housing_units_renter_occupied
+    ACS.housing_units_renter_occupied,
+    ACS.percent_income_spent_on_rent,
+    ACS.different_house_year_ago_different_city,
+    ACS.different_house_year_ago_same_city,
+    ACS.speak_only_english_at_home,
+    ACS.households
     
   FROM
     `learning-agendas.index_project.student_with_census` C
