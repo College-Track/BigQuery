325772ac5538a1810fdb0ee83f643000cc78baff
Baker Renneckar <baker@bakerr.com>
2020-12-01 09:48:57 -0800

    Auto-commit: Tue Dec 01 2020 09:48:52 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
@@ -1,8 +1,19 @@
-WITH merge_data AS (SELECT C.Contact_Id, C.census_track_id, ACS.geo_id, ACS.total_pop
-FROM `learning-agendas.index_project.student_with_census` C
-LEFT JOIN  `bigquery-public-data.census_bureau_acs.censustract_2018_5yr` ACS on ACS.geo_id = C.census_track_id
+WITH merge_data AS (
+  SELECT
+    C.*,
+    ACS.bachelors_degree,
+    ACS.total_pop,
+    ACS.not_us_citizen_pop,
+    ACS.median_income,
+    ACS.income_per_capita,
+    ACS.housing_units_renter_occupied
+    
+  FROM
+    `learning-agendas.index_project.student_with_census` C
+    LEFT JOIN `bigquery-public-data.census_bureau_acs.censustract_2018_5yr` ACS on ACS.geo_id = C.census_track_id
 )
-
-SELECT *
-FROM merge_data
-WHERE total_pop IS NULL
+SELECT
+  *
+FROM
+  merge_data
+  LIMIT 10
