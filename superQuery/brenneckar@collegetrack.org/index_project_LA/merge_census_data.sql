584e5b56f00e2b44b478d5cbc36a7b9b0b4d2d40
Baker Renneckar <baker@bakerr.com>
2020-12-01 09:45:16 -0800

    Auto-commit: Tue Dec 01 2020 09:45:11 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
@@ -0,0 +1,3 @@
+SELECT C.Contact_Id, C.census_track_id, ACS.geo_id, ACS.total_pop
+FROM `learning-agendas.index_project.student_with_census` C
+LEFT JOIN  `bigquery-public-data.census_bureau_acs.censustract_2018_5yr` ACS on ACS.geo_id = C.census_track_id
