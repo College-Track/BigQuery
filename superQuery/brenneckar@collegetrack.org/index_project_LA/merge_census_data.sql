537545a0b5de3f2d50292fd7f022ca61b0a9a8f8
Baker Renneckar <baker@bakerr.com>
2020-12-02 12:10:38 -0800

    Auto-commit: Wed Dec 02 2020 12:10:27 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
@@ -75,18 +75,18 @@ GROUP BY
 SELECT 
 site_short, 
 (avg_us_citizen - avg(avg_us_citizen) over()) / stddev(avg_us_citizen) over() AS us_citizen, -- positive
-(avg_english_primary_language - avg(avg_english_primary_language) over()) / stddev(avg_english_primary_language) over() AS english_primary_language, -- positive
--(avg_first_gen - avg(avg_first_gen) over()) / stddev(avg_first_gen) over() AS first_gen, --negative
-(avg_househld_income_site - avg(avg_househld_income_site) over()) / stddev(avg_househld_income_site) over() AS househld_income_site,--positive
-(avg_percent_bachelors_degree - avg(avg_percent_bachelors_degree) over()) / stddev(avg_percent_bachelors_degree) over() AS bachelors_degree,--positive
-(avg_percent_high_school_diploma - avg(avg_percent_high_school_diploma) over()) / stddev(avg_percent_high_school_diploma) over() AS high_school_diploma, --positive
--(avg_percent_households_public_asst_or_food_stamps - avg(avg_percent_households_public_asst_or_food_stamps) over()) / stddev(avg_percent_households_public_asst_or_food_stamps) over() AS households_public_asst_or_food_stamps,--negative
-(avg_percent_housing_units_renter_occupied - avg(avg_percent_housing_units_renter_occupied) over()) / stddev(avg_percent_housing_units_renter_occupied) over() AS housing_units_renter_occupied,
-(avg_income_per_capita - avg(avg_income_per_capita) over()) / stddev(avg_income_per_capita) over() AS income_per_capita,
-(avg_median_income - avg(avg_median_income) over()) / stddev(avg_median_income) over() AS median_income,
-(avg_percent_not_us_citizen_pop - avg(avg_percent_not_us_citizen_pop) over()) / stddev(avg_percent_not_us_citizen_pop) over() AS percent_not_us_citizen_pop,
-(avg_percent_income_spent_on_rent - avg(avg_percent_income_spent_on_rent) over()) / stddev(avg_percent_income_spent_on_rent) over() AS percent_income_spent_on_rent,
-(avg_percent_unemployed_pop - avg(avg_percent_unemployed_pop) over()) / stddev(avg_percent_unemployed_pop) over() AS percent_unemployed_pop,
-(avg_percent_vacant_housing_units - avg(avg_percent_vacant_housing_units) over()) / stddev(avg_percent_vacant_housing_units) over() AS percent_vacant_housing_units,
+-- (avg_english_primary_language - avg(avg_english_primary_language) over()) / stddev(avg_english_primary_language) over() AS english_primary_language, -- positive
+-- -(avg_first_gen - avg(avg_first_gen) over()) / stddev(avg_first_gen) over() AS first_gen, --negative
+-- (avg_househld_income_site - avg(avg_househld_income_site) over()) / stddev(avg_househld_income_site) over() AS househld_income_site,--positive
+-- (avg_percent_bachelors_degree - avg(avg_percent_bachelors_degree) over()) / stddev(avg_percent_bachelors_degree) over() AS bachelors_degree,--positive
+-- (avg_percent_high_school_diploma - avg(avg_percent_high_school_diploma) over()) / stddev(avg_percent_high_school_diploma) over() AS high_school_diploma, --positive
+-- -(avg_percent_households_public_asst_or_food_stamps - avg(avg_percent_households_public_asst_or_food_stamps) over()) / stddev(avg_percent_households_public_asst_or_food_stamps) over() AS households_public_asst_or_food_stamps,--negative
+-- -(avg_percent_housing_units_renter_occupied - avg(avg_percent_housing_units_renter_occupied) over()) / stddev(avg_percent_housing_units_renter_occupied) over() AS housing_units_renter_occupied, --negative
+-- (avg_income_per_capita - avg(avg_income_per_capita) over()) / stddev(avg_income_per_capita) over() AS income_per_capita,--positive
+-- (avg_median_income - avg(avg_median_income) over()) / stddev(avg_median_income) over() AS median_income,--positive
+-- -(avg_percent_not_us_citizen_pop - avg(avg_percent_not_us_citizen_pop) over()) / stddev(avg_percent_not_us_citizen_pop) over() AS percent_not_us_citizen_pop,--negative
+-- -(avg_percent_income_spent_on_rent - avg(avg_percent_income_spent_on_rent) over()) / stddev(avg_percent_income_spent_on_rent) over() AS percent_income_spent_on_rent,--negative
+-- -(avg_percent_unemployed_pop - avg(avg_percent_unemployed_pop) over()) / stddev(avg_percent_unemployed_pop) over() AS percent_unemployed_pop,--negative
+-- -(avg_percent_vacant_housing_units - avg(avg_percent_vacant_housing_units) over()) / stddev(avg_percent_vacant_housing_units) over() AS percent_vacant_housing_units,--negative
 
 FROM calc_averages
