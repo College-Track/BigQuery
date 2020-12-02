acc05779d4c326712af7cbac16dde829f825ecc1
Baker Renneckar <baker@bakerr.com>
2020-12-02 09:39:51 -0800

    Auto-commit: Wed Dec 02 2020 09:39:44 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/merge_census_data.sql
@@ -22,11 +22,28 @@ WITH gather_census_data AS (
     ACS.speak_only_english_at_home,
     ACS.unemployed_pop,
     ACS.vacant_housing_units
-  FROM `bigquery-public-data.census_bureau_acs.censustract_2018_5yr` ACS 
+  FROM
+    `bigquery-public-data.census_bureau_acs.censustract_2018_5yr` ACS
 )
-  
 SELECT
-C.*, bachelors_degree / pop_25_years_over AS percent_bachelors_degree
+  C.*,
+  bachelors_degree / pop_25_years_over AS percent_bachelors_degree,
+  different_house_year_ago_different_city / pop_25_years_over AS percent_different_house_year_ago_different_city,
+  different_house_year_ago_same_city  / pop_25_years_over AS percent_,
+  high_school_diploma  / pop_25_years_over AS percent_high_school_diploma,
+  high_school_including_ged  / pop_25_years_over AS percent_high_school_including_ged,
+  households_public_asst_or_food_stamps  / households AS percent_households_public_asst_or_food_stamps,
+  housing_units_renter_occupied  / households AS percent_housing_units_renter_occupied,
+  income_per_capita,
+  median_income,
+  not_in_labor_force / pop_25_years_over AS percent_not_in_labor_force,
+  not_us_citizen_pop / pop_25_years_over AS percent_not_us_citizen_pop,
+  occupied_housing_units / households AS percent_occupied_housing_units,
+  percent_income_spent_on_rent,
+  pop_in_labor_force / pop_25_years_over AS percent_pop_in_labor_force,
+--   speak_only_english_at_home / pop_25_years_over AS percent_speak_only_english_at_home,
+  unemployed_pop / pop_25_years_over AS percent_unemployed_pop,
+  vacant_housing_units / households AS percent_vacant_housing_units
 FROM
-`learning-agendas.index_project.student_with_census` C
-LEFT JOIN gather_census_data CENSUS ON C.census_track_id = CENSUS.geo_id
+  `learning-agendas.index_project.student_with_census` C
+  LEFT JOIN gather_census_data CENSUS ON C.census_track_id = CENSUS.geo_id
