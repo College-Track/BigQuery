004e44dddcad52d66624b8f9d6337ee39211eede
Baker Renneckar <baker@bakerr.com>
2020-12-02 12:14:48 -0800

    Auto-commit: Wed Dec 02 2020 12:14:45 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_LA/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/Tab 3.sql
@@ -1,3 +1,3 @@
 SELECT *,
-(us_citizen + english_primary_language + first_gen + househld_income_site) / 4 AS student_self_index
+(us_citizen * @citizen_mod + english_primary_language + first_gen + househld_income_site) / 4 AS student_self_index
 FROM `learning-agendas.index_project.site_z_scores`
