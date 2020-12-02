41759be13b324cf327b3c07f10d8e9b390be1f1e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-12-01 18:35:35 -0800

    Auto-commit: Tue Dec 01 2020 18:34:26 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/Isabel tests/HBCU enrollment.sql
+++ superQuery/ireyna@collegetrack.org/Isabel tests/HBCU enrollment.sql
@@ -5,8 +5,18 @@ High_School_Class__c,
 Indicator_Years_Since_HS_Grad_to_Date__c,
 AT_Grade__c,
 School__c,
+School_Name,
+AT_Enrollment_Status__c,
+Enrolled_in_any_college__c,
 School_Academic_Calendar__c,
-School_Predominant_Degree_Awarded__c
-FROM `data-warehouse-289815.sfdc_templates.contact_at_template`
+School_Predominant_Degree_Awarded__c,
+Historically_Black_College_Univ_HBCU__c
+FROM `data-warehouse-289815.sfdc_templates.contact_at_template` as term
+
+LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` as accnt
+ON term.School__c = accnt.Account_ID__c
+
 WHERE 
 AT_Record_Type_Name = "College/University Semester"
+AND Enrolled_in_any_college__c = TRUE
+AND Historically_Black_College_Univ_HBCU__c = TRUE
