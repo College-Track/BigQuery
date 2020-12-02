d55ec150dd42e27f21346f257570c21cc1f56073
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-12-01 18:28:21 -0800

    Auto-commit: Tue Dec 01 2020 18:27:11 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/ireyna@collegetrack.org/Isabel tests/Tab 8.sql
@@ -0,0 +1,9 @@
+SELECT 
+Contact_Id,
+Full_Name__c,
+High_School_Class__c,
+Indicator_Years_Since_HS_Grad_to_Date__c,
+AT_Grade__c,
+FROM `data-warehouse-289815.sfdc_templates.contact_at_template`
+WHERE 
+AT_Record_Type_Name = "College/University Semester"
