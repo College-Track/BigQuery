7a81c80b3429497aa99dd555ca3f50673a73585b
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-12-03 11:18:11 -0800

    Auto-commit: Thu Dec 03 2020 11:18:04 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/Isabel tests/HBCU enrolled 2020-21AY.sql
+++ superQuery/ireyna@collegetrack.org/Isabel tests/HBCU enrolled 2020-21AY.sql
@@ -6,6 +6,7 @@ Contact_Id,
 Full_Name__c,
 High_School_Class__c,
 AT_Name, #academic term name
+Global_Academic_Semester__c,
 AT_Id,
 Indicator_Years_Since_HS_Grad_to_Date__c,
 AT_Grade__c,
@@ -18,6 +19,24 @@ School_Academic_Calendar__c,
 School_Predominant_Degree_Awarded__c,
 Historically_Black_College_Univ_HBCU__c,
 
+FROM `data-warehouse-289815.sfdc_templates.contact_at_template` as term
+
+LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` as accnt
+ON term.School__c = accnt.Id
+
+LEFT JOIN  `data-warehouse-289815.salesforce_raw.Academic_Year__c` as gay
+ON term.Academic_Year__c = gay.id
+
+WHERE 
+AT_Record_Type_Name = "College/University Semester"
+AND Enrolled_in_any_college__c = TRUE
+AND Historically_Black_College_Univ_HBCU__c = TRUE
+AND College_Track_Status_Name = "Active: Post-Secondary"
+AND gay.name = "AY 2020-21"
+
+/*
+--Get unique count
+SELECT COUNT(distinct Contact_Id)
 FROM `data-warehouse-289815.sfdc_templates.contact_at_template` as term
 
 LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` as accnt
