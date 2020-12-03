487e72a454e7d9b145f26054517c3bbe5eb33f0e
Baker Renneckar <baker@bakerr.com>
2020-12-03 14:25:35 -0800

    Auto-commit: Thu Dec 03 2020 14:25:31 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/index_project_LA/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/index_project_LA/Tab 3.sql
@@ -1,3 +1,4 @@
 SELECT Contact_Id, AT_Id, AT_Grade_c, gpa_semester_cumulative_c
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE college_track_status_c IN ('11A') AND gpa_semester_cumulative_c IS NOT NULL AND AT_Grade_c IN ('9th Grade','10th Grade')
+ORDER BY Contact_Id, AT_Grade_c
