b7deb2c387c612d96a81dd0c3fc12074b4a88865
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2020-12-03 11:27:53 -0800

    Auto-commit: Thu Dec 03 2020 11:27:44 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/Isabel tests/HBCU enrolled 2018-19AY.sql
+++ superQuery/ireyna@collegetrack.org/Isabel tests/HBCU enrolled 2018-19AY.sql
@@ -6,6 +6,7 @@ Contact_Id,
 Full_Name__c,
 High_School_Class__c,
 AT_Name, #academic term name
+Global_Academic_Semester__c,
 AT_Id,
 Indicator_Years_Since_HS_Grad_to_Date__c,
 AT_Grade__c,
@@ -33,3 +34,25 @@ AND Historically_Black_College_Univ_HBCU__c = TRUE
 AND student_audit_status__c = "Active: Post-Secondary"
 AND gay.name = "AY 2018-19"
 AND High_School_Class__c <> '2019' #to remove summer terms where rising freshman erroneously entered as FT/PT. Will be captured in following AY 
+
+/*
+--Get unique count
+select COUNT([DISTINCT Contact_Id)
+
+FROM `data-warehouse-289815.sfdc_templates.contact_at_template` as term
+
+LEFT JOIN `data-warehouse-289815.salesforce_raw.Account` as accnt
+ON term.School__c = accnt.Id
+
+LEFT JOIN  `data-warehouse-289815.salesforce_raw.Academic_Year__c` as gay
+ON term.Academic_Year__c = gay.id
+
+WHERE 
+AT_Record_Type_Name = "College/University Semester"
+AND Enrolled_in_any_college__c = TRUE
+AND Historically_Black_College_Univ_HBCU__c = TRUE
+AND student_audit_status__c = "Active: Post-Secondary"
+AND gay.name = "AY 2018-19"
+AND High_School_Class__c <> '2019' #to remove summer terms where rising freshman erroneously entered as FT/PT. Will be captured in following AY 
+
+*/
