1f7e63c78f30ac2eb4832c24a24da7a94c8127e9
Baker Renneckar <baker@bakerr.com>
2021-01-25 10:56:41 -0800

    Auto-commit: Mon Jan 25 2021 10:56:37 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,4 +1,2 @@
-SELECT C.grade_c,RT.name,  COUNT(C.Id)
-FROM `data-warehouse-289815.salesforce.contact` C
-LEFT JOIN `data-warehouse-289815.salesforce.record_type` RT ON record_type_id = RT.id
-GROUP BY RT.name, grade_c
+SELECT *
+FROM `data-warehouse-289815.salesforce.attachments`
