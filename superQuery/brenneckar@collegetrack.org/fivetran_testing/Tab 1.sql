e73c1d5d6a4f1dd3b6131c83ee250da9f71e4346
Baker Renneckar <baker@bakerr.com>
2021-01-26 11:39:24 -0800

    Auto-commit: Tue Jan 26 2021 11:33:15 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,3 +1,3 @@
-SELECT workshop_instructor_c, attendance_taker_c, Primary_Staff_c
+SELECT workshop_instructor_c, attendance_taker_c
 FROM `data-warehouse-289815.salesforce_clean.class_template`
 LIMIT 100
