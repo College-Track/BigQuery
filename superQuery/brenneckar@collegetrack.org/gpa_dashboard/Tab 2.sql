b7fd40bf30d9cc1d926f206b08542b88a99a626d
Baker Renneckar <baker@bakerr.com>
2021-01-25 16:30:22 -0800

    Auto-commit: Mon Jan 25 2021 16:24:16 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/gpa_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/gpa_dashboard/Tab 2.sql
@@ -4,6 +4,4 @@ SELECT
   
 FROM
   `data-warehouse-289815.salesforce_clean.contact_at_template`
-WHERE term_c = 'Spring'  AND grade_c = '9th Grade' AND AT_Cumulative_GPA IS NOT NULL
-
-LIMIT 1000
+WHERE term_c = 'Spring'
