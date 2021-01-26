6db89720dcb4ea43b703f7e00f9b59aec5bb436b
Baker Renneckar <baker@bakerr.com>
2021-01-26 13:56:33 -0800

    Auto-commit: Tue Jan 26 2021 13:56:30 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/student_dashboard /Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/student_dashboard /Tab 3.sql
@@ -3,4 +3,5 @@ SELECT
 FROM
   `data-studio-260217.student_dashboard.student_dashboard` AS Master
 WHERE
-  LOWER(Master.Email) = LOWER(SESSION_USER())
+  LOWER(Master.Email) = 'bakerrenneckar@gmail.com'
+--   ))
