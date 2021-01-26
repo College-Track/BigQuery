770ede3a68e805461e9e70ddce59d43094120634
Baker Renneckar <baker@bakerr.com>
2021-01-25 16:32:04 -0800

    Auto-commit: Mon Jan 25 2021 16:25:51 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/gpa_dashboard/aggregate_gpa_data.sql
@@ -0,0 +1,46 @@
+WITH gather_data AS (
+  SELECT
+    region_abrev,
+    site_abrev,
+    site_short,
+    high_school_graduating_class_c,
+    GAS_Name,
+    AY_Name,
+    AT_Cumulative_GPA_bucket,
+    sort_AT_Cumulative_GPA,
+    ninth_cum_gpa_bucket,
+    sort_ninth_cum_gpa_bucket,
+        Most_Recent_GPA_Cumulative_bucket,
+    sort_Most_Recent_GPA_Cumulative_bucket,
+    year_over_year_cum_gpa_bucket,
+    sort_year_over_year_cum_gpa_bucket,
+    COUNT(Contact_Id) AS student_count,
+    SUM(above_325_gpa) AS above_325_gpa,
+    SUM(below_275_gpa) AS below_275_gpa,
+    AVG(prev_prev_prev_at_cum_gpa) as avg_prev_year_cum_gpa,
+    AVG(AT_Cumulative_GPA) AS avg_cum_gpa
+    
+  FROM
+    `data-studio-260217.gpa_dashboard.filtered_gpa_data`
+  WHERE GAS_Name = 'Spring 2019-20'
+  GROUP BY
+    region_abrev,
+    site_abrev,
+    site_short,
+    high_school_graduating_class_c,
+    GAS_Name,
+    AY_Name,
+    AT_Cumulative_GPA_bucket,
+    sort_AT_Cumulative_GPA,
+    ninth_cum_gpa_bucket,
+    sort_ninth_cum_gpa_bucket,
+            Most_Recent_GPA_Cumulative_bucket,
+    sort_Most_Recent_GPA_Cumulative_bucket,
+    year_over_year_cum_gpa_bucket,
+    sort_year_over_year_cum_gpa_bucket
+)
+SELECT
+  GD.*
+  
+FROM
+  gather_data GD
