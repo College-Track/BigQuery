99c62efe6026acf8c1d39379c39273af7eb4e431
Baker Renneckar <baker@bakerr.com>
2021-01-26 09:30:24 -0800

    Auto-commit: Tue Jan 26 2021 09:24:18 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,3 +1,3 @@
 SELECT Count(Id)
 FROM `data-warehouse-289815.salesforce.academic_semester_c`
-WHEre is_deleted = false AND created_date >= 2021-01-18
+WHEre is_deleted = false AND created_date >= '2021-01-18'
