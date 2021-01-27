eac071b2a10a7f1050526b46e5d375a565a2af98
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-01-26 19:45:45 -0800

    Auto-commit: Tue Jan 26 2021 19:45:42 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/college_applications_dashboard/Tab 9.sql
+++ superQuery/ireyna@collegetrack.org/college_applications_dashboard/Tab 9.sql
@@ -1,7 +1,7 @@
 SELECT  (SELECT app2.student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS app2
-        WHERE (app2.Predominant_Degree_Awarded_c = "Predominantly bachelor's-degree granting" AND app.student_c=app2.student_c
-        AND admission_status_c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred"))
+        WHERE app2.Predominant_Degree_Awarded_c = "Predominantly bachelor's-degree granting" 
+        AND admission_status_c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred")
         group by app2.student_c
         ) AS  contact_id_accepted_4_year, admission_status_c, predominant_degree_awarded_c
 
