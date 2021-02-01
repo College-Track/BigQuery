fb4471eac5254389eb66949e3e8bc8e8ece3697d
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-02-01 11:17:53 -0800

    Auto-commit: Mon Feb 01 2021 11:17:45 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 2.sql
@@ -0,0 +1,26 @@
+
+    SELECT
+    student_c, 
+    amount_c, 
+    transaction_status_c,
+    id AS bb_id,
+    FORMAT_DATE("%x", finance_reporting_date_c)AS finance_reporting_date, 
+    CASE
+        WHEN record_type_id = "01246000000ZNhsAAG"then "BB Disbursement"
+        WHEN record_type_id = "01246000000ZNhtAAG"then "BB Earnings"
+        END AS BB_Record_Type,
+    CASE
+        WHEN record_type_id = "01246000000ZNhsAAG"then amount_c
+        END AS BB_Disbursement_Amount,
+    CASE
+        WHEN record_type_id = "01246000000ZNhtAAG"then amount_c
+        END AS BB_Earnings_Amount,
+    FORMAT_DATE("%m", CURRENT_DATE()) AS report_month,
+    FORMAT_DATE("%x", CURRENT_DATE()) AS report_date,    
+    
+    FROM
+    `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
+    WHERE
+    finance_reporting_date_c > '2020-07-01'AND
+    record_type_id = "01246000000ZNhsAAG" OR
+    record_type_id = "01246000000ZNhtAAG"
