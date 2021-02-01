c56857822be723402931d1fec4004fbc8503909b
Baker Renneckar <baker@bakerr.com>
2021-02-01 10:23:24 -0800

    Auto-commit: Mon Feb 01 2021 10:23:13 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/gpa_dashboard/filtered_gpa_data.sql
+++ superQuery/brenneckar@collegetrack.org/gpa_dashboard/filtered_gpa_data.sql
@@ -10,7 +10,6 @@ WITH gather_data AS (
     CAT.Gender_c,
     CAT.Ethnic_background_c,
     CAT.student_audit_status_c,
-    
     CAT.AT_Term_GPA,
     CAT.AT_Cumulative_GPA,
     AT_Cumulative_GPA - Prev_Prev_Prev_AT.gpa_semester_cumulative_c AS year_over_year_cum_gpa_change,
@@ -21,13 +20,21 @@ WITH gather_data AS (
     CAT.AT_Cumulative_GPA_bucket,
     CAT.sort_AT_Cumulative_GPA,
     CAT.GPA_Growth_prev_semester_c,
-    `data-warehouse-289815.UDF.determine_buckets`(CAT.GPA_Growth_prev_semester_c, .25, -0.5, 0.5, "") AS gpa_growth_bucket,
-    `data-warehouse-289815.UDF.sort_created_buckets`(CAT.GPA_Growth_prev_semester_c, .25, -0.5, 0.5) AS sort_gpa_growth_bucket,
+    `data-warehouse-289815.UDF.determine_buckets`(
+      CAT.GPA_Growth_prev_semester_c,.25,
+      -0.5,
+      0.5,
+      ""
+    ) AS gpa_growth_bucket,
+    `data-warehouse-289815.UDF.sort_created_buckets`(CAT.GPA_Growth_prev_semester_c,.25, -0.5, 0.5) AS sort_gpa_growth_bucket,
     CAT.Most_Recent_GPA_Cumulative_bucket,
     CAT.sort_Most_Recent_GPA_Cumulative_bucket,
     CAT.Most_Recent_GPA_Cumulative_c,
     CAT.AT_Id,
-    CONCAT('https://ctgraduates.lightning.force.com/', CAT.AT_ID) AS at_url,
+    CONCAT(
+      'https://ctgraduates.lightning.force.com/',
+      CAT.AT_ID
+    ) AS at_url,
     CAT.previous_as_c,
     CAT.Current_HS_CT_Coach_c,
     CAT.AT_Grade_c,
@@ -50,8 +57,8 @@ WITH gather_data AS (
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
     LEFT JOIN `data-warehouse-289815.salesforce.academic_semester_c` Prev_AT ON Prev_AT.id = CAT.previous_academic_semester_c
-  LEFT JOIN `data-warehouse-289815.salesforce.academic_semester_c` Prev_Prev_AT ON Prev_Prev_AT.id = Prev_AT.Previous_Academic_Semester_c
-  LEFT JOIN `data-warehouse-289815.salesforce.academic_semester_c` Prev_Prev_Prev_AT ON Prev_Prev_Prev_AT.id = Prev_Prev_AT.Previous_Academic_Semester_c
+    LEFT JOIN `data-warehouse-289815.salesforce.academic_semester_c` Prev_Prev_AT ON Prev_Prev_AT.id = Prev_AT.Previous_Academic_Semester_c
+    LEFT JOIN `data-warehouse-289815.salesforce.academic_semester_c` Prev_Prev_Prev_AT ON Prev_Prev_Prev_AT.id = Prev_Prev_AT.Previous_Academic_Semester_c
   WHERE
     CAT.college_track_status_c IN ('11A', '12A')
     AND CAT.student_audit_status_c IN ('Current CT HS Student', 'Leave of Absence')
@@ -69,7 +76,6 @@ gather_9th_grade_spring_gpa AS (
     sort_AT_Cumulative_GPA
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template`
-    
   WHERE
     term_c = 'Spring'
     AND AT_Cumulative_GPA IS NOT NULL
@@ -79,6 +85,8 @@ gather_aa_attendance AS (
   SELECT
     student_c,
     academic_semester_c,
+    SUM(Attendance_Denominator_c) AS Attendance_Denominator_c,
+    SUM(Attendance_Numerator_c) AS Attendance_Numerator_c,
     CASE
       WHEN SUM(Attendance_Denominator_c) = 0 THEN NULL
       ELSE SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c)
@@ -91,12 +99,25 @@ gather_aa_attendance AS (
     student_c,
     academic_semester_c
 ),
+ct_career_aa_attendance AS (
+  SELECT
+    student_c,
+    CASE
+      WHEN SUM(Attendance_Denominator_c) = 0 THEN NULL
+      ELSE SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c)
+    END AS attendance_rate_ct_career
+  FROM
+    gather_aa_attendance
+  GROUP BY
+    student_c
+),
 combine_data AS (
   SELECT
     GD.*,
     G9GPA.AT_Cumulative_GPA AS ninth_cum_gpa,
-    CASE WHEN G9GPA.AT_Cumulative_GPA_bucket IS NULL THEN "No Data" 
-    ELSE G9GPA.AT_Cumulative_GPA_bucket
+    CASE
+      WHEN G9GPA.AT_Cumulative_GPA_bucket IS NULL THEN "No Data"
+      ELSE G9GPA.AT_Cumulative_GPA_bucket
     END AS ninth_cum_gpa_bucket,
     G9GPA.sort_AT_Cumulative_GPA AS sort_ninth_cum_gpa_bucket,
     AA.attendance_rate,
@@ -109,13 +130,29 @@ combine_data AS (
       AND AA.attendance_rate <.9 THEN "80% - 89%"
       ELSE "90%+"
     END AS attendance_bucket,
-    `data-warehouse-289815.UDF.determine_buckets`(year_over_year_cum_gpa_change, .25, -0.5, 0.5, "") AS year_over_year_cum_gpa_bucket,
-    `data-warehouse-289815.UDF.sort_created_buckets`(year_over_year_cum_gpa_change, .25, -0.5, 0.5) AS sort_year_over_year_cum_gpa_bucket,
+    `data-warehouse-289815.UDF.determine_buckets`(
+      year_over_year_cum_gpa_change,.25,
+      -0.5,
+      0.5,
+      ""
+    ) AS year_over_year_cum_gpa_bucket,
+    `data-warehouse-289815.UDF.sort_created_buckets`(year_over_year_cum_gpa_change,.25, -0.5, 0.5) AS sort_year_over_year_cum_gpa_bucket,
+    CTAA.attendance_rate_ct_career,
+    CASE
+      WHEN CTAA.attendance_rate_ct_career IS NULL THEN "No Data"
+      WHEN CTAA.attendance_rate_ct_career <.65 THEN "< 65%"
+      WHEN CTAA.attendance_rate_ct_career >=.65
+      AND CTAA.attendance_rate_ct_career <.8 THEN "65% -79%"
+      WHEN CTAA.attendance_rate_ct_career >= 0.8
+      AND CTAA.attendance_rate_ct_career <.9 THEN "80% - 89%"
+      ELSE "90%+"
+    END AS attendance_bucket_ct_career,
     
   FROM
     gather_data GD
     LEFT JOIN gather_9th_grade_spring_gpa G9GPA ON G9GPA.Contact_Id = GD.Contact_Id
     LEFT JOIN gather_aa_attendance AA ON AA.academic_semester_c = GD.AT_Id
+    LEFT JOIN ct_career_aa_attendance CTAA ON CTAA.student_c = GD.Contact_Id
 )
 SELECT
   *,
