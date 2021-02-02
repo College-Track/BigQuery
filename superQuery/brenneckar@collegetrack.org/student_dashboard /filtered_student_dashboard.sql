7d0d5bd7c3ebba14a986176876b2c951dc45a9aa
Baker Renneckar <baker@bakerr.com>
2021-02-02 13:48:17 -0800

    Auto-commit: Tue Feb 02 2021 13:48:06 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/student_dashboard /filtered_student_dashboard.sql
+++ superQuery/brenneckar@collegetrack.org/student_dashboard /filtered_student_dashboard.sql
@@ -7,7 +7,7 @@ WITH gather_student_data AS (
     grade_c,
     full_name_c,
     CASE
-    --   WHEN full_name_c = 'Daniel Aguilar' THEN 'bakerrenneckar@gmail.com'
+      WHEN full_name_c = 'Jordan Soto' THEN 'bakerrenneckar@gmail.com'
       WHEN full_name_c = 'Daniel Aguilar' THEN 'brenneckar@collegetrack.org'
     --   WHEN full_name_c = 'Daniel Aguilar' THEN'baker_test12345@yahoo.com'
 
