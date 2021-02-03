1f0f2689d635f87cb34d3309ac05dfeeefb1f885
Baker Renneckar <baker@bakerr.com>
2021-02-03 15:35:21 -0800

    Auto-commit: Wed Feb 03 2021 15:35:11 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_kpi_prep.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/4_attendance_kpi_prep.sql
@@ -1,61 +1,44 @@
+WITH gather_data AS (
+  SELECT
+    Contact_Id,
+    AT_Id,
+    site_short,
+    HIGH_SCHOOL_GRADUATING_CLASS_c,
+    REPLACE(GAS_Name, ' (Semester)', '') AS Academic_Term,
+    region_short,
+    region_abrev site_abrev,
+    CASE
+      WHEN GPA_prev_semester_cumulative_c < 2.5 THEN '2.49 or less'
+      WHEN GPA_prev_semester_cumulative_c >= 2.5
+      AND GPA_prev_semester_cumulative_c < 2.75 THEN '2.5 - 2.74'
+      WHEN GPA_prev_semester_cumulative_c >= 2.75
+      AND GPA_prev_semester_cumulative_c < 3 THEN '2.75 - 2.99'
+      WHEN GPA_prev_semester_cumulative_c >= 3
+      AND GPA_prev_semester_cumulative_c < 3.5 THEN '3.0 - 3.49'
+      ELSE '3.5 or Greater'
+    END GPA_Bucket,
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_at_template` AS Contact
+),
+calc_attendance AS (
+  SELECT
+    student_c,
+    academic_semester_c,
+    SUM(Attendance_Denominator_c) AS Attendance_Denominator_c,
+    SUM(Attendance_Numerator_c) AS Attendance_Numerator_c,
+    CASE
+      WHEN SUM(Attendance_Denominator_c) = 0 THEN NULL
+      ELSE SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c)
+    END AS attendance_rate
+  FROM
+    `data-warehouse-289815.salesforce_clean.class_template`
+  GROUP BY
+    student_c,
+    academic_semester_c
+)
 SELECT
-  Contact_Id,
-  Full_Name_c,
-  site,
-  HIGH_SCHOOL_GRADUATING_CLASS_c,
-  Indicator_High_Risk_for_Dismissal_c,
-  Indicator_Low_Income_c,
-  Ethnic_background_c,
-  First_Generation_c,
-  College_Track_Status_c,
-  status.Status,
-  Global_Academic_Semester_c,
-  Indicator_Student_on_Intervention_c,
-  Composite_Readiness_Most_Recent_c,
-  REPLACE(GAT.Name, ' (Semester)', '') AS Academic_Term,
-  region,
-  GPA_Bucket_running_cumulative_c,
-  Student_c,
-  GPA_prev_semester_cumulative_c,
-  Indicator_Sem_Attendance_Above_80_c,
-  Indicator_Sem_Attendance_Below_65_c,
-  CASE
-    WHEN GPA_prev_semester_cumulative_c < 2.5 THEN '2.49 or less'
-    WHEN GPA_prev_semester_cumulative_c >= 2.5
-  AND GPA_prev_semester_cumulative_c < 2.75 THEN '2.5 - 2.74'
-    WHEN GPA_prev_semester_cumulative_c >= 2.75 AND GPA_prev_semester_cumulative_c < 3 THEN '2.75 - 2.99'
-    WHEN GPA_prev_semester_cumulative_c >= 3
-  AND GPA_prev_semester_cumulative_c < 3.5 THEN '3.0 - 3.49'
-  ELSE
-  '3.5 or Greater'
-END
-  GPA_Bucket,
-  Contact.site_abrev,
-  Contact.region_abrev
-  
+  GD.*,
+  CA.attendance_rate
 FROM
-  `data-warehouse-289815.salesforce_clean.contact_at_template` AS Contact
-
-LEFT JOIN (
-  SELECT
-    api_name,
-    Status
-  FROM
-    `data-warehouse-289815.roles.ct_status` ) AS status
-    
-ON
-  Contact.College_Track_Status_c = status.api_name
-  
-LEFT JOIN (
-  SELECT
-    Name,
-    Id
-  FROM
-    `data-warehouse-289815.salesforce.global_academic_semester_c`) AS GAT
-ON
-  GAT.Id = Global_Academic_Semester_c
-WHERE
-  Contact.Site_Text_c != 'College Track Arlen'
-  
-  AND (status.Status = 'Current CT HS Student')
-  AND  Contact.attendance_rate_c IS NOT NULL
+  gather_data GD
+  LEFT JOIN calc_attendance CA ON CA.academic_semester_c = GD.AT_Id
