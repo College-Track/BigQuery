1a3a0a8ac2596669450938f115d69ee9d25283c5
Baker Renneckar <baker@bakerr.com>
2021-02-03 17:08:11 -0800

    Auto-commit: Wed Feb 03 2021 17:08:07 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
@@ -1,3 +1,3 @@
-SELECT attendance_numerator_c, attendance_denominator_c
+SELECT attendance_numerator_c, attendance_denominator_c, date_c
 FROM `data-warehouse-289815.salesforce_clean.class_template`
 WHERE Academic_Semester_c ='a1a460000019uAjAAI'
