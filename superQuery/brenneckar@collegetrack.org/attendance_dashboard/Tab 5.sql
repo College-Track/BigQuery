5f8ae6985255762b391b6b5048355320d69f503a
Baker Renneckar <baker@bakerr.com>
2021-02-03 18:09:17 -0800

    Auto-commit: Wed Feb 03 2021 18:09:14 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
-SELECT *
+SELECT SUM(student_count)
 FROM `data-studio-260217.attendance_dashboard.kpi_prep`
 WHERE Workshop_Global_Academic_Semester_c = 'Fall 2020-21' AND site_short = "Boyle Heights"
