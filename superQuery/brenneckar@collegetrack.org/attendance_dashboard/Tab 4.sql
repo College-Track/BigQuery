3d7c2e99535cf987318fb0d88d021ad28ea70647
Baker Renneckar <baker@bakerr.com>
2021-02-03 17:10:48 -0800

    Auto-commit: Wed Feb 03 2021 17:10:45 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 4.sql
@@ -1,3 +1,3 @@
 SELECT attendance_numerator_c, attendance_denominator_c, date_c, workshop_display_name_c
 FROM `data-studio-260217.attendance_dashboard.attendance_filtered_data`
-WHERE Academic_Semester_c ='a1a460000019uAjAAI' AND attendance_denominator_c > 0
+WHERE Academic_Semester_c ='a1a460000019uAjAAI'
