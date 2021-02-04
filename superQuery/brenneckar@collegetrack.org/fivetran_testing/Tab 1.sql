bae8ff2db6bd8919dc3f64403175d21586a8f36d
Baker Renneckar <baker@bakerr.com>
2021-02-04 10:05:50 -0800

    Auto-commit: Thu Feb 04 2021 10:05:44 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,9 +1,6 @@
 SELECT
     roles.site_short,
     roles.role
-  FROM
-    `data-warehouse-289815.salesforce.user` AS Users
-  LEFT JOIN
+
+FROM
     `data-warehouse-289815.roles.role_table` AS Roles
-  ON
-    Roles.Role_Id=Users.user_role_id
