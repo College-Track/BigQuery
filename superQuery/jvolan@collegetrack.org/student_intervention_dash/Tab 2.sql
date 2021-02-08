936438b0da48338dde0afbfaa78130d6f955f26a
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-02-08 10:20:41 -0800

    Auto-commit: Mon Feb 08 2021 10:20:34 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 2.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 2.sql
@@ -3,6 +3,9 @@ SELECT
     WhatId,
     ActivityDate AS Date,
     Subject,
-    Description
+    Description,
+    X18_Digit_Activity_ID__c,
+    Reciprocal_Communication__c
+    
     FROM `data-warehouse-289815.salesforce_raw.Task`
-    WHERE CreatedDate BETWEEN DATE_SUB(CURRENT_DATE(),INTERVAL 6 MONTH) AND CURRENT_DATE()
+    WHERE CreatedDate BETWEEN DATE_SUB(CURRENT_DATE(),INTERVAL 1 MONTH) AND CURRENT_DATE()
