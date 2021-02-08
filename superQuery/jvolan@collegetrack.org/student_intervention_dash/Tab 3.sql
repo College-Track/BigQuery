d0c8369a52653851823334fe10cb80a830e614b2
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-02-08 11:51:25 -0800

    Auto-commit: Mon Feb 08 2021 11:51:18 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 3.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 3.sql
@@ -1,4 +1,4 @@
-SELECT
+ SELECT
     WhoId,
     WhatId AS Related_to,
     ActivityDate AS Date,
@@ -8,4 +8,5 @@ SELECT
     Reciprocal_Communication__c
     
     FROM `data-warehouse-289815.salesforce_raw.Task`
-    WHERE CreatedDate BETWEEN DATE_SUB(CURRENT_DATE(),INTERVAL 12 MONTH) AND CURRENT_DATE()
+    WHERE NOT (Subject LIKE '%List Email%')
+    AND CreatedDate BETWEEN DATE_SUB(CURRENT_DATE(),INTERVAL 12 MONTH) AND CURRENT_DATE()
