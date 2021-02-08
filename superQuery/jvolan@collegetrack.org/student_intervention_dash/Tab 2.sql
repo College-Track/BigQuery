18a908da08780d9b5c7ae5cda9ffd55b73ea26bb
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-02-08 11:15:41 -0800

    Auto-commit: Mon Feb 08 2021 11:15:34 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 2.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 2.sql
@@ -10,7 +10,7 @@ WITH recent_logged_activities AS
     Reciprocal_Communication__c
     
     FROM `data-warehouse-289815.salesforce_raw.Task`
-    WHERE CreatedDate BETWEEN DATE_SUB(CURRENT_DATE(),INTERVAL 12 MONTH) AND CURRENT_DATE()
+    WHERE CreatedDate BETWEEN DATE_SUB(CURRENT_DATE(),INTERVAL 1 MONTH) AND CURRENT_DATE()
 )
 
 SELECT
