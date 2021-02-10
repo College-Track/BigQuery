442c02d63376bdd91fc9c088724dcb4f013bfbdc
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-02-10 13:20:18 -0800

    Auto-commit: Wed Feb 10 2021 13:20:12 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/activities_tasks.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/activities_tasks.sql
@@ -1,11 +1,20 @@
-SELECT #full_name_c,
-subject, who_id AS contact_id_task, id, what_id AS academic_term_task, #description 
+SELECT 
+full_name_c,
+subject, 
+who_id AS contact_id_task,  
+what_id AS academic_term_task, 
+id,
+description 
+
 FROM `data-warehouse-289815.salesforce.task`AS T
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS C
 ON C.AT_id = T.what_id
-WHERE subject = "CT Corporate Residency Summer 2019"
+WHERE subject = "CT Corporate Residency Summer 2020"
 AND T.is_deleted = FALSE
-GROUP BY
 
-#ull_name_c,
-subject, who_id, id, what_id
+/*GROUP BY
+full_name_c,
+subject, who_id, 
+what_id 
+#id, 
+*/
