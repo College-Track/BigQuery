134974bed3cab5d3308123bd7845488e0d2f6b22
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-02-10 12:53:17 -0800

    Auto-commit: Wed Feb 10 2021 12:47:22 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/activities_tasks.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/activities_tasks.sql
@@ -1,6 +1,4 @@
-SELECT full_name_c,subject, who_id, id, description 
-FROM `data-warehouse-289815.salesforce.task`AS T
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` AS C
-ON C.Contact_Id = T.who_id
+SELECT subject, who_id, id, description 
+FROM `data-warehouse-289815.salesforce.task`
 WHERE subject = "CT Corporate Residency Summer 2019"
-AND T.is_deleted = FALSE
+AND is_deleted = FALSE
