a9cfc8af1b3a4d118f554746fb433a8ff179c38b
Baker Renneckar <baker@bakerr.com>
2021-02-10 12:37:02 -0800

    Auto-commit: Wed Feb 10 2021 12:31:01 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -5,11 +5,6 @@ WHERE subject LIKE '%CT Corporate Residency Summer%' AND C.region_short = 'Color
 AND T.is_deleted = false
 )
 
-SELECT subject, count(distinct(who_id)) AS student_count
+SELECT site_short, subject, count(who_id) AS student_count
 FROM gather_data
-GROUP BY subject
-
-
--- SELECT subject, description, count(distinct(who_id)) AS student_count
--- FROM gather_data
--- GROUP BY subject, description
+GROUP BY site_short, subject
