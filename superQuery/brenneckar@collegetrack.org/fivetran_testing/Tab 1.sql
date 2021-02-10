21a901468b1f79219283a596aba9fbca745c2375
Baker Renneckar <baker@bakerr.com>
2021-02-10 12:28:06 -0800

    Auto-commit: Wed Feb 10 2021 12:28:03 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,5 +1,9 @@
-SELECT COUNT(id), subject 
+WITH gather_data AS (SELECT subject, who_id, description, C.site_short
 FROM `data-warehouse-289815.salesforce.task`
+LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = who_id
 WHERE subject LIKE '%CT Corporate Residency Summer%'
 AND is_deleted = false
-GROUP BY subject
+)
+
+SELECT *
+FROM gather_data
