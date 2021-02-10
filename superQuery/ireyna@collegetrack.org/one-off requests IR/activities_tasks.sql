fee302778f56a53ae342367bb8c121a71ab68fc4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-02-10 13:00:11 -0800

    Auto-commit: Wed Feb 10 2021 12:47:03 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/activities_tasks.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/activities_tasks.sql
@@ -1,4 +1,4 @@
-SELECT subject, who_id, id
+SELECT subject, who_id, id, description 
 FROM `data-warehouse-289815.salesforce.task`
 WHERE subject = "CT Corporate Residency Summer 2019"
 AND is_deleted = FALSE
