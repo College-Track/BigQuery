8bac7abf2d71121aa1704f99a1636b34d85c3b69
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-02-11 10:33:31 -0800

    Auto-commit: Thu Feb 11 2021 10:33:26 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 5.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 5.sql
@@ -1,4 +1,4 @@
-WITH workshop_enrolled AS
+WITH workshop_enrollments AS
 (
 SELECT
     COUNT(Class__c) AS workshops_enrolled,
@@ -6,8 +6,10 @@ SELECT
     FROM `data-warehouse-289815.salesforce_raw.Class_Registration__c`
     WHERE Status__c = 'Enrolled'
     GROUP BY Academic_Semester__c
-)
+),
 
+student_at AS
+(
 SELECT
 --basic contact info & demos
     Contact_Id,
@@ -16,9 +18,15 @@ SELECT
     region_abrev AS region,
     AT_Name,
     AT_Id,
-    workshop_enrolled.Academic_Semester__c,
-    workshop_enrolled.workshops_enrolled,
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    LEFT JOIN workshop_enrolled ON workshop_enrolled.Academic_Semester__c = AT_Id
     WHERE current_as_c = TRUE
     AND college_track_status_c IN ("11A", "12A")
+)
+
+SELECT  
+    student_at.*,
+    workshop_enrollments.Academic_Semester__c,
+    workshop_enrollments.workshops_enrolled,
+    
+    FROM student_at
+    LEFT JOIN workshop_enrollments ON workshop_enrollments.Academic_Semester__c = AT_Id
