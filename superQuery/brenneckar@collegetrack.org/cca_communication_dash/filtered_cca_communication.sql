ff506d303f7c914c65a177a15906b8c827072069
Baker Renneckar <baker@bakerr.com>
2021-02-16 12:13:02 -0800

    Auto-commit: Tue Feb 16 2021 12:12:58 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
@@ -1,9 +1,22 @@
 WITH gather_data AS(
-SELECT Contact_Id, full_name_c,
-FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-WHERE current_as_c = True AND college_track_status_c = '15A'
+  SELECT
+    Contact_Id,
+    full_name_c,
+    on_track_c,
+    current_cc_advisor_2_c,
+    current_enrollment_status_c,
+    Current_school_name,
+    Most_Recent_GPA_Cumulative_bucket,
+    most_recent_gpa_semester_bucket
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_at_template`
+  WHERE
+    current_as_c = True
+    AND college_track_status_c = '15A'
 )
-
-SELECT *
-FROM gather_data
-LIMIT 10
+SELECT
+  *
+FROM
+  gather_data
+LIMIT
+  10
