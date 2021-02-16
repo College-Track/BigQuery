d964cca347c7a2fa3360d7c9e4757eacfc1910bf
Baker Renneckar <baker@bakerr.com>
2021-02-16 12:18:34 -0800

    Auto-commit: Tue Feb 16 2021 12:18:29 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
@@ -2,7 +2,6 @@ WITH gather_data AS(
   SELECT
     Contact_Id,
     full_name_c,
-    on_track_c,
     current_cc_advisor_2_c,
     current_enrollment_status_c,
     Current_school_name,
@@ -11,17 +10,33 @@ WITH gather_data AS(
     grade_c,
     credit_accumulation_pace_c,
     high_school_graduating_class_c,
-    credits_accumulated_most_recent_c,
-    indicator_graduated_or_on_track_at_c
+    credits_accumulated_most_recent_c
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template`
   WHERE
     current_as_c = True
     AND college_track_status_c = '15A'
+),
+gather_communication_data AS (
+  SELECT
+    who_id,
+    reciprocal_communication_c,
+    date_of_contact_c
+  FROM
+    `data-warehouse-289815.salesforce.task`
+),
+join_data AS (
+  SELECT
+    GD.*,
+    GCD.reciprocal_communication_c,
+    GCD.date_of_contact_c
+  FROM
+    gather_data GD
+    LEFT JOIN gather_communication_data GCD ON GCD.who_id = GD.Contact_Id
 )
 SELECT
   *
 FROM
-  gather_data
+  join_data
 LIMIT
-  10
+  100
