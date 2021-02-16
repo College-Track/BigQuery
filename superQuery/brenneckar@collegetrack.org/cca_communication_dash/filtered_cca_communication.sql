5e40e0fa00eb537c1d5a15cf9b492ba85ab95265
Baker Renneckar <baker@bakerr.com>
2021-02-16 12:14:26 -0800

    Auto-commit: Tue Feb 16 2021 12:14:22 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
@@ -11,7 +11,8 @@ WITH gather_data AS(
     grade_c,
     credit_accumulation_pace_c,
     high_school_graduating_class_c,
-    credits_accumulated_most_recent_c
+    credits_accumulated_most_recent_c,
+    indicator_graduated_or_on_track_at_c
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template`
   WHERE
