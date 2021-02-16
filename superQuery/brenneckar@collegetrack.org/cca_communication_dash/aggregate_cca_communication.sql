1d49cd307f936e1ccd53a21efa3553098cd38b59
Baker Renneckar <baker@bakerr.com>
2021-02-16 12:49:09 -0800

    Auto-commit: Tue Feb 16 2021 12:49:05 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/aggregate_cca_communication.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/aggregate_cca_communication.sql
@@ -10,6 +10,10 @@ SELECT
     site_abrev,
     region_short,
     region_abrev,
+    reciprocal_30_days_or_less,
+    reciprocal_more_than_60_days,
+    outreach_30_days_or_less,
+    outreach_more_than_60_days,
   COUNT(Contact_Id) AS student_count
 FROM
   `data-studio-260217.cca_communication.filtered_cca_communication`
@@ -24,4 +28,8 @@ GROUP BY
     site_sort,
     site_abrev,
     region_short,
-    region_abrev
+    region_abrev,
+    reciprocal_30_days_or_less,
+    reciprocal_more_than_60_days,
+    outreach_30_days_or_less,
+    outreach_more_than_60_days
