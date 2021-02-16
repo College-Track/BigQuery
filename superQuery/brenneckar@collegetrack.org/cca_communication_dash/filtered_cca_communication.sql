4d79b7e6a2695ede08b48afb35b8e78c8c8c12e8
Baker Renneckar <baker@bakerr.com>
2021-02-16 12:28:53 -0800

    Auto-commit: Tue Feb 16 2021 12:28:48 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
@@ -16,10 +16,55 @@ WITH gather_data AS(
   WHERE
     current_as_c = True
     AND college_track_status_c = '15A'
+),
+gather_communication_data AS (
+  SELECT
+    who_id,
+    reciprocal_communication_c,
+    date_of_contact_c,
+    subject,
+    id AS task_id,
+    description,
+    type
+  FROM
+    `data-warehouse-289815.salesforce.task`
+),
+most_recent_reciprocal AS (
+  SELECT
+    who_id,
+    MAX(date_of_contact_c) AS most_recent_reciprocal_date
+  FROM
+    gather_communication_data
+  WHERE
+    reciprocal_communication_c = TRUE
+  GROUP BY
+    who_id
+),
+most_recent_outreach AS (
+  SELECT
+    who_id,
+    MAX(date_of_contact_c) AS most_recent_outreach_date
+  FROM
+    gather_communication_data
+  GROUP BY
+    who_id
+),
+join_data AS (
+  SELECT
+    GD.*,
+    GCD.*
+  EXCEPT(who_id),
+    MRR.most_recent_reciprocal_date,
+    MRO.most_recent_outreach_date
+  FROM
+    gather_data GD
+    LEFT JOIN gather_communication_data GCD ON GCD.who_id = GD.Contact_Id
+    LEFT JOIN most_recent_reciprocal MRR ON MRR.who_id = GD.Contact_Id
+    LEFT JOIN most_recent_outreach MRO ON MRO.who_id = GD.Contact_Id
 )
 SELECT
   *
 FROM
-  gather_data
+  join_data
 LIMIT
-  10
+  100
