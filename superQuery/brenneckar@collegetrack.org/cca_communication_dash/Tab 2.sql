efcf048a8e9229c44f451305d6531a890ee8f06d
Baker Renneckar <baker@bakerr.com>
2021-02-16 16:14:27 -0800

    Auto-commit: Tue Feb 16 2021 16:14:22 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 2.sql
@@ -1,8 +1,28 @@
+WITH gather_data AS (
+  SELECT
+    Contact_Id,
+    value.*
+  FROM
+    `data-studio-260217.cca_communication.filtered_cca_communication` rubric_colors,
+    UNNEST(
+      `data-warehouse-289815.UDF.unpivot`(rubric_colors, 'sort_Advising_Rubric_')
+    ) value
+)
 SELECT
   Contact_Id,
-  value.*
+--   CASE
+--     WHEN value IS NULL THEN "No Data"
+--     WHEN value = 4 THEN "No Data"
+--     WHEN value = 3 THEN "Green"
+--     WHEN value = 2 THEN "Yellow"
+--     WHEN value = 1 THEN "Red"
+--     ELSE "No Data"
+--   END AS rubric_section_color,
+  CASE
+    WHEN key = 'sort_Advising_Rubric_Career_Readiness_sort' THEN "Career"
+    WHEN key = 'sort_Advising_Rubric_Wellness_sort' THEN 'Wellness'
+    WHEN key = 'sort_Advising_Rubric_Financial_Success_sort' THEN "Finance"
+    WHEN key = 'sort_Advising_Rubric_Academic_Readiness_sort' THEN "Academic"
+  END AS rubric_section
 FROM
-  `data-studio-260217.cca_communication.filtered_cca_communication` rubric_colors,
-  UNNEST(
-    `data-warehouse-289815.UDF.unpivot`(rubric_colors, 'Advising_Rubric_')
-  ) value
+  gather_data
