afb4a525eb48b698b580caa4b0bc21ac348cb8c6
Baker Renneckar <baker@bakerr.com>
2021-02-17 15:25:59 -0800

    Auto-commit: Wed Feb 17 2021 15:25:56 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/student_dashboard /Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/student_dashboard /Tab 4.sql
@@ -1,8 +1,11 @@
 SELECT
   email,
   full_name_c,
-  SPLIT(email, "@")
+  CONCAT(
+    REPLACE(SPLIT(email, "@") [OFFSET(0)], ".",""),
+    CONCAT("@", SPLIT(email, "@") [OFFSET(1)])
+  ) AS test
 FROM
   `data-studio-260217.student_dashboard.student_dashboard`
 WHERE
-  Contact_Id = '0031M000032xrocQAA'
+  Contact_Id = '0034600001FWF7zAAH'
