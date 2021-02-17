80e04265ceb4c7f2ce37980e58e09ce8d0af7a31
Baker Renneckar <baker@bakerr.com>
2021-02-16 16:10:30 -0800

    Auto-commit: Tue Feb 16 2021 16:04:25 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 2.sql
@@ -1,18 +1,8 @@
-WITH gather_data AS (
-  SELECT
-    Contact_Id,
-        Advising_Rubric_Academic_Readiness_c,
-    Advising_Rubric_Career_Readiness_c,
-    Advising_Rubric_Financial_Success_c,
-    Advising_Rubric_Wellness_c,
-    value.*
-  FROM
-    `data-studio-260217.cca_communication.filtered_cca_communication` rubric_colors,
-    UNNEST(
-      `data-warehouse-289815.UDF.unpivot`(rubric_colors, 'sort_Advising_Rubric_')
-    ) value
-)
 SELECT
-  *
+  Contact_Id,
+  value.*
 FROM
-  gather_data
+  `data-studio-260217.cca_communication.filtered_cca_communication` rubric_colors,
+  UNNEST(
+    `data-warehouse-289815.UDF.unpivot`(rubric_colors, 'Advising_Rubric_')
+  ) value
