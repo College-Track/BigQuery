78c66ceec6d6698c784e478aa9ee2d77eb58beb1
Baker Renneckar <baker@bakerr.com>
2021-02-17 13:32:49 -0800

    Auto-commit: Wed Feb 17 2021 13:32:45 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 4.sql
@@ -1,3 +1,20 @@
-SELECT *
-FROM `data-studio-260217.cca_communication.filtered_cca_communication`
-WHERE avg_days_between_outreach < 0
+with gather_data AS (SELECT
+  DISTINCT Contact_Id,
+  full_name_c,
+  ABS(
+    DATE_DIFF(
+      most_recent_outreach_date,
+      first_outreach_date,
+      DAY
+    )) AS days_between_first_last_outreach,
+    most_recent_outreach_date,
+    first_outreach_date,
+    num_of_outreach_comms,
+    avg_days_between_outreach
+    FROM
+      `data-studio-260217.cca_communication.filtered_cca_communication`
+      
+      )
+      
+      SELECT AVG(avg_days_between_outreach)
+      FROM gather_data
