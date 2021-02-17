d82eb31314aa70d802164b3f4092df6202196702
Baker Renneckar <baker@bakerr.com>
2021-02-16 16:27:44 -0800

    Auto-commit: Tue Feb 16 2021 16:14:45 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 2.sql
@@ -1,24 +1,6 @@
 WITH gather_data AS (
   SELECT
     Contact_Id,
-    full_name_c,
-    current_cc_advisor_2_c,
-    current_enrollment_status_c,
-    Current_school_name,
-    Most_Recent_GPA_Cumulative_bucket,
-    most_recent_gpa_semester_bucket,
-    grade_c,
-    credit_accumulation_pace_c,
-    high_school_graduating_class_c,
-    credits_accumulated_most_recent_c,
-    anticipated_date_of_graduation_ay_c,
-    site_short,
-    site_sort,
-    site_abrev,
-    region_short,
-    region_abrev,
-    school_type,
-    
     value.*
   FROM
     `data-studio-260217.cca_communication.filtered_cca_communication` rubric_colors,
@@ -27,16 +9,15 @@ WITH gather_data AS (
     ) value
 )
 SELECT
-  * EXCEPT(key, value),
-
-  CASE
-    WHEN value IS NULL THEN "No Data"
-    WHEN value = '4' THEN "No Data"
-    WHEN value = '3' THEN "Green"
-    WHEN value = '2' THEN "Yellow"
-    WHEN value = '1' THEN "Red"
-    ELSE "No Data"
-  END AS rubric_section_color,
+  *,
+--   CASE
+--     WHEN value IS NULL THEN "No Data"
+--     WHEN value = 4 THEN "No Data"
+--     WHEN value = 3 THEN "Green"
+--     WHEN value = 2 THEN "Yellow"
+--     WHEN value = 1 THEN "Red"
+--     ELSE "No Data"
+--   END AS rubric_section_color,
   CASE
     WHEN key = 'sort_Advising_Rubric_Career_Readiness_sort' THEN "Career"
     WHEN key = 'sort_Advising_Rubric_Wellness_sort' THEN 'Wellness'
