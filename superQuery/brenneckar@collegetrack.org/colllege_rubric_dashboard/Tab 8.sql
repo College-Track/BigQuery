8f0baa43549293df6c333aac07c41156ad4147de
Baker Renneckar <baker@bakerr.com>
2021-02-18 11:13:33 -0800

    Auto-commit: Thu Feb 18 2021 11:13:30 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 8.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/Tab 8.sql
@@ -1,5 +1,5 @@
-SELECT Contact_Id
-Advising_Rubric_Academic_Readiness_v2_c,
+SELECT Contact_Id,
+Advising_Rubric_Academic_Readiness_v_2_c,
 Advising_Rubric_Academic_Readiness_c
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE Advising_Rubric_Academic_Readiness_c IS NOT NULL
