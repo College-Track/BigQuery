cc4e0fa3f183ae558c2abcbfb89f6d15f8dbdb21
Baker Renneckar <baker@bakerr.com>
2021-02-18 16:39:19 -0800

    Auto-commit: Thu Feb 18 2021 16:39:13 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/college_rubric_filtered_view.sql
+++ superQuery/brenneckar@collegetrack.org/colllege_rubric_dashboard/college_rubric_filtered_view.sql
@@ -7,11 +7,11 @@ RIGHT JOIN (
     roles.site_short,
     roles.role
   FROM
-    `data-warehouse-289815.salesforce_raw.User` AS Users
+    `data-warehouse-289815.salesforce_clean.user_clean` AS Users
   LEFT JOIN
     `data-warehouse-289815.roles.role_table` AS Roles
   ON
-    Roles.Role_Id=Users.UserRoleId
+    Roles.Role_Id=Users.user_role_id
   WHERE
     LOWER(users.Email)='lizfuentes@collegetrack.org'
     ) AS Permissions
