96cbae06a5ba03f8690a72760d13014a896c5781
Baker Renneckar <baker@bakerr.com>
2021-02-18 16:38:17 -0800

    Auto-commit: Thu Feb 18 2021 16:38:12 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roles/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/roles/Tab 3.sql
@@ -1,2 +1,2 @@
 INSERT `data-warehouse-289815.salesforce_clean.user_clean` (id, email, user_role_id)
-VALUES ('100','op_testing@collegetrack.org', '00E46000000YcNXEA0')
+VALUES ('99 ','lizfuentes@collegetrack.org', '00E46000000YcNSEA0')
