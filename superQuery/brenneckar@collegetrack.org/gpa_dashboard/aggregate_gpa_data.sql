7f2b61ae0d2e9f6e8485c7ec09ff4b61027434a4
Baker Renneckar <baker@bakerr.com>
2021-02-23 15:51:09 -0800

    Auto-commit: Tue Feb 23 2021 15:51:02 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/gpa_dashboard/aggregate_gpa_data.sql
+++ superQuery/brenneckar@collegetrack.org/gpa_dashboard/aggregate_gpa_data.sql
@@ -24,6 +24,7 @@ WITH gather_data AS (
   FROM
     `data-studio-260217.gpa_dashboard.filtered_gpa_data`
   WHERE GAS_Name = 'Spring 2019-20' -- Has to be manually adjusted each new complete term
+  AND student_audit_status_c IN ('Current CT HS Student', 'Leave of Absence')
   GROUP BY
     region_abrev,
     site_abrev,
