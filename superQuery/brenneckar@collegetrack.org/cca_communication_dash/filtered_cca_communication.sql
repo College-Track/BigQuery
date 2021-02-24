087639982873db1331017ed9f7753d4dab4934c1
Baker Renneckar <baker@bakerr.com>
2021-02-24 10:35:42 -0800

    Auto-commit: Wed Feb 24 2021 10:35:34 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/filtered_cca_communication.sql
@@ -23,27 +23,27 @@ WITH gather_data AS(
     Advising_Rubric_Financial_Success_c,
     Advising_Rubric_Wellness_c,
     CASE
-      WHEN Advising_Rubric_Financial_Success_c = "Red" THEN 1
-      WHEN Advising_Rubric_Financial_Success_c = "Yellow" THEN 2
-      WHEN Advising_Rubric_Financial_Success_c = "Green" THEN 3
+      WHEN advising_rubric_financial_success_v_2_c = "Red" THEN 1
+      WHEN advising_rubric_financial_success_v_2_c = "Yellow" THEN 2
+      WHEN advising_rubric_financial_success_v_2_c = "Green" THEN 3
       ELSE 4
     END AS sort_Advising_Rubric_Financial_Success_sort,
     CASE
-      WHEN Advising_Rubric_Academic_Readiness_c = "Red" THEN 1
-      WHEN Advising_Rubric_Academic_Readiness_c = "Yellow" THEN 2
-      WHEN Advising_Rubric_Academic_Readiness_c = "Green" THEN 3
+      WHEN advising_rubric_academic_readiness_v_2_c = "Red" THEN 1
+      WHEN advising_rubric_academic_readiness_v_2_c = "Yellow" THEN 2
+      WHEN advising_rubric_academic_readiness_v_2_c = "Green" THEN 3
       ELSE 4
     END AS sort_Advising_Rubric_Academic_Readiness_sort,
     CASE
-      WHEN Advising_Rubric_Career_Readiness_c = "Red" THEN 1
-      WHEN Advising_Rubric_Career_Readiness_c = "Yellow" THEN 2
-      WHEN Advising_Rubric_Career_Readiness_c = "Green" THEN 3
+      WHEN advising_rubric_career_readiness_v_2_c = "Red" THEN 1
+      WHEN advising_rubric_career_readiness_v_2_c = "Yellow" THEN 2
+      WHEN advising_rubric_career_readiness_v_2_c = "Green" THEN 3
       ELSE 4
     END AS sort_Advising_Rubric_Career_Readiness_sort,
     CASE
-      WHEN Advising_Rubric_Wellness_c = "Red" THEN 1
-      WHEN Advising_Rubric_Wellness_c = "Yellow" THEN 2
-      WHEN Advising_Rubric_Wellness_c = "Green" THEN 3
+      WHEN advising_rubric_wellness_v_2_c = "Red" THEN 1
+      WHEN advising_rubric_wellness_v_2_c = "Yellow" THEN 2
+      WHEN advising_rubric_wellness_v_2_c = "Green" THEN 3
       ELSE 4
     END AS sort_Advising_Rubric_Wellness_sort,
     CASE
