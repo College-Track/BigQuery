3437575a09dc3390c25af17129a99302ae3dd4c7
Baker Renneckar <baker@bakerr.com>
2021-02-24 10:32:18 -0800

    Auto-commit: Wed Feb 24 2021 10:32:15 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 6.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 6.sql
@@ -1,3 +1,3 @@
 SELECT *
 FROM `data-studio-260217.cca_communication.filtered_cca_communication`
-WHERE Contact_Id = '0034600001TQtymAAD'
+WHERE Contact_Id IN ('0034600001TQtymAAD', '0034600001TQrJRAA1')
