65732d2ed7a87c9c4fbd6413f35227e7b45eedfd
Baker Renneckar <baker@bakerr.com>
2021-02-24 15:38:08 -0800

    Auto-commit: Wed Feb 24 2021 15:38:03 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/users_clean/users_clean.sql
@@ -0,0 +1,34 @@
+WITH gather_data AS (
+  SELECT
+    id,
+    first_name,
+    last_name,
+    is_active,
+    user_role_id,
+    email
+    
+  FROM
+    `data-warehouse-289815.salesforce.user`
+  WHERE
+    is_active = True
+),
+gather_valid_part_time AS (
+  SELECT
+    *
+  FROM
+    `data-warehouse-289815.salesforce_clean.tmp_pt_user`
+  WHERE
+    email NOT IN (
+      SELECT
+        email
+      FROM
+        gather_data
+    )
+)
+
+SELECT *
+FROM gather_data
+UNION ALL (
+SELECT *
+FROM
+gather_valid_part_time)
