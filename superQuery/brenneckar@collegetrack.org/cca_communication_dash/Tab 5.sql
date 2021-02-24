b14ac86806f482c986ea64587ed1f4f9444277f4
Baker Renneckar <baker@bakerr.com>
2021-02-24 09:41:45 -0800

    Auto-commit: Wed Feb 24 2021 09:41:39 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 5.sql
@@ -1,3 +1,38 @@
-SELECT Id, Who_Id, Activity_Date, date_of_contact_c
-FROM `data-warehouse-289815.salesforce.task`
-WHERE Who_Id = '0034600001TQuZdAAL'
+WITH gather_all_communication_data AS (
+  SELECT
+    who_id,
+    reciprocal_communication_c,
+    CASE
+      WHEN activity_date = date_of_contact_c THEN date_of_contact_c
+      WHEN date_of_contact_c IS NULL
+      and activity_date IS NOT NULL THEN activity_date
+      ELSE date_of_contact_c
+    END AS date_of_contact_c,
+    subject,
+    id AS task_id,
+    description,
+    type
+  FROM
+    `data-warehouse-289815.salesforce.task`
+    
+),
+gather_communication_data AS (
+SELECT *
+FROM gather_all_communication_data
+WHERE date_of_contact_c BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 365 DAY) AND CURRENT_DATE()
+
+)
+-- most_recent_outreach AS (
+--   SELECT
+--     who_id,
+--     MAX(date_of_contact_c) AS most_recent_outreach_date,
+--     MIN(date_of_contact_c) AS first_outreach_date,
+--     COUNT(task_id) AS num_of_outreach_comms
+--   FROM
+--     gather_communication_data
+--   GROUP BY
+--     who_id
+-- )
+
+SELECT MAX(date_of_contact_c), MIN(date_of_contact_c)
+FROM gather_communication_data
