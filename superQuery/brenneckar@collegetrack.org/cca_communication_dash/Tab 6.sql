9c883249183adff14d461bd59d0543ba87f6ca8a
Baker Renneckar <baker@bakerr.com>
2021-02-24 11:06:13 -0800

    Auto-commit: Wed Feb 24 2021 11:06:09 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 6.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 6.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM `data-studio-260217.cca_communication.filtered_cca_communication`
-WHERE Contact_Id IN ('0034600001TQtymAAD', '0034600001TQrJRAA1')
+WHERE full_name_c IN ('Omar Martinez', '0034600001TQrJRAA1')
 ORDER BY Contact_Id
