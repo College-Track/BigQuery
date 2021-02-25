fcba9ab9f3c2d72ae7561dd1b071ff9b6a56a122
Baker Renneckar <baker@bakerr.com>
2021-02-25 13:34:49 -0800

    Auto-commit: Thu Feb 25 2021 13:34:36 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/cca_communication_dash/Tab 5.sql
@@ -20,15 +20,15 @@ gather_communication_data AS (
     *
   FROM
     gather_all_communication_data
-    -- LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.Contact_Id = who_id
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.Contact_Id = who_id
   WHERE
-  date_of_contact_c BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 365 DAY)
-    AND CURRENT_DATE()
---   CAT.current_as_c = true AND (CAT.grade_c != "Year 1" AND 
---     date_of_contact_c BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 365 DAY)
---     AND CURRENT_DATE()) OR (CAT.grade_c = "Year 1" AND 
---     date_of_contact_c BETWEEN '2020-06-01'
---     AND CURRENT_DATE())
+--   date_of_contact_c BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 365 DAY)
+--     AND CURRENT_DATE()
+  CAT.current_as_c = true AND (CAT.grade_c != "Year 1" AND 
+    date_of_contact_c BETWEEN DATE_SUB(CURRENT_DATE(), INTERVAL 365 DAY)
+    AND CURRENT_DATE()) OR (CAT.grade_c = "Year 1" AND 
+    date_of_contact_c BETWEEN '2020-06-01'
+    AND CURRENT_DATE())
 ),
 group_outreach_communication_data AS (
   SELECT
