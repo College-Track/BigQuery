4abc6e69411ec09603023fd23eac99c11e2213b2
Baker Renneckar <baker@bakerr.com>
2021-03-03 11:47:47 -0800

    Auto-commit: Wed Mar 03 2021 11:41:47 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 16.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 16.sql
@@ -1,4 +1,3 @@
 SELECT Contact_Id, most_recent_outreach, most_recent_reciprocal, latest_reciprocal_communication_date_c
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
-WHERE most_recent_reciprocal IS NOT NULL
 LIMIT 1000
