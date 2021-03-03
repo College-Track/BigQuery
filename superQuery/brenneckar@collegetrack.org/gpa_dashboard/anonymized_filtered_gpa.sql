d3d696e964e94484f166f0ae2428ff5141f22417
Baker Renneckar <baker@bakerr.com>
2021-03-03 10:56:09 -0800

    Auto-commit: Wed Mar 03 2021 10:56:04 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/gpa_dashboard/anonymized_filtered_gpa.sql
+++ superQuery/brenneckar@collegetrack.org/gpa_dashboard/anonymized_filtered_gpa.sql
@@ -1,6 +1,2 @@
-SELECT
-  *
-EXCEPT
-  (Contact_Id, full_name_c)
-FROM
-  `data-studio-260217.gpa_dashboard.filtered_gpa_data`
+SELECT * EXCEPT (Contact_Id,full_name_c )
+FROM `data-studio-260217.gpa_dashboard.filtered_gpa_data`
