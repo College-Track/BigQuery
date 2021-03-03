19a765c0ebd4e9139cb293a02ce3fc499ac9106e
Baker Renneckar <baker@bakerr.com>
2021-03-03 11:43:54 -0800

    Auto-commit: Wed Mar 03 2021 11:37:43 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 15.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 15.sql
@@ -9,16 +9,26 @@ WITH gather_all_communication_data AS (
       ELSE date_of_contact_c
     END AS date_of_contact_c,
   FROM
-    `data-warehouse-289815.salesforce.task`)
-  
-  
+    `data-warehouse-289815.salesforce.task`
+),
+most_recent_outreach AS (
   SELECT
-    who_id, (SELECT
-      MAX(date_of_contact_c) AS most_recent_outreach
-      FROM
-        gather_all_communication_data
-      GROUP BY
-        who_id
-    )
+    who_id, MAX(date_of_contact_c) AS most_recent_outreach
   FROM
     gather_all_communication_data
+    GROUP BY who_id
+),
+
+most_recent_reciprocal AS (
+  SELECT
+    who_id, MAX(date_of_contact_c) AS most_recent_reciprocal
+  FROM
+    gather_all_communication_data
+    WHERE reciprocal_communication_c = True
+    GROUP BY who_id
+    
+)
+
+SELECT *
+FROM most_recent_reciprocal
+LIMIT 10
