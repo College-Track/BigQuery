2975875757f73b5baf70dd3425320e7f896b26c1
Baker Renneckar <baker@bakerr.com>
2021-03-04 14:45:47 -0800

    Auto-commit: Thu Mar 04 2021 14:45:42 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
@@ -14,17 +14,22 @@ WITH gather_group_members AS (
         name LIKE "%Shared CC Advising%"
     )
 ),
-determine_new_roles AS (SELECT
+determine_new_roles AS (
+  SELECT
+    U.*,
+    GRI.user_role_id AS new_role
+  FROM
+    gather_group_members GM
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.user_clean` U ON U.id = GM.user_or_group_id
+    LEFT JOIN `data-warehouse-289815.roles.group_role_id` GRI ON GRI.group_id = GM.group_id
+)
+SELECT
   U.*,
-  GRI.user_role_id AS new_role
+  DNR.new_role,
+  CASE
+    WHEN DNR.new_role IS NULL THEN U.user_role_id
+    ELSE DNR.new_role
+  END AS user_role_id
 FROM
-  gather_group_members GM
-  LEFT JOIN `data-warehouse-289815.salesforce_clean.user_clean` U ON U.id = GM.user_or_group_id
-  LEFT JOIN `data-warehouse-289815.roles.group_role_id` GRI ON GRI.group_id = GM.group_id
-  )
-  
-  
-SELECT U.*,
-DNR.new_role
-FROM `data-warehouse-289815.salesforce_clean.user_clean` U
-LEFT JOIN determine_new_roles DNR ON DNR.id = U.Id
+  `data-warehouse-289815.salesforce_clean.user_clean` U
+  LEFT JOIN determine_new_roles DNR ON DNR.id = U.Id
