c7b6f16022c484dc76eae0b3a99b60062e86859b
Baker Renneckar <baker@bakerr.com>
2021-03-04 14:29:45 -0800

    Auto-commit: Thu Mar 04 2021 14:29:42 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
@@ -11,7 +11,7 @@ WHERE group_id IN (SELECT Id FROM gather_groups)
 
 )
 
-SELECT GM.*
+SELECT GM.*,
 U.name
 FROM gather_group_members GM
 LEFT JOIN `data-warehouse-289815.salesforce_clean.user_clean` U ON U.id = GM.user_or_group_id
