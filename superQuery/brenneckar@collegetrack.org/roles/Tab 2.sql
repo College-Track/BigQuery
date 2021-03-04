42798bd56062544e84ddad9c7456d4530d0fb0ef
Baker Renneckar <baker@bakerr.com>
2021-03-04 14:41:24 -0800

    Auto-commit: Thu Mar 04 2021 14:41:21 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
@@ -1,19 +1,23 @@
-WITH gather_groups AS (SELECT 
-Id, Name
-FROM `data-warehouse-289815.salesforce.group`
-WHERE name LIKE "%Shared CC Advising%"
-),
-
-gather_group_members AS (
-SELECT user_or_group_id, group_id
-FROM `data-warehouse-289815.salesforce.group_member`
-WHERE group_id IN (SELECT Id FROM gather_groups)
-
+WITH gather_group_members AS (
+  SELECT
+    user_or_group_id,
+    group_id
+  FROM
+    `data-warehouse-289815.salesforce.group_member`
+  WHERE
+    group_id IN (
+      SELECT
+        Id
+      FROM
+        `data-warehouse-289815.salesforce.group`
+      WHERE
+        name LIKE "%Shared CC Advising%"
+    )
 )
-
-SELECT 
-U.*,
-GRI.user_role_id AS new_role
-FROM gather_group_members GM
-LEFT JOIN `data-warehouse-289815.salesforce_clean.user_clean` U ON U.id = GM.user_or_group_id
-LEFT JOIN `data-warehouse-289815.roles.group_role_id` GRI ON GRI.group_id=GM.group_id
+SELECT
+  U.*,
+  GRI.user_role_id AS new_role
+FROM
+  gather_group_members GM
+  LEFT JOIN `data-warehouse-289815.salesforce_clean.user_clean` U ON U.id = GM.user_or_group_id
+  LEFT JOIN `data-warehouse-289815.roles.group_role_id` GRI ON GRI.group_id = GM.group_id
