3f505bcb7bbc9504b0827093427d261889918e29
Baker Renneckar <baker@bakerr.com>
2021-03-04 14:25:17 -0800

    Auto-commit: Thu Mar 04 2021 14:25:15 GMT-0800 (Pacific Standard Time)

--- superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roles/Tab 2.sql
@@ -1,4 +1,13 @@
-SELECT 
-*
+WITH gather_groups AS (SELECT 
+Id, Name
 FROM `data-warehouse-289815.salesforce.group`
 WHERE name LIKE "%Shared CC Advising%"
+)
+
+gather_group_members AS (
+SELECT *
+FROM `data-warehouse-289815.salesforce.group_member`
+)
+
+SELECT *
+FROM gather_group_members
