c1c6e709b8c64b3a81bf98a01fdc1dc84a2eec65
Baker Renneckar <baker@bakerr.com>
2021-03-08 13:09:18 -0800

    Auto-commit: Mon Mar 08 2021 16:09:12 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/gpa_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/gpa_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
-SELECT *
-FROM `data-warehouse-289815.salesforce_clean.user_clean`
-WHERE email = 'jpivaral@collegetrack.org'
+UPDATE `data-warehouse-289815.roles.role_table`
+SET Role_ID = '00E1M000001e0u3UAA'
+WHERE Role = 'Program - Crenshaw'
