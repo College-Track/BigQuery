1549f05964d94c57b50a9a07c01dd3df91ccbd39
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-03-10 12:11:06 -0800

    Auto-commit: Wed Mar 10 2021 11:57:38 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/march temp fix.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/march temp fix.sql
@@ -78,9 +78,7 @@ student_bb_balance_list AS
     current_cc_advisor_2_c AS Current_CCA,
     high_school_graduating_class_c AS HS_Class,
     College_Track_Status_Name AS CT_Status,
-    bb_balance.Finance_BB_Balance,
-    bb_march_disburse_add.bb_add_to_balance,
-    bb_march_earn_minus.bb_minus_from_balance,
+    bb_balance.Finance_BB_Balance + bb_march_disburse_add.bb_add_to_balance - bb_march_earn_minus.bb_minus_from_balance AS adj_bb_balance,
     
     FROM
     `data-warehouse-289815.salesforce_clean.contact_template`
@@ -94,13 +92,7 @@ student_bb_balance_list AS
 bb_raw AS
 (
     SELECT
-    student_bb_balance_list.Contact_Id,
-    student_bb_balance_list.Full_Name_c,
-    student_bb_balance_list.Site,
-    student_bb_balance_list.Current_CCA,
-    student_bb_balance_list.HS_Class,
-    student_bb_balance_list.CT_Status,
-    student_bb_balance_list.Finance_BB_Balance + student_bb_balance_list.bb_add_to_balance - student_bb_balance_list.bb_minus_from_balance AS adj_bb_balance_march,
+    student_bb_balance_list.*,
     bb_d_e_fy.bb_id,
     bb_d_e_fy.BB_Record_Type,
     bb_d_e_fy.amount_c,
