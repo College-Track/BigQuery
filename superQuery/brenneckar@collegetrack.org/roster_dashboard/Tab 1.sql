7093879cb81914a1a44ec91f5cb1ca548de183e7
Baker Renneckar <baker@bakerr.com>
2021-03-11 14:03:50 -0500

    Auto-commit: Thu Mar 11 2021 14:03:47 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 1.sql
@@ -1,5 +1,4 @@
 SELECT site_short, Count(( Contact_Id))
 FROM `data-studio-260217.rosters.filtered_roster`
-WHERE site_short = 'Oakland'
-AND college_track_status_c = '15A'
+WHERE college_track_status_c = '15A'
 GROUP BY site_short
