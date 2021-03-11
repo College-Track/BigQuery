a31fc4ca48d0054ffd64824925dbd0b8c6974e21
Baker Renneckar <baker@bakerr.com>
2021-03-11 14:34:27 -0500

    Auto-commit: Thu Mar 11 2021 14:34:22 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 1.sql
@@ -1,6 +1,4 @@
-SELECT Count(( Contact_Id))
-FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-WHERE college_track_status_c = '15A' AND current_as_c = true
+
 WITH gather_data AS (
   SELECT
     -- Select fields from templates
