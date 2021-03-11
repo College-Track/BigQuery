18f5bc9e374ff2816735f116ac068397531ec60a
Baker Renneckar <baker@bakerr.com>
2021-03-11 14:45:07 -0500

    Auto-commit: Thu Mar 11 2021 14:45:02 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
@@ -1,5 +1,6 @@
 SELECT COUNT(Contact_Id)
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-GROUP BY Contact_Id
 WHERE  current_as_c = true
+
+GROUP BY Contact_Id
 Having COUNT(Contact_Id) > 1
