df08eead54985584640252e1a20101e6c5f60582
Baker Renneckar <baker@bakerr.com>
2021-03-11 14:50:16 -0500

    Auto-commit: Thu Mar 11 2021 14:50:11 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
@@ -1,6 +1,6 @@
 SELECT student_c, COUNT(student_c)
 FROM `data-warehouse-289815.salesforce.academic_semester_c`
-WHERE  current_as_c = true
+WHERE  current_as_c = true and student_c = '0034600001TQqpgAAD'
 
 GROUP BY student_c
 Having COUNT(student_c) > 1
