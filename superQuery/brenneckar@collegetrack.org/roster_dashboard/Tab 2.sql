64573318c60e0670c01a4f3a4d20b29af82af548
Baker Renneckar <baker@bakerr.com>
2021-03-11 14:43:29 -0500

    Auto-commit: Thu Mar 11 2021 14:43:26 GMT-0500 (Eastern Standard Time)

--- superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/roster_dashboard/Tab 2.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(Contact_Id)
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
-WHERE Contact_Id = '0034600001TQqpgAAD'
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+WHERE Contact_Id = '0034600001TQqpgAAD' AND current_as_c = true
