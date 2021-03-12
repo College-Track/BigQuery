83a332230e5bf552636db97abc5b36f62b850fdb
Baker Renneckar <baker@bakerr.com>
2021-03-12 17:17:43 -0500

    Auto-commit: Fri Mar 12 2021 17:11:42 GMT-0500 (Eastern Standard Time)

---
+++ superQuery/brenneckar@collegetrack.org/users_clean/Tab 6.sql
@@ -0,0 +1,18 @@
+SELECT
+  Master.*
+FROM
+  `data-studio-260217.student_dashboard.student_dashboard` AS Master
+RIGHT JOIN (
+  SELECT
+    roles.site_short,
+    roles.role
+  FROM
+    `data-warehouse-289815.salesforce_clean.user_clean` AS Users
+  LEFT JOIN
+    `data-warehouse-289815.roles.role_table` AS Roles
+  ON
+    Roles.Role_Id=Users.user_role_id
+  WHERE
+    LOWER(Users.Email)='swee@collegetrack.org' ) AS Permissions
+ON
+  Master.site_short=Permissions.site_short
