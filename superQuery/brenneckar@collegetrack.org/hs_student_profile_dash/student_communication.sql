2a68db19ea79d768a9150687f88d00fd840c83dd
Baker Renneckar <baker@bakerr.com>
2021-03-15 17:15:17 -0400

    Auto-commit: Mon Mar 15 2021 17:15:08 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_communication.sql
+++ superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_communication.sql
@@ -1,14 +1,22 @@
-SELECT id,
-who_id,
- CASE
-      WHEN activity_date = date_of_contact_c THEN date_of_contact_c
-      WHEN date_of_contact_c IS NULL
-      and activity_date IS NOT NULL THEN activity_date
-      ELSE date_of_contact_c
-    END AS date_of_contact_c,
-subject,
-description,
-reciprocal_communication_c,
-type
-FROM `data-warehouse-289815.salesforce.task`
-WHERE who_id IN (SELECT Contact_Id FROM `data-warehouse-289815.salesforce_clean.contact_template`)
+SELECT
+  id,
+  who_id,
+  CASE
+    WHEN activity_date = date_of_contact_c THEN date_of_contact_c
+    WHEN date_of_contact_c IS NULL
+    and activity_date IS NOT NULL THEN activity_date
+    ELSE date_of_contact_c
+  END AS date_of_contact_c,
+  subject,
+  description,
+  reciprocal_communication_c,
+  type
+FROM
+  `data-warehouse-289815.salesforce.task`
+WHERE
+  who_id IN (
+    SELECT
+      Contact_Id
+    FROM
+      `data-warehouse-289815.salesforce_clean.contact_template`
+  )
