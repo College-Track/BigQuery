373668856b1960193f98d445a838825cbfb794b8
Baker Renneckar <baker@bakerr.com>
2021-03-15 19:08:01 -0400

    Auto-commit: Mon Mar 15 2021 19:07:56 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_.sql
+++ superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_.sql
@@ -6,11 +6,11 @@ SELECT
   most_recent_gpa_semester_c,
   AT_Cumulative_GPA,
   GAS_Name,
-  start_date_c
+  start_date_c,
+  attendance_rate_c
 FROM
   `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE
   college_track_status_c IN ('18a', '11A', '12A', '13A')
   AND years_since_hs_grad_c <= 0
-  AND term_c != "Summer"
   AND start_date_c <= CURRENT_DATE()
