f95ed47a1768661fdba194401a6aeffe40a1d60e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-03-14 21:21:23 -0700

    Auto-commit: Sun Mar 14 2021 21:21:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Advising Rubric & PAT Data.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Advising Rubric & PAT Data.sql
@@ -28,6 +28,7 @@ FROM
   LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` PAT
   ON CAR.Contact_Id = PAT.student_c
 WHERE CAR.current_or_prev_at = "Current AT"
+AND CAR.current_or_prev_at = "true"
 
 GROUP BY
   REGEXP_EXTRACT(at_url, r'(?:^|/)Academic_Semester_c/([^/]+)'),
