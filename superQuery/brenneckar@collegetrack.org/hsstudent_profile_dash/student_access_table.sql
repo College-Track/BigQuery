c22f661d580f3b6119e1ae8df4d1e2c1768c3d42
Baker Renneckar <baker@bakerr.com>
2021-03-15 16:49:16 -0400

    Auto-commit: Mon Mar 15 2021 16:49:08 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/hsstudent_profile_dash/student_access_table.sql
@@ -0,0 +1,3 @@
+SELECT Contact_Id, full_name_c, College_Track_Status_Name, high_school_graduating_class_c, site_short
+FROM `data-warehouse-289815.salesforce_clean.contact_template`
+WHERE college_track_status_c IN ('18a', '11A', '12A', '13A')
