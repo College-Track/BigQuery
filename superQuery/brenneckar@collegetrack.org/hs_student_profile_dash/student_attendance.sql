53d7d610a9f238fefa7b9d30181f32b9d96688d1
Baker Renneckar <baker@bakerr.com>
2021-03-16 14:26:39 -0400

    Auto-commit: Tue Mar 16 2021 14:26:26 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_attendance.sql
+++ superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_attendance.sql
@@ -1,17 +1,46 @@
 WITH gather_attendance_data AS (
   SELECT
     Student_c AS Contact_Id,
-    EXTRACT(ISOWEEK FROM date_c) as date_week,
+    EXTRACT(
+      ISOWEEK
+      FROM
+        date_c
+    ) as date_week,
+    CONCAT(
+      "Week ",
+      CAST(
+        EXTRACT(
+          ISOWEEK
+          FROM
+            date_c
+        ) AS STRING
+      )
+    ) as string_date_week,
     workshop_display_name_c,
     academic_semester_c,
     SUM(Attendance_Numerator_c) AS Attendance_Numerator_c,
     SUM(Attendance_Denominator_c) AS Attendance_Denominator_c,
   FROM
     `data-warehouse-289815.salesforce_clean.class_template`
-    WHERE date_c <= CURRENT_DATE()
+  WHERE
+    date_c <= CURRENT_DATE()
   GROUP BY
     Student_c,
-    EXTRACT(ISOWEEK FROM date_c),
+    EXTRACT(
+      ISOWEEK
+      FROM
+        date_c
+    ),
+    CONCAT(
+      "Week ",
+      CAST(
+        EXTRACT(
+          ISOWEEK
+          FROM
+            date_c
+        ) AS STRING
+      )
+    ),
     workshop_display_name_c,
     academic_semester_c
 ),
