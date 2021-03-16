076a355a2426bac6df62b3ac87fc5e3fe29134ed
Baker Renneckar <baker@bakerr.com>
2021-03-16 18:55:16 -0400

    Auto-commit: Tue Mar 16 2021 18:55:12 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
@@ -0,0 +1,14 @@
+SELECT
+  student_audit_status_c,
+  COUNT(A_T.Id)
+FROM `data-warehouse-289815.salesforce.academic_semester_c` A_T
+  LEFT JOIN `data-warehouse-289815.salesforce.global_academic_semester_c` GAS ON GAS.Id = A_T.global_academic_semester_c
+WHERE
+  GAS.name LIKE '%Spring 2019-20%'
+  AND student_audit_status_c IN (
+    'Active: Post-Secondary',
+    'Current CT HS Student',
+    'Leave of Absence'
+  )
+GROUP BY
+  student_audit_status_c
