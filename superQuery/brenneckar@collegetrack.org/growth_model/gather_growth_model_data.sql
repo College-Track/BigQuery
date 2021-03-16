17f61e0382c50c41def7c57f627a3a16c93ba082
Baker Renneckar <baker@bakerr.com>
2021-03-16 19:10:11 -0400

    Auto-commit: Tue Mar 16 2021 19:10:07 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
@@ -7,21 +7,43 @@ WITH gather_data AS (
     student_audit_status_c,
     GAS_Name,
     AT_Record_Type_Name,
-    grade_c,
-    AY_Name
+    AT_Grade_c,
+    AY_Name,
+    CASE
+      WHEN AT_Grade_c = '9th Grade' THEN 1
+      WHEN AT_Grade_c = '10th Grade' THEN 2
+      WHEN AT_Grade_c = '11th Grade' THEN 3
+      WHEN AT_Grade_c = '12th Grade' THEN 4
+      WHEN AT_Grade_c = 'Year 1' THEN 5
+      WHEN AT_Grade_c = 'Year 2' THEN 6
+      WHEN AT_Grade_c = 'Year 3' THEN 7
+      WHEN AT_Grade_c = 'Year 4' THEN 8
+      WHEN AT_Grade_c = 'Year 5' THEN 9
+      WHEN AT_Grade_c = 'Year 6' THEN 10
+      WHEN AT_Grade_c = 'Year 7' THEN 11
+      WHEN AT_Grade_c = 'Year 8' THEN 11
+      ELSE 12
+    END as grade_sort
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template`
   WHERE
     GAS_Name LIKE "%Spring%"
-    AND student_audit_status_c IN ('Current CT HS Student', 'Active: Post-Secondary', "Leave of Absence")
-    AND start_date_c >='2016-01-01' AND end_date_c <= '2020-06-30'
+    AND student_audit_status_c IN (
+      'Current CT HS Student',
+      'Active: Post-Secondary',
+      "Leave of Absence"
+    )
+    AND start_date_c >= '2016-01-01'
+    AND end_date_c <= '2020-06-30'
+    -- and AT_Grade_c = '9th Grade'
 ),
 group_data AS (
   SELECT
     region_short,
     site_short,
     AY_Name,
-    grade_c,
+    AT_Grade_c,
+    grade_sort,
     -- student_audit_status_c,
     COUNT(Contact_Id) AS student_count
   FROM
@@ -30,13 +52,22 @@ group_data AS (
     region_short,
     site_short,
     AY_Name,
-    grade_c
-    -- student_audit_status_c
+    AT_Grade_c,
+    grade_sort -- student_audit_status_c
 )
-SELECT
-grade_c,
-AVG(student_count)
-FROM
-  group_data
-  
-  GROUP BY grade_c
+-- SELECT
+--   AT_Grade_c,
+--   grade_sort,
+--   AVG(student_count)
+-- FROM
+--   group_data
+-- GROUP BY
+--   AT_Grade_c,
+--   grade_sort
+-- ORDER BY
+--   grade_sort
+
+
+SELECT AT_Grade_c, count(contact_id)
+FROM gather_data
+GROUP BY AT_Grade_c
