c1e54ce80b1e22af8041e00b3a857b1c827e2850
Baker Renneckar <baker@bakerr.com>
2021-03-16 13:49:50 -0400

    Auto-commit: Tue Mar 16 2021 13:43:44 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_attendance.sql
+++ superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_attendance.sql
@@ -1,6 +1,6 @@
 WITH gather_attendance_data AS (
   SELECT
-    Student_c AS Contact_Id,
+    Student_c,
     date_c,
     workshop_display_name_c,
     academic_semester_c,
@@ -8,7 +8,6 @@ WITH gather_attendance_data AS (
     SUM(Attendance_Denominator_c) AS Attendance_Denominator_c,
   FROM
     `data-warehouse-289815.salesforce_clean.class_template`
-    WHERE date_c <= CURRENT_DATE()
   GROUP BY
     Student_c,
     date_c,
@@ -17,11 +16,10 @@ WITH gather_attendance_data AS (
 ),
 join_data AS (
   SELECT
-    GAD.*,
-    SLTT.current_as_c
+    GAD.*
   FROM
     gather_attendance_data GAD
-    LEFT JOIN `data-studio-260217.hs_student_profile_dashboard.student_term_table` SLTT ON SLTT.Contact_Id = GAD.Contact_Id
+    LEFT JOIN `data-studio-260217.hs_student_profile_dashboard.student_term_table` SLTT ON SLTT.Contact_Id = GAD.Student_c
     AND SLTT.AT_Id = GAD.academic_semester_c
 )
 SELECT
