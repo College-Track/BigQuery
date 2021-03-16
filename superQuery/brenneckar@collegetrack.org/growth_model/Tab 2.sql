076b8961ae5d2063890abc7874bd40bf9ef02d4d
Baker Renneckar <baker@bakerr.com>
2021-03-16 19:38:13 -0400

    Auto-commit: Tue Mar 16 2021 19:25:08 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 2.sql
@@ -0,0 +1,3 @@
+SELECT name,  College_Track_FY_HS_Planned_Enrollment_c, College_Track_High_School_Capacity_c,
+FROM `data-warehouse-289815.salesforce.account`
+WHERE College_Track_FY_HS_Planned_Enrollment_c IS NOT NULL
