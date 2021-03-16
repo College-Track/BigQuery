dae63ce7f166745b23dba4522a0e439ad298c9b5
Baker Renneckar <baker@bakerr.com>
2021-03-16 19:07:26 -0400

    Auto-commit: Tue Mar 16 2021 19:07:20 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
@@ -9,18 +9,20 @@ WITH gather_data AS (
     AT_Record_Type_Name,
     grade_c,
     AY_Name,
-    CASE WHEN grade_c ='9th Grade' THEN 1
-    WHEN grade_c ='10th Grade' THEN 2
-    WHEN grade_c ='11th Grade' THEN 3
-    WHEN grade_c ='12th Grade' THEN 4
-    WHEN grade_c ='Year 1' THEN 5
-    WHEN grade_c ='Year 2' THEN 6
-    WHEN grade_c ='Year 4' THEN 7
-    WHEN grade_c ='Year 5' THEN 8
-    WHEN grade_c ='Year 6' THEN 9
-    WHEN grade_c ='Year 7' THEN 10
-    WHEN grade_c ='Year 8' THEN 11
-    ELSE 12
+    CASE
+      WHEN grade_c = '9th Grade' THEN 1
+      WHEN grade_c = '10th Grade' THEN 2
+      WHEN grade_c = '11th Grade' THEN 3
+      WHEN grade_c = '12th Grade' THEN 4
+      WHEN grade_c = 'Year 1' THEN 5
+      WHEN grade_c = 'Year 2' THEN 6
+      WHEN grade_c = 'Year 3' THEN 7
+      WHEN grade_c = 'Year 4' THEN 8
+      WHEN grade_c = 'Year 5' THEN 9
+      WHEN grade_c = 'Year 6' THEN 10
+      WHEN grade_c = 'Year 7' THEN 11
+      WHEN grade_c = 'Year 8' THEN 11
+      ELSE 12
     END as grade_sort
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template`
@@ -50,7 +52,7 @@ group_data AS (
     site_short,
     AY_Name,
     grade_c,
-    grade_sort-- student_audit_status_c
+    grade_sort -- student_audit_status_c
 )
 SELECT
   grade_c,
@@ -61,4 +63,5 @@ FROM
 GROUP BY
   grade_c,
   grade_sort
-  ORDER BY grade_sort
+ORDER BY
+  grade_sort
