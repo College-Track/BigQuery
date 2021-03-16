aebc896af18797192522cf8861a91d023d7bd69b
Baker Renneckar <baker@bakerr.com>
2021-03-16 19:11:50 -0400

    Auto-commit: Tue Mar 16 2021 19:05:36 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
@@ -7,22 +7,20 @@ WITH gather_data AS (
     student_audit_status_c,
     GAS_Name,
     AT_Record_Type_Name,
-    AT_Grade_c,
+    grade_c,
     AY_Name,
-    CASE
-      WHEN AT_Grade_c = '9th Grade' THEN 1
-      WHEN AT_Grade_c = '10th Grade' THEN 2
-      WHEN AT_Grade_c = '11th Grade' THEN 3
-      WHEN AT_Grade_c = '12th Grade' THEN 4
-      WHEN AT_Grade_c = 'Year 1' THEN 5
-      WHEN AT_Grade_c = 'Year 2' THEN 6
-      WHEN AT_Grade_c = 'Year 3' THEN 7
-      WHEN AT_Grade_c = 'Year 4' THEN 8
-      WHEN AT_Grade_c = 'Year 5' THEN 9
-      WHEN AT_Grade_c = 'Year 6' THEN 10
-      WHEN AT_Grade_c = 'Year 7' THEN 11
-      WHEN AT_Grade_c = 'Year 8' THEN 11
-      ELSE 12
+    CASE WHEN grade_c ='9th Grade' THEN 1
+    WHEN grade_c ='10th Grade' THEN 2
+    WHEN grade_c ='11th Grade' THEN 3
+    WHEN grade_c ='12th Grade' THEN 4
+    WHEN grade_c ='Year 1' THEN 5
+    WHEN grade_c ='Year 2' THEN 6
+    WHEN grade_c ='Year 4' THEN 7
+    WHEN grade_c ='Year 5' THEN 8
+    WHEN grade_c ='Year 6' THEN 9
+    WHEN grade_c ='Year 7' THEN 10
+    WHEN grade_c ='Year 8' THEN 11
+    ELSE 12
     END as grade_sort
   FROM
     `data-warehouse-289815.salesforce_clean.contact_at_template`
@@ -35,14 +33,13 @@ WITH gather_data AS (
     )
     AND start_date_c >= '2016-01-01'
     AND end_date_c <= '2020-06-30'
-    -- and AT_Grade_c = '9th Grade'
 ),
 group_data AS (
   SELECT
     region_short,
     site_short,
     AY_Name,
-    AT_Grade_c,
+    grade_c,
     grade_sort,
     -- student_audit_status_c,
     COUNT(Contact_Id) AS student_count
@@ -52,19 +49,14 @@ group_data AS (
     region_short,
     site_short,
     AY_Name,
-    AT_Grade_c,
-    grade_sort -- student_audit_status_c
+    grade_c,
+    grade_sort-- student_audit_status_c
 )
 SELECT
-  AT_Grade_c,
-  grade_sort,
+  grade_c,
   AVG(student_count)
 FROM
   group_data
 GROUP BY
-  AT_Grade_c,
-  grade_sort
-ORDER BY
-  grade_sort
-
-
+  grade_c
+  ORDER BY grade_sort
