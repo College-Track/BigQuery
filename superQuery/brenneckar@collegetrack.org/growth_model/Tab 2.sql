822342b15dad8d15489653ee9c7a2d064c0d423c
Baker Renneckar <baker@bakerr.com>
2021-03-16 21:35:31 -0400

    Auto-commit: Tue Mar 16 2021 21:35:27 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 2.sql
@@ -2,4 +2,5 @@ SELECT
 Contact_Id,
 `data-warehouse-289815.UDF.determine_buckets`(gpa_hs_cumulative_c,0.5, 2.5,3.0,'gpa')
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+WHERE gpa_hs_cumulative_c IS NOT NULL
 LIMIT 10
