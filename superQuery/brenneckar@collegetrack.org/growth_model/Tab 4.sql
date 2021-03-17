f6b61cecf45cbbd46d17179f3a8a5e5eb240a625
Baker Renneckar <baker@bakerr.com>
2021-03-17 19:01:33 -0400

    Auto-commit: Wed Mar 17 2021 19:01:27 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -3,28 +3,40 @@ RETURNS ARRAY <FLOAT64>
 LANGUAGE js AS r"""
 function futureCalculations(start_count, FY, HS_Class, years_ahead) {
     var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
+    var start_year = FY - 2000
+    var end_year = start_year + years_ahead
+
     // var num_iterations = 14 - (HS_Class - FY)
     var grade_index = 3 + (FY - HS_Class)
     var new_count = [];
     var count_index = 0
-    new_count.push(start_count)
-    while (grade_index < 11) {
-        // console.log(grade_index)
-        _tmp_count = new_count[count_index] * rates[grade_index + 1]
-        // console.log(_tmp_count)
-        new_count.push(_tmp_count)
+    new_count.push(["FY" + start_year, start_count])
+    start_year += 1
+    while (start_year <= end_year) {
+
+        _tmp_string = "FY" + start_year
+        if (grade_index < 11) {
+            _tmp_count = new_count[count_index][1] * rates[grade_index + 1]
+        }
+        else {
+            _tmp_count = NaN
+        }
+        var _tmp_array = [_tmp_string, _tmp_count]
+
+        new_count.push(_tmp_array)
         grade_index += 1
         count_index += 1
+        start_year += 1
 
     }
-    var expand_by = years_ahead - new_count.length + 1
-    var extra_items = Array(expand_by).fill(NaN)
+    // var expand_by = years_ahead - new_count.length + 1
+    // var extra_items = Array(expand_by).fill(NaN)
 
-    var final_array = new_count.concat(extra_items)
+    // var final_array = new_count.concat(extra_items)
     // console.log(num_iterations)
 
 
-    return (final_array)
+    return (new_count)
 }
 return (futureCalculations(start_count, FY, HS_Class, years_ahead))
 """;
@@ -67,11 +79,11 @@ WITH numbers AS
 
 
 
-SELECT site, hs_class, student_count, fy_list
+SELECT site, hs_class, student_count
 FROM (
-  SELECT site, hs_class, calc_student_count(start_count, FY, hs_class, 15) count_arrary,
-  list_fy(FY, 15) fY_array
+  SELECT site, hs_class, calc_student_count(start_count, FY, hs_class, 15) count_arrary
+--   list_fy(FY, 15) fY_array
   FROM numbers
   
-) LEFT JOIN UNNEST(count_arrary) student_count
-LEFT JOIN UNNEST(fY_array) fy_list
+), UNNEST(count_arrary) student_count
+-- LEFT JOIN UNNEST(fY_array) fy_list
