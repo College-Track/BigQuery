bf3758935be8ae064594d87a1852fa0dde379299
Baker Renneckar <baker@bakerr.com>
2021-03-16 20:58:05 -0400

    Auto-commit: Tue Mar 16 2021 20:57:56 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
@@ -5,9 +5,14 @@ WITH gather_data AS (
     site_short,
     AT_grade_c,
     high_school_graduating_class_c,
+    CASE
+      WHEN A.College_Track_High_School_Capacity_c = 287 THEN 75
+      ELSE 60
+    END AS first_year_target,
     COUNT(Contact_Id) as fy20_student_count,
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_at_template`
+    `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
+    LEFT JOIN `data-warehouse-289815.salesforce.account` A ON A.Id = CAT.site_c
   WHERE
     GAS_Name LIKE '%Spring 2019-20%'
     AND student_audit_status_c IN (
@@ -16,14 +21,19 @@ WITH gather_data AS (
       "Leave of Absence"
     )
   GROUP BY
-   
     region_short,
+    first_year_target,
     site_short,
     AT_grade_c,
     high_school_graduating_class_c
 )
 SELECT
-  *
+  GD.*
+EXCEPT(join_key),
+  CASE
+    WHEN AT_Grade_c = '9th Grade' THEN fy20_student_count * GMA.ten_grade
+    ELSE 0
+  END AS fy21_projection
 FROM
   gather_data GD
   LEFT JOIN `learning-agendas.growth_model.growth_model_assumptions` GMA ON GMA.join_key = GD.join_key
