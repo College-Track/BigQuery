69d1a69df74170ab1d66d352a71a2bb801332015
Baker Renneckar <baker@bakerr.com>
2021-03-16 20:30:28 -0400

    Auto-commit: Tue Mar 16 2021 20:30:21 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/gather_growth_model_data.sql
@@ -34,10 +34,11 @@ WITH gather_data AS (
     LEFT JOIN `data-warehouse-289815.salesforce.account` A ON A.Id = CAT.site_c
   WHERE
     (
-      (GAS_Name LIKE "%Spring%") --   OR (
-      --     GAS_Name LIKE "%Summer%"
-      --     AND AT_Grade_c = '9th Grade'
-      --   )
+      (GAS_Name LIKE "%Spring%") 
+         OR (
+           GAS_Name LIKE "%Summer%"
+           AND AT_Grade_c = '9th Grade'
+         )
     )
     AND student_audit_status_c IN (
       'Current CT HS Student',
