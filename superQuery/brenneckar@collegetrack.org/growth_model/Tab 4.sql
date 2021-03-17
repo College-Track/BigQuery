579d1c1e08caa8771b4088ff88f15483d750e06a
Baker Renneckar <baker@bakerr.com>
2021-03-17 15:24:43 -0400

    Auto-commit: Wed Mar 17 2021 15:24:30 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -1,4 +1,4 @@
-CREATE TEMP FUNCTION expand_array(start_count FLOAT64, FY FLOAT64, HS_Class FLOAT64)
+CREATE TEMP FUNCTION calc_student_count(start_count FLOAT64, FY FLOAT64, HS_Class FLOAT64)
 RETURNS ARRAY <FLOAT64>
 LANGUAGE js AS r"""
   function futureCalculations(start_count, FY, HS_Class) {
@@ -24,14 +24,48 @@ return (futureCalculations(start_count,FY, HS_Class))
 """;
 
 
+
+CREATE TEMP FUNCTION list_fy(FY FLOAT64, HS_Class FLOAT64)
+RETURNS ARRAY <FLOAT64>
+LANGUAGE js AS r"""
+  function list_fiscal_years(FY, HS_Class) {
+    var num_iterations = 14 - (HS_Class - FY)
+    var grade_index = 3 + (FY - HS_Class)
+    var start_year = FY - 2000
+
+    var fy_list = [];
+    while (grade_index < num_iterations) {
+        // console.log(grade_index)
+        _tmp_string = "FY" + start_year
+
+        fy_list.push(_tmp_string)
+        grade_index += 1
+        start_year += 1
+
+    }
+
+
+    return (fy_list)
+}
+
+return (list_fiscal_years(FY, HS_Class))
+""";
+
+
+
+
+
+
+
 WITH numbers AS
   (SELECT 100 AS start_count, 2021 as FY, 2024 AS hs_class)
 
 
 
-SELECT name,
+SELECT student_count,
 FROM (
-  SELECT expand_array(start_count, FY, hs_class) names
+  SELECT calc_student_count(start_count, FY, hs_class) count_arrary
+--   list_fy(FY, hs_class) fY_array
   FROM numbers
   
-), UNNEST(names) name
+), UNNEST(count_arrary) student_count
