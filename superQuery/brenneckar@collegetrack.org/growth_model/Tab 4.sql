e38f5edf3d6e9e1f892c633132392c845d27e0e6
Baker Renneckar <baker@bakerr.com>
2021-03-17 15:13:29 -0400

    Auto-commit: Wed Mar 17 2021 15:13:22 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -1,17 +1,26 @@
 CREATE TEMP FUNCTION expand_array(start_count FLOAT64, FY FLOAT64, HS_Class FLOAT64)
 RETURNS ARRAY <FLOAT64>
 LANGUAGE js AS r"""
-  var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
-  
-  var num_iterations = HS_Class - FY
-  
-  var new_count = [];
-  
-  new_count.push(start_count * rates[1)
-  
-  new_count.push(new_count[0] * rates[1)
-  
-  return new_count;
+  function futureCalculations(start_count, FY, HS_Class) {
+    var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
+    var num_iterations = 14 - (HS_Class - FY)
+    var grade_index = 3 + (FY - HS_Class)
+    var new_count = [];
+    new_count.push(start_count)
+    while (grade_index < num_iterations) {
+        // console.log(grade_index)
+        _tmp_count = new_count[grade_index] * rates[grade_index + 1]
+        // console.log(_tmp_count)
+        new_count.push(_tmp_count)
+        grade_index += 1
+
+    }
+
+
+    return (new_count)
+}
+
+return (futureCalculations(start_count,FY, HS_Class))
 """;
 
 
