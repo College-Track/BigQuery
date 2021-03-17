b34aedd11288e14ccea9ee71f818b054e9ff2ef9
Baker Renneckar <baker@bakerr.com>
2021-03-17 18:42:43 -0400

    Auto-commit: Wed Mar 17 2021 18:42:35 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -1,45 +1,48 @@
-CREATE TEMP FUNCTION calc_student_count(start_count FLOAT64, FY FLOAT64, HS_Class FLOAT64)
+CREATE TEMP FUNCTION calc_student_count(start_count FLOAT64, FY FLOAT64, HS_Class FLOAT64, years_ahead FLOAT64)
 RETURNS ARRAY <FLOAT64>
 LANGUAGE js AS r"""
-  function futureCalculations(start_count, FY, HS_Class) {
+function futureCalculations(start_count, FY, HS_Class, years_ahead) {
     var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
-    var num_iterations = 14 - (HS_Class - FY)
+    // var num_iterations = 14 - (HS_Class - FY)
     var grade_index = 3 + (FY - HS_Class)
     var new_count = [];
+    var count_index = 0
     new_count.push(start_count)
-    while (grade_index < num_iterations) {
+    while (grade_index < 11) {
         // console.log(grade_index)
-        _tmp_count = new_count[grade_index] * rates[grade_index + 1]
+        _tmp_count = new_count[count_index] * rates[grade_index + 1]
         // console.log(_tmp_count)
         new_count.push(_tmp_count)
         grade_index += 1
+        count_index += 1
 
     }
+    var expand_by = years_ahead - new_count.length + 1
+    var extra_items = Array(expand_by).fill(NaN)
+
+    var final_array = new_count.concat(extra_items)
+    // console.log(num_iterations)
 
 
-    return (new_count)
+    return (final_array)
 }
-
-return (futureCalculations(start_count,FY, HS_Class))
 """;
 
 
 
-CREATE TEMP FUNCTION list_fy(FY FLOAT64, HS_Class FLOAT64)
+CREATE TEMP FUNCTION list_fy(FY FLOAT64, years_ahead FLOAT64)
 RETURNS ARRAY <STRING>
 LANGUAGE js AS r"""
-  function list_fiscal_years(FY, HS_Class) {
-    var num_iterations = 14 - (HS_Class - FY)
-    var grade_index = 3 + (FY - HS_Class)
+  function list_fiscal_years(FY, years_ahead) {
     var start_year = FY - 2000
+    var end_year = start_year + years_ahead
 
     var fy_list = [];
-    while (grade_index < num_iterations) {
+    while (start_year <= end_year) {
         // console.log(grade_index)
         _tmp_string = "FY" + start_year
 
         fy_list.push(_tmp_string)
-        grade_index += 1
         start_year += 1
 
     }
@@ -47,8 +50,6 @@ LANGUAGE js AS r"""
 
     return (fy_list)
 }
-
-return (list_fiscal_years(FY, HS_Class))
 """;
 
 
@@ -66,8 +67,8 @@ WITH numbers AS
 
 SELECT site, hs_class, student_count
 FROM (
-  SELECT site, hs_class, calc_student_count(start_count, FY, hs_class) count_arrary,
-  list_fy(FY, hs_class) fY_array
+  SELECT site, hs_class, calc_student_count(start_count, FY, hs_class, 15) count_arrary,
+  list_fy(FY, 15) fY_array
   FROM numbers
   
 ), UNNEST(count_arrary) student_count
