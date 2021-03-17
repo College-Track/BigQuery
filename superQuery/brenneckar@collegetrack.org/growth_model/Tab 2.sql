8008965c816b34e2afa2a5108a0f46db341c6026
Baker Renneckar <baker@bakerr.com>
2021-03-17 13:20:49 -0400

    Auto-commit: Wed Mar 17 2021 13:20:43 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 2.sql
@@ -1,10 +1,7 @@
-SELECT name, COUNT(*) c
+SELECT names
 FROM (
   SELECT fhoffa.x.nlp_compromise_people(title) names
   FROM `fh-bigquery.reddit_posts.2019_02`
   WHERE subreddit = 'movies'
-), UNNEST(names) name
-WHERE name LIKE '% %'
-GROUP BY 1
-ORDER BY 2 DESC
+)
 LIMIT 10
