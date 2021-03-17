c262dc358688863ca4d4edbd2326643169234aa4
Baker Renneckar <baker@bakerr.com>
2021-03-17 19:11:28 -0400

    Auto-commit: Wed Mar 17 2021 19:05:16 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -73,13 +73,11 @@ return (list_fiscal_years(FY, years_ahead))
 
 
 WITH numbers AS
-  (SELECT 'nola' as site, 100 AS start_count, 2021 as FY, 2024 AS hs_class
-  UNION ALL SELECT 'nola' as site, 75 AS start_count, 2021 as FY, 2020 AS hs_class 
-  UNION ALL SELECT 'co' as site, 100 AS start_count, 2021 as FY, 2022 AS hs_class)
+  (SELECT 'nola' as site, 100 AS start_count, 2021 as FY, 2024 AS hs_class)
 
 
 
-SELECT site, hs_class, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
+SELECT site, hs_class, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS INT64) num_student
 FROM (
   SELECT site, hs_class, calc_student_count(start_count, FY, hs_class, 15) count_arrary
 --   list_fy(FY, 15) fY_array
