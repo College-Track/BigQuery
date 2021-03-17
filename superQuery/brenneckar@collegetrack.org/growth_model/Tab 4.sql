73f9282d9f06df79c7c431f66f0e0ebe84b789a3
Baker Renneckar <baker@bakerr.com>
2021-03-17 14:53:03 -0400

    Auto-commit: Wed Mar 17 2021 14:52:58 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -1,18 +1,28 @@
-CREATE TEMP FUNCTION expand_array(x FLOAT64)
+CREATE TEMP FUNCTION expand_array(start_count FLOAT64, FY FLOAT64, HS_Class FLOAT64)
 RETURNS ARRAY <FLOAT64>
 LANGUAGE js AS r"""
-  return [...Array(x).keys()];
+  var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
+  
+  var num_iterations = HS_Class - FY
+  
+  var new_count = [];
+  
+  new_count.push(start_count * rates[1)
+  
+  new_count.push(new_count[0] * rates[1)
+  
+  return new_count;
 """;
 
 
 WITH numbers AS
-  (SELECT 5 AS x)
+  (SELECT 100 AS start_count, 2021 as FY, 2024 AS hs_class)
 
 
 
 SELECT name,
 FROM (
-  SELECT expand_array(x) names
+  SELECT expand_array(start_count, FY, hs_class) names
   FROM numbers
   
 ), UNNEST(names) name
