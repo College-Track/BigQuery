e2ccc6bf7e7b931b8873fa29096848cedf4edcb7
Baker Renneckar <baker@bakerr.com>
2021-03-17 14:20:27 -0400

    Auto-commit: Wed Mar 17 2021 14:20:21 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -1,4 +1,11 @@
-  SELECT fhoffa.x.nlp_compromise_people(title) names
-  FROM `fh-bigquery.reddit_posts.2019_02`
-  WHERE subreddit = 'movies'
-  LIMIT 10
+CREATE TEMP FUNCTION expand_array(x FLOAT64)
+RETURNS ARRAY <FLOAT64>
+LANGUAGE js AS r"""
+  return [...Array(x).keys()];
+""";
+
+
+WITH numbers AS
+  (SELECT 5 AS x, 5 as y)
+SELECT x, y, expand_array(x) as products
+FROM numbers;
