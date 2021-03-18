f467c0cf4e7d8185e6852334826ba4ae72d062be
Baker Renneckar <baker@bakerr.com>
2021-03-18 12:40:33 -0400

    Auto-commit: Thu Mar 18 2021 12:40:28 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 5.sql
@@ -6,38 +6,42 @@ function futureCalculations(start_count, FY, HS_Class, years_ahead) {
     var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
     var start_year = FY - 2000
     var end_year = start_year + years_ahead
-
-    // var num_iterations = 14 - (HS_Class - FY)
-    var grade_index = 3 + (FY - HS_Class)
+    if (HS_Class > (FY + 3)) {
+        var grade_index = 0
+    } else {
+        var grade_index = 3 + (FY - HS_Class)
+    }
     var new_count = [];
     var count_index = 0
-    new_count.push(["FY" + start_year, start_count])
-    start_year += 1
-    while (start_year <= end_year) {
+    var start_count_entered = false
 
+    while (start_year <= end_year) {
         _tmp_string = "FY" + start_year
-        if (grade_index < 11) {
-            _tmp_count = new_count[count_index][1] * rates[grade_index + 1]
+        if ((start_year + 2000 + 3) < (HS_Class)) {
+            new_count.push([_tmp_string, NaN])
         }
         else {
-            _tmp_count = NaN
-        }
-        var _tmp_array = [_tmp_string, _tmp_count]
+            if (start_count_entered == false) {
+                new_count.push([_tmp_string, start_count])
+                start_count_entered = true
+            }
+            if (grade_index < 11) {
 
-        new_count.push(_tmp_array)
-        grade_index += 1
+                _tmp_count = new_count[count_index][1] * rates[grade_index + 1]
+            }
+            else {
+                _tmp_count = NaN
+            }
+            var _tmp_array = [_tmp_string, _tmp_count]
+            new_count.push(_tmp_array)
+            grade_index += 1
+        }
         count_index += 1
         start_year += 1
-
     }
-    // var expand_by = years_ahead - new_count.length + 1
-    // var extra_items = Array(expand_by).fill(NaN)
-
-    // var final_array = new_count.concat(extra_items)
-    // console.log(num_iterations)
-
 
     return (new_count)
 }
+
 return (futureCalculations(start_count, FY, HS_Class, years_ahead))
 """
