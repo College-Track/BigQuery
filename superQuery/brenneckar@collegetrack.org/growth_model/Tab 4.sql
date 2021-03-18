4d359d0b378501450fbd23e1a25552fbe5de209b
Baker Renneckar <baker@bakerr.com>
2021-03-18 11:21:46 -0400

    Auto-commit: Thu Mar 18 2021 11:21:39 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -29,18 +29,17 @@ WITH gather_data AS (
 
 
 
--- WITH numbers AS
---   (SELECT 'nola' as site, 100 AS start_count, 2021 as FY, 2024 AS hs_class
---   UNION ALL SELECT 'nola' as site, 75 AS start_count, 2021 as FY, 2020 AS hs_class 
---   UNION ALL SELECT 'co' as site, 100 AS start_count, 2021 as FY, 2022 AS hs_class)
 
 
-
-SELECT region_short, site_short, high_school_graduating_class_c, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
-FROM (
-  SELECT region_short, site_short, high_school_graduating_class_c, `learning-agendas.growth_model.calc_projected_student_count`(fy20_student_count, 2021, high_school_graduating_class_c, 15) count_arrary
-  FROM gather_data
+-- SELECT region_short, site_short, high_school_graduating_class_c, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
+-- FROM (
+--   SELECT region_short, site_short, high_school_graduating_class_c, `learning-agendas.growth_model.calc_projected_student_count`(fy20_student_count, 2021, high_school_graduating_class_c, 15) count_arrary
+--   FROM gather_data
   
-), UNNEST(count_arrary) student_count
+-- ), UNNEST(count_arrary) student_count
 
+
+SELECT region_short, site_short, MAX(high_school_graduating_class_c)
+FROM gather_data 
+GROUP BY region_short, site_short
 
