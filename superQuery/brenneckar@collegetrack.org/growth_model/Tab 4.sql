1548c324cd4ae248057de6465e4047267e740c09
Baker Renneckar <baker@bakerr.com>
2021-03-18 13:27:36 -0400

    Auto-commit: Thu Mar 18 2021 13:27:29 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 4.sql
@@ -1,6 +1,6 @@
 WITH gather_data AS (
   SELECT
-    region_short,
+    region_abrev,
     site_short,
     AT_grade_c,
     CAST(high_school_graduating_class_c AS FLOAT64) AS high_school_graduating_class_c,
@@ -21,41 +21,41 @@ WITH gather_data AS (
       "Leave of Absence"
     )
   GROUP BY
-    region_short,
+    region_abrev,
     first_year_target,
     site_short,
     AT_grade_c,
     high_school_graduating_class_c
 ),
 prep_data_for_new_hs_class AS (
-SELECT region_short, site_short, first_year_target, MAX(high_school_graduating_class_c) AS high_school_graduating_class_c
+SELECT region_abrev, site_short, first_year_target, MAX(high_school_graduating_class_c) AS high_school_graduating_class_c
 FROM gather_data 
-GROUP BY region_short, site_short, first_year_target
+GROUP BY region_abrev, site_short, first_year_target
 ),
 
 
 new_hs_classes AS (
-SELECT region_short, site_short, first_year_target, high_school_graduating_class_c
+SELECT region_abrev, site_short, first_year_target, high_school_graduating_class_c
 FROM (
-SELECT region_short, site_short, first_year_target, GENERATE_ARRAY(high_school_graduating_class_c+1, high_school_graduating_class_c+12) AS hs_classes 
+SELECT region_abrev, site_short, first_year_target, GENERATE_ARRAY(high_school_graduating_class_c+1, high_school_graduating_class_c+12) AS hs_classes 
 FROM prep_data_for_new_hs_class
 )
 ,UNNEST(hs_classes) high_school_graduating_class_c
 ),
 
 
-calc_projections AS (SELECT region_short, site_short, high_school_graduating_class_c, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
+calc_projections AS (SELECT region_abrev, site_short, high_school_graduating_class_c, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
 FROM (
-  SELECT region_short, site_short, high_school_graduating_class_c, `learning-agendas.growth_model.calc_projected_student_count`(fy20_student_count, 2020, high_school_graduating_class_c, 15,[0.918175347, 1.080857452, 0.877739525, 0.846273341, 0.945552158,0.947539442, 0.903267551, 0.83901895, 0.47881341, 0.481957966, 0.689493272, 0.419033383]) count_arrary
+  SELECT region_abrev, site_short, high_school_graduating_class_c, `learning-agendas.growth_model.calc_projected_student_count`(fy20_student_count, 2020, high_school_graduating_class_c, 15,[0.918175347, 1.080857452, 0.877739525, 0.846273341, 0.945552158,0.947539442, 0.903267551, 0.83901895, 0.47881341, 0.481957966, 0.689493272, 0.419033383]) count_arrary
   FROM gather_data
   
 ), UNNEST(count_arrary) student_count
 ),
 
 
-calc_projections_new_hs_class AS (SELECT region_short, site_short, high_school_graduating_class_c, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
+calc_projections_new_hs_class AS (SELECT region_abrev, site_short, high_school_graduating_class_c, SPLIT(student_count, ',')[OFFSET(0)] fiscal_year, CAST(SPLIT(student_count, ',')[OFFSET(1)] AS FLOAT64) num_student
 FROM (
-  SELECT region_short, site_short, high_school_graduating_class_c, `learning-agendas.growth_model.calc_projected_student_count`(first_year_target, 2020, high_school_graduating_class_c, 15, [0.918175347, 1.080857452, 0.877739525, 0.846273341, 0.945552158,0.947539442, 0.903267551, 0.83901895, 0.47881341, 0.481957966, 0.689493272, 0.419033383]) count_arrary
+  SELECT region_abrev, site_short, high_school_graduating_class_c, `learning-agendas.growth_model.calc_projected_student_count`(first_year_target, 2020, high_school_graduating_class_c, 15, [0.918175347, 1.080857452, 0.877739525, 0.846273341, 0.945552158,0.947539442, 0.903267551, 0.83901895, 0.47881341, 0.481957966, 0.689493272, 0.419033383]) count_arrary
   FROM new_hs_classes
   
 ), UNNEST(count_arrary) student_count
