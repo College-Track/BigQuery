318b35738335489893523dab1ab4595e2c53464a
Baker Renneckar <baker@bakerr.com>
2021-03-19 23:37:44 -0400

    Auto-commit: Fri Mar 19 2021 23:37:36 GMT-0400 (Eastern Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/growth_model/prep_alumni.sql
@@ -0,0 +1,19 @@
+WITH gather_data AS (
+  SELECT
+    C.region_abrev,
+    C.site_short,
+    C.high_school_graduating_class_c,
+    COUNT(C.Contact_Id) as starting_count,
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template` C
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` A_T ON A_T.AT_Id = C.contact_4_year_degree_earned_at_lookup_c
+    WHERE C.college_track_status_c = '17A'
+    AND A_T.GAS_Name NOT LIKE '%2020-21%' AND A_T.GAS_Name NOT LIKE '%2019-20%'
+    GROUP BY 
+    C.region_abrev,
+    C.site_short,
+    C.high_school_graduating_class_c
+)
+
+SELECT *
+FROM gather_data
