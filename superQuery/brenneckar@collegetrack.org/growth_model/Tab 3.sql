0739897a9df60f2c6a2175879e4adae29f4311ce
Baker Renneckar <baker@bakerr.com>
2021-03-19 23:30:20 -0400

    Auto-commit: Fri Mar 19 2021 23:30:09 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 3.sql
@@ -1,32 +1,12 @@
-CREATE
-OR REPLACE FUNCTION `learning-agendas.growth_model.calc_projection`(
-  AT_Grade_c STRING,
-  base_count FLOAT64,
-  nine_grade FLOAT64,
-  tenth_grade FLOAT64,
-  eleven_grade FLOAT64,
-  twelve_grade FLOAT64,
-  year_1 FLOAT64,
-  year_2 FLOAT64,
-  year_3 FLOAT64,
-  year_4 FLOAT64,
-  year_5 FLOAT64,
-  year_6 FLOAT64,
-  year_7 FLOAT64,
-  year_8 FLOAT64
-) AS (
-  CASE
-    WHEN AT_Grade_c = '9th Grade' THEN base_count * (tenth_grade / nine_grade)
-    WHEN AT_Grade_c = '10th Grade' THEN base_count * (eleven_grade / tenth_grade)
-    WHEN AT_Grade_c = '11th Grade' THEN base_count * (twelve_grade / eleven_grade)
-    WHEN AT_Grade_c = '12th Grade' THEN base_count * (year_1 / twelve_grade)
-    WHEN AT_Grade_c = 'Year 1' THEN base_count * (year_2 / year_1)
-    WHEN AT_Grade_c = 'Year 2' THEN base_count * (year_3 / year_2)
-    WHEN AT_Grade_c = 'Year 3' THEN base_count * (year_4 / year_3)
-    WHEN AT_Grade_c = 'Year 4' THEN base_count * (year_5 / year_4)
-    WHEN AT_Grade_c = 'Year 5' THEN base_count * (year_6 / year_5)
-    WHEN AT_Grade_c = 'Year 6' THEN base_count * (year_7 / year_6)
-    WHEN AT_Grade_c = 'Year 7' THEN base_count * (year_8 / year_7)
-    ELSE 0
-    END
-  );
+WITH gather_data AS (
+  SELECT
+    region_abrev,
+    site_short,
+    Contact_Id
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+    WHERE college_track_status_c = '17A'
+)
+
+SELECT COUNT(contact_Id)
+FROM gather_data
