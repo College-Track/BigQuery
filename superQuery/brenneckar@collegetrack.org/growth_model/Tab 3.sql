09475d545318ed5ad0e03af37597eebb2755ab34
Baker Renneckar <baker@bakerr.com>
2021-03-19 23:30:56 -0400

    Auto-commit: Fri Mar 19 2021 23:30:52 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 3.sql
@@ -2,11 +2,12 @@ WITH gather_data AS (
   SELECT
     region_abrev,
     site_short,
-    Contact_Id
+    Contact_Id,
+    hs_grad_year_c
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE college_track_status_c = '17A'
 )
 
-SELECT COUNT(contact_Id)
+SELECT *
 FROM gather_data
