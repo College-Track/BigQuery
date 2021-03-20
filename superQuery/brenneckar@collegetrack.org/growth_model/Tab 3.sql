71b3f28b37227cdf50030d6e552c7e9a544e32c6
Baker Renneckar <baker@bakerr.com>
2021-03-19 23:33:54 -0400

    Auto-commit: Fri Mar 19 2021 23:33:49 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 3.sql
@@ -1,13 +1,14 @@
 WITH gather_data AS (
   SELECT
-    region_abrev,
-    site_short,
-    Contact_Id,
-    hs_grad_year_c,
-    contact_4_year_degree_earned_at_lookup_c
+    C.region_abrev,
+    C.site_short,
+    C.Contact_Id,
+    C.hs_grad_year_c,
+    A_T.GAS_Name
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
-    WHERE college_track_status_c = '17A'
+    `data-warehouse-289815.salesforce_clean.contact_template` C
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` A_T ON A_T.AT_Id = C.contact_4_year_degree_earned_at_lookup_c
+    WHERE C.college_track_status_c = '17A'
 )
 
 SELECT *
