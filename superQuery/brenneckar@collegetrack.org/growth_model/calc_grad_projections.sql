039d95bb4432ac0a96004dbf9c6f6c6ee63d04d3
Baker Renneckar <baker@bakerr.com>
2021-03-22 18:19:39 -0400

    Auto-commit: Mon Mar 22 2021 18:19:34 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/calc_grad_projections.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/calc_grad_projections.sql
@@ -3,39 +3,58 @@ OR REPLACE FUNCTION `learning-agendas.growth_model.calc_grad_projections`(
 student_count FLOAT64, 
 FY FLOAT64, 
 HS_Class FLOAT64,
-region STRING)
+region STRING, 
+rates ARRAY <FLOAT64>)
 RETURNS ARRAY <STRING>
 LANGUAGE js AS r"""
-function calc_grad_projections(student_count, FY, HS_Class, region) {
+
+function calc_grad_projections(student_count, FY, HS_Class, region, rates) {
+    var alumni_count = []
     var region_rates = {
-        "NOR CAL": [0.31, 0.15, 0.05, 0.08],
-        "NOLA": [0.19, 0.09, 0.06, 0.04],
-        "Other": [0.3, 0.14, 0.05, 0.07]
+        "NOR CAL": [0.31, 0.15, 0.05, 0.04, 0.04],
+        "NOLA": [0.19, 0.09, 0.06, 0.02, 0.02],
+        "Other": [0.3, 0.14, 0.05, 0.035, 0.035]
     };
     if (region in region_rates) {
-        var rates = region_rates[region]
+        var grad_rates = region_rates[region]
     }
     else {
-        var rates = region_rates['Other']
+        var grad_rates = region_rates['Other']
     }
-    var grade_index = (FY - HS_Class) - 4
-    var alumni_count = []
 
-    if (grade_index < 0) {
+    var grad_rate_index = (FY - HS_Class) - 4
+
+    // var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
+    if (HS_Class > (FY + 3)) {
+        var grade_index = 0
+    } else {
+        var grade_index = 3 + (FY - HS_Class)
+    }
+    var estimated_year_1_enrollment = student_count
+
+    if (grade_index < 7) {
         return
     }
-    else if (grade_index > 3) {
+    else if (grade_index > 11) {
         return
     }
+
     else {
-        _tmp_string = "FY" + ((FY - 2000) + 1)
-        _tmp_count = student_count * rates[grade_index]
+        while (grade_index > 4) {
+            estimated_year_1_enrollment /= rates[grade_index]
+            grade_index -= 1
+        }
 
-        alumni_count.push([_tmp_string, _tmp_count, "Alumni"])
-
-
-        return (alumni_count)
     }
+
+    _tmp_string = "FY" + ((FY - 2000) + 1)
+    _tmp_count = estimated_year_1_enrollment * grad_rates[grad_rate_index]
+
+    alumni_count.push([_tmp_string, _tmp_count, "Alumni"])
+
+
+    return (alumni_count)
+
 }
-return (calc_grad_projections(student_count, FY, HS_Class, region))
+return (calc_grad_projections(student_count, FY, HS_Class, region, rates))
 """
