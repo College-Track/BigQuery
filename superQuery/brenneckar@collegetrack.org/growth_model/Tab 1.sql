4cae04bb390120a4b1c366ac1d004f2a79717a1d
Baker Renneckar <baker@bakerr.com>
2021-03-22 12:32:30 -0400

    Auto-commit: Mon Mar 22 2021 12:32:25 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
@@ -1,85 +1,19 @@
 WITH gather_data AS (
   SELECT
-    "join" AS join_key,
     region_short,
     site_short,
-    AT_grade_c,
-    high_school_graduating_class_c,
-    CASE
-      WHEN A.College_Track_High_School_Capacity_c = 287 THEN 75
-      ELSE 60
-    END AS first_year_target,
-    COUNT(Contact_Id) as fy20_student_count,
+    Contact_Id,
+    graduated_4_year_degree_4_years_c,
+    graduated_4_year_degree_5_years_c,
+    graduated_4_year_degree_6_years_c,
+    graduated_4_year_degree_c
+   
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
-    LEFT JOIN `data-warehouse-289815.salesforce.account` A ON A.Id = CAT.site_c
+    `data-warehouse-289815.salesforce_clean.contact_template` C
+    
   WHERE
-    GAS_Name LIKE '%Spring 2019-20%'
-    AND student_audit_status_c IN (
-      'Current CT HS Student',
-      'Active: Post-Secondary',
-      "Leave of Absence"
-    )
-  GROUP BY
-    region_short,
-    first_year_target,
-    site_short,
-    AT_grade_c,
-    high_school_graduating_class_c
-),
-create_new_hs_class AS (
-  SELECT
-    DISTINCT GD.join_key,
-    region_short,
-    site_short,
-    "NA" AS AT_grade_c,
-    '2025' AS high_school_graduating_class_c,
-    first_year_target,
-    0 AS fy20_student_count,
-    (first_year_target * GMA.nine_grade) AS fy21_projection
-  FROM
-    gather_data GD
-    LEFT JOIN `learning-agendas.growth_model.growth_model_assumptions` GMA ON GMA.join_key = GD.join_key
-),
-calc_projection AS (
-  SELECT
-    GD.*
-  EXCEPT(join_key),
-    `learning-agendas.growth_model.calc_projection`(
-      AT_Grade_c,
-      fy20_student_count,
-      GMA.nine_grade,
-      GMA.ten_grade,
-      GMA.eleven_grade,
-      GMA.twelve_grade,
-      GMA.year_1,
-      GMA.year_2,
-      GMA.year_3,
-      GMA.year_4,
-      GMA.year_5,
-      GMA.year_6,
-      GMA.year_7,
-      GMA.year_8
-    ) AS fy21_projection
-  FROM
-    gather_data GD
-    LEFT JOIN `learning-agendas.growth_model.growth_model_assumptions` GMA ON GMA.join_key = GD.join_key
-  UNION ALL
-    (
-      SELECT
-        *
-      EXCEPT(join_key)
-      FROM
-        create_new_hs_class
-    )
-)
-SELECT
-  --   site_short,
-  --   AT_Grade_c,
-  SUM(fy20_student_count),
-  SUM(fy21_projection)
-FROM
-  calc_projection --   WHERE site_short = 'New Orleans'
-  -- GROUP BY
-  --   site_short,
-  --   AT_Grade_c
+  years_since_hs_grad_c >= 6
+  )
+  
+  SELECT *
+  FROM gather_data
