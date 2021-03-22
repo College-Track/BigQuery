dd66e318f8b5ca1a590ccb848281d19f8b2b1d4e
Baker Renneckar <baker@bakerr.com>
2021-03-22 12:36:17 -0400

    Auto-commit: Mon Mar 22 2021 12:36:13 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 1.sql
@@ -4,22 +4,26 @@ WITH gather_data AS (
     site_short,
     Contact_Id,
     CAST(graduated_4_year_degree_4_years_c AS int64) AS graduated_4_year_degree_4_years_c,
-    graduated_4_year_degree_5_years_c,
-    graduated_4_year_degree_6_years_c,
-    graduated_4_year_degree_c
-   
+    CAST(graduated_4_year_degree_5_years_c AS int64) AS graduated_4_year_degree_5_years_c,
+    CAST(graduated_4_year_degree_6_years_c AS int64) AS graduated_4_year_degree_6_years_c,
+    CAST(graduated_4_year_degree_c AS int64) AS graduated_4_year_degree_c,
+    
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template` C
-    
   WHERE
-  years_since_hs_grad_c >= 6
-  and indicator_completed_ct_hs_program_c = true
-  )
-  
-  SELECT region_short,
-    site_short,
-    COUNT(Contact_Id) AS student_count,
-    SUM(graduated_4_year_degree_4_years_c) AS graduated_4_year_degree_4_years_c
-  FROM gather_data
-  GROUP BY region_short,
-    site_short
+    years_since_hs_grad_c >= 6
+    and indicator_completed_ct_hs_program_c = true
+)
+SELECT
+--   region_short,
+--   site_short,
+  COUNT(Contact_Id) AS student_count,
+  SUM(graduated_4_year_degree_4_years_c) AS graduated_4_year_degree_4_years_c,
+  SUM(graduated_4_year_degree_5_years_c) AS graduated_4_year_degree_5_years_c,
+  SUM(graduated_4_year_degree_6_years_c) AS graduated_4_year_degree_6_years_c,
+  SUM(graduated_4_year_degree_c) AS graduated_4_year_degree_c
+FROM
+  gather_data
+-- GROUP BY
+--   region_short,
+--   site_short
