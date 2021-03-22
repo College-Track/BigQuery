d0a50a360a5e6935cb282224a424f4c8eab4643c
Baker Renneckar <baker@bakerr.com>
2021-03-22 16:16:41 -0400

    Auto-commit: Mon Mar 22 2021 16:16:37 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/calc_grad_projections.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/calc_grad_projections.sql
@@ -2,11 +2,22 @@ CREATE
 OR REPLACE FUNCTION `learning-agendas.growth_model.calc_grad_projections`(
 student_count FLOAT64, 
 FY FLOAT64, 
-HS_Class FLOAT64)
+HS_Class FLOAT64,
+region STRING)
 RETURNS ARRAY <STRING>
 LANGUAGE js AS r"""
-function calc_grad_projections(student_count, FY, HS_Class) {
-    var rates = [.75, .5, .25, .1]
+function calc_grad_projections(student_count, FY, HS_Class, region) {
+    var region_rates = {
+        "NOR CAL": [0.31, 0.15, 0.05, 0.08],
+        "NOLA": [0.19, 0.09, 0.06, 0.04],
+        "Other": [0.3, 0.14, 0.05, 0.07]
+    };
+    if (region in region_rates) {
+        var rates = region_rates[region]
+    }
+    else {
+        var rates = region_rates['Other']
+    }
     var grade_index = (FY - HS_Class) - 4
     var alumni_count = []
 
@@ -17,7 +28,7 @@ function calc_grad_projections(student_count, FY, HS_Class) {
         return
     }
     else {
-        _tmp_string = 'FY' + ((FY - 2000) + 1)
+        _tmp_string = "FY" + ((FY - 2000) + 1)
         _tmp_count = student_count * rates[grade_index]
 
         alumni_count.push([_tmp_string, _tmp_count, "Alumni"])
@@ -26,6 +37,5 @@ function calc_grad_projections(student_count, FY, HS_Class) {
         return (alumni_count)
     }
 }
-
-return (calc_grad_projections(student_count, FY, HS_Class))
-""";
+return (calc_grad_projections(student_count, FY, HS_Class, region))
+"""
