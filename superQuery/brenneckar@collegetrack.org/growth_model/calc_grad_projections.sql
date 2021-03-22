178fca46008ea673f49288fdc318b1355431038d
Baker Renneckar <baker@bakerr.com>
2021-03-22 18:45:52 -0400

    Auto-commit: Mon Mar 22 2021 18:45:48 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/calc_grad_projections.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/calc_grad_projections.sql
@@ -5,11 +5,10 @@ FY FLOAT64,
 HS_Class FLOAT64,
 region STRING, 
 rates ARRAY <FLOAT64>,
-improve_grad_rate FLOAT64)
+improve_grad_rates FLOAT64)
 RETURNS ARRAY <STRING>
 LANGUAGE js AS r"""
-
-function calc_grad_projections(student_count, FY, HS_Class, region, rates, improve_grad_rate) {
+function calc_grad_projections(student_count, FY, HS_Class, region, rates, improve_grad_rates) {
     var alumni_count = []
     var region_rates = {
         "NOR CAL": [0.31, 0.15, 0.05, 0.04, 0.04],
@@ -22,9 +21,7 @@ function calc_grad_projections(student_count, FY, HS_Class, region, rates, impro
     else {
         var grad_rates = region_rates['Other']
     }
-
     var grad_rate_index = (FY - HS_Class) - 4
-
     // var rates = [.5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5, .5]
     if (HS_Class > (FY + 3)) {
         var grade_index = 0
@@ -32,30 +29,22 @@ function calc_grad_projections(student_count, FY, HS_Class, region, rates, impro
         var grade_index = 3 + (FY - HS_Class)
     }
     var estimated_year_1_enrollment = student_count
-
     if (grade_index < 7) {
         return
     }
     else if (grade_index > 11) {
         return
     }
-
     else {
         while (grade_index > 4) {
             estimated_year_1_enrollment /= rates[grade_index]
             grade_index -= 1
         }
-
     }
-
     _tmp_string = "FY" + ((FY - 2000) + 1)
-    _tmp_count = estimated_year_1_enrollment * (grad_rates[grad_rate_index]+ improve_grad_rate/5)
-
+    _tmp_count = estimated_year_1_enrollment * (grad_rates[grad_rate_index] _ improve_grad_rates/5)
     alumni_count.push([_tmp_string, _tmp_count, "Alumni"])
-
-
     return (alumni_count)
-
 }
-calc_grad_projections(student_count, FY, HS_Class, region, rates, improve_grad_rate)
+return (calc_grad_projections(student_count, FY, HS_Class, region, rates, improve_grad_rates))
 """
