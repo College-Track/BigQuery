e1504e55a9ce9c6f7918812380bfecaa58ac0778
Baker Renneckar <baker@bakerr.com>
2021-03-22 11:06:01 -0400

    Auto-commit: Mon Mar 22 2021 11:05:56 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/prep_alumni.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/prep_alumni.sql
@@ -3,7 +3,9 @@ WITH gather_data AS (
     C.region_abrev,
     C.site_short,
     C.high_school_graduating_class_c,
-    COUNT(C.Contact_Id) as starting_count,
+    COUNT(C.Contact_Id) as num_student,
+    "FY20" AS fiscal_year,
+    "Alumni" AS student_type
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template` C
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` A_T ON A_T.AT_Id = C.contact_4_year_degree_earned_at_lookup_c
