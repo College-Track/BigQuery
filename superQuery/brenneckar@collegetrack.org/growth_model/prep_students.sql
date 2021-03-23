de07ccaf1264ccc2340f06ed2843dcc0b9b55820
Baker Renneckar <baker@bakerr.com>
2021-03-23 14:28:49 -0400

    Auto-commit: Tue Mar 23 2021 14:28:41 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/prep_students.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/prep_students.sql
@@ -46,7 +46,7 @@ FROM prep_data_for_new_hs_class
 news_site AS (
 SELECT region_abrev, site_short, first_year_target as starting_count, high_school_graduating_class_c
 FROM (
-SELECT @new_region_1 AS region_abrev, @new_site_1 AS site_short, @new_site_1_target AS first_year_target, GENERATE_ARRAY(@new_site_1_start_hs_class, @new_site_1_start_hs_class+12) AS hs_classes 
+SELECT @new_region_1 AS region_abrev, @new_site_1 AS site_short, 0 AS first_year_target, GENERATE_ARRAY(2024, 2024+12) AS hs_classes 
 
 )
 ,UNNEST(hs_classes) high_school_graduating_class_c
@@ -58,6 +58,7 @@ SELECT @new_region_1 AS region_abrev, @new_site_1 AS site_short, @new_site_1_tar
 combined_classes AS (
 SELECT region_abrev, site_short, starting_count, high_school_graduating_class_c
 FROM gather_data
+WHERE starting_count = 0
 UNION ALL (SELECT * FROM new_hs_classes WHERE starting_count=0)
 UNION ALL (SELECT * FROM news_site WHERE starting_count = 0)
 )
