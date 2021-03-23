851d9a21faf52518e66eec7e428697a69ba6a054
Baker Renneckar <baker@bakerr.com>
2021-03-23 14:07:41 -0400

    Auto-commit: Tue Mar 23 2021 14:07:37 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/Tab 7.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/Tab 7.sql
@@ -1,31 +1,11 @@
-CREATE
-OR REPLACE FUNCTION `learning-agendas.growth_model.calc_grad_projections`(
-student_count FLOAT64, 
-FY FLOAT64, 
-HS_Class FLOAT64)
-RETURNS ARRAY <STRING>
-LANGUAGE js AS r"""
-function calc_grad_projections(student_count, FY, HS_Class) {
-    var rates = [.75, .5, .25, .1]
-    var grade_index = (FY - HS_Class) - 4
-    var alumni_count = []
+WITH new_hs_classes AS (
+SELECT region_abrev, site_short, first_year_target as starting_count, high_school_graduating_class_c
+FROM (
+SELECT "test_region" AS region_abrev, "test_site" AS site_short, 0 AS first_year_target, GENERATE_ARRAY(2024, 2024+12) AS hs_classes 
 
-    if (grade_index < 0) {
-        return
-    }
-    else if (grade_index > 3) {
-        return
-    }
-    else {
-        _tmp_string = 'FY' + ((FY - 2000) + 1)
-        _tmp_count = student_count * rates[grade_index]
+)
+,UNNEST(hs_classes) high_school_graduating_class_c
+)
 
-        alumni_count.push([_tmp_string, _tmp_count])
-
-
-        return (alumni_count)
-    }
-}
-
-return (calc_grad_projections(student_count, FY, HS_Class))
-""";
+SELECT *
+FROM new_hs_classes
