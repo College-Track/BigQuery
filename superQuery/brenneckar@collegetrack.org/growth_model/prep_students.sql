2e13e9139409c268fb0634dbbfcb25a304b095dd
Baker Renneckar <baker@bakerr.com>
2021-03-23 14:24:59 -0400

    Auto-commit: Tue Mar 23 2021 14:24:54 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/growth_model/prep_students.sql
+++ superQuery/brenneckar@collegetrack.org/growth_model/prep_students.sql
@@ -46,7 +46,7 @@ FROM prep_data_for_new_hs_class
 news_site AS (
 SELECT region_abrev, site_short, first_year_target as starting_count, high_school_graduating_class_c
 FROM (
-SELECT "test_region" AS region_abrev, "test_site" AS site_short, 0 AS first_year_target, GENERATE_ARRAY(2024, 2024+12) AS hs_classes 
+SELECT @new_region_1 AS region_abrev, "test_site" AS site_short, 0 AS first_year_target, GENERATE_ARRAY(2024, 2024+12) AS hs_classes 
 
 )
 ,UNNEST(hs_classes) high_school_graduating_class_c
