463843145e8a2da76b77461ad180123fb78f7726
Baker Renneckar <baker@bakerr.com>
2021-04-06 11:56:50 -0400

    Auto-commit: Tue Apr 06 2021 11:56:48 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
@@ -1,4 +1,4 @@
-SELECT "national" AS national,
+gahter_data AS (SELECT "national" AS national,
 site_short,
 region_short,
 Contact_Record_Type_Name,
@@ -8,3 +8,16 @@ WHERE college_track_status_c IN ('11A', '15A')
 GROUP BY site_short,
 region_short,
 Contact_Record_Type_Name
+)
+
+SELECT 
+national,
+site_short,
+region_short,
+MAX(IF(Contact_Record_Type_Name = "Student: Post-Secondary", student_count, NULL)) AS ps_student_count,
+MAX(IF(Contact_Record_Type_Name = "Student: High School", student_count, NULL)) AS hs_student_count,
+FROM gather_data
+GROUP BY
+national,
+site_short,
+region_short
