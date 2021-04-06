aec4cda2ca874b45579e5e1f086c2bdddd98926c
Baker Renneckar <baker@bakerr.com>
2021-04-06 11:53:27 -0400

    Auto-commit: Tue Apr 06 2021 11:53:23 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
@@ -1,9 +1,10 @@
 SELECT "national" AS national,
 site_short,
-region_short
+region_short,
 Contact_Record_Type_Name,
 COUNT(Contact_Id)
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
+WHERE college_track_status_c IN ('11A', '15A')
 GROUP BY site_short,
-region_short
+region_short,
 Contact_Record_Type_Name
