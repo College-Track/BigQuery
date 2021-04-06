14f8d2d6e0ab62595bd262ad9daebe0285633123
Baker Renneckar <baker@bakerr.com>
2021-04-06 12:08:53 -0400

    Auto-commit: Tue Apr 06 2021 12:08:49 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
@@ -4,15 +4,20 @@ WITH gather_data AS (
     site_short,
     region_short,
     Contact_Record_Type_Name,
+    indicator_completed_ct_hs_program_c,
+    college_track_status_c,
     COUNT(Contact_Id) AS student_count
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template`
   WHERE
-    college_track_status_c IN ('11A', '15A')
+    college_track_status_c IN ('11A', '15A', '16A', '17A')
+    AND indicator_years_since_hs_graduation_c <= 6
   GROUP BY
     site_short,
     region_short,
-    Contact_Record_Type_Name
+    Contact_Record_Type_Name,
+    indicator_completed_ct_hs_program_c,
+    college_track_status_c
 )
 SELECT
   national,
@@ -20,11 +25,11 @@ SELECT
   region_short,
   SUM(
     IF(
-      Contact_Record_Type_Name = "Student: Post-Secondary",
+      (Contact_Record_Type_Name = "Student: Post-Secondary") AND (college_track_status_c = '15A'),
       student_count,
       NULL
     )
-  ) AS ps_student_count,
+  ) AS active_ps_student_count,
   SUM(
     IF(
       Contact_Record_Type_Name = "Student: High School",
@@ -32,6 +37,13 @@ SELECT
       NULL
     )
   ) AS hs_student_count,
+    SUM(
+    IF(
+      indicator_completed_ct_hs_program_c = true,
+      student_count,
+      NULL
+    )
+  ) AS completed_hs_program_count,
 FROM
   gather_data
 GROUP BY
