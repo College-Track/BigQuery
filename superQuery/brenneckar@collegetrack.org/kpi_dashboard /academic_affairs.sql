95f3b9ed2c76ac9b77a62005f85fb8423c61b4a6
Baker Renneckar <baker@bakerr.com>
2021-04-06 12:53:03 -0400

    Auto-commit: Tue Apr 06 2021 12:52:48 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /academic_affairs.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /academic_affairs.sql
@@ -18,28 +18,31 @@ WITH gather_data AS (
     AND college_track_status_c IN ('11A')
 ),
 aa_attendance_prep AS (
-    SELECT
-      C.student_c,
-      SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c) AS attendance_rate
-    --   SUM(Attendance_Denominator_c) AS Attendance_Denominator_c
-    FROM
-      `data-warehouse-289815.salesforce_clean.class_template` C
-      LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.global_academic_semester_c = C.global_academic_semester_c
-    WHERE
-      department_c = "Academic Affairs"
-      AND CAT.AY_Name = 'AY 2020-21'
-      
-    GROUP BY student_c
-  ),
-  
+  SELECT
+    C.student_c,
+    CASE
+      WHEN SUM(Attendance_Denominator_c) IS NULL THEN NULL
+      ELSE SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c)
+    END AS attendance_rate --   SUM(Attendance_Denominator_c) AS Attendance_Denominator_c
+  FROM
+    `data-warehouse-289815.salesforce_clean.class_template` C
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.global_academic_semester_c = C.global_academic_semester_c
+  WHERE
+    department_c = "Academic Affairs"
+    AND CAT.AY_Name = 'AY 2020-21'
+  GROUP BY
+    student_c
+),
 aa_attendance_kpi AS (
-SELECT student_c,
-CASE WHEN attendance_rate >= 0.8 THEN 1
-ELSE 0
-END AS above_80_aa_attendance
-FROM aa_attendance_prep
-)  
-  
+  SELECT
+    student_c,
+    CASE
+      WHEN attendance_rate >= 0.8 THEN 1
+      ELSE 0
+    END AS above_80_aa_attendance
+  FROM
+    aa_attendance_prep
+)
 SELECT
   site_short,
   SUM(above_325_gpa) AS above_325_gpa,
