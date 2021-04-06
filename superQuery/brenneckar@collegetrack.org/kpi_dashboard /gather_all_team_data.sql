2c47f130840a9f25c239de56a48d21bed5ae17e1
Baker Renneckar <baker@bakerr.com>
2021-04-06 12:32:08 -0400

    Auto-commit: Tue Apr 06 2021 12:32:05 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
@@ -5,7 +5,7 @@ FROM `data-studio-260217.kpi_dashboard.join_prep`
 
 academic_affairs AS (
 SELECT JP.*,
-AA.above_325_gpa
+AA.* EXCEPT(site_short)
 FROM join_prep JP
 LEFT JOIN `data-studio-260217.kpi_dashboard.academic_affairs` AA ON AA.site_short = JP.site_short
 )
