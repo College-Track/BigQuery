3d80d60c48350e7826a018c9c282fb359f0cba1b
Baker Renneckar <baker@bakerr.com>
2021-04-06 11:57:45 -0400

    Auto-commit: Tue Apr 06 2021 11:57:41 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /join_prep.sql
@@ -1,23 +1,40 @@
-WITH gather_data AS (SELECT "national" AS national,
-site_short,
-region_short,
-Contact_Record_Type_Name,
-COUNT(Contact_Id) AS student_count
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
-WHERE college_track_status_c IN ('11A', '15A')
-GROUP BY site_short,
-region_short,
-Contact_Record_Type_Name
+WITH gather_data AS (
+  SELECT
+    "National" AS national,
+    site_short,
+    region_short,
+    Contact_Record_Type_Name,
+    COUNT(Contact_Id) AS student_count
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+  WHERE
+    college_track_status_c IN ('11A', '15A')
+  GROUP BY
+    site_short,
+    region_short,
+    Contact_Record_Type_Name
 )
-
-SELECT 
-national,
-site_short,
-region_short,
-MAX(IF(Contact_Record_Type_Name = "Student: Post-Secondary", student_count, NULL)) AS ps_student_count,
-MAX(IF(Contact_Record_Type_Name = "Student: High School", student_count, NULL)) AS hs_student_count,
-FROM gather_data
+SELECT
+  national,
+  site_short,
+  region_short,
+  MAX(
+    IF(
+      Contact_Record_Type_Name = "Student: Post-Secondary",
+      student_count,
+      NULL
+    )
+  ) AS ps_student_count,
+  MAX(
+    IF(
+      Contact_Record_Type_Name = "Student: High School",
+      student_count,
+      NULL
+    )
+  ) AS hs_student_count,
+FROM
+  gather_data
 GROUP BY
-national,
-site_short,
-region_short
+  national,
+  site_short,
+  region_short
