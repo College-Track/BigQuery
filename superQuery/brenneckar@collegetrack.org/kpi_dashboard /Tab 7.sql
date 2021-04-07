d1e8125dd7f8e41548ffc9731983b4b63f3c599e
Baker Renneckar <baker@bakerr.com>
2021-04-07 13:18:56 -0400

    Auto-commit: Wed Apr 07 2021 13:18:51 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 7.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 7.sql
@@ -1,3 +1,2 @@
-SELECT *
-FROM `data-warehouse-289815.salesforce.fivetran_audit`
-WHERE rows_updated_or_inserted > 500 AND done > '2021-04-06'
+SELECT COUNT(*)
+FROM `data-warehouse-289815.salesforce.class_session_c`
