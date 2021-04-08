3459bfc536436ac0c2f95085c68521cd8cde50dd
Baker Renneckar <baker@bakerr.com>
2021-04-08 13:48:26 -0400

    Auto-commit: Thu Apr 08 2021 13:48:22 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 16.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 16.sql
@@ -1,5 +1,5 @@
 SELECT Contact_Id, AT_Id, previous_academic_semester_c, attendance_rate_c, Attendance_Rate_Previous_Term_c
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE Attendance_Rate_Previous_Term_c IS NOT NULL AND attendance_rate_c IS NOT NULL
-AND AT_Id ='a1a1M0000072PRDQA2'
+AND AT_Id ='a1a1M0000072TfFQAU'
 LIMIT 1000
