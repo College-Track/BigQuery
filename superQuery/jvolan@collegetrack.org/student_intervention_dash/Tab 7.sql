8a5d18fa84ae13cec259525e9dd67cc3e24c7853
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-08 10:07:44 -0700

    Auto-commit: Thu Apr 08 2021 10:07:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 7.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 7.sql
@@ -1,13 +1,14 @@
 SELECT
     AT_Id,
     AT_Name,
+    Student_c,
     GAS_Name,
     AY_Name,
     AT_Grade_c,
     attendance_rate_c,
+    Attendance_Rate_Previous_Term_c,
     enrolled_sessions_c,
     attended_workshops_c,
-    attendance_rate_previous_term_c,
     DEP_GPA_Prev_semester_c AS prev_term_GPA,
     gpa_prev_semester_cumulative_c AS prev_CGPA,
    
