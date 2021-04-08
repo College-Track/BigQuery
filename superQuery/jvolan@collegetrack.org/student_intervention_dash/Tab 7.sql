554ff3c6f9dda45f18412b053ac267569a43a345
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-08 10:04:34 -0700

    Auto-commit: Thu Apr 08 2021 10:04:29 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 7.sql
@@ -0,0 +1,17 @@
+SELECT
+    AT_Id,
+    AT_Name,
+    GAS_Name,
+    AY_Name,
+    AT_Grade_c,
+    attendance_rate_c,
+    enrolled_sessions_c,
+    attended_workshops_c,
+    attendance_rate_previous_term_c,
+    DEP_GPA_Prev_semester_c AS prev_term_GPA,
+    gpa_prev_semester_cumulative_c AS prev_CGPA,
+   
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE current_as_c = TRUE
+    AND college_track_status_c IN ("11A", "12A")
