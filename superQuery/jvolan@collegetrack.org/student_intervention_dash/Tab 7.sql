f9f5b6ffc9073965fa0503291d47c95db2883957
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-08 10:13:44 -0700

    Auto-commit: Thu Apr 08 2021 10:13:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 7.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/Tab 7.sql
@@ -8,3 +8,4 @@ SELECT
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE current_as_c = TRUE
     AND college_track_status_c IN ("11A", "12A")
+    AND AT_id = 'a1a1M0000072PRDQA2'
