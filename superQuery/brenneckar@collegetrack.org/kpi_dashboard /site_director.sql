12de54da759c360e9fdb5db7cc3036ac7ec78972
Baker Renneckar <baker@bakerr.com>
2021-04-09 12:29:46 -0400

    Auto-commit: Fri Apr 09 2021 12:29:36 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /site_director.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /site_director.sql
@@ -2,6 +2,7 @@ WITH gather_hs_data AS (
   SELECT
     Contact_Id,
     site_short,
+    site_c,
 
     CASE
       WHEN grade_c = '12th Grade'
@@ -31,8 +32,16 @@ SELECT
   site_short,
   SUM(above_325_gpa) AS SD_senior_above_325,
   SUM(male_student) AS SD_ninth_grade_male,
-  SUM(first_gen_and_low_income) AS SD_ninth_grade_first_gen_low_income
+  SUM(first_gen_and_low_income) AS SD_ninth_grade_first_gen_low_income,
+  MAX(Account.College_Track_FY_HS_Planned_Enrollment_c) AS hs_budget_capacity
 FROM
-  gather_hs_data
+  gather_hs_data GSD
+  LEFT JOIN `data-warehouse-289815.salesforce.account` Account ON Account.Id = GSD.site_c
+  
 GROUP BY
   site_short
+  
+  
+  
+
+  
