b92c3180680716e29f046807d3cfdcffde4482a4
Baker Renneckar <baker@bakerr.com>
2021-04-09 18:03:40 -0400

    Auto-commit: Fri Apr 09 2021 18:03:33 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/users_clean/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/users_clean/Tab 5.sql
@@ -1,3 +1,20 @@
+WITH gather_group_members AS (
+  SELECT
+    name,
+    user_or_group_id,
+    group_id
+  FROM
+    `data-warehouse-289815.salesforce.group_member`
+--   WHERE
+--     group_id IN (
+--       SELECT
+--         Id
+--       FROM
+--         `data-warehouse-289815.salesforce.group`
+--       WHERE
+--         name LIKE "%Shared CC Advising%"
+--     )
+)
+
 SELECT *
-FROM `data-warehouse-289815.salesforce_clean.user_clean`
-WHERE email = 'lkong@collegetrack.org'
+FROM gather_group_members
