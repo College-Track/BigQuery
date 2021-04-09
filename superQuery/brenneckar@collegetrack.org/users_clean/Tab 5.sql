2a9ff30f44594e148356de64957c9ecb60c24b55
Baker Renneckar <baker@bakerr.com>
2021-04-09 18:04:23 -0400

    Auto-commit: Fri Apr 09 2021 18:04:16 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/users_clean/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/users_clean/Tab 5.sql
@@ -1,20 +1,11 @@
 WITH gather_group_members AS (
-  SELECT
-    name,
-    user_or_group_id,
-    group_id
-  FROM
-    `data-warehouse-289815.salesforce.group_member`
---   WHERE
---     group_id IN (
---       SELECT
---         Id
---       FROM
---         `data-warehouse-289815.salesforce.group`
---       WHERE
---         name LIKE "%Shared CC Advising%"
---     )
+      SELECT
+        Id,
+        Name
+      FROM
+        `data-warehouse-289815.salesforce.group`
+    --   WHERE
+    --     name LIKE "%Shared CC Advising%"
 )
-
 SELECT *
 FROM gather_group_members
