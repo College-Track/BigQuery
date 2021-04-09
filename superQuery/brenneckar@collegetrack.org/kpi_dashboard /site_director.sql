a3e8be743dac66509c3e4d2d6e746580cd6ad813
Baker Renneckar <baker@bakerr.com>
2021-04-09 12:00:09 -0400

    Auto-commit: Fri Apr 09 2021 12:00:01 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /site_director.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /site_director.sql
@@ -3,20 +3,32 @@ WITH gather_hs_data AS (
     Contact_Id,
     site_short,
     CASE
-      WHEN Prev_AT_Cum_GPA >= 3.25 THEN 1
+      WHEN grade_c = 'Senior'
+      AND Prev_AT_Cum_GPA >= 3.25 THEN 1
       ELSE 0
     END AS above_325_gpa,
-    CASE WHEN Gender_c = 'Male' THEN 1
-    ELSE 0
+    CASE
+      WHEN grade_c = ' Freshman'
+      AND Gender_c = 'Male' THEN 1
+      ELSE 0
     END as male_student,
-    indicator_low_income_c,
-    first_generation_fy_20_c
-    -- CASE WHEN (indicator_low_income_c = true) AND (first_generation_fy_20_c = 'yes') THEN 1
-    -- ELSE 0
-    -- END AS first_gen_and_male 
-    FROM `data-warehouse-289815.salesforce_clean.contact_template`
-    WHERE college_track_status_c = '11A'
-    )
-    
-SELECT *
-FROM gather_hs_data
+    CASE
+      WHEN grade_c = 'Freshman'
+      AND (indicator_low_income_c = 'Yes')
+      AND (first_generation_fy_20_c = 'Yes') THEN 1
+      ELSE 0
+    END AS first_gen_and_male,
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+  WHERE
+    college_track_status_c = '11A'
+)
+SELECT
+  site_short,
+  SUM(above_325_gpa) AS SD_senior_above_325,
+  SUM(male_student) AS SD_ninth_grade_male,
+  SUM(first_gen_and_male) AS SD_ninth_grade_first_gen_male
+FROM
+  gather_hs_data
+GROUP BY
+  site_short
