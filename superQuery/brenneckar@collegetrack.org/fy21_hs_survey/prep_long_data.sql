cacc552f054fe94b08d226cd331bec272adfc5cb
Baker Renneckar <baker@bakerr.com>
2021-04-12 13:36:50 -0400

    Auto-commit: Mon Apr 12 2021 13:36:45 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/prep_long_data.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/prep_long_data.sql
@@ -6,6 +6,7 @@ C.high_school_graduating_class_c
 
 FROM `data-studio-260217.surveys.fy21_hs_survey_long` HSSL
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = HSSL.contact_id
+WHERE HSSL.Contact_Id IS NOT NULL AND site_short IS NOT NULL
 
 )
 
