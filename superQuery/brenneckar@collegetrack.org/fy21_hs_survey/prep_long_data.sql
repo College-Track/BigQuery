be8727576e30235af7dea5c0e08e9a1bee01b6de
Baker Renneckar <baker@bakerr.com>
2021-04-12 13:40:02 -0400

    Auto-commit: Mon Apr 12 2021 13:39:57 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/prep_long_data.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/prep_long_data.sql
@@ -1,15 +1,25 @@
 WITH gather_data AS (
-SELECT HSSL.*,
-C.site_short, 
-C.Most_Recent_GPA_Cumulative_bucket,
-C.high_school_graduating_class_c
-
-FROM `data-studio-260217.surveys.fy21_hs_survey_long` HSSL
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = HSSL.contact_id
-WHERE HSSL.Contact_Id IS NOT NULL AND site_short IS NOT NULL
-
+  SELECT
+    HSSL.contact_Id,
+    HSSL.question,
+    CASE WHEN HSSL.question = '5.2 At the end of the semester, what attendance % do you need in order to get $100 for Bank Book?'
+    AND HSSL.answer = '80%' THEN "Correct"
+    ELSE HSSL.answer
+    END AS answer,
+    
+    HSSL.section,
+    HSSL.sub_section,
+    C.site_short,
+    C.Most_Recent_GPA_Cumulative_bucket,
+    C.high_school_graduating_class_c
+  FROM
+    `data-studio-260217.surveys.fy21_hs_survey_long` HSSL
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = HSSL.contact_id
+  WHERE
+    HSSL.Contact_Id IS NOT NULL
+    AND site_short IS NOT NULL
 )
-
-
-SELECT *
-FROM gather_data
+SELECT
+*
+FROM
+  gather_data
