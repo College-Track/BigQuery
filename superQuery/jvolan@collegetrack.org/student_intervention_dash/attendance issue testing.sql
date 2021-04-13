389f7fe0f9f1d96e8d4b3735d8598482f07a4e7f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-13 13:24:59 -0700

    Auto-commit: Tue Apr 13 2021 13:24:53 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/attendance issue testing.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/attendance issue testing.sql
@@ -1,11 +1,8 @@
+
 SELECT
-    AT_Id,
-    AT_Name,
-    attendance_rate_c,
-    Attendance_Rate_Previous_Term_c,
-   
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE current_as_c = TRUE
-    AND college_track_status_c IN ("11A", "12A")
-    AND AT_id = 'a1a1M0000072PRDQA2'
+    date_c,
+    academic_semester_c,
+    FROM `data-warehouse-289815.salesforce_clean.class_template`
+    WHERE Attendance_c IN ('Enrolled','Tardy','Make Up','Drop-in')
+    GROUP BY academic_semester_c, date_c 
+    ORDER BY date_c DESC LIMIT 1
