1f08154fd564ae491e04af8f6284a87bea8f5885
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-13 13:40:28 -0700

    Auto-commit: Tue Apr 13 2021 13:40:24 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/attendance issue testing.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/attendance issue testing.sql
@@ -4,4 +4,4 @@ SELECT
     academic_semester_c,
     FROM `data-warehouse-289815.salesforce_clean.class_template`
     WHERE Attendance_c IN ('Enrolled','Tardy','Make Up','Drop-in')
-    GROUP BY academic_semester_c, date_c
+    GROUP BY academic_semester_c
