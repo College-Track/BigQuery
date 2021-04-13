fedff7f3ea37898e8efff978b611ae7ddc96627c
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-13 13:40:47 -0700

    Auto-commit: Tue Apr 13 2021 13:40:44 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/student_intervention_dash/attendance issue testing.sql
+++ superQuery/jvolan@collegetrack.org/student_intervention_dash/attendance issue testing.sql
@@ -2,6 +2,7 @@
 SELECT
     max(date_c) AS most_recent_attended_workshop,
     academic_semester_c,
+    Student_c,
     FROM `data-warehouse-289815.salesforce_clean.class_template`
     WHERE Attendance_c IN ('Enrolled','Tardy','Make Up','Drop-in')
     GROUP BY academic_semester_c
