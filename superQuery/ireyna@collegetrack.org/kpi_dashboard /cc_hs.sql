3f5478f2b87d5567d9b4fb44be34d51527b110cd
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-14 10:57:56 -0700

    Auto-commit: Wed Apr 14 2021 10:57:50 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -1,4 +1,29 @@
-SELECT
+WITH gather_data AS (
+  SELECT
+    Contact_Id,
+    site_short,
+    estimated_family_contribution_c,
+    CASE
+        WHEN estimated_family_contribution_c IS NOT NULL THEN 1
+        ELSE estimated_family_contribution_c
+    END AS cc_hs_EFC_10th,
+    (SELECT subq.student_c
+        FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq
+        WHERE subq.admission_status_c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred")
+        AND subq.College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
+        AND Contact_Id=subq.student_c
+        group by subq.student_c
+        ) AS  applied_best_good_situational,
+        
+FROM `data-warehouse-289815.salesforce_clean.contact_template` AS Contact   
+LEFT JOIN `data-warehouse-289815.salesforce_clean.college_application_clean` AS CollegeApp 
+        ON Contact.contact_id = CollegeApp.student_c 
+  WHERE
+    grade_c = '12th Grade'
+    AND college_track_status_c = '11A'
+)
+#,prep_metrics_id_count AS (
+  SELECT
     contact_id,
     COUNT(DISTINCT applied_best_good_situational) AS cc_hs_best_good_situational,
     SUM(cc_hs_EFC_10th)
