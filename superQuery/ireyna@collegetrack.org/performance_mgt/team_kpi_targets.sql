b48147162e240f8f58630d312cef01d5d530605f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-13 21:04:47 -0700

    Auto-commit: Tue Apr 13 2021 21:04:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
@@ -10,5 +10,6 @@ enter_the_target_percent_,
     select enter_the_target_numeric_ as value2 from `data-warehouse-289815.google_sheets.team_kpi_target`
     union
     select enter_the_target_percent_ as value3 from `data-warehouse-289815.google_sheets.team_kpi_target`
-) tt where value is not null
+) tt 
+where value is not null
 from `data-warehouse-289815.google_sheets.team_kpi_target`
