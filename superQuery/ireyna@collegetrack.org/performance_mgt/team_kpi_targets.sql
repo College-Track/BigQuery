7cb5317898e9a6adeb6591163c59277a0608413a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-13 21:01:50 -0700

    Auto-commit: Tue Apr 13 2021 21:01:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
@@ -1,12 +1,13 @@
-select 
+SELECT
 submission_id,
-enter_the_target_non_numeric_ AS KPI__target_str,
-enter_the_target_numeric_ AS KPI__target_num,
-enter_the_target_percent_ AS KPI__target_perc
+enter_the_target_non_numeric_, 
+enter_the_target_numeric_, 
+enter_the_target_percent_
 from `data-warehouse-289815.google_sheets.team_kpi_target`
-unpivot
 (
-  value
-  for col in (enter_the_target_non_numeric_, enter_the_target_numeric_, enter_the_target_percent_)
-) un
-order by submission_id
+    select enter_the_target_non_numeric_ as value1 from `data-warehouse-289815.google_sheets.team_kpi_target`
+    union
+    select enter_the_target_numeric_ as value2 from `data-warehouse-289815.google_sheets.team_kpi_target`
+    union
+    select enter_the_target_percent_ as value3 from `data-warehouse-289815.google_sheets.team_kpi_target`
+) tt where value is not null
