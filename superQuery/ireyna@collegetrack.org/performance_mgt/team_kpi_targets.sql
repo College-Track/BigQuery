3eb58ccb0656026a5cb2e67b432e07f8a7bbb98b
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-13 20:59:04 -0700

    Auto-commit: Tue Apr 13 2021 20:58:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
@@ -1,22 +1,12 @@
-/*
-CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.team_kpi_targets`
-OPTIONS
-    (
-    description="Filtered table to house CT Team KPIs and respective Targets"
-    )
-AS
-*/
-SELECT 
-submission_id,
-select_your_team_function AS Team,
-team_role,
-select_role AS Role,
-site AS Site,
-select_kpi AS KPI,
-enter_the_target_non_numeric_ AS KPI_str,
-enter_the_target_numeric_ AS KPI_num,
-enter_the_target_percent_ AS KPI_perc
-
-#select_your_site
-FROM `data-warehouse-289815.google_sheets.team_kpi_target`
-LIMIT 10
+select 
+submission_id,,
+enter_the_target_non_numeric_ AS KPI__target_str,
+enter_the_target_numeric_ AS KPI__target_num,
+enter_the_target_percent_ AS KPI__target_perc
+from `data-warehouse-289815.google_sheets.team_kpi_target`
+unpivot
+(
+  value
+  for col in (enter_the_target_non_numeric_, enter_the_target_numeric_, enter_the_target_percent_)
+) un
+order by submission_id
