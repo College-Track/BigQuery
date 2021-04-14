de4219bf572e74846003fc9241531581c09005e4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-14 11:45:25 -0700

    Auto-commit: Wed Apr 14 2021 11:45:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -1,4 +1,5 @@
-SELECT
+WITH gather_data AS (
+  SELECT
     Contact_Id,
     site_short,
     grade_c,
@@ -7,14 +8,23 @@ SELECT
     CASE
         WHEN (FA_Req_Expected_Financial_Contribution_c IS NOT NULL) AND (fa_req_efc_source_c = 'FAFSA4caster') THEN 1
         ELSE 0
-    END AS hs_EFC_10th,
-    (SELECT subq.student_c
-        FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq
+        END AS hs_EFC_10th,
+    (SELECT contact_id,
+        FROM `data-warehouse-289815.salesforce_clean.contact_template`
+        WHERE grade_c = '10th Grade'
+        AND college_track_status_c = '11A'
+        ) AS count_10th_efc,
+    (SELECT student_c
+        FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subquery_collegeapp
+        LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template`AS subquery_contact
+            ON subquery_contact.contact_id = subquery_collegeapp.student_c 
         #WHERE subq.admission_status_c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred")
-        WHERE subq.College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
-        AND Contact_Id=subq.student_c
-        group by subq.student_c
-        ) AS  applied_best_good_situational,
+        WHERE College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
+        AND Contact_Id=student_c
+        AND grade_c = '12th Grade'
+        AND college_track_status_c = '11A'
+        group by subquery_collegeapp.student_c
+        ) AS applied_best_good_situational,
         
 FROM `data-warehouse-289815.salesforce_clean.contact_template` AS Contact   
 LEFT JOIN `data-warehouse-289815.salesforce_clean.college_application_clean` AS CollegeApp 
@@ -22,3 +32,15 @@ LEFT JOIN `data-warehouse-289815.salesforce_clean.college_application_clean` AS 
         
 WHERE grade_c = '12th Grade'
 AND college_track_status_c = '11A'
+)
+  SELECT
+    site_short,
+    COUNT(DISTINCT applied_best_good_situational) AS cc_hs_best_good_situational,
+    SUM(hs_EFC_10th) AS cc_hs_EFC_10th
+  FROM
+    gather_data
+  GROUP BY
+    site_short
+
+
+
