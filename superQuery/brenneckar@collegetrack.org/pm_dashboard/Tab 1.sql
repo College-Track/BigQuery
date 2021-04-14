0bf1a6a32100dec846b573e64ec3453de7257194
Baker Renneckar <baker@bakerr.com>
2021-04-14 10:44:42 -0400

    Auto-commit: Wed Apr 14 2021 10:44:30 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 1.sql
@@ -1,3 +1,9 @@
-SELECT KPI_Selection.*
-FROM `data-studio-260217.performance_mgt.role_kpi_selection` KPI_Selection
-LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target ON KPI_Target.select_your_team_function = KPI_Selection.function
+SELECT
+  KPI_Selection.*,
+  CASE WHEN KPI_Target.select_role IS NOT NULL THEN true
+  ELSE false
+  END AS target_submitted
+FROM
+  `data-studio-260217.performance_mgt.role_kpi_selection` KPI_Selection
+  LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target ON 
+  KPI_Target.select_your_team_function = KPI_Selection.function AND KPI_Target.select_role = KPI_Selection.role AND KPI_Target.select_kpi = KPI_Selection.KPI
