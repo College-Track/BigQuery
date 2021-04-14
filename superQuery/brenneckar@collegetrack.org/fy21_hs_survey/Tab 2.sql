cce5e8b4034a533e1289303da9e2014cdac32ddd
Baker Renneckar <baker@bakerr.com>
2021-04-14 16:46:29 -0400

    Auto-commit: Wed Apr 14 2021 16:46:24 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
@@ -12,7 +12,7 @@ WITH gather_data AS(
       FROM
         `data-warehouse-289815.salesforce.contact_pipeline_history_c`
       WHERE
-        created_date >= '2021-02-18'
+        created_date >= '2021-02-17T22:00:00.000Z'
         AND Name = 'Started/Restarted CT HS Program'
     )
   GROUP BY
