44a7421b68a5982dc1069008b6f45bd625fd2de0
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-14 11:16:39 -0700

    Auto-commit: Wed Apr 14 2021 11:16:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -11,7 +11,7 @@ WITH gather_data AS (
     (SELECT subq.student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq
         #WHERE subq.admission_status_c IN ("Accepted", "Accepted and Enrolled", "Accepted and Deferred")
-        AND subq.College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
+        WHERE subq.College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
         AND Contact_Id=subq.student_c
         group by subq.student_c
         ) AS  applied_best_good_situational,
