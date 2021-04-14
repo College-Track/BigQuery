dfd1892d0113cd3ae5fe05cb0850f802b1da4202
Baker Renneckar <baker@bakerr.com>
2021-04-14 16:46:11 -0400

    Auto-commit: Wed Apr 14 2021 16:46:04 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
@@ -1,16 +1,24 @@
-WITH gather_data AS( SELECT 
+WITH gather_data AS(
+  SELECT
     site_short,
     COUNT(Contact_Id) as student_count
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
-WHERE College_Track_Status_c IN ('11A', '12A', '18a') 
-AND
-Contact_Id NOT IN (
-    SELECT Contact_c
-    FROM `data-warehouse-289815.salesforce.contact_pipeline_history_c`
-    WHERE created_date >= '2021-02-17' AND Name = 'Started/Restarted CT HS Program'
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+  WHERE
+    College_Track_Status_c IN ('11A', '12A', '18a')
+    AND Contact_Id NOT IN (
+      SELECT
+        Contact_c
+      FROM
+        `data-warehouse-289815.salesforce.contact_pipeline_history_c`
+      WHERE
+        created_date >= '2021-02-18'
+        AND Name = 'Started/Restarted CT HS Program'
     )
-    GROUP BY site_short
+  GROUP BY
+    site_short
 )
-
-SELECT SUM(student_count)
-FROM gather_data
+SELECT
+  SUM(student_count)
+FROM
+  gather_data
