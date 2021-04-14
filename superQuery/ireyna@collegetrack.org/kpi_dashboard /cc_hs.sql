9156ed9de14a86bd5b1746a85681c6e567f7c4a6
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-14 12:46:38 -0700

    Auto-commit: Wed Apr 14 2021 12:46:36 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -2,9 +2,9 @@
         contact_id,
         site_short,
         (SELECT student_c
-        FROM `data-warehouse-289815.salesforce_clean.college_application_clean` 
-        WHERE College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
-        AND Contact_Id=student_c
+        FROM `data-warehouse-289815.salesforce_clean.college_application_clean` AS subq1
+        WHERE College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")  
+        AND Contact_Id=subq1.student_c
         group by student_c
         ) AS applied_best_good_situational,
             
@@ -18,3 +18,4 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE  college_track_status_c = '11A'
     AND grade_c = '12th Grade'
+),
