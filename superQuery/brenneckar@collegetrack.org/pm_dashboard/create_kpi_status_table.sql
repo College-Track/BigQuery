f24fdbb537c8ecf5cfde70517ec1e9226723ba6e
Baker Renneckar <baker@bakerr.com>
2021-04-14 12:12:25 -0400

    Auto-commit: Wed Apr 14 2021 12:12:21 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/create_kpi_status_table.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/create_kpi_status_table.sql
@@ -5,14 +5,12 @@ WITH gather_data AS (
       WHEN KPI_Target.select_role IS NOT NULL THEN true
       ELSE false
     END AS target_submitted,
-    CASE 
-    WHEN enter_the_target_numeric_ IS NOT NULL THEN enter_the_target_numeric_
-    WHEN enter_the_target_percent_ iS NOT NULL THEN enter_the_target_percent_
-    ELSE NULL
+    CASE
+      WHEN enter_the_target_numeric_ IS NOT NULL THEN enter_the_target_numeric_
+      WHEN enter_the_target_percent_ iS NOT NULL THEN enter_the_target_percent_
+    --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_
+      ELSE NULL
     END AS target
-    
-
-
   FROM
     `data-studio-260217.performance_mgt.role_kpi_selection` KPI_Selection
     LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target ON KPI_Target.team_kpi = REPLACE(KPI_Selection.function, ' ', '_')
@@ -23,10 +21,5 @@ SELECT
   *
 FROM
   gather_data
-  WHERE target_submitted = true
-
-  
-  
-  
-  
-  
+WHERE
+  target_submitted = true
