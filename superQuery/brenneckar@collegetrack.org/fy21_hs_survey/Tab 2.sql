bbc859cc674cd0836cf305f1f30567f46911f844
Baker Renneckar <baker@bakerr.com>
2021-04-14 16:50:04 -0400

    Auto-commit: Wed Apr 14 2021 16:49:59 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
@@ -1,6 +1,8 @@
 WITH gather_data AS(
   SELECT
     site_short,
+    high_school_graduating_class_c,
+    Most_Recent_GPA_Cumulative_bucket,
     COUNT(Contact_Id) as student_count
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template`
@@ -16,9 +18,28 @@ WITH gather_data AS(
         AND Name = 'Started/Restarted CT HS Program'
     )
   GROUP BY
-    site_short
+    site_short,
+    high_school_graduating_class_c,
+    Most_Recent_GPA_Cumulative_bucket
+),
+gather_completed_survey_data AS (
+  SELECT
+    site_short,
+    high_school_graduating_class_c,
+    Most_Recent_GPA_Cumulative_bucket,
+    count(contact_Id) as completed_survey_count
+  FROM
+    `data-studio-260217.surveys.fy21_hs_survey_wide_prepped`
+  GROUP BY
+    site_short,
+    high_school_graduating_class_c,
+    Most_Recent_GPA_Cumulative_bucket
 )
 SELECT
-  SUM(student_count)
+  GD.*,
+  gather_completed_survey_data.completed_survey_count
 FROM
-  gather_data
+  gather_data GD
+  LEFT JOIN gather_completed_survey_data ON gather_completed_survey_data.site_short = GD.site_short
+  AND gather_completed_survey_data.high_school_graduating_class_c = GD.high_school_graduating_class_c
+  AND gather_completed_survey_data.Most_Recent_GPA_Cumulative_bucket = GD.Most_Recent_GPA_Cumulative_bucket
