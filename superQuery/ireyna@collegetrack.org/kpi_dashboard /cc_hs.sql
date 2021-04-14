acadbb533aa05fa6d586f818eabb2681c14f1a3e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-14 11:20:56 -0700

    Auto-commit: Wed Apr 14 2021 11:20:53 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -1,12 +1,11 @@
-WITH gather_data AS (
-  SELECT
+ SELECT
     Contact_Id,
     site_short,
     grade_c,
     FA_Req_Expected_Financial_Contribution_c,
     CASE
         WHEN ((FA_Req_Expected_Financial_Contribution_c IS NOT NULL) OR (FA_Req_Expected_Financial_Contribution_c<> 0)) THEN 1
-        ELSE FA_Req_Expected_Financial_Contribution_c
+        ELSE FA_Req_Expected_Financial_Contribution_c = 0
     END AS cc_hs_EFC_10th,
     (SELECT subq.student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq
@@ -22,14 +21,3 @@ LEFT JOIN `data-warehouse-289815.salesforce_clean.college_application_clean` AS 
   WHERE
     grade_c = '12th Grade'
     AND college_track_status_c = '11A'
-)
-#,prep_metrics_id_count AS (
-  SELECT
-    contact_id,
-    COUNT(DISTINCT applied_best_good_situational) AS cc_hs_best_good_situational,
-    cc_hs_EFC_10th
-  FROM
-    gather_data
-  GROUP BY
-    contact_id,
-    cc_hs_EFC_10th
