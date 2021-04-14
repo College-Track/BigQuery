5b17f688d2bc483c235794babe3606f601ba5601
Baker Renneckar <baker@bakerr.com>
2021-04-14 16:40:06 -0400

    Auto-commit: Wed Apr 14 2021 16:40:00 GMT-0400 (Eastern Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 2.sql
@@ -1,3 +1,11 @@
-SELECT distinct answer
-FROM `data-studio-260217.surveys.fy21_hs_survey_long_prepped`
-WHERE answer LIKE '%Yes, I have taken%'
+SELECT 
+    Contact_Id, 
+    site_short
+FROM `data-warehouse-289815.salesforce_clean.contact_template`
+WHERE College_Track_Status_c IN ('11A', '12A', '18a') 
+AND
+Contact_Id NOT IN (
+    SELECT Contact_c
+    FROM `data-warehouse-289815.salesforce.contact_pipeline_history_c`
+    WHERE created_date >= '2021-02-17' AND Name = 'Started/Restarted CT HS Program'
+    )
