2aa70722eb33ec7fbebb5fca80f2b9a4391ccef9
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-15 13:32:48 -0700

    Auto-commit: Thu Apr 15 2021 13:32:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -1,9 +1,9 @@
-
+/*WITH get_contact_data AS
+(
     SELECT
     Contact_Id,
     site_short,
     high_school_graduating_class_c,
-    indicator_completed_ct_hs_program_c,
     current_school_name,
     current_school_type_c_degree,
     current_enrollment_status_c,
@@ -16,3 +16,18 @@
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE indicator_completed_ct_hs_program_c = true
+),
+
+get_on_track AS
+(
+*/
+    SELECT 
+    Contact_Id,
+    on_track_c,
+    graduated_or_on_track_bucket_at_c,
+    indicator_graduated_or_on_track_at_c
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE cumulative_credits_awarded_max_calc_c >0
+    AND previous_as_c = true
+    OR prev_prev_as_c = true
