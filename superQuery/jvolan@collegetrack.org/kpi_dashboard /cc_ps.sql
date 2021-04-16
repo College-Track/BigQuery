b93186617cbeb3b182a1881d86cf76072feec332
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-16 16:05:18 -0700

    Auto-commit: Fri Apr 16 2021 16:05:13 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -1,7 +1,7 @@
 WITH get_fafsa_data AS    
 (
     SELECT 
-    site_short AS site,
+    site_short AS fafsa_site,
     CASE
         WHEN fa_req_fafsa_c = 'Submitted' then 1
         Else 0  
@@ -10,6 +10,16 @@ WITH get_fafsa_data AS
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE college_track_status_c IN ('11A','12A')
     AND grade_c = "12th Grade"
+),
+
+kpi_fafsa_complete AS
+(
+    SELECT
+    fafsa_site,
+    sum(indicator_fafsa_complete) AS cc_ps_fafsa_complete,
+    
+    FROM get_fafsa_data
+    GROUP BY fafsa_site
 ),
 
 get_projected_6_year_grad_data AS
@@ -33,20 +43,22 @@ get_projected_6_year_grad_data AS
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE grade_c = 'Year 6'
     AND indicator_completed_ct_hs_program_c = true
-)
+),
 
-
+kpi_projected_6_year_grad AS
+(
     SELECT
     site_short,
     SUM(projected_6_year_grad) + SUM(alumni_already) AS cc_ps_6_year_grad_num,
     count(Contact_Id) AS cc_ps_6_year_grad_denom,
     SUM(projected_6_year_grad) AS projected_6_year_grad,
     SUM(alumni_already) AS alumni_already,
-    SUM(get_fafsa_data.indicator_fafsa_complete) AS cc_ps_fafsa_complete,
-
     
     FROM get_projected_6_year_grad_data
-    LEFT JOIN get_fafsa_data ON get_fafsa_data.site = site_short
     GROUP BY site_short
+)
 
-
+    SELECT
+    *
+    FROM kpi_projected_6_year_grad
+    LEFT JOIN kpi_fafsa_complete ON kpi_fafsa_complete.fafsa_site = site_short
