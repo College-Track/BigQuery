5270dc347e982ca4a56f12a494fc371290b641fb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-19 14:45:26 -0700

    Auto-commit: Mon Apr 19 2021 14:45:22 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 7.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 7.sql
@@ -1,39 +1,40 @@
 WITH GATHER AS 
 (
+     
     SELECT 
-    Contact_Id,
-    fit_type_current_c,
-    id,
+        contact_id,
+        site_short,
         
         CASE 
             WHEN a.id IS NOT NULL THEN 1
             ELSE 0
             END AS student_has_aspirations,
+        
+        CASE
+            WHEN fit_type_current_c IN ("Best Fit","Good Fit","Local Affordable") THEN 1
+            ELSE 0
+            END AS aspirations_affordable
     
-        (SELECT student_c
-        FROM `data-warehouse-289815.salesforce.college_aspiration_c` AS subq1
-        WHERE fit_type_current_c IN ("Best Fit","Good Fit","Local Affordable")
-        AND Contact_Id=student_c
-        group by student_c
-        ) AS aspirations_affordable,
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_template` c
-        LEFT JOIN `data-warehouse-289815.salesforce.college_aspiration_c` a ON c.contact_id=a.student_c
+    FROM `data-warehouse-289815.salesforce_clean.contact_template` c 
+        LEFT JOIN`data-warehouse-289815.salesforce.college_aspiration_c` a ON c.contact_id=a.student_c
     
     WHERE college_track_status_c = '11A'
     AND c.grade_c = '11th Grade'
    )
    
-   SELECT 
-    contact_id,
-    CASE 
-        WHEN aspirations_affordable IS NOT NULL THEN 1
-        ELSE 0
-        END AS cc_hs_six_aspirations,
-    SUM(student_has_aspirations)
-    
+  SELECT
+        site_short,
+        
+        CASE 
+            WHEN SUM(student_has_aspirations) >= 6 THEN 1
+            ELSE 0
+            END AS cc_hs_aspirations_count,
+        
+        CASE 
+            WHEN SUM(aspirations_affordable) >= 3 THEN 1
+            ELSE 0
+            END AS cc_hs_aspirations_affordable_count
+        
     FROM gather
-    
-    GROUP BY 
-    contact_id,aspirations_affordable
+    GROUP BY site_short
    
