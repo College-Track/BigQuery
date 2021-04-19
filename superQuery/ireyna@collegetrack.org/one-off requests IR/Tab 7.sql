78d9495def0aa472ad4c0156390b362e1dc2bc41
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-19 13:58:56 -0700

    Auto-commit: Mon Apr 19 2021 13:58:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 7.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 7.sql
@@ -1,3 +1,33 @@
-SELECT * 
-FROM `data-warehouse-289815.google_sheets.team_kpi_target`
-LIMIT 25
+WITH GATHER AS 
+(SELECT 
+        student_c,
+        fit_type_current_c,
+        
+        CASE 
+            WHEN a.id IS NOT NULL THEN 1
+            ELSE 0
+            END AS student_has_aspirations,
+    
+        (SELECT student_c
+        FROM `data-warehouse-289815.salesforce.college_aspiration_c` AS subq1
+        WHERE fit_type_current_c IN ("Best Fit","Good Fit","Local Affordable")
+        AND Contact_Id=student_c
+        group by student_c
+        ) AS aspirations_affordable,
+    
+    FROM `data-warehouse-289815.salesforce.college_aspiration_c` a 
+        LEFT JOIN`data-warehouse-289815.salesforce_clean.contact_template` c ON c.contact_id=a.student_c
+    
+    WHERE college_track_status_c = '11A'
+    AND c.grade_c = '11th Grade'
+   )
+   
+   SELECT 
+    student_c,
+    aspirations_affordable,
+    student_has_aspirations
+    
+    FROM gather
+    
+    group by student_c,aspirations_affordable,
+    student_has_aspirations
