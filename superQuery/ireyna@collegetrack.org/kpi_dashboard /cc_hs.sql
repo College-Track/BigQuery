e55357e1098596ec782340f2f354c8e789819dc1
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-19 11:47:43 -0700

    Auto-commit: Mon Apr 19 2021 11:47:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -1,7 +1,7 @@
-SELECT student_c, SUM(Attendance_Numerator_excluding_make_up_c) AS attendance_numerator, SUM(Attendance_Denominator_c) AS attendance_denominator, Cancelled_c
+SELECT student_c, SUM(Attendance_Numerator_excluding_make_up_c) AS attendance_numerator, SUM(Attendance_Denominator_c) AS attendance_denominator
     #SUM(Attendance_Numerator_excluding_make_up_c) / SUM(Attendance_Denominator_c) AS attendance_rate
     FROM `data-warehouse-289815.salesforce_clean.class_template` 
     WHERE Department_c = "College Completion"
     AND Cancelled_c = FALSE
-    AND global_academic_semester_c IN ('a3646000000dMXnAAM','a3646000000dMXoAAM','a3646000000dMXpAAM') #Fall 2020-21, Spring 2020-21
-    GROUP BY student_c,Attendance_Numerator_excluding_make_up_c,Attendance_Denominator_c, Cancelled_c
+    AND global_academic_semester_c IN ('a3646000000dMXnAAM','a3646000000dMXoAAM','a3646000000dMXpAAM') #Fall 2020-21, Spring 2020-21, Summer 2020-21
+    GROUP BY student_c
