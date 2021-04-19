45cf021d642081a8224a99fd6864f54f564a8ecd
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-19 16:43:25 -0700

    Auto-commit: Mon Apr 19 2021 16:43:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -10,23 +10,19 @@ gather_data_tenth_grade AS (
   SELECT
     site_short,
     contact_id,
-    
     CASE
         WHEN (FA_Req_Expected_Financial_Contribution_c IS NOT NULL) AND (fa_req_efc_source_c = 'FAFSA4caster') THEN 1
         ELSE 0
         END AS hs_EFC_10th
-    
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE  college_track_status_c = '11A'
     AND grade_c = '10th Grade'
-    
 ),
 
 gather_data_eleventh_grade AS (
     SELECT 
         contact_id,
         site_short,
-        
         CASE 
             WHEN a.id IS NOT NULL THEN 1
             ELSE 0
@@ -36,34 +32,26 @@ gather_data_eleventh_grade AS (
             WHEN fit_type_current_c IN ("Best Fit","Good Fit","Local Affordable") THEN 1
             ELSE 0
             END AS aspirations_affordable
-    
     FROM `data-warehouse-289815.salesforce_clean.contact_template` c 
         LEFT JOIN`data-warehouse-289815.salesforce.college_aspiration_c` a ON c.contact_id=a.student_c
     
     WHERE college_track_status_c = '11A'
     AND c.grade_c = '11th Grade'
-    
 ),
-
-
 
 gather_attendance_data AS (
     SELECT 
         c.student_c, 
-    
         CASE
             WHEN SUM(Attendance_Denominator_c) = 0 THEN NULL
             ELSE SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c)
             END AS attendance_rate
-    
     FROM `data-warehouse-289815.salesforce_clean.class_template` AS c
         LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT 
         ON CAT.global_academic_semester_c = c.global_academic_semester_c
-        
     WHERE Department_c = "College Completion"
     AND Cancelled_c = FALSE
     AND CAT.AY_Name = 'AY 2020-21'
-    
     GROUP BY c.student_c
 ),
         
@@ -110,7 +98,6 @@ gather_data_twelfth_grade AS (
         
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
         LEFT JOIN gather_attendance_data ON contact_id=student_c
-    
     WHERE  college_track_status_c = '11A'
     AND grade_c = '12th Grade'
 ),
@@ -132,7 +119,7 @@ gather_data_twelfth_grade AS (
     FROM prep_aspiration_kpi
     GROUP BY site_short
 ),*/
-prep_eleventh_grade_metrics AS (
+gather_eleventh_grade_metrics AS (
  SELECT
     site_short,
     contact_id,
@@ -140,30 +127,25 @@ prep_eleventh_grade_metrics AS (
         WHEN student_has_aspirations >= 6 AND aspirations_affordable >= 3 THEN 1
         ELSE 0
         END AS cc_hs_aspirations
-        
     FROM gather_data_eleventh_grade 
-       
 ),
-prep_twelfth_grade_metrics AS(
+
+gather_twelfth_grade_metrics AS(
     SELECT  
         site_short,
-        
         CASE 
             WHEN attendance_rate >= 0.8 THEN 1
             ELSE 0
             END AS cc_hs_above_80_cc_attendance,
-        
         CASE 
             WHEN accepted_enrolled_affordable IS NOT NULL THEN 1
             WHEN applied_accepted_affordable IS NOT NULL THEN 1
             ELSE 0
             END AS cc_hs_accepted_affordable,
-        
         CASE 
             WHEN applied_best_good_situational IS NOT NULL THEN 1
             ELSE 0
             END AS cc_hs_applied_best_good_situational,
-        
         CASE 
             WHEN accepted_enrolled_best_good_situational IS NOT NULL THEN 1
             WHEN applied_accepted_best_good_situational IS NOT NULL THEN 1
@@ -171,27 +153,40 @@ prep_twelfth_grade_metrics AS(
             END AS cc_hs_accepted_best_good_situational
     
     FROM gather_data_twelfth_grade
-)
+),
 
-  SELECT
-    gd.site_short, 
-    SUM(hs_EFC_10th) AS cc_hs_EFC_tenth_grade, #10th grade
-    SUM(cc_hs_aspirations) AS cc_hs_aspirations, #11th grade
-    SUM(cc_hs_above_80_cc_attendance) AS cc_hs_above_80_cc_attendance,#12th grade 
-    SUM(cc_hs_accepted_affordable) AS cc_hs_accepted_affordable,
-    SUM(cc_hs_applied_best_good_situational) AS cc_hs_applied_best_good_situational, #12th grade
-    SUM(cc_hs_accepted_best_good_situational) AS cc_hs_accepted_best_good_situational #12th grade
-    
-  FROM
-        gather_data AS gd
-        
-        LEFT JOIN gather_data_tenth_grade AS tenth_grade_data
-            ON GD.site_short = tenth_grade_data.site_short
-     
-        LEFT JOIN prep_eleventh_grade_metrics AS eleventh_grade_data
-            ON GD.site_short = eleventh_grade_data.site_short
-        
-        LEFT JOIN prep_twelfth_grade_metrics AS twelfth_grade_data
-            ON GD.site_short = twelfth_grade_data.site_short
-    
-GROUP BY gd.site_short
+prep_tenth_grade_metrics AS (
+    SELECT 
+        site_short,
+        SUM(hs_EFC_10th) AS cc_hs_EFC_tenth_grade
+    FROM gather_data_tenth_grade  
+    GROUP BY site_short
+),
+
+prep_eleventh_grade_metrics AS (
+    SELECT 
+        site_short,
+        SUM(cc_hs_aspirations) AS cc_hs_aspirations
+    FROM gather_eleventh_grade_metrics
+    GROUP BY site_short
+),
+
+prep_twelfth_grade_metrics AS (
+    SELECT 
+        site_short,
+        SUM(cc_hs_above_80_cc_attendance) AS cc_hs_above_80_cc_attendance,
+        SUM(cc_hs_accepted_affordable) AS cc_hs_accepted_affordable,
+        SUM(cc_hs_applied_best_good_situational) AS cc_hs_applied_best_good_situational,
+        SUM(cc_hs_accepted_best_good_situational) AS cc_hs_accepted_best_good_situational 
+    FROM gather_twelfth_grade_metrics
+    GROUP BY site_short
+)
+SELECT 
+    gd.site_short,
+    kpi_10th.* EXCEPT(site_short),
+    kpi_11th.* EXCEPT(site_short),
+    kpi_12th.* EXCEPT(site_short)
+    FROM gather_data as gd
+        LEFT JOIN prep_tenth_grade_metrics AS kpi_10th ON gd.site_short = kpi_10th.site_short
+        LEFT JOIN prep_eleventh_grade_metrics AS kpi_11th ON gd.site_short = kpi_11th.site_short
+        LEFT JOIN prep_twelfth_grade_metrics AS kpi_12th ON gd.site_short = kpi_12th.site_short
