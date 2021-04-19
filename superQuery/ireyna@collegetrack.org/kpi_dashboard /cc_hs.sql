581077001e3b63afa99653aef89bce49121d3184
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-19 11:23:05 -0700

    Auto-commit: Mon Apr 19 2021 11:22:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -1,5 +1,5 @@
 SELECT student_c, SUM(Attendance_Numerator_excluding_make_up_c) AS attendance_numerator, SUM(Attendance_Denominator_c) AS attendance_denominator, 
-    SUM(Attendance_Numerator_excluding_make_up_c) / SUM(Attendance_Denominator_c) AS attendance_rate
+    #SUM(Attendance_Numerator_excluding_make_up_c) / SUM(Attendance_Denominator_c) AS attendance_rate
     FROM `data-warehouse-289815.salesforce_clean.class_template` 
     WHERE Department_c = "College Completion"
     GROUP BY student_c,Attendance_Numerator_excluding_make_up_c,Attendance_Denominator_c
