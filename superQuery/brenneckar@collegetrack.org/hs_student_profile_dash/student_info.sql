b85323c08c7e34c9c4dfeb5da956410a68413bed
Baker Renneckar <baker@bakerr.com>
2021-04-19 17:24:54 -0400

    Auto-commit: Mon Apr 19 2021 14:24:44 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_info.sql
+++ superQuery/brenneckar@collegetrack.org/hs_student_profile_dash/student_info.sql
@@ -1,51 +1,53 @@
 WITH gather_contact_data AS (
   SELECT
-    Contact_Id,
-    full_name_c,
-    contact_url,
-    high_school_graduating_class_c,
+    C.Contact_Id,
+    C.full_name_c,
+    C.contact_url,
+    C.high_school_graduating_class_c,
     --   SPLIT(composite_readiness_most_recent_c, '.')[OFFSET(1)] as composite_readiness_most_recent_c,
-    composite_readiness_most_recent_c,
-    site_short,
-    College_Track_Status_Name,
-    email,
-    phone,
-    primary_contact_language_c,
-    primary_contact_c,
-    Gender_c,
-    Ethnic_background_c,
-    first_generation_fy_20_c,
-    birthdate,
-    indicator_low_income_c,
-    annual_household_income_c,
-    Current_school_name,
-    dream_statement_filled_out_c,
-    of_high_school_terms_on_intervention_c,
-    indicator_high_risk_for_dismissal_c,
-    student_has_iep_c,
-    Current_HS_CT_Coach_c,
-    community_service_form_link_c,
-    summer_experience_form_link_c,
-    current_academic_semester_c,
-    summer_experiences_previous_summer_c,
+    C.composite_readiness_most_recent_c,
+    C.site_short,
+    C.College_Track_Status_Name,
+    C.email,
+    C.phone,
+    C.primary_contact_language_c,
+    PC.full_name_c AS primary_contact_name,
+    C.emergency_contact_c,
+    C.Gender_c,
+    C.Ethnic_background_c,
+    C.first_generation_fy_20_c,
+    C.birthdate,
+    C.indicator_low_income_c,
+    C.annual_household_income_c,
+    C.Current_school_name,
+    C.dream_statement_filled_out_c,
+    C.of_high_school_terms_on_intervention_c,
+    C.indicator_high_risk_for_dismissal_c,
+    C.student_has_iep_c,
+    C.Current_HS_CT_Coach_c,
+    C.community_service_form_link_c,
+    C.summer_experience_form_link_c,
+    C.current_academic_semester_c,
+    C.summer_experiences_previous_summer_c,
     CONCAT(
       "https://ctgraduates.lightning.force.com/",
-      current_academic_semester_c
+      C.current_academic_semester_c
     ) AS current_at_url,
-    co_vitality_scorecard_color_most_recent_c,
-    starting_semester_c,
-    student_s_start_academic_year_c,
-    total_bank_book_balance_contact_c,
-    college_applications_all_fit_types_c,
-    community_service_hours_c,
-    student_dashboard_preview
+    C.co_vitality_scorecard_color_most_recent_c,
+    C.starting_semester_c,
+    C.student_s_start_academic_year_c,
+    C.total_bank_book_balance_contact_c,
+    C.college_applications_all_fit_types_c,
+    C.community_service_hours_c,
+    C.student_dashboard_preview
 
     
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
+    `data-warehouse-289815.salesforce_clean.contact_template` C
+    LEFT JOIN `data-warehouse-289815.salesforce.contact` PC ON PC.Id = C.primary_contact_c
   WHERE
-    college_track_status_c IN ('18a', '11A', '12A', '13A')
-    AND years_since_hs_grad_c <= 0
+    C.college_track_status_c IN ('18a', '11A', '12A', '13A')
+    AND C.years_since_hs_grad_c <= 0
 ),
 count_college_aspirations AS (
   SELECT
