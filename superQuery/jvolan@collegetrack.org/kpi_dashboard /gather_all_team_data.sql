4629dd18d9fe2ff56b240f305ba716e659aecf75
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-19 15:18:07 -0700

    Auto-commit: Mon Apr 19 2021 15:18:03 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
@@ -0,0 +1,26 @@
+WITH join_prep AS (
+  SELECT
+    *
+  FROM
+    `data-studio-260217.kpi_dashboard.join_prep`
+),
+join_team_kpis AS (
+  SELECT
+    JP.*,
+    AA.*
+  EXCEPT(site_short),
+  SD.* EXCEPT(site_short),
+  -- IR updating        CC_HS.* EXCEPT(site_short),
+  CC_PS.* EXCEPT(site_short)
+  FROM
+    join_prep JP
+    LEFT JOIN `data-studio-260217.kpi_dashboard.academic_affairs` AA ON AA.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.site_directors` SD ON SD.site_short = JP.site_short
+    -- IR updating      LEFT JOIN `data-studio-260217.kpi_dashboard.cc_hs` CC_HS ON CC_HS.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.cc_ps` CC_PS ON CC_PS.site_short = JP.site_short
+
+)
+SELECT
+  *
+FROM
+  join_team_kpis
