dc027ee46eb5f096c343cd5a36f955d59bbb39ff
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-19 11:39:29 -0700

    Auto-commit: Mon Apr 19 2021 11:39:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -4,4 +4,4 @@ SELECT student_c, SUM(Attendance_Numerator_excluding_make_up_c) AS attendance_nu
     WHERE Department_c = "College Completion"
     AND Cancelled_c = FALSE
     AND global_academic_semester_c IN ('a3646000000dMXnAAM','a3646000000dMXoAAM') #Fall 2020-21, Spring 2020-21
-    GROUP BY student_c,Attendance_Numerator_excluding_make_up_c,Attendance_Denominator_c
+    GROUP BY student_c,Attendance_Numerator_excluding_make_up_c,Attendance_Denominator_c, Cancelled_c
