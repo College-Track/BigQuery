005dd2c7c6480d5425f6e4adb18c4d13624c50c3
Baker Renneckar <baker@bakerr.com>
2021-04-20 14:41:54 -0400

    Auto-commit: Tue Apr 20 2021 11:41:50 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/ps_student_profile_dash/Tab 1.sql
@@ -0,0 +1,19 @@
+SELECT
+  Contact_Id,
+  AT_Id,
+  AT_Term_GPA,
+  Most_Recent_GPA_Cumulative_c,
+  most_recent_gpa_semester_c,
+  AT_Cumulative_GPA,
+  GAS_Name,
+  start_date_c,
+  attendance_rate_c,
+  current_as_c,
+  attended_workshops_c,
+  enrolled_sessions_c
+FROM
+  `data-warehouse-289815.salesforce_clean.contact_at_template`
+WHERE
+  college_track_status_c IN ('15A', '16A')
+  AND indicator_completed_ct_hs_program_c = true
+  AND start_date_c <= CURRENT_DATE()
