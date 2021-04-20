3f8bd68d37ce8308e681f749623142551204ee1d
Baker Renneckar <baker@bakerr.com>
2021-04-20 16:10:21 -0400

    Auto-commit: Tue Apr 20 2021 13:10:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/ps_student_profile_dash/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/ps_student_profile_dash/Tab 1.sql
@@ -11,10 +11,12 @@ SELECT
   current_as_c,
   attended_workshops_c,
   enrolled_sessions_c,
-  Cumulative_Credits_Awarded_All_Terms_c/100 AS Cumulative_Credits_Awarded_All_Terms_c
+  Cumulative_Credits_Awarded_All_Terms_c/100 AS Cumulative_Credits_Awarded_All_Terms_c,
+  AT_Record_Type_Name
 FROM
   `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE
   college_track_status_c IN ('15A', '16A')
   AND indicator_completed_ct_hs_program_c = true
-  AND start_date_c <= CURRENT_DATE()
+  AND start_date_c <= CURRENT_DATE() 
+--   AND AT_Record_Type_Name = ""
