314e048bf7004cea36ff09893ebcf2533eace852
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-20 11:30:53 -0700

    Auto-commit: Tue Apr 20 2021 11:30:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -1,10 +1,3 @@
-CREATE OR REPLACE TABLE `data-studio-260217.kpi_dashboard.student_life` 
-OPTIONS
-    (
-    description= "Aggregating Student Life KPI metrics for the Data Studio KPI dashboard"
-    )
-AS
-
 WITH gather_contact_data AS(
     SELECT
         contact_id,
@@ -28,6 +21,10 @@ gather_mse_data AS ( #current AY
         type_c,
         semester_c,
         AY_name,
+        CASE
+            WHEN sl.id IS NOT NULL THEN 1
+            ELSE 0
+            END AS mse_completed,
         CASE 
             WHEN competitive_c = True THEN 1
             ELSE 0
@@ -37,8 +34,8 @@ gather_mse_data AS ( #current AY
             ELSE 0
             END AS mse_internship
             
-    FROM `data-warehouse-289815.salesforce.student_life_activity_c` AS sl
-        LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS c ON c.at_id = sl.semester_c
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template` AS c
+        LEFT JOIN `data-warehouse-289815.salesforce.student_life_activity_c` AS sl ON c.at_id = sl.semester_c
         
     WHERE sl.record_type_id = '01246000000ZNi8AAG' #Summer Experience
     AND AY_name = 'AY 2020-21'
@@ -86,7 +83,7 @@ aggregate_attendance_kpi AS (
 aggregate_dream_kpi AS (
     SELECT 
         site_short,
-        SUM(dream_declared) as total_dreams
+        SUM(dream_declared) as sl_dreams_declared
     FROM gather_contact_data
     GROUP BY site_short
 ),
@@ -94,8 +91,9 @@ aggregate_dream_kpi AS (
 aggregate_mse_kpis AS (
     SELECT 
         site_short,
-        SUM(mse_competitive) AS total_mse_competitive,
-        SUM(mse_internship) AS total_mse_internship
+        SUM(mse_completed) AS sl_mse_completed,
+        SUM(mse_competitive) AS sl_mse_competitive,
+        SUM(mse_internship) AS sl_mse_internship
     FROM gather_mse_data
     GROUP BY site_short
 )
@@ -109,7 +107,8 @@ SELECT
         LEFT JOIN aggregate_mse_kpis AS mse_kpi ON d.site_short=mse_kpi.site_short
     GROUP BY
         site_short,
-        total_mse_competitive,
-        total_mse_internship,
-        total_dreams,
+        sl_mse_completed,
+        sl_mse_competitive,
+        sl_mse_internship,
+        sl_dreams_declared,
         sl_above_80_attendance
