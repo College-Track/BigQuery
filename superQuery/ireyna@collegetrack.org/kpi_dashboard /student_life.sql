b778c2a8c55b3853ffb6d0fdf2b22cea6b825173
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-20 10:36:53 -0700

    Auto-commit: Tue Apr 20 2021 10:36:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -6,7 +6,7 @@ SELECT
         
         
     FROM `data-warehouse-289815.salesforce.student_life_activity_c` AS sl
-        LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS c ON sl.at_id = c.semester_c
+        LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS c ON c.at_id = sl.semester_c
         
     WHERE sl.record_type_id = '01246000000ZNi8AAG' #Summer Experience
     AND AY_name = 'AY 2020-21'
