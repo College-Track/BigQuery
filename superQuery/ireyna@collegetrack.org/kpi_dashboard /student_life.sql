aa49463b5d19c0c3fb65e238b45e47545b5186bb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-04-20 10:33:12 -0700

    Auto-commit: Tue Apr 20 2021 10:33:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -1,4 +1,4 @@
-SELECT 
+ SELECT 
         contact_id,
         type_c,
         semester_c,
@@ -9,6 +9,6 @@ SELECT
         LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS c ON sl.student_c = c.contact_id
         
     WHERE sl.record_type_id = '01246000000ZNi8AAG' #Summer Experience
-    AND AY_Name = 'AY 2020-21'
+    AND semester_c LIKE '%Summer 2020-21%'
     AND experience_meaningful_c = True
     AND status_c = 'Approved'
