2f182cee5239f8c7bc483f3ebe63ce0039f7e21e
Baker Renneckar <baker@bakerr.com>
2021-04-20 19:12:30 -0400

    Auto-commit: Tue Apr 20 2021 16:12:23 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/ps_student_profile_dash/student_info.sql
+++ superQuery/brenneckar@collegetrack.org/ps_student_profile_dash/student_info.sql
@@ -26,7 +26,6 @@ WITH gather_contact_data AS (
     C.current_cc_advisor_2_c,
     C.current_major_c,
     C.current_major_specific_c,
-    C.anticipated_date_of_graduation_4_year_c,
     C.credit_accumulation_pace_c,
     C.total_bb_earnings_as_of_hs_grad_contact_c,
     C.bb_disbursements_total_c,
@@ -35,10 +34,13 @@ WITH gather_contact_data AS (
     C.most_recent_reciprocal,
     C.PS_Internships_c,
     C.Credits_Accumulated_Most_Recent_c / 100 AS Credits_Accumulated_Most_Recent_c,
+    ADG.GAS_Name AS anticipated_date_of_graduation_4_year_c
+    
 
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template` C
     LEFT JOIN `data-warehouse-289815.salesforce.contact` PC ON PC.Id = C.primary_contact_c -- LEFT JOIN `data-warehouse-289815.salesforce.npe_4_relationship_c` R ON R.npe_4_contact_c = C.Contact_Id
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` ADG ON ADG.AT_Id = C.anticipated_date_of_graduation_4_year_c
     -- LEFT JOIN `data-warehouse-289815.salesforce.contact` EC ON EC.Id = R.npe_4_related_contact_c
   WHERE
     C.college_track_status_c IN ('15A')
