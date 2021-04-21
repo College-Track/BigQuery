514ffab8ecc74a29e88698a8f04bab53725507b5
Baker Renneckar <baker@bakerr.com>
2021-04-21 19:11:14 -0400

    Auto-commit: Wed Apr 21 2021 16:11:05 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 9.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 9.sql
@@ -1,5 +1,4 @@
-SELECT CT.site_short, COUNT(distinct(S.contact_id)) as completion_count
+SELECT my_site_is_run_effectively_examples_i_know_how_to_find_zoom_links_i_receive_site, count(*)
 FROM `data-studio-260217.surveys.fy21_hs_survey` S
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
-
-GROUP BY CT.site_short
+GROUP BY my_site_is_run_effectively_examples_i_know_how_to_find_zoom_links_i_receive_site
