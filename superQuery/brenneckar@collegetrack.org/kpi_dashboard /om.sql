c66a1c644820b77929c2896bce8dc4a4f61d6c11
Baker Renneckar <baker@bakerr.com>
2021-04-21 19:02:43 -0400

    Auto-commit: Wed Apr 21 2021 16:02:38 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /om.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /om.sql
@@ -7,7 +7,7 @@ GROUP BY CT.site_short
 )
 
 SELECT GD.site_short,
-SUM(GD.completion_count) AS completion_count,
+MAX(GD.completion_count) AS completion_count,
 SUM(SC.student_count) AS student_denominator
 from gather_data GD
 LEFT JOIN `data-studio-260217.surveys.fy21_hs_survey_completion` SC ON SC.site_short = GD.site_short
