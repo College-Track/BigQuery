65e925b7a6470a5127e638428bddd342e4b1b2f7
Baker Renneckar <baker@bakerr.com>
2021-04-21 19:00:30 -0400

    Auto-commit: Wed Apr 21 2021 16:00:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /om.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /om.sql
@@ -1,7 +1,9 @@
 WITH gather_data AS (
-SELECT CT.site_short, COUNT(S.contact_id) as completion_count
+SELECT CT.site_short, COUNT(S.contact_id) as completion_count,
+SUM(SC.student_count) AS student_denominator
 FROM `data-studio-260217.surveys.fy21_hs_survey` S
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
+LEFT JOIN `data-studio-260217.surveys.fy21_hs_survey_completion` SC ON SC.site_short = CT.site_short
 GROUP BY CT.site_short
 )
 
