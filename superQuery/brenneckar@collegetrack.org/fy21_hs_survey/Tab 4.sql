28df291c79e51c5995c0843cea4cfe84b03bd856
Baker Renneckar <baker@bakerr.com>
2021-04-23 11:38:47 -0700

    Auto-commit: Fri Apr 23 2021 11:38:44 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 4.sql
@@ -1,13 +1,8 @@
 CREATE
 OR REPLACE FUNCTION `data-studio-260217.surveys.determine_positive_answers`(
   answer STRING
-) RETURNS INT64 LANGUAGE js AS """
-    var positive_answers = ["Strongly Agree",'Agree',"Very Safe","Extremely helpful",'Very helpful',"Extremely Excited",'Quite Excited',"Almost Always", "Extremely Helpful",'Very Helpful']
-    if positive_answers.includes(answer){
-        return 1
-        }
-        
-    else{return 0}
+) RETURNS STRING LANGUAGE js AS """
+   return answer
 
 ;
     
