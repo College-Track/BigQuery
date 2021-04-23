d0f2f5f95e6cf323cba9103506c4c1a43ed11c47
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-23 09:55:20 -0700

    Auto-commit: Fri Apr 23 2021 09:55:16 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working boar/Tab 1.sql
+++ superQuery/jvolan@collegetrack.org/JV working boar/Tab 1.sql
@@ -1,16 +1,4 @@
-WITH get_AT_data AS
-(
-    SELECT
-    AT_Id,
-    student_c,
-
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE college_track_status_c = '15A'
-),
-
-get_tasks_missing_student AS
-(
-    SELECT
+ SELECT
     id AS task_id,
     what_id,
     owner_id,
@@ -23,11 +11,3 @@ get_tasks_missing_student AS
     WHERE who_id IS NULL
     AND (what_id LIKE '%(Semester)%'
     OR what_id LIKE '%(Quarter)%')
-)
-
-    SELECT
-    *,
-    get_AT_data.student_c
-    
-    FROM get_tasks_missing_student
-    LEFT JOIN get_AT_data ON get_AT_data.AT_Id = what_id
