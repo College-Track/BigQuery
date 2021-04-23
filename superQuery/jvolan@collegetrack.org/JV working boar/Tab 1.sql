db80d016f44471cd56fc6684f43581a1dfb96b58
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-23 09:57:44 -0700

    Auto-commit: Fri Apr 23 2021 09:57:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working boar/Tab 1.sql
+++ superQuery/jvolan@collegetrack.org/JV working boar/Tab 1.sql
@@ -1,11 +1,10 @@
- SELECT
+SELECT
     id AS task_id,
     what_id,
     owner_id,
     created_by_id,
     created_date,
     who_id,
-    
     
     FROM `data-warehouse-289815.salesforce.task`
     WHERE who_id IS NULL
