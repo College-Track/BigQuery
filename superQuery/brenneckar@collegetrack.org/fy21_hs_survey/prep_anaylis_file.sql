7812fa952b082fab3b2a73f3b13ac117bb0b3a90
Baker Renneckar <baker@bakerr.com>
2021-04-23 11:53:03 -0700

    Auto-commit: Fri Apr 23 2021 11:52:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/prep_anaylis_file.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/prep_anaylis_file.sql
@@ -4,19 +4,21 @@ WITH gather_data AS (
     CAT.site_short,
     CAT.Most_Recent_GPA_Cumulative_bucket,
     CAT.high_school_graduating_class_c,
-    academic_resources_e_g_textbooks_computers,
-    `data-studio-260217.surveys.determine_positive_answers`(academic_resources_e_g_textbooks_computers) AS  academic_resources_e_g_textbooks_computers_positive,
-    CASE
-      WHEN (
-        how_likely_are_you_to_recommend_college_track_to_a_student_who_wants_to_get = '10 - extremely likely'
-        OR how_likely_are_you_to_recommend_college_track_to_a_student_who_wants_to_get = '9'
-      ) THEN "Promoters"
-      WHEN (
-        how_likely_are_you_to_recommend_college_track_to_a_student_who_wants_to_get = '8'
-        OR how_likely_are_you_to_recommend_college_track_to_a_student_who_wants_to_get = '7'
-      ) THEN "Passives"
-      ELSE "Detractors"
-    END AS NPS_Score
+    -- Likert Awners,
+     `data-studio-260217.surveys.determine_positive_answers`(ct_provides_a_supportive_learning_environment_for_me) AS ct_provides_a_supportive_learning_environment_for_me,
+    `data-studio-260217.surveys.determine_positive_answers`(when_im_at_ct_i_feel_like_i_belong) AS when_im_at_ct_i_feel_like_i_belong,
+    `data-studio-260217.surveys.determine_positive_answers`(the_friends_ive_made_at_ct_support_me_in_working_towards_a_college_degree) AS the_friends_ive_made_at_ct_support_me_in_working_towards_a_college_degree,
+    `data-studio-260217.surveys.determine_positive_answers`(i_feel_connected_to_at_least_one_ct_staff_member) AS i_feel_connected_to_at_least_one_ct_staff_member,
+    `data-studio-260217.surveys.determine_positive_answers`(ct_provides_me_enough_opportunities_to_form_meaningful_relationships_with_staff) AS ct_provides_me_enough_opportunities_to_form_meaningful_relationships_with_staff,
+    `data-studio-260217.surveys.determine_positive_answers`(i_make_it_a_priority_to_attend_my_ct_sessions_workshops) AS i_make_it_a_priority_to_attend_my_ct_sessions_workshops,
+    `data-studio-260217.surveys.determine_positive_answers`(my_site_is_run_effectively_examples_i_know_how_to_find_zoom_links_i_receive_site) AS my_site_is_run_effectively_examples_i_know_how_to_find_zoom_links_i_receive_site,
+    
+    
+    
+    
+    
+    `data-studio-260217.surveys.determine_positive_answers`(academic_resources_e_g_textbooks_computers) AS  academic_resources_e_g_textbooks_computers,
+    
   FROM
     `data-studio-260217.surveys.fy21_hs_survey` HSS
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CAT ON CAT.Contact_Id = HSS.contact_id
@@ -26,13 +28,7 @@ WITH gather_data AS (
 )
 
 SELECT
-  GD.*,
-  CASE WHEN NPS_Score = 'Promoters' THEN 1
-  ELSE 0
-  END AS promoters,
-  CASE WHEN NPS_Score = 'Detractors' THEN 1
-  ELSE 0
-  END AS detractors,
+  GD.*
 
 FROM
   gather_data GD
