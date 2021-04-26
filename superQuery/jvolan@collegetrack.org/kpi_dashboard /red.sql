ac43a946ffdba69c61a181192a1a5110c992ae93
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-26 15:11:34 -0700

    Auto-commit: Mon Apr 26 2021 15:11:30 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /red.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /red.sql
@@ -18,7 +18,7 @@ WITH gather_data AS
     AND college_track_status_c IN ('11A')
 )
     SELECT
-    max(gather_data.gpa_3_25__test_ready)
+    SUM(gather_data.gpa_3_25__test_ready) AS red_gpa_3_25_test_ready
     
     FROM gather_data
     GROUP BY site_short
