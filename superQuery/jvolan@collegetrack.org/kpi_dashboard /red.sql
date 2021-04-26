fbdeefa5a0c0c5d474cc58dac95a93f5ce9c05f9
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-26 15:10:52 -0700

    Auto-commit: Mon Apr 26 2021 15:10:47 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /red.sql
@@ -0,0 +1,24 @@
+WITH gather_data AS
+(
+  SELECT
+    Contact_Id,
+    site_short,
+    CASE
+      WHEN (Prev_AT_Cum_GPA >= 3.25
+      AND composite_readiness_most_recent_c = '1. Ready'
+      AND college_track_status_c ='11A') THEN 1
+      ELSE 0
+    END AS gpa_3_25__test_ready,
+    
+    
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_at_template`
+  WHERE
+    current_as_c = true
+    AND college_track_status_c IN ('11A')
+)
+    SELECT
+    max(gather_data.gpa_3_25__test_ready)
+    
+    FROM gather_data
+    GROUP BY site_short
