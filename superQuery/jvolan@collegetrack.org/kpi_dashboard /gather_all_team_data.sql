a0cfc0da9556e8c73438feac5cd5d276993d03bf
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-04-26 15:15:22 -0700

    Auto-commit: Mon Apr 26 2021 15:15:17 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
@@ -7,17 +7,20 @@ WITH join_prep AS (
 join_team_kpis AS (
   SELECT
     JP.*,
-    AA.*
-  EXCEPT(site_short),
+    AA.* EXCEPT(site_short),
   SD.* EXCEPT(site_short),
-  -- IR updating        CC_HS.* EXCEPT(site_short),
-  CC_PS.* EXCEPT(site_short)
+  CC_HS.* EXCEPT(site_short),
+  CC_PS.* EXCEPT(site_short),
+  OM.* EXCEPT(site_short)
   FROM
     join_prep JP
     LEFT JOIN `data-studio-260217.kpi_dashboard.academic_affairs` AA ON AA.site_short = JP.site_short
     LEFT JOIN `data-studio-260217.kpi_dashboard.site_directors` SD ON SD.site_short = JP.site_short
-    -- IR updating      LEFT JOIN `data-studio-260217.kpi_dashboard.cc_hs` CC_HS ON CC_HS.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.cc_hs` CC_HS ON CC_HS.site_short = JP.site_short
     LEFT JOIN `data-studio-260217.kpi_dashboard.cc_ps` CC_PS ON CC_PS.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.student_life` SL ON SL.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.om` OM ON OM.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.red`RED ON RED.site_short = JP.site_short
 
 )
 SELECT
