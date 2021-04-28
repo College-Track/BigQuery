caffad5e2895dd4c7b6809b18cb9ab32e32b0e97
Baker Renneckar <baker@bakerr.com>
2021-04-28 15:41:19 -0700

    Auto-commit: Wed Apr 28 2021 15:41:13 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /om.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /om.sql
@@ -11,22 +11,16 @@ WITH gather_survey_data AS (
     `data-studio-260217.surveys.fy21_hs_survey` S
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
  
-),
-
-survey_completion AS (
-SELECT site_short,
-SUM(student_count) AS student_count
-FROM `data-studio-260217.surveys.fy21_hs_survey_completion`
-GROUP BY site_short
 )
+
 
 SELECT
   GSD.site_short,
-  COUNT(GSD.contact_Id) AS om_hs_completion_count,
-  MAX(SC.student_count) AS om_hs_survey_denominator,
+--   COUNT(GSD.contact_Id) AS om_hs_completion_count,
+--   MAX(SC.student_count) AS om_hs_survey_denominator,
   SUM(GSD.agree_site_is_run_effectively) AS OM_agree_site_is_run_effectively
 from
   gather_survey_data GSD
-  LEFT JOIN survey_completion SC ON SC.site_short = GSD.site_short
+--   LEFT JOIN survey_completion SC ON SC.site_short = GSD.site_short
 GROUP BY
   GSD.site_short
