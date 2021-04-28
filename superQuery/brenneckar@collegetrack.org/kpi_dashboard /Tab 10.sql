15e630f01b1ae88e968d58ccbb5f9099e545c72f
Baker Renneckar <baker@bakerr.com>
2021-04-28 15:30:42 -0700

    Auto-commit: Wed Apr 28 2021 15:30:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 10.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 10.sql
@@ -1,2 +1,3 @@
-SELECT *
+SELECT site_short, SUM(student_count) AS student_count, SUM)(completed_survey_count) AS completed_survey_count
 FROM `data-studio-260217.surveys.fy21_hs_survey_completion`
+GROUP BY site_short
