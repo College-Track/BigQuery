b1dbfd8c0356a2a91bfd27bc362b2ac2bb8df507
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-03 11:54:47 -0700

    Auto-commit: Mon May 03 2021 11:54:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Avg .sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Avg .sql
@@ -1,5 +1,12 @@
-SELECT SUM(accepted) as number_acceptances
+WITH acceptance_by_student AS
+(SELECT SUM(accepted) as number_acceptances, contact_id, site_short
 FROM `data-studio-260217.college_applications.college_application_filtered_table`
-where site_short = 'Watts'
+-- site_short = 'Watts'
 GROUP BY
+contact_id,
 site_short
+)
+
+SELECT number_acceptances
+FROM acceptance_by_student
+GROUP BY site_short,number_acceptances
