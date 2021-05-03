9912d0cc17acbed1ac732c38d8164fcc7db3775f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-03 11:51:49 -0700

    Auto-commit: Mon May 03 2021 11:51:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Avg .sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Avg .sql
@@ -1,3 +1,5 @@
-SELECT accepted
+SELECT SUM(accepted) as number_acceptances
 FROM `data-studio-260217.college_applications.college_application_filtered_table`
-where site_short = 'Aurora'
+where site_short = 'National'
+GROUP BY
+contact_id
