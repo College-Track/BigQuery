5b9f996dea27b6a5063fa0c74cc89dd675dac730
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-03 12:03:54 -0700

    Auto-commit: Mon May 03 2021 12:03:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Avg .sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Avg .sql
@@ -1,6 +1,6 @@
-SELECT SUM(accepted) as number_acceptances, contact_id, site_short
+SELECT SUM(accepted) as number_acceptances, site_short
 FROM `data-studio-260217.college_applications.college_application_filtered_table`
 -- site_short = 'Watts'
 GROUP BY
-contact_id,
+
 site_short
