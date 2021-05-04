895727f44053975d308f778c54f529cd7c3c0842
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-04 16:07:39 -0700

    Auto-commit: Tue May 04 2021 16:07:34 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -9,6 +9,6 @@ SELECT
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE start_date_c < CURRENT_DATE()
-    AND academic_year_c = 'AY 2020-21'
+    AND AT_Name = 'AY 2020-21'
     AND college_track_status_c = '15A'
     GROUP BY contact_id
