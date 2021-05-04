4af1353fcf37e90c911490b280ef851bfaf71e64
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-04 13:47:25 -0700

    Auto-commit: Tue May 04 2021 13:47:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -7,7 +7,11 @@ version_c,
 status_c,
 test_date_c, 
 id AS test_record_id, #test id
-student_site_c
+student_site_c,
+CASE 
+        WHEN id IS NOT NULL THEN 1
+        ELSE 0
+    END AS covi_assessment_ay
 
 FROM `data-warehouse-289815.salesforce_clean.test_clean` COVI
 
@@ -27,16 +31,24 @@ WHERE record_type_id = '01246000000RNnSAAW'
 AND site_short != 'College Track Arlen'
 AND AY_Name = 'AY 2020-21'
 
-)
+),
 
---prep_kpi AS (
+prep_kpi AS (
 SELECT 
-    CASE 
-        WHEN test_record_id IS NOT NULL THEN 1
-        ELSE 0
-    END AS covi_assessment_ay,
+    covi_assessment_ay,
     student_site_c,
     contact_id
-    
-FROM gather_covi_data as C
-LEFT JOIN gather_at_data as A ON C.academic_semester_c=A.at_id
+ 
+FROM gather_at_data as A     
+LEFT JOIN gather_covi_data as C ON C.academic_semester_c=A.at_id
+group by contact_id, student_site_c,covi_assessment_ay
+)
+
+SELECT 
+SUM (covi_assessment_ay),
+student_site_c
+FROM prep_kpi
+GROUP BY student_site_c
+
+
+
