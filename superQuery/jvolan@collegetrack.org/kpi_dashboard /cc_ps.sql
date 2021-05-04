c6b857f9d1c8f2ad365c5b03e0947fadf8501271
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-04 16:08:55 -0700

    Auto-commit: Tue May 04 2021 16:08:50 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -1,14 +1,10 @@
 SELECT
     contact_id AS persist_contact_id,
     COUNT(AT_Id) AS at_count,
-    SUM(
-    CASE
-        WHEN indicator_persisted_at_c = 1 THEN 1
-        ELSE 0
-    END) AS persist_count
+    SUM(indicator_persisted_at_c) AS persist_count
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE start_date_c < CURRENT_DATE()
-    AND AT_Name = 'AY 2020-21'
+    AND AY_Name = 'AY 2020-21'
     AND college_track_status_c = '15A'
     GROUP BY contact_id
