17d73b148301adb79a4ffde09425ceeb11443d68
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-04 15:45:53 -0700

    Auto-commit: Tue May 04 2021 15:45:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -92,12 +92,12 @@ get_at_data AS
     Contact_Id AS at_contact_id,
     site_short AS at_site,
     CASE    
-        WHEN filing_status_c = "Filed for next year's financial aid" THEN 1
+        WHEN filing_status_c = 'FS_G' THEN 1
         ELSE 0
     END AS indicator_fafsa_complete,
     CASE
         WHEN 
-        loans_c IN ("Not using private loans, PLUS loans, or consumer debt to finance education", "Using private and/or PLUS loans but on track to owe less than $30k at graduation") THEN 1
+        loans_c IN ('LN_G','LN_Y') THEN 1
         ELSE 0
     END AS indicator_loans_less_30k_loans
     
