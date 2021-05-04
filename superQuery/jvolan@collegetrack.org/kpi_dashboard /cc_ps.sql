6e10b8934ea079abc8a0a266235dce3519098aba
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-04 15:27:57 -0700

    Auto-commit: Tue May 04 2021 15:27:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -5,16 +5,15 @@ SELECT
     CASE    
         WHEN 
         (filing_status_c = "Filed for next year's financial aid"
-        AND college_track_status_c = '15A'
-        AND current_as_c = true) THEN 1
+        AND college_track_status_c = '15A') THEN 1
         ELSE 0
     END AS indicator_fafsa_complete,
     CASE
         WHEN 
         (loans_c IN ("Not using private loans, PLUS loans, or consumer debt to finance education", "Using private and/or PLUS loans but on track to owe less than $30k at graduation")
-        AND college_track_status_c = '15A'
-        AND current_as_c = true) THEN 1
+        AND college_track_status_c = '15A') THEN 1
         ELSE 0
     END AS indicator_loans_less_30k_loans
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE current_as_c = true
