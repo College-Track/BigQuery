0b04cbadb2c66e07b3d659cced2152d704d6ac82
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-04 13:48:40 -0700

    Auto-commit: Tue May 04 2021 13:48:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -24,12 +24,13 @@ gather_at_data AS
 SELECT 
 at_id,
 contact_id,
-AY_Name,
+AY_Name
 
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` 
 WHERE record_type_id = '01246000000RNnSAAW' 
 AND site_short != 'College Track Arlen'
 AND AY_Name = 'AY 2020-21'
+AND College_Track_Status_Name = 'Current CT HS Student'
 
 ),
 
