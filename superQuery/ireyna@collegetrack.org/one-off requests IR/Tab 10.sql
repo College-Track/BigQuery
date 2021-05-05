6bd26e787013e6b6dfe7e33c0cc6539e0c2e67c2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-05 15:41:44 -0700

    Auto-commit: Wed May 05 2021 15:41:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 10.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 10.sql
@@ -1,18 +1,30 @@
+WITH
+
+join_term_data_with_covi AS (
+
 SELECT 
     full_name_c,
-    test_date_c,
     C.id AS test_record_id,
-    test_date_c AS first_covi_ay,
-    --PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
+    test_date_c,
+    contact_id,
+    AY_Name,
     student_site_c
-   
-    
-    
+
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` AS A
 LEFT JOIN `data-warehouse-289815.salesforce_clean.test_clean` C ON A.at_id = C.academic_semester_c
 
-WHERE C.test_date_c = (
-    select MIN(c2.test_date_c) FROM `data-warehouse-289815.salesforce_clean.test_clean` C2 where c.id = c2.id)
+)
+SELECT 
+    full_name_c,
+    j.test_record_id,
+    test_date_c AS first_covi_ay,
+    --PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
+    student_site_c
+    
+FROM join_term_data_with_covi AS j
+
+WHERE j.test_date_c = (
+    select MIN(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
     AND AY_Name IN ('AY 2020-21', 'AY 2019-20')
     AND contact_id = '0031M000033Eev9QAC'
     
@@ -20,5 +32,4 @@ GROUP BY
     student_site_c,
     full_name_c,
     test_date_c,
-    id,
-    test_date_c
+    test_record_id
