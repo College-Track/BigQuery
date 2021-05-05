357ff88d6a75c502acc6253f758bce59e5f996a7
Baker Renneckar <baker@bakerr.com>
2021-05-05 10:10:02 -0700

    Auto-commit: Wed May 05 2021 10:09:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 3.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 3.sql
@@ -1,3 +1,3 @@
 SELECT *
-FROM `data-warehouse-289815.salesforce_clean.user_clean`
-WHERE email = 'wmcneel@collegetrack.org'
+FROM `data-warehouse-289815.roles.role_table`
+WHERE Role_ID = '2F00E1M000001e0u3'
