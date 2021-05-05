0257c4b674d84da7f00c59eb83684151a1d30442
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-05 15:48:04 -0700

    Auto-commit: Wed May 05 2021 15:47:57 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Covi Date: MAX & MIN.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Covi Date: MAX & MIN.sql
@@ -12,7 +12,8 @@ SELECT
 
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` AS A
 LEFT JOIN `data-warehouse-289815.salesforce_clean.test_clean` C ON A.at_id = C.academic_semester_c
-
+WHERE c.record_type_id ='0121M000001cmuDQAQ'
+AND status_c = 'Completed'
 ),
 
 first_covi_test_ay AS (
@@ -28,7 +29,7 @@ FROM join_term_data_with_covi AS j
 WHERE j.test_date_c = (
     select MIN(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
     AND AY_Name IN ('AY 2020-21', 'AY 2019-20')
-    AND contact_id = '0031M000033Eev9QAC'
+    AND contact_id = '0031M00002xjHjbQAE'
     
 GROUP BY 
     student_site_c,
@@ -50,7 +51,7 @@ FROM join_term_data_with_covi AS j
 WHERE j.test_date_c = (
     select MAX(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
     AND AY_Name IN ('AY 2020-21', 'AY 2019-20')
-    AND contact_id = '0031M000033Eev9QAC'
+    AND contact_id = '0031M00002xjHjbQAE'
     
 GROUP BY 
     student_site_c,
