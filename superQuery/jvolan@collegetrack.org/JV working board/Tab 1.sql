43029840a01040662e2df736a5dd1ad61e1d0c74
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-04 17:11:14 -0700

    Auto-commit: Tue May 04 2021 17:11:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 1.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 1.sql
@@ -2,21 +2,22 @@ WITH get_persist_at_data AS
 (
   SELECT
     contact_id AS persist_contact_id,
-    COUNT(DISTINCT Contact_Id) AS cc_persist_denom,
+    MAX(CASE
+        WHEN
+        (AT_Enrollment_Status_c IN ('Full-time','Part-time')
+        AND college_track_status_c = '15A'
+        AND AY_Name = 'AY 2020-21'
+        AND term_c = 'Fall'
+        AND AT_school_type IN ('2-year', '4-year')) THEN 1
+        ELSE 0
+    END) AS include_in_reporting_group,
     COUNT(AT_Id) AS at_count,
     SUM(indicator_persisted_at_c) AS persist_count
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE 
-    (start_date_c < CURRENT_DATE()
+    WHERE start_date_c < CURRENT_DATE()
     AND AY_Name = 'AY 2020-21'
-    AND term_c <> 'Summer')
-    AND
-    (AT_Enrollment_Status_c IN ('Full-time','Part-time')
-        AND college_track_status_c = '15A'
-        AND AY_Name = 'AY 2020-21'
-        AND term_c = 'Fall'
-        AND AT_school_type IN ('2-year', '4-year'))
+    AND term_c <> 'Summer'
     GROUP BY contact_id
 ),
 
@@ -24,7 +25,7 @@ persist_calc AS
 (
     SELECT
     persist_contact_id,
-    MAX(cc_persist_denom) AS cc_persist_denom,
+    MAX(include_in_reporting_group) AS cc_persist_denom,
     MAX(
     CASE
         WHEN at_count = persist_count THEN 1
