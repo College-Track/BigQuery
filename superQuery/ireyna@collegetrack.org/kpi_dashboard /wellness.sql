2fb5b5e107ff38302f03049524a583cbc0f74549
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-06 13:27:33 -0700

    Auto-commit: Thu May 06 2021 13:27:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -72,27 +72,11 @@ WHERE AY_Name = 'AY 2019-20'
 SELECT 
     test_date_c AS first_covi_ay,
     raw_covi_score AS first_score,
-    PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
+    PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c, contact_id) AS first_raw_covi_score_median_ay, #median
     student_site_c
     
 FROM join_term_data_with_covi AS j
 WHERE j.test_date_c = (select MIN(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
-AND AY_Name = 'AY 2019-20'
-GROUP BY
-    student_site_c,
-    test_date_c,
-    raw_covi_score
-),
-
-gather_last_covi_ay AS (
-SELECT 
-    test_date_c AS last_covi_ay,
-    raw_covi_score AS last_score,
-    PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c, contact_id) AS last_raw_covi_score_median_ay,#median
-    student_site_c
-
-FROM join_term_data_with_covi AS j
-WHERE j.test_date_c = (select MAX(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
 AND AY_Name = 'AY 2019-20'
 GROUP BY
     student_site_c,
