341c9394184cfa08721b8f51bfcbccde27d7a87b
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-06 10:24:25 -0700

    Auto-commit: Thu May 06 2021 10:24:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -13,10 +13,8 @@ WITH gather_contact_data AS(
         high_school_graduating_class_c,
         Dream_Statement_filled_out_c,
         CASE
-            WHEN (Dream_Statement_filled_out_c = True AND high_school_graduating_class_c = '2024') THEN 1
-            WHEN (Dream_Statement_filled_out_c = False AND high_school_graduating_class_c = '2024') THEN 0 
-            WHEN high_school_graduating_class_c <> '2024' THEN NULL
-            ELSE NULL
+            WHEN Dream_Statement_filled_out_c = True  THEN 1
+            ELSE  0 
             END AS dream_declared
             
     FROM `data-warehouse-289815.salesforce_clean.contact_template` AS C
