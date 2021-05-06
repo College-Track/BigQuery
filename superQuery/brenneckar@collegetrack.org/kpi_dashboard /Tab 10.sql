523149bb8964c0acbad18caf48ba3c71aaa4e3c4
Baker Renneckar <baker@bakerr.com>
2021-05-06 12:01:01 -0700

    Auto-commit: Thu May 06 2021 12:00:57 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 10.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 10.sql
@@ -1,3 +1,3 @@
-SELECT site_short, SUM(student_count) AS student_count, SUM(completed_survey_count) AS completed_survey_count
-FROM `data-studio-260217.surveys.fy21_hs_survey_completion`
-GROUP BY site_short
+SELECT indicator_low_income_c
+FROM `data-warehouse-289815.salesforce_clean.contact_template`
+WHERE indicator_low_income_c IS NOT NULL
