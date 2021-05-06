f9d954b285698b244ef1a1cbbd76424746a903e2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-05 19:13:05 -0700

    Auto-commit: Wed May 05 2021 19:12:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Covi Date: MAX & MIN.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Covi Date: MAX & MIN.sql
@@ -1,3 +1,81 @@
+/*
+
+WITH
+
+join_term_data_with_covi AS (
+
+SELECT 
+    full_name_c,
+    C.id AS test_record_id,
+    test_date_c,
+    contact_id,
+    AY_Name,
+    student_site_c
+
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` AS A
+LEFT JOIN `data-warehouse-289815.salesforce_clean.test_clean` C ON A.at_id = C.academic_semester_c
+WHERE c.record_type_id ='0121M000001cmuDQAQ'
+AND status_c = 'Completed'
+),
+
+first_covi_test_ay AS (
+SELECT 
+    full_name_c,
+    j.test_record_id,
+    test_date_c AS first_covi_ay,
+    --PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
+    student_site_c
+    
+FROM join_term_data_with_covi AS j
+
+WHERE j.test_date_c = (
+    select MIN(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
+    AND AY_Name IN ('AY 2020-21', 'AY 2019-20')
+    AND contact_id = '0031M00002xjHjbQAE'
+    
+GROUP BY 
+    student_site_c,
+    full_name_c,
+    test_date_c,
+    test_record_id
+),
+
+last_covi_test_ay AS (
+SELECT 
+    full_name_c,
+    j.test_record_id,
+    test_date_c AS last_covi_ay,
+    --PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
+    student_site_c
+    
+FROM join_term_data_with_covi AS j
+
+WHERE j.test_date_c = (
+    select MAX(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
+    AND AY_Name IN ('AY 2020-21', 'AY 2019-20')
+    AND contact_id = '0031M00002xjHjbQAE'
+    
+GROUP BY 
+    student_site_c,
+    full_name_c,
+    test_date_c,
+    test_record_id
+)
+SELECT 
+    f.full_name_c,
+    f.student_site_c,
+    first_covi_ay,
+    last_covi_ay
+    
+FROM first_covi_test_ay AS f
+LEFT JOIN last_covi_test_ay AS l ON f.student_site_c = l.student_site_c
+group by f.full_name_c,
+    f.student_site_c,
+    first_covi_ay,
+    last_covi_ay
+*/
+
+    
 WITH 
 
 gather_at_data AS
@@ -75,10 +153,10 @@ gather_first_covi_ay AS (
 SELECT 
     test_date_c AS first_covi_ay,
     raw_covi_score AS first_score,
-    contact_id,
     PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
     student_site_c,
-    test_record_id
+    test_record_id,
+    contact_id
     
 FROM join_term_data_with_covi AS j
 WHERE j.test_date_c = (select MIN(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
@@ -89,10 +167,10 @@ WHERE j.test_date_c = (select MIN(j2.test_date_c) FROM join_term_data_with_covi 
 gather_last_covi_ay AS (
 SELECT 
     test_date_c AS last_covi_ay,
-    contact_id,
     raw_covi_score AS last_score,
     PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS last_raw_covi_score_median_ay,#median
-    student_site_c
+    student_site_c,
+    contact_id
 
 FROM join_term_data_with_covi AS j
 WHERE j.test_date_c = (select MAX(j2.test_date_c) FROM join_term_data_with_covi j2 where j.contact_id = j2.contact_id)
@@ -100,14 +178,14 @@ WHERE j.test_date_c = (select MAX(j2.test_date_c) FROM join_term_data_with_covi 
     AND contact_id = '0034600001TR5uoAAD'
 
     
-)
+),
 /*
 gather_casenotes_data AS (
 SELECT 
 )
 */
 
---prep_kpi AS (
+prep_kpi AS (
 SELECT 
     A.contact_id,
     --covi_assessment_ay,
@@ -125,8 +203,9 @@ SELECT
  
 FROM gather_at_data as A     
 --LEFT JOIN gather_covi_data as C ON C.academic_semester_c = A.at_id
-LEFT JOIN gather_first_covi_ay AS CF ON CF.contact_id = A.contact_id
-LEFT JOIN gather_last_covi_ay AS CL ON CL.contact_id = A.contact_id
+LEFT JOIN gather_first_covi_ay AS CF ON CF.student_site_c = A.site
+LEFT JOIN gather_last_covi_ay AS CL ON CL.student_site_c = A.site
+
 GROUP BY 
     contact_id, 
     student_site_c,
@@ -137,3 +216,26 @@ GROUP BY
     last_raw_covi_score_median_ay,
     last_covi_ay,
     first_covi_ay
+)
+
+SELECT 
+    --SUM (covi_assessment_ay) AS wellness_covi_assessment_ay,
+    wellness_covi_median_growth,
+    first_score,
+    last_score,
+    first_raw_covi_score_median_ay,
+    last_raw_covi_score_median_ay,
+    student_site_c,
+    last_covi_ay,
+    first_covi_ay
+FROM prep_kpi
+WHERE contact_id = '0034600001TR5uoAAD'
+GROUP BY student_site_c, 
+    wellness_covi_median_growth,
+    first_raw_covi_score_median_ay,
+    last_raw_covi_score_median_ay,
+    first_score,
+    last_score,last_covi_ay,
+    first_covi_ay
+    
+  
