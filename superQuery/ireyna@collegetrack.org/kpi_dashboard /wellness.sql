f4eada9dc08b964a509198ac502bc9e53b1b0b6d
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-06 14:04:26 -0700

    Auto-commit: Thu May 06 2021 14:04:23 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -84,6 +84,7 @@ GROUP BY
     raw_covi_score
 )
 --gather_first_test_median AS (
-SELECT MAX(first_raw_covi_score_median_ay)
+SELECT MAX(first_raw_covi_score_median_ay), student_site
 FROM gather_first_covi_ay
 WHERE student_site_c = 'College Track Oakland'
+GROUP BY student_site_c
