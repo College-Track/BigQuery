2544f8566b16ea71d0d38c30596c8db92d0e17c3
Baker Renneckar <baker@bakerr.com>
2021-05-06 12:01:13 -0700

    Auto-commit: Thu May 06 2021 12:01:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /programs_ops.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /programs_ops.sql
@@ -6,13 +6,19 @@ WITH gather_data AS (
       WHEN grade_c = '9th Grade'
       AND (indicator_first_generation_c = true) THEN 1
       ELSE 0
-    END AS incoming_cohort_first_gen
+    END AS incoming_cohort_first_gen,
+        CASE
+      WHEN grade_c = '9th Grade'
+      AND (indicator_low_income_c = 'Yes' ) THEN 1
+      ELSE 0
+    END AS incoming_cohort_low_income,
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template`
 )
 SELECT
   site_short,
-  SUM(incoming_cohort_first_gen) AS pro_ops_incoming_cohort_first_gen
+  SUM(incoming_cohort_first_gen) AS pro_ops_incoming_cohort_first_gen,
+  SUM(incoming_cohort_low_income) AS pro_ops_incoming_cohort_low_income
 FROM
   gather_data
 GROUP BY
