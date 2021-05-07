93dea6a95c2e3ccbb0e93cfda12daf25235fbe94
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-07 11:54:38 -0700

    Auto-commit: Fri May 07 2021 11:54:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -85,8 +85,7 @@ SELECT
     (SELECT raw_covi_score,
      FROM join_term_data_with_covi j2 
      WHERE j.contact_id = j2.contact_id
-     AND MIN(j2.test_date_c
-    ) AS first_test,
+     AND MIN(j2.test_date_c)) AS first_test,
      
     PERCENTILE_CONT(raw_covi_score, .5) OVER (PARTITION by student_site_c) AS first_raw_covi_score_median_ay, #median
     student_site_c,
