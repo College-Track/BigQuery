64514781dbbc734b9dd578984940bd226604a9a9
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-07 14:27:36 -0700

    Auto-commit: Fri May 07 2021 14:27:24 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -10,7 +10,7 @@ SELECT
 
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` 
 WHERE record_type_id = '01246000000RNnSAAW' 
-    AND site ='College Track Oakland' --!= 'College Track Arlen'
+    AND site != 'College Track Arlen'
     AND College_Track_Status_Name = 'Current CT HS Student'
 ),
 
