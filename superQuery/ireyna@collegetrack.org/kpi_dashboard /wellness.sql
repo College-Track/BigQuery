2b6d8d665ad3c44223133ae9acdcc0d40377bc9f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-07 11:27:21 -0700

    Auto-commit: Fri May 07 2021 11:27:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -52,6 +52,20 @@ GROUP BY
     id, #test id
     student_site_c,
     record_type_id
+),
+
+join_term_data_with_covi AS (
+SELECT 
+    test_date_c,
+    student_site_c,
+    raw_covi_score,
+    contact_id,
+    AY_NAME
+   
+FROM gather_at_data AS A
+LEFT JOIN gather_covi_data C ON A.at_id = C.academic_semester_c
+WHERE AY_Name = 'AY 2019-20'
+    AND status_c = 'Completed'
 )
 
 --gather_students_with_more_than_1_covi AS (
@@ -59,6 +73,6 @@ SELECT
     contact_id,
     SUM(covi_assessment_completed_ay) AS total_tests
     
-FROM gather_covi_data 
+FROM join_term_data_with_covi 
 WHERE covi_assessment_completed_ay > 1
 GROUP BY contact_id
