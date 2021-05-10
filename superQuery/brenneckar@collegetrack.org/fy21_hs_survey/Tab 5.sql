039aa6815471b2cf597e536eb43a3c7d19fe2d86
Baker Renneckar <baker@bakerr.com>
2021-05-10 10:13:09 -0700

    Auto-commit: Mon May 10 2021 10:13:02 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 5.sql
@@ -1,6 +1,6 @@
 WITH gather_data AS(
   SELECT
-  distinct
+  
     -- site_short,
     -- high_school_graduating_class_c,
     -- Most_Recent_GPA_Cumulative_bucket,
@@ -18,7 +18,7 @@ WITH gather_data AS(
       FROM
         `data-warehouse-289815.salesforce.contact_pipeline_history_c`
       WHERE
-        created_date >= '2021-02-17T23:59:59.000Z'
+        created_date >= '2021-02-18T00:00:00.000Z'
         AND Name = 'Started/Restarted CT HS Program'
     )
 --   GROUP BY
