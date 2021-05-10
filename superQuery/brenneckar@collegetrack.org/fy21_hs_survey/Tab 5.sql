8f024218a20ca711187e4370d7b531a1c72e6755
Baker Renneckar <baker@bakerr.com>
2021-05-10 10:19:28 -0700

    Auto-commit: Mon May 10 2021 10:19:25 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 5.sql
@@ -1,34 +1,20 @@
-WITH gather_data AS(
+gather_completed_survey_data AS (
   SELECT
-  
-    site_short,
+    -- site_short,
     -- high_school_graduating_class_c,
     -- Most_Recent_GPA_Cumulative_bucket,
     -- Ethnic_background_c,
     -- Gender_c,
-    COUNT(Contact_Id) as student_count
+    count(contact_Id) as completed_survey_count
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
-  WHERE
-    College_Track_Status_c IN ('11A', '12A', '18a')
-    AND grade_c != '8th Grade'
-    AND Contact_Id NOT IN (
-      SELECT
-        Contact_c
-      FROM
-        `data-warehouse-289815.salesforce.contact_pipeline_history_c`
-      WHERE
-        created_date >= '2021-02-17T21:59:59.000Z'
-        AND Name = 'Started/Restarted CT HS Program'
-    )
-  GROUP BY
-    site_short
-    -- high_school_graduating_class_c,
-    -- Most_Recent_GPA_Cumulative_bucket,
-    --     Ethnic_background_c,
-    -- Gender_c
+    `data-studio-260217.surveys.fy21_hs_survey_wide_prepped`
+--   GROUP BY
+--     site_short,
+--     high_school_graduating_class_c,
+--     Most_Recent_GPA_Cumulative_bucket,
+--     Ethnic_background_c,
+--     Gender_c
 )
 
 SELECT *
-FROM gather_data
-ORDER BY site_short
+FROM gather_completed_survey_data
