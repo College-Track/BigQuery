1a177b8385e0e44498d6ab15f54b8ff2880d1e6f
Baker Renneckar <baker@bakerr.com>
2021-05-10 10:17:29 -0700

    Auto-commit: Mon May 10 2021 10:17:23 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 5.sql
@@ -1,7 +1,7 @@
 WITH gather_data AS(
   SELECT
   
-    -- site_short,
+    site_short,
     -- high_school_graduating_class_c,
     -- Most_Recent_GPA_Cumulative_bucket,
     -- Ethnic_background_c,
@@ -21,8 +21,8 @@ WITH gather_data AS(
         created_date >= '2021-02-17T21:59:59.000Z'
         AND Name = 'Started/Restarted CT HS Program'
     )
---   GROUP BY
-    -- site_short
+  GROUP BY
+    site_short
     -- high_school_graduating_class_c,
     -- Most_Recent_GPA_Cumulative_bucket,
     --     Ethnic_background_c,
@@ -31,4 +31,4 @@ WITH gather_data AS(
 
 SELECT *
 FROM gather_data
--- ORDER BY site_short
+ORDER BY site_short
