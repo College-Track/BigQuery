ab0cdf221c149bcae21a92135d2d01b1bb4eaffd
Baker Renneckar <baker@bakerr.com>
2021-05-10 10:31:09 -0700

    Auto-commit: Mon May 10 2021 10:31:00 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/1_fy21_hs_survey_completion_group.sql
@@ -0,0 +1,32 @@
+ WITH gather_data AS(
+  SELECT
+    site_short,
+    high_school_graduating_class_c,
+    Most_Recent_GPA_Cumulative_bucket,
+    Ethnic_background_c,
+    Gender_c,
+    COUNT(Contact_Id) as student_count
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+  WHERE
+    College_Track_Status_c IN ('11A', '12A', '18a')
+    AND grade_c != '8th Grade'
+    AND Contact_Id NOT IN (
+      SELECT
+        Contact_c
+      FROM
+        `data-warehouse-289815.salesforce.contact_pipeline_history_c`
+      WHERE
+        created_date >= '2021-02-17T21:59:59.000Z'
+        AND Name = 'Started/Restarted CT HS Program'
+    )
+  GROUP BY
+    site_short,
+    high_school_graduating_class_c,
+    Most_Recent_GPA_Cumulative_bucket,
+    Ethnic_background_c,
+    Gender_c
+)
+
+SELECT SUM(student_count)
+FROM gather_data
