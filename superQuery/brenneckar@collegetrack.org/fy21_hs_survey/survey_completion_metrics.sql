ce9b486b3f5c318094f0bb67715d6dc3794f6a56
Baker Renneckar <baker@bakerr.com>
2021-05-10 10:40:17 -0700

    Auto-commit: Mon May 10 2021 10:40:08 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/survey_completion_metrics.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/survey_completion_metrics.sql
@@ -1,62 +1,70 @@
 WITH gather_data AS(
   SELECT
-    site_short,
-    high_school_graduating_class_c,
-    Most_Recent_GPA_Cumulative_bucket,
-    Ethnic_background_c,
-    Gender_c,
-    COUNT(Contact_Id) as student_count
+    HSCRP.site_short,
+    HSCRP.high_school_graduating_class_c,
+    HSCRP.Most_Recent_GPA_Cumulative_bucket,
+    HSCRP.Ethnic_background_c,
+    HSCRP.Gender_c,
+    HSCRP.contact_id,
+    CASE
+      WHEN HSWP.contact_id IS NOT NULL THEN 1
+      ELSE 0
+    END AS completed_survey_indicator -- COUNT(Contact_Id) as student_count
   FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
-  WHERE
-    College_Track_Status_c IN ('11A', '12A', '18a')
-    AND grade_c != '8th Grade'
-    AND Contact_Id NOT IN (
-      SELECT
-        Contact_c
-      FROM
-        `data-warehouse-289815.salesforce.contact_pipeline_history_c`
-      WHERE
-        created_date >= '2021-02-17T21:59:59.000Z'
-        AND Name = 'Started/Restarted CT HS Program'
+    `data-studio-260217.surveys.fy21_hs_survey_completion_reporting_group` HSCRP
+    LEFT JOIN `data-studio-260217.surveys.fy21_hs_survey_wide_prepped` HSWP ON HSWP.contact_id = HSCRP.contact_id --   GROUP BY
+    --     site_short,
+    --     high_school_graduating_class_c,
+    --     Most_Recent_GPA_Cumulative_bucket,
+    --     Ethnic_background_c,
+    --     Gender_c
+    -- ),
     )
-  GROUP BY
-    site_short,
-    high_school_graduating_class_c,
-    Most_Recent_GPA_Cumulative_bucket,
-    Ethnic_background_c,
-    Gender_c
-),
-gather_completed_survey_data AS (
   SELECT
     site_short,
     high_school_graduating_class_c,
     Most_Recent_GPA_Cumulative_bucket,
     Ethnic_background_c,
     Gender_c,
-    count(contact_Id) as completed_survey_count
+    COUNT(contact_id) AS student_count,
+    SUM(completed_survey_indicator) AS completed_survey_count
   FROM
-    `data-studio-260217.surveys.fy21_hs_survey_wide_prepped`
+    gather_data
   GROUP BY
     site_short,
     high_school_graduating_class_c,
     Most_Recent_GPA_Cumulative_bucket,
     Ethnic_background_c,
-    Gender_c
-),
-join_data AS (
-  SELECT
-    GD.*,
-    gather_completed_survey_data.completed_survey_count
-  FROM
-    gather_data GD
-    LEFT JOIN gather_completed_survey_data ON gather_completed_survey_data.site_short = GD.site_short
-    AND gather_completed_survey_data.high_school_graduating_class_c = GD.high_school_graduating_class_c
-    AND gather_completed_survey_data.Most_Recent_GPA_Cumulative_bucket = GD.Most_Recent_GPA_Cumulative_bucket
-    AND gather_completed_survey_data.Ethnic_background_c = GD.Ethnic_background_c
-    AND gather_completed_survey_data.Gender_c = GD.Gender_c
-)
-SELECT
-  *
-FROM
-  join_data
+    Gender_c -- gather_completed_survey_data AS (
+    --   SELECT
+    --     site_short,
+    --     high_school_graduating_class_c,
+    --     Most_Recent_GPA_Cumulative_bucket,
+    --     Ethnic_background_c,
+    --     Gender_c,
+    --     count(contact_Id) as completed_survey_count
+    --   FROM
+    --     `data-studio-260217.surveys.fy21_hs_survey_wide_prepped`
+    --   GROUP BY
+    --     site_short,
+    --     high_school_graduating_class_c,
+    --     Most_Recent_GPA_Cumulative_bucket,
+    --     Ethnic_background_c,
+    --     Gender_c
+    -- ),
+    -- join_data AS (
+    --   SELECT
+    --     GD.*,
+    --     gather_completed_survey_data.completed_survey_count
+    --   FROM
+    --     gather_data GD
+    --     LEFT JOIN gather_completed_survey_data ON gather_completed_survey_data.site_short = GD.site_short
+    --     AND gather_completed_survey_data.high_school_graduating_class_c = GD.high_school_graduating_class_c
+    --     AND gather_completed_survey_data.Most_Recent_GPA_Cumulative_bucket = GD.Most_Recent_GPA_Cumulative_bucket
+    --     AND gather_completed_survey_data.Ethnic_background_c = GD.Ethnic_background_c
+    --     AND gather_completed_survey_data.Gender_c = GD.Gender_c
+    -- )
+    -- SELECT
+    --   *
+    -- FROM
+    --   join_data
