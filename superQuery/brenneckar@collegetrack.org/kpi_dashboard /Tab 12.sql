8cfc3c4c0526c6950f290dd7685bf54766475089
Baker Renneckar <baker@bakerr.com>
2021-05-11 13:51:08 -0700

    Auto-commit: Tue May 11 2021 13:51:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
@@ -1,11 +1,11 @@
 WITH gather_test_data AS (
   SELECT
     contact_name_c,
+    site_short,
     AY_Name,
     MIN(
       belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c
     ) AS covi_raw_score
-
   FROM
     `data-warehouse-289815.salesforce_clean.test_clean` T
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = T.academic_semester_c
@@ -13,14 +13,17 @@ WITH gather_test_data AS (
     T.record_type_id = '0121M000001cmuDQAQ'
     AND AY_Name IN ('AY 2019-20', 'AY 2020-21')
   GROUP BY
+    site_short,
     contact_name_c,
     AY_Name
   ORDER BY
+    site_short,
     contact_name_c,
     AY_Name
 ),
 calc_growth AS (
   SELECT
+    site_short,
     contact_name_c,
     covi_raw_score - lag(covi_raw_score) over (
       partition by contact_name_c
@@ -29,14 +32,25 @@ calc_growth AS (
     ) AS covi_growth
   FROM
     gather_test_data
+),
+determine_covi_indicators AS (
+  SELECT
+    site_short,
+    contact_name_c,
+    CASE
+      WHEN covi_growth > 0 THEN 1
+      ELSE 0
+    END AS covi_student_grew
+  FROM
+    calc_growth
+  WHERE
+    covi_growth IS NOT NULL
 )
 SELECT
-  contact_name_c,
-  CASE WHEN covi_growth > 0 THEN 1
-  ELSE 0
-  END AS covi_student_grew
-  
+  site_short,
+  SUM(covi_student_grew) AS SD_covi_student_grew,
+  COUNT(contact_name_c) AS SD_covi_denominator
 FROM
-  calc_growth
-WHERE
-  covi_growth IS NOT NULL
+  determine_covi_indicators
+GROUP BY
+  site_short
