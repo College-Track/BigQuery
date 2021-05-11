5b248bfbdffd96f8b5114a442f2b8edb94af67f8
Baker Renneckar <baker@bakerr.com>
2021-05-11 13:19:12 -0700

    Auto-commit: Tue May 11 2021 13:19:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/test_clean.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/test_clean.sql
@@ -1,5 +1,26 @@
-SELECT  school_type, COUNT(*)
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
-LEFT JOIN `data-warehouse-289815.salesforce.account` A ON A.Id = Current_School_c
-WHERE college_track_status_c = '15A'
-GROUP BY school_type
+
+CREATE OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.test_clean` AS(
+
+
+
+SELECT
+  *
+EXCEPT
+  (
+    _fivetran_synced,
+    created_by_id,
+    last_modified_by_id,
+    last_modified_date,
+    last_referenced_date,
+    last_viewed_date,
+    academic_year_c,
+    admin_temp_1_c,
+    -- contact_name_c,
+    high_school_class_c,
+    hs_grade_level_c,
+    system_modstamp
+  )
+FROM
+  `data-warehouse-289815.salesforce.test_c`
+  WHERE is_deleted = false
+)
