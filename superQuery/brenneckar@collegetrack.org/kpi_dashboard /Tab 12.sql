77782d81e2f686627d7689e713d54de15b0f4d4f
Baker Renneckar <baker@bakerr.com>
2021-05-11 11:37:52 -0700

    Auto-commit: Tue May 11 2021 11:37:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
@@ -2,7 +2,7 @@ WITH gather_test_data AS (
 SELECT 
 test_date_c, Id, act_math_readiness_c, version_c
 FROM `data-warehouse-289815.salesforce_clean.test_clean`
-WHERE act_math_readiness_c IS NOT NULL
+WHERE act_math_readiness_c IS NOT NULL 
 )
 
 SELECT *
