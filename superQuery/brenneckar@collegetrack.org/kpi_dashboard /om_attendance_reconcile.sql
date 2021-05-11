2dfa7b344247eb30d72c7b1488716368abfdf1d0
Baker Renneckar <baker@bakerr.com>
2021-05-11 09:55:03 -0700

    Auto-commit: Tue May 11 2021 09:54:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /om_attendance_reconcile.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /om_attendance_reconcile.sql
@@ -1,4 +1,3 @@
-SELECT EXTRACT(MONTH FROM date_c)
+SELECT COUNT(Class_Attendance_Id)
 FROM `data-warehouse-289815.salesforce_clean.class_template`
-WHERE Outcome_c = 'Scheduled' 
-LIMIT 10
+WHERE Outcome_c = 'Scheduled' AND (EXTRACT(MONTH FROM date_c) <= EXTRACT(MONTH FROM CURRENT_DATE()))
