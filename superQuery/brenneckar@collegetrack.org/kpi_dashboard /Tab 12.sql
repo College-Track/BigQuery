31502e138783635f6b868cb8c963f0f8021a0a71
Baker Renneckar <baker@bakerr.com>
2021-05-11 13:22:37 -0700

    Auto-commit: Tue May 11 2021 13:22:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
@@ -1,14 +1,16 @@
 WITH gather_test_data AS (
   SELECT
     contact_name_c,
+    AY_Name,
     (
       belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c
     ) AS covi_raw_score
   FROM
-    `data-warehouse-289815.salesforce_clean.test_clean`
+    `data-warehouse-289815.salesforce_clean.test_clean` T
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = T.academic_semester_c
   WHERE
     co_vitality_indicator_c IS NOT NULL
-    AND record_type_id = '0121M000001cmuDQAQ'
+    AND T.record_type_id = '0121M000001cmuDQAQ'
 )
 SELECT
   *
