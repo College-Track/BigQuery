974dc8ad6e8b58d0a540c63db11e6a6fe8b69034
Baker Renneckar <baker@bakerr.com>
2021-05-11 13:12:31 -0700

    Auto-commit: Tue May 11 2021 13:12:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
@@ -1,8 +1,8 @@
 WITH gather_test_data AS (
 SELECT 
-test_date_c, Id, act_math_readiness_c, version_c
+test_date_c, Id, act_math_readiness_c, version_c, co_vitality_indicator_c, record_type_id
 FROM `data-warehouse-289815.salesforce_clean.test_clean`
-
+WHERE co_vitality_indicator_c IS NOT NULL
 )
 
 SELECT *
