d280b3ac79f10034a9398dcaa9b57b2f7f665208
Baker Renneckar <baker@bakerr.com>
2021-05-11 13:19:37 -0700

    Auto-commit: Tue May 11 2021 13:19:33 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 12.sql
@@ -1,5 +1,6 @@
 WITH gather_test_data AS (
   SELECT
+    contact_name_c,
     (
       belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c
     ) AS covi_raw_score
