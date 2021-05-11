b9ccdf27786cf93d39b21e155d82ec20158fc4ee
Baker Renneckar <baker@bakerr.com>
2021-05-11 09:59:59 -0700

    Auto-commit: Tue May 11 2021 09:59:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /om_attendance_reconcile.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /om_attendance_reconcile.sql
@@ -1,6 +1,6 @@
 SELECT
   site_short,
-  Class_Attendance_Id
+  Class_c
 FROM
   `data-warehouse-289815.salesforce_clean.class_template` CT
   LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
