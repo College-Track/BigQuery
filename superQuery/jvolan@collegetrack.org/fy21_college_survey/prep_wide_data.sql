3ef59a74f1b8f34a7952dcaf91cea5cb8abd5907
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-11 17:05:19 -0700

    Auto-commit: Tue May 11 2021 17:05:12 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/fy21_college_survey/prep_wide_data.sql
+++ superQuery/jvolan@collegetrack.org/fy21_college_survey/prep_wide_data.sql
@@ -1,26 +1,26 @@
- SELECT
+SELECT
     CASE
         WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin,"Staff turnover / I don't know the staff anymore") THEN 1
         ELSE 0
     END AS staff_turnover_count,
     CASE
-        WHEN "Which of the factors are most responsible for you not feeling as connected to College Track?(select all that apply)" IN ("I'm too busy / too much time required to maintain that connection") THEN 1
+        WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin,"I'm too busy / too much time required to maintain that connection") THEN 1
         ELSE 0
     END AS too_busy_count,
     CASE
-        WHEN "Which of the factors are most responsible for you not feeling as connected to College Track?(select all that apply)" IN ("I rarely see or hear from anyone at College Track besides my CT college advisor") THEN 1
+        WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin, "I rarely see or hear from anyone at College Track besides my CT college advisor") THEN 1
         ELSE 0
     END AS rarely_hear_count,
     CASE
-        WHEN "Which of the factors are most responsible for you not feeling as connected to College Track?(select all that apply)" IN ("I'm in other programs / use school resources that also provide support services") THEN 1
+        WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin, "I'm in other programs / use school resources that also provide support services") THEN 1
         ELSE 0
     END AS other_program_count,
     CASE
-        WHEN "Which of the factors are most responsible for you not feeling as connected to College Track?(select all that apply)" IN ("I'm thriving at college / just don't feel I need much support") THEN 1
+        WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin, "I'm thriving at college / just don't feel I need much support") THEN 1
         ELSE 0
     END AS thriving_count,
      CASE
-        WHEN "Which of the factors are most responsible for you not feeling as connected to College Track?(select all that apply)" IN ("Other (please specify in text box below)") THEN 1
+        WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin, "Other (please specify in text box below)") THEN 1
         ELSE 0
     END AS other_count,
      *,
