52fa5aa21f3b9743d49d0250adb50a23b055b2d7
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-12 07:46:49 -0700

    Auto-commit: Wed May 12 2021 07:46:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/fy21_college_survey/prep_wide_data.sql
+++ superQuery/jvolan@collegetrack.org/fy21_college_survey/prep_wide_data.sql
@@ -27,10 +27,43 @@ AS
         WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin, "I'm thriving at college / just don't feel I need much support") THEN 1
         ELSE 0
     END AS thriving_count,
-     CASE
+    CASE
         WHEN REGEXP_CONTAINS(which_of_the_factors_are_most_responsible_for_you_not_feelin, "Other (please specify in text box below)") THEN 1
         ELSE 0
     END AS other_count,
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Career advice & exploration") THEN 1
+        ELSE 0
+    END AS career_advice_count,
+    
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Graduate school information") THEN 1
+        ELSE 0
+    END AS grad_school_info_count,
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Handling personal finances") THEN 1
+        ELSE 0
+    END AS personal_finances_count,
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Job, internship, & volunteer opprotunities") THEN 1
+        ELSE 0
+    END AS career_opps_count,
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Networking & building connections") THEN 1
+        ELSE 0
+    END AS networking_count,
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Resume, application, & interview advice") THEN 1
+        ELSE 0
+    END AS resume_count,
+    CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Starting a business") THEN 1
+        ELSE 0
+    END AS starting_biz_count,
+     CASE
+        WHEN REGEXP_CONTAINS(what_types_of_support_resources_information_would_you_be_int,"Work life balance") THEN 1
+        ELSE 0
+    END AS work_life_count,
      
 
     FROM `data-studio-260217.surveys.fy21_ps_survey`
