2ed90fb3c2545a578a0abe8d0642f7a49f0db9a4
Baker Renneckar <baker@bakerr.com>
2021-05-12 13:49:57 -0700

    Auto-commit: Wed May 12 2021 13:49:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/users_clean/Delete tmp_pt_user.sql
+++ superQuery/brenneckar@collegetrack.org/users_clean/Delete tmp_pt_user.sql
@@ -1,3 +1,3 @@
-DELETE FROM
-    `data-warehouse-289815.salesforce_clean.tmp_pt_user`
-WHERE true -- where+false makes sure this command will not execute by mistake
+SELECT *
+FROM `data-warehouse-289815.salesforce_clean.user_clean`
+WHERE email LIKE '%dsullivan%'
