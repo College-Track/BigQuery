cb0b0d8702e00cc62f36260c613ded6a9f81c99e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-12 00:32:11 -0700

    Auto-commit: Wed May 12 2021 00:32:06 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
@@ -0,0 +1,29 @@
+WITH gather_data AS (
+  SELECT
+    KPI_Selection.*,
+    CASE
+      WHEN KPI_Target.select_role IS NOT NULL THEN true
+      ELSE false
+    END AS target_submitted,
+    CASE
+      WHEN enter_the_target_numeric_ IS NOT NULL THEN enter_the_target_numeric_
+      WHEN enter_the_target_percent_ iS NOT NULL THEN enter_the_target_percent_
+    --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_
+      ELSE NULL
+    END AS target,
+    CASE
+      WHEN site_kpi = 0 THEN NULL
+      ELSE site_kpi
+    END AS site
+  FROM
+    `data-studio-260217.performance_mgt.role_kpi_selection` KPI_Selection
+    LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target ON KPI_Target.team_kpi = REPLACE(KPI_Selection.function, ' ', '_')
+    AND KPI_Target.select_role = KPI_Selection.role
+    AND KPI_Target.select_kpi = KPI_Selection.KPI
+)
+SELECT
+  *
+FROM
+  gather_data
+-- WHERE
+--   target_submitted = true
