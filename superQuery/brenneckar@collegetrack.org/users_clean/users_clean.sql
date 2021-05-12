fe8d44c5453163a37bb0c0e2616c979b8dfe14be
Baker Renneckar <baker@bakerr.com>
2021-05-12 13:53:25 -0700

    Auto-commit: Wed May 12 2021 13:53:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/users_clean/users_clean.sql
+++ superQuery/brenneckar@collegetrack.org/users_clean/users_clean.sql
@@ -3,3 +3,12 @@
     group_id
   FROM
     `data-warehouse-289815.salesforce.group_member`
+  WHERE
+    group_id IN (
+      SELECT
+        Id
+      FROM
+        `data-warehouse-289815.salesforce.group`
+      WHERE
+        name LIKE "%Regional CC Advising%"
+    )
