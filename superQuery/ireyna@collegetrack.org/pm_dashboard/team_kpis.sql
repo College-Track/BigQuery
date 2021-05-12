cc0e024d1b661b91e84287a755963c99e49bdee3
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-12 09:11:17 -0700

    Auto-commit: Wed May 12 2021 09:11:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
@@ -1,4 +1,13 @@
-WITH gather_data AS (
+
+CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_team_kpis`
+OPTIONS
+    (
+    description="KPIs submitted by Team for FY22. References List of KPIs by role and Targets from FormAssembly Team KPI form"
+    )
+AS
+
+
+WITH gather_kpi_submissions AS (
   SELECT
     KPI_Selection.*,
     CASE
@@ -14,7 +23,27 @@ WITH gather_data AS (
     CASE
       WHEN site_kpi = 0 THEN NULL
       ELSE site_kpi
-    END AS site
+    END AS site,
+    CASE
+      WHEN function IN ('Talent Acquisition','Talent Development','Employee Experience')
+      THEN 1
+      ELSE 0
+    END AS hr_people,
+    CASE 
+      WHEN function IN ('Finance','Strategic Initiatives','Org Performance','IT','Marketing','Program Development','Operations')
+      THEN 1
+      ELSE 0
+    END AS national,
+    CASE
+      WHEN function IN ('Partnerships','Fund Raising')
+      THEN 1
+      ELSE 0
+    END AS development,
+    CASE
+      WHEN function IN ('Mature Site Staff','Non-Mature Site Staff')
+      THEN 1
+      ELSE 0
+    END AS program,
   FROM
     `data-studio-260217.performance_mgt.role_kpi_selection` KPI_Selection
     LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target ON KPI_Target.team_kpi = REPLACE(KPI_Selection.function, ' ', '_')
@@ -24,6 +53,4 @@ WITH gather_data AS (
 SELECT
   *
 FROM
-  gather_data
--- WHERE
---   target_submitted = true
+  gather_kpi_submissions
