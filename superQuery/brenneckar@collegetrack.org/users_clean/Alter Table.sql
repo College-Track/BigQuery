c52e0fb5ff09fb5ceeaeced1bfa5f377c75fbf88
Baker Renneckar <baker@bakerr.com>
2021-05-12 13:45:58 -0700

    Auto-commit: Wed May 12 2021 13:45:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/users_clean/Alter Table.sql
+++ superQuery/brenneckar@collegetrack.org/users_clean/Alter Table.sql
@@ -1,4 +1,8 @@
-INSERT INTO `data-warehouse-289815.roles.group_role_id` (group_name, group_id, user_role_id)
-VALUES ('Regional CC Advising - Boyle Heights', '00G1M000004sJP1UAM', '00E46000000YcNmEAK'),
- ('Regional CC Advising - Denver', '00G1M000004sLcgUAE', '00E46000000YcO1EAK'),
- ('Regional CC Advising - Aurora', '00G1M000004s0IhUAI', '00E46000000YcO1EAK')
+-- INSERT INTO `data-warehouse-289815.roles.group_role_id` (group_name, group_id, user_role_id)
+-- VALUES ('Regional CC Advising - Boyle Heights', '00G1M000004sJP1UAM', '00E46000000YcNmEAK'),
+--  ('Regional CC Advising - Denver', '00G1M000004sLcgUAE', '00E46000000YcO1EAK'),
+--  ('Regional CC Advising - Aurora', '00G1M000004s0IhUAI', '00E46000000YcO1EAK')
+
+
+DELETE FROM `data-warehouse-289815.roles.group_role_id`
+WHERE group_name = 'Shared CC Advising - Denver/Aurora'
