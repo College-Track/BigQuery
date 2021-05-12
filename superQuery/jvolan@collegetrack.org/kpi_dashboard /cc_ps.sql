fc6baf36c6fb928cdd4e37c554234d57db4e73d2
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-12 15:49:38 -0700

    Auto-commit: Wed May 12 2021 15:49:33 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /cc_ps.sql
@@ -13,15 +13,14 @@ SELECT
     END AS fy20_alumni_survey_meaningful_denom,
 --
     indicator_annual_loan_repayment_amount_current_loan_debt_125,
-    indicator_income_proxy
-    indicator_loans_still_owe_proxy_wayn,
-    indicator_loan_proxy_fd,
+    indicator_income_proxy,
     (indicator_annual_loan_repayment_amount_current_loan_debt_125 / indicator_income_proxy) AS annual_debt_income_ratio,
     CASE
         WHEN 
         (indicator_annual_loan_repayment_amount_current_loan_debt_125 / indicator_income_proxy) <=.08 THEN 1
         ELSE 0
-    END AS indicator_gainful_employment
+    END AS indicator_gainful_employment,
+    indicator_loans_still_owe_proxy_wayn,
+    indicator_loan_proxy_fd,
 
-    
     FROM `data-warehouse-289815.surveys.fy20_alumni_survey`
