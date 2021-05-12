5a3d57abc5baf875bd9d35ab4e1f419458d500d6
Baker Renneckar <baker@bakerr.com>
2021-05-12 13:44:49 -0700

    Auto-commit: Wed May 12 2021 13:44:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/users_clean/Alter Table.sql
+++ superQuery/brenneckar@collegetrack.org/users_clean/Alter Table.sql
@@ -1,2 +1,4 @@
-INSERT INTO `data-warehouse-289815.salesforce_clean.user_clean` (id, first_name, last_name, is_active, email, user_role_id)
-VALUES ('abc123', 'Serena', 'Wee', true, 'swee@collegetrack.org', '00E46000000YcNXEA0')
+INSERT INTO `data-warehouse-289815.roles.group_role_id` (group_name, group_id, user_role_id)
+VALUES ('Regional CC Advising - Boyle Heights', '00G1M000004sJP1UAM', '00E46000000YcNmEAK'),
+ ('Regional CC Advising - Denver', '00G1M000004sLcgUAE', '00E46000000YcO1EAK'),
+ ('Regional CC Advising - Aurora', '00G1M000004s0IhUAI', '00E46000000YcO1EAK')
