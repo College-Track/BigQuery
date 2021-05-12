6f522010a2fc9f9d1fb65ed4dac684cd8f079891
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-12 09:42:24 -0700

    Auto-commit: Wed May 12 2021 09:42:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
@@ -19,7 +19,7 @@ WITH gather_kpi_submissions AS (
       WHEN enter_the_target_percent_ iS NOT NULL THEN enter_the_target_percent_
     --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_
       ELSE NULL
-    END AS target,
+    END AS target_fy22,
     CASE
       WHEN site_kpi = 0 THEN NULL
       ELSE site_kpi
