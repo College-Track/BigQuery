632cdfa4030d3a15110d1202bc4b1781859a0b6e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-11 20:00:21 -0700

    Auto-commit: Tue May 11 2021 20:00:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
@@ -1,11 +1,11 @@
-/*
+
 CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.fy21_roles_to_kpi`
 OPTIONS
     (
     description="This table maps CT-wide Staff List to KPIs based on Role anf Function/Team"
     )
 AS
-*/
+
 WITH
 gather_staff_table AS (
 SELECT *
