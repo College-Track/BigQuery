c3f1b8e547ecaadfa333ae78a76e10de86cc6455
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-11 19:51:09 -0700

    Auto-commit: Tue May 11 2021 19:51:04 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
@@ -0,0 +1,23 @@
+/*
+CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.roles_to_kpi`
+OPTIONS
+    (
+    description="This table maps CT-wide Staff List to KPIs based on Role anf Function/Team"
+    )
+AS
+*/
+WITH
+gather_staff_table AS (
+SELECT *
+FROM `data-warehouse-289815.google_sheets.staff_table`
+),
+
+gather_kpis AS (
+SELECT *
+FROM `data-studio-260217.performance_mgt.role_kpi_selection`
+)
+
+SELECT *
+FROM gather_staff_table AS staff
+LEFT JOIN gather_kpis AS kpis ON staff.Job_Title_Description = kpis.Role
+Limit 10
