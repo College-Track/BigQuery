54e866a4d8797d01f701209a3bde4153d105e9c3
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-12 23:32:59 -0700

    Auto-commit: Wed May 12 2021 23:32:55 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
@@ -1,22 +1,2 @@
-
-WITH gather_staff_table AS (
-SELECT *
-FROM `data-warehouse-289815.google_sheets.staff_list`
-),
-
-gather_kpis AS (
 SELECT *
 FROM `data-studio-260217.performance_mgt.role_kpi_selection`
-)
-
-SELECT 
-    staff.* EXCEPT(position_id, team),
-    kpis.*,
-    CASE WHEN kpi IS NULL THEN 1
-        ELSE 0
-        END AS missing_kpis
-FROM gather_staff_table AS staff
-LEFT JOIN gather_kpis AS kpis 
-ON staff.Job_Title_Description = kpis.Role
-AND staff.Team = kpis.function
-
