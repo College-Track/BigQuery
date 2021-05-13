b123b5f942564acd6bfd10c52d00241d877673cc
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-12 21:58:11 -0700

    Auto-commit: Wed May 12 2021 21:58:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/team_kpis.sql
@@ -1,11 +1,4 @@
 
-CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_team_kpis`
-OPTIONS
-    (
-    description="KPIs submitted by Team for FY22. References List of KPIs by role and Targets from FormAssembly Team KPI form"
-    )
-AS
-
 
 WITH gather_kpi_submissions AS (
   SELECT
