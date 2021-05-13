ece64fd933533cfbb970c0f7a419cc20d07a85a9
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-12 23:31:27 -0700

    Auto-commit: Wed May 12 2021 23:31:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/roles_to_kpis.sql
@@ -1,4 +1,11 @@
 
+CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
+OPTIONS
+    (
+    description="This table maps CT-wide Staff List to finalized KPIs based on Role for FY22"
+    )
+AS
+
 WITH gather_staff_table AS (
 SELECT *
 FROM `data-warehouse-289815.google_sheets.staff_list`
@@ -16,5 +23,7 @@ SELECT
         ELSE 0
         END AS missing_kpis
 FROM gather_staff_table AS staff
-LEFT JOIN gather_kpis AS kpis ON staff.Job_Title_Description = kpis.Role
+LEFT JOIN gather_kpis AS kpis 
+ON staff.Job_Title_Description = kpis.Role
+AND staff.Team = kpis.function
 
