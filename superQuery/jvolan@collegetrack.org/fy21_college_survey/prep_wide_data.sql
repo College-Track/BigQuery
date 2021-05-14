5563497b29f25580a06b874d73647e0af7c31d45
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-14 09:40:05 -0700

    Auto-commit: Fri May 14 2021 09:39:57 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/fy21_college_survey/prep_wide_data.sql
+++ superQuery/jvolan@collegetrack.org/fy21_college_survey/prep_wide_data.sql
@@ -67,6 +67,6 @@ WITH data_with_filters AS
 )
     SELECT
     data_with_filters.*,
-    CR.wellness_score_color	
+    wellness_score_color	
     FROM data_with_filters
-    LEFT JOIN `data-studio-260217.college_rubric.filtered_college_rubric`CR ON CR.Contact_Id = wide_contact_id
+    LEFT JOIN `data-studio-260217.college_rubric.filtered_college_rubric` ON Contact_Id = wide_contact_id
