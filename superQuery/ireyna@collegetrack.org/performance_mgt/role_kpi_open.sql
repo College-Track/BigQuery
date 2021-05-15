49d582d0121c041c2f37a7f07b82572cf7fd39db
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-14 17:54:27 -0700

    Auto-commit: Fri May 14 2021 17:54:12 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/role_kpi_open.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/role_kpi_open.sql
@@ -18,19 +18,22 @@ SELECT
     kpi AS kpi_all
     
 FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
-)
+),
 
 
 --KPIs on someone's team, but not mapped to their specific role KPIs.
 --They can select the KPIs on their team that is not one of their KPIs based on their role (MUST be a KPI on their team)
 --Exception: CCAs- they will be able to enter a caseload size and cutomized target for their role during the Inidivudal KPI Selection phase
---team_kpis_not_assigned_to_role AS (
+team_kpis_not_assigned_to_role AS (
 SELECT
     b.first_name,
     b.last_name,
     kpi_all,
     role_all,
     function_all,
+    kpi,
+    role,
+    function,
     CASE 
         WHEN role <> role_all
         AND kpi = kpi_all
@@ -52,8 +55,14 @@ GROUP BY
     kpi_all,
     role_all,
     function_all
+)
 
-
+SELECT 
+    role,
+    kpi,
+    function,
+    kpi_all
+FROM team_kpis_not_assigned_to_role
 /*    
 WHERE kpi NOT IN 
     (SELECT kpi_all FROM gather_kpis_by_team AS c
