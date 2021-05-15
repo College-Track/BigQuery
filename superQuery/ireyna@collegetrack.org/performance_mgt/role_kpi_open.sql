05001a17590ad52b9703a70b8b40b721f7785159
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-14 17:24:30 -0700

    Auto-commit: Fri May 14 2021 17:24:25 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/role_kpi_open.sql
@@ -0,0 +1,35 @@
+/*
+CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.fy22_roles_kpis_open`
+OPTIONS
+    (
+    description="This table lists KPIs available for staff to select based on role during Individual KPI selection phase. If a KPI is on the staff member's team, they can select pre-determined KPIs on their team that was not mapped to their role. "
+    )
+AS
+*/
+
+WITH 
+
+gather_kpis_by_team AS (
+SELECT
+    function AS function_all,
+    role AS role_all,
+    kpi AS kpi_all
+    
+FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
+)
+
+
+--KPIs on someone's team, but not mapped to their specific role KPIs.
+--They can select the KPIs on their team that is not one of their KPIs based on their role (MUST be a KPI on their team)
+--Exception: CCAs- they will be able to enter a caseload size and cutomized target for their role during the Inidivudal KPI Selection phase
+--team_kpis_not_assigned_to_role AS (
+SELECT
+    a.*
+
+FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS a
+LEFT JOIN gather_kpis_by_team AS b
+    ON a.function = b.function_all
+    
+WHERE kpi NOT IN 
+    (SELECT kpi_all FROM gather_kpis_by_team AS c)
+    AND function = function_all
