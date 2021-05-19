29c0b1266523cea5c3c20a61ef4a44bd2e10b16e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 09:57:40 -0700

    Auto-commit: Wed May 19 2021 12:57:38 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 4.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 4.sql
@@ -9,7 +9,7 @@ AS
 
 WITH 
 
-gather_kpis_by_team AS (
+gather_all_kpis AS (
 SELECT
     first_name,
     last_name,
@@ -32,58 +32,37 @@ GROUP BY
 --KPIs on someone's team, but not mapped to their specific role KPIs.
 --They can select the KPIs on their team that is not one of their KPIs based on their role (MUST be a KPI on their team)
 --Exception: CCAs- they will be able to enter a caseload size and cutomized target for their role during the Inidivudal KPI Selection phase
-team_kpis_not_assigned_to_role AS (
+team_kpis AS (
 SELECT
-    kpi AS team_kpi_not_assigned_to_role,
-    role,
     function,
-      CASE 
-        WHEN function = function_all
-        AND kpi <> kpi_all
-        THEN "open_kpi"
-        ELSE kpi
-    END AS open_kpis
+    kpi AS team_kpi
     
-FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS a
-LEFT JOIN gather_kpis_by_team AS b
-    ON a.function = b.function_all
-   
-WHERE 
-    a.role <> b.role_all
-    AND a.kpi <> b.kpi_all
-    
-GROUP BY
-    kpi,
-    role,
-    function,
-    role_all,
-    function_all,
-    kpi_all
-  
-)
+FROM gather_all_kpis
 
+GROUP BY 
+    function,
+    kpi
+),
+
+role_kpis AS (
 SELECT 
     role,
-    team_kpi_not_assigned_to_role,
-    open_kpis,
-    function
-  
-FROM gather_kpis_by_team AS a 
-LEFT JOIN team_kpis_not_assigned_to_role AS b
-ON function_all = function
+    kpi AS role_kpi
 
-WHERE role <> role_all
-GROUP BY
+FROM gather_all_kpis
+
+GROUP BY 
     role,
-    team_kpi_not_assigned_to_role,
-    open_kpis,
-    function
-  
-
-/*
-(SELECT gather2.kpi_all 
-    FROM gather_kpis_by_team AS gather2 
-    WHERE a.role <> gather2.role_all
-    AND a.function = gather2.function_all
-    AND a.kpi <> gather2.kpi_all) AS open_kpi
-    */
+    kpi
+)
+SELECT 
+    team_kpis.function,
+    role_kpi,
+    CASE 
+        WHEN role_kpi <> team_kpi
+        THEN 'open kpi'
+    ELSE role_kpi
+    
+FROM team_kpis AS team_kpis
+LEFT JOIN role_kpis AS role_kpis
+    ON team_kpis.function = role_kpis.function
