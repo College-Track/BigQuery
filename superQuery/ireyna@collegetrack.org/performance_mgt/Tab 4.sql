964a53991e0f9247b5689f97788dfd3fb256a694
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 09:26:04 -0700

    Auto-commit: Wed May 19 2021 12:25:53 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 4.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 4.sql
@@ -1,11 +1,3 @@
-/*
-CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.fy22_roles_kpis_open`
-OPTIONS
-    (
-    description="This table lists KPIs available for staff to select based on role during Individual KPI selection phase. If a KPI is on the staff member's team, they can select pre-determined KPIs on their team that was not mapped to their role. "
-    )
-AS
-*/
 
 WITH 
 
@@ -26,20 +18,15 @@ GROUP BY
     role,
     kpi
     
-),
+)
 
 
 --KPIs on someone's team, but not mapped to their specific role KPIs.
 --They can select the KPIs on their team that is not one of their KPIs based on their role (MUST be a KPI on their team)
 --Exception: CCAs- they will be able to enter a caseload size and cutomized target for their role during the Inidivudal KPI Selection phase
-team_kpis_not_assigned_to_role AS (
+--team_kpis_not_assigned_to_role AS (
 SELECT
-    --b.first_name,
-    --b.last_name,
-    --kpi_all,
-    --role_all,
-    --function_all,
-    kpi AS open_kpi,
+    kpi AS team_kpi_not_assigned_to_role,
     role,
     function
     
@@ -48,38 +35,8 @@ LEFT JOIN gather_kpis_by_team AS b
     ON a.function = b.function_all
 WHERE 
     a.role <> b.role_all
-    AND a.function = b.function_all
-    --AND a.kpi <> b.kpi_all 
     
 GROUP BY
-    --b.first_name,
-    --b.last_name,
     kpi,
     role,
     function
-    --kpi_all,
-    --role_all,
-    --function_all
-)
-
-SELECT 
-    role,
-    open_kpi,
-    --kpi,
-    function,
-    --kpi_all
-FROM team_kpis_not_assigned_to_role
-GROUP BY
-    role,
-    open_kpi,
-    function
-    --kpi,
-    --kpi_all
-
-/*
-(SELECT gather2.kpi_all 
-    FROM gather_kpis_by_team AS gather2 
-    WHERE a.role <> gather2.role_all
-    AND a.function = gather2.function_all
-    AND a.kpi <> gather2.kpi_all) AS open_kpi
-    */
