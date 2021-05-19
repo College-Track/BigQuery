3d210376396894546a85d81ed1b19d1ce7f71b28
Baker Renneckar <baker@bakerr.com>
2021-05-19 10:49:59 -0700

    Auto-commit: Wed May 19 2021 10:49:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -1,4 +1,4 @@
 SELECT besides_my_ct_college_advisor_i_have_at_least_one_ct_staff_m, COUNT(*)
-FROM `data-studio-260217.surveys.fy21_ps_survey_analysis`
+FROM `data-studio-260217.surveys.fy21_ps_survey`
 WHERE site_short = "Sacramento"
 GROUP BY besides_my_ct_college_advisor_i_have_at_least_one_ct_staff_m
