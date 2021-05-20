6a7e681223e57c8552b123c6db65a80070aa6bb4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 19:53:51 -0700

    Auto-commit: Wed May 19 2021 22:53:48 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
@@ -15,7 +15,7 @@ SELECT
     (SELECT b.KPI 
                 FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS b
                 WHERE a.function = b.function AND a.role <> b.role
-                GROUP BY b.function,b.kpi) AS kpi
+                ) AS kpi
     
 FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS a
 
