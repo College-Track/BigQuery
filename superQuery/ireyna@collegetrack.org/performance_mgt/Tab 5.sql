1c65795ace2d60cee348e89c4052a3d56aebd258
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 20:49:49 -0700

    Auto-commit: Wed May 19 2021 23:49:43 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
@@ -1,3 +1,11 @@
+/*
+CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.fy22_roles_kpis_open`
+OPTIONS
+    (
+    description="This table lists KPIs available for staff to select based on role during Individual KPI selection phase. If a KPI is on the staff member's team, they can select pre-determined KPIs on their team that was not mapped to their role. "
+    )
+AS
+*/
 WITH 
 
 gather_all_kpis AS (
@@ -9,13 +17,30 @@ SELECT
     kpi AS team_kpi
 FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
 )
---kpi_by_team AS (
+
 SELECT 
-    a.function,
-    a.role,
-    kpi
-FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` as a
-LEFT JOIN gather_all_kpis AS b ON b.function = a.function --AND team_kpi = kpi
-WHERE a.role <> b.role
+    function,
+    role,
+    team_kpi
+FROM gather_all_kpis as a
+WHERE team_kpi NOT IN (
+    SELECT b.role
+    FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` as b
+    WHERE a.function = b.function
+    GROUP BY b.role)
 GROUP BY 
-    function,kpi,role
+    function,team_kpi,role
+
+
+--SELECT "corba" IN (SELECT account FROM Players) as result;
+
+/*
+ (SELECT kpi 
+    FROM gather_all_kpis AS b 
+    WHERE team_kpi <> KPI 
+    AND a.function = b.function) as kpis_not_assigned
+    */
+--                (SELECT b.KPI 
+--                FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS b
+--                WHERE a.function = b.function
+--                GROUP BY b.first_name,b.last_name,b.kpi)
