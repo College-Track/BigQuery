2eb24007e870570191ac0ba0a4d870f22d83afb7
Baker Renneckar <baker@bakerr.com>
2021-05-20 14:04:51 -0700

    Auto-commit: Thu May 20 2021 14:04:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
+++ superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
@@ -1,6 +1,7 @@
 WITH gather_all_kpis AS (
 SELECT function, role, kpi
 FROM `data-studio-260217.performance_mgt.role_kpi_selection`
+ORDER BY function, role
 )
 
 
@@ -8,3 +9,4 @@ SELECT GAK_ALL.function, GAK_ALL.role, GAK.kpi
 FROM gather_all_kpis GAK_ALL
 LEFT JOIN gather_all_kpis GAK ON GAK_ALL.function = GAK_ALL.function
 WHERE GAK_ALL.role != GAK.role AND GAK_ALL.kpi != GAK.kpi
+ORDER BY GAK_ALL.functon, GAK_ALL.role
