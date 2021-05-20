f7c1e1722fe282e1731a0ce6046125b70077cc55
Baker Renneckar <baker@bakerr.com>
2021-05-20 14:05:22 -0700

    Auto-commit: Thu May 20 2021 14:05:16 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
+++ superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
@@ -9,4 +9,4 @@ SELECT GAK_ALL.function, GAK_ALL.role, GAK.kpi
 FROM gather_all_kpis GAK_ALL
 LEFT JOIN gather_all_kpis GAK ON GAK_ALL.function = GAK_ALL.function
 WHERE GAK_ALL.role != GAK.role AND GAK_ALL.kpi != GAK.kpi
-ORDER BY GAK_ALL.functon, GAK_ALL.role
+ORDER BY function, role
