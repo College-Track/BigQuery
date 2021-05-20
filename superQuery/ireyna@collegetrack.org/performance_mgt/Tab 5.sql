cc9c23add9ac2fccc8f53a4824a19be604b1378a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 19:50:58 -0700

    Auto-commit: Wed May 19 2021 22:50:54 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
@@ -19,7 +19,8 @@ FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS a
 WHERE kpi NOT IN 
                 (SELECT b.KPI 
                 FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS b
-                WHERE a.function = b.function)
+                WHERE a.function = b.function
+                GROUP BY b.first_name,b.last_name,b.kpi)
 GROUP BY
     first_name,
     last_name,
