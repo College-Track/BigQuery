aba82aa4c35889309ba101aadc20e589d846b4eb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 20:44:04 -0700

    Auto-commit: Wed May 19 2021 23:44:00 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
@@ -12,9 +12,10 @@ FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
 --kpi_by_team AS (
 SELECT 
     a.function,
+    a.role,
     kpi
 FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` as a
 LEFT JOIN gather_all_kpis AS b ON b.function = a.function AND team_kpi = kpi
 WHERE a.role <> b.role
 GROUP BY 
-    function,kpi
+    function,kpi,role
