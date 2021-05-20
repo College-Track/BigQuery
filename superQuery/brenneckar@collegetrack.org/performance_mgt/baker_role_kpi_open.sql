4fc564c324ee2a7f690ae01c05ebb549d7bb6d21
Baker Renneckar <baker@bakerr.com>
2021-05-20 13:18:12 -0700

    Auto-commit: Thu May 20 2021 13:18:08 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
@@ -0,0 +1,14 @@
+SELECT
+    first_name,
+    last_name,
+    function AS function_all,
+    role AS role_all,
+    kpi AS kpi_all
+    
+FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
+GROUP BY
+    first_name,
+    last_name,
+    function,
+    role,
+    kpi
