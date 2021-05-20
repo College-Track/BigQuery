4eb7d4ba92dfbce6f0e3fef5e65d9132d2139f9e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-19 19:46:13 -0700

    Auto-commit: Wed May 19 2021 22:45:58 GMT-0400 (Eastern Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/Tab 5.sql
@@ -7,84 +7,23 @@ OPTIONS
 AS
 */
 
-WITH 
-
-gather_all_kpis AS (
 SELECT
     first_name,
     last_name,
-    function AS function_all,
-    role AS role_all,
-    kpi AS kpi_all
+    function,
+    role,
+    kpi
     
-FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi`
+FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS a
 
+WHERE kpi NOT IN 
+                (SELECT b.KPI 
+                FROM `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` AS b
+                WHERE a.function = b.function 
+                AND a.role <> b.role)
 GROUP BY
     first_name,
     last_name,
     function,
     role,
     kpi
-),
-
-
---KPIs on someone's team, but not mapped to their specific role KPIs.
---They can select the KPIs on their team that is not one of their KPIs based on their role (MUST be a KPI on their team)
---Exception: CCAs- they will be able to enter a caseload size and cutomized target for their role during the Inidivudal KPI Selection phase
-team_kpis AS (
-SELECT
-    function_all,
-    role_all,
-    kpi_all AS team_kpi
-    
-FROM gather_all_kpis
-
-GROUP BY 
-    function_all,
-    kpi_all,
-    role_all
-),
-
-role_kpis AS (
-SELECT 
-    kpi_all AS role_kpi_selected,
-    role_all AS role,
-    function_all AS roles_team
-
-FROM gather_all_kpis 
-
-GROUP BY 
-    kpi_all,
-    function_all,
-    role
-    
-),
-select_open_kpi AS (
-SELECT 
-    role,
-    role_kpi_selected,
-    CASE 
-        WHEN roles_team = function_all
-        AND role <> role_all
-        THEN 'should be open kpi'
-    end as open_q,
-    team_kpi
-    
-FROM team_kpis AS a
-FULL JOIN role_kpis 
-ON function_all = roles_team
-)
-SELECT role,team_kpi
-FROM select_open_kpi AS a
-WHERE team_kpi NOT IN (select b.team_kpi from select_open_kpi AS b where a.role = b.role)
-/*)
-SELECT 
-    role,
-    open_q,
-    role_kpi_selected
-
-from select_open_kpi
-
-GROUP BY 
-    role, open_q, role_kpi_selected
-*/
