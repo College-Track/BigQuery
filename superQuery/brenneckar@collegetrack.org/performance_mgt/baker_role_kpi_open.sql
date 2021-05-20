325f5d13ffa9edbb6bd4ba11c17550aeb2b9bae8
Baker Renneckar <baker@bakerr.com>
2021-05-20 14:08:53 -0700

    Auto-commit: Thu May 20 2021 14:08:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
+++ superQuery/brenneckar@collegetrack.org/performance_mgt/baker_role_kpi_open.sql
@@ -17,5 +17,5 @@ WHERE GAFR.role != GAK.role
 
 SELECT CLUK.function, CLUK.role, CLUK.kpi
 FROM create_list_of_unseleted_kpis CLUK 
--- LEFT JOIN gather_all_kpis GAK ON GAK.function = CLUK.function AND GAK.role = CLUK.role
--- WHERE GAK.kpi != CLUK.kpi
+LEFT JOIN gather_all_kpis GAK ON GAK.function = CLUK.function AND GAK.role = CLUK.role
+WHERE GAK.kpi != CLUK.kpi
