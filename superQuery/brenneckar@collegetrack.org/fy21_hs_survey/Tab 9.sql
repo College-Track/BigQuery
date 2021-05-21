db6b25e64089aa0c1ef204ddfda4595c2333bf20
Baker Renneckar <baker@bakerr.com>
2021-05-21 15:06:12 -0700

    Auto-commit: Fri May 21 2021 15:06:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 9.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 9.sql
@@ -8,13 +8,18 @@ WITH gahter_ethnicity_data AS (
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = S.Contact_id
   WHERE
     S.Contact_Id IS NOT NULL
-)
-SELECT
+),
+group_data AS (SELECT
   what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_,
-  COUNT(Contact_Id)
+  COUNT(Contact_Id) as student_count
 FROM
   gahter_ethnicity_data
 WHERE
   what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_ != Ethnic_background_c
 GROUP BY
   what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_
+  )
+  
+
+SELECT SUM(student_count)
+FROM group_data
