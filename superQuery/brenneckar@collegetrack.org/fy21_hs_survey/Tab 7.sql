f0ef7bdb57d1f281535eba1723138b1c587b5baa
Baker Renneckar <baker@bakerr.com>
2021-05-21 14:33:26 -0700

    Auto-commit: Fri May 21 2021 14:33:22 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 7.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 7.sql
@@ -1,26 +1,28 @@
 WITH calc_score AS (
-SELECT 
+  SELECT
     site_short,
     section,
     sub_section,
     question,
     `data-studio-260217.surveys.determine_positive_answers`(answer) AS answer_score,
-
-FROM `data-studio-260217.surveys.fy21_hs_survey_long_prepped`
+  FROM
+    `data-studio-260217.surveys.fy21_hs_survey_long_prepped`
 )
-
-SELECT 
-section,
--- question,
--- site_short,
--- SUM(answer_score)
-avg(answer_score) as score 
-FROM calc_score
--- WHERE sub_section = 'wellness_programming_services_subsection'
-WHERE question != 'Did you engage with Wellness services at your site?'
-AND sub_section != 'virtual_programming_return_to_ct_subsection'
-GROUP BY section
--- site_short
--- ORDER BY section, 
--- score,
--- site_short
+SELECT
+  section,
+  -- question,
+  -- site_short,
+  -- SUM(answer_score)
+  avg(answer_score) as score
+FROM
+  calc_score -- WHERE sub_section = 'wellness_programming_services_subsection'
+WHERE
+  question != 'Did you engage with Wellness services at your site?'
+  AND sub_section = 'virtual_programming_engagement_excitement_subsection'
+  OR sub_section = 'virtual_programming_self_direction_subsection'
+  OR sub_section = 'virtual_programming_collaboration_subsection'
+GROUP BY
+  section -- site_short
+  -- ORDER BY section,
+  -- score,
+  -- site_short
