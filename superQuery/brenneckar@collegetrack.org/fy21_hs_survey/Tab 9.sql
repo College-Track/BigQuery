959dd07f04ed348b8bb17fdb923f38cfe0f6e88a
Baker Renneckar <baker@bakerr.com>
2021-05-21 15:01:33 -0700

    Auto-commit: Fri May 21 2021 15:01:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 9.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 9.sql
@@ -1,3 +1,17 @@
-SELECT S.contact_id, what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_, C.Ethnic_background_c 
-FROM `data-studio-260217.surveys.fy21_hs_survey` S 
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = S.Contact_id
+WITH gahter_ethnicity_data AS (
+  SELECT
+    S.contact_id,
+    what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_,
+    C.Ethnic_background_c
+  FROM
+    `data-studio-260217.surveys.fy21_hs_survey` S
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = S.Contact_id
+  WHERE
+    S.Contact_Id IS NOT NULL
+)
+SELECT
+  COUNT(Contact_Id)
+FROM
+  gahter_ethnicity_data
+WHERE
+  what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_ != Ethnic_background_c
