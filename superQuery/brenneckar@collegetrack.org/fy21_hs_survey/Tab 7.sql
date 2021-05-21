76076a3b4d7f057f42a3565d0405ed1b2038b4cb
Baker Renneckar <baker@bakerr.com>
2021-05-21 00:03:00 -0700

    Auto-commit: Fri May 21 2021 00:02:57 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 7.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 7.sql
@@ -1,11 +1,13 @@
 WITH calc_score AS (
 SELECT 
+    site_short,
     section, 
     `data-studio-260217.surveys.determine_positive_answers`(answer) AS answer_score,
 
 FROM `data-studio-260217.surveys.fy21_hs_survey_long_prepped`
 )
 
-SELECT *
+SELECT section,
+avg(answer_score)
 FROM calc_score
-WHERE answer_score = 1
+GROUP BY section
