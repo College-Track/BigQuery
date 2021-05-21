30c0639aa296dbe7d0fb3993c1a17535c3190d4f
Baker Renneckar <baker@bakerr.com>
2021-05-21 15:00:10 -0700

    Auto-commit: Fri May 21 2021 15:00:04 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 9.sql
+++ superQuery/brenneckar@collegetrack.org/fy21_hs_survey/Tab 9.sql
@@ -1,3 +1,3 @@
-SELECT contact_id, what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_
-FROM `data-studio-260217.surveys.fy21_hs_survey`
-LIMIT 1000
+SELECT S.contact_id, what_race_ethnicity_best_describes_you_for_a_more_detailed_description_of_asian_, C.Ethnic_background_c 
+FROM `data-studio-260217.surveys.fy21_hs_survey` S 
+LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON C.Contact_Id = S.Contact_id
