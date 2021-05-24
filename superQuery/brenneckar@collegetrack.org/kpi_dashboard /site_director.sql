c71bddb1bd8549fd909ae272b941b4f8ee3dbd22
Baker Renneckar <baker@bakerr.com>
2021-05-24 12:34:10 -0700

    Auto-commit: Mon May 24 2021 12:34:05 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /site_director.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /site_director.sql
@@ -9,3 +9,5 @@ college_track_status_c
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE AT_Grade_c = 'Year 1'
 AND AT_Enrollment_Status_c != 'Approved Gap Year'
+AND years_since_hs_grad_c <= 6
+AND indicator_completed_ct_hs_program_c = true
