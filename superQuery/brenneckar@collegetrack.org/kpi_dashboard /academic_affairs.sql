586f72c838fbe0e8d3f2b88286f3abff5d975a79
Baker Renneckar <baker@bakerr.com>
2021-05-24 12:04:46 -0700

    Auto-commit: Mon May 24 2021 12:04:30 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /academic_affairs.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /academic_affairs.sql
@@ -2,13 +2,17 @@ WITH gather_data AS (
   SELECT
     Contact_Id,
     site_short,
+    -- % of students with a 3.25 GPA
+    -- Will need to make this more dynamic to account for GPA lag
     CASE
       WHEN Prev_AT_Cum_GPA >= 3.25 THEN 1
       ELSE 0
     END AS above_325_gpa,
+    -- % of seniors who are composite ready
+    -- Will need to add in the opt-out indicator once it is made.
     CASE
       WHEN composite_readiness_most_recent_c = '1. Ready'
-      AND grade_c = '12th Grade' THEN 1
+      AND (grade_c = '12th Grade' OR (grade_c = 'Year 1' AND years_since_hs_grad_c =0)) THEN 1
       ELSE 0
     END AS composite_ready
   FROM
@@ -17,6 +21,8 @@ WITH gather_data AS (
     current_as_c = true
     AND college_track_status_c IN ('11A')
 ),
+-- % of students with 80% attendance in AA workshops
+-- This KPI is done over two CTEs the filtering occurs in the first and the logic occurs in the second
 aa_attendance_prep AS (
     SELECT
       C.student_c,
@@ -46,11 +52,14 @@ gather_survey_data AS (
   SELECT
     CT.site_short,
     S.contact_id,
+    --  % of students that agree or strongly agree to this statement: “I am in control of my academic performance”
     CASE
       WHEN ct_helps_me_better_understand_that_i_am_in_control_of_my_academic_performance = "Strongly Agree" THEN 1
       WHEN ct_helps_me_better_understand_that_i_am_in_control_of_my_academic_performance = 'Agree' THEN 1
       ELSE 0
     END AS i_am_in_control_of_my_academic_performance,
+    -- Growth in the % of students that agree or strongly agree to this statement: “I feel prepared to engage in academic stretch opportunities.” 
+    -- Not doing any growth calculation here, just displaying the raw percent. Can follow up with VS if that is a concern. 
         CASE
       WHEN i_feel_prepared_to_engage_in_academic_stretch_opportunities = "Strongly Agree" THEN 1
       WHEN i_feel_prepared_to_engage_in_academic_stretch_opportunities = 'Agree' THEN 1
@@ -62,13 +71,6 @@ gather_survey_data AS (
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
  
 )
--- aggregate_survey_data AS (
--- SELECT
--- site_short,
-
--- FROM gather_survey_data
--- GROUP BY site_short
--- )
 
   
 SELECT
