c7b255577b20d581d409ac2c9bfc615c7af2d489
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 14:30:05 -0700

    Auto-commit: Tue May 25 2021 14:30:00 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,6 +1,6 @@
 WITH 
 
---gather contact and academic term data to join with COVI data
+--Gather contact and academic term data to join with COVI data
 gather_at_data AS
 (
 SELECT 
@@ -15,29 +15,25 @@ FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE record_type_id = '01246000000RNnSAAW' --HS student contact record type
     AND site != 'College Track Arlen'
     AND College_Track_Status_Name = 'Current CT HS Student'
-    AND AY_Name = 'AY 2020-21' --To pull in Covi test records within this given AY 
+   -- AND AY_Name = 'AY 2020-21' --To pull in Covi test records within this given AY 
 ),
 
+--Pull Covi assessments completed within the 2020-21 AY
 gather_covi_data AS (
 SELECT 
     contact_name_c AS contact_id_covi,
     academic_semester_c AS covi_at,
     co_vitality_scorecard_color_c,
-    belief_in_self_raw_score_c,
-    belief_in_others_raw_score_c,
-    emotional_competence_raw_score_c,
-    engaged_living_raw_score_c,
+    
     --add Covi Domain scores to obtain total raw Covitality score
     SUM(belief_in_self_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c + engaged_living_raw_score_c) AS raw_covi_score,
-    version_c,
-    status_c,
-    co_vitality_test_completed_date_c, 
+    
     id AS test_record_id,
     student_site_c,
     record_type_id
 
 FROM `data-warehouse-289815.salesforce_clean.test_clean` AS COVI
-LEFT JOIN gather_at_data  AS GAD
+LEFT JOIN gather_at_data AS GAD
     ON GAD.at_id = COVI.academic_semester_c
     
 WHERE COVI.record_type_id ='0121M000001cmuDQAQ' --Covitality test record type
@@ -52,28 +48,25 @@ GROUP BY
     belief_in_others_raw_score_c,
     emotional_competence_raw_score_c,
     engaged_living_raw_score_c,
-    version_c,
-    status_c,
-    co_vitality_test_completed_date_c,
     id, --test record id
     student_site_c,
     record_type_id
-)
+    
+),
 
---Join contact_at data with COVI data to obtain contact id and pull in 2020-21AY Covi data
---join_term_data_with_covi AS (
+--Join COVI data completed 2020-21AY with contact_at data
+join_term_data_with_covi AS (
 SELECT 
     full_name_c,
-    contact_id_covi,
-    --co_vitality_test_completed_date_c,
-    --at_id,
+    contact_id,
+    co_vitality_test_completed_date_c,
     covi_at, 
-    --at_name,
     student_site_c,
     raw_covi_score,
-    contact_id,
     AY_NAME,
     test_record_id,
+    
+    --Indicator for students without a Covi score during 2020-21AY
     CASE 
         WHEN test_record_id IS NOT NULL THEN 1
         ELSE 0
@@ -81,17 +74,24 @@ SELECT
    
 FROM gather_at_data AS A
 LEFT JOIN gather_covi_data AS C ON A.contact_id = C.contact_id_covi
---LEFT JOIN gather_covi_data AS C ON A.at_id = C.covi_at
 
 GROUP BY 
     full_name_c,
-    contact_id_covi,
-    --co_vitality_test_completed_date_c,
-    --at_id,
+    contact_id,
+    co_vitality_test_completed_date_c,
     covi_at,
-    --at_name,
     student_site_c,
     raw_covi_score,
-    contact_id,
     AY_NAME,
     test_record_id
+)
+
+
+--gather_students_with_more_than_1_covi AS (
+SELECT 
+    contact_id,
+    --SUM(covi_assessment_completed_ay) AS sum_of_covi_tests_taken_ay --does sudent have more than 1 covi assessment?
+    
+FROM join_term_data_with_covi 
+WHERE covi_assessment_completed_ay = 0
+GROUP BY contact_id
