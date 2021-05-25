f33142c4d34e034571ae07df97e4007ca95648df
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 14:55:21 -0700

    Auto-commit: Tue May 25 2021 14:55:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -6,68 +6,61 @@ gather_at_data AS
 SELECT 
     full_name_c,
     at_id,
-    at_name,
     contact_id,
     AY_Name,
-    site
+    site_short
 
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` 
 WHERE record_type_id = '01246000000RNnSAAW' --HS student contact record type
     AND site != 'College Track Arlen'
     AND College_Track_Status_Name = 'Current CT HS Student'
-   -- AND AY_Name = 'AY 2020-21' --To pull in Covi test records within this given AY 
 ),
 
---Pull Covi assessments completed within the 2020-21 AY
+--Pull Covi assessments completed within appropriate AYs (2019,20, 2020-21)
 gather_covi_data AS (
 SELECT 
     contact_name_c AS contact_id_covi,
     academic_semester_c AS covi_at,
-    co_vitality_scorecard_color_c,
-    
-    --add Covi Domain scores to obtain total raw Covitality score
-    SUM(belief_in_self_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c + engaged_living_raw_score_c) AS raw_covi_score,
-    
     id AS test_record_id,
-    student_site_c,
-    record_type_id
-
+    site_short,
+    AY_Name,
+    
+    --add Covi Domain scores to obtain total raw Covitality score. Use lowest score if student has 1+ Covi
+    MIN(belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c) AS min_covi_raw_score
+    
 FROM `data-warehouse-289815.salesforce_clean.test_clean` AS COVI
 LEFT JOIN gather_at_data AS GAD
     ON GAD.at_id = COVI.academic_semester_c
     
 WHERE COVI.record_type_id ='0121M000001cmuDQAQ' --Covitality test record type
     AND status_c = 'Completed'
-    AND AY_Name = 'AY 2020-21'
+    AND AY_Name IN ('AY 2019-20', 'AY 2020-21')
     
 GROUP BY 
     contact_name_c,
     academic_semester_c,
-    co_vitality_scorecard_color_c,
-    belief_in_self_raw_score_c,
-    belief_in_others_raw_score_c,
-    emotional_competence_raw_score_c,
-    engaged_living_raw_score_c,
     id, --test record id
-    student_site_c,
-    record_type_id
+    site_short,
+    AY_Name
     
+ORDER BY
+    site_short,
+    contact_name_c,
+    AY_Name
 ),
 
---Join COVI data completed 2020-21AY with contact_at data
+/*--Join COVI data completed 2020-21AY with contact_at data
 join_term_data_with_covi AS (
 SELECT 
-    full_name_c,
     contact_id,
-    covi_at, 
     student_site_c,
-    raw_covi_score,
-    AY_NAME,
     test_record_id,
     
     --Indicator for students without a Covi score during 2020-21AY
     CASE 
-        WHEN test_record_id IS NOT NULL THEN 1
+        WHEN AY_Name = 'AY 2020-21'
+        AND test_record_id IS NOT NULL 
+        THEN 1
         ELSE 0
         END AS covi_assessment_completed_ay
    
@@ -75,21 +68,92 @@ FROM gather_at_data AS A
 LEFT JOIN gather_covi_data AS C ON A.contact_id = C.contact_id_covi
 
 GROUP BY 
-    full_name_c,
     contact_id,
-    covi_at,
     student_site_c,
     raw_covi_score,
     AY_NAME,
     test_record_id
-)
+),
 
-
---gather_students_with_more_than_1_covi AS (
+students_that_completed_covi AS (
 SELECT 
-    contact_id,
-    --SUM(covi_assessment_completed_ay) AS sum_of_covi_tests_taken_ay --does sudent have more than 1 covi assessment?
-    
+    contact_id AS student_completed_covi_ay
+  
 FROM join_term_data_with_covi 
 WHERE covi_assessment_completed_ay = 1
 GROUP BY contact_id
+),
+*/
+--Using same logic from Site Director KPIs to calculate: % of students growing toward average or above social-emotional strengths
+--This KPI is done over four CTEs. The majority of the logic is done in the second CTE.
+calc_covi_growth AS (
+SELECT
+    site_short,
+    contact_id_covi,
+    min_covi_raw_score - lag(min_covi_raw_score) over (
+      partition by contact_id_covi
+      order by AY_Name
+      ) AS covi_growth
+FROM
+    gather_covi_data
+),
+
+determine_covi_indicators AS (
+SELECT
+    A.site_short,
+    A.contact_id_covi,
+    
+     --Indicator for students without a Covi score during 2020-21AY
+    CASE 
+        WHEN AY_Name = 'AY 2020-21'
+        AND test_record_id IS NOT NULL 
+        THEN 1
+        ELSE 0
+        END AS covi_assessment_completed_ay,
+        
+    --Indicator for students demonstrating growth in Covi taken between 2019-20 and 2020-21
+    CASE
+        WHEN covi_growth > 0 THEN 1
+        ELSE 0
+        END AS covi_student_grew
+        
+FROM
+    calc_covi_growth AS A
+LEFT JOIN gather_covi_data AS B
+    ON A.contact_id_covi = B.contact_id_covi
+WHERE
+    covi_growth IS NOT NULL
+),
+
+-- % of students growing toward average or above social-emotional strengths
+-- This KPI is done over four CTEs (could probaly be made more efficient). The majority of the logic is done in the second CTE.
+
+aggregate_covi_data AS (
+SELECT
+  site_short,
+  SUM(covi_student_grew) AS wellness_covi_student_grew,
+  SUM(covi_assessment_completed_ay) AS wellness_covi_assessment_completed_ay,
+  COUNT(contact_id_covi) AS wellness_covi_denominator
+FROM
+  determine_covi_indicators
+GROUP BY
+  site_short
+)
+
+SELECT 
+    A.*
+FROM aggregate_covi_data AS A
+GROUP BY 
+    site_short,
+    wellness_covi_student_grew,
+    wellness_covi_assessment_completed_ay,
+    wellness_covi_denominator
+
+
+/*
+gather_casenotes_data AS (
+SELECT 
+)
+*/
+
+    
