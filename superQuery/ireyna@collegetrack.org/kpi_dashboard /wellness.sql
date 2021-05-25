17529d7dfbab01980ddd69388a4f339b4aad4ac4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 13:16:34 -0700

    Auto-commit: Tue May 25 2021 13:16:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -39,7 +39,6 @@ SELECT
 FROM `data-warehouse-289815.salesforce_clean.test_clean` AS COVI
 WHERE record_type_id ='0121M000001cmuDQAQ' --Covitality test record type
     AND status_c = 'Completed'
-    AND AY_Name = 'AY 2020-21'
 GROUP BY 
     contact_name_c,
     academic_semester_c,
