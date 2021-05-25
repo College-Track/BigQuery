5feeee011bf7b867eca02fc522c98f571986c209
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 13:12:39 -0700

    Auto-commit: Tue May 25 2021 13:12:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,3 +1,8 @@
+WITH 
+
+--gather contact and academic term data to join with COVI data
+gather_at_data AS
+(
 SELECT 
     full_name_c,
     at_id,
@@ -9,3 +14,62 @@ FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE record_type_id = '01246000000RNnSAAW' --HS student
     AND site != 'College Track Arlen'
     AND College_Track_Status_Name = 'Current CT HS Student'
+    AND AY_Name = 'AY 2020-21' --To pull in Academic Terms referenced on Covi test records this AY 
+),
+
+gather_covi_data AS (
+SELECT 
+    contact_name_c AS contact_id_covi,
+    academic_semester_c,
+    co_vitality_scorecard_color_c,
+    belief_in_self_raw_score_c,
+    belief_in_others_raw_score_c,
+    emotional_competence_raw_score_c,
+    engaged_living_raw_score_c,
+    --add Covi Domain scores to obtain total raw Covitality score
+    SUM(belief_in_self_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c + engaged_living_raw_score_c) AS raw_covi_score,
+    version_c,
+    status_c,
+    --test_date_c, 
+    co_vitality_test_completed_date_c, 
+    id AS test_record_id,
+    student_site_c,
+    record_type_id
+
+FROM `data-warehouse-289815.salesforce_clean.test_clean` AS COVI
+WHERE record_type_id ='0121M000001cmuDQAQ' --Covitality test record type
+AND status_c = 'Completed'
+GROUP BY 
+    contact_name_c,
+    academic_semester_c,
+    co_vitality_scorecard_color_c,
+    belief_in_self_raw_score_c,
+    belief_in_others_raw_score_c,
+    emotional_competence_raw_score_c,
+    engaged_living_raw_score_c,
+    version_c,
+    status_c,
+    co_vitality_test_completed_date_c,
+    id, --test record id
+    student_site_c,
+    record_type_id
+)
+
+--Join contact_at data with COVI data to obtain contact id and pull in 2020-21AY Covi data
+--join_term_data_with_covi AS (
+SELECT 
+    co_vitality_test_completed_date_c,
+    student_site_c,
+    raw_covi_score,
+    contact_id,
+    AY_NAME,
+    test_record_id,
+    CASE 
+        WHEN test_record_id IS NOT NULL THEN 1
+        ELSE 0
+        END AS covi_assessment_completed_ay
+   
+FROM gather_at_data AS A
+LEFT JOIN gather_covi_data AS C ON A.contact_id = C.contact_id_covi
+WHERE AY_Name = 'AY 2020-21'
+    AND status_c = 'Completed'
