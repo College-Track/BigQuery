3e525a75c8219f7bb93fba3e752117781a0c4a41
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 22:07:13 -0700

    Auto-commit: Tue May 25 2021 22:06:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -1,4 +1,4 @@
-/*
+ /*
 CREATE OR REPLACE TABLE `data-studio-260217.kpi_dashboard.student_life` 
 OPTIONS
     (
@@ -6,7 +6,6 @@ OPTIONS
     )
 AS
 */
-
 WITH gather_contact_data AS(
     SELECT
         contact_id,
@@ -19,22 +18,17 @@ WITH gather_contact_data AS(
             THEN 1
             ELSE  0 
             END AS dream_declared
-            
-    FROM `data-warehouse-289815.salesforce_clean.contact_template` AS C
-    WHERE (college_track_status_c = '11A' OR indicator_completed_ct_hs_program_c = TRUE)
-
+ 
+FROM `data-warehouse-289815.salesforce_clean.contact_template` AS C
+WHERE (college_track_status_c = '11A' OR indicator_completed_ct_hs_program_c = TRUE)
 ),
-
---pull in students that have at least 1 workshop attendance session in Fall/Spring, or were active in Summer
+--pull in students that have at least 1 workshop attendance session
 --this indicates some threshold of participation in programming, even if attendance numerator < 1
+
 set_mse_reporting_group AS (
 SELECT 
+    COUNT (DISTINCT CT.student_c) AS mse_denom_prev_ay,
     CAT.student_c,
-    /*MAX(CASE 
-        WHEN CAT.student_c IS NOT NULL
-        THEN 1
-        ELSE 0
-    END) AS student_count,*/
     site_short
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT    
 LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` CT
@@ -42,43 +36,42 @@ ON CAT.contact_id = CT.student_c
 
 --pull in students that have a session attendance record in Fall/Spring 2019-20, excluding NSO 
 WHERE site_short <> "College Track Arlen"
-    AND (Attendance_Denominator_c IS NOT NULL
+AND (Attendance_Denominator_c IS NOT NULL
     AND dosage_types_c NOT LIKE '%NSO%'
     AND AY_Name = "AY 2019-20"
     AND term_c IN ("Fall","Spring")
     AND grade_c != '8th Grade'
-    AND (CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
+
+AND (CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
         AND student_audit_status_c IN ('Current CT HS Student','Leave of Absence')))
-        
---pull in students that were active at end of Summer 2019-20; CT Status (AT)
-   OR(AY_Name = "AY 2019-20"
+--pull in students that were active at end of Spring 2019-20 or Summer 2019-20; CT Status (AT)
+
+OR(Attendance_Denominator_c IS NOT NULL
+    AND dosage_types_c NOT LIKE '%NSO%'
+    AND AY_Name = "AY 2019-20"
     AND term_c ='Summer'
     AND grade_c != '8th Grade'
     AND (CAT.global_academic_semester_c = 'a3646000000dMXiAAM' --Summer 2019-20 (Semester)
         AND student_audit_status_c IN ('Current CT HS Student')))
-        
+
 GROUP BY
     site_short, CAT.student_c
 ),
-
---Pull numerator: meaningful summer experience data from current and previous AY (hard-entry 2019-20)
+--Pull meaningful summer experience data from current and previous AY (hard-entry 2019,20, 2020-21)
 gather_mse_data AS (
     SELECT 
         contact_id,
         m.site_short,
-         MAX(CASE 
-        WHEN m.student_c IS NOT NULL
-        THEN 1
-        ELSE 0
-        END) AS student_count,
+        --The following KPIs will pull in: Current CT HS Students and those that Completed the CT HS program. 
+        --This will allow us to include PS student MSEs completed last Summer while still in HS
         --pull completed MSE last Summer
+        
         MAX(CASE
             WHEN (AY_name = 'AY 2019-20'
             AND term_c = 'Summer')
             THEN 1
             ELSE 0
             END) AS mse_completed_prev_AY,
-        --pull competitive MSE last Summer
         MAX(CASE 
             WHEN (competitive_c = True 
             AND AY_name = 'AY 2019-20'
@@ -86,7 +79,6 @@ gather_mse_data AS (
             THEN 1
             ELSE 0
             END) AS mse_competitive_prev_AY,
-        --pull completed internship last AY. Should this be limited to Summer only?
         MAX(CASE
             WHEN (type_c = 'Internship' 
             AND AY_name = 'AY 2019-20')
@@ -94,13 +86,13 @@ gather_mse_data AS (
             ELSE 0
             END) AS mse_internship_prev_AY,
             
-FROM set_mse_reporting_group AS m
-LEFT JOIN `data-warehouse-289815.salesforce.student_life_activity_c` AS sl 
-    ON m.student_c = sl.student_c
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS c
-    ON c.at_id = sl.semester_c 
+    FROM set_mse_reporting_group AS m
+    LEFT JOIN `data-warehouse-289815.salesforce.student_life_activity_c` AS sl 
+        ON m.student_c = sl.student_c
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` AS c
+        ON c.at_id = sl.semester_c 
         
-WHERE sl.record_type_id = '01246000000ZNi8AAG' #Summer Experience
+    WHERE sl.record_type_id = '01246000000ZNi8AAG' #Summer Experience
     AND AY_name IN ('AY 2020-21', 'AY 2019-20')
     AND term_c = 'Summer'
     AND experience_meaningful_c = True
@@ -109,9 +101,7 @@ GROUP BY
     contact_id,
     site_short
 ),
-
---group attendance data by student first, aggregate at site level in prep_attendance_kpi
-gather_attendance_data AS ( 
+gather_attendance_data AS ( #group attendance data by student first, aggregate at site level in prep_attendance_kpi
      SELECT 
         c.student_c, 
         CASE
@@ -126,7 +116,6 @@ gather_attendance_data AS (
     AND CAT.AY_Name = 'AY 2020-21'
     GROUP BY c.student_c
 ),
-
 prep_attendance_kpi AS (
     SELECT 
         site_short,
@@ -137,7 +126,6 @@ prep_attendance_kpi AS (
     FROM gather_contact_data as gd
     LEFT JOIN gather_attendance_data AS attendance ON gd.contact_id = attendance.student_c
 ),
-
 aggregate_attendance_kpi AS (
     SELECT 
         site_short,
@@ -145,7 +133,6 @@ aggregate_attendance_kpi AS (
     FROM prep_attendance_kpi
     GROUP BY site_short
 ),
-
 aggregate_dream_kpi AS (
     SELECT 
         site_short,
@@ -153,36 +140,29 @@ aggregate_dream_kpi AS (
     FROM gather_contact_data
     GROUP BY site_short
 ),
-
 aggregate_mse_kpis AS (
     SELECT 
-        A.site_short,
-        SUM(student_count) AS sl_mse_denom_prev_ay,
+        site_short,
         SUM(mse_completed_prev_AY) AS sl_mse_completed_prev_AY,
         SUM(mse_competitive_prev_AY) AS sl_mse_competitive_prev_AY,
         SUM(mse_internship_prev_AY) AS sl_mse_internship_prev_AY,
-    FROM gather_mse_data AS A
-    LEFT JOIN set_mse_reporting_group AS B
-    ON A.site_short = B.site_short
+    FROM gather_mse_data
     GROUP BY site_short
 )
-
 SELECT 
     d.site_short,
     sl_dreams_declared,
     attendance_kpi.* EXCEPT (site_short),
-    mse_kpi.* EXCEPT (site_short),
-    sl_mse_denom_prev_ay
+    mse_kpi.* EXCEPT (site_short)
     FROM aggregate_dream_kpi AS d 
         LEFT JOIN aggregate_attendance_kpi AS attendance_kpi ON d.site_short=attendance_kpi.site_short
         LEFT JOIN aggregate_mse_kpis AS mse_kpi ON d.site_short=mse_kpi.site_short
+        LEFT JOIN set_mse_reporting_group AS mse_denom ON d.site_short=mse_denom.site_short
     GROUP BY
         site_short,
         sl_mse_completed_prev_AY,
         sl_mse_competitive_prev_AY,
         sl_mse_internship_prev_AY,
-        mse_kpi.sl_mse_denom_prev_ay,
         sl_dreams_declared,
         sl_above_80_attendance
-        
         
