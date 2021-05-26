1d46f1847a9cac3a7016f4d9e79ee3f30d8964a2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 21:06:22 -0700

    Auto-commit: Tue May 25 2021 21:06:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -38,20 +38,21 @@ ON CAT.contact_id = CT.student_c
 
 --pull in students that have a session attendance record in Fall/Spring 2019-20, excluding NSO 
 WHERE site_short <> "College Track Arlen"
-    AND Attendance_Denominator_c IS NOT NULL
+    AND (Attendance_Denominator_c IS NOT NULL
     AND dosage_types_c NOT LIKE '%NSO%'
     AND AY_Name = "AY 2019-20"
     AND term_c IN ("Fall","Spring")
     AND grade_c != '8th Grade'
-    
+    AND (CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
+        AND student_audit_status_c IN ('Current CT HS Student','Leave of Absence')))
 --pull in students that were active at end of Spring 2019-20 or Summer 2019-20; CT Status (AT)
-    AND (
-        CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
-        AND student_audit_status_c IN ('Current CT HS Student','Leave of Absence'))
-    OR  (
-        (CAT.global_academic_semester_c = 'a3646000000dMXiAAM' --Summer 2019-20 (Semester)
-        AND student_audit_status_c IN ('Current CT HS Student'))
-        )
+   OR(Attendance_Denominator_c IS NOT NULL
+    AND dosage_types_c NOT LIKE '%NSO%'
+    AND AY_Name = "AY 2019-20"
+    AND term_c ='Summer'
+    AND grade_c != '8th Grade'
+    AND (CAT.global_academic_semester_c = 'a3646000000dMXiAAM' --Summer 2019-20 (Semester)
+        AND student_audit_status_c IN ('Current CT HS Student')))
         
 GROUP BY
     site_short, CAT.student_c
