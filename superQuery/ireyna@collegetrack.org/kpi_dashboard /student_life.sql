288e6260bd1382070c1b69976dc2e463e1f15333
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-25 21:01:14 -0700

    Auto-commit: Tue May 25 2021 21:01:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -44,9 +44,15 @@ WHERE site_short <> "College Track Arlen"
     AND term_c IN ("Fall","Spring")
     AND grade_c != '8th Grade'
     
---pull in students that were active at end of Spring 2019-20; CT Status (AT)
-    AND CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
-    AND student_audit_status_c IN ('Current CT HS Student','Leave of Absence')
+--pull in students that were active at end of Spring 2019-20 or Summer 2019-20; CT Status (AT)
+    AND (
+        CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
+        AND student_audit_status_c IN ('Current CT HS Student','Leave of Absence'))
+    OR  (
+        (CAT.global_academic_semester_c = 'a3646000000dMXiAAM' --Summer 2019-20 (Semester)
+        AND student_audit_status_c IN ('Current CT HS Student'))
+        )
+        
 GROUP BY
     site_short, CAT.student_c
 ),
