5bd5b4d0b4dc60e7d8cfabae8fc642042d3bd85c
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-26 10:41:33 -0700

    Auto-commit: Wed May 26 2021 10:41:20 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /cc_hs.sql
@@ -6,12 +6,14 @@ OPTIONS
     )
 AS
 
-WITH gather_data AS ( #active CT students; 10th grade EFC; 11th grade college aspirations
+
+--Pull in active CT students for 10th grade EFCKPI; 11th grade college aspirations KPI
+WITH gather_data AS ( 
     SELECT
         contact_id,
         site_short,
         
-         --10th Grade EFC num and demom
+         --10th Grade EFC, FAFSA4Caster only
         CASE
             WHEN (c.grade_c = "10th Grade" 
             AND FA_Req_Expected_Financial_Contribution_c IS NOT NULL 
@@ -25,20 +27,22 @@ WITH gather_data AS ( #active CT students; 10th grade EFC; 11th grade college as
             ELSE 0
             END AS hs_EFC_10th_denom_count,
             
-     --11th Grade Aspirations num and demom
+     --11th Grade Aspirations, any Aspiration
         CASE 
             WHEN (c.grade_c = '11th Grade'
             AND college_track_status_c = '11A'
             AND a.id IS NOT NULL) THEN 1
             ELSE 0
             END AS aspirations_any_count,
-        
+            
+    --11th Grade Aspirations, Affordable colleges
         CASE
             WHEN (c.grade_c = '11th Grade' 
             AND fit_type_current_c IN ("Best Fit","Good Fit","Local Affordable")) THEN 1
             ELSE 0
             END AS aspirations_affordable_count,
             
+    --11th Grade Aspirations reporting group        
         CASE 
             WHEN (c.grade_c = '11th Grade'
             AND college_track_status_c = '11A') THEN 1
@@ -50,7 +54,8 @@ WITH gather_data AS ( #active CT students; 10th grade EFC; 11th grade college as
     WHERE college_track_status_c = '11A'
 ),
 
-gather_attendance_data AS ( #for 12th grade KPI: 80% CC attendance
+--for 12th grade KPI: 80% CC attendance
+gather_attendance_data AS ( 
     SELECT 
         c.student_c, 
         CASE
@@ -65,12 +70,16 @@ gather_attendance_data AS ( #for 12th grade KPI: 80% CC attendance
     AND CAT.AY_Name = 'AY 2020-21'
     GROUP BY c.student_c
 ),
-        
+
+--Affordable college KPIs: Applying and Acceptance to Affordable Colleges and/or Best Fit/Good Fit schools only
 gather_data_twelfth_grade AS (
     SELECT 
         contact_id,
         attendance_rate,
+        site_short,
         
+    --% students completing the financial aid submission and verification processes
+        --May need to confirm which college applications should be included (e.g. accepted and enrolled/deferred). Currently pulling any college app
         (SELECT student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq1
         WHERE FA_Req_FAFSA_c = 'Submitted' 
@@ -78,50 +87,63 @@ gather_data_twelfth_grade AS (
         AND Contact_Id=student_c
         group by student_c
         ) AS gather_fafsa_verification,
-        
+    
+    --% of acceptances to Affordable colleges. This will be done in 2 parts since this can live in 2 fields: Fit Type (Applied), Fit Type (Enrolled)
+        --Pulling in students that were accepted to an affordable option. 
+        --Accepted & Enrolled/Deferred Affordable options will also be pulled in, but in another query below since these will live in Fit Type (enrolled) field
         (SELECT student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq1
-        WHERE admission_status_c = "Accepted" AND College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Local Affordable")
+        WHERE (admission_status_c = "Accepted" AND College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Local Affordable"))
         AND Contact_Id=student_c
         group by student_c
         ) AS applied_accepted_affordable,
         
+     --% of acceptances to Affordable colleges; % hs seniors who matriculate to affordable college
+        --Pulling in from Fit Type (enrolled)
+        --Will also be used to project matriculation to affordable colleges
         (SELECT student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq2
         WHERE admission_status_c IN ("Accepted and Enrolled", "Accepted and Deferred") AND fit_type_enrolled_c IN ("Best Fit","Good Fit","Local Affordable","Situational")
         AND Contact_Id=student_c
         group by student_c
         ) AS accepted_enrolled_affordable,
-        
-        site_short,
-        (SELECT student_c
-        FROM `data-warehouse-289815.salesforce_clean.college_application_clean` AS subq3
-        WHERE College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")  
-        AND Contact_Id=student_c
-        group by student_c
-        ) AS applied_best_good_situational,
-            
+    
+    --% accepted to Best Fit, Good Fit
+        --Same logic as the 2 queries above, except only looking at Good Fit and Best Fit in Fit Type (Applied)     
         (SELECT student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq4
         WHERE admission_status_c = "Accepted" AND College_Fit_Type_Applied_c IN ("Best Fit","Good Fit","Situational")
         AND Contact_Id=student_c
         group by student_c
         ) AS applied_accepted_best_good_situational,
-        
+    
+    --% accepted to Best Fit, Good Fit; % hs seniors who matriculate to Good/Best/Situational
+        --Same logic as the 2 queries above, except only looking at Good Fit and Best Fit in Fit Type (Enrolled) 
+    -- Will also be used to project matriculation
         (SELECT student_c
         FROM `data-warehouse-289815.salesforce_clean.college_application_clean`AS subq5
         WHERE admission_status_c IN ("Accepted and Enrolled", "Accepted and Deferred") AND fit_type_enrolled_c IN ("Best Fit","Good Fit","Situational")
         AND Contact_Id=student_c
         group by student_c
         ) AS accepted_enrolled_best_good_situational,
-        
+    
+    --% applying to Best Fit and Good Fit colleges
+        (SELECT student_c
+        FROM `data-warehouse-289815.salesforce_clean.college_application_clean` AS subq3
+        WHERE College_Fit_Type_Applied_c IN ("Best Fit","Good Fit")  
+        AND Contact_Id=student_c
+        group by student_c
+        ) AS applied_best_good_situational,
+            
+      
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
         LEFT JOIN gather_attendance_data ON contact_id=student_c
     WHERE  college_track_status_c = '11A'
     AND grade_c = '12th Grade'
 ),
 
-gather_eleventh_grade_metrics AS ( #11th grade College Aspirations KPI
+--Prepping 11th grade College Aspirations for aggregation
+gather_eleventh_grade_metrics AS ( 
  SELECT
     site_short,
     aspirations_denom_count,
@@ -133,6 +155,7 @@ gather_eleventh_grade_metrics AS ( #11th grade College Aspirations KPI
     GROUP BY contact_id,site_short, aspirations_denom_count
 ),
 
+--Prepping attendance data, FAFSA Verification KPI, Affordable college data for aggregation
 gather_twelfth_grade_metrics AS(
     SELECT  
         site_short,
@@ -158,10 +181,14 @@ gather_twelfth_grade_metrics AS(
             WHEN applied_accepted_best_good_situational IS NOT NULL THEN 1
             ELSE 0
             END AS cc_hs_accepted_best_good_situational,
+            
+    --Matriculation data not available yet, so projecting matriculation based on College Application enrollment data
         CASE 
             WHEN accepted_enrolled_best_good_situational IS NOT NULL THEN 1
             ELSE 0
             END AS cc_hs_enrolled_best_good_situational,  
+            
+    --Matriculation data not available yet, so projecting matriculation based on College Application enrollment data
         CASE 
             WHEN accepted_enrolled_affordable IS NOT NULL THEN 1
             ELSE 0
@@ -171,9 +198,8 @@ gather_twelfth_grade_metrics AS(
 ),
 
 
---prep KPIs for aggregation
-
-prep_tenth_grade_metrics AS ( #10th Grade EFC KPI
+--Aggregating 10th Grade EFC KPI
+prep_tenth_grade_metrics AS ( 
     SELECT 
         site_short,
         SUM(hs_EFC_10th_count) AS cc_hs_EFC_10th_num,
@@ -182,7 +208,8 @@ prep_tenth_grade_metrics AS ( #10th Grade EFC KPI
     GROUP BY site_short
 ),
 
-prep_eleventh_grade_metrics AS ( #11th grade College Aspirations KPI
+--Aggregating 11th grade College Aspirations KPI
+prep_eleventh_grade_metrics AS ( 
     SELECT 
         site_short,
         SUM(cc_hs_aspirations_num_prep) AS cc_hs_aspirations_num,
@@ -190,7 +217,7 @@ prep_eleventh_grade_metrics AS ( #11th grade College Aspirations KPI
     FROM gather_eleventh_grade_metrics
     GROUP BY site_short
 ),
-
+--Aggregating 12th grade KPIs: CC Attendance 80%, Affordable Colleges, Best Fit/Good Fit colleges, FAFSA verification
 prep_twelfth_grade_metrics AS (
     SELECT 
         site_short,
