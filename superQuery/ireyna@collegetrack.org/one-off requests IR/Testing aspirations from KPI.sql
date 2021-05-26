53374b8ad85d426767a561878c6265eefe0d2d3e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-26 11:56:25 -0700

    Auto-commit: Wed May 26 2021 11:56:20 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Testing aspirations from KPI.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Testing aspirations from KPI.sql
@@ -2,7 +2,7 @@
         full_name_c,contact_id,
         site_short,
         --10th Grade EFC, FAFSA4Caster only
-        MAX(CASE
+        SUM(CASE
             WHEN (
                 c.grade_c = "10th Grade"
                 AND FA_Req_Expected_Financial_Contribution_c IS NOT NULL
@@ -10,7 +10,7 @@
             ) THEN 1
             ELSE 0
         END) AS hs_EFC_10th_count,
-        MAX(CASE
+        SUM(CASE
             WHEN (
                 c.grade_c = "10th Grade"
                 AND college_track_status_c = '11A'
@@ -18,7 +18,7 @@
             ELSE 0
         END) AS hs_EFC_10th_denom_count,
         --11th Grade Aspirations, any Aspiration
-        MAX(CASE
+        SUM(CASE
             WHEN (
                 c.grade_c = '11th Grade'
                 AND college_track_status_c = '11A'
@@ -27,7 +27,7 @@
             ELSE 0
         END) AS aspirations_any_count,
         --11th Grade Aspirations, Affordable colleges
-        MAX(CASE
+        SUM(CASE
             WHEN (
                 c.grade_c = '11th Grade'
                 AND fit_type_current_c IN ("Best Fit", "Good Fit", "Local Affordable")
