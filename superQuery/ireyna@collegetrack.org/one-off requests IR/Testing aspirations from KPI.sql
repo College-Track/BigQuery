af8a212cea8fed589b433143809f2107e8f31de3
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-26 11:51:59 -0700

    Auto-commit: Wed May 26 2021 11:51:53 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Testing aspirations from KPI.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Testing aspirations from KPI.sql
@@ -2,46 +2,47 @@
         full_name_c,contact_id,
         site_short,
         --10th Grade EFC, FAFSA4Caster only
-        CASE
+        MAX(CASE
             WHEN (
                 c.grade_c = "10th Grade"
                 AND FA_Req_Expected_Financial_Contribution_c IS NOT NULL
                 AND fa_req_efc_source_c = 'FAFSA4caster'
             ) THEN 1
             ELSE 0
-        END AS hs_EFC_10th_count,
-        CASE
+        END) AS hs_EFC_10th_count,
+        MAX(CASE
             WHEN (
                 c.grade_c = "10th Grade"
                 AND college_track_status_c = '11A'
             ) THEN 1
             ELSE 0
-        END AS hs_EFC_10th_denom_count,
+        END) AS hs_EFC_10th_denom_count,
         --11th Grade Aspirations, any Aspiration
-        CASE
+        MAX(CASE
             WHEN (
                 c.grade_c = '11th Grade'
                 AND college_track_status_c = '11A'
                 AND a.id IS NOT NULL
             ) THEN 1
             ELSE 0
-        END AS aspirations_any_count,
+        END) AS aspirations_any_count,
         --11th Grade Aspirations, Affordable colleges
-        CASE
+        MAX(CASE
             WHEN (
                 c.grade_c = '11th Grade'
                 AND fit_type_current_c IN ("Best Fit", "Good Fit", "Local Affordable")
             ) THEN 1
             ELSE 0
-        END AS aspirations_affordable_count,
+        END) AS aspirations_affordable_count,
         --11th Grade Aspirations reporting group        
-        CASE
+        MAX(CASE
             WHEN (
                 c.grade_c = '11th Grade'
                 AND college_track_status_c = '11A'
             ) THEN 1
             ELSE 0
-        END AS aspirations_denom_count
+        END) AS aspirations_denom_count
     FROM
         `data-warehouse-289815.salesforce_clean.contact_template` AS c
         LEFT JOIN `data-warehouse-289815.salesforce.college_aspiration_c` a ON c.contact_id = a.student_c
+        group by site_short, contact_id, full_name_c
