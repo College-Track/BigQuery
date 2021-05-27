6db9cc1ff89ab0cf44f5dba9ac1455d7e9668d44
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-27 14:39:55 -0700

    Auto-commit: Thu May 27 2021 14:39:51 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
@@ -0,0 +1,13 @@
+
+    SELECT
+    site_short AS survey_site_short,
+    COUNT(contact_id) AS ps_survey_scholarship_denom,
+    SUM(
+    CASE
+        WHEN i_am_able_to_receive_my_scholarship_funds_from_college_track IN ('StronglyAgree', 'Strongly Agree', 'Agree') THEN 1
+        ELSE 0
+    END) AS ps_survey_scholarship_num
+    
+    FROM  data-studio-260217.surveys.fy21_ps_survey_wide_prepped
+    WHERE i_am_able_to_receive_my_scholarship_funds_from_college_track IS NOT NULL
+    GROUP BY site_short
