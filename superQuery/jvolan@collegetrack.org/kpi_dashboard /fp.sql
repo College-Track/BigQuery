4cba6990feaf438a39d8be4856751a63e7550ea9
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-27 15:10:17 -0700

    Auto-commit: Thu May 27 2021 15:10:13 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
@@ -1,6 +1,4 @@
-WITH gather_survey_data AS
-(
-    SELECT
+SELECT
     site_short AS survey_site_short,
     contact_id AS ps_survey_scholarship_denom,
     CASE
@@ -10,36 +8,3 @@ WITH gather_survey_data AS
     
     FROM  `data-studio-260217.surveys.fy21_ps_survey_wide_prepped`
     WHERE i_am_able_to_receive_my_scholarship_funds_from_college_track IS NOT NULL
-),
-
-gather_contact_data AS
-(
-    SELECT
-    site_short,
-    CASE
-        WHEN fa_req_fafsa_c = 'Submitted' THEN 1
-        ELSE 0
-    END AS fp_12_fafsa_complete_num
-    FROM `data-warehouse-289815.salesforce_clean.contact_template`
-),
-
-join_data AS
-(
-    SELECT
-    site_short,
-    fp_12_fafsa_complete_num,
-    gather_survey_data.ps_survey_scholarship_denom AS ps_survey_scholarship_denom,
-    gather_survey_data.ps_survey_scholarship_num AS ps_survey_scholarship_num,
-     
-    FROM gather_contact_data
-    LEFT JOIN gather_survey_data ON gather_survey_data.survey_site_short = site_short
-)
-    SELECT
-    site_short,
-    SUM(fp_12_fafsa_complete_num) AS fp_12_fasfa_num,
-    COUNT(ps_survey_scholarship_denom) AS ps_survey_scholarship_denom,
-    SUM(ps_survey_scholarship_num) AS ps_survey_scholarship_num,
-    
-    FROM join_data
-    GROUP BY site_short
-     
