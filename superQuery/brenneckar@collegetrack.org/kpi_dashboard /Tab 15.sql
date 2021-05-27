a222ad411b876cfe036e6e33159776a13d4089f0
Baker Renneckar <baker@bakerr.com>
2021-05-27 16:22:29 -0700

    Auto-commit: Thu May 27 2021 16:22:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 15.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard /Tab 15.sql
@@ -1,27 +1,22 @@
--- SELECT
---   site_short,
---   Class_Attendance_Id,
---   Outcome_c
--- FROM
---   `data-warehouse-289815.salesforce_clean.class_template` CT
---   LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
--- WHERE
---   Outcome_c = 'Scheduled'
---   AND (
---     EXTRACT(
---       MONTH
---       FROM
---         date_c
---     ) < EXTRACT(
---       MONTH
---       FROM
---         CURRENT_DATE()
---     )
---   )
---   AND current_as_c = true
---   AND CT.is_deleted = false
-
-
-SELECT *
-FROM `data-warehouse-289815.salesforce.class_attendance_c``
-WHERE Id = 'a2z1M000000eAwXQAU'
+SELECT
+  site_short,
+  Class_Attendance_Id,
+  Outcome_c
+FROM
+  `data-warehouse-289815.salesforce_clean.class_template` CT
+  LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
+WHERE
+  Outcome_c = 'Scheduled'
+  AND (
+    EXTRACT(
+      MONTH
+      FROM
+        date_c
+    ) < EXTRACT(
+      MONTH
+      FROM
+        CURRENT_DATE()
+    )
+  )
+  AND current_as_c = true
+  AND CT.is_deleted = false
