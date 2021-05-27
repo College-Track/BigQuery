4872f38ac9450667de75d64d26f2f05903a198f9
Baker Renneckar <baker@bakerr.com>
2021-05-27 14:13:07 -0700

    Auto-commit: Thu May 27 2021 14:13:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/add_region_to_site_projections.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/add_region_to_site_projections.sql
@@ -1,4 +1,9 @@
-SELECT DISTINCT P.*,
+WITH gather_projection_data AS (SELECT DISTINCT P.*,
 R.region_abrev
 FROM `data-studio-260217.performance_mgt.fy22_projections` P
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` R ON R.site_short = P.site_short
+)
+
+SELECT site_short, COUNT(*)
+FROM gather_projection_data
+GROUP BY site_short
