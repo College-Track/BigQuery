5e414735a21a536034a5fee01f1275354f72ae04
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-27 15:19:42 -0700

    Auto-commit: Thu May 27 2021 15:19:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /student_life.sql
@@ -1,4 +1,4 @@
-  SELECT
+SELECT
         CAT.student_c,
         site_short,
         MAX(
@@ -17,10 +17,10 @@
     FROM
         `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
         LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` CT ON CAT.contact_id = CT.student_c
-    WHERE (CAT.global_academic_semester_c = 'a3646000000dMXhAAM'
-            AND student_audit_status_c = 'Current CT HS Student') 
-                       -- AND (CAT.AY_Name ="Summer 2019-20 (Semester)"
-                         --   AND student_audit_status_c = 'Current CT HS Student')
+    --WHERE (CAT.global_academic_semester_c = 'a3646000000dMXhAAM' --Spring 2019-20 (Semester)
+            --AND student_audit_status_c = 'Current CT HS Student') 
+                       WHERE (CAT.global_academic_semester_c = 'a3646000000dMXiAAM' --Summer 2019-20 (Semester)
+                          AND student_audit_status_c = 'Current CT HS Student')
     GROUP BY
         site_short,
         CAT.student_c
