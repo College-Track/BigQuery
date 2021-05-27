152dca3b1cbf908c7866e7425dab5d62398d1ff1
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-27 09:30:07 -0700

    Auto-commit: Thu May 27 2021 09:30:00 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 3.sql
@@ -0,0 +1,40 @@
+WITH get_at_data AS
+(
+    SELECT
+    AT_Id,
+    Contact_Id AS at_contact_id,
+    site_short AS at_site,
+--% of students completing FAFSA or equivalent
+    CASE    
+        WHEN filing_status_c = 'FS_G' THEN 1
+        ELSE 0
+    END AS indicator_fafsa_complete,
+--% of students on-track to have less than $30K loan debt
+    CASE
+        WHEN 
+        loans_c IN ('LN_G','LN_Y') THEN 1
+        ELSE 0
+    END AS indicator_loans_less_30k_loans,
+--% of students attaining a well-balanced lifestyle    
+    CASE
+        WHEN Overall_Rubric_Color = 'Green' THEN 1
+        ELSE 0
+    END AS indicator_well_balanced,
+--% of students understand that technical and interpersonal skills are needed to create opportunities now and in the future
+    CASE
+        WHEN advising_rubric_career_readiness_v_2_c = 'Green'
+        AND 
+        (academic_networking_50_cred_c = 'AN1_G'
+        OR academic_networking_over_50_credits_c = 'AN2_G') THEN 1
+        ELSE 0
+    END AS indicator_tech_interpersonal_skills,
+    CAST (CURRENT_DATE() AS DATE) AS determine_at_date,
+    FORMAT_DATE("%m", CURRENT_DATE()) AS report_month,
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE college_track_status_c = '15A'
+ )   
+    SELECT
+    *
+    FROM get_at_data
+   
