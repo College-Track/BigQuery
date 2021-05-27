2ae44a8b22dc745434f7efa1704f1fb4b6e37dd6
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-05-27 10:45:20 -0700

    Auto-commit: Thu May 27 2021 10:45:17 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/team_kpi_targets.sql
@@ -1,11 +1,11 @@
-/*
+
 CREATE OR REPLACE TABLE `data-warehouse-289815.performance_mgt.team_kpi_targets`
 OPTIONS
     (
     description="Filtered table to house CT Team KPIs and respective Targets"
     )
 AS
-*/
+
 SELECT 
 submission_id,
 select_your_team_function AS Team,
@@ -13,7 +13,7 @@ select_role AS Role,
 site_kpi AS Site,
 region_kpi AS Region,
 select_kpi AS KPI,
---enter_the_target_non_numeric_ AS KPI__target_str,
+enter_the_target_non_numeric_ AS KPI__target_str,
 enter_the_target_numeric_ AS KPI__target_num,
 enter_the_target_percent_ AS KPI__target_perc
 /*
