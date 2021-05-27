bb248e3f58add2f2cd9b30a5c7005912c94c3b3f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-27 15:03:42 -0700

    Auto-commit: Thu May 27 2021 15:03:38 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
@@ -2,16 +2,14 @@ WITH gather_survey_data AS
 (
     SELECT
     site_short AS survey_site_short,
-    COUNT(contact_id) AS ps_survey_scholarship_denom,
-    SUM(
+    contact_id AS ps_survey_scholarship_denom,
     CASE
         WHEN i_am_able_to_receive_my_scholarship_funds_from_college_track IN ('StronglyAgree', 'Strongly Agree', 'Agree') THEN 1
         ELSE 0
-    END) AS ps_survey_scholarship_num
+    END AS ps_survey_scholarship_num
     
-    FROM  data-studio-260217.surveys.fy21_ps_survey_wide_prepped
+    FROM  `data-studio-260217.surveys.fy21_ps_survey_wide_prepped`
     WHERE i_am_able_to_receive_my_scholarship_funds_from_college_track IS NOT NULL
-    GROUP BY site_short
 ),
 
 gather_contact_data AS
@@ -34,3 +32,5 @@ gather_contact_data AS
      
     FROM gather_contact_data
     LEFT JOIN gather_survey_data ON gather_survey_data.survey_site_short = site_short
+
+
