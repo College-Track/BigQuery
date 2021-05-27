fd542d9190a7281675ebe0b7333980df105994a4
Baker Renneckar <baker@bakerr.com>
2021-05-27 14:33:33 -0700

    Auto-commit: Thu May 27 2021 14:33:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/created_expanded_role_kpi_selection.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/created_expanded_role_kpi_selection.sql
@@ -5,8 +5,25 @@ CREATE TEMP TABLE mature_region_sites
 );
 
 INSERT INTO mature_region_sites
-VALUES ('Mature Site Staff', 'Aurora'),
-('Mature Site Staff', 'Denver')
+VALUES 
+('Mature Site Staff', 'Aurora'),
+('Mature Site Staff', 'Denver'),
+('Mature Site Staff', 'Boyle Heights'),
+('Mature Site Staff', 'East Palo Alto'),
+('Mature Site Staff', 'New Orleans'),
+('Mature Site Staff', 'Sacramento'),
+('Mature Site Staff', 'San Francisco'),
+('Mature Site Staff', 'Watts'),
+('Non-Mature Site Staff', 'Crenshaw'),
+('Non-Mature Site Staff', 'The Durant Center'),
+('Non-Mature Site Staff', 'Ward 8'),
+('Non-Mature Region Staff', 'DC'),
+('Mature Region Staff', 'LA'),
+('Mature Region Staff', 'NOLA'),
+('Mature Region Staff', 'Bay Area'),
+('Mature Region Staff', 'Sacramento')
+
+
  ;
 
 
