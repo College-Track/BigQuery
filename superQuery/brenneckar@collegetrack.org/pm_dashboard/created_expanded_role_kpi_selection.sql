ec262b63467ce0dd4da62f74eaf472f75b49c002
Baker Renneckar <baker@bakerr.com>
2021-05-27 14:41:29 -0700

    Auto-commit: Thu May 27 2021 14:41:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/created_expanded_role_kpi_selection.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/created_expanded_role_kpi_selection.sql
@@ -31,4 +31,3 @@ VALUES
 SELECT R.*, RSC.site_or_region
 FROM `data-studio-260217.performance_mgt.role_kpi_selection` R
 LEFT JOIN region_sites_classification RSC ON RSC.function = R.function
-WHERE R.function = 'Non-Mature Regional Staff'
