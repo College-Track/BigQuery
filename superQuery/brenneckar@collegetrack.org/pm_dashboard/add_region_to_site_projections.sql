7353a16ba8e77bff20ddc54475a88b683435a2b9
Baker Renneckar <baker@bakerr.com>
2021-05-27 14:16:31 -0700

    Auto-commit: Thu May 27 2021 14:16:30 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/add_region_to_site_projections.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/add_region_to_site_projections.sql
@@ -1,6 +1,11 @@
 WITH gather_projection_data AS (
 SELECT *,
 CASE WHEN site_short IN ('Aurora', 'Denver') THEN "CO"
+WHEN site_short IN ('Watts', "Boyle Heights", 'Crenshaw') THEN "LA"
+WHEN site_short IN ('New Orleans') THEN "NOLA"
+WHEN site_short IN ("The Durant Center", "Ward 8") THEN "DC"
+WHEN site_short IN ('San Francisco', "Oakland", "East Palo Alto", 'Sacramento') THEN "NOR CAL"
+ELSE NULL
 END AS region_abrev
 
 FROM `data-studio-260217.performance_mgt.fy22_projections` P
