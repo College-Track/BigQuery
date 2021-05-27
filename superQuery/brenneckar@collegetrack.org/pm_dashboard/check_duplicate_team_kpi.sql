9ccac8390bfb5c95762723ebec3280f8f03a0306
Baker Renneckar <baker@bakerr.com>
2021-05-27 09:50:12 -0700

    Auto-commit: Thu May 27 2021 09:50:12 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/check_duplicate_team_kpi.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/check_duplicate_team_kpi.sql
@@ -1,2 +1,3 @@
-SELECT team_kpi, region_kpi, site_kpi, select_kpi
+SELECT team_kpi, region_kpi, site_kpi, select_kpi, COUNT(*)
 FROM `data-warehouse-289815.google_sheets.team_kpi_target`
+GROUP BY team_kpi, region_kpi, site_kpi, select_kpi
