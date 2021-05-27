2621a1a0b335ca7b47cada0e5559df8c2476e527
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-27 10:22:36 -0700

    Auto-commit: Thu May 27 2021 10:22:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /gather_all_team_data.sql
@@ -15,7 +15,8 @@ join_team_kpis AS (
     OM.* EXCEPT(site_short),
     RED.* EXCEPT(site_short),
     PRO_OPS.* EXCEPT(site_short),
-    OM_ATTEND.* EXCEPT(site_short)
+    OM_ATTEND.* EXCEPT(site_short),
+    WLLNSS.* EXCEPT(site_short)
   FROM
     join_prep JP
     LEFT JOIN `data-studio-260217.kpi_dashboard.academic_affairs` AA ON AA.site_short = JP.site_short
@@ -27,6 +28,7 @@ join_team_kpis AS (
     LEFT JOIN `data-studio-260217.kpi_dashboard.red`RED ON RED.site_short = JP.site_short
     LEFT JOIN `data-studio-260217.kpi_dashboard.program_ops` PRO_OPS ON PRO_OPS.site_short = JP.site_short
     LEFT JOIN `data-studio-260217.kpi_dashboard.om_incomplete_attendance` OM_ATTEND ON OM_ATTEND.site_short = JP.site_short
+    LEFT JOIN `data-studio-260217.kpi_dashboard.wellness` WLLNSS ON WLLNSS.site_short = JP.site_short
 
 )
 SELECT
