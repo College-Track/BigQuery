ec6458e3b0e071bab39e0fa497b823f241291364
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-05-27 15:41:52 -0700

    Auto-commit: Thu May 27 2021 15:41:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /fp.sql
@@ -29,7 +29,7 @@ gather_contact_data AS
     END) AS fp_12_fafsa_complete_num
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE college_track_status_c = '11A'
-    AND (grade_c = "12th Grade" OR (grade_c='Year 1' AND years_since_hs_grad_c = 0))
+    AND (grade_c = "12th Grade" OR (grade_c='Year 1' AND indicator_years_since_hs_graduation_c = 0))
     GROUP BY site_short
 ),
 
