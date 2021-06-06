8ada44f97e075081748747fb4ac8d819ad0cd705
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-05 18:23:44 -0700

    Auto-commit: Sat Jun 05 2021 18:23:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,11 +1,11 @@
 SELECT
-        CAT.student_c,
-        full_name_c,
-        CASE
-        WHEN co_vitality_scorecard_color_c IN ('Blue','Red')
-        THEN 1
-        ELSE 0
-    END AS wellness_blue_red_num,
+        --CAT.student_c,
+        --full_name_c,
+        --CASE
+        --WHEN co_vitality_scorecard_color_c IN ('Blue','Red')
+        --THEN 1
+        --ELSE 0
+    --END AS wellness_blue_red_num,
     SUM(Attendance_Numerator_c) AS attended_wellness_sessions,#attended sessions from AT
         site_short
     FROM
@@ -24,7 +24,7 @@ SELECT
         AND Outcome_c != 'Cancelled'
         AND college_track_status_c = '11A'
     GROUP BY
-            full_name_c,
-            CAT.STUDENT_C, 
-            co_vitality_scorecard_color_c,
+            --full_name_c,
+            --CAT.STUDENT_C, 
+            --co_vitality_scorecard_color_c,
             site_short
