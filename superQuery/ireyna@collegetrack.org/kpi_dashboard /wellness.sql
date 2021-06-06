1909adb9806a1da9aa8ca08ecde6e53acc679c5d
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-05 17:35:12 -0700

    Auto-commit: Sat Jun 05 2021 17:35:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,34 +1,3 @@
-gather_wellness_survey_data AS (
-SELECT
-    CT.site_short,
-    S.contact_id AS students_receiving_wellness_services,
-    
-    CASE
-        WHEN (
-            working_with_college_track_wellness_services_has_assisted_you_in_managing_your_s IN ("Strongly Agree", "Totalmente de acuerdo")
-            OR working_with_college_tracks_wellness_programming_has_helped_you_engage_in_self_c IN ("Strongly Agree", "Totalmente de acuerdo")
-            OR working_with_college_tracks_wellness_services_has_enhanced_your_mental_health IN ("Strongly Agree", "Totalmente de acuerdo")
-            )
-        THEN 1
-        ELSE 0
-        END AS strongly_agree_wellness_services_assisted_them
-        
-FROM `data-studio-260217.surveys.fy21_hs_survey` S
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
-WHERE did_you_engage_with_wellness_services_at_your_site = "Yes"
-),
-
-aggregate_wellness_survey_data AS (
-SELECT 
-    COUNT (DISTINCT students_receiving_wellness_services) AS wellness_survey_wellness_services_assisted_denom,
-    SUM(strongly_agree_wellness_services_assisted_them) AS wellness_survey_wellness_services_assisted_num,
-    site_short
-FROM gather_wellness_survey_data 
-GROUP BY site_short
-),
-
-#For KPI on average # of sessions for reb/blue Covi students 
-#kpi: % of students/# of sessions/amt of time that students with red and blue CoVi scores have spent receiving support/counseling/coaching for their social emotional wellbeing health (either through a workshop, small group or 1:1s)
 with gather_wellness_attendance_data AS (
     SELECT
         CAT.student_c,
