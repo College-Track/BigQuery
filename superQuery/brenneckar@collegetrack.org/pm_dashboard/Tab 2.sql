d3a84007d4dd2fdfb484ab374ff94f9b8aec5727
Baker Renneckar <baker@bakerr.com>
2021-06-07 13:38:00 -0700

    Auto-commit: Mon Jun 07 2021 13:38:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
@@ -1,6 +1,5 @@
 SELECT
-  *
+  site_short
 FROM
   `data-studio-260217.performance_mgt.fy22_projections`
-  WHERE grade = '10th Grade'
-  AND site_short = 'The Durant Center'
+  WHERE grade = 'Year 6'
