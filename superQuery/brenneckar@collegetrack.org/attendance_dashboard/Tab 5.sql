7a66bca78b0754588c223fe8103671d89399768c
Baker Renneckar <baker@bakerr.com>
2021-06-07 15:56:27 -0700

    Auto-commit: Mon Jun 07 2021 15:56:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,9 +1,9 @@
--- SELECT workshop_display_name_c, dosage_types_c,Workshop_Global_Academic_Semester_c, dosage_split
--- FROM `data-studio-260217.attendance_dashboard.attendance_filtered_data`
--- WHERE workshop_display_name_c LIKE '%Summer Bridge%'
+SELECT workshop_display_name_c, dosage_types_c,Workshop_Global_Academic_Semester_c, dosage_split
+FROM `data-studio-260217.attendance_dashboard.attendance_filtered_data`
+WHERE  Workshop_Display_Name_c = 'CC: Virtual Regional Summer Bridge CCCT'
 
 
-SELECT Class_c, Workshop_Display_Name_c
-FROM `data-warehouse-289815.salesforce_clean.class_template` 
-WHERE Workshop_Display_Name_c LIKE '%Summer Bridge%'
-AND Workshop_Display_Name_c = 'CC: Virtual Regional Summer Bridge CCCT'
+-- SELECT Class_c, Workshop_Display_Name_c
+-- FROM `data-warehouse-289815.salesforce_clean.class_template` 
+-- WHERE Workshop_Display_Name_c LIKE '%Summer Bridge%'
+-- AND Workshop_Display_Name_c = 'CC: Virtual Regional Summer Bridge CCCT'
