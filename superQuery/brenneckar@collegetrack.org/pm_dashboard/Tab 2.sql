58058901d683914b6ab98b5ea34bd3351e73fa67
Baker Renneckar <baker@bakerr.com>
2021-06-07 13:55:02 -0700

    Auto-commit: Mon Jun 07 2021 13:55:30 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
@@ -1,6 +1,2 @@
-SELECT
-  grade_c, COUNT(*)
-FROM
-  `data-warehouse-289815.salesforce_clean.contact_template`
-  WHERE site_short IN ('Watts', 'Sacramento', 'Denver')
-  GROUP BY grade_c
+SELECT *
+FROM `data-studio-260217.performance_mgt.fy22_projections`
