f5c95627a2eed4b0837f0851ec2882f589524494
Baker Renneckar <baker@bakerr.com>
2021-06-07 12:27:54 -0700

    Auto-commit: Mon Jun 07 2021 12:28:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/baker_team_kpis.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/baker_team_kpis.sql
@@ -1,3 +1,46 @@
+WITH prep_kpi_targets AS (
+  SELECT
+    team_kpi,
+    region_kpi,
+    site_kpi,
+    select_role,
+    select_kpi,
+    what_is_the_type_of_target_,
+    CASE
+      WHEN KPI_Target.select_role IS NOT NULL THEN true
+      ELSE false
+    END AS target_submitted,
+    CASE
+      WHEN enter_the_target_numeric_ IS NOT NULL THEN enter_the_target_numeric_
+      WHEN enter_the_target_percent_ iS NOT NULL THEN enter_the_target_percent_
+      WHEN what_is_the_type_of_target_ = "Goal is met" THEN 1 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_
+      ELSE NULL
+    END AS target_fy22,
+  FROM
+    `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target
+    -- WHERE email_kpi != 'test@collegetrack.org'-- `data-studio-260217.performance_mgt.expanded_role_kpi_selection` KPI_Selection --List of KPIs by Team/Role
+    -- LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` KPI_Target --ON KPI_Target.team_kpi = REPLACE(KPI_Selection.function, ' ', '_')  #FormAssembly
+    -- ON KPI_Target.select_role = KPI_Selection.role
+    -- AND KPI_Target.select_kpi = KPI_Selection.kpis_by_role
+    -- AND KPI_Target.site_kpi = KPI_Selection.site_or_region
+    -- -- LEFT JOIN `data-warehouse-289815.performance_mgt.fy22_roles_to_kpi` as c
+    -- ON c.kpi = KPI_Selection.kpis_by_role
+    -- AND c.role = KPI_Selection.role
+),
+
+prep_student_type_projections AS (
+SELECT site_short, 
+region_abrev,
+student_type,
+SUM(student_count) AS student_count,
+
+FROM `data-studio-260217.performance_mgt.fy22_projections`
+GROUP BY site_short, 
+region_abrev,
+student_type
+
+),
+prep_grade_projections AS (
 SELECT region_abrev, site_short,
 grade AS student_type,
 SUM(student_count) AS student_count
@@ -5,3 +48,59 @@ FROM `data-studio-260217.performance_mgt.fy22_projections`
 GROUP BY site_short, 
 region_abrev,
 grade
+),
+
+join_projections AS (
+SELECT *
+FROM prep_student_type_projections
+UNION ALL
+SELECT *
+FROM prep_grade_projections
+
+),
+
+prep_non_program_kpis AS (
+  SELECT
+    *
+  FROM
+    `data-studio-260217.performance_mgt.expanded_role_kpi_selection` KPI_by_role
+    LEFT JOIN prep_kpi_targets Non_Program_Targets ON KPI_by_role.role = Non_Program_Targets.select_role
+    AND KPI_by_role.kpis_by_role = Non_Program_Targets.select_kpi
+  WHERE
+    KPI_by_role.function NOT IN (
+      'Non-Mature Site Staff',
+      'Mature Site Staff',
+      'Mature Regional Staff',
+      'Non-Mature Regional Staff'
+    )
+),
+prep_regional_kpis AS (
+  SELECT
+    *
+  FROM
+    `data-studio-260217.performance_mgt.expanded_role_kpi_selection` KPI_by_role
+    LEFT JOIN prep_kpi_targets Non_Program_Targets ON KPI_by_role.role = Non_Program_Targets.select_role
+    AND KPI_by_role.kpis_by_role = Non_Program_Targets.select_kpi
+    AND KPI_by_role.site_or_region = Non_Program_Targets.region_kpi
+  WHERE
+    KPI_by_role.function IN (
+      'Mature Regional Staff',
+      'Non-Mature Regional Staff'
+    )
+),
+prep_site_kpis AS (
+  SELECT
+    KPI_by_role.*,
+    Projections.student_count
+  FROM
+    `data-studio-260217.performance_mgt.expanded_role_kpi_selection` KPI_by_role
+    LEFT JOIN prep_kpi_targets Non_Program_Targets ON KPI_by_role.role = Non_Program_Targets.select_role
+    AND KPI_by_role.kpis_by_role = Non_Program_Targets.select_kpi
+    AND KPI_by_role.site_or_region = Non_Program_Targets.site_kpi
+    LEFT JOIN join_projections Projections ON Projections.site_short = KPI_by_role.site_or_region AND Projections.student_type = KPI_by_role.student_group
+  WHERE
+    KPI_by_role.function IN ('Mature Site Staff', 'Non-Mature Site Staff')
+)
+
+SELECT *
+FROM prep_site_kpis
