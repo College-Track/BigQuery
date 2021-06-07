4996e04e1a8d012551edcc32f1f07da3bcef01eb
Baker Renneckar <baker@bakerr.com>
2021-06-07 13:25:17 -0700

    Auto-commit: Mon Jun 07 2021 13:25:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
@@ -2,5 +2,5 @@ SELECT
   site_or_region, student_group, COUNT(*)
 FROM
   `data-studio-260217.performance_mgt.expanded_role_kpi_selection`
-  GROUP BY site_or_region, student_group
   WHERE site_or_region IS NOT NULL
+  GROUP BY site_or_region, student_group
