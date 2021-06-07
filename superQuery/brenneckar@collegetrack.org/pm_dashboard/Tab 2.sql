d9259ba2c1f6bb96d0c7edff652f9432d5e47614
Baker Renneckar <baker@bakerr.com>
2021-06-07 13:25:10 -0700

    Auto-commit: Mon Jun 07 2021 13:25:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
@@ -1,7 +1,6 @@
 SELECT
-  site_short, COUNT(*)
+  site_or_region, student_group, COUNT(*)
 FROM
-  `data-studio-260217.performance_mgt.fy22_projections`
-  WHERE grade = '10th Grade'
-  GROUP BY site_short
-  ORDER BY site_short
+  `data-studio-260217.performance_mgt.expanded_role_kpi_selection`
+  GROUP BY site_or_region, student_group
+  WHERE site_or_region IS NOT NULL
