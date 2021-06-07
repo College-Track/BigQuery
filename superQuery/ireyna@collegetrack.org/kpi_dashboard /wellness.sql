341853fb3416afab1bb7a1467668e323f900b0d5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-07 13:55:55 -0700

    Auto-commit: Mon Jun 07 2021 13:55:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -51,8 +51,7 @@ WHERE COVI.record_type_id ='0121M000001cmuDQAQ' --Covitality test record type
 ),
 
 --Setting groundwork for KPI indicator: % of students who have taken the the CoVi assessment each academic year (2020-21AY)
---Data gathered through 2 CTEs. Denom should be Current CT HS Student
---Wondering if this should be 2019-20AY instead?
+--Gathered in 2 CTEs
 completing_covi_data AS (
 SELECT 
     site_short,
@@ -82,7 +81,7 @@ GROUP BY
     site_short
 ),
 
--- % of students served by Wellness who "strongly agree" wellness services assisted them in managing their stress, helping them engage in self-care practices and/or enhancing their mental health
+-- KPI: % of students served by Wellness who "strongly agree" wellness services assisted them in managing their stress, helping them engage in self-care practices and/or enhancing their mental health
 -- The denominator for this metric will be students who answered "yes" to receiving wellness services
 gather_wellness_survey_data AS (
 SELECT
@@ -113,7 +112,13 @@ FROM gather_wellness_survey_data
 GROUP BY site_short
 ),
 
-gather_wellness_attendance_reporting_group AS ( #red/blue covi students
+
+--Average # of sessions for reb/blue Covi students or 1:1 (case notes)
+--% of students/# of sessions/amt of time that students with red and blue CoVi scores have spent receiving support/counseling/coaching for their social emotional wellbeing health (either through a workshop, small group or 1:1s)
+--Broken down into various CTEs to gather students with red/blue covi, wellness sessions attended, case notes
+
+--Pulling students with red/blue covi from academic term (2020-21AY)
+gather_red_blue_covi_at AS ( 
 SELECT
         student_c,
         site_short,
@@ -131,9 +136,8 @@ GROUP BY
     site_short
 ),
 
-#For KPI on average # of sessions for reb/blue Covi students 
-#kpi: % of students/# of sessions/amt of time that students with red and blue CoVi scores have spent receiving support/counseling/coaching for their social emotional wellbeing health (either through a workshop, small group or 1:1s)
-with gather_wellness_attendance_data AS ( --prep for SUM of sessions to validate data
+ --prep for SUM of sessions to validate data
+gather_wellness_attendance_data AS (
 SELECT
     SUM(attendance_numerator_c) AS attended_wellness_sessions,
     site_short
@@ -150,7 +154,9 @@ SELECT
     GROUP BY
             site_short
 ),
-prep_attendance_data_for_avg AS (
+
+--Aggregate attended Wellness sessions by student, by site to obtain average sessions in a different CTE
+sum_attendance_data_for_avg AS (
 SELECT
     ct.student_c,
     SUM(attendance_numerator_c) AS sum_attended_wellness_by_student,
@@ -169,47 +175,61 @@ GROUP BY
     ct.student_c,
     site_short
 ),
-#gather case load data
-gather_case_note_data AS (
+
+--Sum case note data for red/blue covi as receiving 1:1 support by student, by site. To use for average case notes
+--1 casenote = 1 session
+sum_case_note_data_for_avg AS (
 SELECT 
-    CASE
+    CAT.student_c,
+    SUM(CASE
         WHEN id IS NOT NULL 
         THEN 1
         ELSE 0
-    END AS wellness_case_note_2020_21,
+    END) AS sum_wellness_case_note_2020_21, #wellness casenotes from 2020-21
     id AS case_note_id, #case note id
     site_short
     
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
-LEFT JOIN `data-warehouse-289815.salesforce.progress_note_c`CSE  ON CAT.AT_Id = CSE.Academic_Semester_c
+LEFT JOIN `data-warehouse-289815.salesforce.progress_note_c` CSE ON CAT.AT_Id = CSE.Academic_Semester_c
 WHERE Type_Counseling_c = TRUE
     AND AY_name = 'AY 2020-21'
 GROUP BY
     site_short,
+    student_c,
     id
 ),
-/*
-aggregate_wellness_session_data AS(
-SELECT
-    SUM(wellness_blue_red_num) AS wellness_blue_red_num,
-    AVG(attended_wellness_sessions) AS avg_attended_sessions, #workshop sessions
-    site_short
-FROM gather_wellness_attendance_data
+
+--Add 1:1 case notes and sessions attended by student to average out later
+combine_session_case_notes AS (
+SELECT 
+    ATTNDCE.student_c,
+    ATTNDCE.site_short,
+    SUM(sum_attended_wellness_by_student + sum_wellness_case_note_2020_21) AS sum_wellness_support_received
+    
+FROM sum_attendance_data_for_avg AS ATTNDCE
+LEFT JOIN sum_case_note_data_for_avg AS CSE ON ATTNDCE.site_short = CSE.site_short
 GROUP BY 
+    student_c,
     site_short
-)*/
-aggregate_kpis_data AS(
-SELECT
-    SUM(wellness_case_note_2020_21) AS wellness_case_notes, #wellness casenotes from 2020-21
-    --SUM(wellness_blue_red_num) AS wellness_blue_red_num,
-    AVG(sum_attended_wellness_by_student) AS avg_attended_sessions, #workshop session
-    attended_wellness_sessions,
-    a.site_short
-FROM gather_case_note_data as b
-left join gather_wellness_attendance_data as a on a.site_short=b.site_short
-LEFT JOIN prep_attendance_data_for_avg as c on a.site_short=c.site_short
-GROUP BY  a.site_short,attended_wellness_sessions
+
 )
+--Aggregate all KPIs - sessions and casenotes only for now
+--aggregate_kpis_data AS(
+SELECT
+    sum_wellness_case_note_2020_21,
+    sum_wellness_support_received,
+    sum_attended_wellness_by_student,
+    a.site_short
+FROM combine_session_case_notes AS a
+LEFT JOIN sum_attendance_data_for_avg AS b on a.site_short=b.site_short
+LEFT JOIN sum_case_note_data_for_avg as c on a.site_short=c.site_short
+GROUP BY  
+    a.site_short,
+    sum_wellness_case_note_2020_21,
+    sum_wellness_support_received,
+    sum_attended_wellness_by_student
+    
+/*)
 select *
 FROM aggregate_kpis_data 
 --Growth KPIs and students completing Covitality KPI
@@ -229,3 +249,4 @@ ON A.site_short = B.site_short
 SELECT 
     *
 FROM prep_all_wellness_kpis 
+*/
