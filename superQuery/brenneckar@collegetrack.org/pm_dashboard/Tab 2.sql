c071213222762c374fe79043a8e2e178ab8f729b
Baker Renneckar <baker@bakerr.com>
2021-06-07 13:29:47 -0700

    Auto-commit: Mon Jun 07 2021 13:30:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/Tab 2.sql
@@ -1,7 +1,6 @@
 SELECT
-  site_or_region, student_group, COUNT(*)
+  *
 FROM
-  `data-studio-260217.performance_mgt.expanded_role_kpi_selection`
-  WHERE site_or_region IS NOT NULL
-  GROUP BY site_or_region, student_group
-  ORDER BY site_or_region, student_group
+  `data-studio-260217.performance_mgt.fy22_projections`
+  WHERE grade = '10th Grade'
+  AND site_short = 'The Durant Center'
