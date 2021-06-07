a7d73141046b919c7eb09aaa8f43e9a346ad1262
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-07 10:03:30 -0700

    Auto-commit: Mon Jun 07 2021 10:03:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -66,14 +66,6 @@ left join gather_wellness_attendance_data as a on a.site_short=b.site_short
 GROUP BY 
     a.site_short
 )
-
---Growth KPIs and students completing Covitality KPI
---prep_all_wellness_kpis AS (
-SELECT
-    A.site_short,
-    wellness_covi_assessment_completed_ay,
-    --wellness_survey_wellness_services_assisted_num,
-   -- wellness_survey_wellness_services_assisted_denom,
-    attended_wellness_sessions AS wellness_sum_attended_sessions,
-    
-FROM  students_that_completed_covi AS A
+select a.*, attended_wellness_Sessions
+FROM aggregate_kpis_data as a
+left join gather_wellness_attendance_data as b on a.site_short = b.site_short
