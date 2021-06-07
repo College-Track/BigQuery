905cbd40d27cdab83ba1c0685d948f95394642f3
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-07 10:10:48 -0700

    Auto-commit: Mon Jun 07 2021 10:10:41 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,3 +1,4 @@
+with gather_wellness_attendance_data AS (
 SELECT
     --SUM(Attendance_Numerator_c) AS attended_wellness_sessions,#attended sessions from AT
     attendance_numerator_c AS attended_wellness_sessions,
