c2d341e6111317235fd84b7ed2b3358fc417484d
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-07 11:12:09 -0700

    Auto-commit: Mon Jun 07 2021 11:12:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -5,13 +5,7 @@ SELECT
             THEN 1
             ELSE 0
         END) AS wellness_blue_red_num
-FROM `data-warehouse-289815.salesforce_clean.class_template` CT
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
-WHERE Attendance_Numerator_c > 0
-    AND department_c = 'Wellness'
-    AND dosage_types_c NOT LIKE '%NSO%'
-    AND AY_NAME = "AY 2020-21"
-    AND grade_c != '8th Grade'
-    AND Outcome_c != 'Cancelled'
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` 
+WHERE grade_c != '8th Grade'
     AND college_track_status_c = '11A'
 GROUP BY ct.student_c
