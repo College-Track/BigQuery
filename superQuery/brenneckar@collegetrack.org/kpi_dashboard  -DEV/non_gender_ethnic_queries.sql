552ba371736db0b05978a5a0452dbdc0ef9841b2
Baker Renneckar <baker@bakerr.com>
2021-06-07 11:39:05 -0700

    Auto-commit: Mon Jun 07 2021 11:39:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard  -DEV/non_gender_ethnic_queries.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard  -DEV/non_gender_ethnic_queries.sql
@@ -6,6 +6,7 @@ WITH gather_hs_metrics  AS (
   FROM
     `data-warehouse-289815.salesforce_clean.contact_template` C
     LEFT JOIN `data-warehouse-289815.salesforce.account` Account ON Account.Id = C.site_c
+    WHERE college_track_status_c = "11A"
   GROUP BY
     site_short
 )
