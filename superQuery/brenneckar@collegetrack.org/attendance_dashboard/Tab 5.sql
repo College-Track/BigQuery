1f5d1a6bbffb077f2929da361dc53260110ec6ed
Baker Renneckar <baker@bakerr.com>
2021-06-07 15:46:47 -0700

    Auto-commit: Mon Jun 07 2021 15:47:16 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
 SELECT workshop_display_name_c, dosage_types_c,Workshop_Global_Academic_Semester_c, dosage_split
 FROM `data-studio-260217.attendance_dashboard.attendance_filtered_data`
-WHERE REGEXP_CONTAINS(workshop_display_name_c, 'Summer')
+WHERE REGEXP_CONTAINS(workshop_display_name_c, 'Summer B')
