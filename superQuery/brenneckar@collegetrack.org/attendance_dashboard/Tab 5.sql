d685effc80e833ab969589ada75ce7f16f7b38f2
Baker Renneckar <baker@bakerr.com>
2021-06-07 15:44:50 -0700

    Auto-commit: Mon Jun 07 2021 15:45:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
+++ superQuery/brenneckar@collegetrack.org/attendance_dashboard/Tab 5.sql
@@ -1,3 +1,3 @@
-SELECT workshop_display_name_c
+SELECT workshop_display_name_c, dosage_types_c
 FROM `data-studio-260217.attendance_dashboard.attendance_filtered_data`
 WHERE REGEXP_CONTAINS(workshop_display_name_c, 'Summer Bridge')
