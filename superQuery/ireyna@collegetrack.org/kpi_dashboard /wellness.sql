4f2ee68b8c7bfe6b404cd8f93cdb126bd532e1f8
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-07 11:15:50 -0700

    Auto-commit: Mon Jun 07 2021 11:15:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,5 +1,6 @@
 SELECT
         student_c,
+        site_short,
         SUM(CASE
             WHEN co_vitality_scorecard_color_c IN ('Blue','Red')
             THEN 1
@@ -8,4 +9,6 @@ SELECT
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` 
 WHERE grade_c != '8th Grade'
     AND college_track_status_c = '11A'
-GROUP BY student_c
+GROUP BY 
+    student_c
+    site_short
