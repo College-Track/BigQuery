663764612d741980b9d4af7be9d5400c7beb60b1
Baker Renneckar <baker@bakerr.com>
2021-06-07 12:21:15 -0700

    Auto-commit: Mon Jun 07 2021 12:21:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/pm_dashboard/baker_team_kpis.sql
+++ superQuery/brenneckar@collegetrack.org/pm_dashboard/baker_team_kpis.sql
@@ -1,2 +1,9 @@
-SELECT *
+SELECT site_short, 
+region_abrev,
+student_type,
+SUM(student_count) AS student_count,
+
 FROM `data-studio-260217.performance_mgt.fy22_projections`
+GROUP BY site_short, 
+region_abrev,
+student_type
