24ec1aff9ef2a7da0c9087651b6e4c4ad806081a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-07 10:16:19 -0700

    Auto-commit: Mon Jun 07 2021 10:16:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,6 +1,6 @@
 SELECT
     ct.student_c,
-    attendance_numerator_c AS attended_wellness_sessions,
+    SUM(attendance_numerator_c) AS sum_attended_wellness_by_student,
     site_short
 FROM `data-warehouse-289815.salesforce_clean.class_template` CT
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
@@ -12,3 +12,6 @@ WHERE
         AND grade_c != '8th Grade'
         AND Outcome_c != 'Cancelled'
         AND college_track_status_c = '11A'
+GROUP BY
+    ct.student_c,
+    site_short
