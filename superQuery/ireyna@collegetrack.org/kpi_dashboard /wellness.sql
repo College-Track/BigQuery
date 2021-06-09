3e35c304df3df1e6b4981749dd3022dab409fbe9
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 15:33:00 -0700

    Auto-commit: Wed Jun 09 2021 15:32:54 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,16 +1,17 @@
 SELECT
-    SUM(attendance_numerator_c) AS sum_attended_wellness_sessions,
-    site_short,academic_semester_c
-    FROM `data-warehouse-289815.salesforce_clean.class_template` CT
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
-    WHERE
-        Attendance_Numerator_c > 0
-        AND department_c = 'Wellness'
-        AND dosage_types_c NOT LIKE '%NSO%'
-        AND AY_NAME = "AY 2020-21"
-        AND grade_c != '8th Grade'
-        AND Outcome_c != 'Cancelled'
-        AND college_track_status_c = '11A'
-        AND co_vitality_scorecard_color_c IN ('Blue','Red')
-    GROUP BY
-            site_short, academic_semester_c
+        student_c,
+        site_short,
+        MAX(CASE
+            WHEN co_vitality_scorecard_color_c IN ('Blue','Red')
+            THEN 1
+            ELSE NULL
+        END) AS wellness_blue_red_denom
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` 
+WHERE grade_c != '8th Grade'
+    AND college_track_status_c = '11A'
+    AND AY_NAME = "AY 2020-21"
+    AND Term_c = "Fall"
+GROUP BY 
+    student_c,
+    site_short,
+    co_vitality_scorecard_color_c
