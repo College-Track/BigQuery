39d28b8b32e152507e2346ca6168ecc188257dd2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 16:46:50 -0700

    Auto-commit: Wed Jun 09 2021 16:46:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,2 +1,2 @@
 SELECT contact_id FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` WHERE AY_NAME = "AY 2020-21"
-    AND Term_c = "Fall"AND co_vitality_scorecard_color_c IN ('Blue','Red')
+    AND Term_c = "Fall"AND co_vitality_scorecard_color_c IN ('Blue','Red') AND college_track_status_c = '11A
