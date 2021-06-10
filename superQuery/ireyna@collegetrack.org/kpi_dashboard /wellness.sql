82fb37a31720cdcfe1e99e0ef2b9533576d2f295
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 17:08:09 -0700

    Auto-commit: Wed Jun 09 2021 17:08:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -8,7 +8,8 @@ SELECT
         Attendance_Numerator_c > 0
         AND department_c = 'Wellness'
         AND dosage_types_c NOT LIKE '%NSO%'
-        AND academic_semester_c IN (SELECT id FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` WHERE AY_Name = "AY 2020-21")
+        AND academic_semester_c IN (SELECT at_id FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` WHERE AY_Name = "AY 2020-21")
         AND Outcome_c != 'Cancelled'
     GROUP BY
             site_short, student_c
+),
