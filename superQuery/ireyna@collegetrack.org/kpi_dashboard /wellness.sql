f33e8b3bdf994e8b6913e20f824d4f88add48cd5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 17:52:15 -0700

    Auto-commit: Wed Jun 09 2021 17:52:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,3 +1,4 @@
+with gather_red_blue_covi_at AS ( 
 SELECT
         student_c,
         site_short,
@@ -18,3 +19,30 @@ GROUP BY
     student_c,
     site_short,
     AY_Name
+),
+
+--Sum students that have a red or blue covitality color at some point during 2020-21AY
+sum_of_blue_red_covi AS (
+SELECT
+        site_short,
+        SUM(wellness_blue_red_denom) AS sum_of_blue_red_covi_for_avg #students with blue/red Covitality scorecard colors for denominator
+FROM gather_red_blue_covi_at
+GROUP BY site_short
+)
+
+--gather Wellness sessions attended during 2020-21
+--gather_wellness_attendance_data AS (
+SELECT
+    SUM(attendance_numerator_c) AS sum_attended_wellness_sessions,
+    site_short
+   
+    FROM gather_red_blue_covi_at AS RB
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` AS CT ON RB.site_short = CT.site_short
+    WHERE
+        Attendance_Numerator_c > 0
+        AND department_c = 'Wellness'
+        AND dosage_types_c NOT LIKE '%NSO%'
+        AND Outcome_c != 'Cancelled'
+        AND AY_NAME = "AY 2020-21"
+    GROUP BY
+            site_shor
