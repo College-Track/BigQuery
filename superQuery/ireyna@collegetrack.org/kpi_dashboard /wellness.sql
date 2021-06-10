efc8ab016f881e5bd282aa179fc1141c49758781
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-10 12:22:54 -0700

    Auto-commit: Thu Jun 10 2021 12:22:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,10 +1,5 @@
 SELECT 
-    CASE
-        WHEN CSE.id IS NOT NULL #case note id
-        THEN 1
-        ELSE 0
-    END AS wellness_case_note_2020_21, #wellness casenotes from 2020-21
-    id AS case_note_id, #case note id
+    COUNT (DISTINCT id) AS case_note_id, #case note id
     site_short
 
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
@@ -14,5 +9,4 @@ WHERE Type_Counseling_c = TRUE
     AND college_track_status_c = '11A'
     AND co_vitality_scorecard_color_c IN ('Blue','Red')
 GROUP BY
-    site_short,
-    id
+    site_short
