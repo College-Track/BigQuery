8fe39f437653dfc559321b346603926d4af3c5a3
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-10 12:22:28 -0700

    Auto-commit: Thu Jun 10 2021 12:22:24 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -1,16 +1,18 @@
-SELECT
-    SUM(attendance_numerator_c) AS sum_attended_wellness_sessions,
+SELECT 
+    CASE
+        WHEN CSE.id IS NOT NULL #case note id
+        THEN 1
+        ELSE 0
+    END AS wellness_case_note_2020_21, #wellness casenotes from 2020-21
+    id AS case_note_id, #case note id
     site_short
-    FROM `data-warehouse-289815.salesforce_clean.class_template` CT
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.AT_Id = CT.Academic_Semester_c
-        WHERE
-        Attendance_Numerator_c > 0
-        AND department_c = 'Wellness'
-        AND dosage_types_c NOT LIKE '%NSO%'
-        AND AY_NAME = "AY 2020-21"
-        AND grade_c != '8th Grade'
-        AND Outcome_c != 'Cancelled'
-        AND college_track_status_c = '11A'
-        AND contact_id IN (SELECT contact_id FROM `data-warehouse-289815.salesforce_clean.contact_at_template` where co_vitality_scorecard_color_c IN  ('Blue','Red') AND AY_name = "AY 2020-21" AND Term_c = "Fall")
-    GROUP BY
-            site_short
+
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
+LEFT JOIN `data-warehouse-289815.salesforce.progress_note_c` CSE ON CAT.AT_Id = CSE.Academic_Semester_c
+WHERE Type_Counseling_c = TRUE
+    AND AY_name = 'AY 2020-21'
+    AND college_track_status_c = '11A'
+    AND co_vitality_scorecard_color_c IN ('Blue','Red')
+GROUP BY
+    site_short,
+    id
