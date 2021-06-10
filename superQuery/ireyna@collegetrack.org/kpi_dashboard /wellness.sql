fc7e3a429b8ab304c443cb14896b51fb95d9b547
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 17:12:25 -0700

    Auto-commit: Wed Jun 09 2021 17:12:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -3,12 +3,11 @@ SELECT
     site_short,CAT.student_c
    
     FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` AS CAT  
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` AS CT ON CAT.contact_id = CT.student_c
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` AS CT ON CAT.at_id = CT.academic_semester_c
     WHERE
         Attendance_Numerator_c > 0
         AND department_c = 'Wellness'
         AND dosage_types_c NOT LIKE '%NSO%'
-        AND academic_semester_c IN (SELECT at_id FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` WHERE AY_Name = "AY 2020-21")
         AND Outcome_c != 'Cancelled'
     GROUP BY
             site_short, student_c
