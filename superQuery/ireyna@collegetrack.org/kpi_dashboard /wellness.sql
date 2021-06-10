39052a3c594a9df715a05452f5ad9bb331828d59
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 17:19:26 -0700

    Auto-commit: Wed Jun 09 2021 17:19:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -32,7 +32,8 @@ GROUP BY site_short
 --gather_wellness_attendance_data AS (
 SELECT
     SUM(attendance_numerator_c) AS sum_attended_wellness_sessions,
-    site_short
+    site_short,
+    CAT.student_c
    
     FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` AS CAT  
     LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` AS CT ON CAT.at_id = CT.academic_semester_c
@@ -41,6 +42,6 @@ SELECT
         AND department_c = 'Wellness'
         AND dosage_types_c NOT LIKE '%NSO%'
         AND Outcome_c != 'Cancelled'
-        AND CT.student_c IN (SELECT student_c FROM gather_red_blue_covi_at)
+       AND CT.student_c IN (SELECT student_c FROM gather_red_blue_covi_at)
     GROUP BY
-            site_short
+            site_short,CAT.student_c
