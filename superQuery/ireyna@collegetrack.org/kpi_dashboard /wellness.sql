8afce3c79d81f1290d100bedc7e980be36d555fe
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-09 17:44:28 -0700

    Auto-commit: Wed Jun 09 2021 17:44:22 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
+++ superQuery/ireyna@collegetrack.org/kpi_dashboard /wellness.sql
@@ -36,8 +36,8 @@ SELECT
     SUM(attendance_numerator_c) AS sum_attended_wellness_sessions,
     site_short
    
-    FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` AS CAT  
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` AS CT ON CAT.at_id = CT.academic_semester_c
+    FROM gather_red_blue_covi_at AS RB
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template` AS CT ON RB.student_c = CT.student_c
     WHERE
         Attendance_Numerator_c > 0
         AND department_c = 'Wellness'
