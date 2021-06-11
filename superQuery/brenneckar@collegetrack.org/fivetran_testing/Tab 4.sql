998f438a07faa42113e476a3c42897feba51bfa7
Baker Renneckar <baker@bakerr.com>
2021-06-11 13:11:49 -0700

    Auto-commit: Fri Jun 11 2021 13:12:25 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -1,3 +1,3 @@
 SELECT COUNT(DISTINCT  Contact_Id)
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
-WHERE college_track_status_c IN ('11A', '15A', '17A')
+-- WHERE college_track_status_c IN ('11A', '15A', '17A')
