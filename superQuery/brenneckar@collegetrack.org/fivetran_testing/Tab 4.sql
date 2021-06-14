5f10cd1643dc8ab44fe5053414c6b2d306733123
Baker Renneckar <baker@bakerr.com>
2021-06-14 16:34:24 -0700

    Auto-commit: Mon Jun 14 2021 16:35:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -2,3 +2,4 @@ SELECT site_short, COUNT(Contact_Id)
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
 WHERE site_short = 'Aurora'
 AND college_track_status_c = '11A'
+GROUP BY site_short
