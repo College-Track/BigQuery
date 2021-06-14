88ae544c54db7e8d2f97ff0cd3cd97d44a7d2923
Baker Renneckar <baker@bakerr.com>
2021-06-14 15:11:10 -0700

    Auto-commit: Mon Jun 14 2021 15:11:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/fivetran_contact_at_template.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/fivetran_contact_at_template.sql
@@ -631,15 +631,15 @@ OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.contact_at_template` AS
       END AS gpa_required_date,
       
       CASE 
-        WHEN term_c = 'Fall' AND A.Name LIKE '%Semester%' THEN DATE_ADD(end_date_c, INTERVAL 210 DAY)
-        WHEN term_c = 'Fall' AND A.Name LIKE '%Quarter%' THEN DATE_ADD(end_date_c, INTERVAL 150 DAY)
-        WHEN term_c = 'Winter' THEN DATE_ADD(end_date_c, INTERVAL 120 DAY)
+        WHEN A.term_c = 'Fall' AND A.Name LIKE '%Semester%' THEN DATE_ADD(end_date_c, INTERVAL 210 DAY)
+        WHEN A.term_c = 'Fall' AND A.Name LIKE '%Quarter%' THEN DATE_ADD(end_date_c, INTERVAL 150 DAY)
+        WHEN A.term_c = 'Winter' THEN DATE_ADD(end_date_c, INTERVAL 120 DAY)
         
-        WHEN term_c = 'Spring' AND A.Name LIKE '%Quarter%' THEN DATE_ADD(end_date_c, INTERVAL 240 DAY)
-        WHEN term_c = 'Spring' AND A.Name LIKE '%Semester%' AND RT.Name = "High School Semester" THEN DATE_ADD(end_date_c, INTERVAL 180 DAY)
-        WHEN term_c = 'Spring' AND A.Name LIKE '%Semester%' AND RT.Name = "College/University Semester" THEN DATE_ADD(end_date_c, INTERVAL 210 DAY)
+        WHEN A.term_c = 'Spring' AND A.Name LIKE '%Quarter%' THEN DATE_ADD(end_date_c, INTERVAL 240 DAY)
+        WHEN A.term_c = 'Spring' AND A.Name LIKE '%Semester%' AND RT.Name = "High School Semester" THEN DATE_ADD(end_date_c, INTERVAL 180 DAY)
+        WHEN A.term_c = 'Spring' AND A.Name LIKE '%Semester%' AND RT.Name = "College/University Semester" THEN DATE_ADD(end_date_c, INTERVAL 210 DAY)
         
-        WHEN term_c = 'Summer' THEN NULL
+        WHEN A.term_c = 'Summer' THEN NULL
         
       END AS next_gpa_required_date
     FROM
