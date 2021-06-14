e09d83d9faab82fffc4e3c7e9ba6b59207144fe8
Baker Renneckar <baker@bakerr.com>
2021-06-14 15:34:14 -0700

    Auto-commit: Mon Jun 14 2021 15:34:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -2,4 +2,5 @@ SELECT DISTINCT GAS_Name, AT_Record_Type_Name, gpa_required_date, next_gpa_requi
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE college_track_status_c IN ('11A')
 AND term_c = 'Spring'
+AND AY_Name ='AY 2020-21'
 LIMIT 100
