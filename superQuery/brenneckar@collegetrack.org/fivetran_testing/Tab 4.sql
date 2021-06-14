9f4cb8c67c49db4e8a0af2527559239b19d10960
Baker Renneckar <baker@bakerr.com>
2021-06-14 16:42:43 -0700

    Auto-commit: Mon Jun 14 2021 16:43:25 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -1,5 +1,30 @@
-SELECT site_short, COUNT(Contact_Id)
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
-WHERE site_short = 'Aurora'
-AND college_track_status_c IN ('11A', '12A', '18a')
-GROUP BY site_short
+WITH gather_ay_attendance AS (
+  SELECT
+    site_short,
+    contact_id,
+    SUM(attended_workshops_c) AS attended_workshops_c,
+    SUM(enrolled_sessions_c) AS enrolled_sessions_c
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_at_template`
+  WHERE
+    AY_Name = "AY 2020-21"
+    AND site_short = 'Aurora'
+  GROUP BY
+    site_short,
+    contact_id
+),
+prep_data AS (
+  SELECT
+  site_short,
+  contact_Id,
+    CASE
+      WHEN enrolled_sessions_c = 0 THEN NULL
+      WHEN (attended_workshops_c / enrolled_sessions_c) >= 0.8 THEN 1
+      ELSE 0
+    END AS above_80_attendance
+  FROM
+  gather_ay_attendance GAA 
+  )
+
+SELECT *
+FROM prep_data
