c3df798ea1977e32a373296146dc78e7a604363e
Baker Renneckar <baker@bakerr.com>
2021-06-14 15:13:10 -0700

    Auto-commit: Mon Jun 14 2021 15:13:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(  Contact_Id)
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
--- WHERE college_track_status_c IN ('11A', '15A', '17A')
+SELECT AT_Name, AT_Record_Type_Name, gpa_required_date, next_gpa_required_date, current_valid_gpa_term, 
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+LIMIT 100
