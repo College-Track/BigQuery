6c39992c5585d316ecec1d8f190b537a84031c3d
Baker Renneckar <baker@bakerr.com>
2021-06-14 17:31:40 -0700

    Auto-commit: Mon Jun 14 2021 17:32:16 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 4.sql
@@ -2,7 +2,7 @@ WITH gather_ay_attendance AS (
   SELECT
     site_short,
     contact_id,
-    -- GAS_Name,
+    GAS_Name,
     SUM(attended_workshops_c) AS attended_workshops_c,
     SUM(enrolled_sessions_c) AS enrolled_sessions_c
   FROM
@@ -14,14 +14,14 @@ WITH gather_ay_attendance AS (
     AND college_track_status_c = '11A'
   GROUP BY
     site_short,
-    -- GAS_Name,
+    GAS_Name,
     contact_id
 ),
 prep_data AS (
   SELECT
   site_short,
   contact_Id,
-    --  GAS_Name,
+     GAS_Name,
   attended_workshops_c,
   enrolled_sessions_c,
 
@@ -39,7 +39,7 @@ prep_data AS (
 -- GROUP BY site_short
 
 SELECT
--- GAS_Name, 
+GAS_Name, 
 SUM(attended_workshops_c), SUM(enrolled_sessions_c), SUM(above_80_attendance)
 FROM prep_data
--- GROUP BY GAS_Name
+GROUP BY GAS_Name
