c56beddce5583c33cbd19f59cdec2503dc63a0f8
Baker Renneckar <baker@bakerr.com>
2021-06-16 11:56:35 -0700

    Auto-commit: Wed Jun 16 2021 11:57:24 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/kpi_dashboard  -DEV/Tab 16.sql
+++ superQuery/brenneckar@collegetrack.org/kpi_dashboard  -DEV/Tab 16.sql
@@ -1,3 +1,3 @@
-SELECT hs_cohort_capacity, hs_student_count, Gender_c, Ethnic_background_c, hs_senior_student_count
+SELECT hs_cohort_capacity, Gender_c, Ethnic_background_c, hs_senior_student_count
 FROM `data-studio-260217.kpi_dashboard_dev.site_directors`
 WHERE site_short = 'Watts'
