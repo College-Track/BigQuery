6ca51d76448ebda2d2e480eff3ed19af7a04749a
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-16 16:59:07 -0700

    Auto-commit: Wed Jun 16 2021 16:59:01 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/AUDIT_task_missing_student_prep.sql
@@ -0,0 +1,43 @@
+WITH get_AT_data AS
+(
+    SELECT
+    AT_Id,
+    AT_Name,
+    full_name_c,
+    student_c AS contact_id,
+    College_Track_Status_Name
+
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE college_track_status_c = '15A'
+),
+
+get_tasks_missing_student AS
+(
+    SELECT
+    id AS task_id,
+    what_id,
+    owner_id,
+    created_by_id,
+    created_date,
+    who_id AS name,
+    get_AT_data.AT_Id,
+    get_AT_data.AT_Name,
+    get_AT_data.full_name_c,
+    get_AT_data.contact_id,
+    get_AT_data.College_Track_Status_Name,
+    is_deleted
+
+
+    FROM `data-warehouse-289815.salesforce.task`
+    LEFT JOIN get_AT_data ON get_AT_data.AT_Id = what_id
+    WHERE is_deleted = false
+)
+
+    SELECT
+    *,
+
+    FROM get_tasks_missing_student
+    WHERE name IS NULL
+    AND 
+    (AT_Name LIKE '%(Semester)%'
+    OR AT_Name LIKE '%(Quarter)%')
