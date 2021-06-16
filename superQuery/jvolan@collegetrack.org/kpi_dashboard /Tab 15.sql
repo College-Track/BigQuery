5f7e6eb30229837ddc4c32199801279d71fc8156
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-16 14:58:07 -0700

    Auto-commit: Wed Jun 16 2021 14:57:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/kpi_dashboard /Tab 15.sql
+++ superQuery/jvolan@collegetrack.org/kpi_dashboard /Tab 15.sql
@@ -4,17 +4,22 @@ WITH gather_survey_data AS
     site_short AS survey_site_short,
     Gender_c AS survey_gender,
     Ethnic_background_c AS survey_ethnic_background,
+    SUM(
     CASE
         WHEN contact_id IS NOT NULL THEN 1
         ELSE 0
-    END AS ps_survey_scholarship_denom,
+    END) AS ps_survey_scholarship_denom,
+    SUM(
     CASE
         WHEN i_am_able_to_receive_my_scholarship_funds_from_college_track IN ('StronglyAgree', 'Strongly Agree', 'Agree') THEN 1
         ELSE 0
-    END AS ps_survey_scholarship_num
+    END) AS ps_survey_scholarship_num
     
     FROM  `data-studio-260217.surveys.fy21_ps_survey_wide_prepped`
     WHERE i_am_able_to_receive_my_scholarship_funds_from_college_track IS NOT NULL
+    GROUP BY site_short,
+    Gender_c,
+    Ethnic_background_c
 ),
 
 get_at_data AS
@@ -24,10 +29,11 @@ get_at_data AS
     Gender_c AS at_gender,
     Ethnic_background_c AS at_ethnic_background,
 --% of college students saying that they know how to apply for Emergency Fund in PAT advising rubric
+    SUM(
     CASE    
         WHEN e_fund_c = 'EF_G' THEN 1
         ELSE 0
-    END AS indicator_efund,
+    END) AS indicator_efund,
 
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE college_track_status_c = '15A'
@@ -38,6 +44,9 @@ get_at_data AS
     OR
     (CURRENT_DATE() > '2021-07-01'
     AND previous_as_c = TRUE))
+    GROUP BY site_short,
+    Gender_c,
+    Ethnic_background_c
 ),
 
 gather_contact_data AS
@@ -47,13 +56,18 @@ gather_contact_data AS
     site_short,
     Gender_c AS contact_gender,
     Ethnic_background_c AS contact_ethnic_background,
+    SUM(
     CASE
-        WHEN fa_req_expected_financial_contribution_c IS NOT NULL THEN 1
+        WHEN (college_track_status_c = '11A'
+        AND (grade_c = "12th Grade" OR (grade_c='Year 1' AND indicator_years_since_hs_graduation_c = 0))
+        AND fa_req_expected_financial_contribution_c IS NOT NULL) THEN 1
         ELSE 0
-    END AS fp_12_efc_num
+    END) AS fp_12_efc_num
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
-    WHERE college_track_status_c = '11A'
-    AND (grade_c = "12th Grade" OR (grade_c='Year 1' AND indicator_years_since_hs_graduation_c = 0))
+    WHERE college_track_status_c IN ('11A', '12A', '15A')
+    GROUP BY site_short,
+    Gender_c,
+    Ethnic_background_c
 ),
 
 --For KPI % of students admitted to Best-Fit College choose to enroll at Best-Fit College
@@ -88,49 +102,54 @@ gather_best_fit_data AS (
 --Define numerator and denominator for KPI % of students admitted to Best-Fit College enroll at Best-Fit College
 prep_best_fit_enrollment_kpi AS (
 SELECT
-    site_short,
+    site_short AS bf_site_short,
     bf_gender,
     bf_ethnic_background,
-    CASE  
+    SUM(CASE  
         WHEN accepted_best_fit IS NOT NULL 
         THEN 1
         ELSE 0
-    END AS fp_accepted_best_fit_denom,
+    END) AS fp_accepted_best_fit_denom,
     
-    CASE 
+    SUM(CASE 
         WHEN accepted_enrolled_best_fit IS NOT NULL
         THEN 1 
         ELSE 0
-    END AS fp_enrolled_best_fit_numerator,
+    END) AS fp_enrolled_best_fit_numerator,
     
 FROM gather_best_fit_data
+GROUP BY site_short,
+bf_gender,
+bf_ethnic_background
+),
 
-)
-
+join_data AS
+(
     SELECT
     a.site_short,
     a.contact_gender,
     a.contact_ethnic_background,
-    SUM(fp_12_efc_num) AS fp_12_efc_num,
-    SUM(gsd.ps_survey_scholarship_denom) AS ps_survey_scholarship_denom,
-    SUM(gsd.ps_survey_scholarship_num) AS ps_survey_scholarship_num,
-    SUM(cat.indicator_efund) AS fp_efund_num,
-    SUM(fp_accepted_best_fit_denom) AS fp_accepted_best_fit_denom,
-    SUM(fp_enrolled_best_fit_numerator) AS fp_enrolled_best_fit_numerator
+    fp_12_efc_num AS fp_12_efc_num,
+    gsd.ps_survey_scholarship_denom,
+    gsd.ps_survey_scholarship_num,
+    cat.indicator_efund AS fp_efund_num,
+    fp_accepted_best_fit_denom,
+    fp_enrolled_best_fit_numerator
     
     
     FROM gather_contact_data AS a
-    LEFT JOIN gather_survey_data AS gsd ON gsd.survey_site_short = site_short AND gsd.survey_gender = a.contact_gender AND gsd.survey_ethnic_background = a.contact_ethnic_background
+    LEFT JOIN gather_survey_data AS gsd ON gsd.survey_site_short = a.site_short AND gsd.survey_gender = a.contact_gender AND gsd.survey_ethnic_background = a.contact_ethnic_background
     LEFT JOIN get_at_data AS cat ON cat.at_site = a.site_short AND cat.at_gender = a.contact_gender AND cat.at_ethnic_background = a.contact_ethnic_background
-    LEFT JOIN prep_best_fit_enrollment_kpi AS bfp ON bfp.site_short=a.site_short AND bfp.bf_gender = a.contact_gender AND bfp.bf_ethnic_background = a.contact_ethnic_background
-    GROUP BY a.site_short,
-    a.contact_gender,
-    a.contact_ethnic_background
+    LEFT JOIN prep_best_fit_enrollment_kpi AS bfp ON bfp.bf_site_short=a.site_short AND bfp.bf_gender = a.contact_gender AND bfp.bf_ethnic_background = a.contact_ethnic_background
+)
 
-
-    
- 
-   
-
-
-     
+ SELECT
+    site_short,
+    SUM(fp_12_efc_num) AS fp_12_efc_num,
+    SUM(ps_survey_scholarship_denom),
+    SUM(ps_survey_scholarship_num),
+    SUM(fp_efund_num) AS fp_efund_num,
+    SUM(fp_accepted_best_fit_denom),
+    SUM(fp_enrolled_best_fit_numerator)
+    FROM join_data
+    GROUP BY site_short
