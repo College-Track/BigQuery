853178ba6b218b49e00e5ef2ab0c77acc9a1435f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-17 09:07:24 -0700

    Auto-commit: Thu Jun 17 2021 09:07:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
@@ -1,7 +1,11 @@
-SELECT region_abrev, site_short,
-grade AS student_type,
-SUM(student_count) AS student_count
-FROM `data-studio-260217.performance_mgt.fy22_projections`
-GROUP BY site_short, 
-region_abrev,
-grade
+SELECT
+* EXCEPT (site_or_region),
+CASE WHEN (student_group IS NOT NULL AND site_or_region = "Bay Area") THEN "NOR CAL"
+ELSE site_or_region
+END AS site_or_region
+FROM
+    `data-studio-260217.performance_mgt.expanded_role_kpi_selection` KPI_by_role
+WHERE
+    KPI_by_role.function IN (
+      'Mature Regional Staff',
+      'Non-Mature Regional Staff'
