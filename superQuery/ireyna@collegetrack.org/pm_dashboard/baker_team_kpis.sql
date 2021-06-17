54a94566167ddaa47dcc492deefbfbbe438b6b94
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-06-17 08:50:34 -0700

    Auto-commit: Thu Jun 17 2021 08:50:30 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
@@ -1,10 +1,8 @@
-SELECT  
-region_abrev,
-site_short,
-student_type,
-SUM(student_count) AS student_count,
-
+prep_grade_projections AS (
+SELECT region_abrev, site_short,
+grade AS student_type,
+SUM(student_count) AS student_count
 FROM `data-studio-260217.performance_mgt.fy22_projections`
 GROUP BY site_short, 
 region_abrev,
-student_type
+grade
