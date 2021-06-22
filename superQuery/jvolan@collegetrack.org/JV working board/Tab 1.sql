93e31a282ea15cdf073e81659803b9e556d82bc8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-22 08:35:24 -0700

    Auto-commit: Tue Jun 22 2021 08:35:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 1.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 1.sql
@@ -1,4 +1,8 @@
-SELECT
+--% of students who persist into the following year (all college students)
+--first pulling in all ATs for time period I want (last fall to next fall)
+WITH get_persist_at_data AS
+(
+  SELECT
     site_short,
     contact_id AS persist_contact_id,
 --indicator to flag which students were enrolled in any college last fall. used to created denominator later
@@ -17,7 +21,7 @@ SELECT
 
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 --Start date for PAT must be prior today to be included. To exclude future PATs upon creation, until they become current AT. want to ignore summer too.
-    WHERE site_short = 'College Track Oakland'
+    WHERE site_short = 'Oakland'
     AND
     (AY_Name = 'AY 2019-20'
     AND term_c <> 'Summer')
@@ -26,3 +30,19 @@ SELECT
     AND term_c = 'Fall')
     GROUP BY contact_id,
     site_short
+    
+)
+
+    SELECT
+    persist_contact_id,
+    MAX(include_in_reporting_group) AS cc_persist_denom,
+  -- if # terms = # of terms meeting persistence defintion, student will be in numerator
+    MAX(
+    CASE
+        WHEN at_count = persist_count THEN 1
+        ELSE 0
+    END) AS indicator_persisted
+    FROM get_persist_at_data
+-- filter out any students who weren't enrolled last fall. denominator
+    WHERE include_in_reporting_group = 1
+    GROUP BY persist_contact_id
