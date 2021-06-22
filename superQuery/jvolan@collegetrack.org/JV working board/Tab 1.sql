fffa3f7ace595cbc0a03a024fd1af95347d1065a
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-22 08:39:44 -0700

    Auto-commit: Tue Jun 22 2021 08:39:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 1.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 1.sql
@@ -31,8 +31,11 @@ WITH get_persist_at_data AS
     GROUP BY contact_id,
     site_short
     
-)
+),
 
+--actually comparing the # terms vs. # of terms meeting persistence defintion, per student
+persist_calc AS
+(
     SELECT
     persist_contact_id,
     site_short,
@@ -46,5 +49,12 @@ WITH get_persist_at_data AS
     FROM get_persist_at_data
 -- filter out any students who weren't enrolled last fall. denominator
     WHERE include_in_reporting_group = 1
-    GROUP BY persist_contact_id,
+    GROUP BY persist_contact_id, 
     site_short
+)
+    SELECT
+    site_short,
+    sum(indicator_persisted) AS indicator_persisted,
+    sum(cc_persist_denom) AS denominator_reporting_group,
+    FROM persist_calc
+    GROUP BY site_short
