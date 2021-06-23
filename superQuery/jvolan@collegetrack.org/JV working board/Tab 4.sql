f5848c93cbd254bb8ac142ddc33727d058c032db
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-23 09:00:56 -0700

    Auto-commit: Wed Jun 23 2021 09:00:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 4.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 4.sql
@@ -1,5 +1,6 @@
     SELECT
-    Contact_Id,
+    count(Contact_Id),
+    site_short,
     CASE
         WHEN
         (anticipated_date_of_graduation_ay_c = 'AY 2020-21'
@@ -15,3 +16,5 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE indicator_completed_ct_hs_program_c = true
     AND academic_year_4_year_degree_earned_c = 'AY 2020-21'
+    GROUP BY site_short, 
+    ps_alum_bucket
