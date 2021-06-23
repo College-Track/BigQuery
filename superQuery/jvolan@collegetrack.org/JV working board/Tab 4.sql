39cc4ea8ef914aafdd4f6ff2ec5b5778f2b7a24b
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-23 08:52:22 -0700

    Auto-commit: Wed Jun 23 2021 08:52:16 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 4.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 4.sql
@@ -1,42 +1,19 @@
     SELECT
-    AT_Id,
-    Contact_Id AS at_contact_id,
-    site_short AS at_site,
---% of students completing FAFSA or equivalent
-    CASE    
-        WHEN filing_status_c = 'FS_G' THEN 1
-        ELSE 0
-    END AS indicator_fafsa_complete,
---% of students on-track to have less than $30K loan debt
+    contact_Id,
+    Gender_c,
+    Ethnic_background_c,
+    site_short,
     CASE
-        WHEN 
-        loans_c IN ('LN_G','LN_Y') THEN 1
-        ELSE 0
-    END AS indicator_loans_less_30k_loans,
---% of students attaining a well-balanced lifestyle    
-    CASE
-        WHEN Overall_Rubric_Color = 'Green' THEN 1
-        ELSE 0
-    END AS indicator_well_balanced,
---% of students understand that technical and interpersonal skills are needed to create opportunities now and in the future
-    CASE
-        WHEN advising_rubric_career_readiness_v_2_c = 'Green'
-        AND 
-        (academic_networking_50_cred_c = 'AN1_G'
-        OR academic_networking_over_50_credits_c = 'AN2_G') THEN 1
-        ELSE 0
-    END AS indicator_tech_interpersonal_skills,
-    CAST (CURRENT_DATE() AS DATE) AS determine_at_date,  
-    current_as_c,
-    previous_as_c,
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE college_track_status_c = '15A'
-    AND(
-    (CURRENT_DATE() < '2021-07-01'
-    AND current_as_c = TRUE)
-    OR
-    (CURRENT_DATE() > '2021-07-01'
-    AND previous_as_c = TRUE))
+        WHEN
+        (anticipated_date_of_graduation_ay_c = 'AY 2020-21'
+         AND college_track_status_c = '15A'
+         AND Credit_Accumulation_Pace_c NOT IN ("6+ Years", 'Credit Data Missing')
+         AND credits_accumulated_most_recent_c >= 80
+         AND Current_Enrollment_Status_c = "Full-time"
+         AND Current_School_Type_c_degree = "Predominantly bachelor's-degree granting") THEN 'projected grad'
+         WHEN academic_year_4_year_degree_earned_c = 'AY 2020-21' THEN 'AY alumni'
+         ELSE ""
+         END AS ps_alum_bucket
 
-   
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    WHERE indicator_completed_ct_hs_program_c = true
