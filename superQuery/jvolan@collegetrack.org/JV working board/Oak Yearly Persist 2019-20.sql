b5d8cac044f651b9317a14685d13fbd727176847
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-23 15:35:23 -0700

    Auto-commit: Wed Jun 23 2021 15:35:17 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Oak Yearly Persist 2019-20.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Oak Yearly Persist 2019-20.sql
@@ -17,6 +17,7 @@ get_persist_at_data AS
 (
   SELECT
     CAT.site_short,
+    CAT.high_school_graduating_class_c,
     CAT.contact_id AS persist_contact_id,
     COUNT(AT_Id) AS at_count,
 --for those same records, counting # of ATs for each student in which they met term to term persistence definition
@@ -37,7 +38,8 @@ get_persist_at_data AS
     (AY_Name = 'AY 2020-21'
     AND term_c = 'Fall')
     GROUP BY CAT.contact_id,
-    CAT.site_short
+    CAT.site_short,
+    CAT.high_school_graduating_class_c
 ),
 
 --actually comparing the # terms vs. # of terms meeting persistence defintion, per student
@@ -46,6 +48,7 @@ persist_calc AS
     SELECT
     persist_contact_id,
     site_short,
+    high_school_graduating_class_c,
     MAX(include_in_reporting_group) AS cc_persist_denom,
   -- if # terms = # of terms meeting persistence defintion, student will be in numerator
     MAX(
@@ -56,14 +59,17 @@ persist_calc AS
     FROM get_persist_at_data
     WHERE include_in_reporting_group = 1
     GROUP BY persist_contact_id, 
-    site_short
+    site_short,
+    high_school_graduating_class_c
 )
     SELECT
     site_short,
     sum(indicator_persisted) AS indicator_persisted,
     sum(cc_persist_denom) AS denominator_reporting_group,
+    (sum(indicator_persisted)/sum(cc_persist_denom)) as percent_persisted
     FROM persist_calc
-    GROUP BY site_short
+    GROUP BY site_short,
+    high_school_graduating_class_c
 
     
     
