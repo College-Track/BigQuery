695cdfb8d78efe240472b0eaf7dd05a74a04c880
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-06-25 11:13:51 -0700

    Auto-commit: Fri Jun 25 2021 11:13:48 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/OAK FY22 internship.sql
@@ -0,0 +1,34 @@
+    SELECT
+    Contact_Id,
+    site_short,
+    CASE
+        WHEN
+        (indicator_completed_ct_hs_program_c = true
+        AND
+        ps_internships_c > 0
+        AND
+        (anticipated_date_of_graduation_ay_c = 'AY 2021-22'
+         AND college_track_status_c = '15A'
+         AND Credit_Accumulation_Pace_c NOT IN ("6+ Years", 'Credit Data Missing')
+         AND credits_accumulated_most_recent_c >= 60
+         AND Current_Enrollment_Status_c = "Full-time"
+         AND Current_School_Type_c_degree = "Predominantly bachelor's-degree granting")) THEN 1
+        ELSE 0
+    END AS fy22_oak_cc_ps_grad_internship_num,
+--denominator
+    CASE
+        WHEN
+        (indicator_completed_ct_hs_program_c = true
+        AND
+        (anticipated_date_of_graduation_ay_c = 'AY 2021-22'
+         AND college_track_status_c = '15A'
+         AND Credit_Accumulation_Pace_c NOT IN ("6+ Years", 'Credit Data Missing')
+         AND credits_accumulated_most_recent_c >= 60
+         AND Current_Enrollment_Status_c = "Full-time"
+         AND Current_School_Type_c_degree = "Predominantly bachelor's-degree granting")) THEN 1
+        ELSE 0
+    END AS fy22_oak_cc_ps_grad_internship_denom,
+
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    WHERE indicator_completed_ct_hs_program_c = true
+    AND site_short = 'Oakland'
