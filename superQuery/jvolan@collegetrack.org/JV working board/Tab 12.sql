42bd74f3abee53d787d34906d1c2c6c8963761ac
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-12 11:47:03 -0700

    Auto-commit: Mon Jul 12 2021 11:46:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
@@ -2,9 +2,13 @@
  AT_Name,
  full_name_c,
  AT_Cumulative_GPA,
- previous_as_c,
- current_as_c
+SUM(
+CASE
+WHEN previous_as_c = true THEN 1
+ELSE 0
+END) AS prev_at_count,
 
  FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
  WHERE college_track_status_c = '15A'
  AND previous_as_c = TRUE
+ GROUP by AT_name,full_name_c,AT_Cumulative_GPA
