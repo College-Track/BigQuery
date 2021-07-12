264adcd1eb28b0a6852706f0c8c3b7db1680b380
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-12 12:00:06 -0700

    Auto-commit: Mon Jul 12 2021 12:00:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 11.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 11.sql
@@ -1,10 +1,10 @@
     SELECT
-    Contact_Id AS at_contact_id,
-    count(AT_Id) AS at_count,
+    count(Contact_Id) AS at_count,
+    AT_id,
     Max(AT_Cumulative_GPA),
 
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE college_track_status_c = '15A'
     AND previous_as_c = TRUE
-    GROUP BY Contact_Id
+    GROUP BY AT_Id
     LIMIT 1
