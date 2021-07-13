34f13c0824ff86363552cbebdfc654ae6de6868b
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-13 11:25:37 -0700

    Auto-commit: Tue Jul 13 2021 11:25:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
@@ -1,17 +1,27 @@
+with gather_contact AS
+(
  SELECT
-    count(contact_Id) as count,
+    Contact_Id,
     high_school_graduating_class_c,
-    sum(CASE
+    CASE
         WHEN
         (indicator_completed_ct_hs_program_c = true
         AND college_track_status_c = '15A'
         AND most_recent_valid_cumulative_gpa >= 2.5) THEN 1
         ELSE 0
-    END) AS cc_ps_gpa_2_5_num,
+    END AS cc_ps_gpa_2_5_num,
 
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
 --needed widest reporting group to ensure I had all students I need to evaluate if they were in one of my custom denoms. Added in more detail filter logic into case statements above.
     WHERE indicator_completed_ct_hs_program_c = true
     AND college_track_status_c ='15A'
-    AND most_recent_valid_cumulative_gpa IS NULL
-    GROUP BY high_school_graduating_class_c
+)
+
+SELECT 
+    atc.Contact_Id AS a_contact_id,
+    AT_Name,
+    AT_Cumulative_GPA,
+    
+    FROM  `data-warehouse-289815.salesforce_clean.contact_at_template`AS atc
+    LEFT JOIN gather_contact ON gather_contact.Contact_Id = atc.Contact_Id
+    WHERE global_academic_semester_c IN ('Fall 2020-21 (Semester)','Fall 2020-21 (Quarter)') 
