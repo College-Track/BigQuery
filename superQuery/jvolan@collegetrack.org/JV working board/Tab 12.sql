eee3bb004428211312cdf1f5f174286d3e32a9ab
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-13 11:27:18 -0700

    Auto-commit: Tue Jul 13 2021 11:27:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
@@ -15,13 +15,20 @@ with gather_contact AS
 --needed widest reporting group to ensure I had all students I need to evaluate if they were in one of my custom denoms. Added in more detail filter logic into case statements above.
     WHERE indicator_completed_ct_hs_program_c = true
     AND college_track_status_c ='15A'
-)
-
+),
+gather_at AS
+(
 SELECT 
-    atc.Contact_Id AS a_contact_id,
+    Contact_Id AS a_contact_id,
     AT_Name,
     AT_Cumulative_GPA,
     
-    FROM  `data-warehouse-289815.salesforce_clean.contact_at_template`AS atc
-    LEFT JOIN gather_contact ON gather_contact.Contact_Id = atc.Contact_Id
+    FROM  `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE global_academic_semester_c IN ('Fall 2020-21 (Semester)','Fall 2020-21 (Quarter)') 
+    )
+    
+    SELECT 
+    *
+    FROM gather_contact
+    LEFT JOIN gather_at ON gather_at.a_contact_id = Contact_Id
+
