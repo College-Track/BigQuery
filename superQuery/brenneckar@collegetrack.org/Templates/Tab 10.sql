22f839345a3ef76e0f997848c8527e9413023636
Baker Renneckar <baker@bakerr.com>
2021-07-13 10:52:11 -0700

    Auto-commit: Tue Jul 13 2021 10:52:08 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
@@ -1,10 +1,7 @@
--- SELECT  C.Contact_id,
---       CAT.AT_Term_GPA AS most_recent_valid_term_gpa,
---       CAT.AT_Cumulative_GPA AS most_recent_valid_cumulative_gpa
+SELECT CAT.*,
+      C.most_recent_valid_term_gpa,
+      C.most_recent_valid_cumulative_gpa
 
--- FROM `data-warehouse-289815.salesforce_clean.contact_template` C
--- LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.Contact_Id = C.Contact_Id
--- WHERE CAT.current_valid_gpa_term = TRUE
-
-SELECT Contact_id, most_recent_valid_term_gpa, most_recent_valid_cumulative_gpa
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
+LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON CAT.Contact_Id = C.Contact_Idmost_recent_valid_cumulative_gpa
 FROM `data-warehouse-289815.salesforce_clean.contact_template`
