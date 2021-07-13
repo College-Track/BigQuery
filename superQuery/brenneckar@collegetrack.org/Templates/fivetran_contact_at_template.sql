5e3f07f664d9faf8588122111a16b36eda3150b8
Baker Renneckar <baker@bakerr.com>
2021-07-13 10:55:06 -0700

    Auto-commit: Tue Jul 13 2021 10:54:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/fivetran_contact_at_template.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/fivetran_contact_at_template.sql
@@ -857,4 +857,19 @@ FROM `data-warehouse-289815.salesforce_clean.contact_template` C
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.Contact_Id = C.Contact_Id
 WHERE CAT.current_valid_gpa_term = TRUE
 
+);
+
+-- Script 5: Refresh Contact At Template
+CREATE
+OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.contact__at_template` AS(
+SELECT CAT.*,
+      C.most_recent_valid_term_gpa,
+      C.most_recent_valid_cumulative_gpa
+
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
+LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON CAT.Contact_Id = C.Contact_Id
+
 )
+
+
+
