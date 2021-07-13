ab8731440ac4ef15c5374eeb523e5707faaa473f
Baker Renneckar <baker@bakerr.com>
2021-07-13 15:13:11 -0700

    Auto-commit: Tue Jul 13 2021 15:13:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -3,7 +3,7 @@ FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE AY_Name IN ('AY 2018-19', 'AY 2019-20')
 AND site_short IN ('San Francisco', "Oakland", 'East Palo Alto')
 AND AT_Grade_c = '12th Grade'
-AND high_school_graduating_class_c = '2019'
+AND high_school_graduating_class_c = '2020'
 GROUP BY Contact_Id, AY_Name
 )
 SELECT AY_Name, COUNT(Contact_Id)
