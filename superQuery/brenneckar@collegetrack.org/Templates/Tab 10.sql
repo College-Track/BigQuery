94218aecc66f822cf5588fe11ebdd32bbeb13b12
Baker Renneckar <baker@bakerr.com>
2021-07-13 10:52:22 -0700

    Auto-commit: Tue Jul 13 2021 10:52:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
@@ -4,4 +4,3 @@ SELECT CAT.*,
 
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
 LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON CAT.Contact_Id = C.Contact_Idmost_recent_valid_cumulative_gpa
-FROM `data-warehouse-289815.salesforce_clean.contact_template`
