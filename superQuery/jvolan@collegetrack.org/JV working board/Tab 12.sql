0778f159df5ca2b7263b2073ca4f37233e59987f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-13 11:28:29 -0700

    Auto-commit: Tue Jul 13 2021 11:28:25 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
@@ -25,7 +25,9 @@ SELECT
     
     FROM  `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE global_academic_semester_c IN ('Fall 2020-21 (Semester)','Fall 2020-21 (Quarter)') 
-    )
+    AND indicator_completed_ct_hs_program_c = true
+    AND college_track_status_c ='15A'
+)
     
     SELECT 
     *,
