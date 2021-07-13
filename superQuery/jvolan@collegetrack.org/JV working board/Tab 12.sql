2aa9113879c21ec0bd377f1eb6f3ae2dbef5e484
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-13 10:48:56 -0700

    Auto-commit: Tue Jul 13 2021 10:48:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
@@ -1,15 +1,16 @@
  SELECT
- AT_Name,
- full_name_c,
- AT_Cumulative_GPA,
- site_short,
-SUM(
-CASE
-WHEN previous_as_c = true THEN 1
-ELSE 0
-END) AS prev_at_count,
+    contact_Id,
+    Gender_c,
+    Ethnic_background_c,
+    site_short,
+    CASE
+        WHEN
+        (indicator_completed_ct_hs_program_c = true
+        AND college_track_status_c = '15A'
+        AND most_recent_valid_cumulative_gpa >= 2.5) THEN 1
+        ELSE 0
+    END AS cc_ps_gpa_2_5_num,
 
- FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
- WHERE college_track_status_c = '15A'
- AND previous_as_c = TRUE
- GROUP by AT_name,full_name_c,AT_Cumulative_GPA,site_short
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+--needed widest reporting group to ensure I had all students I need to evaluate if they were in one of my custom denoms. Added in more detail filter logic into case statements above.
+    WHERE indicator_completed_ct_hs_program_c = true
