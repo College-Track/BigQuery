44583c98a378014fa2bf062cbf058a97d4d80550
Baker Renneckar <baker@bakerr.com>
2021-07-13 15:07:18 -0700

    Auto-commit: Tue Jul 13 2021 15:07:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
+++ superQuery/brenneckar@collegetrack.org/fivetran_testing/Tab 1.sql
@@ -1,3 +1,4 @@
-SELECT *
-FROM `data-warehouse-289815.salesforce_clean.contact_ay_template`
+SELECT Contact_Id, SUM(enrolled_sessions_c)
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
 WHERE AY_Name IN ('AY 2018-19', 'AY 2019-20')
+GROUP BY Conact_id, AY_Name
