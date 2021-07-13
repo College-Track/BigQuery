c9e6455597f23435a6f2e839a4bbf8265bea80d0
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-13 12:26:31 -0700

    Auto-commit: Tue Jul 13 2021 12:26:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 12.sql
@@ -1,9 +1,27 @@
+with gather_contact AS
+(
+ SELECT
+    Contact_Id,
+    high_school_graduating_class_c,
+    most_recent_valid_cumulative_gpa,
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    WHERE indicator_completed_ct_hs_program_c = true
+    AND college_track_status_c ='15A'
+),
+gather_at AS
+(
 SELECT 
-    atc.Contact_Id
+    Contact_Id AS a_contact_id,
     AT_Name,
     AT_Cumulative_GPA,
     
     FROM  `data-warehouse-289815.salesforce_clean.contact_at_template` AS atc
-    WHERE global_academic_semester_c IN ('Fall 2020-21 (Semester)','Fall 2020-21 (Quarter)') 
+    WHERE GAS_Name IN ('Fall 2020-21 (Semester)','Fall 2020-21 (Quarter)') 
     AND indicator_completed_ct_hs_program_c = true
     AND college_track_status_c ='15A'
+)
+    
+    SELECT
+    *
+    FROM gather_at
+    LEFT JOIN gather_contact ON gather_contact.contact_id = a_contact_Id
