7676e458afa1b14c5eb1c145fe04516532570fd7
Baker Renneckar <baker@bakerr.com>
2021-07-13 13:02:44 -0700

    Auto-commit: Tue Jul 13 2021 13:02:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
@@ -5,8 +5,8 @@ with gather_contact AS
     Contact_Id,
     high_school_graduating_class_c,
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
-    WHERE indicator_completed_ct_hs_program_c = true
-    AND college_track_status_c ='15A'
+    -- WHERE indicator_completed_ct_hs_program_c = true
+    -- AND college_track_status_c ='15A'
 ),
 gather_at AS
 (
@@ -14,6 +14,7 @@ SELECT
     Contact_Id AS a_contact_id,
     AT_Name,
     AT_Cumulative_GPA,
+    
     FROM  `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE GAS_Name IN ('Fall 2020-21 (Semester)','Fall 2020-21 (Quarter)')
     AND indicator_completed_ct_hs_program_c = true
@@ -22,6 +23,5 @@ SELECT
     SELECT
     *
     FROM gather_at
-    LEFT JOIN gather_contact ON gather_contact.contact_id = a_contact_Id
-    WHERE most_recent_valid_cumulative_gpa IS NULL
-    AND AT_Cumulative_GPA IS NOT NULL
+    LEFT JOIN gather_contact ON gather_contact.contact_id = gather_at.a_contact_id
+    WHERE most_recent_valid_cumulative_gpa != AT_Cumulative_GPA
