f209ebf754e18baa1cb516f16e9a8c22cdc50498
Baker Renneckar <baker@bakerr.com>
2021-07-13 12:58:19 -0700

    Auto-commit: Tue Jul 13 2021 12:58:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/Tab 10.sql
@@ -1,7 +1,27 @@
-SELECT CAT.Contact_Id,
-      C.most_recent_valid_term_gpa,
-      C.most_recent_valid_cumulative_gpa
-
-FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON CAT.Contact_Id = C.Contact_Id
-WHERE CAT.Contact_id = '0034600001FWEWoAAP'
+with gather_contact AS
+(
+ SELECT
+    most_recent_valid_cumulative_gpa,
+    Contact_Id,
+    high_school_graduating_class_c,
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    WHERE indicator_completed_ct_hs_program_c = true
+    AND college_track_status_c =‘15A’
+),
+gather_at AS
+(
+SELECT
+    Contact_Id AS a_contact_id,
+    AT_Name,
+    AT_Cumulative_GPA,
+    FROM  `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE GAS_Name IN (‘Fall 2020-21 (Semester)’,’Fall 2020-21 (Quarter)’)
+    AND indicator_completed_ct_hs_program_c = true
+    AND college_track_status_c =‘15A’
+)
+    SELECT
+    *
+    FROM gather_at
+    LEFT JOIN gather_contact ON gather_contact.contact_id = a_contact_Id
+    WHERE most_recent_valid_cumulative_gpa IS NULL
+    AND AT_Cumulative_GPA IS NOT NULL
