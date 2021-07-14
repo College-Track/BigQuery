8d0c4fc41208ba65746b9512c227f769407d0fed
Baker Renneckar <baker@bakerr.com>
2021-07-14 10:44:29 -0700

    Auto-commit: Wed Jul 14 2021 10:44:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/brenneckar@collegetrack.org/Templates/fivetran_contact_at_template.sql
+++ superQuery/brenneckar@collegetrack.org/Templates/fivetran_contact_at_template.sql
@@ -847,29 +847,29 @@ FROM format_combined_data
 );
 -- Script 4: Modify Contact Template
 
-CREATE
-OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.contact_template` AS(
-SELECT C.*,
-      CAT.AT_Term_GPA AS most_recent_valid_term_gpa,
-      CAT.AT_Cumulative_GPA AS most_recent_valid_cumulative_gpa
+-- CREATE
+-- OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.contact_template` AS(
+-- SELECT C.*,
+--       CAT.AT_Term_GPA AS most_recent_valid_term_gpa,
+--       CAT.AT_Cumulative_GPA AS most_recent_valid_cumulative_gpa
 
-FROM `data-warehouse-289815.salesforce_clean.contact_template` C
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.Contact_Id = C.Contact_Id
-WHERE CAT.current_valid_gpa_term = TRUE
+-- FROM `data-warehouse-289815.salesforce_clean.contact_template` C
+-- LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.Contact_Id = C.Contact_Id
+-- WHERE CAT.current_valid_gpa_term = TRUE
 
-);
+-- );
 
--- Script 5: Refresh Contact At Template
-CREATE
-OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.contact_at_template` AS(
-SELECT CAT.*,
-      C.most_recent_valid_term_gpa,
-      C.most_recent_valid_cumulative_gpa
+-- -- Script 5: Refresh Contact At Template
+-- CREATE
+-- OR REPLACE TABLE `data-warehouse-289815.salesforce_clean.contact_at_template` AS(
+-- SELECT CAT.*,
+--       C.most_recent_valid_term_gpa,
+--       C.most_recent_valid_cumulative_gpa
 
-FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
-LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON CAT.Contact_Id = C.Contact_Id
+-- FROM `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
+-- LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C ON CAT.Contact_Id = C.Contact_Id
 
-)
+-- )
 
 
 
