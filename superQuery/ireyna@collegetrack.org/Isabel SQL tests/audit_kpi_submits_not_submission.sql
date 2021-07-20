b5c5adeb12444197d70b48316bd9865087427b89
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 09:19:09 -0700

    Auto-commit: Tue Jul 20 2021 09:19:04 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
@@ -10,7 +10,7 @@ FROM gather_all_kpis
 )
 
 --create_list_of_unseleted_kpis_by_role AS (
-SELECT all_kpis.team_kpi, all_kpis.select_role, target_submissions.kpis_by_role
+SELECT target_submissions.team_kpi, target_submissions.select_role, all_kpis.kpis_by_role
 FROM gather_all_kpis all_kpis
 LEFT JOIN `data-warehouse-289815.google_sheets.audit_kpi_target_submissions` target_submissions  #gsheet kpi target submissions
 ON all_kpis.function = target_submissions.team_kpi 
