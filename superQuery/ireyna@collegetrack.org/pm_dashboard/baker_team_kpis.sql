1c6702ab6e3367accea5ca913526ee3e594c4edf
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 10:20:11 -0700

    Auto-commit: Tue Jul 20 2021 10:20:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
@@ -24,6 +24,8 @@ WITH prep_kpi_targets AS (
         THEN 'Student Survey - % of students served by Wellness who "agree" or "strongly agree" wellness services assisted them in managing their stress, helping them engage in self-care practices and/or enhancing their mental health'
         WHEN select_kpi = '% of students matriculating to Best Fit, Good Fit, or Situational Best Fit colleges'
         THEN '% of high school seniors who matriculate to Good, Best, or Situational Best Fit colleges'
+        WHEN select_kpi = '% of college students graduating from college within 6 years'
+        THEN '% of students graduating from college within 6 years'
         ELSE select_kpi
     END AS select_kpi,
     what_is_the_type_of_target_,
