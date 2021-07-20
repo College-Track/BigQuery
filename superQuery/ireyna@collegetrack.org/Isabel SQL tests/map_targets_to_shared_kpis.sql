fb64cc49430ba40409f09ada92d6ec10dc2c160a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-19 21:52:11 -0700

    Auto-commit: Mon Jul 19 2021 21:52:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/map_targets_to_shared_kpis.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/map_targets_to_shared_kpis.sql
@@ -62,7 +62,12 @@ GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,tar
 */
 
 prep_site_targets_by_role AS (
-SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,site_kpi
+SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,site_kpi,
+    CASE 
+        WHEN function = "Mature Site Staff" THEN "Mature_Site_Staff" 
+        WHEN function = "Non-Mature Site Staff" THEN "Non_Mature_Site_Sraff"
+        ELSE NULL 
+        END AS function
     /*CASE WHEN 
         GAK.function = kpi_targets_submitted.team_kpi AND
         GAK.kpis_by_role = kpi_targets_submitted.select_kpi AND 
