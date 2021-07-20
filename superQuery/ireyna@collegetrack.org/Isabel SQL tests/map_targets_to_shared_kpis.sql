7121e44cdc2cfb638ce499d94c50e75c27fcf0f5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-19 21:31:47 -0700

    Auto-commit: Mon Jul 19 2021 21:31:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/map_targets_to_shared_kpis.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/map_targets_to_shared_kpis.sql
@@ -1,3 +1,31 @@
+#map targets to shared KPIs across teams 
+#for sites
+#for regional
+#for non-program roles
+
+/*
+CREATE OR REPLACE TABLE 'data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis'
+OPTIONS
+    (
+    description= "This table maps targets to KPIs shared across roles within Teams/Functions"
+    )
+AS
+*/ 
+
+--SELECT *
+--FROM `data-warehouse-289815.google_sheets.team_kpi_target`
+WITH gather_all_kpis AS (
+SELECT function, role, kpis_by_role
+FROM `data-studio-260217.performance_mgt.role_kpi_selection` #clean list of KPIs by Role
+ORDER BY function, role
+),
+
+gather_unique_function_role AS (
+SELECT DISTINCT function function, role, kpis_by_role 
+FROM gather_all_kpis
+),
+
+kpi_targets_submitted AS (
 SELECT
     team_kpi, 
     select_role,
@@ -13,3 +41,43 @@ CASE
 FROM `data-warehouse-289815.google_sheets.audit_kpi_target_submissions` kpi_targets
 WHERE email_kpi <> "test@collegetrack.org"
 AND disregard_entry_op_hard_coded IS NULL
+),
+
+/*
+targets_by_role AS (
+SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,
+    /*CASE WHEN 
+        GAK.function = kpi_targets_submitted.team_kpi AND
+        GAK.kpis_by_role = kpi_targets_submitted.select_kpi AND 
+        submission_id IS NULL
+        THEN target_fy22
+        ELSE 0
+    END AS mapped_targets --omit this line too
+FROM gather_all_kpis GAK
+LEFT JOIN kpi_targets_submitted 
+ON GAK.kpis_by_role = kpi_targets_submitted.select_kpi
+--WHERE GAFR.select_role != GAK.role
+GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22
+)
+*/
+
+site_targets_by_role AS (
+SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,site_kpi
+    /*CASE WHEN 
+        GAK.function = kpi_targets_submitted.team_kpi AND
+        GAK.kpis_by_role = kpi_targets_submitted.select_kpi AND 
+        submission_id IS NULL
+        THEN target_fy22
+        ELSE 0
+    END AS mapped_targets*/
+FROM gather_all_kpis GAK
+LEFT JOIN kpi_targets_submitted 
+ON GAK.kpis_by_role = kpi_targets_submitted.select_kpi
+WHERE site_kpi <> "0"
+GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22, site_kpi
+)
+
+SELECT target_fy22,team_kpi,kpis_by_role
+FROM site_targets_by_role
+WHERE target_fy22 IS NOT NULL
+GROUP BY  target_fy22,team_kpi,kpis_by_role
