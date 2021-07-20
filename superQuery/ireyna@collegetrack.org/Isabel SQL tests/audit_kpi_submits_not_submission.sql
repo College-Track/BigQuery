10e9aa38800614309e0b3bd81f7ae30cc29f984e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-19 20:30:13 -0700

    Auto-commit: Mon Jul 19 2021 20:30:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
@@ -2,8 +2,14 @@ WITH gather_all_kpis AS (
 SELECT function, role, kpis_by_role
 FROM `data-studio-260217.performance_mgt.role_kpi_selection`
 ORDER BY function, role
-)
+),
 
---gather_unique_function_role AS (
+gather_unique_function_role AS (
 SELECT DISTINCT function function, role 
 FROM gather_all_kpis
+)
+
+--create_list_of_unseleted_kpis_by_role AS (
+SELECT GAFR.function, GAFR.role, GAK.kpis_by_role
+FROM gather_unique_function_role GAFR
+LEFT JOIN gather_all_kpis GAK ON GAK.role = GAFR.role
