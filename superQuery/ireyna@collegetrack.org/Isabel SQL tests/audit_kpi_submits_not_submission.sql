2edc9147d925bb94cb342cd377e808153e422e7e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-19 20:27:26 -0700

    Auto-commit: Mon Jul 19 2021 20:27:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
@@ -20,14 +20,22 @@ SELECT DISTINCT function function, role
 FROM gather_all_kpis
 ), 
 
-create_list_of_unseleted_kpis AS (
+create_list_of_unseleted_kpis_by_role AS (
+SELECT GAFR.function, GAFR.role, GAK.kpis_by_role
+FROM gather_unique_function_role GAFR
+LEFT JOIN gather_all_kpis GAK ON GAK.role = GAFR.role
+WHERE GAFR.role != GAK.role
+
+/*
+create_list_of_unseleted_kpis_by_function AS (
 SELECT GAFR.function, GAFR.role, GAK.kpis_by_role
 FROM gather_unique_function_role GAFR
 LEFT JOIN gather_all_kpis GAK ON GAK.function = GAFR.function
 WHERE GAFR.role != GAK.role
+*/
 )
 
 SELECT DISTINCT CLUK.function, CLUK.role, CLUK.kpis_by_role
-FROM create_list_of_unseleted_kpis CLUK
+FROM create_list_of_unseleted_kpis_by_role CLUK
 LEFT JOIN gather_all_kpis GAK ON GAK.role = CLUK.role AND GAK.kpis_by_role = CLUK.kpis_by_role 
 WHERE GAK.kpis_by_role IS NULL
