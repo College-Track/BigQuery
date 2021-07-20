b916bc8be702bcd2f7f69b4a1f25d88d12b0bacc
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 11:43:03 -0700

    Auto-commit: Tue Jul 20 2021 11:42:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/2_audit_kpi_no_submits.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/2_audit_kpi_no_submits.sql
@@ -1,2 +1,16 @@
-SELECT *
-    FROM `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis`
+WITH gather_all_kpis AS (
+SELECT function, role, kpis_by_role
+FROM `data-studio-260217.performance_mgt.role_kpi_selection` #clean list of KPIs by Role
+ORDER BY function, role
+),
+
+gather_unique_function_role AS (
+SELECT DISTINCT function function, role 
+FROM gather_all_kpis
+)
+
+
+SELECT all_kpis.*, kpis_submitted.*
+FROM gather_all_kpis AS all_kpis
+LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS kpis_submitted
+    ON all_kpis.kpis_by_role = kpis_submitted.role
