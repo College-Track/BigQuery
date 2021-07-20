5e7badab77eac10eb7ca99bac1353352e534cc21
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-19 20:29:13 -0700

    Auto-commit: Mon Jul 19 2021 20:29:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
@@ -4,6 +4,6 @@ FROM `data-studio-260217.performance_mgt.role_kpi_selection`
 ORDER BY function, role
 )
 
-gather_unique_function_role AS (
+--gather_unique_function_role AS (
 SELECT DISTINCT function function, role 
 FROM gather_all_kpis
