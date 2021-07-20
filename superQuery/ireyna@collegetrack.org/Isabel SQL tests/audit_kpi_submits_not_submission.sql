5cb75343f607cdea4895508d5f370df2331277c4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-19 20:22:33 -0700

    Auto-commit: Mon Jul 19 2021 20:22:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/audit_kpi_submits_not_submission.sql
@@ -1,2 +1,33 @@
- SELECT *
- FROM `data-warehouse-289815.google_sheets.team_kpi_target`
+/*
+CREATE OR REPLACE TABLE 'data-studio-260217.performance_mgt.fy22_audit_kpi_targets_no_submissions'
+OPTIONS
+    (
+    description= "This table pulls in targets not submitted by staff"
+    )
+AS
+*/ 
+
+--SELECT *
+--FROM `data-warehouse-289815.google_sheets.team_kpi_target`
+WITH gather_all_kpis AS (
+SELECT function, role, kpis_by_role
+FROM `data-studio-260217.performance_mgt.role_kpi_selection`
+ORDER BY function, role
+),
+
+gather_unique_function_role AS (
+SELECT DISTINCT function function, role 
+FROM gather_all_kpis
+), 
+
+create_list_of_unseleted_kpis AS (
+SELECT GAFR.function, GAFR.role, GAK.kpis_by_role
+FROM gather_unique_function_role GAFR
+LEFT JOIN gather_all_kpis GAK ON GAK.function = GAFR.function
+WHERE GAFR.role != GAK.role
+)
+
+SELECT DISTINCT CLUK.function, CLUK.role, CLUK.kpis_by_role
+FROM create_list_of_unseleted_kpis CLUK
+LEFT JOIN gather_all_kpis GAK ON GAK.role = CLUK.role AND GAK.kpis_by_role = CLUK.kpis_by_role 
+WHERE GAK.kpis_by_role IS NULL
