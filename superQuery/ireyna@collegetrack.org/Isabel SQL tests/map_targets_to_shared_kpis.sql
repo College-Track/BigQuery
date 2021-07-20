7e5977c6e721d287c797cedeba01bc3b6b5c4e15
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 09:48:32 -0700

    Auto-commit: Tue Jul 20 2021 09:48:26 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/map_targets_to_shared_kpis.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/map_targets_to_shared_kpis.sql
@@ -7,7 +7,7 @@
 CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis`
 OPTIONS
     (
-    description= "This table maps targets to KPIs shared across roles within Teams/Functions"
+    description= "This table maps submitted targets to KPIs shared across roles within Teams/Functions"
     )
 AS
 */
@@ -54,78 +54,90 @@ WHERE email_kpi <> "test@collegetrack.org"
 AND disregard_entry_op_hard_coded IS NULL
 ),
 
-/*
-targets_by_role AS (
-SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,
-    /*CASE WHEN 
-        GAK.function = kpi_targets_submitted.team_kpi AND
-        GAK.kpis_by_role = kpi_targets_submitted.select_kpi AND 
-        submission_id IS NULL
-        THEN target_fy22
-        ELSE 0
-    END AS mapped_targets --omit this line too
-FROM gather_all_kpis GAK
+prep_national_targets_by_role AS (
+SELECT target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, all_kpis.kpis_by_role,email_kpi,region_kpi
+FROM gather_all_kpis all_kpis
 LEFT JOIN kpi_targets_submitted 
-ON GAK.kpis_by_role = kpi_targets_submitted.select_kpi
---WHERE GAFR.select_role != GAK.role
-GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22
-)
-*/
+ON all_kpis.kpis_by_role = kpi_targets_submitted.select_kpi
+WHERE site_kpi = "0"
+    AND region_kpi ="0"
+GROUP BY team_kpi,select_role,kpis_by_role,select_kpi,target_fy22,email_kpi,region_kpi
+),
+
+#Targets submitted based on Role, Site and KPI (shared KPI)
+national_targets_by_role AS (
+SELECT team_kpi,region_kpi,target_fy22,kpis_by_role,email_kpi
+FROM prep_national_targets_by_role
+WHERE target_fy22 IS NOT NULL
+GROUP BY  target_fy22,team_kpi,kpis_by_role,email_kpi, region_kpi
+),
+
+#identify Site KPIs that are shared across different roles, and pull in the KPI target submitted for roles with same KPs (on same team)
+national_targets_shared_by_role AS (
+SELECT team_kpi,target_fy22,region_kpi, national_targets_by_role.kpis_by_role,role,email_kpi
+FROM gather_all_kpis
+LEFT JOIN national_targets_by_role ON gather_all_kpis.function = national_targets_by_role.team_kpi 
+AND national_targets_by_role.kpis_by_role = gather_all_kpis.kpis_by_role
+group by team_kpi,target_fy22,region_kpi,role ,kpis_by_role,email_kpi
+),
 
 prep_site_targets_by_role AS (
-SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,site_kpi
+SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,site_kpi,email_kpi
 FROM gather_all_kpis GAK
 LEFT JOIN kpi_targets_submitted 
 ON GAK.kpis_by_role = kpi_targets_submitted.select_kpi
 WHERE site_kpi <> "0"
-GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22,site_kpi
+GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22,site_kpi,email_kpi
 ),
 
 #Targets submitted based on Role, Site and KPI (shared KPI)
 site_targets_by_role AS (
-SELECT site_kpi,target_fy22,team_kpi,kpis_by_role
+SELECT site_kpi,target_fy22,team_kpi,kpis_by_role,email_kpi
 FROM prep_site_targets_by_role
 WHERE target_fy22 IS NOT NULL
-GROUP BY  target_fy22,team_kpi,kpis_by_role,site_kpi
-),
-
-prep_regional_targets_by_role AS (
-SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,region_kpi
-FROM gather_all_kpis GAK
-LEFT JOIN kpi_targets_submitted 
-ON GAK.kpis_by_role = kpi_targets_submitted.select_kpi
-WHERE region_kpi <> "0"
-GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22,region_kpi
-),
-
-#Targets submitted based on Role, Region and KPI (shared KPI)
-regional_targets_by_role AS (
-SELECT region_kpi,target_fy22,team_kpi,kpis_by_role
-FROM prep_regional_targets_by_role
-WHERE target_fy22 IS NOT NULL
-GROUP BY  target_fy22,team_kpi,kpis_by_role,region_kpi
-),
-
-#identify Regional KPIs that are shared, and pull in the KPI target submitted for roles with same KPs (on same team)
-regional_targets_shared_by_role AS (
-SELECT region_kpi,target_fy22,team_kpi,function, regional_targets_by_role.kpis_by_role,role ,
-FROM gather_all_kpis
-LEFT JOIN regional_targets_by_role ON gather_all_kpis.function = regional_targets_by_role.team_kpi 
-AND regional_targets_by_role.kpis_by_role = gather_all_kpis.kpis_by_role
-WHERE region_kpi <> "0"
-group by region_kpi,target_fy22,team_kpi,function,role ,kpis_by_role
+GROUP BY  target_fy22,team_kpi,kpis_by_role,site_kpi,email_kpi
 ),
 
 #identify Site KPIs that are shared across different roles, and pull in the KPI target submitted for roles with same KPs (on same team)
 site_targets_shared_by_role AS (
-SELECT site_kpi,target_fy22,team_kpi,function, site_targets_by_role.kpis_by_role,role ,
+SELECT site_kpi,target_fy22,team_kpi, site_targets_by_role.kpis_by_role,role ,email_kpi
 FROM gather_all_kpis
 LEFT JOIN site_targets_by_role ON gather_all_kpis.function = site_targets_by_role.team_kpi 
 AND site_targets_by_role.kpis_by_role = gather_all_kpis.kpis_by_role
 WHERE site_kpi <> "0"
-group by site_kpi,target_fy22,team_kpi,function,role ,kpis_by_role
+group by site_kpi,target_fy22,team_kpi,role ,kpis_by_role,email_kpi
+),
+
+prep_regional_targets_by_role AS (
+SELECT submission_id,target_fy22,kpi_targets_submitted.team_kpi, kpi_targets_submitted.select_role, GAK.kpis_by_role,region_kpi,email_kpi
+FROM gather_all_kpis GAK
+LEFT JOIN kpi_targets_submitted 
+ON GAK.kpis_by_role = kpi_targets_submitted.select_kpi
+WHERE region_kpi <> "0"
+GROUP BY team_kpi,select_role,kpis_by_role,function,select_kpi,submission_id,target_fy22,region_kpi,email_kpi
+),
+
+#Targets submitted based on Role, Region and KPI (shared KPI)
+regional_targets_by_role AS (
+SELECT region_kpi,target_fy22,team_kpi,kpis_by_role,email_kpi
+FROM prep_regional_targets_by_role
+WHERE target_fy22 IS NOT NULL
+GROUP BY  target_fy22,team_kpi,kpis_by_role,region_kpi,email_kpi
+),
+
+#identify Regional KPIs that are shared, and pull in the KPI target submitted for roles with same KPs (on same team)
+regional_targets_shared_by_role AS (
+SELECT region_kpi,target_fy22,team_kpi,regional_targets_by_role.kpis_by_role,role ,email_kpi
+FROM gather_all_kpis
+LEFT JOIN regional_targets_by_role ON gather_all_kpis.function = regional_targets_by_role.team_kpi 
+AND regional_targets_by_role.kpis_by_role = gather_all_kpis.kpis_by_role
+WHERE region_kpi <> "0"
+group by region_kpi,target_fy22,team_kpi,role ,kpis_by_role,email_kpi
 )
 
+SELECT national_targets_shared_by_role.*
+FROM national_targets_shared_by_role
+UNION ALL
 SELECT regional_targets_shared_by_role.*
 FROM regional_targets_shared_by_role
 UNION ALL
