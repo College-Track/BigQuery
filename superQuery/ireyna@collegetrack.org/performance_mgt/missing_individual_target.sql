25ad815719f88a4478f7b95ff3ef7415c7797037
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 22:30:38 -0700

    Auto-commit: Tue Jul 20 2021 22:30:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/missing_individual_target.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/missing_individual_target.sql
@@ -1,12 +1,3 @@
-/*
-CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.audit_kpi_weird_targets`
-OPTIONS
-    (
-    description= "This table pulls in targets that do not make sense for a numeric, percent or T/F target"
-    )
-AS 
-*/
-
 
 SELECT
     staff_list.first_name,
@@ -33,5 +24,3 @@ SELECT
     
 
 FROM`data-warehouse-289815.google_sheets.individual_kpi_target` AS kpi_targets_submitted
-LEFT JOIN  `data-warehouse-289815.google_sheets.staff_list` AS staff_list
-    ON kpi_targets_submitted.enter_your_college_track_email_address = staff_list.email_address
