1cf0267c3a24f2ab87dfc847e01f2a65294cbd4e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 22:12:12 -0700

    Auto-commit: Tue Jul 20 2021 22:12:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/missing_individual_target.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/missing_individual_target.sql
@@ -19,12 +19,17 @@ SELECT
     add_from_your_team_s_kpi_list,
     what_will_your_self_created_kpi_be_this_year_,
     CASE
-      WHEN enter_the_target_numeric_ IS NOT NULL THEN enter_the_target_numeric_
-      WHEN enter_the_target_percent_ iS NOT NULL THEN enter_the_target_percent_
-      WHEN what_is_the_type_of_target_ = "Goal is met" THEN 1 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
+      WHEN enter_the_target_numeric_kpi_list IS NOT NULL THEN enter_the_target_numeric_kpi_list
+      WHEN enter_the_target_percent_kpi_list iS NOT NULL THEN enter_the_target_percent_kpi_list
+      --WHEN what_is_the_type_of_target_kpi_list = "Goal is met" THEN 1 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
       ELSE NULL
-    END AS target_fy22,
-    
+    END AS target_fy22_kpi_list,
+    CASE
+      WHEN enter_the_target_percent_self_created IS NOT NULL THEN enter_the_target_percent_self_created
+      WHEN enter_the_target_numeric_self_created iS NOT NULL THEN enter_the_target_numeric_self_created
+      --WHEN what_is_the_type_of_target_kpi_list = "Goal is met" THEN 1 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
+      ELSE NULL
+    END AS target_fy22_kpi_self_created,
     
 FROM `data-warehouse-289815.google_sheets.staff_list` AS staff_list
 LEFT JOIN `data-warehouse-289815.google_sheets.individual_kpi_target` AS kpi_targets_submitted
