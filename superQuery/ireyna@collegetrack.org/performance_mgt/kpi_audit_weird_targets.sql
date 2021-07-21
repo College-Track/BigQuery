9479ce3b50e30d51cff2f709cdaf7f5fdd2e145f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 20:26:18 -0700

    Auto-commit: Tue Jul 20 2021 20:26:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
@@ -4,19 +4,14 @@ SELECT
     kpis_by_role,
     what_is_the_type_of_target_,
     target_fy22
- FROM `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
- LEFT JOIN `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
+FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
+ left join `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
+ 
     ON shared_kpis.site_region_team = raw_submitted_kpis.region_kpi
     AND Shared_kpis.kpis_by_role = raw_submitted_kpis.select_kpi
  WHERE target_fy22  > 100 
  OR target_fy22  < 1
-    AND mature_non_mature IN ('Mature_Regional_Staff','Mature_Regional_Staff')
-    AND site_kpi = "0"
+    AND region_kpi IS NULL
+    AND site_kpi IS NOT NULL
     AND what_is_the_type_of_target_ = 'Numeric (but not percent)'
     AND raw_submitted_kpis.email_kpi <> "test@collegetrack.org"
-GROUP BY 
-    site_region_team,
-    Role,
-    kpis_by_role,
-    what_is_the_type_of_target_,
-    target_fy22
