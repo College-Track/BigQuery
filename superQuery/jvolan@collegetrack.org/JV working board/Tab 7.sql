376d36f530acde60975358b36f8617ebca69e66d
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-21 09:41:26 -0700

    Auto-commit: Wed Jul 21 2021 09:41:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
@@ -1,9 +1,10 @@
 
     SELECT
+    workshop_display_name_c,
     dosage_types_c,
     first_session_date_c,
     last_session_date_c,
     sessions_c,
     
-    From `data-warehouse-289815.salesforce_clean.class_template`
+    From `data-warehouse-289815.salesforce.class_c`
     WHERE global_academic_semester_c = 'a3646000000dMXoAAM'
