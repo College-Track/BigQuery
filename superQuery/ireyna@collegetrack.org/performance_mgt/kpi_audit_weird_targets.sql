4fefb9846be860b4b509cb09b7fb31b257a9791a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 19:48:07 -0700

    Auto-commit: Tue Jul 20 2021 19:48:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
@@ -1,8 +1,43 @@
+ 
+ WITH 
+ --Next 3 queries: Flag entries for % targets over 100%
+ 
+ weird_percent_kpis_sites AS (
+ SELECT *
+ FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
+ LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
+    ON shared_kpis.site_region_team = raw_submitted_kpis.site_kpi
+ WHERE raw_submitted_kpis.enter_the_target_percent_  > 1
+    AND what_is_the_type_of_target_ = 'Percent'
+),
+
+weird_percent_kpis_regions AS (    
+SELECT *
+ FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
+ LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
+    ON shared_kpis.site_region_team = raw_submitted_kpis.region_kpi
+ WHERE raw_submitted_kpis.enter_the_target_percent_  > 1
+    AND what_is_the_type_of_target_ = 'Percent'
+),
+
+weird_percent_kpis_national AS (
+    SELECT *
+ FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
+ LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
+    ON shared_kpis.site_region_team = raw_submitted_kpis.team_kpi
+ WHERE raw_submitted_kpis.enter_the_target_percent_  > 1
+    AND region_kpi = "0"
+    AND site_kpi = "0"
+    AND what_is_the_type_of_target_ = 'Percent'
+),
+
+--Flag Numeric targets that are weird
+weird_numeric_targets AS (
 SELECT      
     site_region_team,
     Role,
     kpis_by_role,
-    what_is_the_type_of_target_,
+   -- what_is_the_type_of_target_,
     target_fy22
  FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
  LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
@@ -13,3 +48,12 @@ SELECT
     AND site_kpi = "0"
     AND what_is_the_type_of_target_ = 'Numeric (but not percent)'
     AND raw_submitted_kpis.email_kpi <> "test@collegetrack.org"
+GROUP BY 
+    site_region_team,
+    Role,
+    kpis_by_role,
+   -- what_is_the_type_of_target_,
+    target_fy22
+)
+select *
+from weird_numeric_targets
