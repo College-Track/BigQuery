d4debe28fb91d893d62bc1232e33900c9b315513
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 19:31:42 -0700

    Auto-commit: Tue Jul 20 2021 19:31:38 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
@@ -7,8 +7,8 @@ SELECT
  FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
  LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
     ON shared_kpis.site_region_team = raw_submitted_kpis.team_kpi
- WHERE raw_submitted_kpis.enter_the_target_numeric_  > 100 
- OR raw_submitted_kpis.enter_the_target_numeric_  < 1
+ WHERE target_fy22  > 100 
+ OR target_fy22  < 1
     AND region_kpi = "0"
     AND site_kpi = "0"
     AND what_is_the_type_of_target_ = 'Numeric (but not percent)'
