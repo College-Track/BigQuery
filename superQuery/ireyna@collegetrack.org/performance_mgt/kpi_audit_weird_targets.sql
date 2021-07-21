088671e01d12f8d76cd3d7510d65373a3afc3ab6
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 19:13:34 -0700

    Auto-commit: Tue Jul 20 2021 19:13:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
@@ -1,3 +1,5 @@
  SELECT *
  FROM `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
- WHERE target_fy22 > 1 OR target_fy22 = .1
+ LEFT JOIN `data-studio-260217.performance_mgt.fy22_team_kpis` AS submitted_kpis
+    ON shared_kpis.site_region_team = submitted_kpis.site_or_region
+ WHERE shared_kpis.target_fy22 > 1 OR shared_kpis.target_fy22 = .1
