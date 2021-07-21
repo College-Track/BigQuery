1de1f8e01b64c03e74b0715c3b0c8be6095c46f2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-21 10:50:53 -0700

    Identify missing Individual KPI based on email address only

--- superQuery/ireyna@collegetrack.org/performance_mgt/missing_individual_target.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/missing_individual_target.sql
@@ -1,3 +1,12 @@
+/*
+CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_kpi_audit_no_individual_kpi`
+OPTIONS
+    (
+    description= "This table pulls in staff that have not submitted using the Individual KPI Form"
+    )
+AS 
+*/
+
 WITH 
 
 submitted_individual_kpis AS (
@@ -31,20 +40,16 @@ SELECT
 FROM`data-warehouse-289815.google_sheets.individual_kpi_target` AS kpi_targets_submitted
 )
 
---Identify missing KPI submission using email address
---no_individual_kpi_email AS (
-
 SELECT 
     staff_list.first_name,
     staff_list.last_name,
-    staff_list.full_name,
-    great_select_your_name,
     staff_list.email_address,
-    thanks_select_your_team_program_area,
+    enter_your_college_track_email_address,
     staff_list.team,
     staff_list.program_area,
     staff_list.site,
     staff_list.region
 FROM  `data-warehouse-289815.google_sheets.staff_list` staff_list
-LEFT JOIN  submitted_individual_kpis 
-    ON program_area = thanks_select_your_team_program_area
+LEFT JOIN  submitted_individual_kpis
+    ON email_address = enter_your_college_track_email_address
+WHERE enter_your_college_track_email_address IS NULL
