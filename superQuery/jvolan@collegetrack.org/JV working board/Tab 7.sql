06ca229513b01006982cb44f6fc68508b905d109
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-21 11:02:31 -0700

    Auto-commit: Wed Jul 21 2021 11:02:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
@@ -3,8 +3,10 @@ with get_key AS
     SELECT
     *
     FROM `data-studio-260217.workshop_dosage_duration_tracker.fy22_dosage_expectation_key` 
-)
+),
 
+gather_workshop_data AS
+(
     SELECT
     site_c,
     workshop_display_name_c,
@@ -22,3 +24,18 @@ with get_key AS
     LEFT JOIN get_key ON get_key.Dosage_type = dosage_types_c
     WHERE global_academic_semester_c = 'a3646000000dMXoAAM'
     AND dosage_types_c IN ('Acceleration','Test Prep','Tutoring','Student Life')
+)
+
+    SELECT
+    site_c,
+    dosage_types_c,
+    MAX(sessions_c),
+    MAX(at_total_mins),
+    MAX(CASE
+        WHEN Total_duration_min > at_total_mins THEN 'Less than min required dosage'
+        WHEN Total_duration_max < at_total_mins THEN 'More than max required dosage'
+        ELSE 'Dosage meets expectations'
+    END) AS meeting_dosage_bucket
+        
+    FROM gather_workshop_data
+    GROUP BY site_c, dosage_types_c
