849818bedbe3717b4860ee622e8488a3578f3383
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-20 19:30:00 -0700

    Auto-commit: Tue Jul 20 2021 19:29:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/kpi_audit_weird_targets.sql
@@ -2,6 +2,7 @@ SELECT
     site_region_team,
     Role,
     kpis_by_role,
+    what_is_the_type_of_target_,
     target_fy22
  FROM `data-warehouse-289815.google_sheets.team_kpi_target` AS raw_submitted_kpis
  LEFT JOIN `data-studio-260217.performance_mgt.fy22_targets_to_shared_kpis` AS shared_kpis
