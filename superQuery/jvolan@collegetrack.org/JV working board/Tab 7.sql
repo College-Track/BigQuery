a3d404d46f84e0b7c5e65571e43721aee4743479
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-21 10:53:59 -0700

    Auto-commit: Wed Jul 21 2021 10:53:55 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
@@ -1,3 +1,23 @@
-SELECT
+with get_key AS
+(  
+    SELECT
     *
-    FROM `data-studio-260217.workshop_dosage_duration_tracker.fy22_dosage_expectation_key`
+    FROM `data-studio-260217.workshop_dosage_duration_tracker.fy22_dosage_expectation_key` 
+)
+
+    SELECT
+    site_c,
+    workshop_display_name_c,
+    dosage_types_c,
+    (sessions_c * duration_c) AS at_total_mins,
+    sessions_c,
+    duration_c,
+    first_session_date_c,
+    last_session_date_c,
+    get_key.Dosage_type,
+    get_key.Total_duration_min,
+    get_key.Total_duration_max
+    
+    From `data-warehouse-289815.salesforce.class_c`
+    LEFT JOIN get_key ON get_key.Dosage_type = dosage_types_c
+    WHERE global_academic_semester_c = 'a3646000000dMXoAAM'
