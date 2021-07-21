709d8023a9a0d5acbdd071d1bb6eb5ea17847148
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-21 09:59:53 -0700

    Auto-commit: Wed Jul 21 2021 09:59:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 7.sql
@@ -4,12 +4,22 @@
     workshop_display_name_c,
     dosage_types_c,
     (sessions_c * duration_c) AS at_total_mins,
-    540 AS acceleration_min,
-    675 AS acceleration_rec,
     sessions_c,
     duration_c,
     first_session_date_c,
-    last_session_date_c
+    last_session_date_c,
+--hardcode in the min & reccomended # of mins for each dosage type based on fy22 dosage expectations
+    540 AS acceleration_min,
+    675 AS acceleration_upper,
+    360 AS content_support_min,
+    450 AS content_support_upper,
+    540 AS test_prep_min,
+    675 AS test_prep_upper,
+    270 AS sld_min,
+    450 AS sld_upper,
+    
+    
+    
     
     From `data-warehouse-289815.salesforce.class_c`
     WHERE global_academic_semester_c = 'a3646000000dMXoAAM'
