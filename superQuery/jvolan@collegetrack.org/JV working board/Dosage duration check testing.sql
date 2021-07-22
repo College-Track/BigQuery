783c6b0515cc11459808d80a0c91559dcfb22773
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-22 07:07:10 -0700

    Auto-commit: Thu Jul 22 2021 07:07:03 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/Dosage duration check testing.sql
@@ -0,0 +1,56 @@
+with get_key AS
+(  
+--finalize dosage req based on convo with BR. add AY column
+    SELECT
+    *
+    FROM `data-studio-260217.workshop_dosage_duration_tracker.fy22_dosage_expectations_key` 
+    WHERE academic_year = 'AY 2021-22'
+),
+
+gather_workshop_data AS
+(
+    SELECT
+    site_c,
+    workshop_display_name_c,
+    id,
+    dosage_types_c,
+    (sessions_c * duration_c) AS at_total_mins,
+    sessions_c,
+    duration_c,
+    first_session_date_c,
+    last_session_date_c,
+    get_key.Dosage_type,
+    get_key.Total_duration_min,
+    get_key.Total_duration_max
+    
+    From `data-warehouse-289815.salesforce.class_c`
+    LEFT JOIN get_key ON get_key.Dosage_type = dosage_types_c
+    WHERE global_academic_semester_c = 'a3646000000dMXoAAM'
+    AND dosage_types_c IN ('Acceleration','Test Prep','Tutoring','Student Life')
+)
+
+    SELECT
+    site_c,
+    dosage_types_c,
+    CASE
+        WHEN Total_duration_min > at_total_mins THEN 'Less than min required dosage'
+        WHEN Total_duration_max < at_total_mins THEN 'More than max required dosage'
+        ELSE 'Dosage meets expectations'
+    END AS meeting_dosage_bucket,
+    CASE
+        WHEN 
+        (Total_duration_min > at_total_mins 
+        OR Total_duration_max < at_total_mins) THEN 0
+        ELSE 1
+    END AS meeting_dosage_yn
+        
+    FROM gather_workshop_data
+
+/*could use this format to 
+- display list of workshops (grouped or filtered by dosage type) that are below , above, or just not meeting requirements
+- calc overall % of workshops meeting dosage requirements by site / dosage type
+- count total workshops / sessions (if needed) by dosage type
+*/
+   
+
+  
