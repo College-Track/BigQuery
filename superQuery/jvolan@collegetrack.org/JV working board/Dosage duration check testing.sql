e1b04bbf5f7d261d9ba706435a99e11bcf782f46
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-22 14:54:21 -0700

    Auto-commit: Thu Jul 22 2021 14:54:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Dosage duration check testing.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Dosage duration check testing.sql
@@ -3,19 +3,23 @@ with get_key AS
 --finalize dosage req based on convo with BR. add AY column
     SELECT
     Dosage_type,
-    CASE
-        WHEN 
-        --placeholder to show how I'll do the senior advisory spring vs. fall diference. besides that total_duration_mins should be same during course of year for all others. 
-        --tested it and it works. just swap dosage type & get months how you want it when new fields are in production
-        (EXTRACT(MONTH FROM CURRENT_DATE()) IN (1,2,3,4,5,6)
-        AND Dosage_type = "Tutoring") THEN (Total_duration_min / 2)
-        ELSE Total_duration_min
-    END AS Total_duration_min,
+    Total_duration_min,
     academic_year,
     FROM `data-studio-260217.workshop_dosage_duration_tracker.fy22_dosage_expectations_key` 
     WHERE academic_year = 'AY 2021-22'
+    AND Term = 'Spring'
 ),
 
+get_site_name AS
+(
+    SELECT
+    MAX(site_short) AS site_short,
+    site_c AS c_site,
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    GROUP BY site_c
+),
+    
 gather_workshop_data AS
 (
     SELECT
@@ -31,11 +35,11 @@ gather_workshop_data AS
     last_session_date_c,
     get_key.Dosage_type,
     get_key.Total_duration_min,
-    c.site_short
+    get_site_name.site_short
 
     From `data-warehouse-289815.salesforce.class_c`cl
     LEFT JOIN get_key ON get_key.Dosage_type = dosage_types_c
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.site_c = cl.site_c
+    LEFT JOIN get_site_name ON c_site = cl.site_c
     WHERE global_academic_semester_c = 'a3646000000dMXoAAM'
     AND dosage_types_c IN ('Acceleration','Test Prep','Tutoring','Student Life')
 )
@@ -58,11 +62,6 @@ gather_workshop_data AS
         
     FROM gather_workshop_data
 
-/*could use this format to 
-- display list of workshops (grouped or filtered by dosage type) that are below , above, or just not meeting requirements
-- calc overall % of workshops meeting dosage requirements by site / dosage type
-- count total workshops / sessions (if needed) by dosage type
-*/
    
 
   
