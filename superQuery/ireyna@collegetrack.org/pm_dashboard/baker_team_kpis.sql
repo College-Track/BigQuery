4a7fc73f4cec14b853ffd35f7e1ffe86c9927f31
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-07-27 10:45:33 -0700

    Auto-commit: Tue Jul 27 2021 10:45:23 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/baker_team_kpis.sql
@@ -291,10 +291,10 @@ LEFT JOIN `data-studio-260217.performance_mgt.fy22_projections` Projections ON C
 )
 
 SELECT distinct *,
-CASE WHEN target_submitted = 'Submitted' THEN 1
-ELSE 0
-END AS count_of_submitted_targets,
-CASE WHEN target_submitted != "Not Submitted" THEN 1 -- "Not Required" THEN 1
+--CASE WHEN target_submitted = 'Submitted' THEN 1
+--ELSE 0
+--END AS count_of_submitted_targets,
+CASE WHEN target_submitted = "Submitted" THEN 1 -- "Not Required" THEN 1
 ELSE 0
 END AS count_of_targets
 FROM correct_missing_site_region
