40eb2bb0023e622071862be8183b4c27d3dfc247
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-28 13:40:48 -0700

    Auto-commit: Wed Jul 28 2021 13:40:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/PAT Creation Prep.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/PAT Creation Prep.sql
@@ -3,7 +3,12 @@
     Contact_Id,
     AT_Id AS previous_academic_semester,
     school_c,
-    enrollment_status_c,
+    CASE
+        WHEN 
+        (term_c = 'Summer'
+        AND enrollment_status_c IS NULL) THEN persistence_at_prev_enrollment_status_c
+        ELSE enrollment_status_c
+    END AS enrollment_status,
     '01246000000RNnHAAW' AS record_type_id,
     
     
@@ -11,3 +16,4 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE current_as_c = TRUE
     AND college_track_status_c = '15A'
+    
