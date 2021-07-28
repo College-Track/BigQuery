b936597fe6599371ec4457ffed3d77cf8e92b908
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-07-28 15:45:36 -0700

    Auto-commit: Wed Jul 28 2021 15:45:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/PAT Creation Prep.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/PAT Creation Prep.sql
@@ -20,26 +20,95 @@ SELECT
         ELSE minor_c
     END AS minor,
     --adv rubric fields that carry over term to term
-    financial_aid_package_c,
-    free_checking_account_c,
-    e_fund_c,
-    repayment_plan_c,
-    loan_exit_c,
-    academic_networking_50_cred_c,
-    academic_networking_over_50_credits_c,
-    extracurricular_activity_c,
-    finding_opportunities_75_c,
-    resume_cover_letter_c,
-    career_counselor_25_credits_c,
-    career_field_2550_credits_c,
-    resources_2550_credits_c,
-    internship_5075_credits_c,
-    post_graduate_plans_5075_creds_c,
-    post_graduate_opportunities_75_cred_c,
-    alumni_network_75_credits_c,
     CASE
-        WHEN 
-        (term_c = 'Summer'
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE financial_aid_package_c
+    END AS financial_aid_package,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE free_checking_account_c
+    END AS free_checking_account,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     e_fund_c
+    END AS     e_fund,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     repayment_plan_c
+    END AS     repayment_plan,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE loan_exit_c
+    END AS loan_exit,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     academic_networking_50_cred_c
+    END AS     academic_networking_50_cred,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     academic_networking_over_50_credits_c
+    END AS     academic_networking_over_50_credits,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     extracurricular_activity_c
+    END AS     extracurricular_activity,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     finding_opportunities_75_c
+    END AS     finding_opportunities_75,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     resume_cover_letter_c
+    END AS     resume_cover_letter,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     career_counselor_25_credits_c
+    END AS     career_counselor_25_credits,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     career_field_2550_credits_c
+    END AS     career_field_2550_credits,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     resources_2550_credits_c
+    END AS     resources_2550_credits,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     internship_5075_credits_c
+    END AS     internship_5075_credits,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     post_graduate_plans_5075_creds_c
+    END AS     post_graduate_plans_5075_creds,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     post_graduate_opportunities_75_cred_c
+    END AS     post_graduate_opportunities_75_cred,
+    
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        ELSE     alumni_network_75_credits_c
+    END AS     alumni_network_75_credits,
+    --for active, if summer and enrollment status is blank use prev. at enrollment status.
+    CASE
+        WHEN college_track_status_c <> '15A' THEN NULL 
+        WHEN (college_track_status_c = '15A'
+        AND term_c = 'Summer'
         AND enrollment_status_c IS NULL) THEN persistence_at_prev_enrollment_status_c
         ELSE enrollment_status_c
     END AS enrollment_status,
