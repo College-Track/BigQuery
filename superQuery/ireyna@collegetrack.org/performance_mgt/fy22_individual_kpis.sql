775b5fc961b6ea1ce34406455aa7fe6fe194f12a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-03 13:09:15 -0700

    Auto-commit: Tue Aug 03 2021 13:09:12 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy22_individual_kpis.sql
@@ -0,0 +1,33 @@
+/*
+CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_individual_kpis`
+OPTIONS
+    (
+    description= "This table pulls in targets submitted by staff using the Individual KPI Target submission form"
+    )
+AS
+*/
+
+SELECT 
+    /*CASE
+      WHEN enter_the_target_numeric_kpi_list IS NOT NULL THEN enter_the_target_numeric_kpi_list
+      WHEN enter_the_target_percent_kpi_list IS NOT NULL THEN enter_the_target_percent_kpi_list
+      WHEN what_is_the_type_of_target_kpi_list = "I am not adding another KPI from my team's list" THEN 0 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
+      ELSE NULL
+    END AS target_fy22_kpi,
+    CASE
+      WHEN enter_the_target_percent_kpi_list_2 IS NOT NULL THEN enter_the_target_percent_kpi_list_2
+      ELSE NULL
+    END AS target_fy22_kpi,
+    CASE
+      WHEN enter_the_target_percent_kpi_list_3 IS NOT NULL THEN enter_the_target_percent_kpi_list_3
+      ELSE NULL
+    END AS target_fy22_kpi,
+    CASE
+      WHEN enter_the_target_percent_self_created IS NOT NULL THEN enter_the_target_percent_self_created
+      WHEN enter_the_target_numeric_self_created IS NOT NULL THEN enter_the_target_numeric_self_created
+      WHEN what_is_the_type_of_target_self_created = "I am not adding a self-created KPI" THEN 0 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
+      ELSE NULL
+    END AS target_fy22_kpi
+    */
+    *
+FROM`data-warehouse-289815.google_sheets.individual_kpi_target` AS indiv_kpis
