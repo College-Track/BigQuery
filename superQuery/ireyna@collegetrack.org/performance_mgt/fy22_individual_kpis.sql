63d0c5e83d33f1073e0437dd6641f7cfcc85f5eb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-03 13:21:32 -0700

    Auto-commit: Tue Aug 03 2021 13:21:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy22_individual_kpis.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy22_individual_kpis.sql
@@ -9,9 +9,24 @@ AS
 WITH
 prep_individual_kpis AS (
 SELECT 
-* EXCEPT (response_url,enter_the_target_percent_kpi_list,enter_the_target_numeric_kpi_list),
-case when enter_the_target_percent_kpi_list is null then 0 end as enter_the_target_percent_kpi_list,
-case when enter_the_target_numeric_kpi_list is null then 0 end as enter_the_target_numeric_kpi_list
+    * EXCEPT (response_url,enter_the_target_percent_kpi_list,enter_the_target_numeric_kpi_list,enter_the_target_percent_kpi_list_2,enter_the_target_percent_kpi_list_3),
+    CASE 
+        WHEN enter_the_target_percent_kpi_list IS NULL 
+        THEN 0 
+    END AS enter_the_target_percent_kpi_list,
+        CASE 
+        WHEN enter_the_target_numeric_kpi_list IS NULL 
+        THEN 0 
+    END AS enter_the_target_numeric_kpi_list,
+    CASE 
+        WHEN enter_the_target_percent_kpi_list_2 IS NULL 
+        THEN 0 
+    END AS enter_the_target_percent_kpi_list_2,
+        CASE 
+        WHEN enter_the_target_percent_kpi_list_3 IS NULL 
+        THEN 0 
+    END AS enter_the_target_percent_kpi_list_3
+ 
 /*
     CASE
       WHEN enter_the_target_numeric_kpi_list IS NOT NULL THEN enter_the_target_numeric_kpi_list
@@ -39,13 +54,26 @@ FROM`data-warehouse-289815.google_sheets.individual_kpi_target` AS indiv_kpis
 WHERE Indicator_Disregard_Entry IS NULL
 )
 SELECT 
-*,
+* EXCEPT (enter_the_target_numeric_kpi_list,enter_the_target_percent_kpi_list,enter_the_target_percent_kpi_list_2,enter_the_target_percent_kpi_list_3),
  CASE
       WHEN enter_the_target_numeric_kpi_list IS NOT NULL THEN enter_the_target_numeric_kpi_list
       WHEN enter_the_target_percent_kpi_list IS NOT NULL THEN enter_the_target_percent_kpi_list
       WHEN what_is_the_type_of_target_kpi_list = "I am not adding another KPI from my team's list" THEN 0 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
       ELSE NULL
+    END AS target_fy22_kpi,
+    CASE
+      WHEN enter_the_target_percent_kpi_list_2 IS NOT NULL THEN enter_the_target_percent_kpi_list_2
+      ELSE NULL
+    END AS target_fy22_kpi,
+    CASE
+      WHEN enter_the_target_percent_kpi_list_3 IS NOT NULL THEN enter_the_target_percent_kpi_list_3
+      ELSE NULL
+    END AS target_fy22_kpi,
+    CASE
+      WHEN enter_the_target_percent_self_created IS NOT NULL THEN enter_the_target_percent_self_created
+      WHEN enter_the_target_numeric_self_created IS NOT NULL THEN enter_the_target_numeric_self_created
+      WHEN what_is_the_type_of_target_self_created = "I am not adding a self-created KPI" THEN 0 --   WHEN enter_the_target_non_numeric_ IS NOT NULL THEN enter_the_target_non_numeric_count
+      ELSE NULL
     END AS target_fy22_kpi
-
 
 FROM prep_individual_kpis
