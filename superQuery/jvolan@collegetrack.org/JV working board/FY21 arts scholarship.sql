85ed00527c688c61fa84a3672adea8f8f8b356e2
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-05 13:50:12 -0700

    Auto-commit: Thu Aug 05 2021 13:50:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/FY21 arts scholarship.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/FY21 arts scholarship.sql
@@ -5,5 +5,5 @@ SELECT
     cat.academic_year_c
     
     FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean` st
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` cat ON academic_semester_c = id
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` cat ON st.academic_semester_c = cat.AT_Id
     WHERE st.record_type_id = '01246000000ZNhtAAG'
