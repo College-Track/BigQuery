c9cb776e608366af183f69ea13fea506313b65da
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-05 12:05:16 -0700

    Auto-commit: Thu Aug 05 2021 12:05:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 6.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 6.sql
@@ -1,9 +1,9 @@
 SELECT
-    student_18_digit_contact_id_c AS s_contact_id,
-    scholarship_c,
-    SUM(amount_c) AS total_amount,
+    student_18_digit_contact_id_c AS bb_contact_id,
+    SUM(amount_c) AS fy21_bb_total,
+    
     
     FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
-    WHERE scholarship_c IN ('DOOR','College Track Emergency Fund')
-    AND academic_year_c = 'AY 2020-21'
-    GROUP BY student_18_digit_contact_id_c, scholarship_c
+    WHERE academic_year_c = 'AY 2020-21'
+    AND record_type_id = '01246000000ZNhtAAG'
+    GROUP BY student_18_digit_contact_id_c
