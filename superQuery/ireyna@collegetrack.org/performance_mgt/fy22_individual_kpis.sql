529051e9df04641bffb946fe32345d88e45f930d
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-09 14:42:54 -0700

    Auto-commit: Mon Aug 09 2021 14:42:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy22_individual_kpis.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy22_individual_kpis.sql
@@ -1,12 +1,11 @@
 
-/*
+
 CREATE OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_individual_kpis`
 OPTIONS
     (
     description= "This table pulls in targets submitted by staff using the Individual KPI Target submission form"
     )
 AS
-*/
 
 WITH
 prep_kpis AS (
