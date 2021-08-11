6b81a5d4fbab9abc52ba15f3692374e8bc108578
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-11 09:55:01 -0700

    Auto-commit: Wed Aug 11 2021 09:54:55 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 Student Outcomes.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 Student Outcomes.sql
@@ -37,19 +37,6 @@ gather_attendance AS
         WHEN global_academic_semester_c = 'a3646000000dMXoAAM' THEN enrolled_sessions_c
         ELSE 0
         END) AS sp2021_enrolled,   
-    MAX(CASE
-        WHEN 
-        (global_academic_semester_c = 'a3646000000dMXnAAM'
-        AND enrolled_sessions_c >0) THEN (attended_workshops_c / enrolled_sessions_c)
-        ELSE 0
-        END) AS f2020_attendance_rate,
-    MAX(CASE
-        WHEN 
-        (global_academic_semester_c = 'a3646000000dMXoAAM'
-        AND enrolled_sessions_c >0) THEN (attended_workshops_c / enrolled_sessions_c)
-        ELSE 0
-        END) AS sp2021_attendance_rate,
-    MAX(attended_workshops_c/enrolled_sessions_c) AS fall_spring_overall_attendance_rate,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE global_academic_semester_c IN ('a3646000000dMXnAAM','a3646000000dMXoAAM')
