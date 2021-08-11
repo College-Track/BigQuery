c839a70364fc55be5d12fa294f907bd25d1149df
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-11 10:05:26 -0700

    Auto-commit: Wed Aug 11 2021 10:05:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 Student Outcomes.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 Student Outcomes.sql
@@ -50,20 +50,32 @@ calc_attendance AS
     SELECT
     *,
     CASE
-        WHEN (total_attended / total_enrolled) >=.8 THEN 1 ELSE 0 END AS overall_f_s_80,
+        WHEN ((total_attended / total_enrolled) >=.8 
+        AND total_enrolled >0)
+        THEN 1 ELSE 0 END AS overall_f_s_80,
         
     CASE
-        WHEN (total_attended / total_enrolled) >=.7 THEN 1 ELSE 0 END AS overall_f_s_70,
+        WHEN ((total_attended / total_enrolled) >=.7 
+        AND total_enrolled>0)
+        THEN 1 ELSE 0 END AS overall_f_s_70,
     
     CASE
-        WHEN (f2020_attended / f2020_enrolled) >=.8 THEN 1 ELSE 0 END AS f2020_80,
+        WHEN ((f2020_attended / f2020_enrolled) >=.8
+        AND f2020_enrolled >0)
+        THEN 1 ELSE 0 END AS f2020_80,
     CASE
-        WHEN (f2020_attended / f2020_enrolled) >=.7 THEN 1 ELSE 0 END AS f2020_70,
+        WHEN ((f2020_attended / f2020_enrolled) >=.7
+        AND f2020_enrolled >0)
+        THEN 1 ELSE 0 END AS f2020_70,
         
     CASE
-        WHEN (sp2021_attended / sp2021_enrolled) >=.8 THEN 1 ELSE 0 END AS sp2021_80,
+        WHEN ((sp2021_attended / sp2021_enrolled) >=.8
+        AND sp2021_enrolled >0)
+        THEN 1 ELSE 0 END AS sp2021_80,
     CASE
-        WHEN (sp2021_attended / sp2021_enrolled) >=.7 THEN 1 ELSE 0 END AS sp2021_70,
+        WHEN ((sp2021_attended / sp2021_enrolled) >=.7
+        AND sp2021_enrolled >0)
+        THEN 1 ELSE 0 END AS sp2021_70,
         
     FROM gather_attendance
 )
