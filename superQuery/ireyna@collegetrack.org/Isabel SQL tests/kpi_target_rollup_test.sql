94a3de3850ce8d01d855b33c14eac2fb76c3e951
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-11 15:01:56 -0700

    Auto-commit: Wed Aug 11 2021 15:01:50 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/Isabel SQL tests/kpi_target_rollup_test.sql
+++ superQuery/ireyna@collegetrack.org/Isabel SQL tests/kpi_target_rollup_test.sql
@@ -1,12 +1,17 @@
-SELECT
-function, 
+SELECT 
 role,
-kpis_by_role,
-    (SELECT t2.role 
+    
+    (SELECT t2.kpis_by_role 
     FROM `data-studio-260217.performance_mgt.fy22_team_kpis` AS t2 
-    WHERE  (t2.national = 1 or t2.hr_people = 1) and t2.kpis_by_role=t1.kpis_by_role
-    GROUP BY t2.role) AS national_role_program_kpi_table,
-student_count,
-count_of_targets
+    WHERE t2.kpis_by_role = t1.kpis_by_role 
+    AND t1.program =1 
+    GROUP BY t2.kpis_by_role,t1.program) AS national_rollup_kpi
+--SUM(student_count) AS national_rollup_student_sum
+
 FROM `data-studio-260217.performance_mgt.fy22_team_kpis` AS t1
-WHERE program = 1
+WHERE national = 1 or hr_people = 1
+GROUP BY 
+kpis_by_role,
+program,
+function,
+role
