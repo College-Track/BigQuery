4392af3209147ff38d5ca1a2b5be81155f399875
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-12 16:05:57 -0700

    Auto-commit: Thu Aug 12 2021 16:05:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 GPA add.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 GPA add.sql
@@ -23,13 +23,16 @@ AT_Term_GPA AS e_gpa,
     row_number() over(partition by Contact_Id order by start_date_c) as rn
 from gather_AT
 )a where rn=1
-)
+),
 
+join_data AS    
+(
     SELECT
     Contact_Id,
     high_school_graduating_class_c,
     site_short,
     College_Track_Status_Name,
+    GAS_Name,
     earliest_AT_term_gpa.e_gpa,
     AT_Term_GPA,
     gpa_growth_prev_semester_c,
@@ -40,3 +43,36 @@ from gather_AT
     WHERE site_short = 'Ward 8'
     AND ay_2020_21_student_served_c = "High School Student"
     AND GAS_Name IN ('Fall 2020-21 (Semester)', 'Spring 2020-21 (Semester)')
+)
+    SELECT
+    Contact_Id,
+    site_short,
+    e_gpa,
+    CASE
+        WHEN GAS_Name = 'Fall 2020-21 (Semester)'
+        AND AT_Cumulative_GPA >=3 THEN 1
+        ELSE 0 
+        END AS f_cgpa_3,
+    CASE
+        WHEN GAS_Name = 'Spring 2020-21 (Semester)'
+        AND AT_Term_GPA IS NULL THEN 1
+        ELSE 0
+        END AS missing_spring_gpa,
+    CASE
+        WHEN GAS_Name = 'Spring 2020-21 (Semester)'
+        AND AT_Term_GPA IS NOT NULL THEN 1
+        ELSE 0
+        END AS sp_t_gpa_growth_denom,
+    CASE
+        WHEN GAS_Name = 'Spring 2020-21 (Semester)'
+        AND AT_Term_GPA IS NOT NULL
+        AND (AT_Term_GPA - e_gpa) >0 THEN 1
+        ELSE 0
+        END AS sp_t_gpa_growth_num,
+        
+    FROM join_data
+
+    
+
+
+
