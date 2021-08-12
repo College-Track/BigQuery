041ab22f3197486194fe60488ae160ed50d25bc8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-12 07:05:09 -0700

    Auto-commit: Thu Aug 12 2021 07:05:04 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/FY21 arts scholarship.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/FY21 arts scholarship.sql
@@ -1,9 +1,20 @@
-SELECT
-    st.student_c,
+  SELECT
+    st.student_c AS e_contact_id,
     st.academic_semester_c,
-    st.amount_c,
+    amount_c,
     cat.academic_year_c
-    
+
     FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean` st
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` cat ON st.academic_semester_c = cat.AT_Id
-    WHERE st.record_type_id = '01246000000ZNhtAAG'
+    WHERE scholarship_c = 'a3B46000000HX7xEAG'
+),
+
+fy21_efund AS
+(
+    SELECT
+    e_contact_id,
+    SUM(amount_c) AS fy21_efund_total,
+
+    FROM get_efund
+    WHERE academic_year_c = 'a1b46000000dRR8AAM'
+    GROUP BY e_contact_id
