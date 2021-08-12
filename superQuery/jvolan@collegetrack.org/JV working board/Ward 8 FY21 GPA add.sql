97a6326ee5a40057b5500394cbcd8142e31a65e6
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-12 15:42:26 -0700

    Auto-commit: Thu Aug 12 2021 15:42:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 GPA add.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 GPA add.sql
@@ -1,11 +1,20 @@
-SELECT
+WITH gather_AT AS
+(
+    SELECT
     Contact_Id,
-    start_date_c
+    max(AT_Term_GPA) AS AT_Term_GPA,
+    max(start_date_c) AS start_date_c
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = 'Ward 8'
     AND ay_2020_21_student_served_c = "High School Student"
     AND AT_Term_GPA IS NOT NULL
-    GROUP BY Contact_Id, start_date_c
+    GROUP BY Contact_Id
     ORDER BY start_date_c ASC
-    LIMIT 1
+)
+
+    select * from
+(
+select AT_Term_GPA, row_number() over(partition by Contact_Id order by start_date_c) as rn
+from gather_AT
+)a where rn=1
