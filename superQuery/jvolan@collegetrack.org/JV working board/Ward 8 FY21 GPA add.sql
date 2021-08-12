8f8c2045d5adf6b0c0600eba4ecb3a6139e5a0c2
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-12 15:16:41 -0700

    Auto-commit: Thu Aug 12 2021 15:16:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 GPA add.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Ward 8 FY21 GPA add.sql
@@ -7,4 +7,4 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = 'Ward 8'
     AND ay_2020_21_student_served_c = "High School Student"
-    AND GAS_Name IN ('Fall 2020-21 (Semester), Spring 2020-21 (Semester)')
+    AND GAS_Name IN ('Fall 2020-21 (Semester)', 'Spring 2020-21 (Semester)')
