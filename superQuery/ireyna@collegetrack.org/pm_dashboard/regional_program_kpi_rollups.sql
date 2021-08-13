682a4b8335101bf727c7df9466757cba9b76fd38
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-13 14:34:30 -0700

    Auto-commit: Fri Aug 13 2021 14:34:26 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/regional_program_kpi_rollups.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/regional_program_kpi_rollups.sql
@@ -15,7 +15,10 @@ regional_kpis AS (
 SELECT 
 function AS regional_function,
 role AS regional_role,
-kpis_by_role AS regional_rollup_kpi,
+CASE 
+    WHEN kpis_by_role = '% of all students with GPA 3.25+' THEN '% of students with a 3.25+ cumulative HS GPA'
+    ELSE kpis_by_role
+END AS regional_rollup_kpi,
 site_or_region
 
 FROM `data-studio-260217.performance_mgt.fy22_team_kpis` 
