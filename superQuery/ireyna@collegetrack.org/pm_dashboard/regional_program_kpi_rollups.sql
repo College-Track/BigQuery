83335e26726451f64f8f6f91c8bcefeae1b9a593
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-13 12:48:54 -0700

    Auto-commit: Fri Aug 13 2021 12:48:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/regional_program_kpi_rollups.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/regional_program_kpi_rollups.sql
@@ -2,7 +2,8 @@ SELECT
 CASE
     WHEN kpis_by_role = '% of entering 9th grade students who are low-income AND first-gen'
     THEN program = 1
-    ELSE FALSE
-END AS program
+    ELSE false
+END AS program,
+kpis_by_role
 
 FROM `data-studio-260217.performance_mgt.fy22_team_kpis` 
