265321d60869e0f65ae156429356ccac34ebacdb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-13 14:43:43 -0700

    Auto-commit: Fri Aug 13 2021 14:43:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/pm_dashboard/regional_program_kpi_rollups.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/regional_program_kpi_rollups.sql
@@ -1,8 +1,31 @@
-SELECT 
-function AS regional_function,
-role AS regional_role,
-kpis_by_role AS regional_rollup_kpi,
-site_or_region
+SELECT
+kpis_by_role,
+student_count,
+target_numerator,
+count_of_targets,
+CASE
+    WHEN site_or_region = 'East Palo Alto' THEN 'NOR CAL'
+    WHEN site_or_region = 'Oakland' THEN 'NOR CAL'
+    WHEN site_or_region = 'San Francisco' THEN 'NOR CAL'
+    WHEN site_or_region = 'Sacramento' THEN 'NOR CAL'
+    WHEN site_or_region = 'Boyle Heights' THEN 'LA'
+    WHEN site_or_region = 'Watts' THEN 'LA'
+    WHEN site_or_region = 'Crenshaw' THEN 'LA'
+    WHEN site_or_region = 'Aurora' THEN 'CO'
+    WHEN site_or_region = 'Denver' THEN 'CO'
+    WHEN site_or_region = 'New Orleans' THEN 'NOLA' 
+    WHEN site_or_region = 'The Durant Center' THEN 'DC'
+    WHEN site_or_region = 'Ward 8' THEN 'DC'
+    ELSE site_or_region
+END AS site_or_region,
 
-FROM `data-studio-260217.performance_mgt.fy22_team_kpis` 
-WHERE region_function = 1
+
+FROM `data-studio-260217.performance_mgt.fy22_team_kpis`  team_kpis
+WHERE program = 1
+
+GROUP BY 
+kpis_by_role,
+site_or_region,
+student_count,
+target_numerator,
+count_of_targets
