b105c26975a48d08346fb7df4c071ae4979f10cd
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-17 13:42:02 -0700

    Auto-commit: Tue Aug 17 2021 13:41:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework.sql
@@ -50,7 +50,7 @@ WITH gather_data AS (
         
         --added by IR
         CASE
-            WHEN AY.AY_student_served = 'High School' AND AY_Grade IN ('10th Grade','11th Grade','12th Grade') THEN 1
+            WHEN AY.AY_student_served = 'High School' AND AY_Grade != '9th Grade' THEN 1
             ELSE 0
             END AS tenth_thru_twelfth_grade_count,
             
@@ -80,8 +80,8 @@ WITH gather_data AS (
         --added by IR
         CASE
             WHEN summer_experiences_previous_summer_c >= 1 AND AY.AY_student_served = 'High School'  
-            AND (C.student_s_start_academic_year_c != 'AY 2020-21' OR C.student_s_start_academic_year_c IS NULL)--a1b46000000dRR8AAM
-            AND AY_Grade IN ('10th Grade','11th Grade','12th Grade')  THEN 1
+            AND (C.student_s_start_academic_year_c != 'AY 2020-21' OR C.student_s_start_academic_year_c IS NULL)
+            AND AY_Grade IN ('10th Grade','11th Grade','12th Grade') THEN 1
             ELSE 0
             END AS mse_met,
             
