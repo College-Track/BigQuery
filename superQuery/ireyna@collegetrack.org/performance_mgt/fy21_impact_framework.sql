c2d9d74cb7929a01e8db84ef0b15d7696ebab761
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-17 14:38:32 -0700

    Auto-commit: Tue Aug 17 2021 14:38:23 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework.sql
@@ -50,7 +50,8 @@ WITH gather_data AS (
         
         --added by IR
         CASE
-            WHEN AY.AY_student_served = 'High School' AND AY_Grade != '9th Grade' THEN 1
+            WHEN AY.AY_student_served = 'High School' AND AY_Grade != '9th Grade' 
+            AND (C.student_s_start_academic_year_c != 'a1b46000000dRR8AAM' OR C.student_s_start_academic_year_c IS NULL) THEN 1
             ELSE 0
             END AS tenth_thru_twelfth_grade_count,
             
