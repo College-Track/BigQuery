08420d82281a0a8813c72c785524effc8409af20
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-08-18 13:13:48 -0700

    Auto-commit: Wed Aug 18 2021 13:13:39 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework.sql
@@ -171,14 +171,13 @@ WITH gather_data AS (
             END 
             AS four_year_retention_numerator,
     
-        --CASE 
-        --    WHEN AY.four_year_retention_denominator IS NOT NULL AND AY_Grade = '12th Grade' THEN 1
-        --    --AND College_Track_Status_Name IN ('Did Not Finish CT HS Program','Active: Post-Secondary','Inactive: Post-Secondary')THEN 1
-        --    ELSE
-        --        0 
-        --    END 
-        --    AS 
-        four_year_retention_denominator
+        CASE 
+            WHEN AY.four_year_retention_denominator IS NOT NULL AND AY_Grade = '12th Grade' THEN 1
+            --AND College_Track_Status_Name IN ('Did Not Finish CT HS Program','Active: Post-Secondary','Inactive: Post-Secondary')THEN 1
+            ELSE
+                0 
+            END 
+            AS four_year_retention_denominator
            
 
 
