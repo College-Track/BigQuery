4ee98ea9e082648f97e2df0419ed49c16a7539bc
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-23 13:37:44 -0700

    Auto-commit: Mon Aug 23 2021 13:37:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
@@ -12,16 +12,27 @@ WITH get_student_grade AS
     GROUP BY Contact_Id, AT_Id, AT_Grade_c, global_academic_semester_c
 ),
 
+get_dosage_type AS
+(
+    SELECT
+    Class_c AS d_class,
+    dosage_types_c
+    
+    FROM `data-warehouse-289815.salesforce_clean.class_template`
+),
+
 get_enrollments AS
 (
     SELECT
     Class_c AS e_class,
     academic_semester_c,
     get_student_grade.AT_Grade_c,
-    get_student_grade.global_academic_semester_c
+    get_student_grade.global_academic_semester_c,
+    get_dosage_type.dosage_types_c
 
     FROM `data-warehouse-289815.salesforce.class_registration_c`
     LEFT JOIN get_student_grade ON AT_Id = academic_semester_c
+    LEFT JOIN get_dosage_type ON d_class = Class_c
     WHERE status_c = "Enrolled"
 )
     SELECT
@@ -29,4 +40,5 @@ get_enrollments AS
     
     FROM get_enrollments
     WHERE global_academic_semester_c = "a3646000000dMXuAAM"
-    GROUP BY e_class, AT_Grade_c,global_academic_semester_c
+    AND dosage_types_c = "Advisory"
+    GROUP BY e_class, AT_Grade_c,global_academic_semester_c, dosage_types_c
