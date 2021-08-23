90872ab8daaf87cc88313a5403f9e3580e4822df
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-23 13:10:56 -0700

    Auto-commit: Mon Aug 23 2021 13:10:50 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
@@ -1,23 +1,9 @@
-WITH get_student_grade AS
-(
-    SELECT
+ SELECT
     Contact_Id,
-    AT_grade_c,
-    
+    AT_Id,
+    AT_Grade_c,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE global_academic_semester_c = 'a3646000000dMXuAAM'
     AND college_track_status_c = '11A'
-    GROUP BY Contact_Id, AT_Grade_c
-)
-
-    SELECT
-    Class_c,
-    workshop_display_name_c AS w_name_check,
-    get_student_grade.AT_Grade_c
-
-    FROM `data-warehouse-289815.salesforce_clean.class_template`
-    LEFT JOIN get_student_grade ON Contact_Id = Student_c
-    WHERE global_academic_semester_c = 'a3646000000dMXuAAM'
-    AND dosage_types_c = 'Advisory'
-    GROUP BY Class_c, workshop_display_name_c, get_student_grade.AT_Grade_c
+    GROUP BY Contact_Id, AT_Id, AT_Grade_c
