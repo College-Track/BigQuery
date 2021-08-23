150f38629da467ed9152c88858a4ade492e5efc8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-23 13:20:35 -0700

    Auto-commit: Mon Aug 23 2021 13:20:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
@@ -1,6 +1,4 @@
-WITH get_student_grade AS
-(
-    SELECT
+SELECT
     Contact_Id,
     AT_Id,
     AT_Grade_c,
@@ -9,35 +7,3 @@ WITH get_student_grade AS
     WHERE global_academic_semester_c = 'a3646000000dMXuAAM'
     AND college_track_status_c = '11A'
     GROUP BY Contact_Id, AT_Id, AT_Grade_c
-)
-    SELECT
-    Class_c AS e_class,
-    get_student_grade.AT_Grade_c AS AT_Grade,
-
-    FROM `data-warehouse-289815.salesforce.class_registration_c`
-    LEFT JOIN get_student_grade ON AT_Id = academic_semester_c
-    WHERE status_c = "Enrolled"
-    GROUP BY Class_c, get_student_grade.AT_Grade_c
-
-/*
-get_class_dosage_type AS
-(
-    SELECT
-    Class_c,
-    dosage_types_c,
-    global_academic_semester_c
-    
-    FROM `data-warehouse-289815.salesforce_clean.class_template`
-    WHERE global_academic_semester_c = 'a3646000000dMXuAAM'
-    GROUP BY Class_c, dosage_types_c, global_academic_semester_c
-)
-    
-    SELECT 
-    e_class,
-    AT_Grade,
-    get_class_dosage_type.dosage_types_c
-    
-    FROM enrollments_student_grade
-    LEFT JOIN get_class_dosage_type ON Class_c = e_class
-    GROUP BY e_class, AT_Grade, get_class_dosage_type.dosage_types_c
-*/
