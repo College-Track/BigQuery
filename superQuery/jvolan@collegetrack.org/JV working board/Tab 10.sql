43d0ff2c041b40bb4a634925317d1592d9a3929b
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-23 13:11:41 -0700

    Auto-commit: Mon Aug 23 2021 13:11:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
@@ -5,4 +5,4 @@ SELECT
     
     FROM `data-warehouse-289815.salesforce_clean.class_template`
     WHERE global_academic_semester_c = 'a3646000000dMXuAAM'
-    GROUP BY Class_c, dosage_type_s_c, global_academic_semester_c
+    GROUP BY Class_c, dosage_types_c, global_academic_semester_c
