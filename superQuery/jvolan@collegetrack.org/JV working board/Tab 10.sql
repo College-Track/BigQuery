584155e65b2a973d11c0d42d4433e414ed15dd8b
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-08-23 13:31:47 -0700

    Auto-commit: Mon Aug 23 2021 13:31:42 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 10.sql
@@ -18,16 +18,14 @@ get_enrollments AS
     Class_c AS e_class,
     academic_semester_c,
     get_student_grade.AT_Grade_c,
-    get_student_grade.global_academic_semester_c AS w_gas
+    get_student_grade.global_academic_semester_c
 
     FROM `data-warehouse-289815.salesforce.class_registration_c`
     LEFT JOIN get_student_grade ON AT_Id = academic_semester_c
     WHERE status_c = "Enrolled"
 )
     SELECT
-    * ,
-    c.dosage_types_c,
+    * 
     
     FROM get_enrollments
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.class_template`c ON Class_c = e_class
     WHERE global_academic_semester_c = "a3646000000dMXuAAM"
