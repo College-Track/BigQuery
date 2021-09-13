fd4fb356ed438aca02b69172f6f4127aed55187f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-13 12:44:19 -0700

    Auto-commit: Mon Sep 13 2021 12:44:12 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 15.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 15.sql
@@ -1,32 +1,32 @@
- SELECT
+ WITH bb_balance AS (
+  SELECT
     student_c,
-    amount_c,
-    transaction_status_c,
-    earning_type_c,
-    name,
-    id AS bb_id,
-    CAST(finance_reporting_date_c AS DATE) AS finance_reporting_date,
-    CASE
-      WHEN record_type_id = "01246000000ZNhsAAG"THEN "BB Disbursement"
-      WHEN record_type_id = "01246000000ZNhtAAG"THEN "BB Earnings"
-  END
-    AS BB_Record_Type,
-    CASE
-      WHEN record_type_id = "01246000000ZNhsAAG"THEN amount_c
-  END
-    AS BB_Disbursement_Amount,
-    CASE
-      WHEN record_type_id = "01246000000ZNhtAAG"THEN amount_c
-  END
-    AS BB_Earnings_Amount,
-    academic_year_c,
-    FORMAT_DATE("%m", CURRENT_DATE()) AS report_month,
-    CAST (CURRENT_DATE() AS DATE) AS report_date,
+    MAX(finance_bb_balance_total_c) AS Finance_BB_Balance,
   FROM
-    `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
+    `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
   WHERE
-    finance_reporting_date_c >= '2021-07-01'
-    AND record_type_id = "01246000000ZNhsAAG"
-    AND transaction_status_c = "Approved"
-    OR record_type_id = "01246000000ZNhtAAG"
-    AND finance_reporting_date_c >= '2021-07-01'
+    record_type_id = "01246000000ZNi1AAG"
+    AND finance_bb_balance_total_c > 0
+  GROUP BY
+    student_c )
+    
+  SELECT
+    Contact_Id,
+    Full_Name_c,
+    site_abrev AS Site,
+    current_cc_advisor_2_c AS Current_CCA,
+    high_school_graduating_class_c AS HS_Class,
+    College_Track_Status_Name AS CT_Status,
+    bb_balance.Finance_BB_Balance,
+  FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+  LEFT JOIN
+    bb_balance
+  ON
+    bb_balance.student_c = Contact_Id
+  WHERE
+    College_Track_Status_c IN ('11A',
+      '12A',
+      '15A',
+      '16A')
+    AND bb_balance.Finance_BB_Balance IS NOT NULL
