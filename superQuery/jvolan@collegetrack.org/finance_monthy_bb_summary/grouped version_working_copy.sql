f0b348eeac6da46629df4ddc35a2b357f1277464
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 08:45:49 -0700

    Auto-commit: Tue Sep 14 2021 08:45:45 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/grouped version_working_copy.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/grouped version_working_copy.sql
@@ -1,29 +1,11 @@
-WITH bb_balance AS
-(
-    SELECT
+SELECT
     student_c,
-    max(finance_bb_balance_total_c) as Finance_BB_Balance
+    SUM (amount_c) as bb_disbursement_total,
     
     FROM
-    `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
-    WHERE record_type_id = "01246000000ZNi1AAG"
-    AND finance_bb_balance_total_c > 0
+    `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
+    WHERE
+    record_type_id = "01246000000ZNhsAAG"
+    AND finance_reporting_date_c >= '2021-07-01'
+    AND transaction_status_c = "Approved"
     GROUP BY student_c
- ),
-
-
-student_list_bb_balance AS
-(
- SELECT
-    -- Select fields from templates
-    Contact_Id,
-    Full_Name_c,
-    site_abrev,
-    current_cc_advisor_2_c,
-    high_school_graduating_class_c,
-    bb_balance.Finance_BB_Balance,
-    
-    FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
-    LEFT JOIN bb_balance ON bb_balance.student_c = Contact_Id
-    WHERE College_Track_Status_c IN ('11A', '12A','15A', '16A')
