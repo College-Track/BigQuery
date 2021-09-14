62d4e40ef3dd15f3d7ed3b4c7ff3766eaf7fe955
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-13 20:03:21 -0700

    Auto-commit: Mon Sep 13 2021 20:03:15 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -2,6 +2,9 @@
     Contact_Id,
     AT_Term_GPA,
     AT_Cumulative_GPA,
+    AT_Name,
+    full_name_c,
+    high_school_class_c
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = "The Durant Center"
