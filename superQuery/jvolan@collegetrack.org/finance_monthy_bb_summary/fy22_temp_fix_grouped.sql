d31e70326ec4de9655f793db8303a69e55d44a0b
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 12:39:19 -0700

    Auto-commit: Tue Sep 14 2021 12:39:12 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/fy22_temp_fix_grouped.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/fy22_temp_fix_grouped.sql
@@ -7,7 +7,6 @@ WITH bb_balance AS
     FROM
     `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
     WHERE record_type_id = "01246000000ZNi1AAG"
-    AND finance_bb_balance_total_c > 0
     GROUP BY student_c
  ),
 
