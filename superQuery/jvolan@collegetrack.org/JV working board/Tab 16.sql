ca04f9e8a920fcd16cb73e49edd8bb26afe73f21
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-13 19:58:41 -0700

    Auto-commit: Mon Sep 13 2021 19:58:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -8,4 +8,3 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = "The Durant Center"
     AND College_Track_Status_Name = "Current CT HS Student"
-    AND GAS_Name IN ("Fall 2020-21","Spring 2020-21")
