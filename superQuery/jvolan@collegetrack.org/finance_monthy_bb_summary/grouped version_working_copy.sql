2228813423792898ce6892356f413465f20f35c8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 08:44:31 -0700

    Auto-commit: Tue Sep 14 2021 08:44:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/grouped version_working_copy.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/grouped version_working_copy.sql
@@ -1,4 +1,20 @@
-SELECT
+WITH bb_balance AS
+(
+    SELECT
+    student_c,
+    max(finance_bb_balance_total_c) as Finance_BB_Balance
+    
+    FROM
+    `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
+    WHERE record_type_id = "01246000000ZNi1AAG"
+    AND finance_bb_balance_total_c > 0
+    GROUP BY student_c
+ ),
+
+
+student_list_bb_balance AS
+(
+ SELECT
     -- Select fields from templates
     Contact_Id,
     Full_Name_c,
