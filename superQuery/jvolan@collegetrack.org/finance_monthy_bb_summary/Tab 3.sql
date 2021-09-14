d92a3b367eb5b66f4c6768bca974ff9a6effdb50
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 08:44:54 -0700

    Auto-commit: Tue Sep 14 2021 08:44:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 3.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 3.sql
@@ -1,4 +1,6 @@
-SELECT
+WITH bb_balance AS
+(
+    SELECT
     student_c,
     max(finance_bb_balance_total_c) as Finance_BB_Balance
     
@@ -7,3 +9,20 @@ SELECT
     WHERE record_type_id = "01246000000ZNi1AAG"
     AND finance_bb_balance_total_c > 0
     GROUP BY student_c
+ )
+
+
+
+ SELECT
+    -- Select fields from templates
+    Contact_Id,
+    Full_Name_c,
+    site_abrev,
+    current_cc_advisor_2_c,
+    high_school_graduating_class_c,
+    bb_balance.Finance_BB_Balance,
+    
+    FROM
+    `data-warehouse-289815.salesforce_clean.contact_template`
+    LEFT JOIN bb_balance ON bb_balance.student_c = Contact_Id
+    WHERE College_Track_Status_c IN ('11A', '12A','15A', '16A')
