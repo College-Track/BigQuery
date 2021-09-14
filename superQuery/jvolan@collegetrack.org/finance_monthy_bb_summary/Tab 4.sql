a7744111e06be408ece96c52fa56d1cfc49a4233
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 08:54:57 -0700

    Auto-commit: Tue Sep 14 2021 08:54:50 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 4.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 4.sql
@@ -7,5 +7,5 @@
     `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
     WHERE record_type_id = "01246000000ZNi1AAG"
     AND finance_bb_balance_total_c > 0
-    AND current_ct_status_c IN ('11A', '12A','15A', '16A')
+    AND current_ct_status_c IN ('Current CT HS Student', 'Leave of Absence', 'Active: Post-Secondary','Inactive: Post-Secondary')
     GROUP BY student_c
