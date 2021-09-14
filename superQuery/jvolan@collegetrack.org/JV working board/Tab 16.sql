9d2e16b5473fa3da386850434838f314c082280e
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-13 20:11:37 -0700

    Auto-commit: Mon Sep 13 2021 20:11:33 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -1,3 +1,16 @@
+  SELECT
+    Contact_Id,
+    AVG(AT_Term_GPA),
+    AVG(AT_Cumulative_GPA),
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE site_short = "The Durant Center"
+    AND student_audit_status_c = "Current CT HS Student"
+    AND GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
+    GROUP BY Contact_Id
+
+/* alternate version if you want to account for NULLs
+
     SELECT
     Contact_Id,
     AT_Term_GPA,
@@ -13,8 +26,6 @@
     AND student_audit_status_c = "Current CT HS Student"
     AND GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
 
-
-/* alternate version if you want to account for NULLs
  FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = "The Durant Center"
     AND student_audit_status_c = "Current CT HS Student"
