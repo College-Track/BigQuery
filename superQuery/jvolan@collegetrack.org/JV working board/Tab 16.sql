f87bb19e17067b3984ea9b8e485e2db2fb75b411
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-13 20:09:57 -0700

    Auto-commit: Mon Sep 13 2021 20:09:53 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -11,9 +11,17 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = "The Durant Center"
     AND student_audit_status_c = "Current CT HS Student"
+    AND GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
+
+
+/* alternate version if you want to account for NULLs
+ FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE site_short = "The Durant Center"
+    AND student_audit_status_c = "Current CT HS Student"
     AND 
     (GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
     AND 
     (AT_Term_GPA IS NULL
     OR AT_Cumulative_GPA IS NULL)
     )
+*/
