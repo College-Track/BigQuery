2d9c0cf68f9b1581340268e0d44268b2c58ad461
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-13 20:01:54 -0700

    Auto-commit: Mon Sep 13 2021 20:01:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -1,10 +1,12 @@
     SELECT
     Contact_Id,
-    AVG(AT_Term_GPA),
-    AVG(AT_Cumulative_GPA),
+    AT_Term_GPA,
+    AT_Cumulative_GPA,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE site_short = "The Durant Center"
     AND College_Track_Status_Name = "Current CT HS Student"
     AND GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
-    GROUP BY Contact_Id
+    AND 
+    AT_Term_GPA IS NULL
+    OR AT_Cumulative_GPA IS NULL
