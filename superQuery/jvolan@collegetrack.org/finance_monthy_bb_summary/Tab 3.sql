406e186fcc9d0680773e7cbbb8ac4a150c23daa1
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 08:44:03 -0700

    Auto-commit: Tue Sep 14 2021 08:43:58 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 3.sql
@@ -0,0 +1,9 @@
+SELECT
+    student_c,
+    max(finance_bb_balance_total_c) as Finance_BB_Balance
+    
+    FROM
+    `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
+    WHERE record_type_id = "01246000000ZNi1AAG"
+    AND finance_bb_balance_total_c > 0
+    GROUP BY student_c
