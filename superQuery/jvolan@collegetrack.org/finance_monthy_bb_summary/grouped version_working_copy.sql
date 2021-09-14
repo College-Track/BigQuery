f00b5faccf63860415a5a90cac072a0816bfc80a
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-14 08:48:17 -0700

    Auto-commit: Tue Sep 14 2021 08:48:13 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/grouped version_working_copy.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/grouped version_working_copy.sql
@@ -1,11 +1,11 @@
 SELECT
     student_c,
-    SUM (amount_c) as bb_disbursement_total,
+    SUM (amount_c) as bb_earnings_total,
     
     FROM
     `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
     WHERE
-    record_type_id = "01246000000ZNhsAAG"
-    AND finance_reporting_date_c >= '2021-07-01'
-    AND transaction_status_c = "Approved"
+    record_type_id = "01246000000ZNhtAAG"
+    AND created_date >= '2021-07-01'
+    AND amount_c > 0
     GROUP BY student_c
