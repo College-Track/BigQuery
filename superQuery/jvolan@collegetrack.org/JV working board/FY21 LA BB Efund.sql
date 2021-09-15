a7483c58dddd88cc10c829dc423be5ba99dae495
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-15 16:24:03 -0700

    Auto-commit: Wed Sep 15 2021 16:23:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
@@ -1,6 +1,6 @@
 SELECT
     site_short,
-    SUM(total_bb_earnings_as_of_hs_grad_contact_c)
+    SUM(total_bb_earnings_as_of_hs_grad_contact_c) AS x_2021_total_hs_bb_earnings
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE indicator_completed_ct_hs_program_c = TRUE
