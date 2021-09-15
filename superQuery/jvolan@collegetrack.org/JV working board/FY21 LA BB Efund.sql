d53972f480b14a3d5cefb64908adbfae80e53835
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-15 16:22:54 -0700

    Auto-commit: Wed Sep 15 2021 16:22:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
@@ -3,7 +3,7 @@ SELECT
     SUM(total_bb_earnings_as_of_hs_grad_contact_c)
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
-    WHERE indicator_completed_ct_hs_program_c = 1
+    WHERE indicator_completed_ct_hs_program_c = TRUE
     AND site_short IN ('Boyle Heights','Watts')
     AND high_school_graduating_class_c = 2021
     GROUP BY site_short
