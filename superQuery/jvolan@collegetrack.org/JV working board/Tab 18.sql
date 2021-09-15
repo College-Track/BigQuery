7188f9ea735e87da1cbf49b8fb99686d4b35847b
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-15 16:36:09 -0700

    Auto-commit: Wed Sep 15 2021 16:36:04 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 18.sql
@@ -0,0 +1,13 @@
+SELECT 
+    student_c,
+    Count(id) AS efund_st_count,
+    SUM(amount_c) AS total_efund_disbursement_amount,
+    
+    FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = student_c
+
+    WHERE scholarship_c = 'College Track Emergency Fund'
+    AND date_c >= '2020-07-01'
+    AND date_c < '2021-07-01'
+    AND site_short IN ('Boyle Heights','Watts')
+    GROUP BY student_c
