7588ca34753f06731da18c9e5a873b9f27d0de27
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-15 15:48:40 -0700

    Auto-commit: Wed Sep 15 2021 15:48:34 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
@@ -0,0 +1,14 @@
+
+    SELECT
+    student_c,
+    id,
+    amount_c,
+    disbursement_approval_date_c,
+    
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
+    WHERE record_type_id = '01246000000ZNhsAAG'
+    AND amount_c >0
+    AND disbursement_approval_date_c >= '2020-07-01'
+    AND disbursement_approval_date_c < '2021-07-01'
