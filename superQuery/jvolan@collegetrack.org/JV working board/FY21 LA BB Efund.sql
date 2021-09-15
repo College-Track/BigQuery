93c804201cb5822451b1a958ba11d1a8d79917bf
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-15 16:16:32 -0700

    Auto-commit: Wed Sep 15 2021 16:16:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/FY21 LA BB Efund.sql
@@ -1,15 +1,13 @@
-
-    SELECT
+SELECT 
+    c.site_short,
     COUNT(DISTINCT(student_c)) AS unique_student_count,
-    Count(id) AS bb_disbursement_count,
-    SUM(amount_c) total_bb_disbursement_amount,
-    c.site_short
-
-    FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean` st
+    Count(id) AS efund_count,
+    SUM(amount_c) total_efund_disbursement_amount,
+    
+    FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean`
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = student_c
 
-    WHERE st.record_type_id = '01246000000ZNhsAAG'
-    AND amount_c >0
+    WHERE scholarship_c = 'College Track Emergency Fund'
     AND disbursement_approval_date_c >= '2020-07-01'
     AND disbursement_approval_date_c < '2021-07-01'
     AND site_short IN ('Boyle Heights','Watts')
