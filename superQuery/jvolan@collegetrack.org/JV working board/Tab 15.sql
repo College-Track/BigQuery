9c88185c64bac0eb147786903cea10a5a830da94
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-21 09:37:20 -0700

    Auto-commit: Tue Sep 21 2021 09:37:16 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 15.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 15.sql
@@ -12,3 +12,4 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay 
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
     WHERE AY_student_served IS NOT NULL
+    AND AY_Name = "AY 2021-22"
