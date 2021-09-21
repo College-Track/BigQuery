1dc866dbdda445e5ead743b07a057542e1fed09f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-21 11:22:37 -0700

    Auto-commit: Tue Sep 21 2021 11:22:32 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -1,38 +1,11 @@
-  SELECT
-    GAS_Name,
-    ROUND(AVG(AT_Term_GPA),4) AS avg_term_gpa,
-    ROUND(AVG(AT_Cumulative_GPA),4) AS avg_cgpa,
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE site_short = "The Durant Center"
-    AND student_audit_status_c = "Current CT HS Student"
-    AND GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
-    GROUP BY GAS_Name
+SELECT
+x_18_digit_id_c,
+full_name_c,
+site,
+College_Track_Status_Name,
+total_bank_book_balance_contact_c
 
-/* alternate version if you want to account for NULLs
-
-    SELECT
-    Contact_Id,
-    AT_Term_GPA,
-    AT_Cumulative_GPA,
-    AT_Name,
-    full_name_c,
-    high_school_class_c, 
-    College_Track_Status_Name
-    
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE site_short = "The Durant Center"
-    AND student_audit_status_c = "Current CT HS Student"
-    AND GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
-
- FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE site_short = "The Durant Center"
-    AND student_audit_status_c = "Current CT HS Student"
-    AND 
-    (GAS_Name IN ("Fall 2020-21 (Semester)","Spring 2020-21 (Semester)")
-    AND 
-    (AT_Term_GPA IS NULL
-    OR AT_Cumulative_GPA IS NULL)
-    )
-*/
+FROM `data-warehouse-289815.salesforce_clean.contact_template`
+WHERE college_track_status_c IN ('11A','12A','15A')
+OR (college_track_status_c = '16A'
+AND total_bank_book_balance_contact_c >0)
