42c997a7af35b15b82204c807aabdc2e3b2db0df
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-21 13:50:23 -0700

    Auto-commit: Tue Sep 21 2021 13:50:17 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Dan student served 17-18 to 21-22.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Dan student served 17-18 to 21-22.sql
@@ -1,32 +1,4 @@
-/*with gather_raw_data AS
-(
-    SELECT
-    cay.Contact_Id AS student_count,
-    AY_student_served,
-    AY_enrolled_sessions,
-    AY_School_type,
-    AY_enrollment_status,
-    AY_Name,
-    ct_status_end_of_ay,
-    c.site_short,
-    
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay 
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
-    WHERE AY_student_served IS NOT NULL
-    AND AY_Name IN ("AY 2020-21","AY 2019-20","AY 2018-19","AY 2017-18")
-  )
-  
-    SELECT
-    site_short,
-    AY_Name,
-    AY_student_served,
-    COUNT(student_count)
-    
-    FROM gather_raw_data
-    GROUP BY site_short, AY_Name, AY_student_served
-*/
-
+/*
 with gather_raw_data AS
 (
     SELECT
@@ -54,3 +26,23 @@ with gather_raw_data AS
     
     FROM gather_raw_data
     GROUP BY site_short, AY_Name, AY_student_served
+    
+*/
+
+SELECT
+    cay.Contact_Id AS student_count,
+    AY_student_served,
+    AY_enrolled_sessions,
+    AY_School_type,
+    AY_enrollment_status,
+    AY_Name,
+    ct_status_end_of_ay,
+    c.site_short,
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay 
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
+    WHERE AY_enrollment_status IN ("Full-time", "Part-time")
+    AND AY_student_served NOT IN ("High School", "Alumni")
+    AND site_short = "Aurora"
+    AND AY_Name = "AY 2020-21"
