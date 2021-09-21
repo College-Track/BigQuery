8630fcb0d20444df158b6c9ca04eecb776aa1109
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-21 09:35:21 -0700

    Auto-commit: Tue Sep 21 2021 09:35:17 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 15.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 15.sql
@@ -1,40 +1,12 @@
- WITH bb_balance AS (
-  SELECT
-    student_c,
-    MAX(finance_bb_balance_total_c) AS Finance_BB_Balance,
-  FROM
-    `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
-  WHERE
-    record_type_id = "01246000000ZNi1AAG"
-    AND finance_bb_balance_total_c > 0
-  GROUP BY
-    student_c ),
-
-join_data AS   
-(
-  SELECT
-    Contact_Id,
-    Full_Name_c,
-    site_abrev AS Site,
-    current_cc_advisor_2_c AS Current_CCA,
-    high_school_graduating_class_c AS HS_Class,
-    College_Track_Status_Name AS CT_Status,
-    bb_balance.Finance_BB_Balance,
-  FROM
-    `data-warehouse-289815.salesforce_clean.contact_template`
-  LEFT JOIN
-    bb_balance
-  ON
-    bb_balance.student_c = Contact_Id
-  WHERE
-    College_Track_Status_c IN ('11A',
-      '12A',
-      '15A',
-      '16A')
-    AND bb_balance.Finance_BB_Balance IS NOT NULL
-)
-
     SELECT
-    *
-    FROM join_data
-    WHERE Contact_Id = "0031M0000334qQ7QAI"
+    cay.Contact_Id,
+    AY_student_served,
+    AY_enrolled_sessions,
+    AY_School_type,
+    AY_enrollment_status
+    ct_status_end_of_ay,
+    c.site_short,
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay 
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
