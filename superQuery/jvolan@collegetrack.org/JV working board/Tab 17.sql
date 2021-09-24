d403b360a4ac4206fcfc1dc8da42b1fee7579844
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 12:04:03 -0700

    Auto-commit: Fri Sep 24 2021 12:03:59 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
@@ -0,0 +1,14 @@
+
+  SELECT
+    cat.Contact_Id,
+    cat.ay_2020_21_student_served_c,
+    MAX(AT_Cumulative_GPA)
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template` cat
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_ay_template` cay ON cay.Contact_Id = cat.Contact_Id
+  WHERE
+    cat.AY_Name = "AY 2020-21"
+    AND term_c = 'Spring'
+    AND ay_2020_21_student_served_c = "High School Student"
+    AND site_short IN ("East Palo Alto", "Oakland", "San Francisco")
+    GROUP BY Contact_Id, ay_2020_21_student_served_c
