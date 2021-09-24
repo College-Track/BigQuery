e9b1f7329a86531bb0fc6fa39372e1e58a602f46
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 12:17:57 -0700

    Auto-commit: Fri Sep 24 2021 12:17:52 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
@@ -1,4 +1,5 @@
-
+with gather_12_sp_cgpa AS
+(
   SELECT
     Contact_Id,
     ay_2020_21_student_served_c,
@@ -18,6 +19,11 @@
     AND site_short IN ("East Palo Alto", "Oakland", "San Francisco")
     AND high_school_graduating_class_c = '2021'
     AND AT_Cumulative_GPA IS NOT NULL
- 
+)
 
+    SELECT
+    COUNT(sp_20_21_cgpa_3) AS ay_cgpa_denom,
+    SUM(sp_20_21_cgpa_3) AS ay_cgpa_num,
+    SUM(sp_20_21_cgpa_3) / COUNT(sp_20_21_cgpa_3) AS percent_above_3
     
+    FROM gather_12_sp_cgpa
