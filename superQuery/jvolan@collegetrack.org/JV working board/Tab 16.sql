8801e24497f82232625f347031c0d3b77883ccdd
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 11:53:58 -0700

    Auto-commit: Fri Sep 24 2021 11:53:50 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -2,13 +2,11 @@
     cat.Contact_Id,
     SUM(attended_workshops_c) AS attended_workshops_c,
     SUM(enrolled_sessions_c) AS enrolled_sessions_c,
-    SUM(attended_workshops_c)/SUM(enrolled_sessions_c) AS ay_attendance_rate
-    
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template` cat
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_ay_template` cay ON cay.Contact_Id = cat.Contact_Id
   WHERE
     cat.AY_Name = "AY 2020-21"
     AND term_c != 'Summer'
-  GROUP BY
-    Contact_Id
+    AND ay_2020_21_student_served_c = 'TRUE'
+    GROUP BY Contact_Id
