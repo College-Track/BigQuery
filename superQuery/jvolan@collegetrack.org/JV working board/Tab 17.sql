9f55097cfcba18e0840f7740fc1748faab6fd921
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 15:26:09 -0700

    Auto-commit: Fri Sep 24 2021 15:26:04 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
@@ -1,29 +1,29 @@
-with gather_12_sp_cgpa AS
+with gather_ay_gpa_data AS
 (
   SELECT
-    Contact_Id,
-    ay_2020_21_student_served_c,
-    high_school_graduating_class_c,
-    site_short,
-    AT_Name,
+    cay.Contact_Id,
+    AY_student_served,
+    AY_Name,
+    AY_Cumulative_GPA,
     CASE
-        WHEN AT_Cumulative_GPA >= 3 THEN 1
+        WHEN AY_Cumulative_GPA >= 3 THEN 1
         ELSE 0
-    END AS sp_20_21_cgpa_3
+    END AS ay_cgpa_3,
     
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    
-  
-    WHERE GAS_Name = "Spring 2020-21 (Semester)"
-    AND ay_2020_21_student_served_c = "High School Student"
-    AND site_short IN ("East Palo Alto", "Oakland", "San Francisco")
-    AND high_school_graduating_class_c = '2021'
-    AND AT_Cumulative_GPA IS NOT NULL
+    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
+    WHERE AY_Name IN ('AY 2018-19','AY 2019-20','AY 2020-21')
+    AND AY_student_served = "High School"
+    AND site_short IN  ("East Palo Alto", "Oakland", "San Francisco")
 )
 
     SELECT
-    SUM(sp_20_21_cgpa_3) AS ay_cgpa_num,
-    COUNT(sp_20_21_cgpa_3) AS ay_cgpa_denom,
-    round((SUM(sp_20_21_cgpa_3) / COUNT(sp_20_21_cgpa_3)*100),2) AS percent_above_3
+    AY_Name,
+    SUM(ay_cgpa_3) AS ay_cgpa_3_num,
+    COUNT(ay_cgpa_3) AS ay_cgpa_3_denom,
+    Round(SUM(ay_cgpa_3) / COUNT(ay_cgpa_3),2) AS ay_cgpa_3_denom,
     
-    FROM gather_12_sp_cgpa
+    FROM gather_ay_gpa_data
+    GROUP BY AY_Name
+
+    
