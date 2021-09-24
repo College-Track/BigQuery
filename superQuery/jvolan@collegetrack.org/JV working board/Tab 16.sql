4085b3ab40a2ad38d5a8d402a46c52bf2ec83429
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 14:50:23 -0700

    Auto-commit: Fri Sep 24 2021 14:50:19 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -1,14 +1,15 @@
 with gather_ay_attednace_data AS
 (
   SELECT
-    Contact_Id,
+    cay.Contact_Id,
     AY_student_served,
     AY_Name,
     AY_fall_spring_attended_workshops,
     AY_fall_spring_enrolled_sessions,
     AY_fall_spring_attended_workshops/AY_fall_spring_enrolled_sessions AS ay_fall_spring_attendance_rate
     
-    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template`
+    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
     WHERE AY_Name IN ('AY 2018-19','AY 2019-20','AY 2020-21')
     AND AY_student_served = "High School"
     AND AY_fall_spring_enrolled_sessions > 0
