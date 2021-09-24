7ff9a9ed1b1b3d3eb4fc3027f365a6778f131f39
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 12:06:39 -0700

    Auto-commit: Fri Sep 24 2021 12:06:36 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
@@ -2,7 +2,10 @@
   SELECT
     cat.Contact_Id,
     cat.ay_2020_21_student_served_c,
-    MAX(AT_Cumulative_GPA)
+    CASE
+        WHEN AT_Cumulative_GPA >= 3 THEN 1
+        ELSE 0
+    END AS sp_20_21_cgpa_3
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template` cat
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_ay_template` cay ON cay.Contact_Id = cat.Contact_Id
@@ -13,4 +16,5 @@
     AND site_short IN ("East Palo Alto", "Oakland", "San Francisco")
     AND high_school_graduating_class_c = "2021"
     AND AT_Cumulative_GPA IS NULL
-    GROUP BY Contact_Id, ay_2020_21_student_served_c
+
+    
