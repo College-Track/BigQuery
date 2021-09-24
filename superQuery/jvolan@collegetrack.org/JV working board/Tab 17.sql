d86b218b3c72306943ecfa6e32c3276fc4ce626f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 12:08:08 -0700

    Auto-commit: Fri Sep 24 2021 12:08:03 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 17.sql
@@ -9,12 +9,11 @@
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template` cat
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_ay_template` cay ON cay.Contact_Id = cat.Contact_Id
-  WHERE
-    cat.AY_Name = "AY 2020-21"
-    AND term_c = 'Spring'
+  
+    WHERE GAS_Name = "Spring 2020-21 (Semester)"
     AND ay_2020_21_student_served_c = "High School Student"
     AND site_short IN ("East Palo Alto", "Oakland", "San Francisco")
-    AND high_school_graduating_class_c = "2021"
+    AND high_school_graduating_class_c = '2021'
  
 
     
