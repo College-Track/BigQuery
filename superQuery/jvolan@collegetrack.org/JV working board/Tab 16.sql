5ae16ef7f71bcec5504068c9ef4694cd797a0734
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 14:43:56 -0700

    Auto-commit: Fri Sep 24 2021 14:43:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 16.sql
@@ -1,23 +1,11 @@
-with gather_ay_attendance AS
-(
+
   SELECT
     Contact_Id,
-    ay_2020_21_student_served_c,
-    SUM(attended_workshops_c) AS attended_workshops_c,
-    SUM(enrolled_sessions_c) AS enrolled_sessions_c,
+    AY_student_served,
+    AY_Name,
+    AY_fall_spring_attended_workshops,
+    AY_fall_spring_enrolled_sessions,
     
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-  WHERE
-    AY_Name = "AY 2020-21"
-    AND term_c != 'Summer'
-    AND ay_2020_21_student_served_c = "High School Student"
-    AND site_short IN ("East Palo Alto", "Oakland", "San Francisco")
-    GROUP BY Contact_Id, ay_2020_21_student_served_c
-)
-    SELECT
-    count(Contact_Id),
-    SUM(attended_workshops_c) / SUM(enrolled_sessions_c) AS ay_attendance_rate,
-    
-    FROM gather_ay_attendance
-    WHERE enrolled_sessions_c >0
-    GROUP BY ay_2020_21_student_served_c
+    FROM `data-warehouse-289815.salesforce_clean.contact_ay_template`
+    WHERE AY_Name IN ('AY 2018-19','AY 2019-20','AY 2020-21')
+    AND AY_fall_spring_enrolled_sessions > 0
