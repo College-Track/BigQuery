5aa4a4d87ce7fb2ac167d5d316ecdd9491fbe9e8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-24 14:00:26 -0700

    Auto-commit: Fri Sep 24 2021 14:00:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College account summary/sandbox.sql
+++ superQuery/jvolan@collegetrack.org/College account summary/sandbox.sql
@@ -37,7 +37,7 @@ SELECT
     graduates_with_debt_c,
     avg_debt_of_graduates_c,
     median_debt_of_graduates_c,
-    graduation_rate_overall_c,
+    graduation_rate_overall_c/100 AS graduation_rate_overall_c,
     
     FROM `data-warehouse-289815.salesforce.account`
     WHERE record_type_id = "01246000000RNnLAAW"
