645097eb1d43a484005a0838e09ba088fe7c1e35
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-27 12:23:45 -0700

    Auto-commit: Mon Sep 27 2021 12:23:38 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/College account summary/Tab 3.sql
@@ -0,0 +1,23 @@
+ SELECT
+    Contact_Id,
+    AT_Cumulative_GPA,
+        CASE
+            WHEN AT_Cumulative_GPA >=3.25 THEN 1
+        END AS x_12_cgpa_325,
+        CASE
+            WHEN AT_Cumulative_GPA <3.25
+            AND AT_Cumulative_GPA >=2.75 THEN 1
+        END AS x_12_cgpa_275_325,
+        CASE
+            WHEN AT_Cumulative_GPA <2.75 THEN 1
+        END AS x_12_cgpa_below_275,
+    college_eligibility_gpa_11th_grade AS x_11_cgpa,
+    act_highest_composite_official_c AS act_highest_comp,
+    sat_highest_total_single_sitting_c AS sat_highest_total,
+    readiness_composite_off_c,
+    total_community_service_hours_completed_c,
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE AT_Grade_c = "12th Grade"
+    AND term_c = "Spring"
