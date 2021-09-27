19b2abb4ee3e87733122a3c187d77d5e1de63554
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-27 12:44:56 -0700

    Auto-commit: Mon Sep 27 2021 12:44:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College account summary/sandbox_college_apps_admits.sql
+++ superQuery/jvolan@collegetrack.org/College account summary/sandbox_college_apps_admits.sql
@@ -32,7 +32,18 @@ gather_student_data AS
         CASE
             WHEN AT_Cumulative_GPA <2.75 THEN 1
         END AS x_12_cgpa_below_275,
+        
     college_eligibility_gpa_11th_grade AS x_11_cgpa,
+        CASE
+            WHEN college_eligibility_gpa_11th_grade >=3.25 THEN 1
+        END AS x_11_cgpa_325,
+        CASE
+            WHEN college_eligibility_gpa_11th_grade <3.25
+            AND college_eligibility_gpa_11th_grade >=2.75 THEN 1
+        END AS x_11_cgpa_275_325,
+        CASE
+            WHEN college_eligibility_gpa_11th_grade <2.75 THEN 1
+        END AS x_11_cgpa_below_275,
     act_highest_composite_official_c AS act_highest_comp,
     sat_highest_total_single_sitting_c AS sat_highest_total,
     readiness_composite_off_c,
@@ -59,18 +70,19 @@ join_data AS
 )
     SELECT
     college_name,
-    COUNT(DISTINCT(Contact_Id)) AS unique_applicants,
     COUNT(college_app_id) AS total_applicants,
     SUM(admitted_y_n) AS total_admits,
     ROUND(SUM(admitted_y_n) / COUNT(college_app_id),2) AS ct_admit_rate,
     
     avg(x_12_cgpa) AS avg_12_cgpa,
-    avg(x_11_cgpa) AS avg_11_cgpa,
-    
     SUM(x_12_cgpa_325)/COUNT(x_12_cgpa) AS x_12_325_percent,
     SUM(x_12_cgpa_275_325)/COUNT(x_12_cgpa) AS x_12_cgpa_275_325_percent,
     SUM(x_12_cgpa_below_275)/COUNT(x_12_cgpa) AS x_12_cgpa_below_275_percent,
-
+    
+    avg(x_11_cgpa) AS avg_11_cgpa,
+    SUM(x_11_cgpa_325)/COUNT(x_11_cgpa) AS x_11_325_percent,
+    SUM(x_11_cgpa_275_325)/COUNT(x_11_cgpa) AS x_11_cgpa_275_325_percent,
+    SUM(x_11_cgpa_below_275)/COUNT(x_11_cgpa) AS x_11_cgpa_below_275_percent,
     
     avg(act_highest_comp) AS avg_act_highest_comp,
     avg(sat_highest_total) AS avg_sat_highest_total,
