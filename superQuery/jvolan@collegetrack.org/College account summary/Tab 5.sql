42b74db330e47a91cf2bd771435b5ff6cc23cc2c
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-28 09:16:07 -0700

    Auto-commit: Tue Sep 28 2021 09:15:57 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College account summary/Tab 5.sql
+++ superQuery/jvolan@collegetrack.org/College account summary/Tab 5.sql
@@ -1,4 +1,28 @@
-SELECT
+WITH 
+gather_college_admits AS
+(
+    SELECT
+    ca.id,
+    ca.college_university_c,
+    student_c,
+    admission_status_c,
+    CASE
+        WHEN admission_status_c IN ("Accepted", "Accepted and Enrolled","Accepted and Deferred") THEN 1
+        ELSE 0
+    END AS admitted_y_n,
+    a.name AS admit_college_name,
+    a.id AS admit_college_id,
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.college_application_clean` ca
+    LEFT JOIN `data-warehouse-289815.salesforce.account` a ON a.id = ca.college_university_c
+    WHERE application_status_c = "Applied"
+    AND admission_status_c IN ("Accepted", "Accepted and Enrolled","Accepted and Deferred") 
+),
+
+gather_admit_student_data AS
+(
+    SELECT
     Contact_Id,
     high_school_graduating_class_c,
     site_short,
@@ -44,3 +68,43 @@ SELECT
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE AT_Grade_c = "12th Grade"
     AND term_c = "Spring"
+),
+
+join_admit_data AS
+(
+    SELECT
+    gca.*,
+    gasd.*
+    
+    FROM
+    gather_college_admits gca
+    LEFT JOIN gather_admit_student_data gasd ON gasd.Contact_Id = gca.student_c
+)
+
+    SELECT
+    admit_college_name,
+    admit_college_id,
+    site_short,
+    high_school_graduating_class_c,
+    readiness_composite_off_c,
+    x_11_cgpa_bucket,
+    SUM(admitted_y_n) AS total_admits,
+    
+    avg(college_app_count) AS avg_college_apps_applied,
+    avg(college_acceptance_count) AS avg_college_accept,
+    
+    avg(x_12_cgpa) AS avg_12_cgpa,
+    SUM(x_12_cgpa_325)/COUNT(x_12_cgpa) AS x_12_325_percent,
+    SUM(x_12_cgpa_275_325)/COUNT(x_12_cgpa) AS x_12_cgpa_275_325_percent,
+    SUM(x_12_cgpa_below_275)/COUNT(x_12_cgpa) AS x_12_cgpa_below_275_percent,
+    
+    avg(x_11_cgpa) AS avg_11_cgpa,
+    SUM(x_11_cgpa_325)/COUNT(x_11_cgpa) AS x_11_325_percent,
+    SUM(x_11_cgpa_275_325)/COUNT(x_11_cgpa) AS x_11_cgpa_275_325_percent,
+    SUM(x_11_cgpa_below_275)/COUNT(x_11_cgpa) AS x_11_cgpa_below_275_percent,
+    
+    avg(act_highest_comp) AS avg_act_highest_comp,
+    avg(sat_highest_total) AS avg_sat_highest_total,
+    
+    FROM join_admit_data
+    GROUP BY admit_college_name, admit_college_id, site_short, high_school_graduating_class_c, x_11_cgpa_bucket ,readiness_composite_off_c
