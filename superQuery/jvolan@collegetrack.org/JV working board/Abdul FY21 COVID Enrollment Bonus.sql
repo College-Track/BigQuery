f6deb6f6b9d176ffc341239b569440f47e0ef1aa
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-09-30 15:00:38 -0700

    Auto-commit: Thu Sep 30 2021 15:00:33 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Abdul FY21 COVID Enrollment Bonus.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Abdul FY21 COVID Enrollment Bonus.sql
@@ -1,4 +1,17 @@
-SELECT
+ 
+    SELECT
+    count(Contact_Id),
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE 
+    GAS_Name IN ("Spring 2019-20 (Semester)","Fall 2020-21 (Semester)", "Spring 2020-21 (Semester)")
+    AND student_audit_status_c IN ("Current CT HS Student","Onboarding")
+    AND attended_workshops_c > 0
+    GROUP BY gas_name
+
+/*
+    SELECT
     Contact_Id,
     full_name_c,
     high_school_graduating_class_c,
@@ -11,3 +24,4 @@ SELECT
     AND student_audit_status_c IN ("Current CT HS Student","Onboarding")
     AND attended_workshops_c > 0
     GROUP BY Contact_Id,full_name_c,high_school_graduating_class_c,site_short,College_Track_Status_Name
+*/
