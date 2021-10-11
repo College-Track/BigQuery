fbc669bf5a75e4991a031b84634d7a63839d142f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-11 10:20:27 -0700

    Auto-commit: Mon Oct 11 2021 10:20:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sanbox_c_survey.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sanbox_c_survey.sql
@@ -1,18 +1,21 @@
     SELECT
     Contact_Id,
     current_college_clean AS college_name,
-    a.id,
+    a.id AS account_id,
     "Spring" AS cs_term,
     
     
-    i_felt_i_belonged_on_my_college_campus AS cs_belong_on_campus,
+    CASE
+        WHEN i_felt_i_belonged_on_my_college_campus IS NULL THEN NULL
+        WHEN i_felt_i_belonged_on_my_college_campus IN ("Agree") THEN 1
+        ELSE 0
+    END AS cs_belong_num,
+    
+    
     most_students_i_met_were_focused_on_getting_a_bachelors_degree AS cs_student_ba_focus,
     my_college_is_culturally_competenthelp_note_i_felt_that_the_adults_on_campus_hel AS cs_cultural_comp,
     
-    i_could_pay_for_tuition_and_living_expenses AS cs_afford_school,
-    
-    
-    
+    i_could_pay_for_tuition_and_living_expenses AS cs_afford_school
     
     FROM `data-warehouse-289815.surveys.fy20_ps_survey`
     LEFT JOIN `data-warehouse-289815.salesforce.account` a ON name = current_college_clean
