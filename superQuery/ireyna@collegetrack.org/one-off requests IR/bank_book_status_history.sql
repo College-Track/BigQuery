8a88abc688567729f05386a691b725650f20590e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 16:44:12 -0700

    Auto-commit: Mon Oct 18 2021 16:44:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/bank_book_status_history.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/bank_book_status_history.sql
@@ -1,10 +1,4 @@
-#pull bank book for inactive college students. Pull in when they became inactive to calculate when they went inactive
-
-WITH 
-
-inactive_college_students AS (
-    SELECT
-    Contact_Id,
+ Contact_Id,
     full_name_c,
     site_short,
     high_school_graduating_class_c,
@@ -17,32 +11,3 @@ inactive_college_students AS (
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template` contact 
     WHERE college_track_status_Name = 'Inactive: Post-Secondary'
- ),
-  
-  status_history AS (
-    SELECT 
-        contact_c,
-        status_history.name,
-        start_date_c AS day_marked_inactive,
-        MAX(MAX(start_date_c)) OVER (PARTITION BY contact_c) AS day_marked_inactive_max
-    
-      FROM`data-warehouse-289815.salesforce.contact_pipeline_history_c`  status_history
-        
-    WHERE 
-        status_history.Name = 'Became Inactive Post-Secondary'
-        --AND end_date_c IS NULL
-    GROUP BY 
-        status_history.name,
-        start_date_c,
-        contact_c
-        
-    )
-
-        SELECT 
-            contact.*,
-            status_history.*,
-            DATE_DIFF(CURRENT_DATE(), day_marked_inactive_max, DAY) AS days_since_inactive
-        FROM inactive_college_students AS contact
-        LEFT JOIN status_history AS status_history
-            ON contact.contact_id = status_history.contact_c
-        WHERE day_marked_inactive = day_marked_inactive_max
