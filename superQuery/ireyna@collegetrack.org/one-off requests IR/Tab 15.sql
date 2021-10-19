d57723575e99f408578c8df09dc212b784f8271e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 17:08:00 -0700

    Auto-commit: Mon Oct 18 2021 17:07:55 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
@@ -11,7 +11,7 @@
     contact.Total_BB_Earnings_as_of_HS_Grad_contact_c,
     contact.Total_Bank_Book_Balance_contact_c,
     --MAX(MAX(start_date_c)) OVER (PARTITION BY contact.contact_id) AS last_term_active,
-    at_name
+    
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template` contact 
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` term
@@ -29,7 +29,7 @@
         high_school_graduating_class_c,
         College_Track_Status_Name,
         grade_c,
-        at_name,
+        --at_name,
         anticipated_date_of_graduation_ay_c,
         Total_BB_Earnings_as_of_HS_Grad_contact_c,
         Total_Bank_Book_Balance_contact_c
