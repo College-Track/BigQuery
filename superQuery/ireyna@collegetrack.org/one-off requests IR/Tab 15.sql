6c9268d3dbc08f252df4cec7850e4a04a91545a4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 17:07:14 -0700

    Auto-commit: Mon Oct 18 2021 17:07:08 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
@@ -10,13 +10,17 @@
     contact.anticipated_date_of_graduation_ay_c,
     contact.Total_BB_Earnings_as_of_HS_Grad_contact_c,
     contact.Total_Bank_Book_Balance_contact_c,
-    MAX(MAX(start_date_c)) OVER (PARTITION BY contact.contact_id) AS last_term_active,
-    term.name
+    --MAX(MAX(start_date_c)) OVER (PARTITION BY contact.contact_id) AS last_term_active,
+    at_name
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template` contact 
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` term
         ON contact.contact_id=term.contact_id
     WHERE contact.college_track_status_Name = 'Inactive: Post-Secondary'
+    AND start_date_c IN (SELECT MAX(MAX(start_date_c)) OVER (PARTITION BY contact_id)
+                        FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+                        WHERE ct_status_at_c ='Active: Post-Secondary'
+                        GROUP BY contact_id)
     
     GROUP BY
         Contact_Id,
@@ -25,7 +29,7 @@
         high_school_graduating_class_c,
         College_Track_Status_Name,
         grade_c,
-        name,
+        at_name,
         anticipated_date_of_graduation_ay_c,
         Total_BB_Earnings_as_of_HS_Grad_contact_c,
         Total_Bank_Book_Balance_contact_c
