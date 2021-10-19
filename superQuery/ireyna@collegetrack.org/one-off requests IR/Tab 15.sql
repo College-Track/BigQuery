f21ecb8b8f5498c459cc9519f54841c91e6da029
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 18:58:29 -0700

    Auto-commit: Mon Oct 18 2021 18:58:25 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
@@ -49,7 +49,7 @@ last_term AS (
         ROW_NUMBER() OVER(PARTITION BY contact_id ORDER BY end_date_c DESC) AS row_num --pull last PAT. In case student does not have a PAT where they are active PS
         FROM `data-warehouse-289815.salesforce_clean.contact_at_template` term
         WHERE 
-        AT_Record_Type_Name = 'College/University'
+        AT_Record_Type_Name = 'College/University Semester'
         AND College_Track_Status_Name ='Inactive: Post-Secondary')
     WHERE row_num = 1
     GROUP BY at_id, contact_id, at_name, last_available_term
