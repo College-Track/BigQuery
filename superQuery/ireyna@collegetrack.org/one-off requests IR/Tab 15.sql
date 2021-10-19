fcd89003401651fc9102d116d2883fd24282b0d0
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 17:13:47 -0700

    Auto-commit: Mon Oct 18 2021 17:13:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
@@ -4,4 +4,5 @@ SELECT *
                 MAX(MAX(start_date_c)) OVER (PARTITION BY contact_id) AS start_date_c
                         FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
                         WHERE ct_status_at_c ='Active: Post-Secondary'
+                        AND College_Track_Status_Name ='Inactive: Post-Secondary'
                         GROUP BY contact_id,at_id)
