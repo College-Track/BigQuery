29da34b20e55d52bf305d5c3ca8ec1616b0bef1a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 19:14:19 -0700

    Auto-commit: Mon Oct 18 2021 19:14:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/bank_book_status_history.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/bank_book_status_history.sql
@@ -1,7 +1,4 @@
-#pull bank book for inactive college students. Pull in when they became inactive to calculate when they went inactive
-
-WITH 
-
+with 
 inactive_college_students AS (
     SELECT
     Contact_Id,
@@ -14,32 +11,113 @@ inactive_college_students AS (
     Total_BB_Earnings_as_of_HS_Grad_contact_c,
     Total_Bank_Book_Balance_contact_c
     
-    FROM `data-warehouse-289815.salesforce_clean.contact_template` contact 
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE college_track_status_Name = 'Inactive: Post-Secondary'
- ),
-  
-  status_history AS (
+),
+
+last_active_term AS (
+    SELECT 
+        * EXCEPT (row_num)
+        
+    FROM 
+        (SELECT 
+        at_id,
+        contact_id,
+        at_name,
+        end_date_c AS last_active_term_end_date,
+        ROW_NUMBER() OVER(PARTITION BY contact_id ORDER BY end_date_c DESC) AS row_num --pull last date student was active PS
+        FROM `data-warehouse-289815.salesforce_clean.contact_at_template` term
+        WHERE 
+        ct_status_at_c ='Active: Post-Secondary'
+        AND College_Track_Status_Name ='Inactive: Post-Secondary')
+    WHERE row_num = 1
+    GROUP BY at_id, contact_id, at_name, last_active_term_end_date
+        
+), 
+
+last_term AS (
+    SELECT 
+        * EXCEPT (row_num)
+        
+    FROM 
+        (SELECT 
+        at_id,
+        contact_id,
+        at_name AS last_term,
+        end_date_c AS last_available_term,
+        ROW_NUMBER() OVER(PARTITION BY contact_id ORDER BY end_date_c DESC) AS row_num --pull last PAT. In case student does not have a PAT where they are active PS
+        FROM `data-warehouse-289815.salesforce_clean.contact_at_template` term
+        WHERE 
+        AT_Record_Type_Name = 'College/University Semester'
+        AND College_Track_Status_Name ='Inactive: Post-Secondary')
+    WHERE row_num = 1
+    GROUP BY at_id, contact_id, last_term, last_available_term
+),
+    
+status_history AS (
     SELECT 
     DISTINCT
         contact_c,
-        status_history.name AS status_history,
+        name AS status_history,
         MAX(MAX(start_date_c)) OVER (PARTITION BY contact_c) AS day_marked_inactive_max
     
-      FROM`data-warehouse-289815.salesforce.contact_pipeline_history_c`  status_history
+    FROM`data-warehouse-289815.salesforce.contact_pipeline_history_c`    
+    WHERE Name = 'Became Inactive Post-Secondary'
+    GROUP BY name,contact_c
+)
+       
+    SELECT 
+        DISTINCT
+        ps.Contact_Id,
+        full_name_c,
+        site_short,
+        high_school_graduating_class_c,
+        College_Track_Status_Name,
+        grade_c,
+        anticipated_date_of_graduation_ay_c,
+        Total_BB_Earnings_as_of_HS_Grad_contact_c,
+        Total_Bank_Book_Balance_contact_c,
+        status_history,
+        CASE 
+            WHEN at_name IS NOT NULL 
+            THEN at_name 
+            WHEN last_term IS NOT NULL
+            THEN last_term
+            ELSE NULL
+        END AS pat_name,
         
-    WHERE 
-        status_history.Name = 'Became Inactive Post-Secondary'
-        --AND end_date_c IS NULL
-    GROUP BY 
-        name,
-        contact_c
-    )
-
-        SELECT 
-            contact.*,
-            status_history,
-           -- day_marked_inactive_max,
-            DATE_DIFF(CURRENT_DATE(), day_marked_inactive_max, DAY) AS days_since_inactive
-        FROM inactive_college_students AS contact
-        LEFT JOIN status_history AS status_history
-            ON contact.contact_id = status_history.contact_c
+        CASE 
+            WHEN status_history IS NULL 
+            THEN last_active_term_end_date
+            WHEN last_active_term_end_date IS NULL
+            THEN last_available_term
+            ELSE day_marked_inactive_max
+        END AS approx_inactive_date,
+        
+        DATE_DIFF(CURRENT_DATE(), day_marked_inactive_max, DAY) AS days_since_inactive
+        
+    FROM inactive_college_students AS ps
+    LEFT JOIN status_history AS sh
+        ON ps.contact_id=sh.contact_c
+    LEFT JOIN last_active_term AS term
+        ON ps.contact_id=term.contact_id
+    LEFT JOIN last_term as last
+        ON ps.contact_id=last.contact_id
+    
+    GROUP BY
+        Contact_Id,
+        full_name_c,
+        site_short,
+        high_school_graduating_class_c,
+        College_Track_Status_Name,
+        grade_c,
+        anticipated_date_of_graduation_ay_c,
+        Total_BB_Earnings_as_of_HS_Grad_contact_c,
+        Total_Bank_Book_Balance_contact_c,
+        status_history,
+        at_name,
+        last_term,
+        last_active_term_end_date,
+        day_marked_inactive_max,
+        last_available_term
+        
