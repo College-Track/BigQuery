cc581d982aa98306ca0d159c122b4b60c0182797
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-18 17:13:24 -0700

    Auto-commit: Mon Oct 18 2021 17:13:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/Tab 15.sql
@@ -1,37 +1,7 @@
---with 
---inactive_college_students AS (
-    SELECT
-    contact.Contact_Id,
-    contact.full_name_c,
-    contact.site_short,
-    contact.high_school_graduating_class_c,
-    contact.College_Track_Status_Name,
-    contact.grade_c,
-    contact.anticipated_date_of_graduation_ay_c,
-    contact.Total_BB_Earnings_as_of_HS_Grad_contact_c,
-    contact.Total_Bank_Book_Balance_contact_c,
-    --MAX(MAX(start_date_c)) OVER (PARTITION BY contact.contact_id) AS last_term_active,
-    start_date_c
-    
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_template` contact 
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` term
-        ON contact.contact_id=term.contact_id
-    WHERE contact.college_track_status_Name = 'Inactive: Post-Secondary'
-    AND start_date_c = (SELECT MAX(MAX(start_date_c)) OVER (PARTITION BY contact_id) AS start_date_c
+SELECT *
+    FROM 
+        (SELECT at_id,
+                MAX(MAX(start_date_c)) OVER (PARTITION BY contact_id) AS start_date_c
                         FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
                         WHERE ct_status_at_c ='Active: Post-Secondary'
-                        GROUP BY contact_id)
-    
-    GROUP BY
-        Contact_Id,
-        full_name_c,
-        site_short,
-        high_school_graduating_class_c,
-        College_Track_Status_Name,
-        grade_c,
-        --at_name,
-        anticipated_date_of_graduation_ay_c,
-        Total_BB_Earnings_as_of_HS_Grad_contact_c,
-        Total_Bank_Book_Balance_contact_c,
-        start_date_c
+                        GROUP BY contact_id,at_id)
