6220655ac322c0da9fdcc5c1c604f8dc1ec166b4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 12:25:51 -0700

    Auto-commit: Thu Oct 21 2021 12:25:43 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -0,0 +1,64 @@
+ # of active CO high school and college students for AY2021-22 broken out by Denver and Aurora site
+         # of CO students at each partner high school broken out by Denver and Aurora site (see page 1 of attached March 2021 update)
+
+# of Summer 2021 Corporate Residency Participants in CO
+
+#Do we have enough data to update average GPA and GPA improvement data 
+#GPA average
+#Fall 2020-21 to Spring 2020-21 GPA improvement
+
+
+--pull average cumulative GPA from Spring 2020-21
+    SELECT
+       COUNT(DISTINCT contact_id) AS student_total,
+       site_short,
+       
+     --average GPA. Pulled from Spring 2020-21
+       AVG(AT_Cumulative_GPA) AS avg_cgpa,
+    FROM
+       `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE
+       AY_Name = 'AY 2020-21'
+       AND Term_c = 'Spring'
+       AND AT_Record_Type_Name = 'High School Semester'
+       AND AY_2020_21_student_served_c = 'High School Student'
+       AND region_short = 'Colorado'
+    GROUP BY
+      site_short,
+      at_grade_c,
+      high_school_class_c
+  ;
+--pull term GPA from Fall 2020-21 to Spring 2020-21 to assess growth  
+  WITH
+
+term_gpa AS (
+  
+  SELECT 
+    COUNT(DISTINCT contact_id) AS student_total,
+    site_short,
+    CASE 
+        WHEN term_c = 'Fall' THEN AT_Term_GPA
+    END AS fall_term_gpa,
+    CASE 
+        WHEN term_c = 'Spring' THEN AT_Term_GPA
+    END AS spring_term_gpa
+        
+    FROM
+   `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE
+       AY_Name = 'AY 2020-21'
+       AND Term_c <> 'Summer'
+       AND AT_Record_Type_Name = 'High School Semester'
+       AND AY_2020_21_student_served_c = 'High School Student'
+       AND region_short = 'Colorado'
+       
+    GROUP BY
+        site_short,
+        AT_Term_GPA,
+        term_c
+)
+    SELECT
+        student_total,
+        site_short,
+        fall_term_gpa - spring_term_gpa AS gpa_term_growth
+    from term_gpa
