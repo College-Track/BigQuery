37bdad253868ac64a98067fede9d0d4f32c3dbc5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 10:42:15 -0700

    Auto-commit: Thu Oct 21 2021 10:42:10 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
@@ -1,30 +1,16 @@
-SELECT 
-    contact_id,
-    full_name_c,
-    College_Track_Status_Name,
-    Most_Recent_GPA_Cumulative_c,
-    anticipated_date_of_graduation_ay_c,
-    academic_year_4_year_degree_earned_c,
-    academic_year_term_4_year_degree_earned_c,
-    CASE WHEN College_Track_Status_Name= 'Active: Post-Secondary' THEN Current_school_name
-        ELSE college_4_year_degree_earned_c
-        END AS college_attending_or_attended,
-    CASE WHEN College_Track_Status_Name= 'Active: Post-Secondary' THEN Current_Major_c
-        ELSE major_4_year_degree_earned_c
-        END AS major,
-    CASE WHEN College_Track_Status_Name= 'Active: Post-Secondary' THEN Current_Major_specific_c 
-        ELSE major_other_4_year_degree_earned_c
-        END AS major_other_specific
-
-FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-
-    WHERE
-        AY_Name = 'AY 2020-21'
-        AND Term_c = 'Spring'
-        AND AT_Record_Type_Name = 'College/University Semester'
-        AND full_name_c IN( 
-                        'Natalie Alfaro Rivas', 
-                        'Karen Roman-Vite',
-                        'Yanira Soto',
-                        'Faustina Ngo'
+ SELECT
+        student_c,
+        site_c,
+        global_academic_year_c,
+        program_participation_status_c,
+        organization_c,
+        role_c,
+        hourly_pay_rate_c,
+        record_type_id
+        
+    FROM `data-warehouse-289815.salesforce.career_readiness_c` 
+    WHERE student_c IN ('0034600001TQqAsAAL',
+                        '0034600001TQrd3AAD',
+                        '0034600001TQpWBAA1',
+                        '0034600001TQqwIAAT'
                         )
