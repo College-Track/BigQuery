f1e64eeb2b9b4d6cbc4899dddeac22a2bb26c6a8
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 10:48:03 -0700

    Auto-commit: Thu Oct 21 2021 10:47:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
@@ -1,17 +1,13 @@
-SELECT
-        student_c,
-        site_c,
-        global_academic_year_c,
-        program_participation_status_c,
-        organization_c,
-        role_c,
-        hourly_pay_rate_c,
-        record_type_id
-        
-    FROM `data-warehouse-289815.salesforce.career_readiness_c` 
-    WHERE student_c IN ('0034600001TQqAsAAL',
-                        '0034600001TQrd3AAD',
-                        '0034600001TQpWBAA1',
-                        '0034600001TQqwIAAT',
-                        '0034600001TQsaPAAT'
+SELECT full_name_c,internship_facilitated_by_c,internship_organization_c,internship_compensation_c,internship_related_to_career_interests_c
+    
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+ WHERE AY_Name = 'AY 2020-21'
+        AND Term_c = 'Spring'
+        AND AT_Record_Type_Name = 'College/University Semester'
+        AND full_name_c IN( 
+                        'Natalie Alfaro Rivas', 
+                        'Karen Roman-Vite',
+                        'Yanira Soto',
+                        'Faustina Ngo'
                         )
+        AND internship_current_term_c = TRUE
