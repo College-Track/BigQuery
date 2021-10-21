5488a4b85eae7881f197e1172c9bcac1a2b3bf08
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 10:54:27 -0700

    Auto-commit: Thu Oct 21 2021 10:54:23 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
@@ -1,12 +1,36 @@
-SELECT at_id,full_name_c,internship_sector_c,internship_organization_c,internship_compensation_c,internship_related_to_major_minor_c,internship_related_to_career_interests_c
-    
-FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
- WHERE 
-        AT_Record_Type_Name = 'College/University Semester'
-        AND full_name_c IN( 
+ SELECT 
+        contact_id,
+        full_name_c,
+        College_Track_Status_Name,
+        Most_Recent_GPA_Cumulative_c,
+        anticipated_date_of_graduation_ay_c,
+        academic_year_4_year_degree_earned_c,
+        academic_year_term_4_year_degree_earned_c,
+        CASE WHEN College_Track_Status_Name= 'Active: Post-Secondary' THEN Current_school_name
+            ELSE college_4_year_degree_earned_c
+            END AS college_attending_or_attended,
+        CASE WHEN College_Track_Status_Name= 'Active: Post-Secondary' THEN Current_Major_c
+            ELSE major_4_year_degree_earned_c
+            END AS major,
+        CASE WHEN College_Track_Status_Name= 'Active: Post-Secondary' THEN Current_Major_specific_c 
+            ELSE major_other_4_year_degree_earned_c
+            END AS major_other_specific,
+        at_id,
+        internship_sector_c,
+        internship_organization_c,
+        internship_organization_other_c,
+        internship_compensation_c,
+        internship_related_to_major_minor_c,
+        internship_related_to_career_interests_c
+
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+
+    WHERE
+        full_name_c IN( 
                         'Natalie Alfaro Rivas', 
                         'Karen Roman-Vite',
                         'Yanira Soto',
                         'Faustina Ngo'
                         )
-        AND at_id IN (SELECT at_id FROM `data-warehouse-289815.salesforce_clean.contact_at_template` WHERE internship_current_term_c = TRUE)
+        AND AT_Record_Type_Name = 'College/University Semester'
+        AND internship_current_term_c = TRUE
