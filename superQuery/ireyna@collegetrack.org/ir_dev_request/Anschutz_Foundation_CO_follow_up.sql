8b83420ff8553dfcfa1472bd7cb6a8c72f3cc771
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 15:21:03 -0700

    Auto-commit: Thu Oct 21 2021 15:20:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -1,15 +1,15 @@
 SELECT
-       COUNT(DISTINCT contact_id) AS student_total,
        --site_short,
        region_short,
        
      --average GPA. Pulled from Spring 2020-21
-       AVG(AT_Cumulative_GPA) AS avg_cgpa,
-       --SUM(CASE WHEN AT_Cumulative_GPA >= 3.25 THEN 1 ELSE 0 END) AS cumulative_gpa_3_25
+        AVG(AT_Cumulative_GPA) AS avg_cgpa,
+        --SUM(CASE WHEN AT_Cumulative_GPA >= 3.25 THEN 1 ELSE 0 END) AS cumulative_gpa_3_25
+        COUNT(DISTINCT contact_id) AS student_total,
     FROM
        `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE
-       AY_Name = 'AY 2019-20'
+       AY_Name = 'AY 2020-21'
        AND Term_c = 'Spring'
        AND AT_Record_Type_Name = 'High School Semester'
        AND AY_2019_20_student_served_c = 'High School Student'
