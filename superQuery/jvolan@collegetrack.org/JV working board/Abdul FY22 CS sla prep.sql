0a356c9177ccbdee247c31dbd82aa331e0664f08
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-21 14:51:59 -0700

    Auto-commit: Thu Oct 21 2021 14:51:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 CS sla prep.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 CS sla prep.sql
@@ -8,4 +8,4 @@ SELECT
     FROM `data-warehouse-289815.salesforce.student_life_activity_c`
     WHERE eligible_for_bank_book_service_earnings_c = TRUE
     AND status_c = "Approved"
-    AND DATE(created_date) > DATE(2021,11,01)
+    AND DATE(created_date) > DATE(2021,10,01)
