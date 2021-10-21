56485363b0b516e82bf0da2537baedd124fa502e
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 15:02:11 -0700

    Auto-commit: Thu Oct 21 2021 15:02:07 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -3,16 +3,15 @@
        site_short,
        
      --average GPA. Pulled from Spring 2020-21
-       AVG(AT_Cumulative_GPA) AS avg_cgpa,
+       --AVG(AT_Cumulative_GPA) AS avg_cgpa,
        SUM(CASE WHEN AT_Cumulative_GPA >= 3.25 THEN 1 ELSE 0 END) AS cumulative_gpa_3_25
     FROM
        `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE
-       AY_Name = 'AY 2020-21'
+       AY_Name = 'AY 2019-20'
        AND Term_c = 'Spring'
        AND AT_Record_Type_Name = 'High School Semester'
-       AND AY_2020_21_student_served_c = 'High School Student'
+       AND AY_2019_20_student_served_c = 'High School Student'
        AND region_short = 'Colorado'
-       AND ay_2020_21_student_served_c ='High School Student'
     GROUP BY
       site_short
