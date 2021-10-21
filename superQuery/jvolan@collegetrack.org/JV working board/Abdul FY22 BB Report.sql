84c485a48aa5ff812c11d7219b8cfa266bb38d6e
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-21 09:58:51 -0700

    Auto-commit: Thu Oct 21 2021 09:58:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 BB Report.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 BB Report.sql
@@ -1,4 +1,6 @@
-SELECT
+WITH gather_students AS
+(   
+    SELECT
     Contact_Id,
     College_Track_Status_Name,
     high_school_graduating_class_c,
@@ -6,3 +8,31 @@ SELECT
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     WHERE College_Track_Status_Name = "Current CT HS Student"
+),
+
+gather_bb_apps AS    
+(   
+    SELECT
+    student_c,
+    CONCAT("https://ctgraduates.lightning.force.com/lightning/r/Scholarship_Application__c/", id, "/view") AS bb_app_url,
+    
+    total_bank_book_earnings_current_c,
+    total_service_earnings_c,
+    total_attendance_earnings_c,
+    total_gpaearnings_c,
+    
+    FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
+),
+
+join_data AS
+(
+    SELECT
+    *
+    
+    FROM gather_students
+    LEFT JOIN gather_bb_apps ON student_c = Contact_Id
+)
+
+    SELECT
+    *
+    FROM join_data
