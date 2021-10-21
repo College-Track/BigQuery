ab2d5005f691d47dd3356bc9ddb1069754357e4f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 10:48:22 -0700

    Auto-commit: Thu Oct 21 2021 10:48:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
@@ -1,9 +1,8 @@
 SELECT full_name_c,internship_facilitated_by_c,internship_organization_c,internship_compensation_c,internship_related_to_career_interests_c
     
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
- WHERE AY_Name = 'AY 2020-21'
-        AND Term_c = 'Spring'
-        AND AT_Record_Type_Name = 'College/University Semester'
+ WHERE 
+        AT_Record_Type_Name = 'College/University Semester'
         AND full_name_c IN( 
                         'Natalie Alfaro Rivas', 
                         'Karen Roman-Vite',
