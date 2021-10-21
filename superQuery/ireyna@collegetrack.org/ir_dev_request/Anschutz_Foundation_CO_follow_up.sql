6be5e9379b52dde34b93e0dab877db83690cca3b
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 14:40:43 -0700

    Auto-commit: Thu Oct 21 2021 14:40:40 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -1,15 +1,4 @@
- # of active CO high school and college students for AY2021-22 broken out by Denver and Aurora site
-         # of CO students at each partner high school broken out by Denver and Aurora site (see page 1 of attached March 2021 update)
-
-# of Summer 2021 Corporate Residency Participants in CO
-
-#Do we have enough data to update average GPA and GPA improvement data 
-#GPA average
-#Fall 2020-21 to Spring 2020-21 GPA improvement
-
-
---pull average cumulative GPA from Spring 2020-21
-    SELECT
+SELECT
        COUNT(DISTINCT contact_id) AS student_total,
        site_short,
        
@@ -28,30 +17,3 @@
       site_short,
       at_grade_c,
       high_school_class_c
-  ;
---pull term GPA from Fall 2020-21 to Spring 2020-21 to assess growth  
-  
-  
-  SELECT 
-    full_name_c,
-    contact_id,
-    site_short,
-    gpa_growth_prev_semester_c,
-    CASE WHEN gpa_growth_prev_semester_c>0 THEN 1 ELSE 0 END AS gpa_growth_indicator,
-    AT_Term_GPA
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE
-       AY_Name = 'AY 2020-21'
-       AND Term_c = 'Spring'
-       AND AT_Record_Type_Name = 'High School Semester'
-       AND AY_2020_21_student_served_c = 'High School Student'
-       AND region_short = 'Colorado'
-       
-    GROUP BY
-        site_short,
-        contact_id,
-        AT_Term_GPA,
-        full_name_c,
-        gpa_growth_prev_semester_c,
-        AT_Term_GPA
