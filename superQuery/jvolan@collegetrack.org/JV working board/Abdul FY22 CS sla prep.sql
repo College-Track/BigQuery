a7dc7973a3fb9204bf19b951589fbdb1b7344bcd
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-21 14:56:37 -0700

    Auto-commit: Thu Oct 21 2021 14:56:33 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 CS sla prep.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 CS sla prep.sql
@@ -1,4 +1,7 @@
-SELECT 
+
+WITH gather_new_approved_sla AS
+(
+    SELECT 
     student_c AS sla_student,
     id AS sla_id,
     created_date,
@@ -10,3 +13,42 @@ SELECT
     AND status_c = "Approved"
     AND DATE(created_date) > DATE(2021,10,01)
     ORDER BY sla_student,created_date ASC
+    -- PLACEHOLDER for OP Processed Record for BB = FALSE --
+),
+
+gather_students AS
+(   
+    SELECT
+    Contact_Id,
+    College_Track_Status_Name,
+    high_school_graduating_class_c,
+    site_short,
+    community_service_hours_c AS bb_elig_cs_hours,
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+),
+
+gather_bb_apps AS    
+(   
+    SELECT
+    student_c,
+    --PLACEHOLDER for CT Advised -- 
+    total_service_earnings_c + total_service_earnings_c AS cs_1600_cap,
+
+    FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
+    WHERE scholarship_application_record_type_name = "Bank Book"
+),
+
+join_data AS
+(
+    SELECT
+    *
+    
+    FROM gather_new_approved_sla
+    LEFT JOIN gather_students ON Contact_Id = sla_student
+    LEFT JOIN gather_bb_apps ON student_c = sla_student
+)
+
+    SELECT
+    *
+    FROM join_data
