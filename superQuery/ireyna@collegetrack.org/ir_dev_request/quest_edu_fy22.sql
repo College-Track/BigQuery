76e0cb9c2bafa3577cc62f9df4a3929e2244ad34
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 10:42:31 -0700

    Auto-commit: Thu Oct 21 2021 10:42:27 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
@@ -1,4 +1,4 @@
- SELECT
+SELECT
         student_c,
         site_c,
         global_academic_year_c,
@@ -8,9 +8,4 @@
         hourly_pay_rate_c,
         record_type_id
         
-    FROM `data-warehouse-289815.salesforce.career_readiness_c` 
-    WHERE student_c IN ('0034600001TQqAsAAL',
-                        '0034600001TQrd3AAD',
-                        '0034600001TQpWBAA1',
-                        '0034600001TQqwIAAT'
-                        )
+    FROM `data-warehouse-289815.salesforce.career_readiness_c`
