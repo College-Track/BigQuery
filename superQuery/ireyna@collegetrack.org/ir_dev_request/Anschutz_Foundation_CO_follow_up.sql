659d1ee4c44e3ffe396423f01fae2f47f862b333
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 15:00:40 -0700

    Auto-commit: Thu Oct 21 2021 15:00:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -1,18 +1,18 @@
-SELECT
-       --site_short,
-       region_short,
-       high_school_graduating_class_c,
-     --average GPA. Pulled from Spring 2020-21
-       AVG(college_eligibility_gpa_11th_grade) AS avg_11th_cgpa,
-       COUNT(DISTINCT contact_id) AS student_total
+ SELECT
+       COUNT(DISTINCT contact_id) AS student_total,
+       site_short,
        
+     --average GPA. Pulled from Spring 2020-21
+       AVG(AT_Cumulative_GPA) AS avg_cgpa,
+       SUM(CASE WHEN AT_Cumulative_GPA >= 3.25 THEN 1 ELSE 0 END) AS cumulative_gpa_3_25
     FROM
-       `data-warehouse-289815.salesforce_clean.contact_template`
+       `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE
-        AY_2020_21_student_served_c = 'High School Student'
-        AND region_short = 'Colorado'
-        AND high_school_graduating_class_c IN ('2021','2022')
+       AY_Name = 'AY 2020-21'
+       AND Term_c = 'Spring'
+       AND AT_Record_Type_Name = 'High School Semester'
+       AND AY_2020_21_student_served_c = 'High School Student'
+       AND region_short = 'Colorado'
+       AND ay_2020_21_student_served_c ='High School Student'
     GROUP BY
-     -- site_short,
-      high_school_graduating_class_c,
-      region_short
+      site_short
