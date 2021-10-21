942977efd8f809689e108a718a90dbb1d973ed61
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-21 10:11:58 -0700

    Auto-commit: Thu Oct 21 2021 10:11:53 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/quest_edu_fy22.sql
@@ -1,16 +1,8 @@
-#Ticket: https://collegetrack.zendesk.com/agent/tickets/11874
-/*
-Students
-Natalie Alfaro Rivas, 
-Karen Roman-Vite,
-Yanira Soto,
-Faustina Ngo
-*/
-
 SELECT 
     contact_id,
     full_name_c,
     College_Track_Status_Name,
+    Most_Recent_GPA_Cumulative_c,
     anticipated_date_of_graduation_ay_c,
     academic_year_4_year_degree_earned_c,
     academic_year_term_4_year_degree_earned_c,
@@ -30,7 +22,6 @@ FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
         AY_Name = 'AY 2020-21'
         AND Term_c = 'Spring'
         AND AT_Record_Type_Name = 'College/University Semester'
-        AND AY_2020_21_student_served_c = 'Post-Secondary Student'
         AND full_name_c IN( 
                         'Natalie Alfaro Rivas', 
                         'Karen Roman-Vite',
