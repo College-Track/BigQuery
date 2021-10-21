c2f40b8f8d7c8bbf3ebded8a7179dc1afdd76012
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-21 10:19:59 -0700

    Auto-commit: Thu Oct 21 2021 10:19:55 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Abdul FY21 COVID Enrollment Bonus.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Abdul FY21 COVID Enrollment Bonus.sql
@@ -1,7 +1,8 @@
-SELECT
+WITH gather_students AS
+( 
+    SELECT
     Contact_Id,
-    full_name_c,
-    College_Track_Status_Name,
+    
     COUNT(AT_Id) AS eligble_AT_count,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
@@ -9,4 +10,31 @@ SELECT
     GAS_Name IN ("Spring 2019-20 (Semester)","Fall 2020-21 (Semester)", "Spring 2020-21 (Semester)")
     AND student_audit_status_c IN ("Current CT HS Student","Onboarding")
     AND attended_workshops_c > 0
-    GROUP BY Contact_Id,full_name_c, College_Track_Status_Name
+    GROUP BY Contact_Id
+),
+
+gather_bb_apps AS    
+(   
+    SELECT
+    student_c,
+    id AS bb_app_id,
+    
+    total_service_earnings_c,
+    --placeholder for adding in + ct advised here --
+   
+    
+    FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
+),
+
+join_data AS
+(
+    SELECT
+    gather_students.*,
+    gather_bb_apps.* except (student_c),
+    
+    FROM gather_students
+    LEFT JOIN gather_bb_apps ON student_c = Contact_Id
+)
+    SELECT
+    *
+    FROM join_data
