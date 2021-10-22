95acf139f78f665fd9858affdf049085e91264fb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 12:11:33 -0700

    Auto-commit: Fri Oct 22 2021 12:11:28 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -0,0 +1,19 @@
+
+    SELECT
+        if_site_sort,
+        site_short,
+        matriculated_affordable / twelfth_grade_count AS matriculation_affordable_college,
+       
+
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
+    
+    
+   /*
+Percent of Students on Track to Earn Bachelor's Degree within 6 years
+Matriculation to a 4-yr College
+Freshman Persistence Rate
+Matriculation Rate
+Matriculation to Good, Best, or Situational Best Fit colleges
+Matriculation to Affordable colleges
+Active college students complete at least 1 internship before graduation
+    */
