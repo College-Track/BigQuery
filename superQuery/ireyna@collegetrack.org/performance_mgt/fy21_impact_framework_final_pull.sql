9c2ed46f345b90b72837726f915f90b3217ed023
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 12:12:38 -0700

    Auto-commit: Fri Oct 22 2021 12:12:34 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -5,11 +5,17 @@
         matriculated_affordable,
         twelfth_grade_count,
         matriculated_affordable / twelfth_grade_count AS matriculation_affordable_college,
+        SUM(matriculated_affordable) AS matriculated_total,
+        SUM(twelfth_grade_count) AS senior_total
        
 
     FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
     
-    
+    GROUP By
+         if_site_sort,
+        site_short,
+        matriculated_affordable,
+        twelfth_grade_count,
    /*
 Percent of Students on Track to Earn Bachelor's Degree within 6 years
 Matriculation to a 4-yr College
