5ad8ae14a42a67c81ca3ad95795cd02619fabe93
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 13:41:49 -0700

    Auto-commit: Fri Oct 22 2021 13:41:46 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -1,30 +1,31 @@
-SELECT
-        region_short,
+  SELECT
+        if_site_sort,
+        site_short,
         
-        SUM(matriculation_numerator) AS num_matriculation,
-        SUM(matriculated_affordable) AS num_matriculation_affordable,
-        SUM(matriculated_best_good_situational) AS num_matriculated_best_good_situational,
-        SUM(twelfth_grade_count) AS senior_denominator,
+    --matriculation data
+        matriculation_numerator AS num_matriculation,
+        matriculated_affordable AS num_matriculation_affordable,
+        matriculated_best_good_situational AS num_matriculated_best_good_situational,
+        college_first_enrolled_school_type_numerator AS matriculation_4yr,
+        twelfth_grade_count AS senior_denominator,
         
     --persistence data
-        SUM(indicator_persisted_into_2_nd_year_ct_numerator) AS num_persistence,
-        SUM(persistence_denominator) AS persistence_denominator,
+        indicator_persisted_into_2_nd_year_ct_numerator AS num_persistence,
+        persistence_denominator,
     
     --on-track data
-        SUM(on_track_numerator) AS on_track_numerator,
-        SUM(on_track_denominator) AS on_track_denominator,
+        on_track_numerator,
+        on_track_denominator,
     
     --college students graduating with 1+ internship
-        SUM(had_1_plus_internship_numerator) AS had_1_plus_internship_numerator,
-        SUM(had_1_plus_internship_denominator) AS had_1_plus_internship_denominator,
+        had_1_plus_internship_numerator,
+        had_1_plus_internship_denominator,
         
     --6 year graduation rate
-        SUM(grade_rate_6_years_current_class_numerator) AS num_6_yr_grad_rate,
-        SUM(grade_rate_6_years_current_class_denom) AS grade_rate_6_years_current_class_denom,
+        grade_rate_6_years_current_class_numerator AS num_6_yr_grad_rate,
+        grade_rate_6_years_current_class_denom,
     
     --alumni
-        SUM(alumni_count) AS alumni_count
+        alumni_count
 
-    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
-    GROUP BY
-        region_short
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`
