e681c57d1fa15e778065b5e894a1c6639307770a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 11:35:58 -0700

    Auto-commit: Fri Oct 22 2021 11:35:47 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -1,4 +1,24 @@
 SELECT
+       --site_short,
+       region_short,
+       high_school_graduating_class_c,
+     --average GPA. Pulled from Spring 2020-21
+       AVG(college_eligibility_gpa_11th_grade) AS avg_11th_cgpa,
+       COUNT(DISTINCT contact_id) AS student_total
+       
+    FROM
+       `data-warehouse-289815.salesforce_clean.contact_template`
+    WHERE
+        AY_2020_21_student_served_c = 'High School Student'
+        AND region_short = 'Colorado'
+        AND high_school_graduating_class_c IN ('2021','2022')
+    GROUP BY
+      site_short,
+      high_school_graduating_class_c,
+      region_short
+     ;
+--active HS students at partner high schools
+    SELECT
         COUNT(DISTINCT contact_id) AS student_total,
         AT_School_Name,
         site_short
