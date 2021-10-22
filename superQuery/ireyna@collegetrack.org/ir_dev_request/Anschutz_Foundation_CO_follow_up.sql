7e49d7c6e16b473077d19179451bb9e7b9720a98
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 11:32:22 -0700

    Auto-commit: Fri Oct 22 2021 11:32:09 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -1,19 +1,14 @@
-SELECT
-       --site_short,
-       region_short,
-       
-     --average GPA. Pulled from Spring 2020-21
-        AVG(AT_Cumulative_GPA) AS avg_cgpa,
-        --SUM(CASE WHEN AT_Cumulative_GPA >= 3.25 THEN 1 ELSE 0 END) AS cumulative_gpa_3_25
-        COUNT(DISTINCT contact_id) AS student_total,
-    FROM
-       `data-warehouse-289815.salesforce_clean.contact_at_template`
+ SELECT
+        COUNT(DISTINCT contact_id) AS student_total
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE
-       AY_Name = 'AY 2020-21'
-       AND Term_c = 'Spring'
-       AND AT_Record_Type_Name = 'High School Semester'
-       AND AY_2020_21_student_served_c = 'High School Student'
-       AND region_short = 'Colorado'
-    GROUP BY
-      --site_short,
-      region_short
+        region_short = 'Colorado'
+        AND (AT_Record_Type_Name = 'High School Semester'
+        OR AT_School_Name LIKE '%Rangeview%' OR
+            AT_School_Name  LIKE '%Gateway%' OR
+            AT_School_Name  LIKE'%Kunsmiller%' OR
+            AT_School_Name  LIKE '%Sheridan%' OR
+            AT_School_Name  LIKE '%Abraham%' OR
+            AT_School_Name  LIKE '%South%' OR
+            AT_School_Name  LIKE '%John F. Kennedy%' OR
+            AT_School_Name  LIKE '%Thomas Jefferson%')
