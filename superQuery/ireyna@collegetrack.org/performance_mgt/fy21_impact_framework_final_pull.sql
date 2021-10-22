aac6fc4cb7d3e93e888cc0159c0ddb76bb217b24
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 13:26:10 -0700

    Auto-commit: Fri Oct 22 2021 13:26:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -1,7 +1,6 @@
-
-    SELECT
-        if_site_sort,
-        site_short,
+SELECT
+        
+        region_short,
         
     --matriculation data
         matriculation_numerator AS num_matriculation,
@@ -28,14 +27,4 @@
     --alumni
         alumni_count
 
-    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
-    
-   /*
-Percent of Students on Track to Earn Bachelor's Degree within 6 years
-Matriculation to a 4-yr College
-Freshman Persistence Rate
-Matriculation Rate
-Matriculation to Good, Best, or Situational Best Fit colleges
-Matriculation to Affordable colleges
-Active college students complete at least 1 internship before graduation
-    */
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`
