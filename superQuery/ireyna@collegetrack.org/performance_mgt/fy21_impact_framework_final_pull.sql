e25383624f623e4f72b872082bce93e111fcd8cd
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 13:49:37 -0700

    final query

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -1,4 +1,5 @@
-  SELECT
+--site breakdown
+    SELECT
         if_site_sort,
         site_short,
         
@@ -28,4 +29,39 @@
     --alumni
         alumni_count
 
-    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
+    ;
+--region breakdown
+SELECT
+        region_short,
+        
+        SUM(matriculation_numerator) AS num_matriculation,
+        SUM(matriculated_affordable) AS num_matriculation_affordable,
+        SUM(matriculated_best_good_situational) AS num_matriculated_best_good_situational,
+        SUM(college_first_enrolled_school_type_numerator) AS matriculation_4yr,
+        SUM(twelfth_grade_count) AS senior_denominator,
+        
+    --persistence data
+        SUM(indicator_persisted_into_2_nd_year_ct_numerator) AS num_persistence,
+        SUM(persistence_denominator) AS persistence_denominator,
+    
+    --on-track data
+        SUM(on_track_numerator) AS on_track_numerator,
+        SUM(on_track_denominator) AS on_track_denominator,
+    
+    --college students graduating with 1+ internship
+        SUM(had_1_plus_internship_numerator) AS had_1_plus_internship_numerator,
+        SUM(had_1_plus_internship_denominator) AS had_1_plus_internship_denominator,
+        
+    --6 year graduation rate
+        SUM(grade_rate_6_years_current_class_numerator) AS num_6_yr_grad_rate,
+        SUM(grade_rate_6_years_current_class_denom) AS grade_rate_6_years_current_class_denom,
+    
+    --alumni
+        SUM(alumni_count) AS alumni_count
+
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
+    GROUP BY
+        region_short
+;
+--matriculation to 4 year college
