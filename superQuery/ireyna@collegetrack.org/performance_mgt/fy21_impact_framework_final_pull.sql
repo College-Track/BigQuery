0e60a07348f2b689ac2265a174d286e1b4eb3f41
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 12:47:47 -0700

    added 6 year grad rate and alumni

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -2,6 +2,7 @@
     SELECT
         if_site_sort,
         site_short,
+        
     --matriculation data
         matriculation_numerator AS num_matriculation,
         matriculation_numerator / twelfth_grade_count AS matriculation,
@@ -15,11 +16,27 @@
         twelfth_grade_count AS senior_denominator,
         
     --persistence data
-        indicator_persisted_into_2_nd_year_ct_numerator AS num_persistence
-        indicator_persisted_into_2_nd_year_ct_numerator / persistence_denominator AS freshman_persistencec,
-        
+        indicator_persisted_into_2_nd_year_ct_numerator AS num_persistence,
+        indicator_persisted_into_2_nd_year_ct_numerator / persistence_denominator AS freshman_persistence,
         persistence_denominator,
-
+    
+    --on-track data
+        on_track_numerator,
+        on_track_numerator / on_track_denominator AS on_track,
+        on_track_denominator,
+    
+    --college students graduating with 1+ internship
+        had_1_plus_internship_numerator,
+        had_1_plus_internship_numerator / had_1_plus_internship_denominator AS internships,
+        had_1_plus_internship_denominator,
+        
+    --6 year graduation rate
+        grade_rate_6_years_current_class_numerator AS num_6_yr_grad_rate,
+        grade_rate_6_years_current_class_numerator / grade_rate_6_years_current_class_denom AS six_yr_grad_rate,
+        grade_rate_6_years_current_class_denom,
+    
+    --alumni
+        alumni_count
 
     FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
     
