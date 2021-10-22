19f7df59709704d8a920a2b53e7cfb5632806eeb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 11:38:52 -0700

    Auto-commit: Fri Oct 22 2021 11:38:48 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
+++ superQuery/ireyna@collegetrack.org/ir_dev_request/Anschutz_Foundation_CO_follow_up.sql
@@ -1,24 +1,4 @@
-SELECT
-       --site_short,
-       region_short,
-       high_school_graduating_class_c,
-     --average GPA. Pulled from Spring 2020-21
-       AVG(college_eligibility_gpa_11th_grade) AS avg_11th_cgpa,
-       COUNT(DISTINCT contact_id) AS student_total
-       
-    FROM
-       `data-warehouse-289815.salesforce_clean.contact_template`
-    WHERE
-        AY_2020_21_student_served_c = 'High School Student'
-        AND region_short = 'Colorado'
-        AND high_school_graduating_class_c IN ('2021','2022')
-    GROUP BY
-      site_short,
-      high_school_graduating_class_c,
-      region_short
-     ;
---active HS students at partner high schools
-    SELECT
+ SELECT
         COUNT(DISTINCT contact_id) AS student_total,
         AT_School_Name,
         site_short
@@ -36,6 +16,7 @@ SELECT
             AT_School_Name  LIKE '%John F. Kennedy%' OR
             AT_School_Name  LIKE '%Thomas Jefferson%')
         AND College_Track_Status_Name IN ('Current CT HS Student','Leave of Absence')
+        AND current_as_c = TRUE
     GROUP BY
         site_short,
         AT_School_Name
