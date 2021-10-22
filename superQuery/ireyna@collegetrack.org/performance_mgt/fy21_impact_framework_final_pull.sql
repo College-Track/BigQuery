ece816fae1f642afc4f9cbef4b4b943426c6b3af
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-22 12:49:04 -0700

    Auto-commit: Fri Oct 22 2021 12:49:01 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -5,34 +5,24 @@
         
     --matriculation data
         matriculation_numerator AS num_matriculation,
-        matriculation_numerator / twelfth_grade_count AS matriculation,
-        
         matriculated_affordable AS num_matriculation_affordable,
-        matriculated_affordable / twelfth_grade_count AS matriculation_affordable_college,
-        
         matriculated_best_good_situational AS num_matriculated_best_good_situational,
-        matriculated_best_good_situational / twelfth_grade_count AS matriculated_best_good_situational,
-        
         twelfth_grade_count AS senior_denominator,
         
     --persistence data
         indicator_persisted_into_2_nd_year_ct_numerator AS num_persistence,
-        indicator_persisted_into_2_nd_year_ct_numerator / persistence_denominator AS freshman_persistence,
         persistence_denominator,
     
     --on-track data
         on_track_numerator,
-        on_track_numerator / on_track_denominator AS on_track,
         on_track_denominator,
     
     --college students graduating with 1+ internship
         had_1_plus_internship_numerator,
-        had_1_plus_internship_numerator / had_1_plus_internship_denominator AS internships,
         had_1_plus_internship_denominator,
         
     --6 year graduation rate
         grade_rate_6_years_current_class_numerator AS num_6_yr_grad_rate,
-        grade_rate_6_years_current_class_numerator / grade_rate_6_years_current_class_denom AS six_yr_grad_rate,
         grade_rate_6_years_current_class_denom,
     
     --alumni
