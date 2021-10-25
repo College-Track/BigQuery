db4edef26946e7953a5bdcfa8e4b026b869ec2a0
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-25 11:21:45 -0700

    renaming 80% attendance in Fall and Spring field

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -1,3 +1,43 @@
+--site breakdown
+    SELECT
+        if_site_sort,
+        site_short,
+        
+    --matriculation data
+        matriculation_numerator AS num_matriculation,
+        matriculated_affordable AS num_matriculation_affordable,
+        matriculated_best_good_situational AS num_matriculated_best_good_situational,
+        college_first_enrolled_school_type_numerator AS matriculation_4yr,
+        twelfth_grade_count AS senior_denominator,
+        
+    --persistence data
+        indicator_persisted_into_2_nd_year_ct_numerator AS num_persistence,
+        persistence_denominator,
+    
+    --on-track data
+        on_track_numerator,
+        on_track_denominator,
+    
+    --college students graduating with 1+ internship
+        had_1_plus_internship_numerator,
+        had_1_plus_internship_denominator,
+        
+    --6 year graduation rate
+        grade_rate_6_years_current_class_numerator AS num_6_yr_grad_rate,
+        grade_rate_6_years_current_class_denom,
+    
+    --alumni
+        alumni_count,
+        --MEMO data
+        had_mse_numerator AS mse_numerator,
+        had_mse_denominator AS mse_denominator,
+        
+        above_80_attendance_memo AS attendance_80_2_terms_numerator,
+        high_school_student_count AS high_school_student_count
+
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
+    ;
+--region breakdown
 SELECT
         region_short,
         
@@ -30,7 +70,7 @@ SELECT
         SUM(had_mse_numerator) AS mse_numerator,
         SUM(had_mse_denominator) AS mse_denominator,
         
-        SUM(above_80_attendance_memo) AS denom_attendance_80_2_terms,
+        SUM(above_80_attendance_memo) AS attendance_80_2_terms_numerator,
         SUM(high_school_student_count) AS high_school_student_count
 
     FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
