11a416f9c654884f34aa78b67276ceb489be074a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-25 11:20:39 -0700

    Auto-commit: Mon Oct 25 2021 11:20:36 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_impact_framework_final_pull.sql
@@ -1,38 +1,38 @@
---site breakdown
-    SELECT
-        if_site_sort,
-        site_short,
+SELECT
+        region_short,
         
-    --matriculation data
-        matriculation_numerator AS num_matriculation,
-        matriculated_affordable AS num_matriculation_affordable,
-        matriculated_best_good_situational AS num_matriculated_best_good_situational,
-        college_first_enrolled_school_type_numerator AS matriculation_4yr,
-        twelfth_grade_count AS senior_denominator,
+        SUM(matriculation_numerator) AS num_matriculation,
+        SUM(matriculated_affordable) AS num_matriculation_affordable,
+        SUM(matriculated_best_good_situational) AS num_matriculated_best_good_situational,
+        SUM(college_first_enrolled_school_type_numerator) AS matriculation_4yr,
+        SUM(twelfth_grade_count) AS senior_denominator,
         
     --persistence data
-        indicator_persisted_into_2_nd_year_ct_numerator AS num_persistence,
-        persistence_denominator,
+        SUM(indicator_persisted_into_2_nd_year_ct_numerator) AS num_persistence,
+        SUM(persistence_denominator) AS persistence_denominator,
     
     --on-track data
-        on_track_numerator,
-        on_track_denominator,
+        SUM(on_track_numerator) AS on_track_numerator,
+        SUM(on_track_denominator) AS on_track_denominator,
     
     --college students graduating with 1+ internship
-        had_1_plus_internship_numerator,
-        had_1_plus_internship_denominator,
+        SUM(had_1_plus_internship_numerator) AS had_1_plus_internship_numerator,
+        SUM(had_1_plus_internship_denominator) AS had_1_plus_internship_denominator,
         
     --6 year graduation rate
-        grade_rate_6_years_current_class_numerator AS num_6_yr_grad_rate,
-        grade_rate_6_years_current_class_denom,
+        SUM(grade_rate_6_years_current_class_numerator) AS num_6_yr_grad_rate,
+        SUM(grade_rate_6_years_current_class_denom) AS grade_rate_6_years_current_class_denom,
     
     --alumni
-        alumni_count,
-        --MEMO data
-        had_mse_numerator AS mse_numerator,
-        had_mse_denominator AS mse_denominator,
+        SUM(alumni_count) AS alumni_count,
+    
+    --MEMO data
+        SUM(had_mse_numerator) AS mse_numerator,
+        SUM(had_mse_denominator) AS mse_denominator,
         
-        above_80_attendance_memo AS denom_attendance_80_2_terms,
-        high_school_student_count AS high_school_student_count
+        SUM(above_80_attendance_memo) AS denom_attendance_80_2_terms,
+        SUM(high_school_student_count) AS high_school_student_count
 
-    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` 
+    GROUP BY
+        region_short
