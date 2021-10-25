0c6c52631396eac1b5bb788fbfe08e9bff279c0f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-25 12:19:11 -0700

    Auto-commit: Mon Oct 25 2021 12:19:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/attendance_80%_term_and_annual.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/attendance_80%_term_and_annual.sql
@@ -1,6 +1,3 @@
-WITH
-attendance AS (
-
 SELECT
     ddt.site_short,
     ddt.region_short,
@@ -26,21 +23,3 @@ SELECT
 FROM `data-studio-260217.ddt.ay_summary_table` DDT
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C
     ON DDT.Contact_Id = C.Contact_Id
-    
-    ),
-map_to_attendance_memo AS (
-SELECT 
-    SUM(CASE WHEN above_80_attendance = 0 AND above_80_attendance_memo = 1
-    THEN above_80_attendance_memo 
-    ELSE above_80_attendance
-    END) AS attendance_numerator_memo,
-    SUM(high_school_student_count) AS high_school_student_count,
-    site_short,
-    region_short
-FROM attendance
-GROUP BY 
-    site_short,
-    region_short
-)
-SELECT *
-FROM map_to_attendance_memo
