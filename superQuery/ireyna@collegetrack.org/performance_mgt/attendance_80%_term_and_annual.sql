8193fd9b21b9461289a4b241f4a23c7446d2eaf5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-25 12:20:32 -0700

    Auto-commit: Mon Oct 25 2021 12:20:29 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/attendance_80%_term_and_annual.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/attendance_80%_term_and_annual.sql
@@ -6,15 +6,15 @@ SELECT
         ELSE 0
         END AS high_school_student_count,
     CASE
-        WHEN DDT.above_80_attendance_fall = 'True' AND DDT.above_80_attendance_spring = 'True' AND
-        DDT.AY_student_served = 'High School' THEN 1
+        WHEN DDT.above_80_attendance_fall = 'True' AND DDT.above_80_attendance_spring = 'True'
+        THEN 1
         ELSE
             0
         END
             above_80_attendance_memo,
         
         CASE
-        WHEN DDT.above_80_attendance_ay = 'True' AND DDT.AY_student_served = 'High School' THEN 1
+        WHEN DDT.above_80_attendance_ay = 'True'  THEN 1
         ELSE
             0
         END
@@ -23,3 +23,4 @@ SELECT
 FROM `data-studio-260217.ddt.ay_summary_table` DDT
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` C
     ON DDT.Contact_Id = C.Contact_Id
+WHERE DDT.AY_student_served = 'High School'
