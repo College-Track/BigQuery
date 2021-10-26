c1b1e2eaa7e58d5649d8b0a4303251d93da0988d
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 16:38:03 -0700

    Auto-commit: Tue Oct 26 2021 16:37:58 GMT-0700 (Pacific Daylight Time)

---
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_cfe.sql
@@ -0,0 +1,29 @@
+    SELECT
+    Contact_Id,
+    site_short,
+    college_first_enrolled_school_c AS cfe,
+    CASE
+        WHEN 
+        (College_Track_Status_Name = "CT Alumni"
+        AND college_first_enrolled_school_c = college_4_year_degree_earned_c) THEN "Started & graduated from same HBCU"
+        WHEN 
+        (College_Track_Status_Name = "CT Alumni"
+        AND college_first_enrolled_school_c <> college_4_year_degree_earned_c) THEN "Started HBCU graduated at different school"
+        WHEN
+        (College_Track_Status_Name = "Active: Post-Secondary"
+        AND college_first_enrolled_school_c = Current_School_c) THEN "Started & still at same HBCU"
+        WHEN 
+        (College_Track_Status_Name = "Active: Post-Secondary"
+        AND college_first_enrolled_school_c <> Current_School_c) THEN "Started HBCU, but at different school now"
+        WHEN College_Track_Status_Name = "Inactive: Post-Secondary" THEN "Started HBCU, but went Inactive"
+        ELSE "NA"
+    END AS cfe_bucket,
+    Current_school_name,
+    college_4_year_degree_earned_c,
+
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = college_first_enrolled_school_c
+    WHERE historically_black_college_univ_hbcu_c = TRUE
+    AND indicator_completed_ct_hs_program_c = TRUE
+
