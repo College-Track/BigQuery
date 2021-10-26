d9c0faef77a8f9be9adcc1788dc8959bc11fe851
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 09:53:11 -0700

    Auto-commit: Tue Oct 26 2021 09:53:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
@@ -5,27 +5,12 @@
     College_Track_Status_Name,
     AY_student_served,
     AY_name,
+    AY_Cumulative_GPA,
+    AY_Cumulative_GPA_bucket,
     
-    CASE
-        WHEN (AY_enrolled_sessions IS NULL 
-        OR AY_enrolled_sessions = 0) THEN NULL
-        ELSE (AY_attended_workshops / AY_enrolled_sessions) 
-    END AS ay_attendance_rate,
-        
-    CASE
-        WHEN (AY_enrolled_sessions IS NULL 
-        OR AY_enrolled_sessions = 0) THEN NULL
-        WHEN (AY_attended_workshops / AY_enrolled_sessions) < .65 THEN "<65%"
-        WHEN (AY_attended_workshops / AY_enrolled_sessions) >= .65
-        AND (AY_attended_workshops / AY_enrolled_sessions) < .8 THEN "65% - 79%"
-        WHEN (AY_attended_workshops / AY_enrolled_sessions) >= .8
-        AND (AY_attended_workshops / AY_enrolled_sessions) < .9 THEN "80% - 89%"
-        WHEN (AY_attended_workshops / AY_enrolled_sessions) >= .9 THEN "90%+"
-    END AS attendance_bucket
+
     
     FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
     WHERE site_short IN ('Ward 8', 'The Durant Center')
-    AND AY_name IN ('AY 2019-20','AY 2020-21')
     AND AY_student_served = "High School"
-    
