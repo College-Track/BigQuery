614296860690433f1b90bfa0c94bb8f70c3958d8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 09:30:35 -0700

    Auto-commit: Tue Oct 26 2021 09:30:30 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
@@ -3,8 +3,10 @@
     high_school_graduating_class_c,
     site_short,
     College_Track_Status_Name,
-    AY_student_served
+    AY_student_served,
+    AY_name,
     
     
     FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
+    WHERE site_short IN ('Ward 8', 'Durant')
