b4e63a050e97f79bb33df4b6aa44778b6c7b6dad
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 15:53:02 -0700

    Auto-commit: Tue Oct 26 2021 15:52:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -1,3 +1,5 @@
+  --historical count of terms, students & degrees earned from HBCUs--
+ /*   
     SELECT
     AY_Name,
     site_short,
@@ -11,6 +13,21 @@
     WHERE historically_black_college_univ_hbcu_c = TRUE
     AND student_audit_status_c IN ("Active: Post-Secondary","CT Alumni")
     GROUP BY AY_Name, site_short
+    */
+
+
+ SELECT
+    Contact_Id,
+    AY_Name,
+    site_short,
+    school_c
+   
+    
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
+    WHERE historically_black_college_univ_hbcu_c = TRUE
+    AND student_audit_status_c IN ("Active: Post-Secondary","CT Alumni")
 
     
     
