3a9ab6c05401a0cd9f9ed5e4b9fc51e06ff3acc3
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 11:21:48 -0700

    Auto-commit: Tue Oct 26 2021 11:21:43 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -10,4 +10,6 @@
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
+    WHERE historically_black_college_univ_hbcu_c = TRUE
+
     
