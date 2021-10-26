69e77cc61c9cd081e657b6ace79686b03dd4fcf9
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 15:31:02 -0700

    Auto-commit: Tue Oct 26 2021 15:30:56 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -1,9 +1,10 @@
     SELECT
+    AY_Name,
     COUNT(DISTINCT Contact_Id) AS unique_student_count,
     site_short,
     COUNT(AT_Id) AS at_count,
     COUNT(type_of_degree_earned_c) AS degree_earned_count,
-    AY_Name
+    
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
