3f6681b50aa9f3c5485f60d2b976a0284d15fc40
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 09:38:45 -0700

    Auto-commit: Tue Oct 26 2021 09:38:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
@@ -6,9 +6,16 @@
     AY_student_served,
     AY_name,
     
+    CASE
+        WHEN (AY_enrolled_sessions IS NULL 
+        OR AY_enrolled_sessions = 0) THEN "No Data"
+        WHEN (AY_attended_workshops / AY_enrolled_sessions) < .65 THEN "<65%"
+        ELSE "90%"
+    END AS attendance_bucket
     
     FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
     WHERE site_short IN ('Ward 8', 'The Durant Center')
     AND AY_name IN ('AY 2019-20','AY 2020-21')
     AND AY_student_served = "High School"
+    
