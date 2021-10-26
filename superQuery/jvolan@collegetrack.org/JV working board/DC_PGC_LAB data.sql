1617df251394ba0d0740cf10ffffb2d8ab037f4f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 09:32:11 -0700

    Auto-commit: Tue Oct 26 2021 09:32:06 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/DC_PGC_LAB data.sql
@@ -10,3 +10,4 @@
     FROM `data-warehouse-289815.salesforce_clean.contact_ay_template` cay
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` c ON c.Contact_Id = cay.Contact_Id
     WHERE site_short IN ('Ward 8', 'The Durant Center')
+    AND AY_name IN ('AY 2019-20','AY 2020-21')
