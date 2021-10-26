221f86ae99af5c524071fdbd80a67387a6e15604
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-10-26 12:24:15 -0700

    Auto-commit: Tue Oct 26 2021 12:24:11 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -2,11 +2,13 @@
     Contact_Id,
     site_short,
     high_school_graduating_class_c,
+    College_Track_Status_Name
     
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
     WHERE historically_black_college_univ_hbcu_c = TRUE
-    GROUP BY Contact_id, site_short, high_school_graduating_class_c
+    GROUP BY Contact_id, site_short, high_school_graduating_class_c, College_Track_Status_Name
+
 
     
