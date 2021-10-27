2ef35508ea92280aa6aaecfb06c5d95d0caed5ee
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-27 12:06:53 -0700

    Auto-commit: Wed Oct 27 2021 12:06:49 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/orgscorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/orgscorecard_fy20.sql
@@ -1,4 +1,16 @@
+/*
+    --Add column to outcomes_overview_fy20 table
+    ALTER TABLE `org-scorecard-286421.aggregate_data.outcomes_overview_fy20` 
+    ADD COLUMN fiscal_year STRING
+*/
 
---Add column to outcomes_overview_fy20 table
-ALTER TABLE `org-scorecard-286421.aggregate_data.outcomes_overview_fy20` 
-ADD COLUMN fiscal_year STRING
+ /*    
+    CREATE
+    OR REPLACE TABLE `org-scorecard-286421.aggregate_data.outcomes_overview_fy20` OPTIONS (
+    description = "Org Scorecard FY20 outcomes overview"
+)
+AS 
+ */
+   
+  SELECT *
+  FROM `org-scorecard-286421.aggregate_data.outcomes_overview_fy20`
