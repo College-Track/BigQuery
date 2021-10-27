253e0695b9303c0f0cfb62970dd36030721ddfec
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-27 12:08:21 -0700

    Auto-commit: Wed Oct 27 2021 12:08:18 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/orgscorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/orgscorecard_fy20.sql
@@ -4,15 +4,14 @@
     ADD COLUMN fiscal_year STRING
 */
 
-     
+/*    
     CREATE
     OR REPLACE TABLE `org-scorecard-286421.aggregate_data.outcomes_overview_fy20` OPTIONS (
     description = "Org Scorecard FY20 outcomes overview"
-)
-AS 
-
+    )
+    AS 
+*/
 
 --Populating newly created column with "FY20": fiscal_year 
-  SELECT * EXCEPT (fiscal_year),
-        CASE WHEN fiscal_year IS NULL THEN 'FY20' END AS fiscal_year
-  FROM `org-scorecard-286421.aggregate_data.outcomes_overview_fy20` 
+  SELECT *
+  FROM `org-scorecard-286421.aggregate_data.outcomes_overview_fy20`
