f98f920861aab48266ce88125e791a6c8b83d718
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-10-27 12:03:57 -0700

    Creating column to org_scorecard_outcomes_overview_fy20 table: fiscal year

--- superQuery/ireyna@collegetrack.org/one-off requests IR/orgscorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/orgscorecard_fy20.sql
@@ -1,2 +1,4 @@
-    SELECT *
-    FROM `org-scorecard-286421.aggregate_data.outcomes_overview_fy20`
+
+--Add column to outcomes_overview_fy20 table
+ALTER TABLE `org-scorecard-286421.aggregate_data.outcomes_overview_fy20` 
+ADD COLUMN fiscal_year STRING
