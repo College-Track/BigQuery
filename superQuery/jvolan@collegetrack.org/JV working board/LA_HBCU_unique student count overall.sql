8bb360a7060aa04edcf9e8fa51dee44d7142127e
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:40:44 -0700

    Auto-commit: Mon Nov 01 2021 14:40:37 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
@@ -1,7 +1,11 @@
-/*SELECT
+SELECT
     COUNT(AT_Id) AS at_count,
     COUNT(DISTINCT Contact_Id) AS ay_student_count,
-    COUNT(type_of_degree_earned_c) AS degree_earned_count,
+    COUNT(
+        CASE
+        WHEN type_of_degree_earned_c = "4-Year" THEN 1
+        ELSE 0
+    END) AS degree_earned_count,
     
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
@@ -9,15 +13,3 @@
     WHERE historically_black_college_univ_hbcu_c = TRUE
     AND student_audit_status_c IN ("Active: Post-Secondary","CT Alumni")
     GROUP BY historically_black_college_univ_hbcu_c
-*/
-
-    SELECT 
-    a.name,
-    COUNT(Contact_Id) AS CT_alumni_count,
-
-    FROM `data-warehouse-289815.salesforce_clean.contact_template`
-    LEFT JOIN `data-warehouse-289815.salesforce.account`a ON a.name = college_4_year_degree_earned_c
-    WHERE historically_black_college_univ_hbcu_c = TRUE
-    AND College_Track_Status_Name = "CT Alumni"
-    GROUP BY a.name
-    ORDER BY CT_alumni_count DESC
