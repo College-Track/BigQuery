c38932a65a06cfc2a4a88f60b8199ea3269e3779
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:02:56 -0700

    Auto-commit: Mon Nov 01 2021 14:02:51 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -3,6 +3,8 @@
     amount_awarded_c,
     academic_year_c,
     current_or_intended_college_university_c,
+    a.name AS college_name,
+    historically_black_college_univ_hbcu_c,
  
     FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
     LEFT JOIN `data-warehouse-289815.salesforce.account` a ON a.id = current_or_intended_college_university_c
