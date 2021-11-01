bdb8bba6139356dbe6262f9717d85a273596b2bd
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:23:50 -0700

    Auto-commit: Mon Nov 01 2021 14:23:46 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
@@ -16,6 +16,6 @@
     a.name,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
-    LEFT JOIN `data-warehouse-289815.salesforce.account`a ON id = college_4_year_degree_earned_c
+    LEFT JOIN `data-warehouse-289815.salesforce.account`a ON a.name = college_4_year_degree_earned_c
     WHERE historically_black_college_univ_hbcu_c = TRUE
     GROUP BY a.name
