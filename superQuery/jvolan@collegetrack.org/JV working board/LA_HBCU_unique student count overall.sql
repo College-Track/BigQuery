f2fda181388b1c0b66f299b82606ce8ab2688bc4
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:47:35 -0700

    Auto-commit: Mon Nov 01 2021 14:47:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
@@ -1,15 +1,10 @@
-SELECT
-    COUNT(AT_Id) AS at_count,
-    COUNT(DISTINCT Contact_Id) AS ay_student_count,
-    SUM(
-        CASE
-        WHEN type_of_degree_earned_c = "4-year degree" THEN 1
-        ELSE 0
-    END) AS degree_earned_count,
-    
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
+SELECT 
+    a.name,
+    COUNT(Contact_Id) AS CT_alumni_count,
+
+    FROM `data-warehouse-289815.salesforce_clean.contact_template`
+    LEFT JOIN `data-warehouse-289815.salesforce.account`a ON a.name = college_4_year_degree_earned_c
     WHERE historically_black_college_univ_hbcu_c = TRUE
-    AND student_audit_status_c IN ("Active: Post-Secondary","CT Alumni")
-    GROUP BY historically_black_college_univ_hbcu_c
+    AND College_Track_Status_Name = "CT Alumni"
+    GROUP BY a.name
+    ORDER BY CT_alumni_count DESC
