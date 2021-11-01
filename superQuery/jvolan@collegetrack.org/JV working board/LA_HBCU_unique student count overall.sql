61d6026f5bcf5d8f4c6d3c0d4e45e97a3f695a5f
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:48:38 -0700

    Auto-commit: Mon Nov 01 2021 14:48:28 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_unique student count overall.sql
@@ -1,10 +1,9 @@
 SELECT 
     a.name,
-    COUNT(Contact_Id) AS CT_alumni_count,
+    Contact_Id AS CT_alumni_count
 
     FROM `data-warehouse-289815.salesforce_clean.contact_template`
     LEFT JOIN `data-warehouse-289815.salesforce.account`a ON a.name = college_4_year_degree_earned_c
     WHERE historically_black_college_univ_hbcu_c = TRUE
     AND College_Track_Status_Name = "CT Alumni"
-    GROUP BY a.name
-    ORDER BY CT_alumni_count DESC
+    
