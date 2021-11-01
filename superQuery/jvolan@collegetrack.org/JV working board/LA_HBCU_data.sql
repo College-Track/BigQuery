192ce18666f4b8f59eb70e1fe8925404b9aedba6
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:06:39 -0700

    Auto-commit: Mon Nov 01 2021 14:06:35 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -2,12 +2,14 @@
     student_c,
     amount_awarded_c,
     academic_year_c,
+    ay.name AS ay_name,
     current_or_intended_college_university_c,
     a.name AS college_name,
     historically_black_college_univ_hbcu_c,
  
     FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
     LEFT JOIN `data-warehouse-289815.salesforce.account` a ON a.id = current_or_intended_college_university_c
+    LEFT JOIN `data-warehouse-289815.salesforce.academic_year_c` ay ON  ay.id = academic_year_c
     WHERE scholarship_c = 'a3B46000000HWacEAG'
     AND status_c = 'Won'
     AND historically_black_college_univ_hbcu_c = TRUE
