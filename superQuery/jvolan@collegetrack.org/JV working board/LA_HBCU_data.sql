70144f6362e46bfee592ba7c95dcd0aa0941f976
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-01 14:02:07 -0700

    Auto-commit: Mon Nov 01 2021 14:01:59 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/LA_HBCU_data.sql
@@ -1,96 +1,14 @@
-  --historical count of terms, students & degrees earned from HBCUs--
- /*   
-    SELECT
-    AY_Name,
-    site_short,
-    COUNT(AT_Id) AS at_count,
-    COUNT(DISTINCT Contact_Id) AS unique_student_count,
-    COUNT(type_of_degree_earned_c) AS degree_earned_count,
-    
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
-    WHERE historically_black_college_univ_hbcu_c = TRUE
-    AND student_audit_status_c IN ("Active: Post-Secondary","CT Alumni")
-    GROUP BY AY_Name, site_short
-    */
-
-
-WITH fy_door AS  
-(
     SELECT
     student_c,
-    SUM(amount_awarded_c) AS door_total,
-    academic_year_c
+    amount_awarded_c,
+    academic_year_c,
+    current_or_intended_college_university_c,
  
-    
     FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
+    LEFT JOIN `data-warehouse-289815.salesforce.account` a ON a.id = current_or_intended_college_university_c
     WHERE scholarship_c = 'a3B46000000HWacEAG'
     AND status_c = 'Won'
-    GROUP BY student_c, academic_year_c
-),
-
-fy_e_fund AS
-(
-    SELECT
-    st.student_c,
-    SUM(amount_c) AS e_fund_total,
-    cat.academic_year_c
-
-    FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean` st
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` cat ON st.academic_semester_c = cat.AT_Id
-    WHERE scholarship_c = 'College Track Emergency Fund'
-    GROUP BY st.student_c,cat.academic_year_c
-),
-
-fy_bb_disbursements AS
-(
-    SELECT
-    st.student_c,
-    SUM(amount_c) AS bb_disburse_total,
-    cat.academic_year_c
-    
-    FROM `data-warehouse-289815.salesforce_clean.scholarship_transaction_clean` st
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` cat ON st.academic_semester_c = cat.AT_Id
-    WHERE st.record_type_id = '01246000000ZNhsAAG'
-    AND transaction_status_c = "Approved"
-    GROUP BY st.student_c, cat.academic_year_c
-
-),
-
-gather_students AS 
-( 
- SELECT
-    Contact_Id,
-    AY_Name,
-    academic_year_c,
-    site_short,
-    school_c
-   
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    LEFT JOIN `data-warehouse-289815.salesforce.account` ON id = school_c
-    WHERE historically_black_college_univ_hbcu_c = TRUE
-    AND student_audit_status_c IN ("Active: Post-Secondary","CT Alumni")
-)
-    SELECT
-    Contact_Id,
-    AY_Name,
-    site_short,
-    school_c,
-    gs.academic_year_c,
-    
-    d.academic_year_c,
-    d.door_total,
-    ef.e_fund_total,
-    bb.bb_disburse_total
-    
-    
-    FROM gather_students gs
-    LEFT JOIN fy_door d ON d.student_c = Contact_Id AND d.academic_year_c = gs.academic_year_c
-    LEFT JOIN fy_e_fund ef ON ef.student_c = Contact_Id AND ef.academic_year_c = gs.academic_year_c
-    LEFT JOIN fy_bb_disbursements bb ON bb.student_c = Contact_Id AND bb.academic_year_c = gs.academic_year_c
-    
+    AND historically_black_college_univ_hbcu_c = TRUE
 
 
     
