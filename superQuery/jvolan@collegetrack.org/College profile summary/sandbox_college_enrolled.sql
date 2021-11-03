4ea0560a83ef31829abef12660d2f3fdb5f3d8fb
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-03 15:34:41 -0700

    Auto-commit: Wed Nov 03 2021 15:34:31 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
@@ -38,45 +38,8 @@ WITH gather_all_enrolled_at_data AS
 gather_student_data AS
 (
     SELECT
-    Contact_Id,
-    high_school_graduating_class_c,
-    site_short,
-    AT_Cumulative_GPA AS x_12_cgpa,
-        CASE
-            WHEN AT_Cumulative_GPA >=3.25 THEN 1
-        END AS x_12_cgpa_325,
-        CASE
-            WHEN AT_Cumulative_GPA <3.25
-            AND AT_Cumulative_GPA >=2.75 THEN 1
-        END AS x_12_cgpa_275_325,
-        CASE
-            WHEN AT_Cumulative_GPA <2.75 THEN 1
-        END AS x_12_cgpa_below_275,
-        
-    college_eligibility_gpa_11th_grade AS x_11_cgpa,
-        CASE
-            WHEN college_eligibility_gpa_11th_grade >=3.25 THEN "3.25+"
-            WHEN 
-            (college_eligibility_gpa_11th_grade < 3.25
-            AND college_eligibility_gpa_11th_grade >= 2.75) THEN "2.75-3.25"
-            WHEN college_eligibility_gpa_11th_grade < 2.75 THEN "Below 2.75"
-            ELSE NULL
-        END AS x_11_cgpa_bucket,
-        CASE
-            WHEN college_eligibility_gpa_11th_grade >=3.25 THEN 1
-        END AS x_11_cgpa_325,
-        CASE
-            WHEN college_eligibility_gpa_11th_grade <3.25
-            AND college_eligibility_gpa_11th_grade >=2.75 THEN 1
-        END AS x_11_cgpa_275_325,
-        CASE
-            WHEN college_eligibility_gpa_11th_grade <2.75 THEN 1
-        END AS x_11_cgpa_below_275,
-    readiness_composite_off_c,
-    
-    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE AT_Grade_c = "12th Grade"
-    AND term_c = "Spring"
+    *
+    FROM `data-studio-260217.college_account_summary.gather_hs_contact_data_view`
 ),
 
 join_data AS
