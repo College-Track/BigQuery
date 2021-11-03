649421759478b088a7ff1429ef0b470bb11744ab
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-03 10:26:22 -0700

    Auto-commit: Wed Nov 03 2021 10:26:14 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
@@ -1,10 +1,12 @@
 
+WITH gather_all_enrolled_at_data AS
+(    
     SELECT
-    Contact_Id,
+    COUNT(DISTINCT(Contact_Id)) AS at_unique_student_count,
+    COUNT(AT_Id) AS at_count,
     high_school_graduating_class_c,
     AT_Grade_c,
     GAS_Name,
-    GAS_Start_Date,
     AT_School_Name,
     AT_school_type,
     major_c,
@@ -13,17 +15,22 @@
         ELSE fit_type_at_c
     END AS fit_type_at,
     situational_best_fit_c,
-    credits_attempted_current_term_c,
-    credits_awarded_current_term_c,
-    cumulative_credits_awarded_all_terms_c,
-    credits_accumulated_c,
+    AVG(credits_attempted_current_term_c) AS avg_at_credit_attempt,
+    AVG(credits_awarded_current_term_c) AS avg_at_credit_awarded,
+    AVG(credits_accumulated_c) AS avg_at_credits_accumulated,
+    AVG(cumulative_credits_awarded_all_terms_c) AS avg_c_credits_alltime,
+
     on_track_c,
     
-    AT_Term_GPA,
-    AT_Term_GPA_bucket,
-    AT_Cumulative_GPA,
-    AT_Cumulative_GPA_bucket,
+    AVG(AT_Term_GPA) AS at_term_gpa,
+    AVG(AT_Cumulative_GPA) AS avg_at_cgpa,
     
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE student_audit_status_c IN ("Active: Post-Secondary", "CT Alumni")
+    GROUP BY AT_School_Name, AT_school_type, high_school_graduating_class_c, AT_Grade_c, GAS_Name, major_c, fit_type_at, situational_best_fit_c, on_track_c
+)
+
+    SELECT
+    *
+    FROM gather_all_enrolled_at_data
