07e33c4c234622e95e6ccb01e972e4b2111c50b0
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-03 10:41:08 -0700

    Auto-commit: Wed Nov 03 2021 10:41:00 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
@@ -15,12 +15,17 @@ WITH gather_all_enrolled_at_data AS
         ELSE fit_type_at_c
     END AS fit_type_at,
     situational_best_fit_c,
-    AVG(credits_attempted_current_term_c) AS avg_at_credit_attempt,
-    AVG(credits_awarded_current_term_c) AS avg_at_credit_awarded,
-    AVG(credits_accumulated_c) AS avg_at_credits_accumulated,
-    AVG(cumulative_credits_awarded_all_terms_c) AS avg_c_credits_alltime,
+    SUM(credits_attempted_current_term_c) AS at_credit_attempt_num,
+    SUM(CASE WHEN credits_attempted_current_term_c IS NOT NULL THEN 1 ELSE 0 END) AS at_credit_attempt_denom,
+    SUM(credits_awarded_current_term_c) AS at_credit_awarded_num,
+    SUM(CASE WHEN credits_awarded_current_term_c IS NOT NULL THEN 1 ELSE 0 END) AS at_credit_awarded_denom,
+    AVG(credits_accumulated_c) AS avg_credits_accumulated,
+    SUM(cumulative_credits_awarded_all_terms_c) AS c_credits_alltime_num,
+    SUM(CASE WHEN cumulative_credits_awarded_all_terms_c IS NOT NULL THEN 1 ELSE 0 END) AS c_credits_alltime_denom,
 
-    on_track_c,
+    SUM(CASE WHEN on_track_c = "On-track" THEN 1 ELSE 0 END) AS on_track_count,
+    SUM(CASE WHEN on_track_c = "Near On-track" THEN 1 ELSE 0 END) AS near_on_track_count,
+    SUM(CASE WHEN on_track_c = "Off-track" THEN 1 ELSE 0 END) AS off_track_count,
     
     AVG(AT_Term_GPA) AS at_term_gpa,
     AVG(AT_Cumulative_GPA) AS avg_at_cgpa,
@@ -29,7 +34,7 @@ WITH gather_all_enrolled_at_data AS
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE student_audit_status_c IN ("Active: Post-Secondary", "CT Alumni")
     AND AT_School_Name IS NOT NULL
-    GROUP BY AT_School_Name, AT_school_type, high_school_graduating_class_c, AT_Grade_c, GAS_Name, major_c, fit_type_at, situational_best_fit_c, on_track_c
+    GROUP BY AT_School_Name, AT_school_type, high_school_graduating_class_c, AT_Grade_c, GAS_Name, major_c, fit_type_at, situational_best_fit_c
 )
 
     SELECT
