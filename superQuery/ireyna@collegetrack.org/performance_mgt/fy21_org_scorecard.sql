e7fe025613c85670a55cb3d0047de89ee1509345
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-03 15:18:26 -0700

    Auto-commit: Wed Nov 03 2021 15:18:21 GMT-0700 (Pacific Daylight Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
@@ -5,3 +5,13 @@ SELECT
 
         FROM `org-scorecard-286421.aggregate_data.financial_sustainability_fy20`
         GROUP BY Account,__students,Capacity_Target,__Capacty,Fundraising_Target
+
+    UNION ALL
+
+    SELECT 
+        --* EXCEPT (site_short,Account),
+        --site_short(site_short)  AS Account, --Map abbreviated site names to site_short
+        CASE WHEN site_short = 'NATIONAL' THEN 'National' ELSE site_short END AS Account
+
+        FROM `org-scorecard-286421.aggregate_data.financial_sustainability_fy20`
+        GROUP BY Account,__students,Capacity_Target,__Capacty,Fundraising_Target
