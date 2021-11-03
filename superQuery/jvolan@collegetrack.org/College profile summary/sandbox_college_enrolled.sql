07ab31b0f16f612885af92b920c0c459112bf0dd
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-03 13:50:07 -0700

    Auto-commit: Wed Nov 03 2021 13:49:58 GMT-0700 (Pacific Daylight Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sandbox_college_enrolled.sql
@@ -9,11 +9,13 @@ WITH gather_all_enrolled_at_data AS
     AT_School_Name,
     AT_school_type,
     major_c,
-    CASE
-        when current_as_c = TRUE THEN fit_type_current_c
-        ELSE fit_type_at_c
-    END AS fit_type_at,
-    situational_best_fit_c,
+    CASE WHEN current_as_c = TRUE THEN fit_type_current_c ELSE fit_type_at_c END AS fit_type_at,
+    CASE WHEN situational_best_fit_c = "High Grad Rate Program" THEN 1 ELSE 0 END AS sfit_high_grad_program,
+    CASE WHEN situational_best_fit_c = "High Grad Rate Major" THEN 1 ELSE 0 END AS sfit_high_grad_major,
+    CASE WHEN situational_best_fit_c = "High Grad Rate and Affordable" THEN 1 ELSE 0 END AS sfit_high_grad_afford,
+    CASE WHEN situational_best_fit_c = "DOOR" THEN 1 ELSE 0 END AS sfit_door,
+    CASE WHEN situational_best_fit_c = "Other" THEN 1 ELSE 0 END AS sfit_other,
+
     credits_attempted_current_term_c,
     credits_awarded_current_term_c,
     credits_accumulated_c,
@@ -102,7 +104,11 @@ group_data AS
         when current_as_c = TRUE THEN fit_type_current_c
         ELSE fit_type_at_c
     END AS fit_type_at,
-    situational_best_fit_c,
+    SUM(sfit_high_grad_program) AS sfit_high_grad_program_count,
+    SUM(sfit_high_grad_major) AS sfit_high_grad_major_count,
+    SUM(sfit_high_grad_afford) AS sfit_high_grad_afford_count,
+    SUM(sfit_door) AS sfit_door_count,
+    SUM(sfit_other) AS sfit_other_count,
     SUM(credits_attempted_current_term_c) AS at_credit_attempt_num,
     SUM(CASE WHEN credits_attempted_current_term_c IS NOT NULL THEN 1 ELSE 0 END) AS at_credit_attempt_denom,
     SUM(credits_awarded_current_term_c) AS at_credit_awarded_num,
@@ -116,15 +122,18 @@ group_data AS
     SUM(off_track_count) AS off_track_count,
     SUM(missing_on_track_data_count) AS missing_on_track_data_count,
     
-    AVG(AT_Term_GPA) AS at_term_gpa,
-    AVG(AT_Cumulative_GPA) AS avg_at_cgpa,
+    SUM(AT_Term_GPA) AS at_term_gpa_num,
+    SUM(CASE WHEN AT_Term_GPA IS NOT NULL THEN 1 ELSE 0 END) AS at_term_gpa_denom,
+    SUM(AT_Cumulative_GPA) AS avg_at_cgpa_num,
+    SUM(CASE WHEN AT_Cumulative_GPA IS NOT NULL THEN 1 ELSE 0 END) AS at_cgpa_denom,
+
     
     site_short,
     x_11_cgpa_bucket,
     readiness_composite_off_c
     
     FROM join_data
-    GROUP BY AT_School_Name, AT_school_type, high_school_graduating_class_c, AT_Grade_c, GAS_Name, major_c, fit_type_at, situational_best_fit_c, site_short, x_11_cgpa_bucket,readiness_composite_off_c
+    GROUP BY AT_School_Name, AT_school_type, high_school_graduating_class_c, AT_Grade_c, GAS_Name, major_c, fit_type_at, site_short, x_11_cgpa_bucket,readiness_composite_off_c
 )
     SELECT
     * 
