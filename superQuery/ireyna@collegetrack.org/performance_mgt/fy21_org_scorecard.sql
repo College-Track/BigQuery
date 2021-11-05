88d54f5b82ff33152f40b368444f209fe7631120
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-04 18:30:47 -0700

    updated fy20 org scorecard query

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
@@ -286,21 +286,7 @@ SELECT
             END AS site_sort,
         * EXCEPT (Account),
         
-       mapRegionShort (ACCOUNT) AS Region,
-        CASE
-            WHEN Account = 'East Palo Alto' THEN 'Northern California Region'
-            WHEN Account = 'Oakland' THEN 'Northern California Region'
-            WHEN Account = 'San Francisco' THEN 'Northern California Region'
-            WHEN Account = 'New Orleans' THEN 'New Orleans Region'
-            WHEN Account = 'Aurora' THEN 'Colorado Region'
-            WHEN Account = 'Boyle Heights' THEN 'Los Angeles Region'
-            WHEN Account = 'Sacramento' THEN 'Northern California Region'
-            WHEN Account = 'Watts' THEN 'Los Angeles Region'
-            WHEN Account = 'Denver' THEN 'Colorado Region'
-            WHEN Account = 'The Durant Center' THEN 'DC Region'
-            WHEN Account = 'Ward 8' THEN 'DC Region'
-            WHEN Account = 'Crenshaw' THEN 'Los Angeles Region'
-        END AS siff
+       mapRegionShort (Account) AS Region, --crease Region column based on Account site name
      
             
 FROM join_all
