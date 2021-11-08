f7071627e64b8ea058a738dac3a4854284049842
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-07 17:38:09 -0800

    Auto-commit: Sun Nov 07 2021 17:37:59 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
@@ -290,7 +290,7 @@ SELECT
             WHEN program.Account = 'Ward 8' THEN 11
             WHEN program.Account = 'Crenshaw' THEN 12
             END AS site_sort,
-       program.*, 
+       program.* EXCEPT (account), 
        hr.* EXCEPT (site,region)
 FROM add_region_site AS program
 LEFT JOIN hr_financial_sustainability_hs_capacity AS hr ON program.site=hr.site AND program.region = hr.region
