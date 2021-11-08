c3f7a2fa8a4368be43784edd9555d2c253f2cce5
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-08 15:09:24 -0800

    Auto-commit: Mon Nov 08 2021 15:09:20 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sandbox_alumni.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sandbox_alumni.sql
@@ -2,8 +2,19 @@
     SELECT
     Contact_Id,
     AT_Id,
-    school_c,
+    AT_School_Name,
+    major_c,
     type_of_degree_earned_c,
+    years_to_complete_4_year_degree_c,
+    graduated_4_year_degree_4_years_c,
+    graduated_4_year_degree_5_years_c,
+    graduated_4_year_degree_6_years_c,
+    graduated_4_year_degree_c,
+    AT_Cumulative_GPA,
+    indicator_1_post_secondary_internship_c,
+    ps_internships_c,
+    AY_Name,
+    end_date_c,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE type_of_degree_earned_c = "4-year degree"
