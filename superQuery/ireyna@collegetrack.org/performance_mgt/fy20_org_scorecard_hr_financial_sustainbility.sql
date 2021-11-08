890988e6a55d0a2cb32c12770318a30bee313d29
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-07 17:04:51 -0800

    Auto-commit: Sun Nov 07 2021 17:04:47 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_hr_financial_sustainbility.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_hr_financial_sustainbility.sql
@@ -165,7 +165,7 @@ SELECT
     mapRegion(region) AS Region,
     --Account AS site_or_region_hr_finance,
     --CONCAT(Account,"_hr_finance_capacity") AS Account_hr_finance, --append 'hr_finance"capacity' to each region/site to differntiate outcomes
-    CASE WHEN Account IS NOT NULL THEN 1 ELSE 0 END AS objective_indicator_hr_financial_hs_capacity,   
+    CASE WHEN (Site or region) IS NOT NULL THEN 1 ELSE 0 END AS objective_indicator_hr_financial_hs_capacity,   
       
 FROM join_all
 ) ;
