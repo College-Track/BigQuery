9b46b473c5cb7882a267c28ac2819379fa6692c1
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-07 17:03:29 -0800

    Auto-commit: Sun Nov 07 2021 17:03:25 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_hr_financial_sustainbility.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_hr_financial_sustainbility.sql
@@ -160,9 +160,9 @@ LEFT JOIN hr_identities AS C                ON A.Region = C.Account
 )
 
 SELECT 
-    * EXCEPT (Account, Site),
-    mapSite(Account) AS site,
-    mapRegion(Account) AS Region,
+    * EXCEPT (region, site_short),
+    mapSite(site_short) AS site,
+    mapRegion(region) AS Region,
     --Account AS site_or_region_hr_finance,
     --CONCAT(Account,"_hr_finance_capacity") AS Account_hr_finance, --append 'hr_finance"capacity' to each region/site to differntiate outcomes
     CASE WHEN Account IS NOT NULL THEN 1 ELSE 0 END AS objective_indicator_hr_financial_hs_capacity,   
