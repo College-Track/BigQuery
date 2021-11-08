67f3d74367d7d89e7dc589d89b007825ac9aed41
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-07 16:36:50 -0800

    Auto-commit: Sun Nov 07 2021 16:36:40 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard.sql
@@ -157,9 +157,11 @@ LEFT JOIN hr_identities AS C                ON A.Account = C.Account
 )
 
 SELECT 
-    --CONCAT(Account,"_hr_finance_capacity") AS Account, --append 'hr_finance"capacity' to each region/site to differntiate outcomes
-    CASE WHEN Account IS NOT NULL THEN 1 ELSE 0 END AS hr_financial_hs_capacity,
-       * --EXCEPT (Account),
+    *,
+    --Account AS site_or_region_hr_finance,
+    CONCAT(Account,"_hr_finance_capacity") AS Account_hr_finance, --append 'hr_finance"capacity' to each region/site to differntiate outcomes
+    CASE WHEN Account IS NOT NULL THEN 1 ELSE 0 END AS objective_indicator_hr_financial_hs_capacity,   
+      
 FROM join_all
 )
 ;
