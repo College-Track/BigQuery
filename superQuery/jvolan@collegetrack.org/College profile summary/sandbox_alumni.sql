af8244e71e9e452c3dbe2c7327d459af3bccdd16
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-08 14:10:25 -0800

    Auto-commit: Mon Nov 08 2021 14:10:21 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/sandbox_alumni.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/sandbox_alumni.sql
@@ -1,6 +1,9 @@
 
     SELECT
-    *
+    Contact_Id,
+    AT_Id,
+    school_c,
+    type_of_degree_earned_c,
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE type_of_degree_earned_c = "4-year degree"
