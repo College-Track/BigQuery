ea609ae14da7e3d4e9ce9a1c525847c1b6fbf020
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-09 08:57:48 -0800

    Auto-commit: Tue Nov 09 2021 08:57:44 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests,tests IR/transpose_org_scorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests,tests IR/transpose_org_scorecard_fy20.sql
@@ -1,129 +1,4 @@
---Using FY21_eoy_combined_metrics table to consolidate org scorecard data
-/* Added Column: fiscal_year
- ALTER TABLE `data-studio-260217.performance_mgt.org_scorecard_fy20`
-  ADD COLUMN fiscal_year STRING;
-*/
 
-/*Populated new fiscal_year column with 'FY20'
-  UPDATE
-  `data-studio-260217.performance_mgt.org_scorecard_fy20` 
-SET
-  fiscal_year = "FY20"
-WHERE 
-    fiscal_year IS NULL
- */
-  
-
-CREATE TEMPORARY FUNCTION mapSite (Account STRING) AS ( --Remap abbreviated Account names to site_short
-   CASE 
-            WHEN Account = 'College Track East Palo Alto' THEN 'East Palo Alto'
-            WHEN Account = 'College Track Oakland' THEN 'Oakland'
-            WHEN Account = 'College Track San Francisco' THEN 'San Francisco'
-            WHEN Account = 'College Track New Orleans' THEN 'New Orleans'
-            WHEN Account = 'College Track Aurora' THEN 'Aurora'
-            WHEN Account = 'College Track Boyle Heights' THEN 'Boyle Heights'
-            WHEN Account = 'College Track Sacramento' THEN 'Sacramento'
-            WHEN Account = 'College Track Watts' THEN 'Watts'
-            WHEN Account = 'College Track Denver' THEN 'Denver'
-            WHEN Account = 'College Track at The Durant Center' THEN 'The Durant Center'
-            WHEN Account = 'College Track Ward 8' THEN 'Ward 8'
-            WHEN Account = 'College Track Crenshaw' THEN 'Crenshaw'
-            WHEN Account = 'EPA' THEN 'East Palo Alto'
-            WHEN Account = 'OAK' THEN 'Oakland'
-            WHEN Account = 'SF' THEN 'San Francisco'
-            WHEN Account = 'NOLA' THEN 'New Orleans'
-            WHEN Account = 'AUR' THEN 'Aurora'
-            WHEN Account = 'BH' THEN 'Boyle Heights'
-            WHEN Account = 'SAC' THEN 'Sacramento'
-            WHEN Account = 'WATTS' THEN 'Watts'
-            WHEN Account = 'DEN' THEN 'Denver'
-            WHEN Account = 'PGC' THEN 'The Durant Center'
-            WHEN Account = 'DC8' THEN 'Ward 8'
-            WHEN Account = 'CREN' THEN 'Crenshaw'
-            WHEN Account = 'NATIONAL' THEN 'National'
-            WHEN Account = 'National' THEN 'National'
-            WHEN Account = 'NATIONAL (AS LOCATION)'THEN 'National (As Location)'
-       END)
-       ;
-CREATE TEMPORARY FUNCTION mapRegion(Account STRING) AS ( --map Region based on Site, remap region abbreviations to region_short
-    CASE 
-            WHEN Account = 'College Track Northern California Region' THEN 'Northern California Region'
-            WHEN Account = 'College Track New Orleans Region' THEN 'New Orleans Region'
-            WHEN Account = 'College Track Colorado Region' THEN 'Colorado Region'
-            WHEN Account = 'College Track Los Angeles Region' THEN 'Los Angeles Region'
-            WHEN Account = 'College Track DC Region' THEN 'DC Region'
-            WHEN Account = 'EPA' THEN 'Northern California Region'
-            WHEN Account = 'OAK' THEN 'Northern California Region'
-            WHEN Account = 'SF' THEN 'Northern California Region'
-            WHEN Account = 'NOLA' THEN 'New Orleans Region'
-            WHEN Account = 'AUR' THEN 'Colorado Region'
-            WHEN Account = 'BH' THEN 'Los Angeles Region'
-            WHEN Account = 'SAC' THEN 'Northern California Region'
-            WHEN Account = 'WATTS' THEN 'Los Angeles Region'
-            WHEN Account = 'DEN' THEN 'Colorado Region'
-            WHEN Account = 'PGC' THEN 'DC Region'
-            WHEN Account = 'DC8' THEN 'DC Region'
-            WHEN Account = 'CREN' THEN 'Los Angeles Region'
-            WHEN Account = 'NATIONAL' THEN 'National'
-            WHEN Account = 'National' THEN 'National'
-            WHEN Account = 'NATIONAL (AS LOCATION)' THEN 'National (As Location)'
-            WHEN Account = 'NORCAL' THEN 'Northern California Region'
-            WHEN Account = 'LA' THEN 'Los Angeles Region'
-            WHEN Account = 'CO' THEN 'Colorado Region'
-            WHEN Account = 'NOLA' THEN 'New Orleans Region'
-            WHEN Account = 'DC' THEN 'DC Region'
-            WHEN Account = 'College Track East Palo Alto' THEN 'Northern California Region'
-            WHEN Account = 'College Track Oakland' THEN 'Northern California Region'
-            WHEN Account = 'College Track San Francisco' THEN 'Northern California Region'
-            WHEN Account = 'College Track New Orleans' THEN 'New Orleans Region'
-            WHEN Account = 'College Track Aurora' THEN 'Colorado Region'
-            WHEN Account = 'College Track Boyle Heights' THEN 'Los Angeles Region'
-            WHEN Account = 'College Track Sacramento' THEN 'Northern California Region'
-            WHEN Account = 'College Track Watts' THEN 'Los Angeles Region'
-            WHEN Account = 'College Track Denver' THEN 'Colorado Region'
-            WHEN Account = 'College Track at The Durant Center' THEN 'DC Region'
-            WHEN Account = 'College Track Ward 8' THEN 'DC Region'
-            WHEN Account = 'College Track Crenshaw' THEN 'Los Angeles Region'
-        END)
-        ;
-CREATE TEMPORARY FUNCTION mapRegionShort (Account STRING) AS (
-        CASE
-            WHEN Account = 'East Palo Alto' THEN 'Northern California Region'
-            WHEN Account = 'Oakland' THEN 'Northern California Region'
-            WHEN Account = 'San Francisco' THEN 'Northern California Region'
-            WHEN Account = 'New Orleans' THEN 'New Orleans Region'
-            WHEN Account = 'Aurora' THEN 'Colorado Region'
-            WHEN Account = 'Boyle Heights' THEN 'Los Angeles Region'
-            WHEN Account = 'Sacramento' THEN 'Northern California Region'
-            WHEN Account = 'Watts' THEN 'Los Angeles Region'
-            WHEN Account = 'Denver' THEN 'Colorado Region'
-            WHEN Account = 'The Durant Center' THEN 'DC Region'
-            WHEN Account = 'Ward 8' THEN 'DC Region'
-            WHEN Account = 'Crenshaw' THEN 'Los Angeles Region'
-        END)
-        ;
-CREATE TEMP TABLE hr_financial_sustainability_hs_capacity AS
-(
-WITH
-financial_sustainability AS (
-     SELECT 
-            * EXCEPT (site_short, Account),
-            mapSite(Account) AS Account, --site_abbrev to site_short 
-      
-        FROM `org-scorecard-286421.aggregate_data.financial_sustainability_fy20`
-        WHERE Account LIKE '%College Track%' -- only looking at values that are site_long
-
-        UNION DISTINCT
-
-        SELECT 
-            * EXCEPT (Account,site_short),
-            mapRegion(Account) AS Account, --region abrev to region_short
-        FROM `org-scorecard-286421.aggregate_data.financial_sustainability_fy20`
-        WHERE Account NOT LIKE '%College Track%' --only looking at values that are region_abrev
-)
-SELECT *
-FROM financial_sustainability);
---ALTER TABLE hr_financial_sustainability_hs_capacity ADD COLUMN Measure STRING;
 
 WITH
 unpivot AS (
@@ -147,3 +22,119 @@ unpivot AS (
         (Max(NUMERATOR) FOR Account
        IN ('DC Region','Colorado Region','Los Angeles Region','New Orleans Region','Northern California Region'))
      
+    /*UNPIVOT INCLUDE NULLS  
+        (Outcome FOR Measure IN (hs_capacity_outcome,fundraising_target_outcome) --Create a "Measure" column 
+        ) AS UNPVt,*/
+    /*PIVOT (Max(numerator) FOR Account
+       IN (
+       
+       "DC Region",'Colorado Region','Los Angeles Region','New Orleans Region','Northern California Region',
+        'East Palo Alto',
+        'Oakland',
+        'San Francisco',
+        'New Orleans',
+        'Aurora',
+        'Boyle Heights',
+        'Sacramento',
+        'Watts',
+        'Denver',
+        'The Durant Center',
+        'Ward 8',
+        'National')
+        ) AS pivot
+        )
+ 
+      
+
+ 
+/*
+--This works for 2 transformations
+SELECT * 
+FROM 
+(SELECT  __Capacty, Fundraising_Target
+FROM hr_financial_sustainability_hs_capacity )
+    UNPIVOT INCLUDE NULLS  (
+    Outcome FOR Measure in (__Capacty,Fundraising_Target) 
+) AS UNPVT
+
+SELECT `Account`,
+  SPLIT(kv, ':')[OFFSET(0)] Measure,
+  SPLIT(kv, ':')[OFFSET(1)] Outcome,
+  SPLIT(kv, ':')[SAFE_OFFSET(2)] Values
+FROM hr_financial_sustainability_hs_capacity t,
+UNNEST(SPLIT(REGEXP_REPLACE(TO_JSON_STRING(t), r'[{}"]', ''))) kv
+WHERE SPLIT(kv, ':')[OFFSET(0)] != 'Account'
+AND SPLIT(kv, ':')[OFFSET(0)] NOT IN ('__students','Capacity_Target')
+AND SPLIT(kv, ':')[OFFSET(2)] IN ('__students','Capacity_Target')
+
+
+SELECT * 
+FROM
+    (SELECT Fundraising_Target,__Capacty FROM hr_financial_sustainability_hs_capacity)
+UNPIVOT INCLUDE NULLS 
+        (Fundraising_Target FOR Measure
+        IN (Outcome)
+        
+SELECT Account, __Capacty, Fundraising_Target
+FROM hr_financial_sustainability_hs_capacity 
+UNPIVOT INCLUDE NULLS  (Outcome FOR Accounts in (  __Capacty, Fundraising_Target) ) AS UNPVT
+
+*/    
+     
+     /* 
+  multi_column_unpivot:
+    values_column_set
+    FOR name_new_column
+    IN (new_column_Names_sets_to_unpivot)
+
+*/
+/*
+hr_tenure AS ( 
+    SELECT 
+        * EXCEPT (site,region), 
+        mapSite(site) as Account
+        FROM`org-scorecard-286421.aggregate_data.HR_outcomes_tenure_engagement`
+        WHERE site IS NOT NULL
+
+    UNION DISTINCT
+
+    SELECT 
+        * EXCEPT (site,region), 
+        mapRegion(region) as Account
+        FROM`org-scorecard-286421.aggregate_data.HR_outcomes_tenure_engagement`
+        where region IS NOT NULL
+),
+
+hr_identities AS (
+    SELECT 
+        * EXCEPT (Account,string_field_5),
+        mapRegion(Account)  AS Account --mapping site names and region abbreviations to region_short
+        FROM`org-scorecard-286421.aggregate_data.HR_outcomes_identity`
+        WHERE Account IS NOT NULL
+),
+
+join_all AS (
+SELECT 
+    DISTINCT
+    A.*,
+    B.* EXCEPT (Account),
+    C.* EXCEPT (Account)
+FROM hr_tenure AS A                    
+LEFT JOIN financial_sustainability AS B     ON A.Account = B.Account 
+LEFT JOIN hr_identities AS C                ON A.Account = C.Account    
+ 
+)
+
+SELECT 
+    *,
+    CASE 
+        WHEN Account LIKE '%Region%' THEN Account
+        ELSE NULL 
+        END AS Region,
+    CASE 
+        WHEN Account NOT LIKE '%Region%' THEN Account
+        ELSE NULL 
+        END AS Site,
+ 
+FROM join_all
+);
