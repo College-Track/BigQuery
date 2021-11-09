afe90f003345db7b6af30e94c14833eda34bec5a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-09 11:01:56 -0800

    Auto-commit: Tue Nov 09 2021 11:01:53 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_transpose_recruit_and_retain_students.sql
@@ -0,0 +1,37 @@
+WITH
+
+objective_1_site AS (
+    SELECT 
+        * EXCEPT (Site__Account_Name,Region__Account_Name),
+         CASE WHEN Region__Account_Name = 'NATIONAL' THEN 'National' ELSE mapRegion(Region__Account_Name) END AS Account
+        
+        FROM `org-scorecard-286421.aggregate_data.objective_1_site`
+    
+        UNION DISTINCT
+
+     SELECT 
+        * EXCEPT (Site__Account_Name,Region__Account_Name),
+        CASE WHEN Site__Account_Name = 'NATIONAL' THEN 'National' ELSE mapSite(Site__Account_Name) END AS Account
+
+        FROM `org-scorecard-286421.aggregate_data.objective_1_site`  
+),
+
+objective_1_region AS (
+    SELECT 
+        * EXCEPT (site),
+        --mapRegion(site)  AS Account2
+        Site AS Account2
+
+    FROM(
+        SELECT * EXCEPT (Region), Region AS site
+        FROM `org-scorecard-286421.aggregate_data.objective_1_region`
+        )
+)--,
+--join AS (
+SELECT 
+    DISTINCT
+    A.* EXCEPT (National),
+    B.* EXCEPT (Account)
+FROM objective_1_site AS A
+LEFT JOIN objective_1_region AS B  ON A.Account = B.Account2
+)
