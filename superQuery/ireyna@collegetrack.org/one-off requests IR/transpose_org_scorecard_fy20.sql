241610f814889ffa9adb99e157f794ce31c21e7a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-08 17:02:59 -0800

    Auto-commit: Mon Nov 08 2021 17:02:54 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
@@ -134,15 +134,16 @@ financial_sustainability AS (
 )
 SELECT *
 FROM financial_sustainability);
-ALTER TABLE hr_financial_sustainability_hs_capacity ADD COLUMN Measure STRING;
+--ALTER TABLE hr_financial_sustainability_hs_capacity ADD COLUMN Measure STRING;
 
 SELECT * 
 FROM
     (SELECT Account,capacity_target FROM hr_financial_sustainability_hs_capacity)
-UNPIVOT( (Account,capacity_target) FOR capacity_target
+UNPIVOT( (Account,capacity_target) FOR new_name
         IN (Account,capacity_target)
     ) AS test
-
+    
+  
 
 /*
 hr_tenure AS ( 
