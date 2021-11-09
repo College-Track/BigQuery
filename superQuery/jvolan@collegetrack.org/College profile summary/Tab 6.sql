3d35ca5cf27225bd9e8c7263a78458edc857a2e8
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-11-09 11:43:57 -0800

    Auto-commit: Tue Nov 09 2021 11:43:51 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/College profile summary/Tab 6.sql
+++ superQuery/jvolan@collegetrack.org/College profile summary/Tab 6.sql
@@ -1,3 +1,4 @@
+/*
 SELECT
     c.Contact_Id,
     high_school_graduating_class_c,
@@ -29,3 +30,29 @@ SELECT
     WHERE site_short IN ('Ward 8', 'The Durant Center')
     AND AY_name IN ('AY 2019-20','AY 2020-21')
     AND AY_student_served = "High School"
+    */
+    
+    SELECT
+    Contact_Id,
+    high_school_graduating_class_c,
+    site_short,
+    College_Track_Status_Name,
+    CASE
+        WHEN (enrolled_sessions_c IS NULL
+        OR enrolled_sessions_c = 0) THEN NULL
+        WHEN (attended_workshops_c / enrolled_sessions_c) < .25 THEN "<25%"
+        WHEN (attended_workshops_c / enrolled_sessions_c) >= .25
+        AND (attended_workshops_c / enrolled_sessions_c) < .45 THEN "25% - 44.5%"
+        WHEN (attended_workshops_c / enrolled_sessions_c) >= .45
+        AND (attended_workshops_c / enrolled_sessions_c) < .65 THEN "45% - 64.5%"
+        WHEN (attended_workshops_c / enrolled_sessions_c) >= .65
+        AND (attended_workshops_c / enrolled_sessions_c) < .8 THEN "65% - 79%"
+        WHEN (attended_workshops_c / enrolled_sessions_c) >= .8
+        AND (attended_workshops_c / enrolled_sessions_c) < .9 THEN "80% - 89%"
+        WHEN (attended_workshops_c / enrolled_sessions_c) >= .9 THEN "90%+"
+    END AS attendance_bucket
+    
+    FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
+    WHERE current_as_c = TRUE
+    AND site_short IN ('Ward 8', 'The Durant Center')
+
