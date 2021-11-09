a78bd5dd3505b762c17430a2b00ba428dada7d66
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-09 11:05:23 -0800

    Auto-commit: Tue Nov 09 2021 11:05:19 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_transpose_recruit_and_retain_students.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_transpose_recruit_and_retain_students.sql
@@ -95,9 +95,9 @@ CREATE TEMPORARY FUNCTION mapRegionShort (Account STRING) AS (
         ;
 /*CREATE TEMP TABLE recruit_and_retain AS
 (*/
-WITH
+--WITH
 
-objective_1_site AS (
+--objective_1_site AS (
     SELECT 
         * EXCEPT (Site__Account_Name,Region__Account_Name),
          CASE WHEN Region__Account_Name = 'NATIONAL' THEN 'National' ELSE mapRegion(Region__Account_Name) END AS Account
@@ -111,23 +111,3 @@ objective_1_site AS (
         CASE WHEN Site__Account_Name = 'NATIONAL' THEN 'National' ELSE mapSite(Site__Account_Name) END AS Account
 
         FROM `org-scorecard-286421.aggregate_data.objective_1_site`  
-),
-
-objective_1_region AS (
-    SELECT 
-        * EXCEPT (site),
-        --mapRegion(site)  AS Account2
-        Site AS Account2
-
-    FROM(
-        SELECT * EXCEPT (Region), Region AS site
-        FROM `org-scorecard-286421.aggregate_data.objective_1_region`
-        )
-)--,
---join AS (
-SELECT 
-    DISTINCT
-    A.* ,
-    B.* --EXCEPT (Account)
-FROM objective_1_site AS A
-LEFT JOIN objective_1_region AS B  ON A.Account = B.Account2
