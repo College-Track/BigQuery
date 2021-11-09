5eaf9cc5b29a56a7989fa712e05fa90ece0e5435
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-08 19:28:45 -0800

    Auto-commit: Mon Nov 08 2021 19:28:43 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
@@ -146,8 +146,8 @@ unpivot AS (
     /*UNPIVOT INCLUDE NULLS  
         (Outcome FOR Measure IN (hs_capacity_outcome,fundraising_target_outcome) --Create a "Measure" column 
         ) AS UNPVt,*/
-    PIVOT (Max(Capacity_Target) FOR Account
-       IN ('DC Region','Colorado Region','Los Angeles Region','New Orleans Region','Northern California Region')
+    PIVOT (Max(numerator) FOR Account
+       IN (['DC Region]','Colorado Region','Los Angeles Region','New Orleans Region','Northern California Region')
         )
         )
     SELECT * FROM unpivot
