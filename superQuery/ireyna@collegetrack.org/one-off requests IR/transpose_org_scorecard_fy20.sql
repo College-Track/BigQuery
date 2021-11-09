a7f521a9aefe31ddc3c93e3d12eb4a56b0354e53
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-08 17:43:38 -0800

    Auto-commit: Mon Nov 08 2021 17:43:32 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
@@ -131,7 +131,7 @@ SELECT `Account`,
 FROM hr_financial_sustainability_hs_capacity t,
 UNNEST(SPLIT(REGEXP_REPLACE(TO_JSON_STRING(t), r'[{}"]', ''))) kv
 WHERE SPLIT(kv, ':')[OFFSET(0)] != 'Account'
-
+AND SPLIT(kv, ':')[OFFSET(0)] NOT IN ('__students','Capacity_Target')
 
 
 
