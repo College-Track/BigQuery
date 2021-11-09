95aa7cb8f9b1643f51f21459e9aa7587739b787d
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-08 20:05:30 -0800

    Auto-commit: Mon Nov 08 2021 20:05:27 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests,tests IR/org_scorecard_fy20_hsCapacity_financ.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests,tests IR/org_scorecard_fy20_hsCapacity_financ.sql
@@ -1,5 +1,41 @@
 --tranforming table orientation of financial susainability table
 
+    SELECT
+        CASE 
+        WHEN sum_male IS NOT NULL THEN sum_male
+        WHEN sum_male IS NOT NULL THEN sum_male
+        WHEN sum_male IS NOT NULL THEN sum_male
+        WHEN sum_male IS NOT NULL THEN sum_male
+        WHEN sum_male IS NOT NULL THEN sum_male
+        WHEN 
+sum_low_income_first_gen
+denom_hs_admits
+percent_male_fy20
+percent_low_income_first_gen_fy20
+sum_active_hs
+denom_annual_retention
+percent_active_FY20
+Meaningful_Summer_Experiences
+CoVi_growth
+GPA___Composite
+Matriculate_to_Best__Good__or_Situational
+on_track
+_6_yr_grad_rate
+full_time_employment_or_grad_school_6_months
+gainful_employment_standard
+meaningful_employment
+__students
+Capacity_Target
+__Capacty
+Fundraising_Target
+ENGAGEMENT_SCORE
+TENURE
+Non_white
+LGBTQ
+Male
+First_Gen
+    FROM  `data-studio-260217.performance_mgt.org_scorecard_fy20`
+/*
 WITH
 unpivot AS (
     SELECT 
@@ -34,6 +70,6 @@ unpivot AS (
         * 
     FROM unpivot
     UNPIVOT 
-        (Outcome FOR Measure IN (hs_capacity_outcome,fundraising_target_outcome) --Create a "Measure" column 
+        (Outcome FOR Measure IN (fundraising_target_outcome), IN (hs_capacity_outcome)) --Create a "Measure" column 
         ) AS UNPVt
-   
+   */
