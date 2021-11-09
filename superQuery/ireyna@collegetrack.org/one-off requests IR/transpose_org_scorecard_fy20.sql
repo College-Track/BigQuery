a61a8f91c88e0e896125d1cd804651145b487b65
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-08 18:24:29 -0800

    Auto-commit: Mon Nov 08 2021 18:24:23 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests IR/transpose_org_scorecard_fy20.sql
@@ -128,7 +128,7 @@ FROM financial_sustainability);
 SELECT Account, __Capacty, Fundraising_Target,	__students,	Capacity_Target
 FROM hr_financial_sustainability_hs_capacity 
     UNPIVOT INCLUDE NULLS  (
-    (__Capacty,Fundraising_Target) FOR Accounts in (__students,	Capacity_Target) 
+    (__Capacty,Fundraising_Target) FOR Accounts in (__students,	Capacity_Target,__Capacty,Fundraising_Target) 
 ) AS UNPVT
 
 
