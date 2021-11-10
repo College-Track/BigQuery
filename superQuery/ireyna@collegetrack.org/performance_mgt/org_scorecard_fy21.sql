abfe1f339f2e836550ea6de88d6f3aab2e0e0a07
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-10 15:25:20 -0800

    Auto-commit: Wed Nov 10 2021 15:25:11 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,4 +1,76 @@
 #Using data-studio-260217.performance_mgt.fy21_eoy_combined_metrics table
+create temp table social_emotional_growth AS (
+WITH
+    gather_covi_data AS (
+    SELECT
+        contact_name_c,
+        site_short,
+        region_short,
+        AY_Name,
+        MIN(belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c) AS covi_raw_score
+    FROM `data-warehouse-289815.salesforce_clean.test_clean` T
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
+        ON CAT.AT_Id = T.academic_semester_c
+    WHERE
+        T.record_type_id = '0121M000001cmuDQAQ'
+        AND AY_Name IN ('AY 2019-20', 'AY 2020-21')
+        AND AY_2020_21_student_served_c = 'High School Student'
+    GROUP BY
+        site_short,
+        region_short,
+        contact_name_c,
+        AY_Name
+    ORDER BY
+        site_short,
+        region_short,
+        contact_name_c,
+        AY_Name
+),
+
+-- CONT'D: Org Scoreard COVI % of students growing toward average or above social-emotional strengths
+calc_covi_growth AS (
+    SELECT
+        site_short,
+        region_short,
+        contact_name_c,
+        covi_raw_score - LAG(covi_raw_score) OVER (PARTITION BY contact_name_c ORDER BY AY_Name) AS covi_growth --Returns the value of the value_expression on a preceding row.
+    FROM gather_covi_data
+
+),
+
+-- CONT'D: Org Scoreard COVI % of students growing toward average or above social-emotional strengths
+determine_covi_indicators AS (
+    SELECT
+        site_short,
+        region_short,
+        contact_name_c,
+        SUM(CASE
+            WHEN covi_growth > 0
+            THEN 1
+            ELSE 0
+        END) AS covi_student_grew
+
+    FROM calc_covi_growth
+    WHERE
+            covi_growth IS NOT NULL
+
+    GROUP BY
+        site_short,
+        region_short,
+    contact_name_c
+)
+
+        SELECT
+            site_short,
+            region_short,
+            SUM(covi_student_grew) AS social_emotional_growth_numerator,
+            COUNT(contact_name_c) AS social_emotional_growth_denominator
+        FROM
+            determine_covi_indicators
+        GROUP BY
+            site_short,
+            region_short
+           ); 
 
 --site breakdown
     SELECT 
@@ -6,7 +78,7 @@
         male_numerator/ninth_grade_denominator AS percent_male_fy21,
         low_income_first_gen_numerator/ninth_grade_denominator AS percent_low_income_first_gen_fy21,
         annual_retention_numerator/annual_retention_denominator AS percent_annual_retention_fy21,
-        social_emotional_growth_numerator/high_school_student_count AS percent_social_emotional_growth_fy21,
+        a.social_emotional_growth_numerator/b.social_emotional_growth_denominator AS percent_social_emotional_growth_fy21,
         above_325_gpa_seniors_numerator/senior_325_gpa_only_denominator AS percent_seniors_above_325_fy21,
         senior_325_gpa_and_test_ready_numerator/senior_325_gpa_and_test_ready_denominator AS percent_seniors_above_325_and_test_ready_fy21,
         matriculated_best_good_situational_numerator/matriculation_senior_denominator AS percent_matriculate_best_good_situational_fy21,
@@ -61,7 +133,61 @@
         CASE WHEN had_mse_denominator = 0 THEN NULL ELSE had_mse_denominator END AS mse_denominator
     
         FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  
-        )
+        ) AS A
+    LEFT JOIN social_emotional_growth AS B ON a.site_short = b.site_short
+
+
+
+    ;
+--region breakdown
+SELECT
+    
+        region_short,
         
+        SUM(matriculation_numerator) AS num_matriculation,
+        SUM(matriculated_affordable) AS num_matriculation_affordable,
+        SUM(matriculated_best_good_situational) AS num_matriculated_best_good_situational,
+        SUM(college_first_enrolled_school_type_numerator) AS matriculation_4yr,
+        SUM(twelfth_grade_count) AS senior_denominator,
     
+    --3.0 & 3.25 GPA data
+        SUM(above_3_gpa)AS students_above_3_gpa_numerator,
+        SUM(above_325_gpa_seniors) AS above_325_gpa_seniors_numerator,
+        SUM(above_325_gpa_and_test_ready_seniors) AS above_325_gpa_and_test_ready_seniors_numerator,
+        SUM(high_school_student_count) AS hs_students_denominator,
+        SUM(non_opt_out_seniors) AS senior_readiness_gpa_opt_ins_denominator,
+        
+    --persistence data
+        SUM(indicator_persisted_into_2_nd_year_ct_numerator) AS num_persistence,
+        SUM(persistence_denominator) AS persistence_denominator,
     
+    --on-track data
+        SUM(on_track_numerator) AS on_track_numerator,
+        SUM(on_track_denominator) AS on_track_denominator,
+    
+    --college students graduating with 1+ internship
+        SUM(had_1_plus_internship_numerator) AS had_1_plus_internship_numerator,
+        SUM(had_1_plus_internship_denominator) AS had_1_plus_internship_denominator,
+        
+    --6 year graduation rate
+        SUM(grade_rate_6_years_current_class_numerator) AS num_6_yr_grad_rate,
+        SUM(grade_rate_6_years_current_class_denom) AS grade_rate_6_years_current_class_denom,
+    
+    --alumni
+        SUM(alumni_count) AS alumni_count,
+    
+    --MEMO data
+        SUM(had_mse_numerator) AS mse_numerator,
+        SUM(had_mse_denominator) AS mse_denominator,
+        
+        SUM(above_80_attendance_memo) AS above_80_attendance_memo_orig,
+        
+        SUM(CASE WHEN above_80_attendance = 0 AND above_80_attendance_memo = 1
+            THEN above_80_attendance_memo  ELSE above_80_attendance
+            END) AS attendance_numerator_memo,
+        SUM(high_school_student_count) AS high_school_student_count,
+
+    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`
+    GROUP BY
+        region_short
+;
