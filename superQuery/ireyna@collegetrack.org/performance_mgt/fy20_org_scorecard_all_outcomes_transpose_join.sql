20b118f842bd5f86e6eed089dc8c9d7f2de759a5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-09 18:14:44 -0800

    Auto-commit: Tue Nov 09 2021 18:14:36 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy20_org_scorecard_all_outcomes_transpose_join.sql
@@ -0,0 +1,32 @@
+SELECT *
+FROM `org-scorecard-286421.transposed_tables.admit_demographics_annual_retention_transposed`
+
+UNION DISTINCT 
+
+SELECT *
+FROM `org-scorecard-286421.transposed_tables.mse_covi_gpa_composite`
+
+UNION DISTINCT
+
+SELECT *
+FROM `org-scorecard-286421.transposed_tables.college_graduates_fy20_transpose`
+
+UNION DISTINCT 
+
+SELECT *
+FROM `org-scorecard-286421.transposed_tables.college_outcomes_transpose`
+
+UNION DISTINCT 
+
+SELECT *
+FROM `org-scorecard-286421.transposed_tables.financial_sustainability_hs_capacity_transposed`
+
+UNION DISTINCT 
+
+SELECT *
+FROM  `org-scorecard-286421.transposed_tables.hr_tenure_engagement_fy20_transpose`
+
+UNION DISTINCT
+
+SELECT *
+FROM `org-scorecard-286421.transposed_tables.hr_diverse_identities_fy20_transpose`
