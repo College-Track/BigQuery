7f6992dc807ae67ad424546cd0b784cac793b834
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-10 15:37:51 -0800

    updated query to pull newly developed social emotional growth table

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,73 +1,4 @@
 #Using data-studio-260217.performance_mgt.fy21_eoy_combined_metrics table
-CREATE TEMP TABLE social_emotional_growth AS ( --social emotional growth denominator 
-WITH
-    gather_covi_data AS (
-    SELECT
-        contact_name_c,
-        site_short,
-        region_short,
-        AY_Name,
-        MIN(belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c) AS covi_raw_score
-    FROM `data-warehouse-289815.salesforce_clean.test_clean` T
-    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT
-        ON CAT.AT_Id = T.academic_semester_c
-    WHERE
-        T.record_type_id = '0121M000001cmuDQAQ'
-        AND AY_Name IN ('AY 2019-20', 'AY 2020-21')
-        AND AY_2020_21_student_served_c = 'High School Student'
-    GROUP BY
-        site_short,
-        region_short,
-        contact_name_c,
-        AY_Name
-    ORDER BY
-        site_short,
-        region_short,
-        contact_name_c,
-        AY_Name
-),
-
--- CONT'D: Org Scoreard COVI % of students growing toward average or above social-emotional strengths
-calc_covi_growth AS (
-    SELECT
-        site_short,
-        region_short,
-        contact_name_c,
-        covi_raw_score - LAG(covi_raw_score) OVER (PARTITION BY contact_name_c ORDER BY AY_Name) AS covi_growth --Returns the value of the value_expression on a preceding row.
-    FROM gather_covi_data
-
-),
-
--- CONT'D: Org Scoreard COVI % of students growing toward average or above social-emotional strengths
-determine_covi_indicators AS (
-    SELECT
-        site_short,
-        region_short,
-        contact_name_c,
-        SUM(CASE
-            WHEN covi_growth > 0
-            THEN 1
-            ELSE 0
-        END) AS covi_student_grew
-
-    FROM calc_covi_growth
-    WHERE
-            covi_growth IS NOT NULL
-
-    GROUP BY
-        site_short,
-        region_short,
-        contact_name_c
-)
-
-SELECT
-    site_short,
-    region_short,
-    SUM(covi_student_grew) AS social_emotional_growth_numerator,
-    COUNT(contact_name_c) AS social_emotional_growth_denominator
-FROM determine_covi_indicators
-GROUP BY site_short, region_short
-           );  --pull social emotional growth denominator
 
 /*CREATE OR REPLACE TABLE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
 OPTIONS
@@ -137,4 +68,4 @@ OPTIONS
     
         FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  
         ) AS A
-    LEFT JOIN social_emotional_growth AS B ON a.site_short = b.site_short --to pull in social emotional growth denominator
+    LEFT JOIN `org-scorecard-286421.aggregate_data.social_emotional_growth_fy21` AS B ON a.site_short = b.site_short --to pull in social emotional growth denominator
