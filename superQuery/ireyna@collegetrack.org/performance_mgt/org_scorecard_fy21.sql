44cf8a01e4f16961f04dbf143b4c49f2a70be90f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-10 15:06:07 -0800

    Auto-commit: Wed Nov 10 2021 15:06:02 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,59 +1,67 @@
 #Using data-studio-260217.performance_mgt.fy21_eoy_combined_metrics table
 
 --site breakdown
-    SELECT
+    SELECT 
+        *,
+        male_numerator/ninth_grade_denominator AS percent_male_fy21,
+        low_income_first_gen_numerator/ninth_grade_denominator AS percent_low_income_first_gen_fy21,
+        annual_retention_numerator/annual_retention_denominator AS percent_annual_retention_fy21,
+        social_emotional_growth_numerator/high_school_student_count AS percent_social_emotional_growth_fy21,
+        above_325_gpa_seniors_numerator/senior_325_gpa_only_denominator AS percent_seniors_above_325_fy21,
+        senior_325_gpa_and_test_ready_numerator/senior_325_gpa_and_test_ready_denominator AS percent_seniors_above_325_and_test_ready_fy21,
+        matriculated_best_good_situational_numerator/matriculation_senior_denominator AS percent_matriculate_best_good_situational_fy21,
+        on_track_numerator/on_track_denominator AS percent_on_track_fy21,
+        six_yr_grad_rate_numerator/grade_rate_6_years_current_class_denom AS percent_6_year_grad_rate_fy21,
+        mse_numerator/mse_denominator AS percent_mse_fy21
+        
+    FROM 
+        (SELECT --gather org scorecard metrics to calculate outcomes in main SELECT statement, account for ZEROs
         site_sort,
         site_short,
         region_short,
         
     --admits: male, first-gen & low-income
-        male AS male_numerator,
-        low_income_and_first_gen AS low_income_first_gen_numerator,
-        ninth_grade_count AS ninth_grade_denominator,
-        --male/ninth_grade_count AS percent_male_admits_fy21,
-        --low_income_and_first_gen/ninth_grade_count AS percent_low_income_first_gen_fy21,
+        CASE WHEN male = 0 THEN NULL ELSE male END AS male_numerator,
+        CASE WHEN low_income_and_first_gen = 0 THEN NULL ELSE low_income_and_first_gen END AS low_income_first_gen_numerator,
+        CASE WHEN ninth_grade_count = 0 THEN NULL ELSE ninth_grade_count END AS ninth_grade_denominator,
     
     --annual retention
         annual_retention_numerator,
         annual_retention_denominator,
-        annual_retention_numerator/annual_retention_denominator AS percent_annual_retention_fy21,
         
     --social-emotional growth
-        covi_student_grew AS social_emotional_growth_numerator,
+        CASE WHEN covi_student_grew = 0 THEN NULL ELSE covi_student_grew END AS social_emotional_growth_numerator,
+        high_school_student_count,
         --covi_student_grew/high_school_student_count AS percent_social_emotional_growth_fy21,
         
-        
     --GPA data: seniors 3.25+ and/or composite ready
-        above_325_gpa_seniors AS above_325_gpa_seniors_numerator,
-        above_325_gpa_and_test_ready_seniors AS senior_325_gpa_and_test_ready_numerator,
-        twelfth_grade_count AS senior_325_gpa_only_denominator, --all seniors for 3.25 gpa only
-        twelfth_grade_count_valid_test AS senior_325_gpa_and_test_ready_denominator, --students that did not opt out of tests for 3.25 gpa & readiness
-        --above_325_gpa_seniors/twelfth_grade_count AS percent_seniors_above_325_fy21,
-        --above_325_gpa_and_test_ready_seniors/twelfth_grade_count_valid_test AS percent_seniors_above_325_and_test_ready_fy21,
+        CASE WHEN above_325_gpa_seniors = 0 THEN NULL ELSE above_325_gpa_seniors END AS above_325_gpa_seniors_numerator,
+        CASE WHEN above_325_gpa_and_test_ready_seniors = 0 THEN NULL ELSE above_325_gpa_and_test_ready_seniors END AS senior_325_gpa_and_test_ready_numerator,
+        CASE WHEN twelfth_grade_count = 0 THEN NULL ELSE twelfth_grade_count END AS senior_325_gpa_only_denominator, --all seniors for 3.25 gpa only
+        CASE WHEN twelfth_grade_count_valid_test = 0 THEN NULL ELSE twelfth_grade_count_valid_test END AS senior_325_gpa_and_test_ready_denominator, --students that did not opt out of tests for 3.25 gpa & readiness
+        
         
     --matriculation data
-        matriculated_best_good_situational AS matriculated_best_good_situational_numerator,
-        twelfth_grade_count AS matriculation_senior_denominator,
-        --matriculated_best_good_situational/twelfth_grade_count AS percent_matriculate_best_good_situational_fy21,
+        CASE WHEN matriculated_best_good_situational = 0 THEN NULL ELSE matriculated_best_good_situational END AS matriculated_best_good_situational_numerator,
+        CASE WHEN twelfth_grade_count = 0 THEN NULL ELSE twelfth_grade_count END AS matriculation_senior_denominator,
     
     --on-track data
-        on_track_numerator,
-        on_track_denominator,
-        --on_track_numerator/on_track_denominator AS percent_on_track_fy21,
+        CASE WHEN on_track_numerator = 0 THEN NULL ELSE on_track_numerator END AS on_track_numerator,
+        CASE WHEN on_track_denominator = 0 THEN NULL ELSE on_track_denominator END AS on_track_denominator,
         
     --6 year graduation rate
-        grade_rate_6_years_current_class_numerator AS six_yr_grad_rate_numerator,
-        grade_rate_6_years_current_class_denom,
-        --grade_rate_6_years_current_class_numerator/grade_rate_6_years_current_class_denom AS percent_6_year_grad_rate_fy21,
+        CASE WHEN grade_rate_6_years_current_class_numerator = 0 THEN NULL ELSE grade_rate_6_years_current_class_numerator END AS six_yr_grad_rate_numerator,
+        CASE WHEN grade_rate_6_years_current_class_denom = 0 THEN NULL ELSE grade_rate_6_years_current_class_denom END AS grade_rate_6_years_current_class_denom,
     
     --alumni
         alumni_count,
         
     --meaningful summer experiences
-        had_mse_numerator AS mse_numerator,
-        had_mse_denominator AS mse_denominator,
-        --had_mse_numerator/had_mse_denominator AS percent_mse_fy21,
+        CASE WHEN had_mse_numerator = 0 THEN NULL ELSE had_mse_numerator END AS mse_numerator,
+        CASE WHEN had_mse_denominator = 0 THEN NULL ELSE had_mse_denominator END AS mse_denominator
+    
+        FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  
+        )
         
     
-
-    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`
+    
