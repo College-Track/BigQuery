ef4acbf1b8551322ddf74c11731b87f8aad087dd
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-17 15:46:44 -0800

    Auto-commit: Wed Nov 17 2021 15:46:42 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard_functions.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard_functions.sql
@@ -1,5 +1,5 @@
 
-CREATE OR REPLACE TABLE FUNCTION  `org-scorecard-286421.aggregate_data.mapObjectiveUDF`(STRING)
+CREATE OR REPLACE TABLE FUNCTION  `org-scorecard-286421.aggregate_data.mapObjectiveUDF`(mapObjective STRING)
 AS
   SELECT
     CASE 
@@ -7,50 +7,55 @@ AS
             WHEN male_numerator IS NOT NULL THEN 'Objective_1'
             WHEN low_income_first_gen_numerator IS NULL THEN 'Objective_1'
             WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_1'
-            
-            WHEN Measure LIKE %social_emotional_academic_foundation% THEN 'Objective_2'
-            WHEN Measure LIKE %college_students% THEN 'Objective_3'
+            WHEN ninth_grade_denominator IS NULL THEN 'Objective_1'
+            WHEN ninth_grade_denominator IS NOT NULL THEN 'Objective_1'
+            WHEN annual_retention_numerator IS NULL THEN 'Objective_1'
+            WHEN annual_retention_numerator IS NOT NULL THEN 'Objective_1'
+            WHEN percent_male_fy21 IS NULL THEN 'Objective_1'
+            WHEN percent_male_fy21 IS NOT NULL THEN 'Objective_1'
+            WHEN percent_low_income_first_gen_fy21 IS NULL THEN 'Objective_1'
+            WHEN percent_low_income_first_gen_fy21 IS NOT NULL THEN 'Objective_1'
+            WHEN percent_annual_retention_fy21 IS NULL THEN 'Objective_1'
+            WHEN percent_annual_retention_fy21 IS NOT NULL THEN 'Objective_1'
+            WHEN social_emotional_growth_numerator IS NULL THEN 'Objective_2'
+            WHEN social_emotional_growth_numerator IS NOT NULL THEN 'Objective_2'
+            WHEN social_emotional_growth_denominator IS NULL THEN 'Objective_2'
+            WHEN social_emotional_growth_denominator IS NOT NULL THEN 'Objective_2'
+            WHEN percent_social_emotional_growth_fy21 IS NULL THEN 'Objective_2'
+            WHEN percent_social_emotional_growth_fy21 IS NOT NULL THEN 'Objective_2'
+            WHEN above_325_gpa_seniors_numerator IS NULL THEN 'Objective_2'
+            WHEN above_325_gpa_seniors_numerator IS NOT NULL THEN 'Objective_2'
+            WHEN senior_325_gpa_and_test_ready_numerator IS NULL THEN 'Objective_2'
+            WHEN senior_325_gpa_and_test_ready_numerator IS NOT NULL THEN 'Objective_2'
+            WHEN senior_325_gpa_only_denominator IS NULL THEN 'Objective_2'
+            WHEN senior_325_gpa_only_denominator IS NOT NULL THEN 'Objective_2'
+            WHEN senior_325_gpa_and_test_ready_denominator IS NULL THEN 'Objective_2'
+            WHEN senior_325_gpa_and_test_ready_denominator IS NOT NULL THEN 'Objective_2'
+            WHEN mse_numerator IS NULL THEN 'Objective_2'
+            WHEN mse_numerator IS NOT NULL THEN 'Objective_2'
+            WHEN mse_denominator IS NULL THEN 'Objective_2'
+            WHEN mse_denominator IS NOT NULL THEN 'Objective_2'
+            WHEN percent_social_emotional_growth_fy21 IS NULL THEN 'Objective_2'
+            WHEN percent_social_emotional_growth_fy21 IS NOT NULL THEN 'Objective_2'
+            WHEN percent_seniors_above_325_fy21 IS NULL THEN 'Objective_2'
+            WHEN percent_seniors_above_325_fy21 IS NOT NULL THEN 'Objective_2'
+            WHEN percent_seniors_above_325_and_test_ready_fy21 IS NULL THEN 'Objective_2'
+            WHEN percent_seniors_above_325_and_test_ready_fy21 IS NOT NULL THEN 'Objective_2'
+            WHEN percent_mse_fy21 IS NULL THEN 'Objective_2'
+            WHEN percent_mse_fy21 IS NOT NULL THEN 'Objective_2'
+            WHEN matriculated_best_good_situational_numerator IS NULL THEN 'Objective_3'
+            WHEN matriculation_senior_denominator IS NOT NULL THEN 'Objective_3'
+            WHEN on_track_numerator IS NULL THEN 'Objective_3'
+            WHEN on_track_denominator IS NOT NULL THEN 'Objective_3'
+            WHEN six_yr_grad_rate_numerator IS NULL THEN 'Objective_3'
+            WHEN grade_rate_6_years_current_class_denom IS NOT NULL THEN 'Objective_3'
+            WHEN percent_matriculate_best_good_situational_fy21 IS NULL THEN 'Objective_3'
+            WHEN percent_on_track_fy21 IS NOT NULL THEN 'Objective_3'
+            WHEN percent_6_year_grad_rate_fy21 IS NULL THEN 'Objective_3'
+            WHEN low_income_first_gen_numerator IS NOT NULL THEN 'Objective_3'
+        ELSE NULL
+        END AS Objective
   FROM  `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
   
-  
-    THEN recruit_and_retain
-    ninth_grade_denominator THEN recruit_and_retain
-     = annual_retention_numerator THEN recruit_and_retain
-     = ninth_grade_denominator THEN recruit_and_retain
-     = annual_retention_denominator THEN recruit_and_retain
-     = percent_male_fy21 THEN recruit_and_retain                             
-     = percent_low_income_first_gen_fy21 THEN recruit_and_retain
-     = percent_annual_retention_fy21 THEN recruit_and_retain     
-     = annual_retention_denominator THEN recruit_and_retain
-     = percent_male_fy21 THEN recruit_and_retain                             
-     = percent_low_income_first_gen_fy21 THEN recruit_and_retain
-     = percent_annual_retention_fy21 THEN recruit_and_retain 
-     = social_emotional_growth_numerator THEN social_emotional_academic_foundation
-     = social_emotional_growth_denominator THEN social_emotional_academic_foundation
-     = percent_social_emotional_growth_fy21 THEN social_emotional_academic_foundation
-     = above_325_gpa_seniors_numerator THEN social_emotional_academic_foundation
-     = senior_325_gpa_and_test_ready_numerator THEN social_emotional_academic_foundation                             
-     = senior_325_gpa_only_denominator THEN social_emotional_academic_foundation
-     = senior_325_gpa_and_test_ready_denominator THEN social_emotional_academic_foundation    
-     = mse_numerator THEN social_emotional_academic_foundation                             
-     = mse_denominator THEN social_emotional_academic_foundation
-     = percent_social_emotional_growth_fy21 THEN social_emotional_academic_foundation
-     = percent_seniors_above_325_fy21 THEN social_emotional_academic_foundation  
-     = percent_seniors_above_325_and_test_ready_fy21 THEN social_emotional_academic_foundation  
-     = percent_mse_fy21 THEN social_emotional_academic_foundation     
-     = matriculated_best_good_situational_numerator THEN college_students                             
-     = matriculation_senior_denominator THEN college_students
-     = on_track_numerator THEN college_students     
-     = on_track_denominator THEN college_students
-     = six_yr_grad_rate_numerator THEN college_students                             
-     = grade_rate_6_years_current_class_denom THEN college_students
-     = percent_matriculate_best_good_situational_fy21 THEN college_students         
-     = percent_on_track_fy21 THEN college_students  
-     = percent_6_year_grad_rate_fy21 THEN college_students
+       
+    
