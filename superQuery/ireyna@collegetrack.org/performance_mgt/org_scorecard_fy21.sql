32be5b24a53255cd022561163f3b9bb8b03b3abf
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-16 16:28:14 -0800

    Auto-commit: Tue Nov 16 2021 16:28:10 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,27 +1,4 @@
-#Using data-studio-260217.performance_mgt.fy21_eoy_combined_metrics table
-/*
-CREATE OR REPLACE TABLE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
-OPTIONS
-    (
-    description="This table pulls org scorecard program outcomes for fy21.  Numerator and denominators are included per site. Does not include graduate/employment outcomes, annual fundraising outcomes, hs capacity, or hr-related outcomes." 
-    )
-    AS
-    */
-    SELECT 
-        *,
-        male_numerator/ninth_grade_denominator AS percent_male_fy21,
-        low_income_first_gen_numerator/ninth_grade_denominator AS percent_low_income_first_gen_fy21,
-        annual_retention_numerator/annual_retention_denominator AS percent_annual_retention_fy21,
-        social_emotional_growth_numerator/social_emotional_growth_denominator AS percent_social_emotional_growth_fy21,
-        above_325_gpa_seniors_numerator/senior_325_gpa_only_denominator AS percent_seniors_above_325_fy21,
-        senior_325_gpa_and_test_ready_numerator/senior_325_gpa_and_test_ready_denominator AS percent_seniors_above_325_and_test_ready_fy21,
-        matriculated_best_good_situational_numerator/matriculation_senior_denominator AS percent_matriculate_best_good_situational_fy21,
-        on_track_numerator/on_track_denominator AS percent_on_track_fy21,
-        six_yr_grad_rate_numerator/grade_rate_6_years_current_class_denom AS percent_6_year_grad_rate_fy21,
-        mse_numerator/mse_denominator AS percent_mse_fy21
-        
-    FROM 
-        (SELECT --gather org scorecard metrics to calculate outcomes in main SELECT statement, account for ZEROs
+SELECT --gather org scorecard metrics to calculate outcomes in main SELECT statement, account for ZEROs
         site_sort,
         site_short,
         region_short,
@@ -66,13 +43,3 @@ OPTIONS
         CASE WHEN had_mse_denominator = 0 THEN NULL ELSE had_mse_denominator END AS mse_denominator
     
         FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  
-        ) ;
-        
-       /*
-ALTER TABLE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
-    ADD COLUMN fiscal_year STRING;
-UPDATE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21` --Populate 'fiscal year' with 'FY20'
-    SET fiscal_year = "FY21"
-    WHERE fiscal_year IS NULL
-        ;
-   */
