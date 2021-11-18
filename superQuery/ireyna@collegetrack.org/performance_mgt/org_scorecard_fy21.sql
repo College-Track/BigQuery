b31cf4466ba5f15d925b677a41bd8c07cdbc12de
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-17 16:08:25 -0800

    Auto-commit: Wed Nov 17 2021 16:08:19 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,45 +1,6 @@
-SELECT --gather org scorecard metrics to calculate outcomes in main SELECT statement, account for ZEROs
-        site_sort,
-        site_short,
-        region_short,
-        
-    --admits: male, first-gen & low-income; San Francsico has 1 student as 9th grade, should be zero - did not recruit
-        male  AS male_numerator,
-        low_income_and_first_gen  AS low_income_first_gen_numerator,
-         ninth_grade_count  AS ninth_grade_denominator,
-    
-    --annual retention
-        annual_retention_numerator,
-        annual_retention_denominator,
-        
-    --social-emotional growth
-        CASE WHEN covi_student_grew = 0 THEN NULL ELSE covi_student_grew END AS social_emotional_growth_numerator,
-        CASE WHEN covi_deonominator = 0 THEN NULL ELSE covi_deonominator END AS social_emotional_growth_denominator,
-        
-    --GPA data: seniors 3.25+ and/or composite ready
-        CASE WHEN above_325_gpa_seniors = 0 THEN NULL ELSE above_325_gpa_seniors END AS above_325_gpa_seniors_numerator,
-        CASE WHEN above_325_gpa_and_test_ready_seniors = 0 THEN NULL ELSE above_325_gpa_and_test_ready_seniors END AS senior_325_gpa_and_test_ready_numerator,
-        CASE WHEN twelfth_grade_count = 0 THEN NULL ELSE twelfth_grade_count END AS senior_325_gpa_only_denominator, --all seniors for 3.25 gpa only
-        CASE WHEN twelfth_grade_count_valid_test = 0 THEN NULL ELSE twelfth_grade_count_valid_test END AS senior_325_gpa_and_test_ready_denominator, --students that did not opt out of tests for 3.25 gpa & readiness
-        
-        
-    --matriculation data
-        CASE WHEN matriculated_best_good_situational = 0 THEN NULL ELSE matriculated_best_good_situational END AS matriculated_best_good_situational_numerator,
-        CASE WHEN twelfth_grade_count = 0 THEN NULL ELSE twelfth_grade_count END AS matriculation_senior_denominator,
-    
-    --on-track data
-        CASE WHEN on_track_numerator = 0 THEN NULL ELSE on_track_numerator END AS on_track_numerator,
-        CASE WHEN on_track_denominator = 0 THEN NULL ELSE on_track_denominator END AS on_track_denominator,
-        
-    --6 year graduation rate
-        CASE WHEN grade_rate_6_years_current_class_numerator = 0 THEN NULL ELSE grade_rate_6_years_current_class_numerator END AS six_yr_grad_rate_numerator,
-        CASE WHEN grade_rate_6_years_current_class_denom = 0 THEN NULL ELSE grade_rate_6_years_current_class_denom END AS grade_rate_6_years_current_class_denom,
-    
-    --alumni
-        alumni_count,
-        
-    --meaningful summer experiences
-        CASE WHEN had_mse_numerator = 0 THEN NULL ELSE had_mse_numerator END AS mse_numerator,
-        CASE WHEN had_mse_denominator = 0 THEN NULL ELSE had_mse_denominator END AS mse_denominator
-    
-        FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  
+    */
+CREATE TEMP TABLE ORG_SCORECARD_FY21 AS (SELECT * FROM `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`);
+    ALTER TABLE ORG_SCORECARD_FY21
+ADD COLUMN site_or_region STRING;
+INSERT INTO ORG_SCORECARD_FY21 (site_or_region) VALUES (site_short,region_short,'National');
+SELECT * FROM ORG_SCORECARD_FY21;
