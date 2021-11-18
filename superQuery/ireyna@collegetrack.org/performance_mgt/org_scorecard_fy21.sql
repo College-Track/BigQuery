5b45557c2cbf3a6127d4c374dc0efeca9f7f149a
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-17 17:00:09 -0800

    Auto-commit: Wed Nov 17 2021 17:00:06 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,70 +1,4 @@
-#Using data-studio-260217.performance_mgt.fy21_eoy_combined_metrics table
-/*
-ALTER TABLE  `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
-    ADD COLUMN site_or_region STRING,
-    
-    ;
-    ;
-INSERT INTO `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21` (site_or_region) VALUES ('National')
-    ; */
-    /*
-CREATE OR REPLACE TABLE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
-OPTIONS
-    (
-    description="This table pulls org scorecard program outcomes for fy21.  Numerator and denominators are included per site. Does not include graduate/employment outcomes, annual fundraising outcomes, hs capacity, or hr-related outcomes." 
-    )
-    AS
-    */
-CREATE TEMPORARY FUNCTION AccountAbrev (Account STRING) AS (
-    CASE
-        WHEN Account LIKE '%Northern California%' THEN 'NORCAL'
-        WHEN Account LIKE '%Colorado%' THEN 'CO'
-        WHEN Account LIKE '%Los Angeles%' THEN 'LA'
-        WHEN Account LIKE '%New Orleans_RG%' THEN 'NOLA_RG'
-        WHEN Account LIKE '%DC%' THEN 'DC'
-        WHEN Account LIKE '%Denver%' THEN 'DEN'
-        WHEN Account LIKE '%Aurora%' THEN 'AUR'
-        WHEN Account LIKE '%San Francisco%' THEN 'SF'
-        WHEN Account LIKE '%East Palo Alto%' THEN 'EPA'
-        WHEN Account LIKE '%Sacramento%' THEN 'SAC'
-        WHEN Account LIKE '%Oakland%' THEN 'OAK'
-        WHEN Account LIKE '%Watts%' THEN 'WATTS'
-        WHEN Account LIKE '%Boyle Heights%' THEN 'BH'
-        WHEN Account LIKE '%Ward 8%' THEN 'DC8'
-        WHEN Account LIKE '%Durant%' THEN 'PGC'
-        WHEN Account LIKE '%New Orleans%' THEN 'NOLA'
-        WHEN Account LIKE '%Crenshaw%' THEN 'CREN'
-        WHEN Account = 'National' THEN 'NATIONAL'
-        WHEN Account = 'National (AS LOCATION)' THEN 'NATIONAL_AS_LOCATION'
-      END)
-      ;
-CREATE TEMP TABLE ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region STRING); --create table that stores new column to add to fy21_org_scorecard table
-INSERT INTO ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region) VALUES  ('National'), --add site_or_region column with values
-                                                        ('East Palo Alto'),
-                                                        ('Oakland'),
-                                                        ('San Francisco'),
-                                                        ('New Orleans'),
-                                                        ('Aurora'),
-                                                        ('Boyle Heights'),
-                                                        ('Sacramento'),
-                                                        ('Watts'),
-                                                        ('Denver'),
-                                                        ('The Durant Center'),
-                                                        ('Ward 8'),
-                                                        ('Crenshaw'),
-                                                        ('Northern California'),
-                                                        ('Los Angeles'),
-                                                        ('New Orleans_RG'),
-                                                        ('Colorado'),
-                                                        ('Washington DC');
-    
-WITH prep_orgscorecard AS (
-    SELECT EOY.* , site_or_region, AccountAbrev(site_or_region) AS site_or_region_abbrev --add newly created column
-    FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  AS EOY
-    FULL JOIN ORG_SCORECARD_FY21_ADD_COLUMN AS TEMP ON EOY.site_short=TEMP.site_or_region 
-    )
-, aggregate_outcomes_as_percent AS (
-    SELECT 
+ SELECT 
         *,
         male_numerator/ninth_grade_denominator AS percent_male_fy21,
         low_income_first_gen_numerator/ninth_grade_denominator AS percent_low_income_first_gen_fy21,
@@ -125,19 +59,5 @@ WITH prep_orgscorecard AS (
         CASE WHEN had_mse_numerator = 0 THEN NULL ELSE had_mse_numerator END AS mse_numerator,
         CASE WHEN had_mse_denominator = 0 THEN NULL ELSE had_mse_denominator END AS mse_denominator
     
-        FROM prep_orgscorecard AS EOY
+        FROM  `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics` AS EOY
         LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` AS C ON EOY.site_short=C.site_short
-        )
-        )
-    SELECT *
-    FROM aggregate_outcomes_as_percent AS EOY
-   
-        
-       /*
-ALTER TABLE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
-    ADD COLUMN fiscal_year STRING;
-UPDATE `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21` --Populate 'fiscal year' with 'FY20'
-    SET fiscal_year = "FY21"
-    WHERE fiscal_year IS NULL
-        ;
-   */
