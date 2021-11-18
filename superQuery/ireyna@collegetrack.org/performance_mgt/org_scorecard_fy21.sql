ea1a3b1a9aae2456b74ca005345e85f670231ea5
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-18 13:38:02 -0800

    Auto-commit: Thu Nov 18 2021 13:37:58 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -2,12 +2,31 @@
 #Adding columns to fy21_org_scorecard_program table
 
 WITH 
-add_NORCAL_values AS (
+add_norcal_values AS (
 
 SELECT 
     site_short,site_abrev,site_or_region,site_or_region_abbrev,
+--Populate values for NORCAL region
     CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(male_numerator) OVER () ELSE male_numerator END AS male_numerator,
-   -- CASE WHEN site_abrev IN ('EPA','OAK','SF','SAC') THEN SUM(ninth_grade_denominator) OVER () ELSE male_numerator END AS male_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(ninth_grade_denominator) OVER () ELSE ninth_grade_denominator END AS ninth_grade_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(low_income_first_gen_numerator) OVER () ELSE low_income_first_gen_numerator END AS low_income_first_gen_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(annual_retention_numerator) OVER () ELSE annual_retention_numerator END AS annual_retention_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(annual_retention_denominator) OVER () ELSE annual_retention_denominator END AS annual_retention_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(social_emotional_growth_numerator) OVER () ELSE social_emotional_growth_numerator END AS social_emotional_growth_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(social_emotional_growth_denominator) OVER () ELSE social_emotional_growth_denominator END AS social_emotional_growth_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(mse_numerator) OVER () ELSE mse_numerator END AS mse_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(mse_denominator) OVER () ELSE mse_denominator END AS mse_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(above_325_gpa_seniors_numerator) OVER () ELSE above_325_gpa_seniors_numerator END AS above_325_gpa_seniors_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(senior_325_gpa_and_test_ready_numerator) OVER () ELSE senior_325_gpa_and_test_ready_numerator END AS senior_325_gpa_and_test_ready_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(senior_325_gpa_only_denominator) OVER () ELSE senior_325_gpa_only_denominator END AS senior_325_gpa_only_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(senior_325_gpa_and_test_ready_denominator) OVER () ELSE senior_325_gpa_and_test_ready_denominator END AS senior_325_gpa_and_test_ready_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(matriculated_best_good_situational_numerator) OVER () ELSE matriculated_best_good_situational_numerator END AS matriculated_best_good_situational_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(matriculation_senior_denominator) OVER () ELSE matriculation_senior_denominator END AS matriculation_senior_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(on_track_numerator) OVER () ELSE on_track_numerator END AS on_track_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(on_track_denominator) OVER () ELSE on_track_denominator END AS on_track_denominator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(six_yr_grad_rate_numerator) OVER () ELSE six_yr_grad_rate_numerator END AS six_yr_grad_rate_numerator,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(grade_rate_6_years_current_class_denom) OVER () ELSE grade_rate_6_years_current_class_denom END AS grade_rate_6_years_current_class_denom,
+    CASE WHEN site_or_region_abbrev = "NORCAL" THEN SUM(alumni_count) OVER () ELSE alumni_count END AS alumni_count
 
 FROM  `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
 WHERE region_abrev = 'NOR CAL' OR site_or_region_abbrev = "NORCAL"
