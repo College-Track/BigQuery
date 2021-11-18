f791288f6f1323476e9b4b6996f42d7b085714c2
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-17 16:09:00 -0800

    Auto-commit: Wed Nov 17 2021 16:08:55 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,4 +1,3 @@
-    */
 CREATE TEMP TABLE ORG_SCORECARD_FY21 AS (SELECT * FROM `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`);
     ALTER TABLE ORG_SCORECARD_FY21
 ADD COLUMN site_or_region STRING;
