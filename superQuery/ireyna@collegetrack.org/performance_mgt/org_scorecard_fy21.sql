5b1e11280fcd3e60d46ed1944f088dd7658c408c
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-17 16:39:23 -0800

    Auto-commit: Wed Nov 17 2021 16:39:20 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -15,8 +15,30 @@ OPTIONS
     )
     AS
     */
-CREATE TEMP TABLE ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region STRING);
-INSERT INTO ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region) VALUES  ('National'),
+CREATE TEMPORARY FUNCTION AccountAbrev (Account STRING) AS (
+    CASE
+        WHEN Account LIKE '%Northern California%' THEN 'NORCAL'
+        WHEN Account LIKE '%Colorado%' THEN 'CO'
+        WHEN Account LIKE '%Los Angeles%' THEN 'LA'
+        WHEN Account LIKE '%New Orleans_RG%' THEN 'NOLA_RG'
+        WHEN Account LIKE '%DC%' THEN 'DC'
+        WHEN Account LIKE '%Denver%' THEN 'DEN'
+        WHEN Account LIKE '%Aurora%' THEN 'AUR'
+        WHEN Account LIKE '%San Francisco%' THEN 'SF'
+        WHEN Account LIKE '%East Palo Alto%' THEN 'EPA'
+        WHEN Account LIKE '%Sacramento%' THEN 'SAC'
+        WHEN Account LIKE '%Oakland%' THEN 'OAK'
+        WHEN Account LIKE '%Watts%' THEN 'WATTS'
+        WHEN Account LIKE '%Boyle Heights%' THEN 'BH'
+        WHEN Account LIKE '%Ward 8%' THEN 'DC8'
+        WHEN Account LIKE '%Durant%' THEN 'PGC'
+        WHEN Account LIKE '%New Orleans%' THEN 'NOLA'
+        WHEN Account LIKE '%Crenshaw%' THEN 'CREN'
+        WHEN Account = 'National' THEN 'NATIONAL'
+        WHEN Account = 'National (AS LOCATION)' THEN 'NATIONAL_AS_LOCATION'
+      END
+CREATE TEMP TABLE ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region STRING); --create table that stores new column to add to fy21_org_scorecard table
+INSERT INTO ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region) VALUES  ('National'), --add site_or_region column with values
                                                         ('East Palo Alto'),
                                                         ('Oakland'),
                                                         ('San Francisco'),
@@ -35,7 +57,11 @@ INSERT INTO ORG_SCORECARD_FY21_ADD_COLUMN (site_or_region) VALUES  ('National'),
                                                         ('Colorado'),
                                                         ('Washington DC');
     
---WITH prep_orgscorecard AS (
+WITH prep_orgscorecard AS (
     SELECT EOY.* , site_or_region
     FROM `data-studio-260217.performance_mgt.fy21_eoy_combined_metrics`  AS EOY
     FULL JOIN ORG_SCORECARD_FY21_ADD_COLUMN AS TEMP ON EOY.site_short=TEMP.site_or_region 
+    )
+--, create_account_abbrev_column AS (
+    SELECT *, AccountAbrev(site_or_region) AS site_or_region_abbrev
+    FROM prep_orgscorecard 
