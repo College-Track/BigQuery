667f4397062dd72b8cf1beed5ef7b0c8e943a39c
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-19 10:56:04 -0800

    Auto-commit: Fri Nov 19 2021 10:56:00 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -1,6 +1,7 @@
-    SELECT 
+  SELECT 
     region_abrev,
     site_or_region_abbrev,
+    site_or_region,
     CASE WHEN site_or_region_abbrev = "NATIONAL" THEN SUM(male_numerator) OVER () ELSE male_numerator END AS male_numerator, --pull grand total, add value only to National
     CASE WHEN site_or_region_abbrev = "NATIONAL" THEN SUM(ninth_grade_denominator) OVER () ELSE ninth_grade_denominator END AS ninth_grade_denominator,
     CASE WHEN site_or_region_abbrev = "NATIONAL" THEN SUM(low_income_first_gen_numerator) OVER () ELSE low_income_first_gen_numerator END AS low_income_first_gen_numerator,
