e858c7fe23fdc4f98e3668cc55eec0d4216d198f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-22 15:22:55 -0800

    Auto-commit: Mon Nov 22 2021 15:22:51 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -66,7 +66,7 @@ SELECT DISTINCT
 
 FROM `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21` AS b
 LEFT JOIN add_national_values AS a ON a.region_abrev=b.region_abrev
-
+WHERE b.region_abrev = 'NATIONAL' 
 /*
 SELECT *  FROM `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
 WHERE region_abrev <> 'NATIONAL'
