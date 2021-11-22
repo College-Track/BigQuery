bb2226c6b79dc913424cead119dba955309bc29c
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-22 14:37:24 -0800

    Auto-commit: Mon Nov 22 2021 14:37:20 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_fy21.sql
@@ -13,12 +13,12 @@ SELECT
     CASE WHEN site_or_region = "National" THEN SUM(senior_325_gpa_and_test_ready_numerator) OVER () ELSE senior_325_gpa_and_test_ready_numerator END AS senior_325_gpa_and_test_ready_numerator,
     CASE WHEN site_or_region = "National" THEN SUM(senior_325_gpa_only_denominator) OVER () ELSE senior_325_gpa_only_denominator END AS senior_325_gpa_only_denominator,
     CASE WHEN site_or_region = "National" THEN SUM(senior_325_gpa_and_test_ready_denominator) OVER () ELSE senior_325_gpa_and_test_ready_denominator END AS senior_325_gpa_and_test_ready_denominator,
-    CASE WHEN site_or_region_abbrev = "National" THEN SUM(matriculated_best_good_situational_numerator) OVER () ELSE matriculated_best_good_situational_numerator END AS matriculated_best_good_situational_numerator,
-    CASE WHEN site_or_region_abbrev = "National" THEN SUM(matriculation_senior_denominator) OVER () ELSE matriculation_senior_denominator END AS matriculation_senior_denominator,
-    CASE WHEN site_or_region_abbrev = "National" THEN SUM(on_track_numerator) OVER () ELSE on_track_numerator END AS on_track_numerator,
-    CASE WHEN site_or_region_abbrev = "National" THEN SUM(on_track_denominator) OVER () ELSE on_track_denominator END AS on_track_denominator,
-    CASE WHEN site_or_region_abbrev = "National" THEN SUM(six_yr_grad_rate_numerator) OVER () ELSE six_yr_grad_rate_numerator END AS six_yr_grad_rate_numerator,
-    CASE WHEN site_or_region_abbrev = "National" THEN SUM(grade_rate_6_years_current_class_denom) OVER () ELSE grade_rate_6_years_current_class_denom END AS grade_rate_6_years_current_class_denom,
+    CASE WHEN site_or_region = "National" THEN SUM(matriculated_best_good_situational_numerator) OVER () ELSE matriculated_best_good_situational_numerator END AS matriculated_best_good_situational_numerator,
+    CASE WHEN site_or_region = "National" THEN SUM(matriculation_senior_denominator) OVER () ELSE matriculation_senior_denominator END AS matriculation_senior_denominator,
+    CASE WHEN site_or_region = "National" THEN SUM(on_track_numerator) OVER () ELSE on_track_numerator END AS on_track_numerator,
+    CASE WHEN site_or_region = "National" THEN SUM(on_track_denominator) OVER () ELSE on_track_denominator END AS on_track_denominator,
+    CASE WHEN site_or_region = "National" THEN SUM(six_yr_grad_rate_numerator) OVER () ELSE six_yr_grad_rate_numerator END AS six_yr_grad_rate_numerator,
+    CASE WHEN site_or_region = "National" THEN SUM(grade_rate_6_years_current_class_denom) OVER () ELSE grade_rate_6_years_current_class_denom END AS grade_rate_6_years_current_class_denom,
     --CASE WHEN site_or_region_abbrev = "NOLA_RG" THEN SUM(alumni_count) OVER () ELSE alumni_count END AS alumni_count
 
 FROM  `org-scorecard-286421.aggregate_data.org_scorecard_program_fy21`
