6db27b156c45c2846ef7ba70b4837ef2b941ef6c
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-23 15:14:12 -0800

    Auto-commit: Tue Nov 23 2021 15:14:07 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_transposed_tables.sql
@@ -0,0 +1,51 @@
+--Houses all org scorecard outcomes across fiscal years as a tranposed table for the DS Overview Page
+
+SELECT Objective
+,fiscal_year
+,EPA
+,OAK
+,SF
+,NOLA
+,AUR
+,BH
+,SAC
+,WATTS
+,DEN
+,PGC
+,DC8
+,CREN
+,DC
+,CO
+,LA
+,NOLA_RG
+,NORCAL
+,NATIONAL
+,NATIONAL_AS_LOCATION
+,Measure
+ 
+FROM `org-scorecard-286421.transposed_tables.fy21_org_scorecard_hs_college_transposed` 
+UNION ALL
+SELECT Objective
+,fiscal_year
+,EPA
+,OAK
+,SF
+,NOLA
+,AUR
+,BH
+,SAC
+,WATTS
+,DEN
+,PGC
+,DC8
+,CREN
+,DC
+,CO
+,LA
+,NOLA_RG
+,NORCAL
+,NATIONAL
+,NATIONAL_AS_LOCATION
+,Measure
+
+FROM `org-scorecard-286421.transposed_tables.org_scorecard_fy20_overview`
