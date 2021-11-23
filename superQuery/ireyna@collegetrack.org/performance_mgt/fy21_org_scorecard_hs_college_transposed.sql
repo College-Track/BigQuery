710156d9964b845c371eec7684e2dbf1141ab4b4
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-23 09:08:06 -0800

    Testing Objective column added to fy21 org scorecard transposed table (hs and college measures only)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/fy21_org_scorecard_hs_college_transposed.sql
@@ -0,0 +1,48 @@
+--Add Objective column and values to table: fy21_org_scorecard_hs_college_transposed
+
+
+SELECT 
+            *,
+            CASE WHEN Measure IN    (
+                                    'male_numerator'
+                                    ,'low_income_first_gen_numerator'
+                                    ,'ninth_grade_denominator'
+                                    ,'annual_retention_numerator'
+                                    ,'annual_retention_denominator'
+                                    ,'percent_male_fy21'
+                                    ,'percent_low_income_first_gen_fy21'
+                                    ,'percent_annual_retention_fy21'
+                                    )
+                THEN 'Objective_1' 
+                WHEN Measure IN    (
+                                    'social_emotional_growth_numerator'
+                                    ,'social_emotional_growth_denominator'
+                                    ,'above_325_gpa_seniors_numerator'
+                                    ,'senior_325_gpa_and_test_ready_numerator'
+                                    ,'senior_325_gpa_only_denominator'
+                                    ,'senior_325_gpa_and_test_ready_denominator'
+                                    ,'mse_numerator'
+                                    ,'mse_denominator'
+                                    ,'percent_social_emotional_growth_fy21'
+                                    ,'percent_seniors_above_325_fy21'
+                                    ,'percent_seniors_above_325_and_test_ready_fy21'
+                                    ,'percent_mse_fy21'
+                                    )
+                THEN 'Objective_2'
+                WHEN Measure IN    (
+                                    'matriculated_best_good_situational_numerator'
+                                    ,'matriculation_senior_denominator'
+                                    ,'on_track_numerator'
+                                    ,'on_track_denominator'
+                                    ,'six_yr_grad_rate_numerator'
+                                    ,'grade_rate_6_years_current_class_denom'
+                                    ,'percent_matriculated_best_good_situational_fy21'
+                                    ,'percent_on_track_fy21'
+                                    ,'percent_6_year_grad_rate_fy21'
+                                    )
+                THEN 'Objective_3' 
+                ELSE NULL 
+                END AS Objective
+            
+FROM `org-scorecard-286421.transposed_tables.fy21_org_scorecard_hs_college_transposed`
+
