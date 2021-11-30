47f7fd77fd3431c0a9ba51937e370584811e271d
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-11-30 14:30:25 -0800

    Auto-commit: Tue Nov 30 2021 14:30:18 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_transposed_tables.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_transposed_tables.sql
@@ -10,22 +10,22 @@ OPTIONS
 SELECT 
     * EXCEPT (measure_datastudio),
     CASE 
-        WHEN Measure = 'percent_6_year_grad_rate' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Students graduating from college within 6 years (70%)'
-        WHEN Measure = 'percent_gainful_employment' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Graduates meeting gainful employment standard (85%)'
-        WHEN Measure = 'percent_annual_fundraising_target' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'annual_fundraising	Annual Fundraising target (100%)'
-        WHEN Measure = 'percent_meaningful_employment' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Graduates with meaningful employment (85%)'
-        WHEN Measure = 'percent_employment_grad_school' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Graduates with full-time employment or enrolled in graduate school within 6 months of graduation (65%)'
-        WHEN Measure = 'percent_tenure' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Staff with full-time tenure of 3+ years in organization (35%)'
-        WHEN Measure = 'percent_social_emotional_growth' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Students growing toward average or above social-emotional strengths'
-        WHEN Measure = 'percent_matriculated_best_good_situational' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Students matriculating to Best Fit, Good Fit, or Situational Best Fit colleges (50%)'
-        WHEN Measure = 'percent_on_track' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Students with enough credits accumulated to graduate in 6 years (80%)'
-        WHEN Measure = 'percent_seniors_above_325_and_test_ready' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Seniors with GPA 3.25+ and Composite Ready (55%)'
-        WHEN Measure = 'percent_annual_retention' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'High school students retained annually (90%)'
-        WHEN Measure = 'percent_hs_capacity' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'High school capacity enrolled (95%)'
-        WHEN Measure = 'percent_male' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN '9th grade students are male (50%)'
-        WHEN Measure = 'percent_low_income_first_gen' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN '9th grade students are low-income and first-generation (80%)'
-        WHEN Measure = 'percent_mse' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN'Students with meaningful summer experiences (85%)'
-        WHEN Measure = 'percent_staff_engagement' AND fiscal_year = 'FY21' measure_datastudio IS NULL THEN 'Staff engagement score above average nonprofit benchmark (Y)'
+        WHEN Measure = 'percent_6_year_grad_rate' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Students graduating from college within 6 years (70%)'
+        WHEN Measure = 'percent_gainful_employment' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Graduates meeting gainful employment standard (85%)'
+        WHEN Measure = 'percent_annual_fundraising_target' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'annual_fundraising	Annual Fundraising target (100%)'
+        WHEN Measure = 'percent_meaningful_employment' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Graduates with meaningful employment (85%)'
+        WHEN Measure = 'percent_employment_grad_school' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Graduates with full-time employment or enrolled in graduate school within 6 months of graduation (65%)'
+        WHEN Measure = 'percent_tenure' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Staff with full-time tenure of 3+ years in organization (35%)'
+        WHEN Measure = 'percent_social_emotional_growth' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Students growing toward average or above social-emotional strengths'
+        WHEN Measure = 'percent_matriculated_best_good_situational' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Students matriculating to Best Fit, Good Fit, or Situational Best Fit colleges (50%)'
+        WHEN Measure = 'percent_on_track' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Students with enough credits accumulated to graduate in 6 years (80%)'
+        WHEN Measure = 'percent_seniors_above_325_and_test_ready' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Seniors with GPA 3.25+ and Composite Ready (55%)'
+        WHEN Measure = 'percent_annual_retention' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'High school students retained annually (90%)'
+        WHEN Measure = 'percent_hs_capacity' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'High school capacity enrolled (95%)'
+        WHEN Measure = 'percent_male' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN '9th grade students are male (50%)'
+        WHEN Measure = 'percent_low_income_first_gen' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN '9th grade students are low-income and first-generation (80%)'
+        WHEN Measure = 'percent_mse' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN'Students with meaningful summer experiences (85%)'
+        WHEN Measure = 'percent_staff_engagement' AND fiscal_year = 'FY21' AND measure_datastudio IS NULL THEN 'Staff engagement score above average nonprofit benchmark (Y)'
         ELSE measure_datastudio
         END AS measure_datastudio
 FROM `org-scorecard-286421.transposed_tables.org_scorecard_overview_all_fy`
