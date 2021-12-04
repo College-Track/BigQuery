637575c0b791e0127a7279a154f7892469a31c54
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-12-03 17:23:31 -0800

    Auto-commit: Fri Dec 03 2021 17:23:16 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_all.sql
@@ -0,0 +1,22 @@
+--Combine Org Scorecard outcomes for FY20-current year, unpivoted
+CREATE OR REPLACE TABLE `org-scorecard-286421.aggregate_data.org_scorecard_outcomes`
+OPTIONS
+    (
+    description="This table pulls Org Scorecard outcomes, numerators (where applicable), denominator (where applicable)" 
+    )
+    AS 
+
+
+WITH
+
+unpivot_table AS (
+    SELECT * 
+
+     
+     FROM `org-scorecard-286421.transposed_tables.org_scorecard_overview_all_fy`
+
+    UNPIVOT INCLUDE NULLS  
+        (Outcome FOR site_or_region IN (EPA,OAK,SF,AUR,BH,SAC,WATTS,DEN,PGC,DC8,CREN,DC,CO,LA,NOLA_RG,NOLA,NORCAL,NATIONAL,NATIONAL_AS_LOCATION) --Create a "site_or_region" column 
+        ) AS UNPVt
+    )
+    SELECT * FROM unpivot_table
