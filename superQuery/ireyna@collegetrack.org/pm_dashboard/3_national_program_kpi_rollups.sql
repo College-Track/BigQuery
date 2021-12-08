6c68dc9ad47d8ed4cc5ace93b048b2d62b3f6fc7
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-12-08 13:19:05 -0800

    Updated National KPI table with more concise code on National Rollups, corrected National Director of Mental Health and Wellness rollup

--- superQuery/ireyna@collegetrack.org/pm_dashboard/3_national_program_kpi_rollups.sql
+++ superQuery/ireyna@collegetrack.org/pm_dashboard/3_national_program_kpi_rollups.sql
@@ -1,22 +1,21 @@
 
-
+/*
 CREATE
 OR REPLACE TABLE `data-studio-260217.performance_mgt.fy22_national_kpis`  OPTIONS (
   description = "KPIs submitted by National teams for FY22. This also rolls up the numerator and denominator for National KPIs that are based on weighted Program KPI targets. References List of KPIs by role Ghseet, and Targets submitted thru FormAssembly Team KPI"
 )
 AS
+*/
 
 
 WITH 
 
 --pull roles that are only National roles
 national_kpis AS (
-
 SELECT 
-function AS national_function,
-role AS national_role,
-kpis_by_role AS national_rollup_kpi,
---SUM(student_count) AS national_rollup_student_sum
+    function AS national_function,
+    role AS national_role,
+    kpis_by_role AS national_rollup_kpi,
 
 --Remove select KPIs from Nikki Wardlaw Director of Philanthropic Initiatives role. Fulfilling 2 roles in FY22, allowed to remove KPIs     
 CASE
@@ -31,9 +30,11 @@ WHERE (national = 1 or hr_people = 1)
 --Prep mapping of program KPI from Regions: % low income AND first gen to National
 region_kpis AS (
 SELECT  
+    target_submitted,
     function, 
     site_or_region,
     kpis_by_role,
+    target_fy22,
 
     --Crenshaw is a non-mature site and Site Director sets target. Use Site Director weighted target vs. LA RED weighted target. Blank out RED student_count
      CASE 
@@ -50,51 +51,60 @@ SELECT
    
 FROM `data-studio-260217.performance_mgt.fy22_team_kpis` 
 WHERE region_function = 1
-AND kpis_by_role = '% of entering 9th grade students who are low-income AND first-gen'
+    AND kpis_by_role = '% of entering 9th grade students who are low-income AND first-gen'
+    AND target_submitted= 'Submitted'
 
 GROUP BY 
-function, 
-kpis_by_role,
-student_count,
-target_numerator,
-count_of_targets,
-site_or_region
+    function, 
+    kpis_by_role,
+    target_fy22,
+    student_count,
+    target_numerator,
+    count_of_targets,
+    site_or_region,
+    target_submitted
 ),
 
 --pull KPIs that are only program KPIs, to map to National later
 program_kpis AS (
 SELECT
-function, 
-site_or_region,
-kpis_by_role,
-student_count,
-target_numerator,
-count_of_targets
+    target_submitted,
+    function, 
+    site_or_region,
+    kpis_by_role,
+    target_fy22,
+    student_count,
+    target_numerator,
+    count_of_targets
 FROM `data-studio-260217.performance_mgt.fy22_team_kpis` 
-WHERE program = 1
-
+WHERE program = 1 AND target_submitted = 'Submitted'
 GROUP BY 
-function, 
-kpis_by_role,
-student_count,
-target_numerator,
-count_of_targets,
-site_or_region
-),
+    function, 
+    kpis_by_role,
+    target_fy22,
+    student_count,
+    target_numerator,
+    count_of_targets,
+    site_or_region
+    ,target_submitted
+)
 
-combine_regional_program_kpis AS (
+,combine_regional_program_kpis AS (
 SELECT program_kpis.*
 FROM program_kpis 
 
-UNION ALL 
+UNION DISTINCT 
 
 SELECT region_kpis.*
 FROM region_kpis
-),
+)
 
-identify_program_rollups_for_national AS ( #25 KPIs for FY22
+,identify_program_rollups_for_national AS ( #25 KPIs for FY22 --up to here looks accurate for roll up identification
 SELECT
-    national.* EXCEPT (national),
+    DISTINCT national.* EXCEPT (national),
+    target_fy22,
+    program.target_submitted,
+    site_or_region,
     CASE 
         WHEN national_rollup_kpi IS NOT NULL 
         THEN 1
@@ -105,80 +115,70 @@ LEFT JOIN combine_regional_program_kpis AS program
     ON national.national_rollup_kpi = program.kpis_by_role
     
 WHERE national.national_rollup_kpi = program.kpis_by_role
-    AND program.kpis_by_role NOT IN ('% of students growing toward average or above social-emotional strengths',
+    AND program.kpis_by_role NOT IN ('% of students growing toward average or above social-emotional strengths', --excluded for FY21 OR 
                                     'Staff engagement score above average nonprofit benchmark',
                                     '% of students engaged in career exploration, readiness events or internships')
 GROUP BY 
     national_function,
     national_rollup_kpi,
-    national_role
-),
-
---SUM up student count and target numerators for Program KPIs
-sum_program_student_count AS(
-SELECT 
-    SUM(student_count) AS program_student_sum,
-    SUM(target_numerator) AS program_target_numerator_sum,
-    kpis_by_role
-FROM combine_regional_program_kpis
-WHERE count_of_targets = 1
-GROUP BY kpis_by_role
-),
-
---Map aggregated values from Program KPIs (student_count, target_numerator) that rollup to National here
-national_rollups AS (
-SELECT 
-CASE
-    WHEN site_or_region IS NOT NULL THEN "National"
-    END AS National_location,
-national_function,
-national_rollup_kpi,
-national_role,
---count_of_targets,
-student_count AS natl_student_count,
-program_student_sum,
-target_numerator AS natl_target_numerator,
-program_target_numerator_sum,
-indicator_program_rollup_for_national
-
-FROM identify_program_rollups_for_national AS natl
-LEFT JOIN  `data-studio-260217.performance_mgt.fy22_team_kpis` AS team_kpis
-    ON team_kpis.kpis_by_role = natl.national_rollup_kpi
-LEFT JOIN sum_program_student_count AS sum_student
-    ON sum_student.kpis_by_role=natl.national_rollup_kpi
+    national_role,
+    target_fy22,
+    target_submitted
+    ,site_or_region
 )
 
+--SUM up student count and target numerators for Program KPIs. Use this CTE to see which sites submitted what targets, their student count, to verify FY22 national rollup
+,sum_program_student_count AS(
+SELECT  
+    kpis_by_role,
+    SUM(program_target_numerator_sum)/SUM(program_student_sum) AS target_fy22 --national rollup (see subquery for logic)
+FROM 
+    (
+        SELECT 
+            CASE 
+                WHEN student_count IS NULL THEN count_of_targets --if there is no student count, take the target that was set. Will be divided by count of targets to get the target_fy22
+                WHEN student_count = 0 THEN NULL 
+                ELSE SUM(student_count) END AS program_student_sum,
+            CASE 
+                WHEN target_numerator IS NULL THEN target_fy22 --if there is no student count (and therefore no numerator), take the count of targets
+                WHEN target_numerator = 0 THEN NULL 
+                ELSE SUM(target_numerator) END AS program_target_numerator_sum,
+            kpis_by_role,
+            target_fy22,
+            count_of_targets
+            --site_or_region #bring back in to see which sites set targets for each KPI that rolls up
+        FROM combine_regional_program_kpis
+        WHERE count_of_targets = 1 
+        GROUP BY kpis_by_role,target_fy22,student_count,target_numerator,count_of_targets
+    )
+GROUP BY 
+    kpis_by_role
+    --site_or_region #bring back in to see which sites set targets for each KPI that rolls up
+)
+--Pull in fy22 targets for National rollups and pull in field: indicator_program_rollup_for_national (from identify_program_rollups_for_national CTE)
 SELECT 
-distinct * EXCEPT (national_function,
-                    national_role, student_count)
+DISTINCT 
+        team_kpis.function,
+        role,
+        CASE WHEN sum_student.target_fy22 IS NOT NULL THEN sum_student.target_fy22 ELSE team_kpis.target_fy22 END AS target_fy22,
+        CASE WHEN indicator_program_rollup_for_national = 1 THEN natl_rollups.national_rollup_kpi ELSE team_kpis.kpis_by_role END AS kpis_by_role,
+        natl_rollups.indicator_program_rollup_for_national,
 
 FROM  `data-studio-260217.performance_mgt.fy22_team_kpis` AS team_kpis
-LEFT JOIN national_rollups AS natl
-    ON natl.national_rollup_kpi = team_kpis.kpis_by_role
-    AND natl.national_function = team_kpis.function
-WHERE (national =1 OR hr_people=1)
+LEFT JOIN sum_program_student_count AS sum_student
+    ON sum_student.kpis_by_role = team_kpis.kpis_by_role
+
+LEFT JOIN identify_program_rollups_for_national AS natl_rollups 
+    ON natl_rollups.national_rollup_kpi = team_kpis.kpis_by_role
+    
+WHERE (national =1 OR hr_people=1) 
+
 GROUP BY 
-function,
-role,
+team_kpis.function,
+team_kpis.role,
 kpis_by_role,
-site_or_region,
 target_fy22,
-target_submitted,
 hr_people,
 national,
 development,
-region_function,
-program,
-Region,
-Site,
-student_count,
-target_numerator,
-natl_target_numerator,
-count_of_targets,
-national_rollup_kpi,
-program_student_sum,
-program_target_numerator_sum,
-natl_student_count,
-indicator_program_rollup_for_national,
-national_location,
-regional_executive_rollup
+indicator_program_rollup_for_national
