065d5763406d6cb8b81e938ef0c1a6f2721621d3
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-12-14 09:51:25 -0800

    Auto-commit: Tue Dec 14 2021 09:51:18 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests,tests IR/persistence_fall_to_fall_detail_fit_type.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests,tests IR/persistence_fall_to_fall_detail_fit_type.sql
@@ -224,14 +224,18 @@ enrollment_indicators AS (
     SELECT 
         *,
         CASE 
+            WHEN alumni_persistence = 'Graduated' 
+            THEN 1
             WHEN fit_type_fall_2020_21 IN ("Best Fit","Situational","Good Fit","Local Affordable") 
-            AND fit_type_fall_2021_22 IN ("Best Fit","Situational","Good Fit","Local Affordable") 
+                AND fit_type_fall_2021_22 IN ("Best Fit","Situational","Good Fit","Local Affordable") 
             THEN 1 -- "Persisting - Affordable College Enrollment"
             ELSE 0 -- "Did not persist to affordable option"
             END AS persisted_with_affordable_enrollment,
         CASE 
+            WHEN alumni_persistence = 'Graduated' 
+            THEN 1
             WHEN fit_type_fall_2020_21 = "Best Fit"
-            AND fit_type_fall_2021_22 = "Best Fit"
+                AND fit_type_fall_2021_22 = "Best Fit"
             THEN 1 
             ELSE 0
         END AS persisted_best_fit_enrollment
