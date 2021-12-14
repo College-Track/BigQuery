dc8b28715803e9858d676c80676f3b5400b5d365
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-12-14 11:40:59 -0800

    Auto-commit: Tue Dec 14 2021 11:40:53 GMT-0800 (Pacific Standard Time)

---
+++ superQuery/ireyna@collegetrack.org/one-off requests,tests IR/tulane_students_situational_best_fit.sql
@@ -0,0 +1,16 @@
+SELECT 
+    contact_id,
+    full_name_c,
+    high_school_graduating_class_c,
+    site_short,
+    at_id,
+    fit_type_at_c,
+    situational_best_fit_c,
+    situational_best_fit_context_c
+    
+FROM `data-warehouse-289815.salesforce_clean.contact_at_template` AS contact_at 
+
+WHERE
+    indicator_completed_Ct_hs_program_c = TRUE
+    AND AT_School_Name = 'Tulane University of Louisiana'
+    AND fit_type_at_c NOT IN ('Best Fit','Situational')
