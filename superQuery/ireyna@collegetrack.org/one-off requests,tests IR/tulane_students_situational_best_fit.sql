c2fdbc183bdcf4d5a2bba134357a7b13b3f65bfb
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-12-14 11:44:20 -0800

    Auto-commit: Tue Dec 14 2021 11:44:16 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests,tests IR/tulane_students_situational_best_fit.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests,tests IR/tulane_students_situational_best_fit.sql
@@ -4,7 +4,7 @@ SELECT
     high_school_graduating_class_c,
     site_short,
     AT_Grade_c,
-    global_academic_semester_c,
+    GAS_Name AS academic_term,
     at_id,
     fit_type_at_c,
     situational_best_fit_c,
