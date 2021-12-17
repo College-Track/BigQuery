3f771ba6e74cf61da9125b0bddc430251746f3e8
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2021-12-16 16:58:05 -0800

    Auto-commit: Thu Dec 16 2021 16:58:00 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/one-off requests,tests IR/tulane_students_situational_best_fit.sql
+++ superQuery/ireyna@collegetrack.org/one-off requests,tests IR/tulane_students_situational_best_fit.sql
@@ -12,7 +12,8 @@ SELECT
     fit_type_at_c,
     situational_best_fit_c,
     situational_best_fit_context_c,
-    cc_advisor_c
+    cc_advisor_c,
+    start_date_c
     
 FROM `data-warehouse-289815.salesforce_clean.contact_at_template` AS contact_at 
 
