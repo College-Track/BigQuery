7d57a287e1d4181e2a372cad5c396b88acb6833e
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-12-17 10:24:33 -0800

    Auto-commit: Fri Dec 17 2021 10:24:29 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 28.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 28.sql
@@ -8,5 +8,7 @@
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE AY_Name = "AY 2020-21"
-    AND term_c IN ("Spring","Summer")
     AND AT_Record_Type_Name = "College/University Semester"
+    AND term_c = "Spring"
+    OR (term_c = "Summer"
+    AND credits_accumulated_c >0)
