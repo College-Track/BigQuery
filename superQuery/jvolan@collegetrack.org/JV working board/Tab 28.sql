418a0fbb42e109c2b777e55ca28454d056f815e3
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2021-12-17 10:39:43 -0800

    Auto-commit: Fri Dec 17 2021 10:39:39 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Tab 28.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Tab 28.sql
@@ -1,26 +1,9 @@
-WITH gather_ATs AS
-(
-    SELECT
+SELECT
     Contact_Id,
-    site,
-    indicator_graduated_or_on_track_at_c,
-    credits_accumulated_c,
-    term_c,
-    start_date_c,
+    site
     
     FROM `data-warehouse-289815.salesforce_clean.contact_at_template`
-    WHERE AY_Name = "AY 2020-21"
-    AND AT_Record_Type_Name = "College/University Semester"
-    AND term_c = "Spring"
-    OR (term_c = "Summer"
-    AND credits_accumulated_c >0)
-    AND indicator_completed_ct_hs_program_c = TRUE
+    WHERE AT_Record_Type_Name = "College/University Semester"
+    AND enrollment_status_c = "Approved Gap Year"
+    AND AT_Grade_c = "Year 1"
     AND high_school_graduating_class_c IN ("2020","2019","2018","2017","2016")
-)
-    SELECT
-    site,
-    Contact_Id,
-    Max(indicator_graduated_or_on_track_at_c) AS on_track_sp_summer_combine,
-
-    FROM gather_ATs
-    GROUP BY site, Contact_Id
