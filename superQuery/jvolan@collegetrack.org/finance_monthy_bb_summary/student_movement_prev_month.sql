8498d65bd1eae98303879fc73e4a643cfef2f1ed
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2022-01-04 09:27:13 -0800

    Auto-commit: Tue Jan 04 2022 09:27:08 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/student_movement_prev_month.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/student_movement_prev_month.sql
@@ -30,7 +30,15 @@ AS(
 status_history_prev_month_only AS
 (
     SELECT
-    *
+    contact_c,
+    name AS movement_type,
+    Academic_Semester_c,
+    academic_year_lu_c,
+    Start_Date_c,
+    Days_at_this_Stage_c,
+    extract(Month FROM Start_Date_c) AS month,
+    extract(Month FROM CURRENT_DATE()) AS current_month,
+    extract(month from date_sub(current_date,interval 1 Month)) AS prev_month, 
     
     FROM data-warehouse-289815.salesforce.contact_pipeline_history_c 
     WHERE     
