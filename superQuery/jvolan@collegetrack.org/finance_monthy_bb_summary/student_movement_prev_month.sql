592b266d3c6da5609318ae45e76d5eee5b30077c
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2022-01-04 09:34:35 -0800

    Auto-commit: Tue Jan 04 2022 09:34:30 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/student_movement_prev_month.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/student_movement_prev_month.sql
@@ -8,7 +8,7 @@ AS(
     FROM data-warehouse-289815.salesforce_clean.contact_template
     ), 
     
-/*status_history 
+status_history 
 AS( 
     SELECT 
     contact_c,
@@ -23,33 +23,16 @@ AS(
 
     FROM data-warehouse-289815.salesforce.contact_pipeline_history_c 
     WHERE name in ('Went on Leave of Absence','Became Inactive Post-Secondary','Dismissed/Left CT HS Program','Became CT Alumni')
-),
-
-*/
-
-status_history_prev_month_only AS
-(
-    SELECT
-    contact_c,
-    name AS movement_type,
-    Academic_Semester_c,
-    academic_year_lu_c,
-    Start_Date_c,
-    Days_at_this_Stage_c,
-    extract(Month FROM Start_Date_c) AS month,
-    extract(Month FROM CURRENT_DATE()) AS current_month,
-    extract(month from date_sub(current_date,interval 1 Month)) AS prev_month, 
-    
-    FROM data-warehouse-289815.salesforce.contact_pipeline_history_c 
-    WHERE     
-    extract(month from date_sub(current_date,interval 1 Month)) = extract(Month FROM Start_Date_c)
+    AND extract(Month FROM Start_Date_c) = extract(month from date_sub(current_date,interval 1 Month))
+    AND start_date_c + 365 >= CURRENT_DATE()
 )   
     SELECT 
-    status_history_prev_month_only.*, 
+    status_history.*, 
     ct_site.student, 
     ct_site.site_short, 
     ct_site.region_short, 
     ct_site.hs_class, 
     ct_site.college_track_status_name, 
     
-    FROM status_history_prev_month_only LEFT JOIN ct_site ON ct_site.contact_id = contact_c WHERE ct_site.site_short NOT IN ('Arlen')
+    FROM status_history 
+    LEFT JOIN ct_site ON ct_site.contact_id = contact_c WHERE ct_site.site_short NOT IN ('Arlen')
