3b927ca5834839e065462f9661085121682daf1d
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2022-01-03 16:50:48 -0800

    Auto-commit: Mon Jan 03 2022 16:50:44 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 6.sql
+++ superQuery/jvolan@collegetrack.org/finance_monthy_bb_summary/Tab 6.sql
@@ -1,15 +1,4 @@
-WITH ct_site 
-AS( 
-    SELECT contact_id, 
-    name AS student, 
-    HIGH_SCHOOL_GRADUATING_CLASS_c AS hs_class, 
-    site_short, region_short, college_track_status_name, 
-    
-    FROM data-warehouse-289815.salesforce_clean.contact_template ), 
-    
-status_history 
-AS( 
-    SELECT 
+SELECT 
     contact_c,
     name AS movement_type,
     Academic_Semester_c,
@@ -22,29 +11,3 @@ AS(
     
     FROM data-warehouse-289815.salesforce.contact_pipeline_history_c 
     WHERE name in ('Went on Leave of Absence','Became Inactive Post-Secondary','Dismissed/Left CT HS Program','Became CT Alumni')
-),
-
-status_history_prev_month_only AS
-(
-    SELECT
-    *
-    
-    FROM status_history
-    WHERE
-    (
-    (current_month = 1
-    AND prev_month = 12)
-    OR
-    (current_month != 1
-    AND month = prev_month)
-    )
-)   
-    SELECT 
-    status_history_prev_month_only.*, 
-    ct_site.student, 
-    ct_site.site_short, 
-    ct_site.region_short, 
-    ct_site.hs_class, 
-    ct_site.college_track_status_name, 
-    
-    FROM status_history_prev_month_only LEFT JOIN ct_site ON ct_site.contact_id = contact_c WHERE ct_site.site_short NOT IN ('Arlen')
