c55792604115a4012570e6e378966b245fa8b382
jvolan123 <38270713+jvolan123@users.noreply.github.com>
2022-01-05 14:56:47 -0800

    Auto-commit: Wed Jan 05 2022 14:56:38 GMT-0800 (Pacific Standard Time)

--- superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 CS sla prep.sql
+++ superQuery/jvolan@collegetrack.org/JV working board/Abdul FY22 CS sla prep.sql
@@ -1,7 +1,9 @@
 SELECT
     student_c,
     id AS bb_app_id,
-    total_service_earnings_c + total_ct_advised_earnings_c AS cs_1600_cap,
+    total_service_earnings_c,
+    total_ct_advised_earnings_c,
+    (total_service_earnings_c + total_ct_advised_earnings_c) AS cs_1600_cap,
 
     FROM `data-warehouse-289815.salesforce_clean.scholarship_application_clean`
     WHERE scholarship_application_record_type_name = "Bank Book"
