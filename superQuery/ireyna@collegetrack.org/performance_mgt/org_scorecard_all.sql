e66ee280121e8828969edb9f56662beed87d972f
Isabel Reyna <58746747+isabelreyna@users.noreply.github.com>
2022-01-10 15:17:10 -0800

    Auto-commit: Mon Jan 10 2022 15:17:03 GMT-0800 (Pacific Standard Time)

--- superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_all.sql
+++ superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_all.sql
@@ -1,24 +1,13 @@
 --Combine Org Scorecard outcomes for FY20-current year, unpivoted
 
-/*
+
 CREATE OR REPLACE TABLE `org-scorecard-286421.aggregate_data.org_scorecard_outcomes`
 OPTIONS
     (
-    description="This table pulls Org Scorecard outcomes, numerators (where applicable), denominator (where applicable)" 
+    description=
+    "This table pulls Org Scorecard outcomes, numerators (where applicable), denominator (where applicable). Query used to create table (12/3/21): https://github.com/College-Track/BigQuery/blob/ab0982ac7376611b3247e7854bcd0853d9dbe5da/superQuery/ireyna@collegetrack.org/performance_mgt/org_scorecard_all.sql. Updated on 1/10/2022 to include additional columns based on data enclosed (from query)"
     )
     AS 
-*/
 
-WITH
 
-unpivot_table AS (
-    SELECT * 
-
-     
-     FROM `org-scorecard-286421.transposed_tables.org_scorecard_overview_all_fy`
-
-    UNPIVOT INCLUDE NULLS  
-        (Outcome FOR site_or_region IN (EPA,OAK,SF,AUR,BH,SAC,WATTS,DEN,PGC,DC8,CREN,DC,CO,LA,NOLA_RG,NOLA,NORCAL,NATIONAL,NATIONAL_AS_LOCATION) --Create a "site_or_region" column 
-        ) AS UNPVt
-    )
-    SELECT * FROM unpivot_table
+SELECT * FROM `org-scorecard-286421.aggregate_data.org_scorecard_outcomes_all_fy_01_10_22`
